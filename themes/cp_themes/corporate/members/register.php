fe1283ad02af17006c4c54b708da4c62b6a3b930
Derek Allard <derek.allard@ellislab.com>
2009-12-08 15:01:29 +0000

    Changed the register member pages of Default, Fruit and Corporate themes so that error messages display more nicely. Spaces to tabs in those views

--- themes/cp_themes/corporate/members/register.php
+++ themes/cp_themes/corporate/members/register.php
@@ -13,107 +13,101 @@ if ($EE_view_disable !== TRUE)
 	<div class="contents">
 
 		<div class="heading"><h2><?=lang('register_member')?></h2></div>
-		
 		<div class="pageContents">
 
 			<?=form_open('C=members'.AMP.'M=new_member_form')?>
-            <?php 
-            $this->table->set_template($cp_table_template);
-            $this->table->set_heading(
-                array('data' => '&nbsp;', 'style' => 'width:50%;'),
-                '&nbsp;'
-            );
-            
-            // Username
-            $this->table->add_row(array(
-                    form_error('username').
-                    form_label(lang('username'), 'username'),
-                    form_input(array(
-                        'id'    => 'username',
-                        'name'  => 'username',
-                        'class' => 'fullfield',
-                        'value' => set_value('username')
-                        )
-                    )
-                )
-            );
-            
-            // Password
-            $this->table->add_row(array(
-                    form_error('password').
-                    form_label(lang('password'), 'password'),
-                    form_password(array(
-                        'id'    => 'password',
-                        'name'  => 'password',
-                        'class' => 'fullfield',
-                        'value' => set_value('password')
-                        )
-                    )
-                )
-            );
-            
-            // Password Confirm
-            $this->table->add_row(array(
-                    form_error('password_confirm').
-                    form_label(lang('password_confirm'), 'password_confirm'),
-                    form_password(array(
-                        'id'    => 'password_confirm',
-                        'name'  => 'password_confirm',
-                        'class' => 'fullfield',
-                        'value' => set_value('password_confirm')
-                        )
-                    )
-                )
-            );
-            
-            // Screen Name
-            $this->table->add_row(array(
-                    form_error('screen_name').
-                    form_label(lang('screen_name'), 'screen_name'),
-                    form_input(array(
-                        'id'    => 'screen_name',
-                        'name'  => 'screen_name',
-                        'class' => 'fullfield',
-                        'value' => set_value('screen_name')
-                        )
-                    )
-                )
-            );
-            
-            // Email
-            $this->table->add_row(array(
-                    form_error('email').
-                    form_label(lang('email'), 'email'),
-                    form_input(array(
-                        'id'    => 'email',
-                        'name'  => 'email',
-                        'class' => 'fullfield',
-                        'value' => set_value('email')
-                        )
-                    )
-                )
-            );
-            
-            
-            // Member Group Assignment
-            if ($this->cp->allowed_group('can_admin_mbr_groups'))
-            {
-                $this->table->add_row(array(
-                        form_error('group_id').
-                        form_label(lang('member_group_assignment'), 'group_id'),
-                        form_dropdown('group_id', $member_groups, set_value('group_id', 5), 'id="group_id"')
-                    )
-                );   
-            }
-            
-            echo $this->table->generate();
-            ?>
-				<p class="centerSubmit"><?=form_submit('members', lang('register_member'), 'class="submit"')?></p>
+			<?php
+			$this->table->set_template($cp_pad_table_template);
+			$this->table->set_heading(
+				array('data' => '&nbsp;', 'style' => 'width:50%;'),
+				'&nbsp;'
+			);
 
-				<?=form_close()?>	
-							
-				</div> <!-- pageContents -->
-		</div> <!-- contents -->
+			// Username
+			$this->table->add_row(array(
+					form_label(lang('username'), 'username').NBS.form_error('username'),
+					form_input(array(
+						'id'	=> 'username',
+						'name'	=> 'username',
+						'class' => 'field',
+						'value' => set_value('username')
+						)
+					)
+				)
+			);
+
+			// Password
+			$this->table->add_row(array(
+					form_label(lang('password'), 'password').NBS.form_error('password'),
+					form_password(array(
+						'id'	=> 'password',
+						'name'	=> 'password',
+						'class' => 'field',
+						'value' => set_value('password')
+						)
+					)
+				)
+			);
+
+			// Password Confirm
+			$this->table->add_row(array(
+					form_label(lang('password_confirm'), 'password_confirm').NBS.form_error('password_confirm'),
+					form_password(array(
+						'id'	=> 'password_confirm',
+						'name'	=> 'password_confirm',
+						'class' => 'field',
+						'value' => set_value('password_confirm')
+						)
+					)
+				)
+			);
+
+			// Screen Name
+			$this->table->add_row(array(
+					form_label(lang('screen_name'), 'screen_name').NBS.form_error('screen_name'),
+					form_input(array(
+						'id'	=> 'screen_name',
+						'name'	=> 'screen_name',
+						'class' => 'field',
+						'value' => set_value('screen_name')
+						)
+					)
+				)
+			);
+
+			// Email
+			$this->table->add_row(array(
+					form_label(lang('email'), 'email').NBS.form_error('email'),
+					form_input(array(
+						'id'	=> 'email',
+						'name'	=> 'email',
+						'class' => 'field',
+						'value' => set_value('email')
+						)
+					)
+				)
+			);
+
+
+			// Member Group Assignment
+			if ($this->cp->allowed_group('can_admin_mbr_groups'))
+			{
+				$this->table->add_row(array(
+						form_label(lang('member_group_assignment'), 'group_id').NBS.form_error('group_id'),
+						form_dropdown('group_id', $member_groups, set_value('group_id', 5), 'id="group_id"')
+					)
+				);
+			}
+
+			echo $this->table->generate();
+			?>
+				<p><?=form_submit('members', lang('register_member'), 'class="submit"')?></p>
+
+				<?=form_close()?>
+
+		</div>
+
+	</div> <!-- contents -->
 </div> <!-- mainContent -->
 
 <?php
