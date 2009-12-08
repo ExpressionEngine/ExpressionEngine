5d1f09113c1daf398a0617a72b67b51e23774b61
Pascal Kriete <pascal.kriete@ellislab.com>
2009-12-08 11:44:10 +0000

    removing a comment

--- system/expressionengine/fieldtypes/ft.select.php
+++ system/expressionengine/fieldtypes/ft.select.php
@@ -82,29 +82,6 @@ class Select_ft extends EE_Fieldtype {
 		
 		$this->EE->load->helper('custom_field_helper');
 		create_multi_select_thing($data);
-		
-		/*
-		
-		$this->EE->table->add_row(
-			'<p class="field_format_option select_format">'.
-				form_radio('field_pre_populate', 'n', $data['field_pre_populate_n'], 'id="field_pre_populate_n"').
-				lang('field_populate_manually', 'field_pre_populate_n').BR.
-				form_radio('field_pre_populate', 'y', $data['field_pre_populate_y'], 'id="field_pre_populate_y"').
-				lang('field_populate_from_channel', 'field_pre_populate_y').
-			'</p>',
-			'<p class="field_format_option select_format_n">'.
-				lang('field_list_items', 'select_list_items').
-				lang('multi_list_items', 'multi_select_list_items').BR.
-				lang('field_list_instructions').BR.
-				form_textarea(array('id'=>'field_list_items','name'=>'field_list_items', 'rows'=>10, 'cols'=>50, 'value'=>$data['field_list_items'])).
-			'</p>
-			<p class="field_format_option select_format_y">'.
-				lang('select_channel_for_field', 'field_pre_populate_id').
-				form_dropdown('field_pre_populate_id', $data['field_pre_populate_id_options'], $data['field_pre_populate_id_select'], 'id="field_pre_populate_id"').
-			'</p>'
-		);
-		
-		*/
 	}
 
 	// --------------------------------------------------------------------
