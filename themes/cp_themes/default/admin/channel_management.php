2212777a53153b71d00d3275ec8a7a8b7998e52d
Greg Aker <greg.aker@ellislab.com>
2009-12-23 19:45:34 +0000

    http://expressionengine.com/bug_tracker/bug/9798/
    replaced delete text with icon

--- themes/cp_themes/default/admin/channel_management.php
+++ themes/cp_themes/default/admin/channel_management.php
@@ -37,7 +37,7 @@ if ($EE_view_disable !== TRUE)
 											$channel->channel_name,
 											'<a href="'.BASE.AMP.'C=admin_content'.AMP.'M=channel_edit'.AMP.'channel_id='.$channel->channel_id.'">'.lang('edit_preferences').'</a>',
 											'<a href="'.BASE.AMP.'C=admin_content'.AMP.'M=channel_edit_group_assignments'.AMP.'channel_id='.$channel->channel_id.'">'.lang('edit_groups').'</a>',
-											'<a href="'.BASE.AMP.'C=admin_content'.AMP.'M=channel_delete_confirm'.AMP.'channel_id='.$channel->channel_id.'">'.lang('delete').'</a>'
+											'<a href="'.BASE.AMP.'C=admin_content'.AMP.'M=channel_delete_confirm'.AMP.'channel_id='.$channel->channel_id.'" title="'.lang('delete').'"><img src="'.$cp_theme_url.'images/icon-delete.png" alt="'.lang('delete').'" width="10" height="13" /></a>'
 										);
 				}
 
