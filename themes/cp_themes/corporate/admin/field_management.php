d2e0ef4658ba1948b927d1de696e777734b07cc1
Greg Aker <greg.aker@ellislab.com>
2009-12-23 19:54:19 +0000

    Fixing an unclosed div tag on corporate theme add/edit fields page.
    http://expressionengine.com/bug_tracker/bug/10427/

--- themes/cp_themes/corporate/admin/field_management.php
+++ themes/cp_themes/corporate/admin/field_management.php
@@ -12,7 +12,7 @@ if ($EE_view_disable !== TRUE)
 	<?php $this->load->view('_shared/right_nav')?>
 	<div class="contents">
 
-		<div class="heading"><h2><?=$cp_page_title?></h2>
+		<div class="heading"><h2><?=$cp_page_title?></h2></div>
         
 		<div class="pageContents">
 			<h3><?=lang('field_group').': '.$group_name?></h3>
