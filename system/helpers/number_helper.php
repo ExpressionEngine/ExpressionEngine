a12e3f9c3fe750a7950ac8307db37be13b84e81e
Derek Allard <derek.allard@ellislab.com>
2010-01-17 16:09:39 +0000

    optional precision argument in byte_format()

--- system/helpers/number_helper.php
+++ system/helpers/number_helper.php
@@ -43,22 +43,22 @@ if ( ! function_exists('byte_format'))
 	
 		if ($num >= 1000000000000) 
 		{
-			$num = round($num / 1099511627776, 1);
+			$num = round($num / 1099511627776, $precision);
 			$unit = $CI->lang->line('terabyte_abbr');
 		}
 		elseif ($num >= 1000000000) 
 		{
-			$num = round($num / 1073741824, 1);
+			$num = round($num / 1073741824, $precision);
 			$unit = $CI->lang->line('gigabyte_abbr');
 		}
 		elseif ($num >= 1000000) 
 		{
-			$num = round($num / 1048576, 1);
+			$num = round($num / 1048576, $precision);
 			$unit = $CI->lang->line('megabyte_abbr');
 		}
 		elseif ($num >= 1000) 
 		{
-			$num = round($num / 1024, 1);
+			$num = round($num / 1024, $precision);
 			$unit = $CI->lang->line('kilobyte_abbr');
 		}
 		else
