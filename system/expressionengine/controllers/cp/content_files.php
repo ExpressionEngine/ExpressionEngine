2ecb8e21ef06315fa8977e950c02fdac5b32a11c
Greg Aker <greg.aker@ellislab.com>
2010-02-11 18:24:06 +0000

    Cleaning foreign characters from uploaded file names
    http://expressionengine.com/bug_tracker/bug/10862/

--- system/expressionengine/controllers/cp/content_files.php
+++ system/expressionengine/controllers/cp/content_files.php
@@ -314,10 +314,10 @@ class Content_files extends Controller {
 		$upload_dir_result = $this->tools_model->get_upload_preferences($this->session->userdata('member_group'), $upload_id);
 		$upload_dir_prefs = $upload_dir_result->row();
 		
-		// $filename = $this->input->get_post('userfile');
-		// var_dump($_POST);
-		// $filename = substr($filename, 0, strrpos($filename, '.'));
-		// var_dump(url_title($filename, $this->config->item('word_separator'), TRUE)); exit;
+		$filename = $_FILES['userfile']['name'];
+
+		$filename = substr($filename, 0, strrpos($filename, '.'));
+
 		switch($upload_dir_prefs->allowed_types)
 		{
 			case 'all' : $config['allowed_types'] = '*';
@@ -328,7 +328,7 @@ class Content_files extends Controller {
 				$config['allowed_types'] = $upload_dir_prefs->allowed_types;
 		}
 
-		// $config['file_name'] = url_title($filename, $this->config->item('word_separator'), TRUE);
+		$config['file_name'] = url_title($filename, $this->config->item('word_separator'), TRUE);
 		$config['upload_path'] = $upload_dir_prefs->server_path;
 		$config['max_size']	= $upload_dir_prefs->max_size;
 		$config['max_width']  = $upload_dir_prefs->max_width;
