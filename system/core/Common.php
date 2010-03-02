ee4f5e1ceac30c02748bf57fbe8ec726ffdbdc8d
Derek Jones <derek.jones@ellislab.com>
2010-03-02 13:13:47 -0600

    more comprehensive is_really_writable() - avoids performance hit on Unix servers

--- system/core/Common.php
+++ system/core/Common.php
@@ -66,6 +66,14 @@
  */	
 	function is_really_writable($file)
 	{
+		// If we're on a Unix server with safe_mode off we call is_writable
+		if (DIRECTORY_SEPARATOR == '/' AND @ini_get("safe_mode") == FALSE)
+		{
+			return is_writable($file);
+		}
+
+		// For windows servers and safe_mode "on" installations we'll actually
+		// write a file then read it.  Bah...
 		if (is_dir($file))
 		{
 			$file = rtrim($file, '/').'/'.md5(mt_rand(1,100).mt_rand(1,100));
