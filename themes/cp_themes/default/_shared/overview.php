18f4d1970fd979790d1e5d22bca8306b75bc0fac
Greg Aker <greg.aker@ellislab.com>
2010-03-08 16:27:09 -0600

    Updating overview view files to use cp->masked_url() for the help links

--- themes/cp_themes/default/_shared/overview.php
+++ themes/cp_themes/default/_shared/overview.php
@@ -39,11 +39,11 @@ if ($EE_view_disable !== TRUE)
 					<?php if ($this->input->get('C') == 'content' && $menu_key == 'publish'):?>
 						<td class="overviewItemName"><a href="<?=$menu_value?>"><?=lang($menu_key)?></a></td>
 						<td class="overviewItemDesc"></td>
-						<td class="overviewItemHelp"><a rel="external" href="<?=$this->config->item('base_url').$this->config->item('index_page').'?URL='.$this->menu->generate_help_link($controller, $menu_key, $controller)?>"><img src="<?=$cp_theme_url?>images/external_link.png"/></a></td>
+						<td class="overviewItemHelp"><a rel="external" href="<?=$this->cp->masked_url($this->menu->generate_help_link($controller, $menu_key, $controller))?>"><img src="<?=$cp_theme_url?>images/external_link.png"/></a></td>
 					<?php else:?>
 						<td class="overviewItemName"><a href="<?=$menu_value?>"><?=lang('nav_'.$menu_key)?></a></td>
 						<td class="overviewItemDesc"><?=lang('nav_'.$menu_key.'_short_desc')?></td>
-						<td class="overviewItemHelp"><a rel="external" href="<?=$this->config->item('base_url').$this->config->item('index_page').'?URL='.$this->menu->generate_help_link($controller, $menu_key, $controller)?>"><img src="<?=$cp_theme_url?>images/external_link.png"/></a></td>
+						<td class="overviewItemHelp"><a rel="external" href="<?=$this->cp->masked_url($this->menu->generate_help_link($controller, $menu_key, $controller))?>"><img src="<?=$cp_theme_url?>images/external_link.png"/></a></td>
 					<?php endif;?>
 					</tr>
 			</table>
@@ -64,11 +64,11 @@ if ($EE_view_disable !== TRUE)
 						<?php if ($this->input->get('C') == 'content' && $menu_key == 'publish'):?>
 							<td class="overviewItemName"><a href="<?=$item_value?>"><?=lang($item_key)?></a></td>
 							<td class="overviewItemDesc">&nbsp;</td>
-							<td class="overviewItemHelp"><a rel="external" href="<?=$this->config->item('base_url').$this->config->item('index_page').'?URL='.$this->menu->generate_help_link($controller, $item_key, $controller)?>"><img src="<?=$cp_theme_url?>images/external_link.png"/></a></td>
+							<td class="overviewItemHelp"><a rel="external" href="<?=$this->cp->masked_url($this->menu->generate_help_link($controller, $item_key, $controller))?>"><img src="<?=$cp_theme_url?>images/external_link.png"/></a></td>
 						<?php else:?>
 							<td class="overviewItemName"><a href="<?=$item_value?>"><?=lang('nav_'.$item_key)?></a></td>
 							<td class="overviewItemDesc"><?=lang('nav_'.$item_key.'_short_desc')?></td>
-							<td class="overviewItemHelp"><a rel="external" href="<?=$this->config->item('base_url').$this->config->item('index_page').'?URL='.$this->menu->generate_help_link($controller, $item_key, $controller)?>"><img src="<?=$cp_theme_url?>images/external_link.png"/></a></td>							
+							<td class="overviewItemHelp"><a rel="external" href="<?=$this->cp->masked_url($this->menu->generate_help_link($controller, $item_key, $controller))?>"><img src="<?=$cp_theme_url?>images/external_link.png"/></a></td>							
 						<?php endif;?>
 						</tr>
 					<?php 
