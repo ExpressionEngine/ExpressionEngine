ed5441140a54635391739d6c5e331a30787bc59c
Robin Sowell <robin.sowell@ellislab.com>
2010-03-25 15:08:36 -0400

    Added db changes to transform site_pages array to the 202 update file.  Still needs testing.

--- system/expressionengine/installer/updates/ud_202.php
+++ system/expressionengine/installer/updates/ud_202.php
@@ -64,15 +64,60 @@ class Updater {
 		// If the action id is for the Weblog class, change it
 		$Q[] = "UPDATE exp_actions SET class = 'Channel' WHERE class = 'Weblog'";
 		
+		// If they have existing Pages, saved array needs to be updated to new format
+        if ($this->EE->db->field_exists('site_pages', 'sites'))
+		{
+			$this->EE->db->select('site_id, site_pages, site_system_preferences');
+        	$this->EE->db->where('site_pages !=', '');
+        	$query = $this->EE->db->get('sites');
+
+        	if ($query->num_rows() == 0)
+        	{
+				foreach ($query->result_array() as $row)
+				{
+					$system_prefs =  base64_decode($row['site_system_preferences']);
+					$old_pages = base64_decode($row['site_pages']);
+
+					if ( ! is_string($old_pages) OR substr($old_pages, 0, 2) != 'a:')
+					{
+						// Error or will lose data.  Which is borked!
+						continue;
+
+					}
+					else
+					{
+						$new_pages[$row['site_id']] = unserialize($old_pages);
+						
+					}
+					
+					if ( ! is_string($system_prefs) OR substr($system_prefs, 0, 2) != 'a:')
+					{
+						$new_pages[$row['site_id']]['url'] = '';
+					}
+					else
+					{
+						$prefs = unserialize($system_prefs);
+						
+						$url = (isset($prefs['site_url'])) ? $prefs['site_url'].'/' : '/';
+						$url .= (isset($prefs['site_index'])) ? $prefs['site_index'].'/' : '/';
+
+						$new_pages[$row['site_id']]['url'] = preg_replace("#(^|[^:])//+#", "\\1/", $url);						
+					}
+					
+					$Q[] = "UPDATE exp_sites SET site_pages = '".base64_encode(serialize($new_pages))."' WHERE site_id = '".$row['site_id']."'";
+
+				}
+			}
+
+
 		foreach ($Q as $num => $sql)
 		{
 			$this->EE->progress->update_state("Running Query $num of $count");
 	        $this->EE->db->query($sql);
 		}
-		
+	
 		// Finished!
         return TRUE;
-
     }
 }   
 /* END CLASS */
