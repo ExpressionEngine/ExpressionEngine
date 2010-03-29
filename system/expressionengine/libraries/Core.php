2e58fe7d85da43aa20a56858cfe94800de6c3007
Greg Aker <greg.aker@ellislab.com>
2010-03-29 09:40:43 -0500

    PHP error in core.php when MSM is enabled due to count() not isset() when looking at $uris

--- system/expressionengine/libraries/Core.php
+++ system/expressionengine/libraries/Core.php
@@ -587,7 +587,7 @@ class EE_Core {
 				
 				$match_uri = ($this->EE->uri->uri_string == '') ? '/' : '/'.trim($this->EE->uri->uri_string, '/').'/';
 				
-				if ($pages !== FALSE && count($pages[$this->EE->config->item('site_id')]['uris']) > 0 && ($entry_id = array_search($match_uri, $pages[$this->EE->config->item('site_id')]['uris'])) !== FALSE)
+				if ($pages !== FALSE && isset($pages[$this->EE->config->item('site_id')]['uris']) && ($entry_id = array_search($match_uri, $pages[$this->EE->config->item('site_id')]['uris'])) !== FALSE)
 				{
 					$query = $this->EE->db->query("SELECT t.template_name, tg.group_name
 										 FROM exp_templates t, exp_template_groups tg 
