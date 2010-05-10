a7faca2826ab7abd2265e7cfdd19a1cf0923c9e6
Robin Sowell <robin.sowell@ellislab.com>
2010-05-10 11:07:08 -0400

    Adding fieldtypes language file.

---
+++ system/expressionengine/language/english/lang.fieldtypes.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang = array(
+
+'add_file'	=>
+'Add File',
+
+'remove_file' =>
+'Remove File',
+
+// IGNORE
+''=>'');
+/* End of file lang.fieldtypes.php */
+/* Location: ./system/expressionengine/language/english/lang.fieldtypes.php */
