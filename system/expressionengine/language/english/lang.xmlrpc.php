661a597b37839710fd728c7df0cad6cbf6b222bd
Robin Sowell <robin.sowell@ellislab.com>
2010-05-11 11:05:28 -0400

    Ripping todos- had to add a language file for xmlrpc no license message.

---
+++ system/expressionengine/language/english/lang.xmlrpc.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang = array(
+
+'invalid_license' =>
+'Invalid License',
+
+''=>''
+);
+
+/* End of file lang.xmlrpc.php */
+/* Location: ./system/expressionengine/language/english/lang.xmlrpc.php */
