848cbef8957edf3c05d054cca625f9e76dac8b0e
Derek Jones <derek.jones@ellislab.com>
2010-06-23 13:14:54 -0500

    Fixed a bug where any/all searches were not ignoring "stopwords" Fixed a bug where keywords being used were not the sanitized ones (yikes!) Optimized any/all searches, i.e. LIKE searches by: Eliminating empty and non-unique terms Limiting to 32 words Lightened forum search queries by changing query limit from 500 to 250

--- system/expressionengine/modules/forum/mod.forum.php
+++ system/expressionengine/modules/forum/mod.forum.php
@@ -30,7 +30,7 @@ class Forum {
 	var $version			= '3.1.0';
 	var $build				= '20100415';
 	var $use_site_profile	= FALSE;
-	var $search_limit		= 500; // Maximum number of search results
+	var $search_limit		= 250; // Maximum number of search results (x2 since it can include this number of topics + this number of posts)
 	var $return_data 		= '';
 	var $body_extra			= '';
 	var $theme				= '';
