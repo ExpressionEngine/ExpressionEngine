f5ba75f8d8bcccefb13945acfc40759403c523ca
Greg Aker <greg.aker@ellislab.com>
2010-07-06 16:50:13 -0500

    commit to compress files

---
+++ system/expressionengine/javascript/src/jquery/plugins/ee_navigation.js
@@ -0,0 +1,192 @@
+/*!
+ * ExpressionEngine - by EllisLab
+ *
+ * @package		ExpressionEngine
+ * @author		ExpressionEngine Dev Team
+ * @copyright	Copyright (c) 2003 - 2010, EllisLab, Inc.
+ * @license		http://expressionengine.com/docs/license.html
+ * @link		http://expressionengine.com
+ * @since		Version 2.0
+ * @filesource
+ */
+
+// ------------------------------------------------------------------------
+
+/*!
+ * ExpressionEngine Navigation Plugin
+ *
+ * @package		ExpressionEngine
+ * @subpackage	Control Panel
+ * @category	Control Panel
+ * @author		ExpressionEngine Dev Team
+ * @link		http://expressionengine.com
+ */
+
+jQuery(document).ready(function() {
+
+var $ = jQuery;
+
+// Common strings to improve compressability
+var ACTIVE = "active",
+	HOVER = "hover",
+	NAV = "#navigationTabs",
+	TICTAC = "first_level",
+	PARENT = "parent",
+
+	nav = $(NAV),
+	top_level = $(NAV+">li."+PARENT),
+	t, current_hovered, moving = false;
+
+
+// Mouse navigation
+// -----------------------------------------------
+
+function delay_show_next() {
+
+	if ( ! current_hovered) {
+		var el = $(current_hovered);
+		el.parent().find('.'+ACTIVE+', .'+HOVER).removeClass(ACTIVE).removeClass(HOVER);
+		return el.addClass(ACTIVE).addClass(HOVER);
+	}
+	
+	window.clearTimeout(t);
+	moving = true;
+
+	t = window.setTimeout(function() {
+		var el = $(current_hovered);
+		el.parent().find('.'+ACTIVE+', .'+HOVER).removeClass(ACTIVE).removeClass(HOVER);
+		el.addClass(ACTIVE).addClass(HOVER);
+		
+		moving = false;
+	}, 60);	// remember, IE timeouts step in 15ms
+}
+
+// Mouse leaves nav - close all
+nav.mouseleave(function() {
+	nav.find('.'+ACTIVE).removeClass(ACTIVE);
+});
+
+// Move along the main menu - each should open in turn
+top_level.mouseenter(function() {
+	if (nav.find('.'+ACTIVE).length) {
+		nav.find('.'+ACTIVE).removeClass(ACTIVE);
+		$(this).addClass(ACTIVE);
+	}
+});
+
+// Toggle menu open / closed
+top_level.find("a."+TICTAC).click(function() {
+	var el = $(this).parent();
+	
+	if (el.hasClass(ACTIVE)) {
+		el.removeClass(ACTIVE);
+	}
+	else {
+		el.addClass(ACTIVE);
+	}
+	return false;
+});
+
+// Small delay in showing or hiding submenus to make mouse navigation smoother
+top_level.find("ul li").hover(function() {
+	current_hovered = this;
+	
+	if ( ! moving) {
+		delay_show_next();
+	}
+}, function() {
+	$(this).removeClass(HOVER);
+}).find('.'+PARENT+'>a').click(function() {
+	return false;
+});
+
+
+// Keyboard navigation
+// -----------------------------------------------
+
+function move_top_level(obj, current_li, direction) {
+	
+	current_li.parents("."+ACTIVE).removeClass(ACTIVE);
+	current_li = current_li.closest(NAV+">li");
+	
+	if (direction && current_li[direction]().length) {
+		obj.setFocus(current_li[direction]().children("a"));
+	}
+}
+
+nav.ee_focus("a."+TICTAC, {
+	removeTabs: "a",	
+	onEnter: function(event) {
+		var target = $(event.target),
+			li = target.parent();
+
+		if (li.hasClass(PARENT)) {
+			li.addClass(ACTIVE);
+			this.setFocus(li.find("ul>li>a").eq(0));
+		}
+	},
+	onRight: function(event) {
+		var target = $(event.target),
+			li = target.parent();
+
+		if (li.hasClass(PARENT) && ! target.hasClass(TICTAC)) {
+			li.addClass(ACTIVE);
+			this.setFocus(li.find("ul>li>a").eq(0));
+		}
+		else {
+			move_top_level(this, li, "next");
+		}
+	},
+	onLeft: function(event) {
+		var target = $(event.target),
+			li = target.parent();
+		
+		if (target.hasClass(TICTAC) && li.prev().length) {
+			this.setFocus(li.prev().children("a"));
+		}
+		else {
+			li = li.parent().closest("."+PARENT);
+			li.removeClass(ACTIVE);
+			
+			if (li.children("a."+TICTAC).length) {
+				move_top_level(this, li, "prev");
+			}
+			else {
+				this.setFocus(li.children("a").eq(0));
+			}
+		}
+	},
+	onUp: function(event) {
+		var target = $(event.target),
+			li = target.parent(),
+			prev = li.prevAll(":not(.nav_divider)");
+		
+		if ( ! target.hasClass(TICTAC) && li.prev.length) {
+			this.setFocus(prev.eq(0).children("a"));
+		}
+	},
+	onDown: function(event) {
+		var target = $(event.target),
+			li = target.parent(),
+			next = li.nextAll(":not(.nav_divider)");
+
+		if ( ! target.hasClass(TICTAC) && next.length) {
+			this.setFocus(next.eq(0).children("a"));
+		}
+		else if (li.hasClass(PARENT)) {
+			li.addClass(ACTIVE);
+			this.setFocus(li.find("ul>li>a").eq(0));
+		}
+	},
+	onEscape: function(event) {
+		var target = $(event.target),
+			li = target.parent();
+		
+		move_top_level(this, li);
+	},
+	onBlur: function(event) {
+		this.getElements().parent.find('.'+ACTIVE).removeClass(ACTIVE);
+	}
+});
+
+});
---
+++ system/expressionengine/javascript/src/jquery/plugins/ee_notice.js
@@ -0,0 +1,253 @@
+/*!
+ * ExpressionEngine - by EllisLab
+ *
+ * @package		ExpressionEngine
+ * @author		ExpressionEngine Dev Team
+ * @copyright	Copyright (c) 2003 - 2010, EllisLab, Inc.
+ * @license		http://expressionengine.com/docs/license.html
+ * @link		http://expressionengine.com
+ * @since		Version 2.0
+ * @filesource
+ */
+
+// ------------------------------------------------------------------------
+
+/*!
+ * ExpressionEngine JS Notification Plugin
+ *
+ * @package		ExpressionEngine
+ * @subpackage	Control Panel
+ * @category	Control Panel
+ * @author		ExpressionEngine Dev Team
+ * @link		http://expressionengine.com
+ */
+
+/* Usage Notes:
+ *
+ * $.ee_notice(message, options);
+ *
+ * message			- the text/html to show (required)
+ *					- can also be an array of objects: [{message:"text"}, {message:"another", type:"error"}]
+ *
+ * Options:
+ * type				- message severity, options are: notice | success | error (default = notice)
+ * open				- open drawer automatically | bool (default = false [except for errors])
+ * close_on_click   - close the drawer on click. | bool (default = true)
+ *
+ * Force Hide:
+ * $.ee_notice.destroy()
+ */
+(function($) {
+	
+	var type_store = {},
+		info_callback = function() {},	// @todo move to $.noop in 1.4
+		store_ref, drawer, options, active;
+
+	function _init() {
+		var type_counts = $("#notice_counts");
+		
+		// Set plugin globals
+		type_store = {
+			"error"	  : { count: 0,  last: "",  counter: type_counts.find(".notice_error").get(0) },
+			"alert"	  : { count: 0,  last: "",  counter: type_counts.find(".notice_alert").get(0) },
+			"success" : { count: 0,  last: "",  counter: type_counts.find(".notice_success").get(0) }
+		};
+		
+		drawer = $("#notice_texts_container");
+		
+		// Set up events
+		type_counts.find('span').click(count_click_handler);
+	}
+	
+	$.ee_notice = function(message, params) {
+		if ( ! drawer) {
+			_init();
+		}
+				
+		if ($.isArray(message)) {
+			$.each(message ,function(k, v) {
+				$.ee_notice(v.message, v);
+			});
+			return;
+		}
+		
+		options = $.extend({
+			type: 'notice',
+			open: false,
+			close_on_click: true
+		}, params);
+		
+		// Ha! Well done, Pascal
+		if (options.type == 'notice') {
+			options.type = 'alert';
+		}
+		
+		store_ref = type_store[options.type];
+
+		if (store_ref) {
+			
+			increment_type_count();
+			
+			var notice_list = $(".notice_texts.notice_" + options.type);
+			
+			if (store_ref.last == message) {
+				var last_message = notice_list.children().slice(-1),
+					subcount = last_message.find('.subcount');
+				
+				if ( ! subcount.length) {
+					last_message.prepend('<span class="subcount">'+2+'</span>');
+				}
+				else {
+					subcount.text(parseInt(subcount.text()) + 1);
+				}
+			}
+			else {
+				notice_list.append("<p>" + message + "</p>");
+				store_ref.last = message;
+			}
+		}
+		else if (options.type == 'custom') {
+			$(".notice_texts.notice_custom").html(message);
+		}
+		else {
+			throw "Invalid notification type.";
+		}
+		
+		if (options.type == 'error' || options.open) {
+			open_notice_drawer(options.type);
+		}
+		
+		return $.ee_notice;
+	}
+	
+	$.ee_notice.destroy = function() {
+		if (drawer) {
+			close_notice_drawer();
+		}
+	}
+	
+	$.ee_notice.show_info = function(click_callback) {
+		if ( ! drawer) {
+			_init();
+		}
+		
+		$("#notice_flag").css("display", "inline");
+		$('.notice_info').show();
+		info_callback = click_callback;
+		
+		repaint_flag();
+	}
+	
+	$.ee_notice.hide_info = function() {
+		$('.notice_info').hide();
+		var total = 0;
+		
+		$.each(type_store, function(k, v) {
+			total += v.count;
+		});
+		
+		if ( ! total) {
+			$("#notice_flag").hide();
+		}
+	}
+	
+	function repaint_flag() {
+		if ( ! $.browser == 'safari') {
+			return;
+		}
+		setTimeout(function() {
+			window.scrollBy(0, 1);
+			window.scrollBy(0, -1);	
+		}, 15);
+	}
+	
+	function increment_type_count() {
+		$("#notice_flag").css("display", "inline");
+		set_type_count(options.type, store_ref.count + 1);
+	}
+	
+	function set_type_count(type, count) {
+		if ( ! type_store[type]) {
+		 	return;
+		}
+		
+		type_store[type].count = count;
+		
+		var tc = type_store[type].counter;
+		
+		if (tc.lastChild.nodeType == 3) {
+			tc.removeChild(tc.lastChild);
+		}
+		
+		if (count == 0) {
+			type_store[type].last = '';
+			$(tc).hide();
+		}
+		else {
+			tc.innerHTML += '&nbsp;&nbsp;' + count;
+			$(tc).show();
+			
+			repaint_flag();
+		}
+	}
+	
+	function open_notice_drawer(type) {
+		if (active != type) {
+			drawer.find('.notice_texts').hide().end().find('.notice_'+type).show();
+			drawer.slideDown('fast', repaint_flag);
+			active = type;
+			
+			if (store_ref) {
+				set_active(store_ref.counter);
+			}
+		}
+		
+		if (options.close_on_click) {
+    		if ( ! drawer.data('close_bound')) {
+    			drawer.data('close_bound', true);
+
+    			drawer.click(function() {
+    				drawer.one('mouseout', close_notice_drawer);
+    			});
+    		}		    
+		}
+	}
+	
+	function close_notice_drawer() {
+		drawer.slideUp('fast', function() {
+			drawer.find('.notice_texts').html('');
+			$.each(type_store, function(k, v) {
+				set_type_count(k, 0);
+			});
+			
+			$("#notice_flag").hide();
+			$("#active_notice").attr("id", "");
+			
+			active = false;
+		});
+	}
+	
+	function count_click_handler() {
+		var type = this.className.substr(7);
+		
+		if (type == 'info') {
+			return info_callback();
+		}
+		
+		open_notice_drawer(type);
+		set_active(this);
+
+		return false;
+	}
+	
+	function set_active(el) {
+		$("#active_notice").attr("id", "");
+		el.id = "active_notice";
+	}
+	
+})(jQuery);
+
+
+
+/* End of file ee_notice.js */
+/* Location: ./system/expressionengine/javascript/jquery/plugins/ee_notice.js */
