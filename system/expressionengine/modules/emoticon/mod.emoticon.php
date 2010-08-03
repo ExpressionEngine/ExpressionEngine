bb861b59263fa41ec87815c5ec3e1dbba1859a3a
Greg Aker <greg.aker@ellislab.com>
2010-08-03 14:22:28 -0500

    Fixing http://expressionengine.com/bug_tracker/bug/13436/
    Also removing unused methods from the smileys module.

--- system/expressionengine/modules/emoticon/mod.emoticon.php
+++ system/expressionengine/modules/emoticon/mod.emoticon.php
@@ -1,30 +1,27 @@
-<?php
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * ExpressionEngine - by EllisLab
+ *
+ * @package		ExpressionEngine
+ * @author		ExpressionEngine Dev Team
+ * @copyright	Copyright (c) 2003 - 2010, EllisLab, Inc.
+ * @license		http://expressionengine.com/user_guide/license.html
+ * @link		http://expressionengine.com
+ * @since		Version 2.0
+ * @filesource
+ */
 
-/*
-=====================================================
- ExpressionEngine - by EllisLab
------------------------------------------------------
- http://expressionengine.com/
------------------------------------------------------
- Copyright (c) 2003 - 2010, EllisLab, Inc.
-=====================================================
- THIS IS COPYRIGHTED SOFTWARE
- PLEASE READ THE LICENSE AGREEMENT
- http://expressionengine.com/user_guide/license.html
-=====================================================
- File: core.emoticon.php
------------------------------------------------------
- Purpose: Emoticon (smiley) class
-=====================================================
-*/
+// ------------------------------------------------------------------------
 
-if ( ! defined('EXT'))
-{
-	exit('Invalid file request');
-}
-
-
-
+/**
+ * ExpressionEngine Emotion Module
+ *
+ * @package		ExpressionEngine
+ * @subpackage	Modules
+ * @category	Modules
+ * @author		ExpressionEngine Dev Team
+ * @link		http://expressionengine.com
+ */
 class Emoticon {
 
 	var $smileys	 = FALSE;
