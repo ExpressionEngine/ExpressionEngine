f03f8889248804e07634e980094726bb24f929c3
Robin Sowell <robin.sowell@ellislab.com>
2010-08-31 14:12:07 -0400

    bleh- needed to do that check for null data in the edit/update as well.

--- system/expressionengine/libraries/api/Api_channel_entries.php
+++ system/expressionengine/libraries/api/Api_channel_entries.php
@@ -1920,6 +1920,28 @@ class Api_channel_entries extends Api {
 			}
 			else
 			{
+				
+				// Check that data complies with mysql strict mode rules
+				$all_fields = $this->EE->db->field_data('channel_data');
+
+				foreach ($all_fields as $field)
+				{
+					if (strncmp($field->name, 'field_id_', 9) == 0)
+					{
+						if ($field->type == 'text' OR $field->type == 'blob')
+						{
+							if ( ! isset($cust_fields[$field->name]) OR is_null($cust_fields[$field->name]))
+							{
+								$cust_fields[$field->name] = '';
+							}
+						}
+						elseif ($field->type == 'int' && isset($cust_fields[$field->name]) && $cust_fields[$field->name] == '')
+						{
+							unset($cust_fields[$field->name]);
+						}
+					}
+				} 		
+				
 				$this->EE->db->where('entry_id', $this->entry_id);
 				$this->EE->db->update('channel_data', $cust_fields);
 			}
