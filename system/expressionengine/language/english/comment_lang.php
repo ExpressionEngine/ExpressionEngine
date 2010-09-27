d7e23b48ae4e8e944b17de64641bd36f1bafe91a
Derek Jones <derek.jones@ellislab.com>
2010-09-27 10:41:55 -0500

    tweaked Comment Edit Time Limit language variable a bit (to include that setting to 0 is no limit)

--- system/expressionengine/language/english/comment_lang.php
+++ system/expressionengine/language/english/comment_lang.php
@@ -282,7 +282,7 @@ $lang = array(
 'No Valid Comments',
 
 'comment_edit_time_limit' =>
-'Comment Edit Time Limit (in seconds)',
+'Comment Edit Time Limit (in seconds, set to 0 for no limit)',
 
 'already_subscribed' =>
 'You are already subscribed to this comment.',
