aee103e62d12a90af4c53291a2ed4984d7cb8b54
Robin Sowell <robin.sowell@ellislab.com>
2010-10-04 11:03:41 -0400

    reverting re http://expressionengine.com/forums/viewthread/169088/

--- themes/site_themes/agile_records/default_content.php
+++ themes/site_themes/agile_records/default_content.php
@@ -96,7 +96,7 @@ $Q[] = "INSERT INTO `exp_category_posts` (`entry_id`, `cat_id`) VALUES
 
 // Add upload locations
 $Q[] = "INSERT INTO `exp_upload_prefs` (`id`, `site_id`, `name`, `server_path`, `url`, `allowed_types`, `max_size`, `max_height`, `max_width`, `properties`, `pre_format`, `post_format`, `file_properties`, `file_pre_format`, `file_post_format`) VALUES 
-(1, 1, 'Main Upload Directory', '".str_replace(SYSDIR.'/', '', FCPATH).'images/'.$this->userdata['upload_folder']."', '".$this->userdata['site_url'].'images/'.$this->userdata['upload_folder']."', 'all', '', '', '', 'style=\"border: 0;\" alt=\"image\"', '', '', '', '', ''),
+(1, 1, 'Main Upload Directory', '".$this->userdata['image_path'].$this->userdata['upload_folder']."', '".$this->userdata['site_url'].'images/'.$this->userdata['upload_folder']."', 'all', '', '', '', 'style=\"border: 0;\" alt=\"image\"', '', '', '', '', ''),
 (2, 1, 'About', '".$this->theme_path."agile_records/images/uploads/', '".$this->userdata['site_url']."themes/site_themes/agile_records/images/uploads/', 'img', '', '', '', '', '', '', '', '', '')";
 
 chmod($this->theme_path."agile_records/images/uploads/", DIR_WRITE_MODE);
