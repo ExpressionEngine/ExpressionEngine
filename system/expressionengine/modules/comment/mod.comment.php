b3b2d270beb5f5f49b2c810f8ca1ee06e7edfa90
Robin Sowell <robin.sowell@ellislab.com>
2010-10-19 13:19:07 -0400

    Pagination acting up when dynamic on and Px was the first segment- preg incorrect.

--- system/expressionengine/modules/comment/mod.comment.php
+++ system/expressionengine/modules/comment/mod.comment.php
@@ -139,7 +139,7 @@ class Comment {
 
 		if ( ! $dynamic)
 		{
-			if (preg_match("#n(\d+)#i", $qstring, $match) OR preg_match("#/N(\d+)#i", $qstring, $match))
+			if (preg_match("#/n(\d+)#i", '/'.$qstring, $match) OR preg_match("#/N(\d+)#i", $qstring, $match))
 			{				
 				$current_page = $match['1'];
 				$uristr  = $this->EE->functions->remove_double_slashes(str_replace($match['0'], '', $uristr));
@@ -147,13 +147,12 @@ class Comment {
 		}
 		else
 		{
-			if (preg_match("#/P(\d+)#i", $qstring, $match))
+			if (preg_match("#/P(\d+)#", '/'.$qstring, $match))
 			{
 				$current_page = $match['1'];
-
 				$uristr  = $this->EE->functions->remove_double_slashes(str_replace($match['0'], '', $uristr));
 				$qstring = $this->EE->functions->remove_double_slashes(str_replace($match['0'], '', $qstring));
-			}			
+			}
 		}
 		
 		if  ($dynamic == TRUE OR $force_entry == TRUE)
