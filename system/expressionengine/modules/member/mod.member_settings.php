ab60636613beb6526dac89b3447afa14b4ecc75f
Robin Sowell <robin.sowell@ellislab.com>
2010-11-04 18:29:23 -0400

    Quick roll back.

--- system/expressionengine/modules/member/mod.member_settings.php
+++ system/expressionengine/modules/member/mod.member_settings.php
@@ -111,7 +111,6 @@ class Member_settings extends Member {
 					array($this->EE->lang->line('mbr_not_allowed_to_view_profiles')));
 		}
 
-
 		// No member id, no view
 		if ($this->cur_id == '')
 		{
