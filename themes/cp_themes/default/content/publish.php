f75c2b561e531ae275ad373556c6f6abf8ba854c
Pascal Kriete <pascal.kriete@ellislab.com>
2010-11-22 16:47:16 -0500

    Mocking up the new autosave notice on publish.

--- themes/cp_themes/default/content/publish.php
+++ themes/cp_themes/default/content/publish.php
@@ -221,6 +221,7 @@ if ($EE_view_disable !== TRUE)
 			<?php endforeach;?>
 
 			<ul id="publish_submit_buttons">
+				<li id="autosave_notice" style="margin-right: 7px; color: #5F6C74;">Autosaved at 22:45</li>
 			<?php if ($show_revision_cluster == 'y'):?><li><input type="submit" class="submit" name="save_revision" id="revision_button" value="<?=lang('save_revision')?>" /></li><?php endif?>
 				<li><input type="submit" class="submit" name="submit" id="submit_button" value="<?=lang('submit')?>" /></li>
 			</ul>
