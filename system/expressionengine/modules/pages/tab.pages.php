a0faf4f7565d869f107b37b299d44d62d3cab185
Greg Aker <greg.aker@ellislab.com>
2010-11-29 14:49:08 -0600

    Adding pages tab file

---
+++ system/expressionengine/modules/pages/tab.pages.php
@@ -0,0 +1,39 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * ExpressionEngine - by EllisLab
+ *
+ * @package		ExpressionEngine
+ * @author		ExpressionEngine Dev Team
+ * @copyright	Copyright (c) 2003 - 2010, EllisLab, Inc.
+ * @license		http://expressionengine.com/user_guide/license.html
+ * @link		http://expressionengine.com
+ * @since		Version 2.0
+ * @filesource
+ */
+
+// --------------------------------------------------------------------
+
+/**
+ * ExpressionEngine Discussion Pages Module
+ *
+ * @package		ExpressionEngine
+ * @subpackage	Modules
+ * @category	Modules
+ * @author		ExpressionEngine Dev Team
+ * @link		http://expressionengine.com
+ */
+class Pages_tab {
+
+	public function __construct()
+	{
+		$this->EE =& get_instance();
+	}
+
+	// --------------------------------------------------------------------	
+	
+	public function publish_tabs($channel_id, $entry_id = '')
+	{
+
+	}
+}
--- system/expressionengine/modules/pages/upd.pages.php
+++ system/expressionengine/modules/pages/upd.pages.php
@@ -1,29 +1,28 @@
-<?php
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-/*
-=====================================================
- ExpressionEngine - by EllisLab
------------------------------------------------------
- http://expressionengine.com/
------------------------------------------------------
- Copyright (c) 2003 - 2010, EllisLab, Inc.
-=====================================================
- THIS IS COPYRIGHTED SOFTWARE
- PLEASE READ THE LICENSE AGREEMENT
- http://expressionengine.com/user_guide/license.html
-=====================================================
- File: mcp.pages.php
------------------------------------------------------
- Purpose: Pages class - CP
-=====================================================
-*/
-if ( ! defined('EXT'))
-{
-	exit('Invalid file request');
-}
+/**
+ * ExpressionEngine - by EllisLab
+ *
+ * @package		ExpressionEngine
+ * @author		ExpressionEngine Dev Team
+ * @copyright	Copyright (c) 2003 - 2010, EllisLab, Inc.
+ * @license		http://expressionengine.com/user_guide/license.html
+ * @link		http://expressionengine.com
+ * @since		Version 2.0
+ * @filesource
+ */
 
+// --------------------------------------------------------------------
 
-
+/**
+ * ExpressionEngine Discussion Pages Module
+ *
+ * @package		ExpressionEngine
+ * @subpackage	Modules
+ * @category	Modules
+ * @author		ExpressionEngine Dev Team
+ * @link		http://expressionengine.com
+ */
 class Pages_upd {
 
 	var $version		= '2.0';
@@ -133,9 +132,16 @@ class Pages_upd {
 	 * @access	public
 	 * @return	bool
 	 */
-	function update($current='')
-	{
-		return FALSE;
+	function update($current = '')
+	{	
+				var_dump($this->version);exit;
+		if ($current == $this->version)
+		{
+			return FALSE;
+		}
+		
+
+		
 	}
 
 
