0d38e25d5b484ed4006cbd1ac129260dc5dcdf84
Greg Aker <greg.aker@ellislab.com>
2010-12-20 11:58:25 -0600

    Adding 2.1.3 build file and updating build.properties for this release

---
+++ system/installer/updates/ud_213.php
@@ -0,0 +1,43 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * ExpressionEngine - by EllisLab
+ *
+ * @package     ExpressionEngine
+ * @author      ExpressionEngine Dev Team
+ * @copyright   Copyright (c) 2003 - 2010, EllisLab, Inc.
+ * @license     http://expressionengine.com/user_guide/license.html
+ * @link        http://expressionengine.com
+ * @since       Version 2.0
+ * @filesource
+ */
+ 
+// ------------------------------------------------------------------------
+
+/**
+ * ExpressionEngine Update Class
+ *
+ * @package     ExpressionEngine
+ * @subpackage  Core
+ * @category    Core
+ * @author      ExpressionEngine Dev Team
+ * @link        http://expressionengine.com
+ */
+class Updater {
+
+	var $version_suffix = '';
+
+    function Updater()
+    {
+        $this->EE =& get_instance();
+    }
+
+    function do_update()
+    {
+		return TRUE;
+	}
+}   
+/* END CLASS */
+
+/* End of file ud_213.php */
+/* Location: ./system/expressionengine/installer/updates/ud_213.php */
