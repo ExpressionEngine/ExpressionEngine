6330c9ca4c2752a264640dd52b26f5c02e0224af
Greg Aker <greg.aker@ellislab.com>
2011-01-28 16:47:11 -0600

    Adding 214 update file

---
+++ system/installer/updates/ud_214.php
@@ -0,0 +1,40 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * ExpressionEngine - by EllisLab
+ *
+ * @package     ExpressionEngine
+ * @author      ExpressionEngine Dev Team
+ * @copyright   Copyright (c) 2003 - 2011, EllisLab, Inc.
+ * @license     http://expressionengine.com/user_guide/license.html
+ * @link        http://expressionengine.com
+ * @since       Version 2.0
+ * @filesource
+ */
+ 
+// ------------------------------------------------------------------------
+
+/**
+ * ExpressionEngine Update Class
+ *
+ * @package     ExpressionEngine
+ * @subpackage  Core
+ * @category    Core
+ * @author      ExpressionEngine Dev Team
+ * @link        http://expressionengine.com
+ */
+class Updater {
+
+	var $version_suffix = '';
+
+	function Updater()
+	{
+		$this->EE =& get_instance();
+	}
+
+	function do_update() { }
+}   
+/* END CLASS */
+
+/* End of file ud_213.php */
+/* Location: ./system/expressionengine/installer/updates/ud_213.php */
