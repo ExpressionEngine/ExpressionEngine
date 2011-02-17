8dc43160de224586f0a8bca9efc2401fffb67f99
Barry Cogan <barry.cogan@ellislab.com>
2011-02-16 21:00:35 -0700

    Adding file accidentally deleted back to the repo from rev 9651

---
+++ themes/cp_themes/default/admin/preference_delete_confirm.php
@@ -0,0 +1,46 @@
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+if ($EE_view_disable !== TRUE)
+{
+	$this->load->view('_shared/header');
+	$this->load->view('_shared/main_menu');
+	$this->load->view('_shared/sidebar');
+	$this->load->view('_shared/breadcrumbs');
+}
+?>
+
+<div id="mainContent"<?=$maincontent_state?>>
+	<?php $this->load->view('_shared/right_nav')?>
+	<div class="contents">
+
+		<div class="heading"><h2 class="edit"><?=$cp_page_title?></h2></div>
+		<div class="pageContents">
+
+			<?=form_open($form_action, $form_extra, $form_hidden)?>
+			
+			<p class="notice"><?=$message?></p>
+			
+			<p>
+			<?php foreach ($items as $item): ?>
+			<?=$item?><br />
+			<?php endforeach; ?>
+			</p>
+			
+			<p><?=form_submit(array('name' => 'submit', 'value' => lang('delete'), 'class' => 'submit'))?></p>
+
+			<?=form_close()?>
+
+		</div>
+	</div><!-- contents -->
+</div><!-- mainContent -->
+
+
+<?php
+if ($EE_view_disable !== TRUE)
+{
+	$this->load->view('_shared/accessories');
+	$this->load->view('_shared/footer');
+}
+
+
+/* End of file preference_delete_confirm.php */
+/* Location: ./themes/cp_themes/default/admin/preference_delete_confirm.php */
