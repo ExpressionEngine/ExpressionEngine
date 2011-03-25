f054a57f51d2a625d1a1de142c9aea11ae0d90ff
Wes Baker <wes.baker@ellislab.com>
2011-03-25 11:03:53 -0400

    Fixed image rotation and clarified image edit query string to say file_id and not file (it was already using file_id anyways).

--- themes/cp_themes/default/content/files/edit_image.php
+++ themes/cp_themes/default/content/files/edit_image.php
@@ -18,7 +18,8 @@ if ( ! $EE_view_disable)
 		
 		<div class="pageContents group">
 			<div id="file_manager_toolbar">
-				<?=form_open('C=content_files'.AMP.'M=edit_image', array('id'=>'image_crop_form'), array('action'=> 'crop', 'upload_dir' => $upload_dir, 'file' => $file))?>
+				<?=form_open('C=content_files'.AMP.'M=edit_image', array('id'=>'image_crop_form'), 
+					array('action'=> 'crop', 'upload_dir' => $upload_dir, 'file' => $file, 'file_id' => $file_id))?>
 					<h3 class="accordion"><?=lang('crop')?></h3>
 					<div id="file_manager_crop">
 						<ul>
@@ -47,7 +48,7 @@ if ( ! $EE_view_disable)
 					</div>
 				<?=form_close()?>
 				<?=form_open('C=content_files'.AMP.'M=edit_image', array('id'=>'image_rotate_form'), 
-								array('action'=> 'rotate', 'upload_dir' => $upload_dir, 'file' => $file))?>
+					array('action'=> 'rotate', 'upload_dir' => $upload_dir, 'file' => $file, 'file_id' => $file_id))?>
 					<h3 class="accordion"><?=lang('rotate')?></h3>
 					<div id="rotate_fieldset">
 						<ul>
@@ -84,7 +85,8 @@ if ( ! $EE_view_disable)
 						</p>
 					</div>
 				<?=form_close()?>
-				<?=form_open('C=content_files'.AMP.'M=edit_image', array('id'=>'image_resize_form'), array('action'=> 'resize', 'upload_dir' => $upload_dir, 'file' => $file))?>
+				<?=form_open('C=content_files'.AMP.'M=edit_image', array('id'=>'image_resize_form'), 
+					array('action'=> 'resize', 'upload_dir' => $upload_dir, 'file' => $file, 'file_id' => $file_id))?>
 					<h3 class="accordion"><?=lang('resize')?></h3>
 					<div id="resize_fieldset">
 						<ul>
