8fa79b4cbc06a6623c647591752f4ec828e59c0f
Pascal Kriete <pascal.kriete@ellislab.com>
2011-03-29 15:59:30 -0400

    Playing with the table test

--- system/tests/codeigniter/libraries/Table_test.php
+++ system/tests/codeigniter/libraries/Table_test.php
@@ -1,28 +1,38 @@
 <?php
 
-// class Table_test extends PHPUnit_Framework_TestCase
-// {
-//      public function setUp()
-//      {
-//              $this->CI =& get_instance();
-//      }
-// 
-//      
-//      
-//      // public function testSetHeader()
-//      // {
-//      //      $this->EE->load->library('table');
-//      //      $this->EE->table->set_heading(
-//      //              'test',
-//      //              'test2',
-//      //              'test3'
-//      //      );
-//      //      
-//      //      print_r($this->EE->table->heading);
-//      //      
-//      //      
-//      // }
-//      
-//      
-//      
-// }
+class Table_test extends PHPUnit_Framework_TestCase
+{
+	static $cls;
+	protected $table;
+	
+	public static function setUpBeforeClass()
+	{
+		$CI = get_instance();
+		$CI->load->library('table');
+		self::$cls = get_class($CI->table);
+	}
+	
+	public function setUp()
+	{
+		$cls = self::$cls;
+		$this->table = new $cls;
+	}
+	
+	public function testSetHeader()
+	{
+		$this->table->set_heading(
+			'test',
+			'test2',
+			'test3'
+		);
+		
+		$this->assertEquals(
+			array(
+				array('data' => 'test'),
+				array('data' => 'test2'),
+				array('data' => 'test3')
+			),
+			$this->table->heading
+		);
+	}
+}
