f9e2d00013ab492a0148a7a377fc6373f52af4b2
Wes Baker <wes.baker@ellislab.com>
2011-04-07 15:54:15 -0400

    File modal with iframe is beginning to work.

--- themes/cp_themes/default/_shared/file/upload_inner.php
+++ themes/cp_themes/default/_shared/file/upload_inner.php
@@ -1,66 +1,28 @@
-<!DOCTYPE html>
-<html lang="en" id="file_upload_iframe">
-<head>
-	<meta charset="utf-8">
+<?php $this->load->view('_shared/file/iframe_header'); ?>
 
-	<?=$this->view->head_title($cp_page_title)?>
-	<?=$this->view->head_link('css/jquery-ui-1.7.2.custom.css'); ?>
-	<?=$this->view->head_link('css/global.css'); ?>
-	<?=$this->view->head_link('css/override.css'); ?>
+<h3 class="closed"><a href="#"><?=lang('file_upload')?></a></h3>
 
-	<?php if ($this->extensions->active_hook('cp_css_end') === TRUE):?>
-	<link rel="stylesheet" href="<?=BASE.AMP.'C=css'.AMP.'M=cp_global_ext';?>" type="text/css" />
-	<?php endif;?>
-	<!--[if lte IE 7]><?=$this->view->head_link('css/iefix.css')?><![endif]-->
+<iframe id='target_upload' name='target_upload' src='' style='width:200px;height:50px;border:1;display:none;'></iframe>
+<?=form_open_multipart('C=content_files'.AMP.'M=upload_file', array('id'=>'upload_form'))?>
 
-	<?php 
-	if (isset($cp_global_js))
-	{
-		echo $cp_global_js;
-	} ?>
+<p>
+<?php if (count($upload_directories) > 1):?>
+	<?=form_label(lang('upload_dir_choose'), 'upload_dir')?>
+	<?=form_dropdown('upload_dir', $upload_directories, '', 'id="upload_dir"')?>
+<?php else:?>
+	<input type="hidden" name="upload_dir" value="<?=key($upload_directories)?>" id="upload_dir" />
+<?php endif;?>
+</p>
+
+<p>
+	<?=form_label(lang('upload_file'), 'upload_file', array('class' => 'visualEscapism'))?>
+	<?=form_upload(array('id'=>'upload_file','name'=>'userfile','size'=>15,'class'=>'field'))?>
+</p>
+
+<p class="custom_field_add"><button type="submit" class="submit submit_alt"><img src="<?=$cp_theme_url?>images/upload_item.png" width="12" height="14" alt="<?=lang('upload')?>" />&nbsp;&nbsp;<?=lang('upload')?></button></p>
+
+<p id="progress"><img src="<?=$cp_theme_url?>images/indicator.gif" alt="<?=lang('loading')?>..." /><br /><?=lang('loading')?>...</p>
+
+<?=form_close()?>
 	
-	<?=$this->view->script_tag('jquery/jquery.js')?>
-
-	<?php if ($advanced_css_mtime): ?>
-	<script charset="utf-8" type="text/javascript" src="<?=BASE.AMP.'C=javascript'.AMP.'M=load'.AMP.'file=css'.AMP.'theme='.$this->cp->cp_theme.AMP.'v='.$advanced_css_mtime?>"></script>
-	<?php endif;?>
-
-	<?php
-	if (isset($script_head)) 
-	{
-		echo $script_head;
-	}
-
-	foreach ($this->cp->its_all_in_your_head as $item)
-	{
-		echo $item."\n";
-	}
-	?>
-</head>
-<body>
-	<h3 class="closed"><a href="#"><?=lang('file_upload')?></a></h3>
-	
-	<iframe id='target_upload' name='target_upload' src='' style='width:200px;height:50px;border:1;display:none;'></iframe>
-	<?=form_open_multipart('C=content_files'.AMP.'M=upload_file', array('id'=>'upload_form'))?>
-
-	<p>
-	<?php if (count($upload_directories) > 1):?>
-		<?=form_label(lang('upload_dir_choose'), 'upload_dir')?>
-		<?=form_dropdown('upload_dir', $upload_directories, '', 'id="upload_dir"')?>
-	<?php else:?>
-		<input type="hidden" name="upload_dir" value="<?=key($upload_directories)?>" id="upload_dir" />
-	<?php endif;?>
-	</p>
-	
-	<p>
-		<?=form_label(lang('upload_file'), 'upload_file', array('class' => 'visualEscapism'))?>
-		<?=form_upload(array('id'=>'upload_file','name'=>'userfile','size'=>15,'class'=>'field'))?>
-	</p>
-
-	<p class="custom_field_add"><button type="submit" class="submit submit_alt"><img src="<?=$cp_theme_url?>images/upload_item.png" width="12" height="14" alt="<?=lang('upload')?>" />&nbsp;&nbsp;<?=lang('upload')?></button></p>
-
-	<p id="progress"><img src="<?=$cp_theme_url?>images/indicator.gif" alt="<?=lang('loading')?>..." /><br /><?=lang('loading')?>...</p>
-
-	<?=form_close()?>
-</body>
-</html>
+<?php $this->load->view('_shared/file/iframe_footer') ?>
