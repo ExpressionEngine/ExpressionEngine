20b13ed32cc9633ff22e5e2194585f5e1b6a2d3a
Greg Aker <greg.aker@ellislab.com>
2011-04-12 10:23:10 -0500

    Fixed a bug (#15122) where the avatar upload path was incorrectly reported in the member preferences when the directory was not writeable.

--- system/expressionengine/core/EE_Config.php
+++ system/expressionengine/core/EE_Config.php
@@ -631,15 +631,19 @@ class EE_Config Extends CI_Config {
 				
 				if ( ! @is_dir($fp))
 				{
-					$this->_config_path_errors[$this->EE->lang->line('invalid_path')][$val] = $this->EE->lang->line($val) .': ' .$new_values[$val];
+					$this->_config_path_errors[lang('invalid_path')][$val] = lang($val).': '.$fp;
 				}
 
-				if ( (! is_really_writable($fp)) && ($val != 'theme_folder_path'))
+				if (( ! is_really_writable($fp)) && ($val != 'theme_folder_path'))
 				{
-					if ( ! isset($this->_config_path_errors[$this->EE->lang->line('invalid_path')][$val]))
+					if ( ! isset($this->_config_path_errors[lang('invalid_path')][$val]))
 					{
-						$this->_config_path_errors[$this->EE->lang->line('not_writable_path')][$val] = $this->EE->lang->line($val) .': ' .$new_values[$val];
+						if ($val == 'avatar_path')
+						{
+							$new_values[$val] = $new_values[$val].'uploads/';
+						}
 						
+						$this->_config_path_errors[lang('not_writable_path')][$val] = lang($val) .': ' .$new_values[$val];
 					}
 				}
 			}
