1e92e4f49170c4674ba8b8ba23c00f076cc8a47d
Pascal Kriete <pascal.kriete@ellislab.com>
2011-04-15 10:22:12 -0400

    Removing the old js tools from our repo. It's in dropbox/source\ code

--- system/expressionengine/third_party/js_tool/config/config.php
+++
@@ -1,6 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-$config['js_tool_title'] = 'Javascript Tool';
-
-/* End of file js_tool.php */
-/* Location: ./system/expressionengine/third_party/js_tool/config/js_tool.php */
--- system/expressionengine/third_party/js_tool/mobile/index.php
+++
@@ -1,3 +0,0 @@
-<div>
-<h1>Mobile Test!</h1>
-</div>
