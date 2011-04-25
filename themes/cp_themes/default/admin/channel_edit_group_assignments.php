77e6b8ae5251eeeb7dec02a281d461cc8b6c8a2f
Greg Aker <greg.aker@ellislab.com>
2011-04-25 14:26:45 -0500

    Fixed a bug (#15676) where the channel name was not listed on the Edit Group Assignments page.

--- themes/cp_themes/default/admin/channel_edit_group_assignments.php
+++ themes/cp_themes/default/admin/channel_edit_group_assignments.php
@@ -12,7 +12,7 @@ if ($EE_view_disable !== TRUE)
 	<?php $this->load->view('_shared/right_nav')?>
 	<div class="contents">
 
-		<div class="heading"><h2 class="edit"><?=$cp_page_title?></h2></div>
+		<div class="heading"><h2 class="edit"><?=$cp_page_title?> &mdash; <?=$channel_name?></h2></div>
 		<div class="pageContents">
 
 			<?=form_open('C=admin_content'.AMP.'M=channel_update_group_assignments', '', $form_hidden)?>
