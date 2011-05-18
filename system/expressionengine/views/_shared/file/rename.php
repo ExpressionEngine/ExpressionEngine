f5c4f2cf3f6a73364d7abdfccb9a0cd5f17650d8
Wes Baker <wes.baker@ellislab.com>
2011-05-18 12:02:44 -0400

    Cleaned up file rename stuff, added language keys. Also added extension to rename dialog, but it's not editable.

--- system/expressionengine/views/_shared/file/rename.php
+++ system/expressionengine/views/_shared/file/rename.php
@@ -2,10 +2,15 @@
 
 <div class="upload_rename">
 	<h2><?= lang('upload_rename') ?></h2>
+	<p><?= lang('upload_rename_message') ?></p>
 	<?= form_open('C=content_files'.AMP.'M=update_file', '', array('file_id' => $file['file_id'], 'file_json' => $file_json, 'file_ext' => $file_ext, 'directory_id' => $file['upload_location_id'])) ?>
-	<p><input type="text" name="new_file_name" value="<?= $file_name ?>" /></p>
-	<p><input type="submit" name="submit" value="Submit" /></p>
+	<p><input type="text" name="new_file_name" value="<?= $file_name ?>" class="text" /><span class="extension">.<?= $file_ext ?></span></p>
 	<?= form_close() ?>
 </div> <!-- .upload_rename -->
+
+<script>
+	var file = <?= $file_json ?>;
+	parent.$.ee_fileuploader.file_exists(file);
+</script>
 
 <?php $this->load->view('_shared/file/iframe_footer') ?>
--- system/expressionengine/views/_shared/file/upload_modal.php
+++ system/expressionengine/views/_shared/file/upload_modal.php
@@ -2,8 +2,9 @@
 	<iframe name="upload_iframe" src="<?= $base_url ?>"></iframe>
 	<div class="button_bar ui-dialog-titlebar ui-widget-header ui-corner-all ui-helper-clearfix upload_step_1">
 		<input type="submit" class="before_upload submit" name="upload_file" value="<?= lang('upload_file') ?>" id="upload_file" />
+		<input type="submit" class="file_exists submit" name="rename_file" value="<?= lang('rename_file') ?>" id="rename_file" />
 		<a href="#" class="after_upload filemanager cancel" id="browse_files"><?= lang('browse_files') ?></a>
-		<a href="#" class="after_upload filemanager submit" id="edit_file"><?= lang('edit_file') ?></a>
+		<input type="submit" class="after_upload filemanager submit" name="edit_file" value="<?= lang('edit_file') ?>" id="edit_file" />
 		<input type="submit" class="after_upload filebrowser submit" name="choose_file" value="<?= lang('use_file') ?>" id="choose_file" />
 	</div>
 </div>
