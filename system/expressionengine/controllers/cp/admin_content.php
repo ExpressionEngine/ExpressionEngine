8780c6153bc0b9254ac8f64b70d2818cf6fce2ad
Greg Aker <greg.aker@ellislab.com>
2011-06-06 10:32:12 -0500

    Removing a duplicate query in admin_content

--- system/expressionengine/controllers/cp/admin_content.php
+++ system/expressionengine/controllers/cp/admin_content.php
@@ -3773,11 +3773,6 @@ class Admin_content extends CI_Controller {
 		
 		$field_query = $this->db->get();
 
-		$field_query = $this->db->query("SELECT f.* FROM exp_channel_fields AS f, exp_field_groups AS g
-						WHERE f.group_id = g.group_id
-						AND g.site_id = '".$this->db->escape_str($this->config->item('site_id'))."'
-						AND f.field_id = '{$vars['field_id']}'");
-
 		if ($vars['field_id'] == '')
 		{
 			$type = 'new';
