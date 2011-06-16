1fc294e83c47bf9d52817e22686aa53646cc17db
Wes Baker <wes.baker@ellislab.com>
2011-06-16 18:27:12 -0400

    Started work in on SafeCracker and EE File field integration.

--- system/expressionengine/modules/safecracker/libraries/safecracker_lib.php
+++ system/expressionengine/modules/safecracker/libraries/safecracker_lib.php
@@ -841,10 +841,13 @@ class Safecracker_lib
 			*/
 		}
 		
-		$js_defaults = array(
-			'ui' => array('core', 'widget', 'button', 'dialog'),
-			'plugin' => array('scrollable', 'scrollable.navigator', 'ee_filebrowser', 'markitup', 'thickbox'),
-		);
+		$this->EE->cp->add_js_script(array(
+			'file' 		=> 'saef',
+			'ui' 		=> array('core', 'widget', 'button', 'dialog'),
+			'plugin' 	=> array('scrollable', 'scrollable.navigator', 'ee_filebrowser', 'ee_fileuploader', 'markitup', 'thickbox')
+		));
+		
+		$js_defaults = array();
 		
 		if (version_compare(APP_VER, '2.1.3', '>'))
 		{
