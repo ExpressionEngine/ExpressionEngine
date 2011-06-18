5150d04e78ce94f6c4171575e784e0a8422ee5cb
James Mathias <james.mathias@ellislab.com>
2011-06-18 15:06:03 -0700

    removed rel="external" from rendered template button, as that code affects ALL the buttons in that location, need to discover a way to target only the specific render template linkw

--- system/expressionengine/views/_shared/right_nav.php
+++ system/expressionengine/views/_shared/right_nav.php
@@ -2,7 +2,7 @@
 <div class="rightNav">
 	<div style="float: left; width: 100%;">
 	<?php foreach($cp_right_nav as $lang_key => $link): ?>
-		<span class="button"><a title="<?=lang($lang_key)?>" class="submit" href="<?=$link?>" rel="external"><?=lang($lang_key)?></a></span>
+		<span class="button"><a title="<?=lang($lang_key)?>" class="submit" href="<?=$link?>"><?=lang($lang_key)?></a></span>
 	<?php endforeach; ?>
 	</div>
 	<div class="clear_left"></div>
