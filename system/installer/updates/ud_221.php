fdbaf1395e18050700db71d11d73a854908aad7d
Pascal Kriete <pascal.kriete@ellislab.com>
2011-06-29 13:00:37 -0400

    Adding 221 update file and incrementing the installer version number to 2.2.1

---
+++ system/installer/updates/ud_221.php
@@ -0,0 +1,40 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * ExpressionEngine - by EllisLab
+ *
+ * @package     ExpressionEngine
+ * @author      ExpressionEngine Dev Team
+ * @copyright   Copyright (c) 2003 - 2011, EllisLab, Inc.
+ * @license     http://expressionengine.com/user_guide/license.html
+ * @link        http://expressionengine.com
+ * @since       Version 2.0
+ * @filesource
+ */
+ 
+// ------------------------------------------------------------------------
+
+/**
+ * ExpressionEngine Update Class
+ *
+ * @package     ExpressionEngine
+ * @subpackage  Core
+ * @category    Core
+ * @author      ExpressionEngine Dev Team
+ * @link        http://expressionengine.com
+ */
+class Updater {
+
+	var $version_suffix = '';
+
+    function do_update()
+    {
+		// What's this database you speak of?
+        return TRUE;
+
+    }
+}   
+/* END CLASS */
+
+/* End of file ud_221.php */
+/* Location: ./system/expressionengine/installer/updates/ud_221.php */
