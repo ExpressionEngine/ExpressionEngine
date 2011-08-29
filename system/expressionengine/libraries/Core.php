c7849c86681276aa1f16ce696603c778e82fafcb
Wes Baker <wes@wesbaker.com>
2011-08-29 16:54:09 -0400

    Bumped build number in EE_Core because I'm OCD like that.

--- system/expressionengine/libraries/Core.php
+++ system/expressionengine/libraries/Core.php
@@ -73,7 +73,7 @@ class EE_Core {
 		// application constants
 		define('IS_FREELANCER',	FALSE);
 		define('APP_NAME',		'ExpressionEngine'.(IS_FREELANCER ? ' Freelancer' : ''));
-		define('APP_BUILD',		'20110705');
+		define('APP_BUILD',		'20110801');
 		define('APP_VER',		substr($this->EE->config->item('app_version'), 0, 1).'.'.substr($this->EE->config->item('app_version'), 1, 1).'.'.substr($this->EE->config->item('app_version'), 2));
 		define('SLASH',			'&#47;');
 		define('LD',			'{');
