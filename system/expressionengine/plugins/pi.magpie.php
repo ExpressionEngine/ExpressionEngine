9b0c98f4b6555b78d1fcd355afe99c5591bdd4a8
Derek Jones <derek.jones@ellislab.com>
2011-09-14 10:10:37 -0500

    added a BSD notice to the Magpie class, following a conversation with Kellan which granted us a retroactive BSD license for Magpie

--- system/expressionengine/plugins/pi.magpie.php
+++ system/expressionengine/plugins/pi.magpie.php
@@ -466,6 +466,33 @@ Added ability to override caching options when using fetch_rss() directly.
 //  BEGIN MagpieRSS Class
 // -------------------------------------------
 
+The MagpieRSS class is used here under a BSD license with the author's permission.
+
+Copyright (c) 2002, Kellan Elliott-McCrea All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+- Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+- Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+ 
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+
 
  * Project:	  MagpieRSS: a simple RSS integration tool
  * File:		  rss_parse.inc  - parse an RSS or Atom feed
