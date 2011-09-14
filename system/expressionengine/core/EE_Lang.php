0d6606d3031539a7f8097008b96180daef16b907
Wes Baker <wes@wesbaker.com>
2011-09-14 10:16:18 -0400

    Fixed deft_lang definition in lang->load. Thanks to Brian Litzinger.
    FYI, this is inconsistent with lines 129-130, where we use get_config() to load
    in the config file. We're not using that here since we've already used
    get_instance() and get_config() would be one more thing on top of that.

--- system/expressionengine/core/EE_Lang.php
+++ system/expressionengine/core/EE_Lang.php
@@ -64,7 +64,7 @@ class EE_Lang extends CI_Lang {
 			}
 		}
 
-		$deft_lang = ( ! isset($config['language'])) ? 'english' : $config['language'];
+		$deft_lang = ( ! $EE->config->item('language')) ? 'english' : $EE->config->item('language');
 		
 		// Sec.ur.ity code.  ::sigh::
 		$package = ($package == '') ? $EE->security->sanitize_filename(str_replace(array('lang.', '.php'), '', $which)) : $EE->security->sanitize_filename($package);
