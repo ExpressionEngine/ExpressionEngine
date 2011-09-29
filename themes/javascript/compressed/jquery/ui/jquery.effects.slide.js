23a1ef3309bccf24c58d799b3ad1753f959bf470
Wes Baker <wes@wesbaker.com>
2011-09-29 10:41:12 -0400

    Updated compressed Javascript.

--- themes/javascript/compressed/jquery/ui/jquery.effects.slide.js
+++ themes/javascript/compressed/jquery/ui/jquery.effects.slide.js
@@ -1,9 +1,9 @@
 /*
- * jQuery UI Effects Slide 1.8.1
+ * jQuery UI Effects Slide 1.8.16
  *
- * Copyright (c) 2010 AUTHORS.txt (http://jqueryui.com/about)
- * Dual licensed under the MIT (MIT-LICENSE.txt)
- * and GPL (GPL-LICENSE.txt) licenses.
+ * Copyright 2011, AUTHORS.txt (http://jqueryui.com/about)
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ * http://jquery.org/license
  *
  * http://docs.jquery.com/UI/Effects/Slide
  *
@@ -11,5 +11,5 @@
  *	jquery.effects.core.js
  */
 
-(function(c){c.effects.slide=function(d){return this.queue(function(){var a=c(this),h=["position","top","left"],e=c.effects.setMode(a,d.options.mode||"show"),b=d.options.direction||"left";c.effects.save(a,h);a.show();c.effects.createWrapper(a).css({overflow:"hidden"});var f=b=="up"||b=="down"?"top":"left",b=b=="up"||b=="left"?"pos":"neg",g=d.options.distance||(f=="top"?a.outerHeight({margin:!0}):a.outerWidth({margin:!0}));e=="show"&&a.css(f,b=="pos"?-g:g);var i={};i[f]=(e=="show"?b=="pos"?"+=":"-=":
-b=="pos"?"-=":"+=")+g;a.animate(i,{queue:!1,duration:d.duration,easing:d.options.easing,complete:function(){e=="hide"&&a.hide();c.effects.restore(a,h);c.effects.removeWrapper(a);d.callback&&d.callback.apply(this,arguments);a.dequeue()}})})}})(jQuery);
+(function(c){c.effects.slide=function(d){return this.queue(function(){var a=c(this),h=["position","top","bottom","left","right"],f=c.effects.setMode(a,d.options.mode||"show"),b=d.options.direction||"left";c.effects.save(a,h);a.show();c.effects.createWrapper(a).css({overflow:"hidden"});var g=b=="up"||b=="down"?"top":"left",b=b=="up"||b=="left"?"pos":"neg",e=d.options.distance||(g=="top"?a.outerHeight({margin:!0}):a.outerWidth({margin:!0}));f=="show"&&a.css(g,b=="pos"?isNaN(e)?"-"+e:-e:e);var i={};
+i[g]=(f=="show"?b=="pos"?"+=":"-=":b=="pos"?"-=":"+=")+e;a.animate(i,{queue:!1,duration:d.duration,easing:d.options.easing,complete:function(){f=="hide"&&a.hide();c.effects.restore(a,h);c.effects.removeWrapper(a);d.callback&&d.callback.apply(this,arguments);a.dequeue()}})})}})(jQuery);
