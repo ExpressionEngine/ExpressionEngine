e97ce083b47931a4ce246a8f25040bdef9c60a2b
Jack McDade <jack@jackmcdade.com>
2011-10-26 13:33:14 -0400

    Added core_template_route hook

--- system/expressionengine/libraries/Core.php
+++ system/expressionengine/libraries/Core.php
@@ -535,6 +535,19 @@ class EE_Core {
 			$this->EE->output->set_output($member->manager());
 			return;
 		}
+		
+		// -------------------------------------------
+		// 'core_template_route' hook.
+		//  - Reassign the template group and template loaded for parsing
+		//
+			if ($this->EE->extensions->active_hook('core_template_route') === TRUE)
+			{
+				$edata = $this->EE->extensions->call('core_template_route', $this->EE->uri->uri_string);
+				if (is_array($edata) && count($edata) == 2)
+					list($template_group, $template) = $edata;
+			}
+		//
+		// -------------------------------------------
 
 		// Look for a page in the pages module
 		if ($template_group == '' && $template == '')
