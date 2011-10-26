18a41b093318c679e8a8f07e0cac2899c1002c0e
Kevin Cupp <kevin.cupp@gmail.com>
2011-10-26 10:48:33 -0400

    Amendment to bug #16348 fix, replacing get_authors_simple() to just call get_authors() and put in a @deprecated notice

--- system/expressionengine/models/member_model.php
+++ system/expressionengine/models/member_model.php
@@ -662,6 +662,23 @@ class Member_model extends CI_Model {
 	}
 	
 	// --------------------------------------------------------------------
+	
+	/**
+	 * Get Authors Simple
+	 *
+	 * This function returns a set of members who are authors in a set channel- member group data is omitted
+	 *
+	 * @deprecated	Use member_model->get_authors instead
+	 * @access	public
+	 * @param	integer
+	 * @return	mixed
+	 */
+	function get_authors_simple($author_id = FALSE, $limit = FALSE, $offset = FALSE)
+	{
+		return $this->get_authors($author_id, $limit, $offset);
+	}
+	
+	// --------------------------------------------------------------------
 
 	/**
 	 * Get Member Groups
