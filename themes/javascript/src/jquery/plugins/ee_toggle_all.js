30ffe119c89c46e0a2a965a985e53a46bbc0d1d3
Brandon Jones <brandon.jones@ellislab.com>
2011-12-07 19:41:11 -0500

    Altering the toggle plugin to support clicks on the header and TDs surrounding the checkboxes. Bleh

--- themes/javascript/src/jquery/plugins/ee_toggle_all.js
+++ themes/javascript/src/jquery/plugins/ee_toggle_all.js
@@ -11,24 +11,42 @@ $.fn.toggle_all = function() {
 		var $table = $(this);
 		
 		// Loop through each selected header with a checkbox
-		$table.find('th:has(input[type=checkbox])').each(function(index, val) {
-			
-			// Name the table header, figure out it's index, get the header
-			// checkbox, and select all the data
-			var $table_header = $(this),
-				column = $table_header.index(),
-				$header_checkbox = $table_header.find('input[type=checkbox]'),
-				$table_data = $table.find('td:nth-child(' + (column + 1) + ') input[type=checkbox]');
+		$table.find('th input[type=checkbox]').each(function(index, val) {
 
-			// Listen for clicks to the header checkbox
-			$header_checkbox.click(function(event) {
-				var checked = $(this).prop('checked');
-				$table_data.prop('checked', checked);
+			// Name the checkbox and its header, get the column index,
+			// and select all the td elements
+			var $header_checkbox = $(this),
+				$header = $(this).closest('th'),
+				column = $header.index(),
+				$table_data = $table.find('td:nth-child(' + (column + 1) + ')');
+
+			// Listen for clicks to the header/checkbox
+			$header.click(function(event) {
+				var checked = $header_checkbox.prop('checked');
+
+				// Toggle the actual checkbox if only the header was clicked
+				if (event.target.tagName == "TH") {
+					checked = ! checked;
+					$header_checkbox.prop('checked', checked);
+				}
+
+				$table_data.find('input').each(function(){
+					$(this).prop('checked', checked);
+				});
 			});
 
-			// Also listen to checks on the normal checkboxes, to see if all of
+
+			// Also listen to checks on the normal checkboxes/TDs, to see if all of
 			// them have been (un)checked
 			$table_data.click(function(event) {
+
+				// Toggle the actual checkbox if only the td was clicked
+				if (event.target.tagName == "TD") {
+					$(this).find('input').each(function(){
+						$(this).prop('checked', ! $(this).prop('checked'));
+					});
+				}
+
 				if ($table_data.size() == $table.find('td:nth-child('+ (column + 1) +') input[type=checkbox]:checked').size()) {
 					$header_checkbox.prop('checked', true);
 				} else if ($table.find('td:nth-child('+ (column + 1) +') input[type=checkbox]:checked').size() == 0) {
