c90606cb06ad29b8ba59b4606133f251d9bb4822
Kevin Cupp <kevin.cupp@gmail.com>
2012-01-05 22:54:05 -0500

    Updating copyrights in docblocks to 2012

--- system/installer/updates/ud_141.php
+++ system/installer/updates/ud_141.php
@@ -4,7 +4,7 @@
  *
  * @package		ExpressionEngine
  * @author		ExpressionEngine Dev Team
- * @copyright	Copyright (c) 2003 - 2011, EllisLab, Inc.
+ * @copyright	Copyright (c) 2003 - 2012, EllisLab, Inc.
  * @license		http://expressionengine.com/user_guide/license.html
  * @link		http://expressionengine.com
  * @since		Version 2.0
