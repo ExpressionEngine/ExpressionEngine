d3466b62d202ba838cf45dd10e6f067511750a4a
Greg Ferrell <info@studiokoi.com>
2012-01-30 00:53:04 -0500

    Adjusted Referrer->log_referrer to look for TRUE instead of FALSE in order to work with proposed changes to nation_ban_check

--- system/expressionengine/libraries/Referrer.php
+++ system/expressionengine/libraries/Referrer.php
@@ -44,7 +44,7 @@ class EE_Referrer {
 	function log_referrer()
 	{  
 		// Is the nation of the user banend?
-		if ($this->EE->session->nation_ban_check(FALSE) === FALSE)
+		if ($this->EE->session->nation_ban_check(FALSE) === TRUE)
 		{
 			return;
 		}
