8db5a24c9a30925d876deca504d777264d89d072
Wes Baker <wes@wesbaker.com>
2012-02-06 13:17:20 -0500

    Recompressed javascript.

--- themes/javascript/compressed/jquery/plugins/toolbox.mousewheel.js
+++ themes/javascript/compressed/jquery/plugins/toolbox.mousewheel.js
@@ -30,4 +30,4 @@
  Date:    Tue Apr 20 19:26:58 2010 +0000 
 */
 (function(b){function c(a){switch(a.type){case "mousemove":return b.extend(a.data,{clientX:a.clientX,clientY:a.clientY,pageX:a.pageX,pageY:a.pageY});case "DOMMouseScroll":b.extend(a,a.data);a.delta=-a.detail/3;break;case "mousewheel":a.delta=a.wheelDelta/120}a.type="wheel";return b.event.handle.call(this,a,a.delta)}b.fn.mousewheel=function(a){return this[a?"bind":"trigger"]("wheel",a)};b.event.special.wheel={setup:function(){b.event.add(this,d,c,{})},teardown:function(){b.event.remove(this,d,c)}};
-var d=!b.browser.mozilla?"mousewheel":"DOMMouseScroll"+("1.9">b.browser.version?" mousemove":"")})(jQuery);
+var d=!b.browser.mozilla?"mousewheel":"DOMMouseScroll"+(b.browser.version<"1.9"?" mousemove":"")})(jQuery);
