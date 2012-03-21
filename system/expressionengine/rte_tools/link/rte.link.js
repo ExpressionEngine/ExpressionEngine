646de8e6f9858075c16b1e7ed5084bddd760e442
Kevin Cupp <kevin.cupp@gmail.com>
2012-03-21 14:14:49 -0400

    Adding some spacing around link dialog elements

--- system/expressionengine/rte_tools/link/rte.link.js
+++ system/expressionengine/rte_tools/link/rte.link.js
@@ -1,6 +1,6 @@
 (function(){
 	
-	var	$link_dialog	= $('<div>' +
+	var	$link_dialog	= $('<div class="rte-link-dialog">' +
 							'<p><label>* ' + EE.rte.link.dialog.url_field_label + '</label>' +
 							'<input type="url" required="required"/></p>' +
 							'<p><label>' + EE.rte.link.dialog.title_field_label + '</label>' +
