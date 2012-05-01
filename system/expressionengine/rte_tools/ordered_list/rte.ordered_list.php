42285c5ae461e5889661256ed5576328dbf5ea55
Pascal Kriete <pascal@pascalkriete.com>
2012-05-01 15:25:35 -0400

    Whitespace

--- system/expressionengine/rte_tools/ordered_list/rte.ordered_list.php
+++ system/expressionengine/rte_tools/ordered_list/rte.ordered_list.php
@@ -77,14 +77,14 @@ class Ordered_list_rte {
 		WysiHat.addButton('ordered_list', {
 			label:			EE.rte.ordered_list.add,
 			'toggle-text':	EE.rte.ordered_list.remove,
-	    	handler: function(state) {
-	    		this.make('orderedList');
-	    		this.Selection.set(state.selection);
+			handler: function(state) {
+				this.make('orderedList');
+				this.Selection.set(state.selection);
 			},
 			query: function() {
 				return this.is('orderedList');
 			}
-	    });
+		});
 		
 <?php	$buffer = ob_get_contents();
 		ob_end_clean(); 
