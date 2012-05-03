0326d0c49f761462269e62a4121eb2a0cc4e4c7c
Brandon Jones <brandon.jones@ellislab.com>
2012-05-03 15:59:29 -0400

    Making the RTE a fieldtype

--- system/expressionengine/fieldtypes/textarea/ft.textarea.php
+++ system/expressionengine/fieldtypes/textarea/ft.textarea.php
@@ -42,127 +42,13 @@ class Textarea_ft extends EE_Fieldtype {
 
 	function display_field($data)
 	{		
-		$field = array(
+		return form_textarea(array(
 			'name'	=> $this->field_name,
 			'id'	=> $this->field_name,
-		//	'value'	=> $data, // set below
+			'value'	=> $data,
 			'rows'	=> $this->settings['field_ta_rows'],
 			'dir'	=> $this->settings['field_text_direction']
-		);
-
-
-		// form prepped nonsense
-		$data = htmlspecialchars_decode($data, ENT_QUOTES);
-		$code_marker = unique_marker('code');
-		$code_chunks = array();
-
-		$field_ft = isset($this->settings['field_fmt']) ? $this->settings['field_fmt'] : '';
-
-		if ($field_ft == 'xhtml')
-		{
-			$data = trim($data);
-
-			// Undo any existing newline formatting. Typography will change
-			// it anyways and the rtf will add its own. Having this here
-			// prevents growing-newline syndrome in the rtf and lets us switch
-			// between rtf and non-rtf.
-
-			$data = preg_replace("/<\/p>\n*<p>/is", "\n\n", $data);
-			$data = preg_replace("/<br( \/)?>\n/is", "\n", $data);
-		}
-
-		// remove code chunks
-		if (preg_match_all("/\[code\](.+?)\[\/code\]/si", $data, $matches))
-		{
-			foreach ($matches[1] as $i => $chunk)
-			{
-				$code_chunks[] = trim($chunk);
-				$data = str_replace($matches[0][$i], $code_marker.$i, $data);
-			}
-		}
-
-		// RTE?
-		if ( ! isset($this->settings['field_enable_rte']))
-		{
-			$this->settings['field_enable_rte'] = 'n';
-		}
-
-		// Check to see if it's an RTE, don't forget to check the RTE module and
-		// the user's preferences
-		if ($this->EE->session->userdata('rte_enabled') == 'y' 
-			AND $this->EE->config->item('rte_enabled') == 'y'
-			AND $this->settings['field_enable_rte'] == 'y')
-		{
-			$field['class']	= 'rte';
-
-			foreach ($code_chunks as &$chunk)
-			{
-				$chunk = htmlentities($chunk, ENT_QUOTES, 'UTF-8');
-				$chunk = str_replace("\n", '<br>', $chunk);
-			}
-
-			// xhtml vs br
-			if ($this->settings['field_fmt'] == 'xhtml')
-			{
-				$this->EE->load->library('typography');
-
-				$data = $this->EE->typography->_format_newlines($data);
-
-				// Remove double paragraph tags
-				$data = preg_replace("/(<\/?p>)\\1/is", "\\1", $data);
-			}
-		}
-
-		// put code chunks back
-		foreach ($code_chunks as $i => $chunk)
-		{
-			$data = str_replace($code_marker.$i, '[code]'.$chunk.'[/code]', $data);
-		}
-		
-		// Swap {filedir_x} with the real URL. It will be converted back
-		// upon submit by the RTE Image tool.
-		$this->EE->load->model('file_upload_preferences_model');
-		$dirs = $this->EE->file_upload_preferences_model->get_file_upload_preferences($this->EE->session->userdata('group_id'));
-		
-		foreach($dirs as $d)
-		{
-			// tag to replace
-			$filedir = "{filedir_{$d['id']}}";
-
-			$data = str_replace($filedir, $d['url'], $data);
-		}
-	
-		$data = htmlspecialchars($data, ENT_QUOTES);
-
-		$field['value'] = $data;
-		return form_textarea($field);
-	}
-
-	// --------------------------------------------------------------------
-
-	function save($data)
-	{
-		if ( ! isset($this->settings['field_enable_rte']) OR
-			$this->settings['field_enable_rte'] == 'n')
-		{
-			return $data;
-		}
-
-		$data = str_replace('<br>', "\n", $data); // must happen before the decode or we won't know which are ours
-		$data = htmlspecialchars_decode($data, ENT_QUOTES);
-
-		// decode double encoded code chunks
-		if (preg_match_all("/\[code\](.+?)\[\/code\]/si", $data, $matches))
-		{
-			foreach ($matches[1] as $chunk)
-			{
-				$chunk = trim($chunk);
-				$chunk = html_entity_decode($chunk, ENT_QUOTES, 'UTF-8');
-				$data = str_replace($matches[0][$i], '[code]'.$chunk.'[/code]', $data);
-			}
-		}
-
-		return $data;
+		));
 	}
 
 	// --------------------------------------------------------------------
@@ -185,14 +71,7 @@ class Textarea_ft extends EE_Fieldtype {
 	function display_settings($data)
 	{
 		$prefix = 'textarea';
-		
-		// RTE setup
-		$this->EE->load->model('addons_model');
-		if ( ! isset($data['field_enable_rte']))
-		{
-			$data['field_enable_rte'] = 'n';
-		}
-		
+
 		$field_rows	= ($data['field_ta_rows'] == '') ? 6 : $data['field_ta_rows'];
 		
 		$this->EE->table->add_row(
@@ -201,48 +80,6 @@ class Textarea_ft extends EE_Fieldtype {
 		);
 		
 		$this->field_formatting_row($data, $prefix);
-		if ($this->EE->addons_model->module_installed('rte') &&
-		 	$this->EE->config->item('rte_enabled') == 'y')
-		{
-			$this->EE->lang->loadfile('rte');
-			
-			# RTE-related JavaScript
-			$this->EE->javascript->output('
-				// RTE adjustments
-				// Can’t have RTE alongside Formatting Buttons, Smileys or WriteMode
-				var
-				$dependent	= $("[name=textarea_field_enable_rte]," +
-								"[name=textarea_field_show_formatting_btns]," +
-								"[name=textarea_field_show_smileys]," +
-								"[name=textarea_field_show_writemode]," +
-								"[name=textarea_field_show_fmt]," +
-								"[name=textarea_field_show_glossary]," +
-								"[name=textarea_field_show_file_selector]")
-									.change(function(){
-										var
-										$this	= $(this),
-										name	= $this.attr("name"),
-										value	= $("[name=" + name + "]:checked").val();
-										if ( name == "textarea_field_enable_rte" &&
-										 	 value == "y" )
-										{
-											$dependent
-												.not("[name=" + name + "]")
-												.filter("[value=n]")
-												.attr("checked",true);
-										}
-										else if ( name != "textarea_field_enable_rte" &&
-										 	 	  value == "y" )
-										{
-											$("[name=textarea_field_enable_rte][value=n]")
-												.attr("checked",true);
-										}
-									 });
-			');
-			$this->EE->javascript->compile();
-						
-			$this->_yes_no_row($data, 'enable_rte_for_field', 'field_enable_rte', $prefix);
-		}
 		$this->text_direction_row($data, $prefix);
 		$this->field_show_formatting_btns_row($data, $prefix);
 		$this->field_show_smileys_row($data, $prefix);
@@ -251,15 +88,6 @@ class Textarea_ft extends EE_Fieldtype {
 		$this->field_show_writemode_row($data, $prefix);
 		$this->field_show_file_selector_row($data, $prefix);
 	}
-	
-	// --------------------------------------------------------------------
-
-	function save_settings($data)
-	{		
-		return array(
-			'field_enable_rte'	=> $this->EE->input->post('textarea_field_enable_rte')
-		);
-	}	
 }
 
 // END Textarea_ft class
