54573c4db1daba46a73027dc728c7a3509d7604e
Pascal Kriete <pascal@pascalkriete.com>
2012-05-07 10:36:11 -0400

    Adding EL to the wysihat license block.

--- themes/javascript/src/jquery/plugins/wysihat.js
+++ themes/javascript/src/jquery/plugins/wysihat.js
@@ -2,6 +2,8 @@
  *  (c) 2008-2010 Joshua Peek
  *  JQ-WysiHat - jQuery port of WysiHat to run on jQuery
  *  (c) 2010 Scott Williams & Aaron Gustafson
+ *  EL-WysiHat - Extensive rewrite of JQ-WysiHat for ExpressionEngine
+ *  (c) 2012 EllisLab, Inc.
  *
  *  WysiHat is freely distributable under the terms of an MIT-style license.
  *--------------------------------------------------------------------------*/
