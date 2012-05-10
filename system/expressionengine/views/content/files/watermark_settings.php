791009afc21502e1f1b2037878dba11b30273d2e
Pascal Kriete <pascal@pascalkriete.com>
2012-05-10 16:27:25 -0400

    There is really no need to hardcode a list of numbers that starts at 0.

--- system/expressionengine/views/content/files/watermark_settings.php
+++ system/expressionengine/views/content/files/watermark_settings.php
@@ -56,14 +56,8 @@
 				</td>
 			</tr>
 			<tr class="odd">
-				<?php
-				$options = array('0' => 0, '1' => 1, '2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7,
-				 '8' => 8, '9' => 9, '10' => 10, '11' => 11, '12' => 12, '13' => 13, '14' => 14, '15' => 15, 
-				'16' => 16, '17' => 17, '18' => 18, '19' => 19, '20' => 20, '21' => 21, '22' => 22, '23' => 23, 
-				'24' => 24, '25' => 25, '26' => 26, '27' => 27, '28' => 28, '29' => 29, '30' => 30);
-				?>
 				<td><?=form_label(lang('wm_padding'), 'wm_padding'); ?></td>
-				<td><?=form_dropdown('wm_padding', $options, $wm_padding); ?></td>
+				<td><?=form_dropdown('wm_padding', range(0, 30), $wm_padding); ?></td>
 			</tr>
 			<tr class="even">
 				<td><?=form_label(lang('wm_hor_offset'), 'wm_hor_offset'); ?></td>
