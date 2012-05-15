2497b3d0f00f174735b8d59e7c82cf9825874d0c
Pascal Kriete <pascal@pascalkriete.com>
2012-05-15 00:45:17 -0400

    Adding comments

--- system/expressionengine/core/EE_Controller.php
+++ system/expressionengine/core/EE_Controller.php
@@ -1,6 +1,28 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+/**
+ * ExpressionEngine - by EllisLab
+ *
+ * @package		ExpressionEngine
+ * @author		EllisLab Dev Team
+ * @copyright	Copyright (c) 2003 - 2012, EllisLab, Inc.
+ * @license		http://expressionengine.com/user_guide/license.html
+ * @link		http://expressionengine.com
+ * @since		Version 2.0
+ * @filesource
+ */
+ 
+// ------------------------------------------------------------------------
 
+/**
+ * ExpressionEngine Controller
+ *
+ * @package		ExpressionEngine
+ * @subpackage	Core
+ * @category	Core
+ * @author		EllisLab Dev Team
+ * @link		http://expressionengine.com
+ */
 class EE_Controller extends CI_Controller {
 
 	function __construct()
@@ -13,6 +35,17 @@ class EE_Controller extends CI_Controller {
 	}
 }
 
+// ------------------------------------------------------------------------
+
+/**
+ * ExpressionEngine Control Panel Controller
+ *
+ * @package		ExpressionEngine
+ * @subpackage	Core
+ * @category	Core
+ * @author		EllisLab Dev Team
+ * @link		http://expressionengine.com
+ */
 class CP_Controller extends EE_Controller {
 	
 	function __construct()
