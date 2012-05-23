3c375153baab12cf1128e08b839dca96b2fb9dfe
Wes Baker <wes@wesbaker.com>
2012-05-23 10:33:22 -0400

    Bumping build and version numbers.

--- system/expressionengine/libraries/Core.php
+++ system/expressionengine/libraries/Core.php
@@ -85,8 +85,8 @@ class EE_Core {
 		// application constants
 		define('IS_FREELANCER',	FALSE);
 		define('APP_NAME',		'ExpressionEngine'.(IS_FREELANCER ? ' Freelancer' : ''));
-		define('APP_BUILD',		'20120507');
-		define('APP_VER',		'2.5.0');
+		define('APP_BUILD',		'20120523');
+		define('APP_VER',		'2.5.1');
 		define('SLASH',			'&#47;');
 		define('LD',			'{');
 		define('RD',			'}');
