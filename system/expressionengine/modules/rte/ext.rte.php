1dc5a80c24184fb15885faf31c70d35adad393cc
Kevin Cupp <kevin.cupp@gmail.com>
2012-05-24 20:39:21 -0400

    Simplifying RTE JS loading on publish form

--- system/expressionengine/modules/rte/ext.rte.php
+++ system/expressionengine/modules/rte/ext.rte.php
@@ -272,33 +272,10 @@ class Rte_ext {
 	 */
 	function publish_form_entry_data($results)
 	{
-		// WysiHat
-		$this->EE->cp->add_to_head($this->EE->view->head_link('css/rte.css'));
-		$this->EE->cp->add_js_script(array('plugin' => 'wysihat'));
-
 		// Build toolset JS and include CP-only tools
-		$toolset_js = $this->EE->rte_lib->build_js(0, '.WysiHat-field', NULL, TRUE);
-		
-		// JS config
-		$this->EE->javascript->set_global(array(
-			'rte'	=> array(
-				'js'	=> array(
-					'toolset_js'	=> $toolset_js
-				)
-			)
-		));
-
-		$js = "
-			var d = document,
-				script = d.getElementsByTagName('script')[0],
-				tag = d.createElement('script');
-			tag.type = 'text/javascript';
-			tag.async = true;
-			tag.innerHTML = EE.rte.js.toolset_js;
-			script.parentNode.insertBefore(tag, script);
-		";
-
-		$this->EE->javascript->output($js);
+		$this->EE->javascript->output(
+			$this->EE->rte_lib->build_js(0, '.WysiHat-field', NULL, TRUE)
+		);
 		
 		return $results;
 	}
