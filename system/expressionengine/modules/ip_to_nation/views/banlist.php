f8c9c441452ee463da911a782508dbae9dd8510e
Pascal Kriete <pascal@pascalkriete.com>
2012-05-30 18:07:39 -0400

    Starting to update IP2Nation to support IPv6.
    New dataset from MaxMind and some minor refactoring. Updating the dataset
    still needs work.

--- system/expressionengine/modules/ip_to_nation/views/banlist.php
+++ system/expressionengine/modules/ip_to_nation/views/banlist.php
@@ -4,10 +4,12 @@
 	<p><?=lang('ban_info')?></p>
 
 	<ul class="inline_labels">
-	<?php foreach($countries as $country):?>
+	<?php foreach($country_list as $country):?>
 		<li>
-			<?=form_checkbox($country['code'], 'y', $country['status'], "id='code_{$country['code']}'")?> 
-			<label for="code_<?=$country['code']?>"><?=$country['name']?></label>
+			<label for="code_<?=$country['code']?>">
+				<?=form_checkbox($country['code'], 'y', $country['status'], "id='code_{$country['code']}'")?> 
+				<?=$country['name']?>
+			</label>
 		</li>
 	<?php endforeach;?>
 	</ul>
