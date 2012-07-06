e6ea6a07093d81eb650ef149fcef88bb932a5213
Wes Baker <wes@wesbaker.com>
2012-07-06 15:39:44 -0400

    Simplifying conditionals for EE_Input::filter_get_data

--- system/expressionengine/core/EE_Input.php
+++ system/expressionengine/core/EE_Input.php
@@ -97,31 +97,28 @@ class EE_Input extends CI_Input {
 			}		
 		}
 	
-		if (isset($_GET))
+		if (isset($_GET) AND $filter_keys === TRUE)
 		{
 			foreach($_GET as $key => $val)
 			{
-				if ($filter_keys == TRUE)
+				if (is_array($val))
 				{
-					if (is_array($val))
+					$data = '';
+					
+					if ((int) config_item('debug') == 2)
 					{
-						$data = '';
-						
-						if ((int) config_item('debug') == 2)
-						{
-							$data = '<br>'.htmlentities(print_r($data, TRUE));
-						}
-						
-						set_status_header(503);
-						exit(sprintf("Invalid GET Data - Array %s", $data));
+						$data = '<br>'.htmlentities(print_r($data, TRUE));
 					}
-					elseif (preg_match("#(;|\?|exec\s*\(|system\s*\(|passthru\s*\(|cmd\s*\(|[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})#i", $val))
-					{
-						$data = ((int) config_item('debug') == 2) ? '<br>'.htmlentities($val) : '';
-						
-						set_status_header(503);
-						exit(sprintf("Invalid GET Data %s", $data));
-					}   
+					
+					set_status_header(503);
+					exit(sprintf("Invalid GET Data - Array %s", $data));
+				}
+				elseif (preg_match("#(;|\?|exec\s*\(|system\s*\(|passthru\s*\(|cmd\s*\(|[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})#i", $val))
+				{
+					$data = ((int) config_item('debug') == 2) ? '<br>'.htmlentities($val) : '';
+					
+					set_status_header(503);
+					exit(sprintf("Invalid GET Data %s", $data));
 				}
 			}	
 		}	
