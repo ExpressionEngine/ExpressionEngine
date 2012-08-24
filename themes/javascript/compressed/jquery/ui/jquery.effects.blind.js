3b5ee3a81cafbb4c5a337008d6bb8a554ac7cf20
Kevin Cupp <kevin.cupp@gmail.com>
2012-08-24 14:44:08 -0400

    Compressing javascript

--- themes/javascript/compressed/jquery/ui/jquery.effects.blind.js
+++ themes/javascript/compressed/jquery/ui/jquery.effects.blind.js
@@ -11,5 +11,5 @@
  *	jquery.effects.core.js
  */
 
-(function(b){b.effects.blind=function(c){return this.queue(function(){var a=b(this),g=["position","top","bottom","left","right"],f=b.effects.setMode(a,c.options.mode||"hide"),d=c.options.direction||"vertical";b.effects.save(a,g);a.show();var e=b.effects.createWrapper(a).css({overflow:"hidden"}),h=d=="vertical"?"height":"width",d=d=="vertical"?e.height():e.width();f=="show"&&e.css(h,0);var i={};i[h]=f=="show"?d:0;e.animate(i,c.duration,c.options.easing,function(){f=="hide"&&a.hide();b.effects.restore(a,
+(function(b){b.effects.blind=function(c){return this.queue(function(){var a=b(this),g=["position","top","bottom","left","right"],f=b.effects.setMode(a,c.options.mode||"hide"),d=c.options.direction||"vertical";b.effects.save(a,g);a.show();var e=b.effects.createWrapper(a).css({overflow:"hidden"}),h="vertical"==d?"height":"width",d="vertical"==d?e.height():e.width();"show"==f&&e.css(h,0);var i={};i[h]="show"==f?d:0;e.animate(i,c.duration,c.options.easing,function(){"hide"==f&&a.hide();b.effects.restore(a,
 g);b.effects.removeWrapper(a);c.callback&&c.callback.apply(a[0],arguments);a.dequeue()})})}})(jQuery);
