1b0f77f98efbc16dea36b387fb281e898fe2ec84
Wes Baker <wes@wesbaker.com>
2012-09-14 15:00:06 -0400

    Adding routine to updater to XSS clean all custom links (menu and quicklinks).
    Conflicts:
    	system/codeigniter

--- system/installer/updates/ud_254.php
+++ system/installer/updates/ud_254.php
@@ -74,6 +74,57 @@ class Updater {
 			)
 		);
 	}
+
+	// ---------------------------------------------------------------------
+
+	/**
+	 * Clean up the menu and quicklink items so there's no XSS funny business
+	 */
+	private function _xss_clean_custom_links()
+	{
+		$members = $this->EE->db->select('member_id, quick_links, quick_tabs')
+			->where('quick_links IS NOT NULL')
+			->or_where('quick_tabs IS NOT NULL')
+			->get('members')
+			->result_array();
+		
+		// Sanitize quick_links and quick_tabs		
+		foreach ($members as $index => $data)
+		{
+			// Sanitize quick_links and quick_tabs
+			$members[$index]['quick_links'] = $this->_sanitize_custom_links($data['quick_links']);
+			$members[$index]['quick_tabs'] = $this->_sanitize_custom_links($data['quick_tabs']);
+		}
+
+		$this->EE->db->update_batch('members', $members, 'member_id');
+	}
+
+	/**
+	 * Clean up custom links given a string containing multiple links broken up 
+	 * by newlines, with links broken up by pipes. 
+	 *
+	 * This does not remove the [removed] replacements.
+	 * 
+	 * @param  String $string String containing multiple custom links separated 
+	 *                        by newlines with links broken up by pipes
+	 * @return String         Sanitized string containing custom links
+	 */
+	private function _sanitize_custom_links($string)
+	{
+		// Each string is comprised of multiple links broken up by newlines
+		$lines = explode("\n", $string);
+		
+		foreach ($lines as $index => $line)
+		{
+			// Each link is three parts, the first being the name (which is 
+			// where we're concerned about XSS cleaning), the link, the order
+			$links = explode('|', $line);
+			$links[0] = $this->EE->security->xss_clean($links[0]);	
+			$lines[$index] = implode('|', $links);
+		}
+		
+		return implode("\n", $lines);
+	}
 }	
 /* END CLASS */
 
