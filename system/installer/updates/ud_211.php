6d26209618a04102c6bb1dc39383b2ebfb116ea9
Kevin Smith <kevin@gohearsay.com>
2012-09-14 23:46:12 -0500

    Converted the part of the 211 update that moves comment notifications to the 'comment_subscriptions' table.
    One little thing I added: lines 168-177 now remove the notify flag from the comment that was just processed. If the update dies in the middle and needs to be restarted, this keeps us from moving any comment's subscription to the new table more than once.

--- system/installer/updates/ud_211.php
+++ system/installer/updates/ud_211.php
@@ -86,96 +86,104 @@ class Updater {
 		$this->EE->migrate->insert_set('specialty_templates', $values, $unique);
 		
 		// Do we need to move comment notifications?
-		if ( ! $this->EE->db->table_exists('exp_comments'))
+		// We should skip it if the Comments module isn't installed.
+		if ( ! $this->EE->db->table_exists('comments'))
 		{
         	return TRUE;			
 		}		
 		
-		if ( ! $this->EE->db->table_exists('comment_subscriptions'))
-		{
-			$this->EE->progress->update_state("Creating Comment Subscription Table");
-				
-				$fields = array(
-					'subscription_id'	=> array('type' => 'int'	, 'constraint' => '10', 'unsigned' => TRUE, 'auto_increment' => TRUE),
-					'entry_id'			=> array('type' => 'int'	, 'constraint' => '10', 'unsigned' => TRUE),
-					'member_id'			=> array('type' => 'int'	, 'constraint' => '10', 'default' => 0),
-					'email'				=> array('type' => 'varchar', 'constraint' => '50'),
-					'subscription_date'	=> array('type' => 'varchar', 'constraint' => '10'),
-					'notification_sent'	=> array('type' => 'char'	, 'constraint' => '1', 'default' => 'n'),
-					'hash'				=> array('type' => 'varchar', 'constraint' => '15')
-				);
+		$this->EE->progress->update_state("Creating Comment Subscription Table");
+			
+			$fields = array(
+				'subscription_id'	=> array('type' => 'int'	, 'constraint' => '10', 'unsigned' => TRUE, 'auto_increment' => TRUE),
+				'entry_id'			=> array('type' => 'int'	, 'constraint' => '10', 'unsigned' => TRUE),
+				'member_id'			=> array('type' => 'int'	, 'constraint' => '10', 'default' => 0),
+				'email'				=> array('type' => 'varchar', 'constraint' => '50'),
+				'subscription_date'	=> array('type' => 'varchar', 'constraint' => '10'),
+				'notification_sent'	=> array('type' => 'char'	, 'constraint' => '1', 'default' => 'n'),
+				'hash'				=> array('type' => 'varchar', 'constraint' => '15')
+			);
 
-			$this->EE->load->dbforge();
-			$this->EE->dbforge->add_field($fields);
-			$this->EE->dbforge->add_key('subscription_id', TRUE);
-			$this->EE->dbforge->add_key(array('entry_id', 'member_id'));
-			$this->EE->dbforge->create_table('comment_subscriptions');
+		$this->EE->load->dbforge();
+		$this->EE->dbforge->add_field($fields);
+		$this->EE->dbforge->add_key('subscription_id', TRUE);
+		$this->EE->dbforge->add_key(array('entry_id', 'member_id'));
+		$this->EE->dbforge->create_table('comment_subscriptions', TRUE);		
+
+
+		// this step can be a doozy.  Set time limit to infinity.
+        // Server process timeouts are out of our control, unfortunately
+        @set_time_limit(0);
+        $this->EE->db->save_queries = FALSE;
+        
+        $this->EE->progress->update_state('Moving Comment Notifications to Subscriptions');
+                
+        $batch = 50;
+		$offset = 0;
+		$progress   = "Moving Comment Notifications: %s";
+		
+		$this->EE->db->distinct();
+		$this->EE->db->select('entry_id, email, name, author_id');
+		$this->EE->db->where('notify', 'y');
+		
+		$total = $this->EE->db->count_all_results('comments');
+
+		if (count($total) > 0)
+		{
+			for ($i = 0; $i < $total; $i = $i + $batch)
+            {
+            	$this->EE->progress->update_state(str_replace('%s', "{$offset} of {$count} queries", $progress));	
+
+				$data = array();
+		
+				$this->EE->db->distinct();
+				$this->EE->db->select('entry_id, email, name, author_id');
+				$this->EE->db->where('notify', 'y');
+				$this->EE->db->limit($batch, $offset);
+				$comment_data = $this->EE->db->get('comments');
+							
+				$s_date = NULL;
+					
+				// convert to comments
+            	foreach($comment_data->result_array() as $row)
+            	{
+					$author_id = $row['author_id'];
+					$rand = $author_id.$this->random('alnum', 8);
+					$email = ($row['email'] == '') ? NULL : $row['email'];
+			
+               		$data[] = array(
+                    		'entry_id'       		=> $row['entry_id'],
+                    		'member_id'     		=> $author_id,
+                    		'email'        			=> $email,
+                    		'subscription_date'		=> $s_date,
+                    		'notification_sent'     => 'n',
+                    		'hash'           		=> $rand
+                			);
+				}
+				
+				if (count($data) > 0)
+				{
+					if ($this->EE->db->insert_batch('comment_subscriptions', $data))
+					{
+						// Remove the notify flag from comment in the
+						// comments table so that it won't be converted again
+						// in case the comment subscription conversion doesn't
+						// complete and has to be run again.
+						$this->EE->db->set('notify', '');
+						$this->EE->db->where('entry_id', $row['entry_id']);
+						$this->EE->db->where('email', $row['email']);
+						$this->EE->db->where('name', $row['name']);
+						$this->EE->db->where('author_id', $row['author_id']);
+						$this->EE->db->update('comments');
+					}
+				}
+				
+				$offset = $offset + $batch; 
+			}
 		}
 		
-		if ($this->EE->db->field_exists('notify', 'comments'))
-		{
-			// this step can be a doozy.  Set time limit to infinity.
-	        // Server process timeouts are out of our control, unfortunately
-	        @set_time_limit(0);
-	        $this->EE->db->save_queries = FALSE;
-	        
-	        $this->EE->progress->update_state('Moving Comment Notifications to Subscriptions');
-	                
-	        $batch = 50;
-			$offset = 0;
-			$progress   = "Moving Comment Notifications: %s";
-			
-			$this->EE->db->distinct();
-			$this->EE->db->select('entry_id, email, name, author_id');
-			$this->EE->db->where('notify', 'y');
-			
-			$total = $this->EE->db->count_all_results('comments');
-
-			if (count($total) > 0)
-			{
-				for ($i = 0; $i < $total; $i = $i + $batch)
-	            {
-	            	$this->EE->progress->update_state(str_replace('%s', "{$offset} of {$count} queries", $progress));	
-
-					$data = array();
-			
-					$this->EE->db->distinct();
-					$this->EE->db->select('entry_id, email, name, author_id');
-					$this->EE->db->where('notify', 'y');
-					$this->EE->db->limit($batch, $offset);
-					$comment_data = $this->EE->db->get('comments');
-								
-					$s_date = NULL;
-						
-					// convert to comments
-	            	foreach($comment_data->result_array() as $row)
-	            	{
-						$author_id = $row['author_id'];
-						$rand = $author_id.$this->random('alnum', 8);
-						$email = ($row['email'] == '') ? NULL : $row['email'];
-				
-	               		$data[] = array(
-	                    		'entry_id'       		=> $row['entry_id'],
-	                    		'member_id'     		=> $author_id,
-	                    		'email'        			=> $email,
-	                    		'subscription_date'		=> $s_date,
-	                    		'notification_sent'     => 'n',
-	                    		'hash'           		=> $rand
-	                			);
-					}
-					
-					if (count($data) > 0)
-					{
-						$this->EE->db->insert_batch('comment_subscriptions', $data);
-					}
-					
-					$offset = $offset + $batch; 
-				}
-			}
-
-			//  Lastly- we get rid of the notify field
-			$this->EE->db->query("ALTER TABLE `exp_comments` DROP COLUMN `notify`");
-		}
+		//  Lastly- we get rid of the notify field
+		$this->EE->migrate->drop_column('comments', 'notify');
 		
 		return TRUE;
 	}
