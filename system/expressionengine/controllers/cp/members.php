04cd8374021a134b88651d96f1ddea542e3ae2ce
Daniel Bingham <dbingham@theroadgoeson.com>
2012-10-08 17:53:43 -0400

    Removing the "all" option from the sites menu.

--- system/expressionengine/controllers/cp/members.php
+++ system/expressionengine/controllers/cp/members.php
@@ -1619,7 +1619,7 @@ class Members extends CI_Controller {
 							->order_by('site_label')
 							->get('sites');
 		
-		$sites_dropdown = (!$group_id && $msm_enabled) ? array('all'=>'All Sites') : array();
+		$sites_dropdown = array();
 		
 		// Setup Sites dropdown
 		foreach ($sites_q->result() as $row)
