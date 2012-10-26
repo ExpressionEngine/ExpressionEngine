66182c9d65a117caa1892bd9d6019a9e2c8b03da
Daniel Bingham <dbingham@theroadgoeson.com>
2012-10-26 13:05:47 -0400

    Fixing moderator and super admin comment editing logic in Comment::entries().

--- system/expressionengine/modules/comment/mod.comment.php
+++ system/expressionengine/modules/comment/mod.comment.php
@@ -790,20 +790,19 @@ class Comment {
 			$cond['editable'] = FALSE;
 			$cond['can_moderate_comment'] = FALSE;
 		
-			if ($row['comment_date'] > $this->_comment_edit_time_limit()) 
+			if ($this->EE->session->userdata['group_id'] == 1 
+				OR $this->EE->session->userdata['can_edit_all_comments'] == 'y' 
+				OR ($this->EE->session->userdata['can_edit_own_comments'] == 'y' && $row['entry_author_id'] == $this->EE->session->userdata['member_id'])
+				)
 			{
-				if ($this->EE->session->userdata['group_id'] == 1 OR 
-					$this->EE->session->userdata['can_edit_all_comments'] == 'y' OR 
-					($this->EE->session->userdata['can_edit_own_comments'] == 'y' && $row['entry_author_id'] == $this->EE->session->userdata['member_id'])
-					)
-				{
+				$cond['editable'] = TRUE;
+				$cond['can_moderate_comment'] = TRUE;
+			}
+			elseif ($this->EE->session->userdata['member_id'] != '0'  
+				&& $row['author_id'] == $this->EE->session->userdata['member_id']
+				&& $row['comment_date'] > $this->_comment_edit_time_limit())
+			{
 					$cond['editable'] = TRUE;
-					$cond['can_moderate_comment'] = TRUE;
-				}
-				elseif ($this->EE->session->userdata['member_id'] != '0'  && $row['author_id'] == $this->EE->session->userdata['member_id'])
-				{
-					$cond['editable'] = TRUE;
-				}
 			}
 
 			if ( isset($mfields) && is_array($mfields) && count($mfields) > 0)
