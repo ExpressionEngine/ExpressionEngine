e749d558b5aa18a1e98bec8ae5d9104359a789d5
Daniel Bingham <dbingham@theroadgoeson.com>
2012-11-28 13:41:09 -0500

    Fixed a bug (#18350) where File Manager search was defaulting to searching only filename where expected behavior was for it to search all fields.

--- system/expressionengine/controllers/cp/content_files.php
+++ system/expressionengine/controllers/cp/content_files.php
@@ -189,7 +189,7 @@ class Content_files extends CP_Controller {
 		);
 
 		$search_select_options = array(
-			''				=> lang('search_in'),
+			'all'				=> lang('search_in'),
 			'file_name'		=> lang('file_name'),
 			'file_title'	=> lang('file_title'),
 			'custom_field'	=> lang('custom_fields'),
