2bc50d7bdee63a1b427ddd894676fdb1d16f125c
Robin Sowell <robin.sowell@ellislab.com>
2012-11-30 14:39:32 -0500

    Improved clearing of Tracker cookie when on a Page URL.
    This was insane- in 1.x, somehow //$FNS->set_cookie('tracker', serialize($tracker), '0'); was resulting in a disallowed key message.  The key triggering it was, for example:
    s:11:"/home/test/"
    
    Which yes, should indeed trigger that warning.  If I deleted the tracker cookie and commented out this line, then there was no further problem.
    
    This line basically just deletes the cookie.  WHY do we delete if for a PAGE?  Makes no sense to me.  But if we want to delete it, new code is better- and does not cause that error.
    
    But the error?  Still puzzles me.  As does why we want to delete the tracker array when we hit a Page page.

--- system/expressionengine/libraries/Session.php
+++ system/expressionengine/libraries/Session.php
@@ -948,7 +948,7 @@ class EE_Session {
 		
 		if (REQ == 'PAGE')
 		{		
-			$this->EE->functions->set_cookie('tracker', serialize($tracker), '0'); 
+			$this->EE->functions->set_cookie('tracker'); 
 		}
 		
 		return $tracker;
