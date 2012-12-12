f2548cee964abb44f93480a0ca12cfdb54bbf38d
Derek Jones <derek.jones@ellislab.com>
2012-12-11 21:51:42 -0800

    Adding conditional to hide nag banner from non-Core installs

--- system/expressionengine/views/_shared/header.php
+++ system/expressionengine/views/_shared/header.php
@@ -54,9 +54,13 @@
 	</div>
 </div>
 <![endif]-->
+
+<?php if (IS_CORE): ?>
 <div class="nanner">
 	<p>Core is a free, feature limited version of ExpressionEngine <a href="https://store.ellislab.com/"><b>Upgrade Now</b></a> to get <a href="http://ellislab.com/expressionengine">all the features</a></p>
 </div>
+<?php endif;?>
+
 <div id="branding"></div>
 
 <?php
