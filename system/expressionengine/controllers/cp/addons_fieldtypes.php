91cbcf3f1faeadbdf06aa83b3ec778d51357e62a
Pascal Kriete <pascal@pascalkriete.com>
2013-01-17 20:59:08 -0500

    Missing copyright block.

--- system/expressionengine/controllers/cp/addons_fieldtypes.php
+++ system/expressionengine/controllers/cp/addons_fieldtypes.php
@@ -1,5 +1,27 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * ExpressionEngine - by EllisLab
+ *
+ * @package		ExpressionEngine
+ * @author		EllisLab Dev Team
+ * @copyright	Copyright (c) 2003 - 2012, EllisLab, Inc.
+ * @license		http://ellislab.com/expressionengine/user-guide/license.html
+ * @link		http://ellislab.com
+ * @since		Version 2.0
+ * @filesource
+ */
+ 
+// ------------------------------------------------------------------------
 
+/**
+ * ExpressionEngine Fieldtype Administration Class
+ *
+ * @package		ExpressionEngine
+ * @subpackage	Control Panel
+ * @category	Control Panel
+ * @author		EllisLab Dev Team
+ * @link		http://ellislab.com
+ */
 class Addons_fieldtypes extends CP_Controller {
 
 
