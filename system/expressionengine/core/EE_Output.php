fb05ded00f15171dfca43f3cac4c9460e9135e3a
Pascal Kriete <pascal@pascalkriete.com>
2013-01-23 10:18:44 -0500

    Always sending UTF-8 headers for json responses.
    Was causing issues on a server with funny defaults.

--- system/expressionengine/core/EE_Output.php
+++ system/expressionengine/core/EE_Output.php
@@ -408,7 +408,7 @@ class EE_Output extends CI_Output {
 		{
 			if (is_array($msg))
 			{
-				@header('Content-Type: application/json');
+				@header('Content-Type: application/json; charset=UTF-8');
 			}
 			else
 			{
