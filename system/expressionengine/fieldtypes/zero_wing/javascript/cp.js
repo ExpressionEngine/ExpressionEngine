bd5cd921d504e48c76f8e94b094c1cd449fefdf5
Pascal Kriete <pascal@pascalkriete.com>
2013-02-19 11:50:29 -0500

    Very rough fieldtype js

---
+++ system/expressionengine/fieldtypes/zero_wing/javascript/cp.js
@@ -0,0 +1,360 @@
+(function($) {
+/*
+	var accented = {
+	    0: /[\u2070\u2080\u24EA\uFF10]/gi,
+	    1: /[\u00B9\u2081\u2460\uFF11]/gi,
+	    2: /[\u00B2\u2082\u2461\uFF12]/gi,
+	    3: /[\u00B3\u2083\u2462\uFF13]/gi,
+	    4: /[\u2074\u2084\u2463\uFF14]/gi,
+	    5: /[\u2075\u2085\u2464\uFF15]/gi,
+	    6: /[\u2076\u2086\u2465\uFF16]/gi,
+	    7: /[\u2077\u2087\u2466\uFF17]/gi,
+	    8: /[\u2078\u2088\u2467\uFF18]/gi,
+	    9: /[\u2079\u2089\u2468\uFF19]/gi,
+	    a: /[\u00AA\u00E0-\u00E5\u0101\u0103\u0105\u01CE\u01DF\u01E1\u01FB\u0201\u0203\u0227\u1D43\u1E01\u1E9A\u1EA1\u1EA3\u1EA5\u1EA7\u1EA9\u1EAB\u1EAD\u1EAF\u1EB1\u1EB3\u1EB5\u1EB7\u24D0\uFF41]/gi,
+	    b: /[\u1D47\u1E03\u1E05\u1E07\u24D1\uFF42]/gi,
+	    c: /[\u00E7\u0107\u0109\u010B\u010D\u1D9C\u1E09\u24D2\uFF43]/gi,
+	    d: /[\u010F\u1D48\u1E0B\u1E0D\u1E0F\u1E11\u1E13\u217E\u24D3\uFF44]/gi,
+	    e: /[\u00E8-\u00EB\u0113\u0115\u0117\u0119\u011B\u0205\u0207\u0229\u1D49\u1E15\u1E17\u1E19\u1E1B\u1E1D\u1EB9\u1EBB\u1EBD\u1EBF\u1EC1\u1EC3\u1EC5\u1EC7\u2091\u212F\u24D4\uFF45]/gi,
+	    f: /[\u1DA0\u1E1F\u24D5\uFF46]/gi,
+	    g: /[\u011D\u011F\u0121\u0123\u01E7\u01F5\u1D4D\u1E21\u210A\u24D6\uFF47]/gi,
+	    h: /[\u0125\u021F\u02B0\u1E23\u1E25\u1E27\u1E29\u1E2B\u1E96\u210E\u24D7\uFF48]/gi,
+	    i: /[\u00EC-\u00EF\u0129\u012B\u012D\u012F\u0133\u01D0\u0209\u020B\u1D62\u1E2D\u1E2F\u1EC9\u1ECB\u2071\u2139\u2170\u24D8\uFF49]/gi,
+	    j: /[\u0135\u01F0\u02B2\u24D9\u2C7C\uFF4A]/gi,
+	    k: /[\u0137\u01E9\u1D4F\u1E31\u1E33\u1E35\u24DA\uFF4B]/gi,
+	    l: /[\u013A\u013C\u013E\u0140\u01C9\u02E1\u1E37\u1E39\u1E3B\u1E3D\u2113\u217C\u24DB\uFF4C]/gi,
+	    m: /[\u1D50\u1E3F\u1E41\u1E43\u217F\u24DC\uFF4D]/gi,
+	    n: /[\u00F1\u0144\u0146\u0148\u01F9\u1E45\u1E47\u1E49\u1E4B\u207F\u24DD\uFF4E]/gi,
+	    o: /[\u00BA\u00F2-\u00F6\u014D\u014F\u0151\u01A1\u01D2\u01EB\u01ED\u020D\u020F\u022B\u022D\u022F\u0231\u1D52\u1E4D\u1E4F\u1E51\u1E53\u1ECD\u1ECF\u1ED1\u1ED3\u1ED5\u1ED7\u1ED9\u1EDB\u1EDD\u1EDF\u1EE1\u1EE3\u2092\u2134\u24DE\uFF4F]/gi,
+	    p: /[\u1D56\u1E55\u1E57\u24DF\uFF50]/gi,
+	    q: /[\u02A0\u24E0\uFF51]/gi,
+	    r: /[\u0155\u0157\u0159\u0211\u0213\u02B3\u1D63\u1E59\u1E5B\u1E5D\u1E5F\u24E1\uFF52]/gi,
+	    s: /[\u015B\u015D\u015F\u0161\u017F\u0219\u02E2\u1E61\u1E63\u1E65\u1E67\u1E69\u1E9B\u24E2\uFF53]/gi,
+	    t: /[\u0163\u0165\u021B\u1D57\u1E6B\u1E6D\u1E6F\u1E71\u1E97\u24E3\uFF54]/gi,
+	    u: /[\u00F9-\u00FC\u0169\u016B\u016D\u016F\u0171\u0173\u01B0\u01D4\u01D6\u01D8\u01DA\u01DC\u0215\u0217\u1D58\u1D64\u1E73\u1E75\u1E77\u1E79\u1E7B\u1EE5\u1EE7\u1EE9\u1EEB\u1EED\u1EEF\u1EF1\u24E4\uFF55]/gi,
+	    v: /[\u1D5B\u1D65\u1E7D\u1E7F\u2174\u24E5\uFF56]/gi,
+	    w: /[\u0175\u02B7\u1E81\u1E83\u1E85\u1E87\u1E89\u1E98\u24E6\uFF57]/gi,
+	    x: /[\u02E3\u1E8B\u1E8D\u2093\u2179\u24E7\uFF58]/gi,
+	    y: /[\u00FD\u00FF\u0177\u0233\u02B8\u1E8F\u1E99\u1EF3\u1EF5\u1EF7\u1EF9\u24E8\uFF59]/gi,
+	    z: /[\u017A\u017C\u017E\u1DBB\u1E91\u1E93\u1E95\u24E9\uFF5A]/gi
+	};
+
+	word = "üBer";
+
+	_.each(accented, function(reg, letter) {
+		word = word.replace(reg, letter)
+	});
+*/
+
+
+	function ZeroWing(field) {
+		this.root = $(field);
+		this.active = $(field+'-active');
+		this.search_field = $(field+'-filter');
+
+		var default_list = this.root.find('li');
+
+		this.cache = _.map(this.root.find('label'), function(el, i) {
+			return $(el).text();
+		});
+
+		// map indices to list items
+		this.default_obj = _.object(
+			_.range(default_list.length),
+			_.map(default_list, $) // [$(n1), $(n2), ...]
+		);
+
+		this.active.find('ul').sortable({
+			axis: 'y',
+			handle: 'span'
+		});
+	}
+
+	ZeroWing.prototype = {
+
+		init: function() {
+			this._checkScrollBars();
+			this._addClassOnSelect();
+			this._moveToActiveOnSelect();
+			this._disallowClickSelection();
+			this._setupFilter();
+		},
+
+		_checkScrollBars: function() {
+			if (this.root.prop('scrollHeight') <= this.root.prop('clientHeight')) {
+				this.root.removeClass('force-scroll');
+			}
+
+			if (this.active.prop('scrollHeight') <= this.active.prop('clientHeight')) {
+				this.active.removeClass('force-scroll');
+			}
+		},
+
+		_addClassOnSelect: function() {
+			var self = this;
+
+			this.root.on('click', 'li', function(evt) {
+				evt.preventDefault();
+
+				var box = $(this).find(':checkbox');
+					was_checked = box.is(':checked');
+
+				$(this).toggleClass('selected', !was_checked);
+				box.attr('checked', !was_checked);
+
+				// refocus the search after event bubbles
+				_.defer($.proxy(self.search_field, 'focus'));
+			});
+		},
+
+		_moveToActiveOnSelect: function() {
+			var self = this,
+				active_map = {};
+
+			var default_list = this.root.find('li');
+
+
+			// bind the event
+			this.root.on('click.moveover', 'li', function(evt) {
+
+				var box = $(this).find(':checkbox');
+					is_checked = box.is(':checked');
+
+				var idx = default_list.index(this);
+
+				if ( ! is_checked) {
+					active_map[idx].remove();
+					delete active_map[idx];
+				} else {
+					var new_el = $('<li><span class="reorder-handle">&nbsp;</span>' + self.cache[idx] + '</li>');
+					$(self.active).find('ul').append(new_el);
+					active_map[idx] = new_el;
+				}
+			});
+
+			// move exisiting selections over
+			var self = this;
+			this.root.find(':checkbox').filter(':checked').closest('li').each(function() {
+				var idx = $(this).index();
+				var new_el = $('<li><span class="reorder-handle">&nbsp;</span>' + self.cache[idx] + '</li>');
+				$(self.active).find('ul').append(new_el);
+				active_map[idx] = new_el;
+			});
+		},
+
+		_setupFilter: function() {
+
+			var ul = this.root.find('ul');
+
+			this.search_field.on(
+				'interact',
+				_.debounce(
+					$.proxy(this, '_filterResults', this.default_obj, ul),
+					100
+				)
+			);
+		},
+
+		_filterResults: function(default_obj, ul, evt) {
+
+
+			var t1 = (new Date()).getTime();
+
+			// chrome won't use the custom scroll bar if you overflow before
+			// adding the class. So we add the class and remove it if it's not
+			// overflowing. Silly browsers.
+			this.root.addClass('force-scroll');
+
+			// User input and the node we're working with
+			var search = evt.target.value.toLowerCase(),
+				search_length = search.length;
+
+			// We take the element off the dom temporarily for processing.
+			// This vastly improves performance at > 500 items.
+			// Normally that makes perfect sense, but I must admit
+			// in this case it's a little strange, since we move them off-dom
+			// individually to reorder them. Something about not forcing
+			// repaints every time? Not sure, but this this works, so it's
+			// staying.
+			ul.find('li').detach();
+
+
+			// no search, show all, use default order
+			if (search_length == 0) {
+				_.each(default_obj, function(el) {
+					el[0].style.display = '';
+				});
+
+				return this._insertInOrder(ul, default_obj);
+			}
+
+			// compute a score for each item in the list
+			var scores = _.map(
+				this.cache,
+				_.partial(this._scoreString, search)
+			);
+
+			// Manually hide and unhide. Could be prettier, but can't be quicker.
+			_.each(default_obj, function(el, i) {
+				if (scores[i] === 0) {
+					el[0].style.display = 'none';
+				} else {
+					el[0].style.display = '';
+				}
+			});
+
+			// Create an array of numbers from 0 to n, where n
+			// is the total number of items. The numbers shall
+			// be sorted in the desired final sorting order.
+			var order = _.sortBy(
+				_.range(this.cache.length),
+				function(i) { return -scores[i]; }
+			);
+
+			// Move li's to the desired positions
+			this._insertInOrder(ul, default_obj, order);
+
+			// And finally show hide the scroll bar
+			this._checkScrollBars();
+			
+
+			/* Alternative sorting algorithm:
+
+			// Does fuzzy match but keeps the list alphabetic
+			// much easier implementation but doesn't always return
+			// the most intuitive result.
+
+			var textMatch = evt.target.value.replace(/\W/, '').split('').join('\\w*'),
+				reg = new RegExp(textMatch, 'i'),
+				that = this;
+
+			var matches = _.filter(this.cache, function(el, i) {
+				var show = !! el.replace(/\W/, '').match(reg);
+				that.root.find('li').eq(i).toggle(show);
+				return show;
+			});
+
+			that._checkScrollBar();
+			*/
+		},
+
+		/*
+		A cutesy attempt at best-match fuzzy matching. The fuzzy
+		matching part can actually be done quite simply with some
+		regex (see commented out portion at bottom of function).
+		The problem is that those results aren't the most natural
+		unless you can order them logically. This code tries to
+		do just that.
+		*/
+		// Search string, list item
+		_scoreString: function(search, item) {
+			var score = 0,
+				letter_offset = 1,
+				item_length = item.length,
+				search_length = search.length;
+
+			item = item.toLowerCase();
+
+			// First letter match is an big plus
+			if (item[0] == search[0]) {
+				score += 1;
+			}
+
+			for (var i = 0; i < search_length; i++)
+			{
+				var char_loc = item.indexOf(
+					search.charAt(i).toLowerCase()
+				);
+
+				switch (char_loc) {
+					case -1: return 0;				// not found, not our word
+					case  0: score += 0.6;			// first position, good
+						if (i == letter_offset)		// consecutive, better
+							score += 0.4;
+						break;
+					default: score += 0.4 / letter_offset	// meh
+				}
+
+				letter_offset += char_loc;
+				item = item.substr(char_loc + 1);
+			}
+
+			// Score per letter * letter per item letter looked at
+			return (score / search_length) * (search_length / letter_offset);
+		},
+
+		_insertInOrder: function(parent, items, order) {
+
+			if ( ! order) {
+				order = _.range(_.size(items));
+			}
+
+			// I know it's tempting to do this with a simple jquery append
+			// on a dummy object. Don't do it - it's sloooow.
+
+			var dummy = document.createElement('ul');
+
+			_.each(order, function(i) {
+				dummy.appendChild(items[i][0]);
+			});
+
+			var children = _.toArray(dummy.childNodes),
+				childLength = children.length,
+				i = 0;
+
+
+			// Performance tweak, to make it feel more responsive. Appending
+			// even as a documentFragment requires a lot of style calculations
+			// that block the rendering process. Since the user never sees more
+			// than 20 items, we'll do the first 100 immediately and then defer
+			// on the others in steps of 100.
+
+			(function batch() {
+				parent.append(children.slice(i++, 100));
+
+				if (i < childLength) {
+					_.defer(batch);
+				}
+			})();
+		},
+
+
+		// Quick clicking can sometimes lead to double and triple
+		// click selections. If we think that might have happened
+		// we'll simply remove them.
+
+		_disallowClickSelection: function() {
+			var cnt = 0,
+				self = this;
+
+			this.root
+				.dblclick(self._deselect)
+				.click(function() {
+					cnt++;
+					_.debounce(function() {
+						cnt = 0;
+					}, 500);
+
+					if (cnt >= 2) {
+						self._deselect();
+					}
+				}
+			);
+		},
+
+		_deselect: function() {
+			// Aren't you glad we wrote that rte and speak fluent range
+
+			if (window.getSelection) {
+				window.getSelection().removeAllRanges();
+			} else if (document.selection) {
+				document.selection.empty();
+			}
+		}
+	};
+
+	EE.setup_multi_field = function(el) {
+		new ZeroWing(el).init();
+	};
+
+})(jQuery);
