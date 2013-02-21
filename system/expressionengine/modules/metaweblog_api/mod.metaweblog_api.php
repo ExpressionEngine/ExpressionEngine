4b2a9ddbff8115223d30ee8e1e32cc6d3ab285a9
Robin Sowell <robin.sowell@ellislab.com>
2013-02-21 13:17:00 -0500

    Fixed a bug (#19223) in the Metaweblog API where failed new entries did not elicit a proper fault code.

--- system/expressionengine/modules/metaweblog_api/mod.metaweblog_api.php
+++ system/expressionengine/modules/metaweblog_api/mod.metaweblog_api.php
@@ -404,11 +404,15 @@ class Metaweblog_api {
 		{
 			$errors = $this->EE->api_channel_entries->get_errors();
 
-			$response = array(
-				'errors' => array($errors, 'array')
-			);
+			$this->EE->lang->loadfile('content');
+			$mssg = "\n";
+			
+			foreach ($errors as $val)
+			{
+				$mssg .= lang($val)."\n";
+			}
 
-			return $this->EE->xmlrpc->send_response($response);
+			return $this->EE->xmlrpc->send_error_message('804', lang('new_entry_errors').$mssg);
 		}
 		
 		//Return Entry ID of new entry - defaults to string, so nothing fancy
