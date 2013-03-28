0567311a284b7499a2978c37d031c594d1be174f
Pascal Kriete <pascal@pascalkriete.com>
2013-03-28 19:43:49 -0400

    More docblocks

--- system/expressionengine/libraries/Channel_entries_parser.php
+++ system/expressionengine/libraries/Channel_entries_parser.php
@@ -46,6 +46,7 @@ class EE_Channel_entries_parser {
 	 *
 	 * @param prefix  - A prefix to apply to all data and tags.Allow for nesting
 	 *					of similar tags.
+	 *
 	 * @return Object<EE_Channel_parser>
 	 */
 	public function create($tagdata, $prefix = '')
@@ -111,54 +112,131 @@ class EE_Channel_parser {
 		$this->_plugins = $plugins;
 	}
 
+	// --------------------------------------------------------------------
+
+	/**
+	 * Tagdata getter
+	 *
+	 * Returns the tag chunk that the parser should process
+	 *
+	 * @return String	tagdata
+	 */
 	public function tagdata()
 	{
 		return $this->_tagdata;
 	}
 
+	// --------------------------------------------------------------------
+
+	/**
+	 * Prefix getter
+	 *
+	 * Returns a prefix if one was specified in the constructor.
+	 *
+	 * @return String	prefix
+	 */
 	public function prefix()
 	{
 		return $this->_prefix;
 	}
 
+	// --------------------------------------------------------------------
+
+	/**
+	 * Plugins getter
+	 *
+	 * Plugins handle all of the actual heavy lifting. You can add your
+	 * own by calling EE->Channel_entries_parser->register_plugin().
+	 *
+	 * @return Object<EE_Channel_parser_plugins>
+	 */
 	public function plugins()
 	{
 		return $this->_plugins;
 	}
 
-	public function pre_parser(Channel $channel, array $entry_ids, array $config = array())
-	{
-		return new EE_Channel_preparser($channel, $this, $entry_ids, $config);
-	}
+	// --------------------------------------------------------------------
 
-	public function data_parser(EE_Channel_preparser $pre)
-	{
-		return new EE_Channel_data_parser($pre, $this);
-	}
-
-	/*
-
-	// short way
-	$p = new Parser($tagdata, $prefix);
-	$p->parse($channel, $entries, array( ... ));
-
-	// long way
-	$pre = $p->pre_parser($channel);
-	$parser = $p->data_parser($pre);
-
-	$parser->parse($entries, array(
-		'disable' => array('relationships', 'categories'),
-		'callbacks' => array(
-			'pre_loop' => array($this, 'method');
-		)
-	));
-
-	*/
+	/**
+	 * Parser untilty function parser
+	 *
+	 * Does in one call what the pre_parser() and data_parser() typically
+	 * do in two calls. A little less flexible since you don't have access
+	 * to the preparser before the data is parsed, but usually this is what
+	 * you want.
+	 *
+	 * @param channel - The current channel object. Used to get access to the
+	 *					custom fields. They are stored in public arrays so we
+	 *					cannot assume they remain unchanged =( .
+	 *
+	 * @param entries - An array of data arrays. Required: 'entries'.
+	 *
+	 *	  entries	 =>	array of {entry_id => row_data} that should be
+	 *					used as the data for the template.
+	 *	  categories => array of {category_id => cat_data}
+	 *
+	 *	  Other keys as required by parsing plugins.
+	 *
+	 * @param config  - Additional configuration options, such as
+	 *
+	 *	  disabled   => Skip specific parsing steps
+	 *	  callbacks  => Hook into certain parsing steps for more processing
+	 *
+	 * @return Object<EE_Channel_data_parser>
+	 */
 	public function parse(Channel $channel, array $entries, array $config = array())
 	{
 		$pre = $this->pre_parser($channel, array_keys($entries), $config);
 		$parser = $this->data_parser($pre);
 
 		return $parser->parse($entries, $config);
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * The pre-parser factory
+	 *
+	 * Parsing happens in two steps. We first take a look at the tagdata and
+	 * doing any required prep works. This lets us avoid heavy computation in
+	 * the replacement loop. The pre-parser is step one.
+	 *
+	 * @param channel   - The current channel object. Used to get access to the
+	 *					  custom fields. They are stored in public arrays so we
+	 *					  cannot assume they remain unchanged =( .
+	 *
+	 * @param entry_ids - An array of entry ids. This can be used to retrieve
+	 *					  additional data ahead of time. A good example of that
+	 *					  would be the relationship parser.
+	 *
+	 * @param config    - A configuration array:
+	 *
+	 *	 disabled:	(array) Skip specific parsing steps
+	 *				Takes the same values as the channel module's disable
+	 *				parameter, which is one of its uses.
+	 *
+	 * @return Object<EE_Channel_preparser>
+	 */
+	public function pre_parser(Channel $channel, array $entry_ids, array $config = array())
+	{
+		return new EE_Channel_preparser($channel, $this, $entry_ids, $config);
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * Data parser
+	 *
+	 * After the tagdata has been processed by the preparser, it's time to
+	 * iterate over the actual data rows and assemble a final template. That
+	 * is what the data parser is for.
+	 *
+	 * @param Object<EE_Channel_preparser> - a preparsed tag chunk
+	 *
+	 * @return Object<EE_Channel_data_parser>
+	 */
+	public function data_parser(EE_Channel_preparser $pre)
+	{
+		return new EE_Channel_data_parser($pre, $this);
 	}
 }
