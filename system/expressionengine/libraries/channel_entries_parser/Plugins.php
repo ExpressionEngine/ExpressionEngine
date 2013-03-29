bc4366855f36d4ce6f52fcf7eed755415d93adac
Pascal Kriete <pascal@pascalkriete.com>
2013-03-28 20:08:32 -0400

    More docblocks.

--- system/expressionengine/libraries/channel_entries_parser/Plugins.php
+++ system/expressionengine/libraries/channel_entries_parser/Plugins.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * ExpressionEngine - by EllisLab
  *
@@ -11,18 +10,8 @@
  * @since		Version 2.0
  * @filesource
  */
- 
-// ------------------------------------------------------------------------
 
-/**
- * ExpressionEngine Channel Parser Plugins
- *
- * @package		ExpressionEngine
- * @subpackage	Core
- * @category	Core
- * @author		EllisLab Dev Team
- * @link		http://ellislab.com
- */
+// ------------------------------------------------------------------------
 
 require_once APPPATH.'libraries/channel_entries_parser/plugins/Category.php';
 require_once APPPATH.'libraries/channel_entries_parser/plugins/Header_and_footer.php';
@@ -36,9 +25,25 @@ require_once APPPATH.'libraries/channel_entries_parser/plugins/Simple_conditiona
 require_once APPPATH.'libraries/channel_entries_parser/plugins/Simple_variable.php';
 require_once APPPATH.'libraries/channel_entries_parser/plugins/Switch.php';
 
-// @todo check if plugin exists when registering
+// ------------------------------------------------------------------------
+
+/**
+ * ExpressionEngine Channel Parser Plugins
+ *
+ * @package		ExpressionEngine
+ * @subpackage	Core
+ * @category	Core
+ * @author		EllisLab Dev Team
+ * @link		http://ellislab.com
+ */
 class EE_Channel_parser_plugins {
 
+	/**
+	 * These do not actually behave separately at present, but you
+	 * may encounter edge-case bugs if you try to parse single tags
+	 * before the tag pairs are out of the way, so you're encouraged
+	 * to use the correct type for your plugin.
+	 */
 	protected $pair_plugins = array();
 	protected $single_plugins = array();
 
@@ -47,7 +52,7 @@ class EE_Channel_parser_plugins {
 		// Dear third party devs, use the register_plugin method on
 		// EE->channel_entries_parser for your own additions. Gracias.
 
-		// don't mess with the order, it matters!
+		// Prep built-in parsers. Don't mess with the order, it matters!
 		$this->register_pair('EE_Channel_category_parser');
 		$this->register_pair('EE_Channel_custom_field_pair_parser');
 		$this->register_pair('EE_Channel_header_and_footer_parser');
@@ -62,21 +67,49 @@ class EE_Channel_parser_plugins {
 		$this->register_single('EE_Channel_custom_member_field_parser');
 	}
 
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Register a plugin that parses a pair tag.
+	 *
+	 * @param String	Class name of new plugin
+	 */
 	public function register_pair($class)
 	{
 		$this->pair_plugins[] = new $class();
 	}
 
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Register a plugin that parses a single tag.
+	 *
+	 * @param String	Class name of new plugin
+	 */
 	public function register_single($class)
 	{
 		$this->single_plugins[] = new $class();
 	}
 
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Pair tag parsing plugins
+	 *
+	 * @return Array	List of pair tag parsing plugins
+	 */
 	public function pair()
 	{
 		return $this->pair_plugins;
 	}
 
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Single tag parsing plugins
+	 *
+	 * @return Array	List of single tag parsing plugins
+	 */
 	public function single()
 	{
 		return $this->single_plugins;
@@ -84,11 +117,54 @@ class EE_Channel_parser_plugins {
 }
 
 
+// ------------------------------------------------------------------------
+
+/**
+ * ExpressionEngine Channel Parser Plugin Interface
+ *
+ * @package		ExpressionEngine
+ * @subpackage	Core
+ * @category	Core
+ * @author		EllisLab Dev Team
+ * @link		http://ellislab.com
+ */
 interface EE_Channel_parser_plugin {
 
-	public function disabled(array $disabled); // return bool
-	public function replace($tagdata, EE_Channel_data_parser $obj, $pre); // return $tagdata
+	/**
+	 * Check if your plugin or something something parsed by it
+	 * is in the disable= parameter.
+	 *
+	 * @param array		A list of "disabled" features
+	 * @return Boolean	Is disabled?
+	 */
+	public function disabled(array $disabled);
 
-//	public function clear(); // remove leftovers?
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Do any pre-processing on the tagdata or other data available
+	 * through the pre-parser.
+	 *
+	 * The return value of this method will be passed to the replace
+	 * method as a third parameter.
+	 *
+	 * @param String	The tagdata to be parsed
+	 * @param Object	The preparser object.
+	 * @return mixed	[optional]
+	 */
+	public function pre_process($tagdata, EE_Channel_preparser $pre);
+
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Replace all tags that this plugin can deal with.
+	 *
+	 * @param String	The tagdata to be parsed
+	 * @param Object	The channel parser object
+	 * @param Mixed		The results from the preparse method
+	 *
+	 * @return String	The processed tagdata
+	 */
+	public function replace($tagdata, EE_Channel_data_parser $obj, $pre_process_result);
 
 }
