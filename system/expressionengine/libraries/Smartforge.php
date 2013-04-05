205ab5ddf7fe6e5f3e0053cd4045e6dbc0c9b94a
Kevin Smith <kevin@gohearsay.com>
2013-04-05 14:06:00 -0500

    Avoids complex string syntax for all usages of ee()-> in SmartForge.

--- system/expressionengine/libraries/Smartforge.php
+++ system/expressionengine/libraries/Smartforge.php
@@ -48,7 +48,7 @@ class Smartforge {
 		// Check to make sure table doesn't already exist
 		if (ee()->db->table_exists($table))
 		{
-			ee()->logger->updater("Could not create table '{ee()->db->dbprefix}$table'. Table already exists.", TRUE);
+			ee()->logger->updater("Could not create table '".ee()->db->dbprefix."$table'. Table already exists.", TRUE);
 
 			return FALSE;
 		}
@@ -73,7 +73,7 @@ class Smartforge {
 		// Check to make sure table exists
 		if ( ! ee()->db->table_exists($table))
 		{
-			ee()->logger->updater(__METHOD__." failed. Table '{ee()->db->dbprefix}$table' does not exist.", TRUE);
+			ee()->logger->updater(__METHOD__." failed. Table '".ee()->db->dbprefix."$table' does not exist.", TRUE);
 
 			return FALSE;
 		}
@@ -83,7 +83,7 @@ class Smartforge {
 			return ee()->dbforge->rename_table($table, $new_table);
 		}
 
-		ee()->logger->updater("Could not rename '{ee()->db->dbprefix}$table' to '{ee()->db->dbprefix}$new_table'. Table '{ee()->db->dbprefix}$new_table' already exists.", TRUE);
+		ee()->logger->updater("Could not rename '".ee()->db->dbprefix."$table' to '".ee()->db->dbprefix."$new_table'. Table '".ee()->db->dbprefix."$new_table' already exists.", TRUE);
 
 		return FALSE;
 	}
@@ -108,7 +108,7 @@ class Smartforge {
 		// Check to make sure table exists
 		if ( ! ee()->db->table_exists($table))
 		{
-			ee()->logger->updater(__METHOD__." failed. Table '{ee()->db->dbprefix}$table' does not exist.", TRUE);
+			ee()->logger->updater(__METHOD__." failed. Table '".ee()->db->dbprefix."$table' does not exist.", TRUE);
 
 			return FALSE;
 		}
@@ -126,7 +126,7 @@ class Smartforge {
 			}
 			else
 			{
-				ee()->logger->updater("Could not add column '{ee()->db->dbprefix}$table.$k'. Column already exists.", TRUE);
+				ee()->logger->updater("Could not add column '".ee()->db->dbprefix."$table.$k'. Column already exists.", TRUE);
 			}
 		}
 
@@ -150,7 +150,7 @@ class Smartforge {
 		// Check to make sure table exists
 		if ( ! ee()->db->table_exists($table))
 		{
-			ee()->logger->updater(__METHOD__." failed. Table '{ee()->db->dbprefix}$table' does not exist.", TRUE);
+			ee()->logger->updater(__METHOD__." failed. Table '".ee()->db->dbprefix."$table' does not exist.", TRUE);
 
 			return FALSE;
 		}
@@ -160,7 +160,7 @@ class Smartforge {
 			return ee()->dbforge->drop_column($table, $column_name);
 		}
 
-		ee()->logger->updater("Could not drop column '{ee()->db->dbprefix}$table.$column_name'. Column does not exist.", TRUE);
+		ee()->logger->updater("Could not drop column '".ee()->db->dbprefix."$table.$column_name'. Column does not exist.", TRUE);
 
 		return FALSE;
 	}
@@ -191,7 +191,7 @@ class Smartforge {
 		// Check to make sure table exists
 		if ( ! ee()->db->table_exists($table))
 		{
-			ee()->logger->updater(__METHOD__." failed. Table '{ee()->db->dbprefix}$table' does not exist.", TRUE);
+			ee()->logger->updater(__METHOD__." failed. Table '".ee()->db->dbprefix."$table' does not exist.", TRUE);
 
 			return FALSE;
 		}
@@ -210,7 +210,7 @@ class Smartforge {
 					// Drop column A.
 					ee()->dbforge->drop_column($table, $k);
 
-					ee()->logger->updater("Could not rename column '{ee()->db->dbprefix}$table.$k' to '{ee()->db->dbprefix}$table.{$v['name']}' since it already exists. Column '{ee()->db->dbprefix}$table.$k' was removed to clean up.", TRUE);
+					ee()->logger->updater("Could not rename column '".ee()->db->dbprefix."$table.$k' to '".ee()->db->dbprefix."$table.{$v['name']}' since it already exists. Column '".ee()->db->dbprefix."$table.$k' was removed to clean up.", TRUE);
 				}
 				else
 				{
@@ -224,7 +224,7 @@ class Smartforge {
 			}
 			else
 			{
-				ee()->logger->updater("Could not modify column '{ee()->db->dbprefix}$table.$k'. Column does not exist.", TRUE);
+				ee()->logger->updater("Could not modify column '".ee()->db->dbprefix."$table.$k'. Column does not exist.", TRUE);
 			}
 
 
@@ -254,7 +254,7 @@ class Smartforge {
 		// Check to make sure table exists
 		if ( ! ee()->db->table_exists($table))
 		{
-			ee()->logger->updater(__METHOD__." failed. Table '{ee()->db->dbprefix}$table' does not exist.", TRUE);
+			ee()->logger->updater(__METHOD__." failed. Table '".ee()->db->dbprefix."$table' does not exist.", TRUE);
 
 			return FALSE;
 		}
@@ -311,7 +311,7 @@ class Smartforge {
 		// Check to make sure table exists
 		if ( ! ee()->db->table_exists($table))
 		{
-			ee()->logger->updater(__METHOD__." failed. Table '{ee()->db->dbprefix}$table' does not exist.", TRUE);
+			ee()->logger->updater(__METHOD__." failed. Table '".ee()->db->dbprefix."$table' does not exist.", TRUE);
 
 			return FALSE;
 		}
@@ -327,18 +327,18 @@ class Smartforge {
 		}
 
 		// Check to make sure this key doesn't already exist.
-		$query = ee()->db->query("SHOW INDEX FROM {ee()->db->dbprefix}.$table WHERE Key_name = '{$key_name}'");
+		$query = ee()->db->query("SHOW INDEX FROM ".ee()->db->dbprefix."$table WHERE Key_name = '{$key_name}'");
 
 		if ($query->num_rows() == 0)
 		{
 			// Create key
 			if ($key_name == 'PRIMARY')
 			{
-				$sql = "ALTER TABLE `{ee()->db->dbprefix}.$table` ADD PRIMARY KEY (`{$col_name}`)";
+				$sql = "ALTER TABLE `".ee()->db->dbprefix."$table` ADD PRIMARY KEY (`{$col_name}`)";
 			}
 			else
 			{
-				$sql = "ALTER TABLE `{ee()->db->dbprefix}.$table` ADD INDEX {$key_name} (`{$col_name}`)";
+				$sql = "ALTER TABLE `".ee()->db->dbprefix."$table` ADD INDEX {$key_name} (`{$col_name}`)";
 			}			
 
 			if (ee()->db->query($sql) === TRUE)
@@ -347,7 +347,7 @@ class Smartforge {
 			}
 		}
 
-		ee()->logger->updater("Could not create key '$key_name' on table '{ee()->db->dbprefix}$table'. Key already exists.", TRUE);
+		ee()->logger->updater("Could not create key '$key_name' on table '".ee()->db->dbprefix."$table'. Key already exists.", TRUE);
 
 		return FALSE;
 	}
@@ -369,13 +369,13 @@ class Smartforge {
 		// Check to make sure table exists
 		if ( ! ee()->db->table_exists($table))
 		{
-			ee()->logger->updater(__METHOD__." failed. Table '{ee()->db->dbprefix}$table' does not exist.", TRUE);
+			ee()->logger->updater(__METHOD__." failed. Table '".ee()->db->dbprefix."$table' does not exist.", TRUE);
 
 			return FALSE;
 		}
 
 		// Check to make sure this key exists.
-		$query = ee()->db->query("SHOW INDEX FROM {ee()->db->dbprefix}.$table WHERE Key_name = '{$key_name}'");
+		$query = ee()->db->query("SHOW INDEX FROM ".ee()->db->dbprefix."$table WHERE Key_name = '{$key_name}'");
 
 		if ($query->num_rows() !== 0)
 		{
@@ -384,11 +384,11 @@ class Smartforge {
 			{
 				// This should be rare since MySQL requires auto-increment
 				// columns to have a primary key.
-				$sql = "ALTER TABLE {$table} DROP PRIMARY KEY";
+				$sql = "ALTER TABLE `".ee()->db->dbprefix."$table` DROP PRIMARY KEY";
 			}
 			else
 			{
-				$sql = "ALTER TABLE {$table} DROP KEY {$key_name}";
+				$sql = "ALTER TABLE `".ee()->db->dbprefix."$table` DROP KEY {$key_name}";
 			}
 
 			if (ee()->db->query($sql) === TRUE)
@@ -397,7 +397,7 @@ class Smartforge {
 			}
 		}
 
-		ee()->logger->updater("Could not drop key '$key_name' from table '{ee()->db->dbprefix}$table'. Key does not exist.", TRUE);
+		ee()->logger->updater("Could not drop key '$key_name' from table '".ee()->db->dbprefix."$table'. Key does not exist.", TRUE);
 
 		return FALSE;
 	}
