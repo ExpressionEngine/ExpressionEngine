1a4bebc3220eac62dc84347788028bca11c20a7d
Robin Sowell <robin.sowell@ellislab.com>
2013-04-05 15:42:20 -0400

    Default field types had relationships going in as 'rel' instead of 'relationship'

--- system/installer/schema/mysql_schema.php
+++ system/installer/schema/mysql_schema.php
@@ -1427,7 +1427,7 @@ class EE_Schema {
 		}
 	
 		// Default field types
-		$default_fts = array('select', 'text', 'textarea', 'date', 'file', 'multi_select', 'checkboxes', 'radio', 'rel', 'rte');
+		$default_fts = array('select', 'text', 'textarea', 'date', 'file', 'multi_select', 'checkboxes', 'radio', 'relationship', 'rte');
 		
 		foreach($default_fts as $name)
 		{
