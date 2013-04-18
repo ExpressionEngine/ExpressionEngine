16e159c774bf48a5a5e45b9959436f1661b0ae5d
Pascal Kriete <pascal@pascalkriete.com>
2013-04-18 15:55:40 -0400

    Removing references to old channel titles fields. Ick.

--- system/expressionengine/libraries/api/Api_channel_fields.php
+++ system/expressionengine/libraries/api/Api_channel_fields.php
@@ -1201,7 +1201,6 @@ class Api_channel_fields extends Api {
 			'field_name', 'field_label', 'field_instructions',
 			'field_type', 'field_list_items', 'field_pre_populate',
 			'field_pre_channel_id', 'field_pre_field_id',
-			'field_related_id', 'field_related_orderby', 'field_related_sort', 'field_related_max',
 			'field_ta_rows', 'field_maxl', 'field_required',
 			'field_text_direction', 'field_search', 'field_is_hidden', 'field_fmt', 'field_show_fmt',
 			'field_order'
