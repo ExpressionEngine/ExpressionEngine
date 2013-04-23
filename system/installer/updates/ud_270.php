c353eaec37e776c8554b6c95396f26840e84d044
Pascal Kriete <pascal@pascalkriete.com>
2013-04-23 16:53:38 -0400

    Removing updated sites.

--- system/installer/updates/ud_270.php
+++ system/installer/updates/ud_270.php
@@ -34,6 +34,54 @@ class Updater {
 	 */
 	public function do_update()
 	{
+		ee()->load->dbforge();
+
+		$steps = new ProgressIterator(
+			array(
+				'_drop_updated_pings',
+				'_drop_updated_sites'
+			)
+		);
+
+		foreach ($steps as $k => $v)
+		{
+			$this->$v();
+		}
+
+		return TRUE;
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * Drop ping data and columns
+	 */
+	private function _drop_pings()
+	{
+
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * Drop updated sites module data
+	 */
+	private function _drop_updated_sites()
+	{
+		$query = ee()->db
+			->select('module_id')
+			->get_where('modules', array('module_name' => 'Updated_sites'));
+
+		if ($query->num_rows())
+		{
+			ee()->db->delete('module_member_groups', array('module_id' => $query->row('module_id')));
+			ee()->db->delete('modules', array('module_name' => 'Updated_sites'));
+			ee()->db->delete('actions', array('class' => 'Updated_sites'));
+
+			ee()->dbforge->drop_table('updated_sites');
+			ee()->dbforge->drop_table('updated_site_pings');			
+		}
+
 		return TRUE;
 	}
 }	
