7171e8f350f5a3c35f37a54d98b9e79d6d95c515
Pascal Kriete <pascal@pascalkriete.com>
2013-04-23 17:20:18 -0400

    Mobile and corporate go the way of Fruit.

--- themes/cp_themes/mobile/tools/tools_data.php
+++
@@ -1,9 +0,0 @@
-<ul id="tools_data" title="<?=lang('tools_data')?>">
-	<li><a href="<?=BASE.AMP."C=tools_data".AMP."M=clear_caching"?>"><?=lang('clear_caching')?></a></li>
-	<li><a href="<?=BASE.AMP."C=tools_data".AMP."M=sql_manager"?>"><?=lang('sql_manager')?></a></li>
-	<li><a href="<?=BASE.AMP."C=tools_data".AMP."M=search_and_replace"?>"><?=lang('search_and_replace')?></a></li>
-	<li><a href="<?=BASE.AMP."C=tools_data".AMP."M=recount_stats"?>"><?=lang('recount_stats')?></a></li>
-</ul>
-<?php
-/* End of file tools_data.php */
-/* Location: ./themes/cp_themes/mobile/tools/tools_data.php */
