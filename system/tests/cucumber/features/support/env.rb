6e83dc07c5d0bbb5576b6ed1b07c02227789602d
Kevin Cupp <kevin.cupp@gmail.com>
2013-04-28 17:24:29 -0400

    Some Cucumber tweaks

--- system/tests/cucumber/features/support/env.rb
+++ system/tests/cucumber/features/support/env.rb
@@ -5,6 +5,7 @@ require 'capybara/rspec'
 Capybara.run_server = false
 Capybara.default_selector = :css
 Capybara.default_driver = :webkit
+Capybara.javascript_driver = :webkit
 
 # Make a file called env_local.rb and set this
 # Capybara.app_host = 'http://expressionengine2/'
