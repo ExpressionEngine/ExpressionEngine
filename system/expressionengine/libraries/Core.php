de6a03d26c1006e3dda557f79cae950579891d0b
Wes Baker <wes.baker@ellislab.com>
2013-04-30 17:50:53 -0400

    Version and build number bumps.

--- system/expressionengine/libraries/Core.php
+++ system/expressionengine/libraries/Core.php
@@ -86,8 +86,8 @@ class EE_Core {
 		// application constants
 		define('IS_CORE',		FALSE);
 		define('APP_NAME',		'ExpressionEngine'.(IS_CORE ? ' Core' : ''));
-		define('APP_BUILD',		'20130315');
-		define('APP_VER',		'2.6.0');
+		define('APP_BUILD',		'20130501');
+		define('APP_VER',		'2.6.1');
 		define('SLASH',			'&#47;');
 		define('LD',			'{');
 		define('RD',			'}');
