6e7e4455a98fda0710139462c49e4b3f676db694
Pascal Kriete <pascal@pascalkriete.com>
2013-06-21 15:03:59 -0400

    Removing some commented out experimental code

--- system/expressionengine/libraries/EE_Javascript.php
+++ system/expressionengine/libraries/EE_Javascript.php
@@ -82,25 +82,6 @@ class EE_Javascript extends CI_Javascript {
 	 */
 	function compile($view_var = 'script_foot', $script_tags = TRUE)
 	{
-		// Experimental reduction of inline js
-		/*
-		foreach($this->js->jquery_code_for_compile as $k => $v)
-		{
-			// Remove Comments
-			$v = preg_replace('/\/\/[^\n\r\'";{}]*[\n\r]/', ' ', $v);
-			$v = preg_replace('/\/\*[^*]*\*+([^\/\'";{}][^*]*\*+)*\//', ' ', $v);
-			
-			// Safe Whitespace Replacements
-			$v = preg_replace('/[\t ]+/', ' ', $v);
-			$v = preg_replace('/(\s*?\n+\s*?)+/', "\n", $v);
-			
-			// Common Ones
-			$v = preg_replace('/([;]\n|\) \{|\{\n)/', '', $v);
-			
-			$this->js->jquery_code_for_compile[$k] = $v;
-		}
-		*/
-		
 		parent::compile($view_var, $script_tags);
 		
 		$global_js = $this->inline('
