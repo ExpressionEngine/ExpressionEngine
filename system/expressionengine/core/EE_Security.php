0776b61eb03c0a7c6ef24c6981e77a8082ab9ac6
Pascal Kriete <pascal@pascalkriete.com>
2013-07-05 11:21:57 -0400

    Making sure only ajax requests with a valid xid get a new xid in return.

--- system/expressionengine/core/EE_Security.php
+++ system/expressionengine/core/EE_Security.php
@@ -89,7 +89,7 @@ class EE_Security extends CI_Security {
 		define('REQUEST_XID', $request_xid);
 		define('XID_SECURE_HASH', $hash);
 
-		if (AJAX_REQUEST)
+		if (AJAX_REQUEST && count($_POST))
 		{
 			header('X-EEXID: '.XID_SECURE_HASH);
 		}
