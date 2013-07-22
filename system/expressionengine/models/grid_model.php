7e174fb844d1470fb796e805286907c832ff58d1
Kevin Cupp <kevin.cupp@gmail.com>
2013-07-22 15:28:21 -0400

    Adding hooks for Grid

--- system/expressionengine/models/grid_model.php
+++ system/expressionengine/models/grid_model.php
@@ -359,13 +359,35 @@ class Grid_model extends CI_Model {
 
  			ee()->load->helper('array_helper');
 
- 			$rows = ee()->db->where_in('entry_id', $entry_ids)
+ 			ee()->db->where_in('entry_id', $entry_ids)
  				->order_by(
  					element('orderby', $options, 'row_order'),
  					element('sort', $options, 'asc')
- 				)
- 				->get($this->_data_table($entity_name, $field_id))
- 				->result_array();
+ 				);
+
+ 			// -------------------------------------------
+ 			// 'relationships_query' hook.
+ 			// - Allows developers to modify and run the query for Grid data
+ 			//
+ 				if (ee()->extensions->active_hook('grid_query') === TRUE)
+ 				{
+ 					$rows = ee()->extensions->call(
+ 						'grid_query',
+ 						$entry_ids,
+ 						$field_id,
+ 						$entity_name,
+ 						$this->_data_table($entity_name, $field_id),
+ 						ee()->db->_compile_select(FALSE, FALSE)
+ 					);
+ 				}
+ 				else
+ 				{
+ 					$rows = ee()->db->get(
+ 						$this->_data_table($entity_name, $field_id)
+ 					)->result_array();
+ 				}
+ 			//
+ 			// -------------------------------------------
 
  			// Add these rows to the cache
  			foreach ($rows as $row)
@@ -697,19 +719,37 @@ class Grid_model extends CI_Model {
  			->get($table_name)
  			->result_array();
 
+ 		// Put rows into an array for easy passing and returning for the hook
+ 		$data = array(
+ 			'new_rows' => $new_rows,
+ 			'updated_rows' => $updated_rows,
+ 			'deleted_rows' => $deleted_rows
+ 		);
+
+ 		// -------------------------------------------
+ 		// 'relationships_post_save' hook.
+ 		//  - Allow developers to modify or add to the Grid data array before saving
+ 		//
+ 			if (ee()->extensions->active_hook('grid_save') === TRUE)
+ 			{
+ 				$data = ee()->extensions->call('grid_save', $entry_id, $field_id, $entity_name, $data);
+ 			}
+ 		//
+ 		// -------------------------------------------
+
  		// Batch update and insert rows to save queries
- 		if ( ! empty($updated_rows))
+ 		if ( ! empty($data['updated_rows']))
  		{
- 			ee()->db->update_batch($table_name, $updated_rows, 'row_id');
+ 			ee()->db->update_batch($table_name, $data['updated_rows'], 'row_id');
  		}
 
- 		if ( ! empty($new_rows))
+ 		if ( ! empty($data['new_rows']))
  		{
- 			ee()->db->insert_batch($table_name, $new_rows);
+ 			ee()->db->insert_batch($table_name, $data['new_rows']);
  		}
 
  		// Return deleted row IDs
- 		return $deleted_rows;
+ 		return $data['deleted_rows'];
  	}
 
  	// ------------------------------------------------------------------------
