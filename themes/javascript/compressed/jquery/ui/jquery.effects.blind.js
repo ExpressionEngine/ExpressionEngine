3e5b2c9fca423bbd03aff2321617edb59b60cfcc
Daniel Bingham <daniel.bingham@ellislab.com>
2013-08-07 17:28:48 -0400

    Recompressing the js source pre-build.

--- themes/javascript/compressed/jquery/ui/jquery.effects.blind.js
+++ themes/javascript/compressed/jquery/ui/jquery.effects.blind.js
@@ -11,5 +11,5 @@
  *	jquery.effects.core.js
  */
 
-(function(b){b.effects.blind=function(c){return this.queue(function(){var a=b(this),g=["position","top","bottom","left","right"],f=b.effects.setMode(a,c.options.mode||"hide"),d=c.options.direction||"vertical";b.effects.save(a,g);a.show();var e=b.effects.createWrapper(a).css({overflow:"hidden"}),h="vertical"==d?"height":"width",d="vertical"==d?e.height():e.width();"show"==f&&e.css(h,0);var i={};i[h]="show"==f?d:0;e.animate(i,c.duration,c.options.easing,function(){"hide"==f&&a.hide();b.effects.restore(a,
+(function(b,l){b.effects.blind=function(c){return this.queue(function(){var a=b(this),g=["position","top","bottom","left","right"],f=b.effects.setMode(a,c.options.mode||"hide"),d=c.options.direction||"vertical";b.effects.save(a,g);a.show();var e=b.effects.createWrapper(a).css({overflow:"hidden"}),h="vertical"==d?"height":"width",d="vertical"==d?e.height():e.width();"show"==f&&e.css(h,0);var k={};k[h]="show"==f?d:0;e.animate(k,c.duration,c.options.easing,function(){"hide"==f&&a.hide();b.effects.restore(a,
 g);b.effects.removeWrapper(a);c.callback&&c.callback.apply(a[0],arguments);a.dequeue()})})}})(jQuery);
