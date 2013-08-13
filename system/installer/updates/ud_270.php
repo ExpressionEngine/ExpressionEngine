0f1a9712af5d690205ba58af1acac33f31e5c47c
Wes Baker <wes.baker@ellislab.com>
2013-08-13 15:02:20 -0400

    Cleaning up quick tabs for the new URL scheme.
    Fixes #1123

--- system/installer/updates/ud_270.php
+++ system/installer/updates/ud_270.php
@@ -55,7 +55,8 @@ class Updater {
 				'_modify_channel_data_relationship_fields',
 				'_modify_channel_data_default_fields',
 				'_modify_category_data_fields',
-				'_clear_dev_log'
+				'_clear_dev_log',
+				'_clean_quick_tabs',
 			)
 		);
 
@@ -935,6 +936,53 @@ If you do not wish to reset your password, ignore this message. It will expire i
 				)
 			)
 		);
+	}
+
+	// -------------------------------------------------------------------
+
+	/**
+	 * Clean up the quick tab links so they no longer have index.php and session
+	 * ID in them
+	 * @return void
+	 */
+	protected function _clean_quick_tabs()
+	{
+		$members = ee()->db->select('member_id, quick_tabs')
+			->where('quick_tabs IS NOT NULL')
+			->like('quick_tabs', 'index.php')
+			->get('members')
+			->result_array();
+
+		foreach ($members as $index => $member)
+		{
+			$members[$index]['quick_tabs'] = $this->_clean_quick_tab_links($member['quick_tabs']);
+		}
+
+		ee()->db->update_batch('members', $members, 'member_id');
+	}
+
+	// -------------------------------------------------------------------
+
+	/**
+	 * Remove the index.php and Session ID from quick tabs
+	 * @param  string $string Quick Tab string
+	 * @return string         Cleaned up quick tab string
+	 */
+	private function _clean_quick_tab_links($string)
+	{
+		// Each string is comprised of multiple links broken up by newlines
+		$lines = explode("\n", $string);
+
+		foreach ($lines as $index => $line)
+		{
+			// Each link is three parts, the first being the name (which is
+			// where we're concerned about XSS cleaning), the link, the order
+			$links = explode('|', $line);
+			$links[1] = substr($links[1], stripos($links[1], 'C='));
+			$lines[$index] = implode('|', $links);
+		}
+
+		return implode("\n", $lines);
 	}
 }
 /* END CLASS */
