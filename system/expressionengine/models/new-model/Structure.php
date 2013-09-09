99d8b775041d63a992a71c90ea0e7794ee849631
Pascal Kriete <pascal@pascalkriete.com>
2013-09-09 15:37:21 -0400

    Prelim documentation for Structure

---
+++ system/expressionengine/models/new-model/Structure.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Structure Interface
+ *
+ * Classes implementing this should define the structure of a collection of data.
+ * For example, Channel is the structural element for ChannelEntries.
+ */
+interface Structure {
+	/**
+	 * Display the settings form
+	 *
+	 * @return String   HTML Settings form
+	 */
+	public function displaySettings();
+
+	/**
+	 * Save the setting data.
+	 *
+	 * Should call validateSettings() before saving
+	 *
+	 * @return void
+	 */
+	public function saveSettings();
+
+	/**
+	 * Validate the setting data
+	 *
+	 * @throws StructureInvalidException if missing / invalid data
+	 * @return void
+	 */
+	public function validateSettings();
+
+	/**
+	 * Display the CP form form
+	 *
+	 * @param $content  An object implementing the Content interface
+	 * @return String   HTML for the entry / edit form
+	 */
+	public function form($content);
+
+}
