ae276b83b5bdc2623f3da02c5fb0f69067c65429
Daniel Bingham <daniel.bingham@ellislab.com>
2013-09-09 15:36:28 -0400

    Betting documentation for Content.

--- system/expressionengine/models/new-model/Content.php
+++ system/expressionengine/models/new-model/Content.php
@@ -8,22 +8,42 @@ interface Content {
 	/**
 	 * Renders the piece of content for the front end, parses the tag data
 	 * called by the module when rendering tagdata.
+	 *
+	 * @param	ParsedTemplate|string	$template	The parsed template from
+	 * 						the template engine or a string of tagdata.
+	 *
+	 * @return	Template|string	The parsed template with relevant tags replaced
+	 *							or the tagdata string with relevant tags replaced.
 	 */
 	public function render($template);
 
 	/**
 	 * A link back to the owning Structure that defines the structure of this
 	 * piece of content.  (A link back to Channel.)
+	 *
+	 * @return	Structure	A link to the Structure objects that defines this
+	 * 						Content's structure.
 	 */
 	public function structure();
 
 	/**
 	 * Saves this piece of content after being populated from a form.
+	 *
+	 * @return	void	
+	 *
+	 * @throws	ContentInvalidException	If content fails to validate a 
+	 *						ContentInvalidException will be thrown with errors
+	 *						on the exception object.
 	 */	
 	public function save();
 
 	/**
 	 * Validates a piece of content for saving, called by save.
+	 *
+	 * @return	void	
+	 *
+	 * @throws	ContentInvalidException If content fails to validate a 
+	 * 						ContentInvalidException will be thrown with errors.
 	 */
 	public function validate();
 	
