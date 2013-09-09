04c01d5c96c6a8f3c8763fad6e90fc63e632a9e4
Pascal Kriete <pascal@pascalkriete.com>
2013-09-09 17:29:00 -0400

    Pseudo-code member model

---
+++ system/expressionengine/models/new-model/Member.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Member module pseudo-implementation for a user
+ *
+ * Creating (not sure about exceptions):
+ * $member = new Member();
+ * $member->username($email);
+ * try {
+ *     $member->save();
+ * } catch (MemberValidationException $ex) {
+ *     var_dump($member->error());
+ * }
+ *
+ * Authenticating:
+ * $member = new Member(array(
+ *     'username' => $username,
+ *     'password' => $password
+ * ));
+ *
+ * if ( ! $member->authenticate()) {
+ *     // handle failed login
+ * }
+ *
+ */
+class Member implements User {
+
+	private $entity;
+
+	/**
+	 * Create a new member / fill an existing member
+	 *
+	 * @param Array  $data  Default fields to populate
+	 */
+	public function __construct(array $data = NULL)
+	{
+		if (isset($data))
+		{
+			// call setters to populate data
+			foreach ($data as $key => $value)
+			{
+				$this->$key($value);
+			}
+		}
+	}
+
+	/**
+	 * Authenticate the member using EE's authentication
+	 *
+	 * Needs to have username/email
+	 */
+	public function authenticate()
+	{
+		$result = ee()->auth->verify_username(/* ... */);
+		// generate error / session?
+	}
+
+	/**
+	 * Validate the user data.
+	 */
+	public function validate()
+	{
+		// run validation and throw exception?
+		// check things such as required fields?
+		// was password set, but not hashed
+	}
+
+	/**
+	 * Save the user data
+	 */
+	public function save()
+	{
+		$this->validate();
+		// save data
+	}
+
+	/**
+	 * Implement password setter directly to disable the getter on the password.
+	 */
+	public function password($password)
+	{
+		if ($this->salt()) // we're already logged in
+		{
+			$this->password = hash($password, $this->salt);
+		}
+		else
+		{
+			// New user, or authenticating:
+			// Set password, but flag for hashing so the raw password
+			// cannot be saved. Need to set raw password in case authenticate
+			// gets called.
+		}
+	}
+
+	/**
+	 * Getters and Setters
+	 */
+	public function __call($key, $value = NULL)
+	{
+		if ( ! isset($value))
+		{
+			return $this->entity->$key;
+		}
+
+		$this->entity->$key = $value;
+	}
+}
