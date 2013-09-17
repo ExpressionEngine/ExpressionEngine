778e82ca21f08ad4d0e67924ab2a6fed92825592
Daniel Bingham <daniel.bingham@ellislab.com>
2013-09-17 12:46:37 -0400

    Cleaning up template a bit.

--- system/expressionengine/Model/Template/Template.php
+++ system/expressionengine/Model/Template/Template.php
@@ -23,44 +23,7 @@ class Template extends Model {
 	{
 	
 	}
-
-	/**
-	 * Get one or more settings objects for the Template model.
-	 *
-	 * @param	string	$name	The name of the settings object you wish to
-	 * 						retrieve.  Available objects are:
-	 *							access: 		Member Group access settings
-	 * 							preferences: 	Template Preference settings
-	 *
-	 * @return	Setting|Setting[]	If a parameter is given then a single Settings
-	 * 					object is returned.  If that Settings object is not found
-	 * 					NULL is returned.  If no parameter is given an array of
-	 * 					Settings objects is returned.  
-	 */
-	public function getSettings($name=NULL)
-	{
-		$set = new SettingsSet(array(
-    		'preference' => 'PreferenceSettings',
-			'access' => 'AccessSettings'
-		));
-    	
-    	if (isset($name))
-    	{
-        	return $set->getSetting($name);
-    	}
-    	return $set->getSettings();
-	}
-
-	/**
-	 * Get the form used to define and edit this Template.
-	 *
-	 * TODO-MODEL I'm not sure this makes sense.
-	 */
-	public function getForm()
-	{
-
-	}
-
+	
 	/**
 	 *
 	 */
