4de0f2841368392b5a2a5e4a1eb97968d1e784d7
Pascal Kriete <pascal@pascalkriete.com>
2013-09-26 01:51:37 -0400

    Adding a bas model autoloader so I can test.

---
+++ system/expressionengine/Model/Autoloader.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * Really basic autoloader using the PSR-4 proposal autoloading rules.
+ *
+ * I think that makes more sense in a namespaced application than PSR-0. Those
+ * underscore rules seem to just be dead weight from the pre-5.3 days.
+ */
+class Autoloader {
+
+	protected $prefixes = array(
+		'EllisLab\ExpressionEngine' => APPPATH
+	);
+
+	/**
+	 * Register the autoloader with PHP
+	 */
+	public function register()
+	{
+		spl_autoload_register(array($this, 'loadClass'));
+	}
+
+	/**
+	 * Remove the autoloader
+	 */
+	public function unregister()
+	{
+		spl_autoload_unregister(array($this, 'loadClass'));
+	}
+
+	/**
+	 * Map a namespace prefix to a path
+	 */
+	public function addPrefix($namespace, $path)
+	{
+		$this->prefixes[$namespace] = $path;
+	}
+
+	/**
+	 * Handle the autoload call.
+	 *
+	 * @param String $class Fully qualified class name. As of 5.3.3 this does
+	 *                      not include a leading slash.
+	 * @return void
+	 */
+	public function loadClass($class)
+	{
+		$base_dir = APPPATH;
+
+		// @todo this prefix handling will not do sub-namespaces correctly
+		foreach ($this->prefixes as $prefix => $path)
+		{
+			if (strpos($class, $prefix) === 0)
+			{
+				$class_path = substr($class, strlen($prefix));
+
+				require $base_dir.str_replace('\\', '/', $class_path).'.php';
+				return;
+			}
+		}
+	}
+}
