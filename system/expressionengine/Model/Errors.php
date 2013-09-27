0c1bfc9ca75ad7a90695d2bef0ffc00282186f13
Pascal Kriete <pascal@pascalkriete.com>
2013-09-27 14:55:06 -0400

    Errors namespace

--- system/expressionengine/Model/Errors.php
+++ system/expressionengine/Model/Errors.php
@@ -1,4 +1,5 @@
-<?PHP
+<?php
+namespace EllisLab\ExpressionEngine\Model;
 
 /**
  * Wrapper class for multiple errors, to be returned from validation.
@@ -10,7 +11,7 @@ class Errors {
 	 * Did validation result in any errors?
 	 *
 	 * @return	boolean	TRUE if there are errors, FALSE otherwise.
-	 */	
+	 */
 	public function hasErrors() {
 		return ( ! empty($this->errors));
 	}
