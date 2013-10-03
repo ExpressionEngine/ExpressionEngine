06d175cdbd54dcea1da3263534cd4261a1635d62
Wes Baker <wes.baker@ellislab.com>
2013-10-03 17:16:24 -0400

    Installing an add-on package with an extension now prompts to enable extensions while installing.
    Fixes #968

--- system/expressionengine/views/addons/package_settings.php
+++ system/expressionengine/views/addons/package_settings.php
@@ -4,10 +4,10 @@
 
 <?php
 $this->table->set_heading(
-						array('data' => lang('component'), 'style' => 'width:40%;'),
-						array('data' => lang('status')),
-						array('data' => lang('current_status'))
-						);
+	array('data' => lang('component'), 'style' => 'width:40%;'),
+	array('data' => lang('status')),
+	array('data' => lang('current_status'))
+);
 
 foreach ($components as $comp => $info)
 {
@@ -16,17 +16,17 @@ foreach ($components as $comp => $info)
 		NBS.NBS.NBS.NBS.NBS.
 		form_radio('install_'.$comp, 'uninstall', $info['installed'], 'id="uninstall_'.$comp.'"').NBS.
 		form_label(lang('uninstall'), 'uninstall_'.$comp);
-	
+
 	if (isset($required[$comp]) && count($required[$comp]))
 	{
-		$fields = lang('required_by').NBS.implode(',', $required[$comp]);
+		$fields = lang('required_by').NBS.implode(', ', $required[$comp]);
 	}
-	
+
 	$this->table->add_row(
-							lang($comp),
-							$fields,
-							$info['installed'] ? lang('installed') : lang('not_installed')
-						);
+		lang($comp),
+		$fields,
+		$info['installed'] ? lang('installed') : lang('not_installed')
+	);
 }
 echo $this->table->generate();
 $this->table->clear();
