1ac54dd55fe74117bf518346a0c94b4b59a98b7d
Wes Baker <wes.baker@ellislab.com>
2013-10-29 10:25:49 -0400

    mod.query whitespace.

--- system/expressionengine/modules/query/mod.query.php
+++ system/expressionengine/modules/query/mod.query.php
@@ -29,9 +29,9 @@ EXAMPLE:
 {exp:query sql="select * from exp_members where username = 'joe' "}
 
  <h1>{username}</h1>
- 
+
  <p>{email}</p>
- 
+
  <p>{url}</p>
 
 {/exp:query}
@@ -40,10 +40,10 @@ EXAMPLE:
 
 class Query {
 
-	var $return_data = ''; 
-	
+	var $return_data = '';
+
 	// Pagination variables
-	
+
 	var $paginate				= FALSE;
 	var $pagination_links		= '';
 	var $page_next				= '';
@@ -74,76 +74,76 @@ class Query {
 	function basic_select()
 	{
 		// Extract the query from the tag chunk
-		
+
 		if (($sql = ee()->TMPL->fetch_param('sql')) === FALSE)
 		{
-			return FALSE;			
+			return FALSE;
 		}
-		
+
 		if (substr(strtolower(trim($sql)), 0, 6) != 'select')
 		{
-			return FALSE;			
+			return FALSE;
 		}
-		
+
 		/** --------------------------------------
 		/**  Pagination checkeroo!
 		/** --------------------------------------*/
 
 		if (preg_match("/".LD."paginate".RD."(.+?)".LD.'\/'."paginate".RD."/s", ee()->TMPL->tagdata, $match))
-		{ 
+		{
 			// Run the query
-						
+
 			$query = ee()->db->query("SELECT COUNT(*) AS count FROM ({$sql}) AS query");
 
 			if ($query->row('count')  == 0)
 			{
 				return $this->return_data = ee()->TMPL->no_results();
 			}
-		
+
 			$this->paginate		 = TRUE;
 			$this->paginate_data = $match['1'];
 			$this->basepath		 = ee()->functions->create_url(ee()->uri->uri_string, 1);
-						
+
 			ee()->TMPL->tagdata = preg_replace("/".LD."paginate".RD.".+?".LD.'\/'."paginate".RD."/s", "", ee()->TMPL->tagdata);
-			
+
 			if (ee()->uri->query_string != '' && preg_match("#^P(\d+)|/P(\d+)#", ee()->uri->query_string, $match))
-			{					
-				$this->p_page = (isset($match['2'])) ? $match['2'] : $match['1'];	
-					
+			{
+				$this->p_page = (isset($match['2'])) ? $match['2'] : $match['1'];
+
 				$this->basepath = reduce_double_slashes(str_replace($match['0'], '', $this->basepath));
 			}
-			
+
 			$this->total_rows = $query->row('count') ;
 			$this->p_limit  = ( ! ee()->TMPL->fetch_param('limit'))  ? 50 : ee()->TMPL->fetch_param('limit');
 			$this->p_page = ($this->p_page == '' OR ($this->p_limit > 1 AND $this->p_page == 1)) ? 0 : $this->p_page;
-				
+
 			if ($this->p_page > $this->total_rows)
 			{
 				$this->p_page = 0;
 			}
-								
+
 			$this->current_page = floor(($this->p_page / $this->p_limit) + 1);
-				
+
 			$this->total_pages = intval(floor($this->total_rows / $this->p_limit));
-			
+
 			/** ----------------------------------------
 			/**  Create the pagination
 			/** ----------------------------------------*/
-			
-			if ($this->total_rows % $this->p_limit) 
+
+			if ($this->total_rows % $this->p_limit)
 			{
 				$this->total_pages++;
 			}
-			
+
 			if ($this->total_rows > $this->p_limit)
 			{
 				ee()->load->library('pagination');
-				
+
 				if (strpos($this->basepath, SELF) === FALSE && ee()->config->item('site_index') != '')
 				{
 					$this->basepath .= SELF;
 				}
-				
+
 				$this->basepath = rtrim($this->basepath,'/').'/';
 
 				$config['base_url']		= $this->basepath;
@@ -153,23 +153,23 @@ class Query {
 				$config['cur_page']		= $this->p_page;
 				$config['first_link'] 	= ee()->lang->line('pag_first_link');
 				$config['last_link'] 	= ee()->lang->line('pag_last_link');
-				
+
 				// Allows $config['cur_page'] to override
 				$config['uri_segment'] = 0;
 
 				ee()->pagination->initialize($config);
 				$this->pagination_links = ee()->pagination->create_links();
-				
+
 				if ((($this->total_pages * $this->p_limit) - $this->p_limit) > $this->p_page)
 				{
 					$this->page_next = $this->basepath.'P'.($this->p_page + $this->p_limit);
 				}
-				
-				if (($this->p_page - $this->p_limit ) >= 0) 
-				{						
+
+				if (($this->p_page - $this->p_limit ) >= 0)
+				{
 					$this->page_previous = $this->basepath.'P'.($this->p_page - $this->p_limit);
 				}
-				
+
 				$sql .= " LIMIT ".$this->p_page.', '.$this->p_limit;
 			}
 			else
@@ -177,30 +177,30 @@ class Query {
 				$this->p_page = '';
 			}
 		}
-		
+
 		$query = ee()->db->query($sql);
-		
+
 		if ($query->num_rows() == 0)
 		{
 			return $this->return_data = ee()->TMPL->no_results();
 		}
 
 		$variables = array();
-		
+
 		foreach ($query->result_array() as $count => $row)
 		{
 			$variables[] = $row;
 		}
-		
+
 		$this->return_data = ee()->TMPL->parse_variables(ee()->TMPL->tagdata, $variables);
 
-		
+
 		if ($this->paginate == TRUE)
 		{
 			$this->paginate_data = str_replace(LD.'current_page'.RD, $this->current_page, $this->paginate_data);
 			$this->paginate_data = str_replace(LD.'total_pages'.RD,	$this->total_pages, $this->paginate_data);
 			$this->paginate_data = str_replace(LD.'pagination_links'.RD, $this->pagination_links, $this->paginate_data);
-			
+
 			if (preg_match("/".LD."if previous_page".RD."(.+?)".LD.'\/'."if".RD."/s", $this->paginate_data, $match))
 			{
 				if ($this->page_previous == '')
@@ -211,12 +211,12 @@ class Query {
 				{
 					$match['1'] = preg_replace("/".LD.'path.*?'.RD."/", 	$this->page_previous, $match['1']);
 					$match['1'] = preg_replace("/".LD.'auto_path'.RD."/",	$this->page_previous, $match['1']);
-			
+
 					$this->paginate_data = str_replace($match['0'],	$match['1'], $this->paginate_data);
 				}
 			 	}
-			
-			
+
+
 			if (preg_match("/".LD."if next_page".RD."(.+?)".LD.'\/'."if".RD."/s", $this->paginate_data, $match))
 			{
 				if ($this->page_next == '')
@@ -227,13 +227,13 @@ class Query {
 				{
 					$match['1'] = preg_replace("/".LD.'path.*?'.RD."/", 	$this->page_next, $match['1']);
 					$match['1'] = preg_replace("/".LD.'auto_path'.RD."/",	$this->page_next, $match['1']);
-			
+
 					$this->paginate_data = str_replace($match['0'],	$match['1'], $this->paginate_data);
 				}
 			}
-				
+
 			$position = ( ! ee()->TMPL->fetch_param('paginate')) ? '' : ee()->TMPL->fetch_param('paginate');
-			
+
 			switch ($position)
 			{
 				case "top"	: $this->return_data  = $this->paginate_data.$this->return_data;
@@ -244,12 +244,12 @@ class Query {
 					break;
 			}
 		}
-		
-		
+
+
 	}
 
-	
-	
+
+
 }
 // END CLASS
 
