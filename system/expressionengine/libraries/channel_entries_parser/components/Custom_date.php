2a78faa20795e4e24a1620362d53f67ec3a9c611
Seth Croston Barber <seth.barber@ellislab.com>
2013-11-22 14:59:44 -0800

    Moving the custom dates to once rather than single.

--- system/expressionengine/libraries/channel_entries_parser/components/Custom_date.php
+++ system/expressionengine/libraries/channel_entries_parser/components/Custom_date.php
@@ -39,47 +39,15 @@ class EE_Channel_custom_date_parser implements EE_Channel_parser_component {
 	// ------------------------------------------------------------------------
 
 	/**
-	 * Find all custom date variables in the template. The return value
-	 * will be passed to process for the main parsing loop.
+	 * Fulfilling the requirements of the abstract class we inherit from.
 	 *
 	 * @param String	The tagdata to be parsed
 	 * @param Object	The preparser object.
-	 * @return Array	Found custom date tags.
+	 * @return Array	An empty array.
 	 */
 	public function pre_process($tagdata, EE_Channel_preparser $pre)
 	{
-		$prefix = $pre->prefix();
-		$channel = $pre->channel();
-
-		$custom_date_fields = array();
-
-		if (count($channel->dfields) == 0)
-		{
-			return $custom_date_fields;
-		}
-
-		foreach ($channel->dfields as $site_id => $dfields)
-		{
-  			foreach($dfields as $key => $value)
-  			{
-  				if ( ! $pre->has_tag($key))
-  				{
-  					continue;
-  				}
-
-  				$key = $prefix.$key;
-
-				if (preg_match_all("/".LD.$key.".*?".RD."/s", $tagdata, $matches))
-				{
-					for ($j = 0; $j < count($matches[0]); $j++)
-					{
-						$custom_date_fields[] = str_replace(array(LD, RD), '', $matches[0][$j]);
-					}
-				}
-			}
-		}
-
-		return $custom_date_fields;
+		return array();
 	}
 
 	// ------------------------------------------------------------------------
@@ -95,44 +63,29 @@ class EE_Channel_custom_date_parser implements EE_Channel_parser_component {
 	 */
 	public function replace($tagdata, EE_Channel_data_parser $obj, $custom_date_fields)
 	{
-		if ( ! count($custom_date_fields))
-		{
-			return $tagdata;
-		}
-
 		$tag = $obj->tag();
 		$data = $obj->row();
-		$prefix = $obj->prefix();
 
 		$dfields = $obj->channel()->dfields;
 
-		if (in_array($tag, $custom_date_fields) && isset($dfields[$data['site_id']]))
+		foreach ($dfields[$data['site_id']] as $dtag => $dval)
 		{
-			foreach ($dfields[$data['site_id']] as $dtag => $dval)
+			if ($data['field_id_'.$dval] == 0 OR $data['field_id_'.$dval] == '')
 			{
-				if (strncmp($tag.' ', $prefix.$dtag.' ', strlen($prefix.$dtag.' ')) !== 0)
-				{
-					continue;
-				}
-
-
-				if ($data['field_id_'.$dval] == 0 OR $data['field_id_'.$dval] == '')
-				{
-					$tagdata = str_replace(LD.$tag.RD, '', $tagdata);
-					continue;
-				}
-
-				// If date is fixed, get timezone to convert timestamp to,
-				// otherwise localize it normally
-				$localize = TRUE;
-
-				if (isset($data['field_dt_'.$dval]) AND $data['field_dt_'.$dval] != '')
-				{
-					$localize = $data['field_dt_'.$dval];
-				}
-
-				$tagdata = ee()->TMPL->parse_date_variables($tagdata, array($dtag => $data['field_id_'.$dval]), $localize);
+				$tagdata = str_replace(LD.$tag.RD, '', $tagdata);
+				continue;
 			}
+
+			// If date is fixed, get timezone to convert timestamp to,
+			// otherwise localize it normally
+			$localize = TRUE;
+
+			if (isset($data['field_dt_'.$dval]) AND $data['field_dt_'.$dval] != '')
+			{
+				$localize = $data['field_dt_'.$dval];
+			}
+
+			$tagdata = ee()->TMPL->parse_date_variables($tagdata, array($dtag => $data['field_id_'.$dval]), $localize);
 		}
 
 		return $tagdata;
