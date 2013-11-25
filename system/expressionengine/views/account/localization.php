27557c4915bb2d4d741b9977a6249d75c60536e0
Seth Croston Barber <seth.barber@ellislab.com>
2013-11-25 15:55:21 -0800

    Converted the forms to use form_preference.

--- system/expressionengine/views/account/localization.php
+++ system/expressionengine/views/account/localization.php
@@ -15,63 +15,7 @@
 	<?php
 	foreach ($fields as $name => $details)
 	{
-		$pref = '';
-
-		switch ($details['type'])
-		{
-			case 's':
-				$label = lang($name, $name);
-
-				if (is_array($details['value']))
-				{
-					$pref = form_dropdown($name, $details['value'], $details['selected'], 'id="'.$name.'"');
-				}
-				else
-				{
-					$pref = '<span class="notice">'.lang('not_available').'</span>';
-				}
-
-				break;
-			case 'r':
-				$label = form_label(lang($name));
-
-				if (is_array($details['value']))
-				{
-					foreach ($details['value'] as $options)
-					{
-						$pref .= form_radio($options).NBS.lang($options['label']).NBS.NBS.NBS.NBS;
-					}
-				}
-				else
-				{
-					$pref = '<span class="notice">'.lang('not_available').'</span>';
-				}
-
-				break;
-			case 't':
-				$label = lang($name, $name);
-				$pref = form_textarea($details['value']);
-				break;
-			case 'f':
-				$label = lang($name, $name);
-				break;
-			case 'i':
-				$label = lang($name, $name);
-
-				$extra = ($name == 'license_number' && IS_CORE) ? array('value' => 'CORE LICENSE', 'disabled' => 'disabled') : array();
-				$pref = form_input(array_merge($details['value'], array('id' => $name, 'class' => 'input fullfield', 'size' => 20, 'maxlength' => 120), $extra));
-				break;
-			case 'p':
-				$label = lang($name, $name);
-
-				$pref = form_password(array_merge($details['value'], array('id' => $name, 'class' => 'input fullfield', 'size' => 20, 'maxlength' => 120)));
-				break;
-			case 'c':
-				$label = lang($name, $name);
-				$pref = $details['value'];
-		}
-
-		echo "<p>" . $label . $pref . "</p>";
+		echo "<p>" . lang($name, $name) . form_preference($name, $details) . "</p>";
 	}
 	?>
 
