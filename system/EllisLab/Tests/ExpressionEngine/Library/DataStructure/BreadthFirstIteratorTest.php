4147b0de84e0d1b041ad38aec2018a34d581f406
Pascal Kriete <pascal@pascalkriete.com>
2013-12-06 14:53:29 -0500

    Empty tree test files

---
+++ system/EllisLab/Tests/ExpressionEngine/Library/DataStructure/BreadthFirstIteratorTest.php
@@ -0,0 +1,7 @@
+<?php
+namespace EllisLab\Tests\ExpressionEngine\Library\DataStructure\Tree;
+
+
+class BreadthFirstIteratorTest extends \PHPUnit_Framework_TestCase {
+
+}
---
+++ system/EllisLab/Tests/ExpressionEngine/Library/DataStructure/TreeIteratorTest.php
@@ -0,0 +1,7 @@
+<?php
+namespace EllisLab\Tests\ExpressionEngine\Library\DataStructure\Tree;
+
+
+class TreeIteratorTest extends \PHPUnit_Framework_TestCase {
+
+}
