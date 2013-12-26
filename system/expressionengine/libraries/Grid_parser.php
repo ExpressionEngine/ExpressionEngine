6d2f46ce599da023cd8d2b6bd669a403204a3d7a
Kevin Cupp <kevin.cupp@gmail.com>
2013-12-26 14:51:07 -0500

    Fixed a bug where a Grid could not contain a column with the same name as its parent Grid field.

--- system/expressionengine/libraries/Grid_parser.php
+++ system/expressionengine/libraries/Grid_parser.php
@@ -58,7 +58,7 @@ class Grid_parser {
 	{
 		// Bail out if there are no grid fields present to parse
 		if ( ! preg_match_all(
-				"/".LD.'\/?('.preg_quote($pre_parser->prefix()).'(?:(?:'.implode('|', array_flip($grid_fields)).'):?)+)\b([^}{]*)?'.RD."/",
+				"/".LD.'\/?('.preg_quote($pre_parser->prefix()).'(?:(?:'.implode('|', array_flip($grid_fields)).'):?))\b([^}{]*)?'.RD."/",
 				$tagdata,
 				$matches,
 				PREG_SET_ORDER)
@@ -397,7 +397,7 @@ class Grid_parser {
 
 		// Gather the variables to parse
 		if ( ! preg_match_all(
-				"/".LD.'?[^\/]((?:(?:'.preg_quote($field_name).'):?)+)\b([^}{]*)?'.RD."/",
+				"/".LD.'?[^\/]((?:(?:'.preg_quote($field_name).'):?))\b([^}{]*)?'.RD."/",
 				$tagdata,
 				$matches,
 				PREG_SET_ORDER) || empty($row)
