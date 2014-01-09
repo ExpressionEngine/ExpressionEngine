f6f32a319232c48012005263a780a633512ab8b7
Pascal Kriete <pascal@pascalkriete.com>
2014-01-09 13:25:38 -0500

    Updating EE_Security to use the new csrf library in its check. Long term we might want to move it out completely.

--- system/expressionengine/core/EE_Security.php
+++ system/expressionengine/core/EE_Security.php
@@ -45,11 +45,8 @@ class EE_Security extends CI_Security {
 	const CSRF_STRICT = 1;	// require single-use token for ajax requests
 	const CSRF_EXEMPT = 2;	// opt-out of xid checks
 
-	private $_xid_ttl = 7200;
-	private $_checked_xids = array();
-
 	// Small note, if you feel the urge to add a constructor,
-	// do not call get_instance(). The CI Security library
+	// do not call ee() or get_instance(). The CI Security library
 	// is sometimes instantiated before the controller is loaded.
 	// i.e. when turning CI's csrf_protection on. Which you shouldn't
 	// do in EE anywho. -pk
@@ -70,74 +67,58 @@ class EE_Security extends CI_Security {
 	 */
 	public function have_valid_xid($flags = self::CSRF_STRICT)
 	{
-		$hash = '';
-		$request_xid = '';
+		$is_valid = FALSE;
+		$run_check = TRUE;
 
-		if (ee()->config->item('secure_forms') == 'y')
+		// Check all of the exceptions
+		if (ee()->config->item('secure_forms') == 'n')
 		{
-			if (count($_POST) > 0)
-			{
-				$run_check = TRUE;
-				$request_xid = FALSE;
-
-				// exempt trumps all
-				if ($flags & self::CSRF_EXEMPT)
-				{
-					$run_check = FALSE;
-				}
-				// only check ajax in strict mode
-				elseif (AJAX_REQUEST && ! ($flags & self::CSRF_STRICT))
-				{
-					$run_check = FALSE;
-				}
-
-				// A class is only passed when the check is optional (currently, ajax actions)
-
-				if (isset($class) && ! ($class instanceOf Strict_XID))
-				{
-					$run_check = FALSE;
-				}
-
-				// ajax requests use a header
-				if (AJAX_REQUEST)
-				{
-					$request_xid = ee()->input->server('HTTP_X_EEXID');
-				}
-
-				// the ajax header trumps post, but for backwards compat we will
-				// fall back to post. Also here for non-ajax, obviously.
-				if ( ! $request_xid)
-				{
-					$request_xid = ee()->input->post('XID');
-				}
-
-				if ($run_check)
-				{
-					if ( ! $request_xid OR ! $this->secure_forms_check($request_xid))
-					{
-						return FALSE;
-					}
-				}
-
-				// @deprecated since 2.7
-				if (REQ == 'CP')
-				{
-					unset($_POST['XID']);
-				}
-			}
-
-			$hash = $this->generate_xid();
+			$run_check = FALSE;
+		}
+		// exempt trumps all
+		elseif ($flags & self::CSRF_EXEMPT)
+		{
+			$run_check = FALSE;
+		}
+		// for now, only check non-cp ajax in strict mode
+		elseif (AJAX_REQUEST && REQ != 'CP' && ! ($flags & self::CSRF_STRICT))
+		{
+			$run_check = FALSE;
 		}
 
-		define('REQUEST_XID', $request_xid);
-		define('XID_SECURE_HASH', $hash);
+		// Check the token if we must
+		ee()->load->library('csrf');
 
+		if ($run_check)
+		{
+			$is_valid = ee()->csrf->check();
+		}
+		else
+		{
+			$is_valid = TRUE;
+		}
+
+		// Retrieve the current token
+		$csrf_token	= ee()->csrf->get_user_token();
+
+		// Remove csrf information from the post array to make this feature
+		// completely transparent to the developer.
+		unset($_POST['XID']);
+		unset($_POST['CSRF_TOKEN']);
+
+		// Set the constant and the legacy constants. Le sigh.
+		define('CSRF_TOKEN', $csrf_token);
+		define('REQUEST_XID', $csrf_token);
+		define('XID_SECURE_HASH', $csrf_token);
+
+		// Send the header and legacy header for ajax requests
 		if (AJAX_REQUEST && ee()->input->server('REQUEST_METHOD') == 'POST')
 		{
-			header('X-EEXID: '.XID_SECURE_HASH);
+			header('X-CSRF-TOKEN: '.CSRF_TOKEN);
+			header('X-EEXID: '.CSRF_TOKEN);
 		}
 
-		return TRUE;
+		return $is_valid;
 	}
 
 	// --------------------------------------------------------------------
@@ -150,28 +131,12 @@ class EE_Security extends CI_Security {
 	 */
 	public function secure_forms_check($xid)
 	{
-		if (ee()->config->item('secure_forms') != 'y' OR $xid === FALSE)
-		{
-			return TRUE;
-		}
+		ee()->load->library('logger');
+		ee()->logger->deprecated('2.8');
 
-		if ( ! array_key_exists($xid, $this->_checked_xids))
-		{
-			ee()->db->update(
-				'security_hashes',
-				array('used' => 1),
-				array(
-					'used'			=> 0,
-					'hash' 			=> $xid,
-					'session_id' 	=> (string) ee()->session->userdata('session_id'),
-					'date >' 		=> ee()->localize->now - $this->_xid_ttl
-				)
-			);
-
-			$this->_checked_xids[$xid] = (ee()->db->affected_rows() != 0);
-		}
-
-		return $this->_checked_xids[$xid];
+		// third party code request handling code should not run before our
+		// check, so if we get here, it's true
+		return TRUE;
 	}
 
 	// --------------------------------------------------------------------
@@ -187,25 +152,11 @@ class EE_Security extends CI_Security {
 	 */
 	public function check_xid($xid = REQUEST_XID)
 	{
-		if (ee()->config->item('secure_forms') != 'y' OR $xid === FALSE)
-		{
-			return TRUE;
-		}
+		ee()->load->library('logger');
+		ee()->logger->deprecated('2.8');
 
-		$total = ee()->db->where(array(
-				'used'			=> 0,
-				'hash' 			=> $xid,
-				'session_id' 	=> ee()->session->userdata('session_id'),
-				'date >' 		=> ee()->localize->now - $this->_xid_ttl
-			))
-			->from('security_hashes')
-			->count_all_results();
-
-		if ($total === 0)
-		{
-			return FALSE;
-		}
-
+		// third party code request handling code should not run before our
+		// check, so if we get here, it's true
 		return TRUE;
 	}
 
@@ -220,21 +171,15 @@ class EE_Security extends CI_Security {
 	 */
 	public function generate_xid($count = 1, $array = FALSE)
 	{
+		ee()->load->library('logger');
+		ee()->logger->deprecated('2.8', 'Use the CSRF_TOKEN constant.');
+
 		$hashes = array();
-		$inserts = array();
 
 		for ($i = 0; $i < $count; $i++)
 		{
-			$hash = ee()->functions->random('encrypt');
-			$inserts[] = array(
-				'date' 			=> ee()->localize->now,
-				'session_id'	=> ee()->session->userdata('session_id'),
-				'hash' 			=> $hash
-			);
-			$hashes[] = $hash;
+			$hashes[] = CSRF_TOKEN;
 		}
-
-		ee()->db->insert_batch('security_hashes', $inserts);
 
 		return (count($hashes) > 1 OR $array) ? $hashes : $hashes[0];
 	}
@@ -249,14 +194,10 @@ class EE_Security extends CI_Security {
 	 */
 	public function delete_xid($xid = REQUEST_XID)
 	{
-		if (ee()->config->item('secure_forms') != 'y' OR $xid === FALSE)
-		{
-			return;
-		}
+		ee()->load->library('logger');
+		ee()->logger->deprecated('2.8');
 
-		ee()->db->where('hash', $xid)
-			->or_where('date <', ee()->localize->now - $this->_xid_ttl)
-			->delete('security_hashes');
+		return;
 	}
 
 	// --------------------------------------------------------------------
@@ -274,16 +215,10 @@ class EE_Security extends CI_Security {
 	 */
 	public function restore_xid($xid = REQUEST_XID)
 	{
-		if (ee()->config->item('secure_forms') != 'y' OR $xid === FALSE)
-		{
-			return;
-		}
+		ee()->load->library('logger');
+		ee()->logger->deprecated('2.8');
 
-		ee()->db->update(
-			'security_hashes',
-			array('used' => 0),
-			array('hash' => $xid)
-		);
+		return;
 	}
 
 	// --------------------------------------------------------------------
@@ -293,12 +228,10 @@ class EE_Security extends CI_Security {
 	 */
 	public function garbage_collect_xids()
 	{
-		ee()->db->delete(
-			'security_hashes',
-			array(
-				'date <' => (ee()->localize->now - $this->_xid_ttl)
-			)
-		);
+		ee()->load->library('logger');
+		ee()->logger->deprecated('2.8');
+
+		return;
 	}
 
 }
