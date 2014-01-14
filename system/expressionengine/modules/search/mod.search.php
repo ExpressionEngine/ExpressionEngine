2e985feb7301a91a2d49259a4ea8879a0172f07d
Kevin Cupp <kevin.cupp@gmail.com>
2014-01-14 11:02:20 -0500

    Removing search module object parameter from channel_search_modify_query hook

--- system/expressionengine/modules/search/mod.search.php
+++ system/expressionengine/modules/search/mod.search.php
@@ -1060,18 +1060,24 @@ class Search {
 
 		// -------------------------------------------
 		// 'channel_search_modify_query' hook.
-		//  - Take the whole query string array, do what you wish
-		//  - added 2.7.x
+		//  - Take the whole query string, do what you wish
+		//  - added 2.8
 		//
 			if (ee()->extensions->active_hook('channel_search_modify_query') === TRUE)
 			{
-				$modified_sql = ee()->extensions->call('channel_search_modify_query', $this, $sql);
+				$modified_sql = ee()->extensions->call('channel_search_modify_query', $sql);
 
 				// Make sure its valid
-				if (is_string($modified_sql) && $modified_sql != '') $sql = $modified_sql;
+				if (is_string($modified_sql) && $modified_sql != '')
+				{
+					$sql = $modified_sql;
+				}
 
 				// This will save the custom query and the total results to exp_search
-				if (ee()->extensions->end_script === TRUE) return $sql;
+				if (ee()->extensions->end_script === TRUE)
+				{
+					return $sql;
+				}
 			}
 		//
 		// -------------------------------------------
