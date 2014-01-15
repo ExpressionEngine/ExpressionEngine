56542cfa0c310c320ee80e1e84d7648c51488d6a
Kevin Smith <kevin@gohearsay.com>
2014-01-15 10:59:38 -0600

    Removes index_page from config.php
    EE uses site_index instead.

--- system/installer/config/config_tmpl.php
+++ system/installer/config/config_tmpl.php
@@ -24,18 +24,6 @@ $config['allow_extensions'] = '{allow_extensions}';
 
 /*
 |--------------------------------------------------------------------------
-| Index File
-|--------------------------------------------------------------------------
-|
-| Typically this will be your index.php file, unless you've renamed it to
-| something else. If you are using mod_rewrite to remove the page set this
-| variable so that it is blank.
-|
-*/
-$config['index_page'] = '{index_page}';
-
-/*
-|--------------------------------------------------------------------------
 | URI PROTOCOL
 |--------------------------------------------------------------------------
 |
