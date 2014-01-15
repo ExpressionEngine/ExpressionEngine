a2ba48a15a73b5ff8613b374d8abc13fe4deb51a
Kevin Smith <kevin@gohearsay.com>
2014-01-15 11:36:41 -0600

    Cleaned up the instructions for the error logging directory path to make them EE-specific.

--- system/installer/config/config_tmpl.php
+++ system/installer/config/config_tmpl.php
@@ -91,8 +91,12 @@ $config['log_threshold'] = {log_threshold};
 | Error Logging Directory Path
 |--------------------------------------------------------------------------
 |
-| Leave this BLANK unless you would like to set something other than the default
-| system/logs/ folder.  Use a full server path with trailing slash.
+| Leave this BLANK unless you would like to set something other than the
+| default system/expressionengine/logs/ directory. Use a full server path
+| with trailing slash.
+|
+| Note: You may need to create this directory if your server does not
+| create it automatically.
 |
 */
 $config['log_path'] = '{log_path}';
