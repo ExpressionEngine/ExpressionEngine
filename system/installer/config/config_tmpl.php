79ca0756f11565252fe51c2cd0676dd501e07783
Kevin Smith <kevin@gohearsay.com>
2014-01-22 14:42:08 -0600

    Removes site_label from the default config.php template since it has no effect.

--- system/installer/config/config_tmpl.php
+++ system/installer/config/config_tmpl.php
@@ -20,8 +20,6 @@ $config['is_system_on'] = '{is_system_on}';
 $config['allow_extensions'] = '{allow_extensions}';
 {extra_config}
 
-// END EE config items
-
 /*
 |--------------------------------------------------------------------------
 | URI PROTOCOL
