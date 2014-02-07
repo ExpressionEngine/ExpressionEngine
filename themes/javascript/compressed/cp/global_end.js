525932681394dcc7ea70bd01b3a7d020b3c22c2f
Wes Baker <wes.baker@ellislab.com>
2014-02-07 15:06:43 -0500

    Compressing javascript.

--- themes/javascript/compressed/cp/global_end.js
+++ themes/javascript/compressed/cp/global_end.js
@@ -10,4 +10,4 @@
  * @filesource
  */
 
-$("table").each(function(){var a;$(this).data("table_config")&&(a=$(this).data("table_config"),$.isPlainObject(a)||(a=$.parseJSON(a)),$(this).table(a));jQuery().toggle_all&&$(this).toggle_all()});
+(function(a){a("table").each(function(){var b;a(this).data("table_config")&&(b=a(this).data("table_config"),a.isPlainObject(b)||(b=a.parseJSON(b)),a(this).table(b));jQuery().toggle_all&&a(this).toggle_all()})})(jQuery);
