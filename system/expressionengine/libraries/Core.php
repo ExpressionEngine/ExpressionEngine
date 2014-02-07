faddab1615061626836cc7ce1e3845be61c12247
Wes Baker <wes.baker@ellislab.com>
2014-02-07 15:10:01 -0500

    Bumping the version numbers.

--- system/expressionengine/libraries/Core.php
+++ system/expressionengine/libraries/Core.php
@@ -86,8 +86,8 @@ class EE_Core {
 		// application constants
 		define('IS_CORE',		FALSE);
 		define('APP_NAME',		'ExpressionEngine'.(IS_CORE ? ' Core' : ''));
-		define('APP_BUILD',		'20131210');
-		define('APP_VER',		'2.7.3');
+		define('APP_BUILD',		'20140207');
+		define('APP_VER',		'2.8.0');
 		define('SLASH',			'&#47;');
 		define('LD',			'{');
 		define('RD',			'}');
