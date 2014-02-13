d6d92924267da92537d518b69e52f4c650a39fbb
Kevin Cupp <kevin.cupp@gmail.com>
2014-02-13 16:59:47 -0500

    Using regex to replace the AMP in cp_url

--- system/expressionengine/helpers/EE_url_helper.php
+++ system/expressionengine/helpers/EE_url_helper.php
@@ -48,13 +48,8 @@ function cp_url($path, $qs = '')
 
 	if ($s)
 	{
-		$qs = $qs.AMP.'S='.$s;
-
 		// Remove AMP from the beginning of the query string if it exists
-		if (substr($qs, 0, strlen(AMP)) == AMP)
-		{
-			$qs = substr($qs, strlen(AMP));
-		}
+		$qs = preg_replace('#^'.AMP.'#', '', $qs.AMP.'S='.$s);
 	}
 
 	$path = rtrim('?/cp/'.$path, '/');
