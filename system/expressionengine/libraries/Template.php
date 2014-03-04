fb7e06b99fb8ed7894779b26a8b387465be23fe3
Pascal Kriete <pascal@pascalkriete.com>
2014-03-04 12:06:35 -0500

    Adding layout_conditionals default to template library class variables.

--- system/expressionengine/libraries/Template.php
+++ system/expressionengine/libraries/Template.php
@@ -24,85 +24,84 @@
  */
 class EE_Template {
 
-	var $loop_count	  		=	0;			// Main loop counter.
-	var $depth				=	0;			// Sub-template loop depth
-	var $in_point			=  '';			// String position of matched opening tag
-	var $template			=  '';			// The requested template (page)
-	var $final_template	 	=  '';			// The finalized template
-	var $fl_tmpl		 	=  '';			// 'Floating' copy of the template.  Used as a temporary "work area".
-	var $cache_hash	  		=  '';			// md5 checksum of the template name.  Used as title of cache file.
-	var $cache_status		=  '';			// Status of page cache (NO_CACHE, CURRENT, EXPIRED)
-	var $tag_cache_status	=  '';			// Status of tag cache (NO_CACHE, CURRENT, EXPIRED)
-	var $cache_timestamp	=  '';
-	var $template_type  	=  '';			// Type of template (webpage, rss)
-	var $embed_type			=  '';			// Type of template for embedded template
-	var $template_hits		=	0;
-	var $php_parse_location =  'output';	// Where in the chain the PHP gets parsed
-	var $template_edit_date	=	'';			// Template edit date
-	var $templates_sofar	=   '';			// Templates processed so far, subtemplate tracker
-	var $attempted_fetch	=  array();		// Templates attempted to fetch but may have bailed due to recursive embeds
-	var $encode_email		=  TRUE;		// Whether to use the email encoder.  This is set automatically
-	var $hit_lock_override	=  FALSE;		// Set to TRUE if you want hits tracked on sub-templates
-	var $hit_lock			=  FALSE;		// Lets us lock the hit counter if sub-templates are contained in a template
-	var $parse_php			=  FALSE;		// Whether to parse PHP or not
-	var $protect_javascript =  TRUE;		// Protect javascript in conditionals
+	public $loop_count	  		= 0;			// Main loop counter.
+	public $depth				= 0;			// Sub-template loop depth
+	public $in_point			= '';			// String position of matched opening tag
+	public $template			= '';			// The requested template (page)
+	public $final_template	 	= '';			// The finalized template
+	public $fl_tmpl		 		= '';			// 'Floating' copy of the template.  Used as a temporary "work area".
+	public $cache_hash	  		= '';			// md5 checksum of the template name.  Used as title of cache file.
+	public $cache_status		= '';			// Status of page cache (NO_CACHE, CURRENT, EXPIRED)
+	public $tag_cache_status	= '';			// Status of tag cache (NO_CACHE, CURRENT, EXPIRED)
+	public $cache_timestamp		= '';
+	public $template_type  		= '';			// Type of template (webpage, rss)
+	public $embed_type			= '';			// Type of template for embedded template
+	public $template_hits		= 0;
+	public $php_parse_location 	= 'output';	// Where in the chain the PHP gets parsed
+	public $template_edit_date	= '';			// Template edit date
+	public $templates_sofar		=  '';			// Templates processed so far, subtemplate tracker
+	public $attempted_fetch		= array();		// Templates attempted to fetch but may have bailed due to recursive embeds
+	public $encode_email		= TRUE;		// Whether to use the email encoder.  This is set automatically
+	public $hit_lock_override	= FALSE;		// Set to TRUE if you want hits tracked on sub-templates
+	public $hit_lock			= FALSE;		// Lets us lock the hit counter if sub-templates are contained in a template
+	public $parse_php			= FALSE;		// Whether to parse PHP or not
+	public $protect_javascript 	= TRUE;		// Protect javascript in conditionals
+    public $strict_urls			= FALSE;		// Whether to make URLs operate strictly or not.  This is set via a template global pref
 
-	var $group_name			= '';			// Group of template being parsed
-	var $template_name		= '';			// Name of template being parsed
-	var $template_id		= 0;
+	public $group_name			= '';			// Group of template being parsed
+	public $template_name		= '';			// Name of template being parsed
+	public $template_id			= 0;
 
-	var $tag_data			= array();		// Data contained in tags
-	var $modules		 	= array();		// List of installed modules
-	var $module_data		= array();		// Data for modules from exp_channels
-	var $plugins		 	= array();		// List of installed plug-ins
+	public $tag_data			= array();		// Data contained in tags
+	public $modules		 		= array();		// List of installed modules
+	public $module_data			= array();		// Data for modules from exp_channels
+	public $plugins		 		= array();		// List of installed plug-ins
 
-	var $var_single	  		= array();		// "Single" variables
-	var $var_cond			= array();		// "Conditional" variables
-	var $var_pair			= array();		// "Paired" variables
-	var $global_vars		= array();		// This array can be set via the path.php file
-	var $embed_vars		 	= array();		// This array can be set via the {embed} tag
-	var $layout_vars		= array();		// This array can be set via the {layout} tag
-	var $segment_vars		= array();		// Array of segment variables
-	var $template_route_vars = array();		// Array of segment variables
+	public $var_single	  		= array();		// "Single" variables
+	public $var_cond			= array();		// "Conditional" variables
+	public $var_pair			= array();		// "Paired" variables
+	public $global_vars			= array();		// This array can be set via the path.php file
+	public $embed_vars		 	= array();		// This array can be set via the {embed} tag
+	public $layout_vars			= array();		// This array can be set via the {layout} tag
+	public $segment_vars		= array();		// Array of segment variables
+	public $template_route_vars = array();		// Array of segment variables
 
-	var $tagparts			= array();		// The parts of the tag: {exp:comment:form}
-	var $tagdata			= '';			// The chunk between tag pairs.  This is what modules will utilize
-	var $tagproper			= '';			// The full opening tag
-	var $no_results			= '';			// The contents of the {if no_results}{/if} conditionals
-	var $no_results_block	= '';			// The {if no_results}{/if} chunk
-	var $search_fields		= array();		// Special array of tag parameters that begin with 'search:'
-	var $date_vars			= array();		// Date variables found in the tagdata (FALSE if date variables do not exist in tagdata)
-	var $unfound_vars		= array();		// These are variables that have not been found in the tagdata and can be ignored
-	var $conditional_vars	= array();		// Used by the template variable parser to prep conditionals
-	var $TYPE				= FALSE;		// FALSE if Typography has not been instantiated, Typography Class object otherwise
+	public $tagparts			= array();		// The parts of the tag: {exp:comment:form}
+	public $tagdata				= '';			// The chunk between tag pairs.  This is what modules will utilize
+	public $tagproper			= '';			// The full opening tag
+	public $no_results			= '';			// The contents of the {if no_results}{/if} conditionals
+	public $no_results_block	= '';			// The {if no_results}{/if} chunk
+	public $search_fields		= array();		// Special array of tag parameters that begin with 'search:'
+	public $date_vars			= array();		// Date variables found in the tagdata (FALSE if date variables do not exist in tagdata)
+	public $unfound_vars		= array();		// These are variables that have not been found in the tagdata and can be ignored
+	public $conditional_vars	= array();		// Used by the template variable parser to prep conditionals
+	public $layout_conditionals = array();		// Used for {if layout:variable conditionals
+	public $TYPE				= FALSE;		// FALSE if Typography has not been instantiated, Typography Class object otherwise
 
-	var $related_data		= array();		//  A multi-dimensional array containing any related tags
-	var $related_id			= '';			// Used temporarily for the related ID number
-	var $related_markers	= array();		// Used temporarily
+	public $related_data		= array();		//  A multi-dimensional array containing any related tags
+	public $related_id			= '';			// Used temporarily for the related ID number
+	public $related_markers		= array();		// Used temporarily
+	public $reverse_related_data = array();	//  A multi-dimensional array containing any reverse related tags
 
-	var $site_ids			= array();		// Site IDs for the Sites Request for a Tag
-	var $sites				= array();		// Array of sites with site_id as key and site_name as value, used to determine site_ids for tag, above.
-	var $site_prefs_cache	= array();		// Array of cached site prefs, to allow fetching of another site's template files
+	public $site_ids			= array();		// Site IDs for the Sites Request for a Tag
+	public $sites				= array();		// Array of sites with site_id as key and site_name as value, used to determine site_ids for tag, above.
+	public $site_prefs_cache	= array();		// Array of cached site prefs, to allow fetching of another site's template files
 
-	var $reverse_related_data = array();	//  A multi-dimensional array containing any reverse related tags
+	public $disable_caching		= FALSE;
+
+	public $debugging			= FALSE;		// Template parser debugging on?
+	public $cease_processing	= FALSE;		// Used with no_results() method.
+	public $log					= array();		// Log of Template processing
+	public $start_microtime		= 0;			// For Logging (= microtime())
+
+	public $form_id				= '';		// 	Form Id
+	public $form_class 			= '';		// 	Form Class
+
+	public $realm				= 'Restricted Content';  // Localize?
+	public $marker				= '0o93H7pQ09L8X1t49cHY01Z5j4TT91fGfr'; // Temporary marker used as a place-holder for template data
 
 	protected $_tag_cache_prefix	= 'tag_cache';	// Tag cache key namespace
 	protected $_page_cache_prefix	= 'page_cache'; // Page cache key namespace
-	var $disable_caching			= FALSE;
-
-	var $debugging			= FALSE;		// Template parser debugging on?
-	var $cease_processing	= FALSE;		// Used with no_results() method.
-	var $log				= array();		// Log of Template processing
-	var $start_microtime	= 0;			// For Logging (= microtime())
-
-    var $strict_urls		= FALSE;		// Whether to make URLs operate strictly or not.  This is set via a template global pref
-
-	var $realm				= 'Restricted Content';  // Localize?
-
-	var $marker = '0o93H7pQ09L8X1t49cHY01Z5j4TT91fGfr'; // Temporary marker used as a place-holder for template data
-
-	var $form_id			= '';		// 	Form Id
-	var $form_class 		= '';		// 	Form Class
 
 	// --------------------------------------------------------------------
 
