35302640721d4807e78caa06a7d118661cdf5079
Seth Croston Barber <seth.barber@ellislab.com>
2014-03-06 09:40:29 -0800

    Fixed a bug (#20055): radio buttons were double encoding

--- system/expressionengine/fieldtypes/radio/ft.radio.php
+++ system/expressionengine/fieldtypes/radio/ft.radio.php
@@ -105,10 +105,10 @@ class Radio_ft extends EE_Fieldtype {
 
 		$r = '';
 
-		foreach($field_options as $option)
+		foreach($field_options as $key => $value)
 		{
-			$selected = ($option == $data);
-			$r .= '<label>'.form_radio($this->field_name, $option, $selected).NBS.$option.'</label>';
+			$selected = ($key == $data);
+			$r .= '<label>'.form_radio($this->field_name, $value, $selected).NBS.$key.'</label>';
 		}
 
 		switch ($container)
@@ -179,7 +179,7 @@ class Radio_ft extends EE_Fieldtype {
 				foreach (explode("\n", trim($this->settings['field_list_items'])) as $v)
 				{
 					$v = trim($v);
-					$field_options[form_prep($v)] = form_prep($v);
+					$field_options[form_prep($v)] = $v;
 				}
 			}
 			else
