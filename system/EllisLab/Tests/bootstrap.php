7419f4383b48f53961da87a3f5c3731241e8e2e7
Pascal Kriete <pascal@pascalkriete.com>
2014-03-06 17:22:03 -0500

    Cleaning up the testing bootstrap.

--- system/EllisLab/Tests/bootstrap.php
+++ system/EllisLab/Tests/bootstrap.php
@@ -4,21 +4,17 @@
 error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', 1);
 
+$project_base = realpath(dirname(__FILE__).'/../../').'/';
+
 // Path constants
-define('PROJECT_BASE',	realpath(__DIR__.'/../../').'/');
-define('BASEPATH',		PROJECT_BASE.'codeigniter/system/');
-define('APPPATH',		PROJECT_BASE.'expressionengine/');
+define('BASEPATH', $project_base.'codeigniter/system/');
+define('APPPATH',  $project_base.'expressionengine/');
 
-// Create a test suite autoloader
-spl_autoload_register(function($class) {
+// Minor CI annoyance
+function log_message() {}
 
-	if (strpos($class, 'EllisLab\\') === 0)
-	{
-		$file = __DIR__.'/../../'.str_replace('\\', '/', $class) . '.php';
+// Add hamcrest matchers
+require_once __DIR__ . '/vendor/hamcrest/hamcrest-php/hamcrest/Hamcrest.php';
 
-		if (file_exists($file))
-		{
-			require_once $file;
-		}
-	}
-});
+// add the composer autoloader
+require_once __DIR__ . '/vendor/autoload.php';
