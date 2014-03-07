237aabac22c0637a12b87f428ec7f001c752d529
Wes Baker <wes.baker@ellislab.com>
2014-03-07 17:17:24 -0500

    Now requiring PHP 5.3.10
    Pretty anti-climactic commit if you ask me...

--- system/installer/controllers/wizard.php
+++ system/installer/controllers/wizard.php
@@ -26,7 +26,7 @@ class Wizard extends CI_Controller {
 
 	var $version			= '2.8.0';	// The version being installed
 	var $installed_version	= ''; 		// The version the user is currently running (assuming they are running EE)
-	var $minimum_php		= '5.2.4';	// Minimum version required to run EE
+	var $minimum_php		= '5.3.10';	// Minimum version required to run EE
 	var $schema				= NULL;		// This will contain the schema object with our queries
 	var $languages			= array(); 	// Available languages the installer supports (set dynamically based on what is in the "languages" folder)
 	var $mylang				= 'english';// Set dynamically by the user when they run the installer
