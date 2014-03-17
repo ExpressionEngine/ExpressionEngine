1b97143d7151a877d5c60c70fcf25fd3274cc746
Quinn Chrzan <quinnchrzan@gmail.com>
2014-03-17 17:47:37 -0400

    Fix for bug #20235 where checking for duplicate routes caused PHP errors

--- system/expressionengine/libraries/template_router/Route.php
+++ system/expressionengine/libraries/template_router/Route.php
@@ -152,9 +152,19 @@ class EE_Route {
 	 */
 	public function equals(EE_Route $route)
 	{
+		if(count($this->segments) != count($route->segments))
+		{
+			return FALSE;
+		}
+
 		foreach($this->segments as $index => $segment)
 		{
 			$comparison = $route->segments[$index];
+
+			if (gettype($segment) !== gettype($comparison))
+			{
+				return FALSE;
+			}
 
 			if (is_string($segment))
 			{
@@ -165,11 +175,6 @@ class EE_Route {
 			}
 			else
 			{
-				if (is_string($comparison))
-				{
-					return FALSE;
-				}
-
 				$segment_rules = array_map('serialize', $segment->rules);
 				$comparison_rules = array_map('serialize', $comparison->rules);
 				$diff = array_diff($segment_rules, $comparison_rules);
