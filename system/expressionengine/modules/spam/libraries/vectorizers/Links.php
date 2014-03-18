a44f2d66caca842f16c0c0af83bc3b509f6e7838
Quinn Chrzan <quinnchrzan@gmail.com>
2014-03-18 11:52:51 -0400

    Adding rule for number of URLs

---
+++ system/expressionengine/modules/spam/libraries/vectorizers/Links.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * ExpressionEngine - by EllisLab
+ *
+ * @package		ExpressionEngine
+ * @author		EllisLab Dev Team
+ * @copyright	Copyright (c) 2003 - 2014, EllisLab, Inc.
+ * @license		http://ellislab.com/expressionengine/user-guide/license.html
+ * @link		http://ellislab.com
+ * @since		Version 3.0
+ * @filesource
+ */
+
+// ------------------------------------------------------------------------
+
+/**
+ * ExpressionEngine Spam Module
+ *
+ * @package		ExpressionEngine
+ * @subpackage	Modules
+ * @category	Modules
+ * @author		EllisLab Dev Team
+ * @link		http://ellislab.com
+ */
+
+class Links {
+
+	/**
+	 * Calculates the amount of links in the source
+	 * 
+	 * @param string $source The source text 
+	 * @access public
+	 * @return float The calculated ratio
+	 */
+	public static function vectorize($source)
+	{
+		$pattern = '#[-a-zA-Z0-9@:%_\+.~\#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~\#?&//=]*)?#si';
+		return preg_match_all($pattern, $source);
+	}
+
+}
+
+?>
