7ba2fd9ea3829be545e133906e209bc9cdc90914
Pascal Kriete <pascal@pascalkriete.com>
2014-03-24 18:22:50 -0400

    Further refining the true false disaster.

--- system/tests/phpunit-new/tests/PrepConditionalsTest.php
+++ system/tests/phpunit-new/tests/PrepConditionalsTest.php
@@ -215,8 +215,10 @@ class PrepConditionalsTest extends PHPUnit_Framework_TestCase {
 	protected function wonkyMutableBooleans()
 	{
 		return array(
-			array('TRUE can be a variable',	 '{if xyz == true}out{/if}', '{if "1" == "baz"}out{/if}',	array('xyz' => TRUE, 'true' => "baz")),
-			array('FALSE can be a variable', '{if xyz == false}out{/if}', '{if "1" == "bat"}out{/if}',	array('xyz' => TRUE, 'false' => "bat")),
+			array('TRUE can NOT be a variable',	 '{if xyz == TRUE}out{/if}', '{if "1" == FALSE}out{/if}',	array('xyz' => TRUE, 'TRUE' => "baz")),
+			array('FALSE can NOT be a variable', '{if xyz == FALSE}out{/if}', '{if "1" == FALSE}out{/if}',	array('xyz' => TRUE, 'FALSE' => "bat")),
+			array('true can be a variable?!',	 '{if xyz == true}out{/if}', '{if "1" == "baz"}out{/if}',	array('xyz' => TRUE, 'true' => "baz")),
+			array('false can be a variable?!',	 '{if xyz == false}out{/if}', '{if "1" == "bat"}out{/if}',	array('xyz' => TRUE, 'false' => "bat")),
 		);
 	}
 
