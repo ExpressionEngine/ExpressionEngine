001d5c8545a222cb871bdf8a84a8ecd38b103d45
Daniel Bingham <daniel.bingham@ellislab.com>
2014-03-25 15:33:09 -0400

    Updating ChannelEntry for underscore masking of meta data.

--- system/EllisLab/ExpressionEngine/Module/Channel/Model/Gateway/ChannelDataGateway.php
+++ system/EllisLab/ExpressionEngine/Module/Channel/Model/Gateway/ChannelDataGateway.php
@@ -4,24 +4,22 @@ namespace EllisLab\ExpressionEngine\Module\Channel\Model\Gateway;
 use EllisLab\ExpressionEngine\Model\Gateway\FieldDataGateway;
 
 class ChannelDataGateway extends FieldDataGateway {
-	protected static $meta = array(
-		'table_name' => 'channel_data',
-		'field_table' => 'channel_fields',
-		'field_id_name' => 'field_id',
-		'primary_key' => 'entry_id',
-		'related_gateways' => array(
-			'entry_id' => array(
-				'gateway' => 'ChannelTitleGateway',
-				'key'	 => 'entry_id'
-			),
-			'channel_id' => array(
-				'gateway' => 'ChannelGateway',
-				'key'	 => 'channel_id'
-			),
-			'site_id' => array(
-				'gateway' => 'SiteGateway',
-				'key'	 => 'site_id'
-			)
+	protected static $_table_name = 'channel_data';
+	protected static $_field_table = 'channel_fields';
+	protected static $_field_id_name = 'field_id';
+	protected static $_primary_key = 'entry_id';
+	protected static $_related_gateways = array(
+		'entry_id' => array(
+			'gateway' => 'ChannelTitleGateway',
+			'key'	 => 'entry_id'
+		),
+		'channel_id' => array(
+			'gateway' => 'ChannelGateway',
+			'key'	 => 'channel_id'
+		),
+		'site_id' => array(
+			'gateway' => 'SiteGateway',
+			'key'	 => 'site_id'
 		)
 	);
 
