3b503cafbd759a45fc47a19c57937f99e92c72e5
Pascal Kriete <pascal@pascalkriete.com>
2014-04-01 16:10:07 -0400

    sigh...

--- system/tests/circleci/runtests.sh
+++ system/tests/circleci/runtests.sh
@@ -48,7 +48,7 @@ do
 		((STATUS+=$?))
 
 		# Remove CLI colors
-		sed -ir "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})*)?m//g" $CIRCLE_ARTIFACTS/$PHPVERSION/phpunit.txt
+		sed -i -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})*)?m//g" $CIRCLE_ARTIFACTS/$PHPVERSION/phpunit.txt
 
 	popd
 
