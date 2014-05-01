6ec38b309e4856a7b5befd8950eaeac5e39fbc3d
Kevin Cupp <kevin.cupp@gmail.com>
2014-05-01 19:11:17 -0400

    Taking PHP Info out of the design

--- system/expressionengine/controllers/cp/utilities.php
+++ system/expressionengine/controllers/cp/utilities.php
@@ -99,44 +99,12 @@ class Utilities extends CP_Controller {
 	 */
 	public function php()
 	{
-		// Here, we'll capture the output of PHP Info and modify its markup
-		// to appear in our control panel
-		ob_start();
+		if ( ! $this->cp->allowed_group('can_access_tools'))
+		{
+			show_error(lang('unauthorized_access'));
+		}
 
-		phpinfo();
-
-		$buffer = ob_get_contents();
-
-		ob_end_clean();
-
-		$output = (preg_match("/<body.*?".">(.*)<\/body>/is", $buffer, $match)) ? $match['1'] : $buffer;
-		$output = preg_replace("/width\=\".*?\"/", "width=\"100%\"", $output);
-		$output = preg_replace("/<hr.*?>/", "<br />", $output); // <?
-		$output = preg_replace("/<a href=\"http:\/\/www.php.net\/\">.*?<\/a>/", "", $output);
-		$output = preg_replace("/<a href=\"http:\/\/www.zend.com\/\">.*?<\/a>/", "", $output);
-		$output = preg_replace("/<a.*?<\/a>/", "", $output);// <?
-		$output = preg_replace("/<th(.*?)>/", "<th \\1 >", $output);
-		$output = preg_replace("/<tr(.*?).*?".">/", "<tr \\1>\n", $output);
-		$output = preg_replace("/<td.*?".">/", "<td valign=\"top\">", $output);
-		$output = preg_replace("/<h2 align=\"center\">PHP License<\/h2>.*?<\/table>/si", "", $output);
-		$output = preg_replace("/ align=\"center\"/", "", $output);
-		$output = preg_replace("/<table(.*?)bgcolor=\".*?\">/", "\n\n<table\\1>", $output);
-		$output = preg_replace("/<table(.*?)>/", "\n\n<table\\1 class=\"mainTable\" cellspacing=\"0\">", $output);
-		$output = preg_replace("/<h2>PHP License.*?<\/table>/is", "", $output);
-		$output = preg_replace("/<br \/>\n*<br \/>/is", "", $output);
-		$output = preg_replace('/<h(1|2)\s*(class="p")?/i', '<h\\1', $output);
-		$output = str_replace("<h1></h1>", "", $output);
-		$output = str_replace("<h2></h2>", "", $output);
-
-		ee()->view->phpinfo = $output;
-		ee()->view->cp_page_title = lang('php_info');
-
-		// Need a separate page title because this lang key has an abbr tag,
-		// can't also use it in <title/>
-		ee()->view->page_title = sprintf(lang('php_info_title'), phpversion());
-
-		ee()->cp->add_to_head($this->view->head_link('css/v3/phpinfo.css'));
-		ee()->cp->render('utilities/php-info');
+		exit(phpinfo());
 	}
 
 	// --------------------------------------------------------------------
