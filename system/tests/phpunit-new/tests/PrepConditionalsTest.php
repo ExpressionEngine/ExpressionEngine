5e50f765dbb61c44db612d54bc875bf6346b7d48
Seth Croston Barber <seth.barber@ellislab.com>
2014-05-12 14:44:26 -0700

    Whitespace alignment

--- system/tests/phpunit-new/tests/PrepConditionalsTest.php
+++ system/tests/phpunit-new/tests/PrepConditionalsTest.php
@@ -526,8 +526,8 @@ class PrepConditionalsTest extends PHPUnit_Framework_TestCase {
 	protected function wonkyMutableBooleans()
 	{
 		return array(
-			array('TRUE CANNOT be a variable',	'{if xyz == TRUE}out{/if}', '{if "1" == TRUE}out{/if}',	array('xyz' => TRUE, 'TRUE' => "baz")),
-			array('FALSE CANNOT be a variable',	'{if xyz == FALSE}out{/if}', '{if "1" == FALSE}out{/if}',	array('xyz' => TRUE, 'FALSE' => "bat")),
+			array('TRUE CANNOT be a variable',		'{if xyz == TRUE}out{/if}', '{if "1" == TRUE}out{/if}',	array('xyz' => TRUE, 'TRUE' => "baz")),
+			array('FALSE CANNOT be a variable',		'{if xyz == FALSE}out{/if}', '{if "1" == FALSE}out{/if}',	array('xyz' => TRUE, 'FALSE' => "bat")),
 			array('true CANNOT be a variable?!',	'{if xyz == true}out{/if}', '{if "1" == TRUE}out{/if}',	array('xyz' => TRUE, 'true' => "baz")),
 			array('false CANNOT be a variable?!',	'{if xyz == false}out{/if}', '{if "1" == FALSE}out{/if}',	array('xyz' => TRUE, 'false' => "bat")),
 			array('true CANNOT equal false',		'{if true == FALSE}out{/if}', '{if TRUE == FALSE}out{/if}',	array('xyz' => TRUE, 'true' => ""))
