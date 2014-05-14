0787018510ebeed59bdf158dc16b759e63e218cb
Pascal Kriete <pascal@pascalkriete.com>
2014-05-14 11:43:47 -0400

    Figure out if a conditional is comprised of only scalars.
    This indicates a simple / evaluate-able conditional.

--- system/expressionengine/libraries/template/Conditional_parser.php
+++ system/expressionengine/libraries/template/Conditional_parser.php
@@ -38,11 +38,20 @@ class Conditional_parser extends RecursiveDescentParser {
 		return $this->closeBuffer();
 	}
 
+	/**
+	 * Set the variables to use for the parsing
+	 */
 	public function setVariables($vars)
 	{
 		$this->variables = $vars;
 	}
 
+	/**
+	 * Turn safety on.
+	 *
+	 * When safety is on, any non-scalars are turned into FALSE so that
+	 * they can't be used to muck with our eval.
+	 */
 	public function safetyOn()
 	{
 		$this->safety = TRUE;
@@ -61,9 +70,11 @@ class Conditional_parser extends RecursiveDescentParser {
 		}
 		elseif ($this->accept('IF'))
 		{
-			$this->conditional();
+			$if = new If_statement($this);
+
+			$this->conditional($if);
 			$this->expect('ENDIF');
-			$this->output('{/if}');
+			$if->end_if();
 			$this->template();
 		}
 	}
@@ -79,28 +90,70 @@ class Conditional_parser extends RecursiveDescentParser {
 	 *     template
 	 * {/if}
 	 */
-	protected function conditional()
+	protected function conditional($conditional)
 	{
 		$this->openBuffer();
 
-		$this->condition();
+		$can_evaluate = $this->condition();
 
-		$this->output('{if ' . $this->closeBuffer() . '}');
-		$this->template();
+		if ($conditional->add_if($this->closeBuffer(), $can_evaluate))
+		{
+			$this->template();
+		}
+		else
+		{
+			$this->skip_conditional_body();
+		}
 
 		while ($this->accept('ELSEIF'))
 		{
 			$this->openBuffer();
-			$this->condition();
+			$can_evaluate = $this->condition();
 
-			$this->output('{if:elseif ' . $this->closeBuffer() . '}');
-			$this->template();
+			if ($conditional->add_elseif($this->closeBuffer(), $can_evaluate))
+			{
+				$this->template();
+			}
+			else
+			{
+				$this->skip_conditional_body();
+			}
 		}
 
 		if ($this->accept('ELSE'))
 		{
-			$this->output('{if:else}');
-			$this->template();
+			if ($conditional->add_else())
+			{
+				$this->template();
+			}
+			else
+			{
+				$this->skip_conditional_body();
+			}
+		}
+	}
+
+	/**
+	 * Seek past the body of the conditional. This method
+	 * will also skip any nested conditionals since we don't
+	 * need to evaluate those if they are not going to be output.
+	 */
+	protected function skip_conditional_body()
+	{
+		$conditional_depth = 0;
+
+		while ($this->is('TEMPLATE_STRING') || $conditional_depth > 0)
+		{
+			if ($this->is('IF'))
+			{
+				$conditional_depth++;
+			}
+			if ($this->is('ENDIF'))
+			{
+				$conditional_depth--;
+			}
+
+			$this->next();
 		}
 	}
 
@@ -111,8 +164,10 @@ class Conditional_parser extends RecursiveDescentParser {
 	 */
 	protected function condition()
 	{
-		$this->expression();
+		$can_evaluate = $this->expression();
 		$this->expect('ENDCOND');
+
+		return $can_evaluate;
 	}
 
 	/**
@@ -123,15 +178,18 @@ class Conditional_parser extends RecursiveDescentParser {
 	 */
 	protected function expression()
 	{
+		// An empty conditional can technically evaluate, so by default
+		// everything is true.
+		$can_evaluate = TRUE;
+
 		if ($this->accept('LP'))
 		{
 			$this->output('(');
 
-			$this->expression();
+			$can_evaluate = $this->expression();
 
 			$this->output(')');
 			$this->expect('RP');
-
 		}
 		elseif ($this->is('STRING') || $this->is('NUMBER') || $this->is('BOOL'))
 		{
@@ -140,16 +198,19 @@ class Conditional_parser extends RecursiveDescentParser {
 		}
 		elseif ($this->is('VARIABLE'))
 		{
+			$can_evaluate = FALSE;
 			$this->output($this->variable($this->value()));
 			$this->next();
 		}
 		elseif ($this->is('TAG'))
 		{
+			$can_evaluate = FALSE;
 			$this->output($this->tag($this->value()));
 			$this->next();
 		}
 		elseif ($this->is('MISC'))
 		{
+			$can_evaluate = FALSE;
 			$this->output($this->misc($this->value()));
 			$this->next();
 		}
@@ -160,8 +221,17 @@ class Conditional_parser extends RecursiveDescentParser {
 			$this->output($this->value());
 			$this->whitespace();
 			$this->next();
-			$this->expression();
+			$sub_expression_can_eval = $this->expression();
+
+			// If we already cannot evaluate, the sub expression result
+			// does not matter and might accidental flip us to false.
+			if ($can_evaluate === TRUE)
+			{
+				$can_evaluate = $sub_expression_can_eval;
+			}
 		}
+
+		return $can_evaluate;
 	}
 
 	/**
@@ -309,7 +379,7 @@ class Conditional_parser extends RecursiveDescentParser {
 	/**
 	 * Add to the current output buffer
 	 */
-	protected function output($value)
+	public function output($value)
 	{
 		$this->output .= $value;
 	}
@@ -385,19 +455,24 @@ abstract class RecursiveDescentParser {
 	abstract public function parse();
 
 	/**
-	 * Helper functions
+	 * Compare the current token to a token name
 	 */
-
 	protected function is($token_name)
 	{
 		return ($this->token[0] == $token_name);
 	}
 
+	/**
+	 * Get the current token value
+	 */
 	protected function value()
 	{
 		return $this->token[1];
 	}
 
+	/**
+	 * Compare the current token to a token name, and advance if it matches.
+	 */
 	protected function accept($token_name)
 	{
 		if ($this->is($token_name))
@@ -409,6 +484,9 @@ abstract class RecursiveDescentParser {
 		return FALSE;
 	}
 
+	/**
+	 * Enforce an expected token.
+	 */
 	protected function expect($token_name)
 	{
 		if ($this->accept($token_name))
@@ -416,11 +494,170 @@ abstract class RecursiveDescentParser {
 			return TRUE;
 		}
 
-		throw new Exception('Unexpected ' . $this->token[0] . '(' . $this->token[1] . ')' . ' expected ' . $token_name . '.');
+		throw new UnsafeConditionalException('Unexpected ' . $this->token[0] . '(' . $this->token[1] . ')' . ' expected ' . $token_name . '.');
 	}
 
+	/**
+	 * Move to the next token.
+	 */
 	protected function next()
 	{
 		$this->token = array_shift($this->tokens);
+	}
+}
+
+class If_statement {
+
+	protected $parser;
+	protected $output = '';
+
+	protected $last_could_eval = TRUE;
+	protected $all_previous_could_eval = TRUE;
+
+	protected $last_result = TRUE;
+	protected $output_has_if = FALSE;
+	protected $done = FALSE; // true if no more should be printed
+
+	public function __construct(Conditional_parser $parser)
+	{
+		$this->parser = $parser;
+	}
+
+	public function add_if($condition, $can_eval)
+	{
+		if ($can_eval)
+		{
+			$this->evaluate($condition);
+		}
+		else
+		{
+			$this->output_condition($condition);
+		}
+
+		$this->set_last_could_eval($can_eval);
+
+		return $this->should_add_body();
+	}
+
+	public function add_elseif($condition, $can_eval)
+	{
+		if ($this->is_done())
+		{
+			return;
+		}
+
+		if ($can_eval)
+		{
+			$result = $this->evaluate($condition);
+
+			// If not all previous ones have evaluated, then we can't
+			// make a determination on a true branch since a previous may also
+			// be true, rendering this one moot. We'll output an easily parsable
+			// alternative for the next pass
+			if ( ! $this->all_previous_could_eval && $result == TRUE)
+			{
+				$this->output_condition('TRUE');
+			}
+		}
+		else
+		{
+			$this->output_condition($condition);
+		}
+
+		$this->set_last_could_eval($can_eval);
+
+		return $this->should_add_body();
+	}
+
+
+	public function add_else()
+	{
+		// done? don't process
+		if ($this->is_done())
+		{
+			return;
+		}
+
+		if ( ! $this->all_previous_could_eval)
+		{
+			$this->parser->output('{if:else}');
+		}
+
+		$this->last_result = TRUE;
+		$this->set_last_could_eval(TRUE);
+
+		return $this->should_add_body();
+	}
+
+	public function should_add_body()
+	{
+		// done? definitely don't add the body
+		if ($this->done)
+		{
+			return FALSE;
+		}
+
+		// eval'd and false? don't show the body
+		if ($this->last_could_eval == TRUE && $this->last_result == FALSE)
+		{
+			return FALSE;
+		}
+
+		return TRUE;
+	}
+
+	public function end_if()
+	{
+		if ($this->output_has_if)
+		{
+			$this->parser->output('{/if}');
+		}
+
+		return $this->output;
+	}
+
+	protected function output_condition($condition)
+	{
+		// otherwise we print it.
+		if ( ! $this->output_has_if)
+		{
+			$this->output_has_if = TRUE;
+			$this->parser->output('{if ' . $condition . '}');
+		}
+		else
+		{
+			$this->parser->output('{if:elseif ' . $condition . '}');
+		}
+	}
+
+	protected function is_done()
+	{
+		// Everything has eval'd and we've hit a true one?
+		// That means we're done here.
+		if ($this->all_previous_could_eval && $this->last_could_eval && $this->last_result == TRUE)
+		{
+			$this->done = TRUE;
+		}
+
+		return $this->done;
+	}
+
+	protected function set_last_could_eval($value)
+	{
+		$this->last_could_eval = $value;
+
+		if ($value === FALSE)
+		{
+			$this->all_previous_could_eval = FALSE;
+		}
+	}
+
+	protected function evaluate($condition)
+	{
+		$result = FALSE;
+		eval("\$result = (".$condition.");");
+
+		$this->last_result = (bool) $result;
+		return $this->last_result;
 	}
 }
