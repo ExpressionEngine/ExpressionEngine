27603021abc20d7470bf56337db3f430a887a579
Pascal Kriete <pascal@pascalkriete.com>
2014-05-14 17:02:17 -0400

    New folder structure testing skeleton.

---
+++ system/EllisLab/Tests/ExpressionEngine/Library/Parser/Conditional/ConditionalLexerTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace EllisLab\Tests\ExpressionEngine\Library\Parser\Conditional;
+
+class ConditionalLexerTest extends \PHPUnit_Framework_TestCase {
+
+	public function testEmpty()
+	{
+		$this->assertTrue(TRUE);
+	}
+}
---
+++ system/EllisLab/Tests/ExpressionEngine/Library/Parser/Conditional/ConditionalParserTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace EllisLab\Tests\ExpressionEngine\Library\Parser\Conditional;
+
+class ConditionalParserTest extends \PHPUnit_Framework_TestCase {
+
+	public function testEmpty()
+	{
+		$this->assertTrue(TRUE);
+	}
+}
