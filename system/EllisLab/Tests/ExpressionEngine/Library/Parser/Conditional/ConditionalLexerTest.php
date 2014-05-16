f3085887c78903c8e6eafd6216a9936bfc8cab20
Seth Croston Barber <seth.barber@ellislab.com>
2014-05-16 15:52:29 -0700

    Finished the invalid operator tests (added the without spaces tests)

--- system/EllisLab/Tests/ExpressionEngine/Library/Parser/Conditional/ConditionalLexerTest.php
+++ system/EllisLab/Tests/ExpressionEngine/Library/Parser/Conditional/ConditionalLexerTest.php
@@ -8,10 +8,10 @@ class ConditionalLexerTest extends \PHPUnit_Framework_TestCase {
 
 	protected $valueTypes = array(
 		'bool'				=> array('token' => array('BOOL', 'TRUE'),							'value' => 'TRUE'),
-		'int'				=> array('token' => array('NUMBER', '5'),							'value' => 5),
-		'negative'			=> array('token' => array('NUMBER', '-5'),							'value' => -5),
-		'bigfloat'			=> array('token' => array('NUMBER', '5.1'),							'value' => 5.1),
-		'littlefloat'		=> array('token' => array('NUMBER', '.1'),							'value' => .1),
+		'int'				=> array('token' => array('NUMBER', '5'),							'value' => '5'),
+		'negative'			=> array('token' => array('NUMBER', '-5'),							'value' => '-5'),
+		'bigfloat'			=> array('token' => array('NUMBER', '5.1'),							'value' => '5.1'),
+		'littlefloat'		=> array('token' => array('NUMBER', '.1'),							'value' => '.1'),
 		'string'			=> array('token' => array('STRING', 'string'),						'value' => '"string"'),
 		'dash-string'		=> array('token' => array('STRING', 'dash-string'),					'value' => '"dash-string"'),
 		'dot.string'		=> array('token' => array('STRING', 'dot.string'),					'value' => '"dot.string"'),
@@ -80,7 +80,9 @@ class ConditionalLexerTest extends \PHPUnit_Framework_TestCase {
 			array(),
 			$this->validOperatorsWithSpaces(),
 			$this->validOperatorsWithoutSpaces(),
-			$this->invalidOperatorsWithSpaces()
+			$this->invalidOperatorsWithSpaces(),
+			$this->invalidOperatorsWithoutSpaces(),
+			$this->edgyInvalidOperatorsWithoutSpaces()
 		);
 	}
 
@@ -254,6 +256,64 @@ class ConditionalLexerTest extends \PHPUnit_Framework_TestCase {
 			'===', '!=='
 		);
 
+		// Build out some combinations
+		foreach ($valid_operators as $first)
+		{
+			foreach ($valid_operators as $second)
+			{
+				$operator = $first.$second;
+
+				if (in_array($operator, $valid_operators))
+				{
+					continue;
+				}
+
+				$invalid_operators[] = $operator;
+			}
+		}
+
+		$invalid_operators = array_unique($invalid_operators);
+
+		foreach ($invalid_operators as $operator)
+		{
+			foreach ($this->valueTypes as $type => $value)
+			{
+				$expected = array(
+					$value['token'],
+					array('WHITESPACE',	' '),
+					array('MISC', $operator),
+					array('WHITESPACE',	' '),
+					$value['token']
+				);
+
+				$return[] = array(
+					"The \"{$operator}\" operator with {$type} values",
+					$this->assembleCommonCondition($value['value']." ".$operator." ".$value['value']),
+					$this->assembleCommonTokens($expected)
+				);
+			}
+		}
+
+		return $return;
+	}
+
+	// Things change without spaces around the operator
+	protected function invalidOperatorsWithoutSpaces()
+	{
+		$return = array();
+
+		$valid_operators = array(
+			'||', '&&', '**',
+			'==', '!=', '<=', '>=', '<>', '<', '>',
+			'%', '+', '-', '*', '/',
+			'.', '!', '^'
+		);
+
+		// Manual invalid operator assignments
+		$invalid_operators = array(
+			'===', '!=='
+		);
+
 		$edge_cases = array();
 
 		// Build out some combinations
@@ -268,7 +328,6 @@ class ConditionalLexerTest extends \PHPUnit_Framework_TestCase {
 					continue;
 				}
 
-				// We'll be handling these edge cases later
 				if ($second == '.' || $second == '-')
 				{
 					$edge_cases[] = $operator;
@@ -285,26 +344,171 @@ class ConditionalLexerTest extends \PHPUnit_Framework_TestCase {
 
 		foreach ($invalid_operators as $operator)
 		{
-			// Testing our common value types for edge-cases.
-			// We don't need to care about permutations here just combinations
-			// because we need to ensure that these value types are found
-			// on both sides of an operator.
 			foreach ($this->valueTypes as $type => $value)
 			{
+				// Float followed by a period is an error state and throws an
+				// exception.
+				if ((strpos($type, 'float') !== FALSE) && (strpos($operator, '.') === 0))
+				{
+					continue;
+				}
+
 				$expected = array(
 					$value['token'],
-					array('WHITESPACE',	' '),
-					array('MISC',	$operator),
-					array('WHITESPACE',	' '),
+					array('MISC', $operator),
 					$value['token']
 				);
 
 				$return[] = array(
 					"The \"{$operator}\" operator with {$type} values",
-					$this->assembleCommonCondition($value['value']." ".$operator." ".$value['value']),
+					$this->assembleCommonCondition($value['value'].$operator.$value['value']),
 					$this->assembleCommonTokens($expected)
 				);
 			}
+		}
+
+		// When a period or dash end an operator and the next character is
+		// a digit then it's not an operator any longer
+		foreach ($edge_cases as $operator)
+		{
+			foreach ($this->valueTypes as $type => $value)
+			{
+				// To avoid confusing code these will be done "by hand"
+				if ($value['token'][0] == 'NUMBER')
+				{
+					continue;
+				}
+
+				$expected = array(
+					$value['token'],
+					array('MISC', $operator),
+					$value['token']
+				);
+
+				$return[] = array(
+					"The \"{$operator}\" operator with {$type} values",
+					$this->assembleCommonCondition($value['value'].$operator.$value['value']),
+					$this->assembleCommonTokens($expected)
+				);
+			}
+		}
+
+		return $return;
+	}
+
+	protected function edgyInvalidOperatorsWithoutSpaces()
+	{
+		$return = array();
+
+		$negative_edgy_operators = array(
+			'||-', '&&-', '**-', '==-', '!=-', '<=-', '>=-', '<>-', '<-', '>-',
+			'%-', '+-', '--', '*-', '/-', '.-', '!-', '^-',
+		);
+
+		foreach ($negative_edgy_operators as $operator)
+		{
+			// First the cases where these things are MISC
+			foreach (array('negative', 'littlefloat') as $type)
+			{
+				if ($type == 'littlefloat' && $operator == '.-')
+				{
+					continue; // this an exception
+				}
+
+				$value = $this->valueTypes[$type];
+				$expected = array(
+					$value['token'],
+					array('MISC', $operator),
+					$value['token']
+				);
+
+				$return[] = array(
+					"The \"{$operator}\" operator with {$type} values",
+					$this->assembleCommonCondition($value['value'].$operator.$value['value']),
+					$this->assembleCommonTokens($expected)
+				);
+			}
+
+			// Now what to do when these things were followed by a digit
+			foreach (array('int', 'bigfloat') as $type)
+			{
+				if ($type == 'bigfloat' && $operator == '.-')
+				{
+					continue; // this an exception
+				}
+
+				$value = $this->valueTypes[$type];
+				$left = $value['token'];
+				$right = array(
+					$left[0],
+					'-'.$left[1]
+				);
+
+				$expected = array(
+					$left,
+					array('OPERATOR', substr($operator, 0, -1)),
+					$right
+				);
+
+				$return[] = array(
+					"The \"{$operator}\" operator with {$type} values",
+					$this->assembleCommonCondition($value['value'].$operator.$value['value']),
+					$this->assembleCommonTokens($expected)
+				);
+			}
+		}
+
+		$float_edgy_operators = array(
+			'||.', '&&.', '**.', '==.', '!=.', '<=.', '>=.', '<>.', '<.', '>.',
+			'%.', '+.', '-.', '*.', '/.', '..', '!.', '^.'
+		);
+
+		foreach ($float_edgy_operators as $operator)
+		{
+			// First the cases where these things are MISC
+			foreach (array('negative', 'littlefloat') as $type)
+			{
+				if ($type == 'littlefloat' && $operator == '..')
+				{
+					continue; // this an exception
+				}
+
+				$value = $this->valueTypes[$type];
+				$expected = array(
+					$value['token'],
+					array('MISC', $operator),
+					$value['token']
+				);
+
+				$return[] = array(
+					"The \"{$operator}\" operator with {$type} values",
+					$this->assembleCommonCondition($value['value'].$operator.$value['value']),
+					$this->assembleCommonTokens($expected)
+				);
+			}
+
+			// Now what to do when these things were followed by a digit
+			// If followed by bigfloat we are in an error state and an exception
+			// is thrown. So we will only test ints.
+			$type = 'int';
+			$value = $this->valueTypes[$type];
+			$left = $value['token'];
+			$right = array(
+				$left[0],
+				'.'.$left[1]
+			);
+
+			$expected = array(
+				$left,
+				array('OPERATOR', substr($operator, 0, -1)),
+				$right
+			);
+
+			$return[] = array(
+				"The \"{$operator}\" operator with {$type} values",
+				$this->assembleCommonCondition($value['value'].$operator.$value['value']),
+				$this->assembleCommonTokens($expected)
+			);
 		}
 
 		return $return;
