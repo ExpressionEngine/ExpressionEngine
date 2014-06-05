f5fb36ddea270da481254a1117c2330d4e6149bf
Seth Croston Barber <seth.barber@ellislab.com>
2014-06-05 15:40:54 -0700

    Removing the "solo" class.

--- system/expressionengine/views/logs/cp.php
+++ system/expressionengine/views/logs/cp.php
@@ -25,7 +25,7 @@
 
 			<div class="item">
 				<ul class="toolbar">
-					<li class="solo remove"><a href="<?=cp_url('logs/delete/cp/'.$row['id'])?>" title="remove"></a></li>
+					<li class="remove"><a href="<?=cp_url('logs/delete/cp/'.$row['id'])?>" title="remove"></a></li>
 				</ul>
 				<h3><b><?=lang('date_logged')?>:</b> <?=$row['act_date']?>, <b><?=lang('site')?>:</b> <?=$row['site_label']?><br><b><?=lang('username')?>:</b> <?=$row['username']?>, <b><abbr title="<?=lang('internet_protocol')?>"><?=lang('ip')?></abbr>:</b> <?=$row['ip_address']?></h3>
 				<div class="message">
