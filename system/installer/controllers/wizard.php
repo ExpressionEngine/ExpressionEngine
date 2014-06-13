2925bde10f13dd2c2f8a1c553bf378132dd2a877
Pascal Kriete <pascal@pascalkriete.com>
2014-06-13 11:19:35 -0400

    Library to track update notices that get printed at the end of the update.

--- system/installer/controllers/wizard.php
+++ system/installer/controllers/wizard.php
@@ -187,6 +187,10 @@ class Wizard extends CI_Controller {
 		$this->load->library('localize');
 		$this->load->library('cp');
 
+		// Update notices are used to print info at the end of
+		// the update
+		$this->load->library('update_notices');
+
 		// Set the image URL
 		$this->image_path = $this->_set_image_path();
 
@@ -516,6 +520,9 @@ class Wizard extends CI_Controller {
 
 			$this->logger->updater("Update complete. Now running version {$this->version}.");
 
+			// List any update notices we have
+			$vars['update_notices'] = $this->update_notices->get();
+
 			$this->_set_output('uptodate', $vars);
 			return FALSE;
 		}
@@ -629,6 +636,9 @@ class Wizard extends CI_Controller {
 	function _license($show_error = FALSE)
 	{
 		$data['show_error'] = $show_error;
+
+		// clear the update notices if we have any from last time
+		$this->update_notices->clear();
 
 		if ($this->is_installed == FALSE)
 		{
