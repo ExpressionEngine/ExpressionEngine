34d538d89f0013cfb8b139c70c85d745e9d3d133
Seth Croston Barber <seth.barber@ellislab.com>
2014-06-13 11:05:20 -0700

    Added tests for the cp_links method.

--- system/EllisLab/Tests/ExpressionEngine/Library/CP/PaginationTest.php
+++ system/EllisLab/Tests/ExpressionEngine/Library/CP/PaginationTest.php
@@ -107,4 +107,130 @@ class PaginationTest extends \PHPUnit_Framework_TestCase {
 		$links = Pagination::create(10, 0, 1)->cp_links($url);
 		$this->assertEquals(array(), $links, "0 Pages creates no links");
 	}
+
+	/**
+	 * Test the cp_links() method
+	 *
+	 * @dataProvider cpLinksDataProvider
+	 */
+	public function testCpLinks($description, $per_page, $total_count, $current_page, $url, $pages, $page_variable, $expected)
+	{
+		// First with the constructor
+		$pagination = new Pagination($per_page, $total_count, $current_page);
+		$links = $pagination->cp_links($url, $pages, $page_variable);
+		$this->assertEquals($expected, $links, $description);
+
+		// Now with the create() method
+		$links = Pagination::create($per_page, $total_count, $current_page)->cp_links($url, $pages, $page_variable);
+		$this->assertEquals($expected, $links, $description);
+	}
+
+	public function cpLinksDataProvider()
+	{
+		$url = new \EllisLab\ExpressionEngine\Library\CP\Url('foo/bar');
+
+		$return = array();
+
+		// When the current page is the first page there should be no "prev" link
+		$expected = array(
+			'current_page' => 1,
+			'first' => 'index.php?/cp/foo/bar',
+			'next' => 'index.php?/cp/foo/bar&page=2',
+			'last' => 'index.php?/cp/foo/bar&page=10',
+			'pages' => array(
+				'1' => 'index.php?/cp/foo/bar&page=1',
+				'2' => 'index.php?/cp/foo/bar&page=2',
+				'3' => 'index.php?/cp/foo/bar&page=3',
+			)
+		);
+
+		$return[] = array('No "Prev" Link', 10, 100, 1, $url, 3, 'page', $expected);
+
+		// When the current page is the last page there should be no "next" link
+		$expected = array(
+			'current_page' => 10,
+			'first' => 'index.php?/cp/foo/bar',
+			'prev' => 'index.php?/cp/foo/bar&page=9',
+			'last' => 'index.php?/cp/foo/bar&page=10',
+			'pages' => array(
+				'8' => 'index.php?/cp/foo/bar&page=8',
+				'9' => 'index.php?/cp/foo/bar&page=9',
+				'10' => 'index.php?/cp/foo/bar&page=10',
+			)
+		);
+		$return[] = array('No "Next" Link', 10, 100, 10, $url, 3, 'page', $expected);
+
+		// Test the 'page' variable
+		// Also testing that current_page 2 still starts the pages array at 1
+		$expected = array(
+			'current_page' => 2,
+			'first' => 'index.php?/cp/foo/bar',
+			'prev' => 'index.php?/cp/foo/bar&p=1',
+			'next' => 'index.php?/cp/foo/bar&p=3',
+			'last' => 'index.php?/cp/foo/bar&p=10',
+			'pages' => array(
+				'1' => 'index.php?/cp/foo/bar&p=1',
+				'2' => 'index.php?/cp/foo/bar&p=2',
+				'3' => 'index.php?/cp/foo/bar&p=3',
+			)
+		);
+
+		$return[] = array('"p" for the page_variable', 10, 100, 2, $url, 3, 'p', $expected);
+
+		// Testing a small $pages
+		$expected = array(
+			'current_page' => 2,
+			'first' => 'index.php?/cp/foo/bar',
+			'prev' => 'index.php?/cp/foo/bar&p=1',
+			'next' => 'index.php?/cp/foo/bar&p=3',
+			'last' => 'index.php?/cp/foo/bar&p=10',
+			'pages' => array(
+				'1' => 'index.php?/cp/foo/bar&p=1',
+				'2' => 'index.php?/cp/foo/bar&p=2',
+			)
+		);
+
+		$return[] = array('"p" for the page_variable', 10, 100, 2, $url, 2, 'p', $expected);
+
+		// Testing a large $pages
+		$expected = array(
+			'current_page' => 2,
+			'first' => 'index.php?/cp/foo/bar',
+			'prev' => 'index.php?/cp/foo/bar&p=1',
+			'next' => 'index.php?/cp/foo/bar&p=3',
+			'last' => 'index.php?/cp/foo/bar&p=10',
+			'pages' => array(
+				'1' => 'index.php?/cp/foo/bar&p=1',
+				'2' => 'index.php?/cp/foo/bar&p=2',
+				'3' => 'index.php?/cp/foo/bar&p=3',
+				'4' => 'index.php?/cp/foo/bar&p=4',
+				'5' => 'index.php?/cp/foo/bar&p=5',
+				'6' => 'index.php?/cp/foo/bar&p=6',
+				'7' => 'index.php?/cp/foo/bar&p=7',
+				'8' => 'index.php?/cp/foo/bar&p=8',
+				'9' => 'index.php?/cp/foo/bar&p=9',
+				'10' => 'index.php?/cp/foo/bar&p=10',
+			)
+		);
+
+		$return[] = array('"p" for the page_variable', 10, 100, 2, $url, 10, 'p', $expected);
+
+		// Testing where we want to show more pages than available
+		$expected = array(
+			'current_page' => 2,
+			'first' => 'index.php?/cp/foo/bar',
+			'prev' => 'index.php?/cp/foo/bar&p=1',
+			'next' => 'index.php?/cp/foo/bar&p=3',
+			'last' => 'index.php?/cp/foo/bar&p=3',
+			'pages' => array(
+				'1' => 'index.php?/cp/foo/bar&p=1',
+				'2' => 'index.php?/cp/foo/bar&p=2',
+				'3' => 'index.php?/cp/foo/bar&p=3',
+			)
+		);
+
+		$return[] = array('"p" for the page_variable', 10, 30, 2, $url, 4, 'p', $expected);
+
+		return $return;
+	}
 }
