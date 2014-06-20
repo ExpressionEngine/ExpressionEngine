5ce768eec0bef38634947d3fe536589286771d66
Seth Croston Barber <seth.barber@ellislab.com>
2014-06-20 15:32:26 -0700

    Added tests for the search log.

---
+++ system/tests/rspec/pages/logs_search.rb
@@ -0,0 +1,70 @@
+class SearchLog < ControlPanelPage
+  set_url_matcher /logs\/search/
+
+  element :title, 'div.box form h1'
+
+	element :phrase_search, 'input[name=filter_by_phrase]'
+	element :submit_button, 'input.submit'
+	element :username_filter, 'select[name=filter_by_username]'
+	element :site_filter, 'select[name=filter_by_site]'
+	element :date_filter, 'select[name=filter_by_date]'
+	element :perpage_filter, 'select[name=perpage]'
+
+  element :alert, 'div.alert'
+  element :no_results, 'p.no-results'
+  element :remove_all, 'a.btn.remove'
+  element :pagination, 'div.paginate'
+
+  elements :pages, 'div.paginate ul li a'
+  elements :items, 'section.item-wrap div.item'
+
+  def generate_data (count: 250, site_id: nil, member_id: nil, screen_name: nil, ip_address: nil, timestamp_min: nil, timestamp_max: nil, type: nil, terms: nil)
+    command = "cd fixtures && php searchLog.php"
+
+    if count
+      command += " --count " + count.to_s
+    end
+
+    if site_id
+      command += " --site-id " + site_id.to_s
+    end
+
+    if member_id
+      command += " --member-id " + member_id.to_s
+    end
+
+    if screen_name
+      command += " --screen-name '" + screen_name.to_s + "'"
+    end
+
+    if ip_address
+      command += " --ip-address '" + ip_address.to_s + "'"
+    end
+
+    if timestamp_min
+      command += " --timestamp-min " + timestamp_min.to_s
+    end
+
+    if timestamp_max
+      command += " --timestamp-max " + timestamp_max.to_s
+    end
+
+    if type
+      command += " --type '" + type.to_s + "'"
+    end
+
+    if terms
+      command += " --terms '" + terms.to_s + "'"
+    end
+
+    command += " > /dev/null 2>&1"
+
+    system(command)
+  end
+
+	def load
+		self.open_dev_menu
+		click_link 'Logs'
+		click_link 'Search'
+	end
+end
---
+++ system/tests/rspec/tests/test_search_log.rb
@@ -0,0 +1,164 @@
+require './bootstrap.rb'
+
+feature 'Search Log' do
+
+  before(:each) do
+    cp_session
+
+    @page = SearchLog.new
+    @page.generate_data(count: 150, timestamp_min: 26)
+    @page.generate_data(count: 35, member_id: 2, screen_name: 'johndoe', timestamp_min: 25)
+    add_member(username: 'johndoe')
+    @page.load
+
+    # These should always be true at all times if not something has gone wrong
+    @page.displayed?
+    @page.title.text.should eq 'Search Logs'
+    @page.should have_phrase_search
+    @page.should have_submit_button
+    @page.should have_username_filter
+    # @page.should have_site_filter # This will not be present if MSM is diabled or we are running Core
+    @page.should have_date_filter
+    @page.should have_perpage_filter
+  end
+
+  it 'shows the Search Logs page' do
+    @page.should have_remove_all
+    @page.should have_pagination
+
+    @page.perpage_filter.value.should eq "50"
+
+    @page.should have(6).pages
+    @page.pages.map {|name| name.text}.should == ["First", "1", "2", "3", "Next", "Last"]
+
+    @page.should have(50).items # Default is 50 per page
+  end
+
+  # Confirming phrase search
+  # @TODO pending phrase search working
+
+  # Confirming individual filter behavior
+  it 'filters by username' do
+    @page.username_filter.select "johndoe"
+    @page.submit_button.click
+
+    @page.username_filter.has_select?('filter_by_username', :selected => "johndoe")
+    @page.should have(35).items
+    @page.should_not have_pagination
+  end
+
+  # @TODO Need data for extra site in order to filter by it
+  # it 'filters by site' do
+  #   @page.site_filter.select "foobarbaz"
+  #   @page.submit_button.click
+  #
+  #   @page.should have(x).items
+  # end
+
+  # Since this logs in a user we should have 1 entry!
+  it 'filters by date' do
+    @page.generate_data(count: 23, timestamp_max: 22)
+    @page.load
+
+    @page.date_filter.select "Last 24 Hours"
+    @page.submit_button.click
+
+    @page.date_filter.has_select?('filter_by_date', :selected => "Last 24 Hours")
+    @page.should have(23).items
+  end
+
+  it 'can change page size' do
+    @page.perpage_filter.select "25 results"
+    @page.submit_button.click
+
+    @page.perpage_filter.has_select?('perpage', :selected => "25 results")
+    @page.should have(25).items
+    @page.should have_pagination
+    @page.should have(6).pages
+    @page.pages.map {|name| name.text}.should == ["First", "1", "2", "3", "Next", "Last"]
+  end
+
+  # Confirming combining filters work
+  it 'can combine username and page size filters' do
+    @page.perpage_filter.select "150 results"
+    @page.submit_button.click
+
+    # First, confirm we have both 'admin' and 'johndoe' on same page
+    @page.perpage_filter.has_select?('perpage', :selected => "150 results")
+    @page.should have(150).items
+    @page.should have_pagination
+    @page.should have_text "johndoe"
+    @page.should have_text "admin"
+
+    # Now, combine the filters
+    @page.perpage_filter.select "150 results"
+    @page.username_filter.select "johndoe"
+    @page.submit_button.click
+
+    @page.perpage_filter.has_select?('perpage', :selected => "150 results")
+    @page.username_filter.has_select?('filter_by_username', :selected => "johndoe")
+    @page.should have(35).items
+    @page.should_not have_pagination
+    @page.should_not have_text "admin"
+  end
+
+  # @TODO pending phrase search working
+  # it 'can combine phrase search with filters' do
+  # end
+
+  # Confirming the log deletion action
+  it 'can remove a single entry' do
+    our_terms = "Rspec entry to be deleted"
+
+    @page.generate_data(count: 1, timestamp_max: 0, terms: our_terms)
+    @page.load
+
+    log = @page.find('section.item-wrap div.item', :text => our_terms)
+    log.find('li.remove a').click
+
+    @page.should have_alert
+    @page.should have_no_content our_terms
+  end
+
+  it 'can remove all entries' do
+    @page.remove_all.click
+
+    @page.should have_alert
+    @page.should have_no_results
+    @page.should_not have_pagination
+  end
+
+  # Confirming Pagination behavior
+  it 'shows the Prev button when on page 2' do
+    click_link "Next"
+
+    @page.should have_pagination
+    @page.should have(7).pages
+    @page.pages.map {|name| name.text}.should == ["First", "Previous", "1", "2", "3", "Next", "Last"]
+  end
+
+  it 'does not show Next on the last page' do
+    click_link "Last"
+
+    @page.should have_pagination
+    @page.should have(6).pages
+    @page.pages.map {|name| name.text}.should == ["First", "Previous", "2", "3", "4", "Last"]
+  end
+
+  it 'does not lose a filter value when paginating' do
+    @page.perpage_filter.select "25 results"
+    @page.submit_button.click
+
+    @page.perpage_filter.has_select?('perpage', :selected => "25 results")
+    @page.should have(25).items
+
+    click_link "Next"
+
+    @page.perpage_filter.has_select?('perpage', :selected => "25 results")
+    @page.should have(25).items
+    @page.should have_pagination
+    @page.should have(7).pages
+    @page.pages.map {|name| name.text}.should == ["First", "Previous", "1", "2", "3", "Next", "Last"]
+  end
+
+end
