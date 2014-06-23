72524847a180c0e2f35beb220200a3cd8a5ee238
Seth Croston Barber <seth.barber@ellislab.com>
2014-06-23 12:28:38 -0700

    Added tests for the email log.

---
+++ system/tests/rspec/pages/logs_email.rb
@@ -0,0 +1,84 @@
+class EmailLog < ControlPanelPage
+	set_url_matcher /logs\/email/
+
+	element :title, 'div.box form h1'
+
+	element :phrase_search, 'input[name=filter_by_phrase]'
+	element :submit_button, 'input.submit'
+	element :username_filter, 'select[name=filter_by_username]'
+	element :date_filter, 'select[name=filter_by_date]'
+	element :perpage_filter, 'select[name=perpage]'
+
+	element :alert, 'div.alert'
+	element :no_results, 'p.no-results'
+	element :remove_all, 'a.btn.remove'
+	element :pagination, 'div.paginate'
+
+	elements :pages, 'div.paginate ul li a'
+	elements :items, 'section.item-wrap div.item'
+
+	def generate_data(
+		count: 250,
+		member_id: nil,
+		member_name: nil,
+		ip_address: nil,
+		timestamp_min: nil,
+		timestamp_max: nil,
+		recipient: nil,
+		recipient_name: nil,
+		subject: nil,
+		message: nil
+		)
+		command = "cd fixtures && php emailLog.php"
+
+		if count
+			command += " --count " + count.to_s
+		end
+
+		if member_id
+			command += " --member-id " + member_id.to_s
+		end
+
+		if member_name
+			command += " --member-name '" + member_name.to_s + "'"
+		end
+
+		if ip_address
+			command += " --ip-address '" + ip_address.to_s + "'"
+		end
+
+		if timestamp_min
+			command += " --timestamp-min " + timestamp_min.to_s
+		end
+
+		if timestamp_max
+			command += " --timestamp-max " + timestamp_max.to_s
+		end
+
+		if recipient
+			command += " --recipient '" + recipient.to_s + "'"
+		end
+
+		if recipient_name
+			command += " --recipient-name '" + recipient_name.to_s + "'"
+		end
+
+		if subject
+			command += " --subject '" + subject.to_s + "'"
+		end
+
+		if message
+			command += " --message '" + message.to_s + "'"
+		end
+
+		command += " > /dev/null 2>&1"
+
+		system(command)
+	end
+
+	def load
+		self.open_dev_menu
+		click_link 'Logs'
+		click_link 'e-mail'
+	end
+end
