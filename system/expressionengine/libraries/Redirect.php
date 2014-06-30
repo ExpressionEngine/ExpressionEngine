68ab8baa6480ff23eeeb2a50727d6c04529cb64d
Daniel Bingham <daniel.bingham@ellislab.com>
2014-06-30 14:55:14 -0400

    Comparing xss cleaned URL (in link context) to original and erroring out if xss_clean() changed anything.

--- system/expressionengine/libraries/Redirect.php
+++ system/expressionengine/libraries/Redirect.php
@@ -34,35 +34,37 @@ if ( ! isset($_GET['URL']))
 }
 
 
-// $_GET['URL'] = str_replace(array("\r", "\r\n", "\n", '%3A','%3a','%2F','%2f'), array('', '', '', ':', ':', '/', '/'), $_GET['URL']);
-
 if (strncmp($_GET['URL'], 'http', 4) != 0 && strpos($_GET['URL'], '://') === FALSE && substr($_GET['URL'], 0, 1) != '/')
 {
 	$_GET['URL'] = "http://".$_GET['URL'];
 }
-
-// $_GET['URL'] = str_replace( array('"', "'", ')', '(', ';', '}', '{', 'script%', 'script&', '&#40', '&#41'), '', strip_tags($_GET['URL']));
 
 
 $host = ( ! isset($_SERVER['HTTP_HOST'])) ? '' : (substr($_SERVER['HTTP_HOST'],0,4) == 'www.' ? substr($_SERVER['HTTP_HOST'], 4) : $_SERVER['HTTP_HOST']);
 
 $force_redirect = ($request_type != 'CP' && config_item('force_redirect') == TRUE) ? TRUE: FALSE;
 
+$link = "<a rel=\"nofollow\" href='".$_GET['URL']."'>".$_GET['URL']."</a>";
+
+if ( $link !== ee()->security->xss_clean($link) )
+{
+	echo 'The link you are being redirected to contained some potentially mallicious or dangerous code. We recommend you hit the back button and report the link that generated this message.';
+	die();
+}
+
 
 if ($force_redirect == TRUE OR ( ! isset($_SERVER['HTTP_REFERER']) OR ! stristr($_SERVER['HTTP_REFERER'], $host)))
 {
 	// Possibly not from our site, so we give the user the option
 	// Of clicking the link or not
-	$link = ee()->security->xss_clean("<a rel=\"nofollow\" href='".$_GET['URL']."'>".$_GET['URL']."</a>");
 	$str = "<html>\n<head>\n<meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>\n<title>Redirect</title>\n</head>\n<body>".
 			"<p>To proceed to the URL you have requested, click the link below:</p>".
 			"<p>$link</p>\n</body>\n</html>";
 }
 else
 {
-	$link = ee()->security->xss_clean('<meta http-equiv="refresh" content="0; URL='.$_GET['URL'].'">');
 	$str = "<html>\n<head>\n<meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>\n<title>Redirect</title>\n".
-		   $link .
+		   '<meta http-equiv="refresh" content="0; URL='.$_GET['URL'].'">' .
 		   "\n</head>\n<body>\n</body>\n</html>";
 }
 
