57855c8bce40b4279050adc7eb75a4265ec8a366
Seth Croston Barber <seth.barber@ellislab.com>
2014-07-17 11:08:52 -0700

    Removed unnecessary CSS classes.

--- system/expressionengine/views/utilities/translate/list.php
+++ system/expressionengine/views/utilities/translate/list.php
@@ -9,26 +9,20 @@
 	<?php $this->view('_shared/form_messages')?>
 	<table cellspacing="0">
 		<tr>
-			<th class="first highlight"><?=lang('file_name')?> <a href="<?=$file_name_sort_url?>" class="ico sort <?=$file_name_direction?> right"></a></th>
+			<th class="highlight"><?=lang('file_name')?> <a href="<?=$file_name_sort_url?>" class="ico sort <?=$file_name_direction?> right"></a></th>
 			<th><?=lang('manage')?></th>
-			<th class="last check-ctrl"><input type="checkbox" title="<?=strtolower(lang('select_all'))?>"></th>
+			<th class="check-ctrl"><input type="checkbox" title="<?=strtolower(lang('select_all'))?>"></th>
 		</tr>
 
 	<?php foreach($files as $i => $file): ?>
-	<?php if ($i == count($files) - 1): ?>
-		<tr class="last">
-	<?php elseif ($i % 2): ?>
-		<tr class="alt">
-	<?php else: ?>
 		<tr>
-	<?php endif; ?>
-			<td<?php if ($i == count($files) - 1): ?> class="first"<?php endif; ?>><?=$file['filename']?></td>
+			<td><?=$file['filename']?></td>
 			<td>
 				<ul class="toolbar">
 					<li class="edit"><a href="<?=cp_url('utilities/translate/' . $language . '/edit/' . $file['name'])?>" title="<?=strtolower(lang('edit'))?>"></a></li>
 				</ul>
 			</td>
-			<td<?php if ($i == count($files) - 1): ?> class="last"<?php endif; ?>><input type="checkbox" name="selection[]" value="<?=$file['name']?>"></td>
+			<td><input type="checkbox" name="selection[]" value="<?=$file['name']?>"></td>
 		</tr>
 	<?php endforeach; ?>
 
