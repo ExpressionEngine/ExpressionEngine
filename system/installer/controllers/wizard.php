17789475697b94a1439674bbf7902e977333fcf4
Derek Jones <derek.jones@ellislab.com>
2014-08-05 13:29:09 -0700

    Validation for username and screen name in installer
    bonus, prevents XSS and markup in both.

--- system/installer/controllers/wizard.php
+++ system/installer/controllers/wizard.php
@@ -929,6 +929,17 @@ PAPAYA;
 			$this->userdata['screen_name'] = $this->userdata['username'];
 		}
 
+		// check screen name and username for valid format
+		if (strlen($this->userdata['username']) > 50 OR preg_match("/[\|'\"!<>\{\}]/", $this->userdata['username']))
+		{
+			$errors[] = "Username is invalid. Must be less than 50 characters and cannot include the following characters: ".htmlentities('|\'"!<>{}');
+		}
+
+		if (preg_match('/[\{\}<>]/', $this->userdata['screen_name']))
+		{
+			$errors[] = "Screen Name is invalid. Must not include the following characters: ".htmlentities('{}<>');
+		}
+
 		// DB Prefix has some character restrictions
 		if ( ! preg_match("/^[0-9a-zA-Z\$_]*$/", $this->userdata['db_prefix']))
 		{
