81bc9e7960b6654f22063cc3513041e98ab4f6f1
Pascal Kriete <mail@pascal.io>
2014-08-26 15:06:48 -0400

    Moving site gateway under site

--- system/EllisLab/ExpressionEngine/Model/Site/Gateway/SiteGateway.php
+++ system/EllisLab/ExpressionEngine/Model/Site/Gateway/SiteGateway.php
@@ -1,12 +1,35 @@
 <?php
-namespace EllisLab\ExpressionEngine\Model\Gateway;
+namespace EllisLab\ExpressionEngine\Model\Site\Gateway;
 
-use EllisLab\ExpressionEngine\Model\Gateway\RowDataGateway;
+use EllisLab\ExpressionEngine\Service\Model\RowDataGateway;
 
+/**
+ * ExpressionEngine - by EllisLab
+ *
+ * @package		ExpressionEngine
+ * @author		EllisLab Dev Team
+ * @copyright	Copyright (c) 2003 - 2014, EllisLab, Inc.
+ * @license		http://ellislab.com/expressionengine/user-guide/license.html
+ * @link		http://ellislab.com
+ * @since		Version 3.0
+ * @filesource
+ */
+
+// ------------------------------------------------------------------------
+
+/**
+ * ExpressionEngine Site Table
+ *
+ * @package		ExpressionEngine
+ * @subpackage	Site\Gateway
+ * @category	Model
+ * @author		EllisLab Dev Team
+ * @link		http://ellislab.com
+ */
 class SiteGateway extends RowDataGateway {
+
 	protected static $_table_name = 'sites';
 	protected static $_primary_key = 'site_id';
-
 
 	// Properties
 	protected $site_id;
