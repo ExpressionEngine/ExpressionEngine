6e89cbc1e35df95b0677b0436ea7a1fffeffa0b6
Pascal Kriete <mail@pascal.io>
2014-08-26 15:39:27 -0400

    Action gateway to addons and action model stub for consistency.

---
+++ system/EllisLab/ExpressionEngine/Model/Addon/Gateway/ActionGateway.php
@@ -0,0 +1,39 @@
+<?php
+namespace EllisLab\ExpressionEngine\Model\Addon\Gateway;
+
+use EllisLab\ExpressionEngine\Service\Model\RowDataGateway;
+
+/**
+ * ExpressionEngine - by EllisLab
+ *
+ * @package		ExpressionEngine
+ * @author		EllisLab Dev Team
+ * @copyright	Copyright (c) 2003 - 2014, EllisLab, Inc.
+ * @license		http://ellislab.com/expressionengine/user-guide/license.html
+ * @link		http://ellislab.com
+ * @since		Version 3.0
+ * @filesource
+ */
+
+// ------------------------------------------------------------------------
+
+/**
+ * ExpressionEngine Action Table
+ *
+ * @package		ExpressionEngine
+ * @subpackage	Addon\Gateway
+ * @category	Model
+ * @author		EllisLab Dev Team
+ * @link		http://ellislab.com
+ */
+class ActionGateway extends RowDataGateway {
+	protected static $_table_name = 'actions';
+	protected static $_primary_key = 'action_id';
+
+	// Properties
+	protected $action_id;
+	protected $class;
+	protected $method;
+	protected $csrf_exempt;
+
+}
