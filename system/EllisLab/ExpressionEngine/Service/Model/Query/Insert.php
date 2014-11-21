9eb2a430c7865b2083fd2cfb0ff2ef2b329f87be
Pascal Kriete <mail@pascal.io>
2014-11-21 10:45:31 -0500

    Insert and update query strategies for models

---
+++ system/EllisLab/ExpressionEngine/Service/Model/Query/Insert.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace EllisLab\ExpressionEngine\Service\Model\Query;
+
+/**
+ * Insert is really just an update without the where.
+ */
+class Insert extends Update {
+
+	protected function actOnGateway($gateway)
+	{
+		$query = $this->store
+			->rawQuery()
+			->set($gateway->getValues())
+			->insert($gateway->getTable());
+	}
+}
