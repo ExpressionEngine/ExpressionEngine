54efcef7da3e1eb442c0f34b9894603a296d1a0f
Pascal Kriete <mail@pascal.io>
2014-11-21 10:32:01 -0500

    Query parent object

--- system/EllisLab/ExpressionEngine/Service/Model/Query/Query.php
+++ system/EllisLab/ExpressionEngine/Service/Model/Query/Query.php
@@ -1,89 +1,19 @@
 <?php
+
 namespace EllisLab\ExpressionEngine\Service\Model\Query;
 
-/**
- * ExpressionEngine - by EllisLab
- *
- * @package		ExpressionEngine
- * @author		EllisLab Dev Team
- * @copyright	Copyright (c) 2003 - 2014, EllisLab, Inc.
- * @license		http://ellislab.com/expressionengine/user-guide/license.html
- * @link		http://ellislab.com
- * @since		Version 3.0
- * @filesource
- */
+use EllisLab\ExpressionEngine\Service\Model\DataStore;
 
-// ------------------------------------------------------------------------
+abstract class Query {
 
-/**
- * ExpressionEngine Model Database Query Class
- *
- * @package		ExpressionEngine
- * @subpackage	Model
- * @category	Service
- * @author		EllisLab Dev Team
- * @link		http://ellislab.com
- */
-class Query {
+	protected $db = NULL;
+	protected $builder = NULL;
 
-	public function __construct($factory, $graph, $db)
+	public function __construct(DataStore $store, Builder $builder)
 	{
-		$this->db = $db;
-		$this->graph = $graph;
-		$this->factory = $factory;
+		$this->store = $store;
+		$this->builder = $builder;
 	}
 
-	/**
-	 *
-	 */
-	public function executeFetch($builder)
-	{
-		return $this->runStrategy('Fetch', $builder);
-	}
-
-	/**
-	 *
-	 */
-	public function executeCount($builder)
-	{
-		return $this->runStrategy('Count', $builder);
-	}
-
-	/**
-	 *
-	 */
-	public function executeDelete($builder)
-	{
-		return $this->runStrategy('Delete', $builder);
-	}
-
-	/**
-	 *
-	 */
-	public function executeUpdate($builder)
-	{
-		return $this->runStrategy('Update', $builder);
-	}
-
-	/**
-	 *
-	 */
-	public function executeCreate($builder)
-	{
-		return $this->runStrategy('Create', $builder);
-	}
-
-	/**
-	 *
-	 */
-	protected function runStrategy($strategy_name, $builder)
-	{
-		$class = __NAMESPACE__.'\\Strategy\\'.$strategy_name;
-
-		// build the query and run it
-		$strategy = new $class($builder, $this->factory, $this->graph);
-		$strategy->setDb($this->db);
-
-		return $strategy->run();
-	}
+	abstract public function run();
 }
