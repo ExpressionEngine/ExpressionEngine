7f92489ada98ca6e5e3730c50c75584e064ed717
Seth Croston Barber <seth.barber@ellislab.com>
2014-11-21 15:40:45 -0800

    Noting the tests that need to be written

--- system/tests/rspec/tests/test_addon_manager.rb
+++ system/tests/rspec/tests/test_addon_manager.rb
@@ -26,4 +26,20 @@ feature 'Add-On Manager' do
 		@page.addon_name_header[:class].should eq 'highlight'
 	end
 
+	# Can reverse sort by Add-On name
+	# Can sort by Version
+	# Can reverse sort by Version
+	# Can filter by status
+	# Can filter by developer
+	# Can adjust the perpage
+	# Pagination displays
+	# Pagination > Next works
+	# Can search
+	# "Bad" search returns "no results"
+	# Can install a single add-on
+	# Can bulk-install add-ons
+	# Removing an add-on triggers a modal (and removes, and have a "not-installed" class)
+	# The settings buttons "work" (200 response)
+	# The guide buttons "work" (200 response)
+
 end
