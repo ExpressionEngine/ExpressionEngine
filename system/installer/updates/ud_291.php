f8991a2a5700533103e3d890829183714c11697e
Derek Jones <derek.jones@ellislab.com>
2014-11-26 06:48:15 -0800

    updating conflicting files for clean merge

--- system/installer/updates/ud_291.php
+++ system/installer/updates/ud_291.php
@@ -34,45 +34,7 @@ class Updater {
 	 */
 	public function do_update()
 	{
-		ee()->load->dbforge();
-
-		$steps = new ProgressIterator(
-			array(
-				'_recompile_template_routes'
-			)
-		);
-
-		foreach ($steps as $k => $v)
-		{
-			$this->$v();
-		}
-
 		return TRUE;
-	}
-
-	// -------------------------------------------------------------------------
-
-	/**
-	 * Load all routes and resave to get rid of md5 hashes
-	 * 
-	 * @access private
-	 * @return void
-	 */
-	private function _recompile_template_routes()
-	{
-		ee()->db->select('template_id, route_required, route');
-		ee()->db->from('templates');
-		ee()->db->join('template_routes', 'templates.template_id = template_routes.template_id');
-		ee()->db->where('route_parsed is not null');
-		$query = ee()->db->get();
-
-		foreach ($query->result() as $template)
-		{
-			$ee_route = new EE_Route($template->route, $template->route_required == 'y');
-			$compiled = $ee_route->compile();
-			$data = array('route_parsed' => $compiled);
-			$this->template_model->update_template_route($template->template_id, $data);
-		}
 	}
 }
 /* END CLASS */
