3ddc7978d5b91a7ade36db0d3dbabb56aae67e57
Seth Croston Barber <seth.barber@ellislab.com>
2014-12-01 13:55:47 -0800

    Adjusting the tests to account for installing/removing plugins

--- system/tests/rspec/tests/test_addon_manager.rb
+++ system/tests/rspec/tests/test_addon_manager.rb
@@ -108,9 +108,8 @@ feature 'Add-On Manager' do
 
 		@page.status_filter.text.should eq "status (installed)"
 		@page.should_not have_css 'tr.not-installed'
-		@page.should have_pagination
-		@page.should have(5).pages
-		@page.pages.map {|name| name.text}.should == ["First", "1", "2", "Next", "Last"]
+		@page.should_not have_pagination
+		@page.should have(18).addons
 
 		# By uninstalled
 		@page.status_filter.click
@@ -121,7 +120,7 @@ feature 'Add-On Manager' do
 		@page.status_filter.text.should eq "status (uninstalled)"
 		@page.should have_css 'tr.not-installed'
 		@page.all('tr.not-installed').count().should == 20
-		@page.should_not have_pagination
+		@page.should have_pagination
 		@page.should have(21).addons
 
 		# By 'needs updates'
@@ -267,9 +266,7 @@ feature 'Add-On Manager' do
 		no_php_js_errors
 
 		@page.should_not have_css 'tr.not-installed'
-		@page.should have_pagination
-		@page.should have(5).pages
-		@page.pages.map {|name| name.text}.should == ["First", "1", "2", "Next", "Last"]
+		@page.should_not have_pagination
 
 		# Now by Third Party
 		@page.developer_filter.click
@@ -341,6 +338,12 @@ feature 'Add-On Manager' do
 		@page.status_filter_menu.click_link "uninstalled"
 		no_php_js_errors
 
+		# Show 50 should show everything
+		@page.perpage_filter.click
+		@page.wait_until_perpage_filter_menu_visible
+		@page.perpage_filter_menu.click_link "50 results"
+		no_php_js_errors
+
 		addons = @page.addon_names.map {|addon| addon.text}
 
 		# Header at 0, first "real" row is 1
@@ -394,7 +397,7 @@ feature 'Add-On Manager' do
 		@page.wait_until_modal_visible
 		@page.modal_title.text.should eq "Confirm Removal"
 		@page.modal.text.should include "You are attempting to remove the following items, please confirm this action."
-		@page.modal.text.should include 'Add-On: 20 Add-Ons'
+		@page.modal.text.should include 'Add-On: 17 Add-Ons'
 	end
 
 	it 'can remove add-ons' do
@@ -416,7 +419,7 @@ feature 'Add-On Manager' do
 		@page.status_filter.text.should eq "status (installed)"
 		@page.should have_alert
 		@page.alert.text.should include "Add-Ons Removed"
-		# @page.alert.text.should include addons.join(', ') @TODO renable when plugins can be removed
+		@page.alert.text.should include addons.join(', ')
 	end
 
 	# The settings buttons "work" (200 response)
