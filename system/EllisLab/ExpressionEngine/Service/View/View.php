2e788aea247abb0239e09d25f3ad5420859986e2
Seth Croston Barber <seth.barber@ellislab.com>
2014-12-09 15:47:32 -0800

    Implementing a host of fascade functions & the start/endBlock methods

--- system/EllisLab/ExpressionEngine/Service/View/View.php
+++ system/EllisLab/ExpressionEngine/Service/View/View.php
@@ -30,11 +30,71 @@ class View {
 	public function __construct($path)
 	{
 		$this->path = $path;
+		$this->cp = ee()->cp;
+	}
+
+	public function parse($path, $vars)
+	{
+		extract($vars);
+		include($path);
 	}
 
 	public function render(array $vars)
 	{
 		return ee()->load->view($this->path, $vars, TRUE);
+	}
+
+	public function ee_view($view, $vars = array(), $return = FALSE)
+	{
+		return ee()->load->ee_view($view, $vars, $return);
+	}
+
+	public function view($view, $vars = array(), $return = FALSE)
+	{
+		return ee()->load->view($view, $vars, $return);
+	}
+
+	/**
+	 * Allows our Views to define blocks to be used in a template/layout context
+	 *
+	 * @param str $name The name of the block
+	 */
+	public function startBlock($name)
+	{
+		$this->blockStack[] = array($name, FALSE);
+		ob_start();
+	}
+
+	public function startOrAppendBlock($name)
+	{
+		$this->blockStack[] = array($name, TRUE);
+		ob_start();
+	}
+
+	/**
+	 * Ends the block storing the buffer on the View::blocks array based on the
+	 * most recently specified name via startBlock.
+	 */
+	public function endBlock()
+	{
+		list($name, $append) = array_pop($this->blockStack);
+
+		if ($name === NULL)
+		{
+			return; // @TODO Throw an error?
+		}
+
+		$buffer = '';
+
+		if ($append && isset(ee()->view->blocks[$name]))
+		{
+			$buffer .= ee()->view->blocks[$name];
+		}
+
+		$buffer .= ob_get_contents();
+		ob_end_clean();
+
+		ee()->view->blocks[$name] = $buffer;
 	}
 }
 // END CLASS
