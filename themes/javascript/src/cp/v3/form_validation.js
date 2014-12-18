e7b414c1e7ccf11c4b921c2184131617d54ad7d7
Kevin Cupp <kevin.cupp@gmail.com>
2014-12-18 15:50:15 -0500

    Binding text field timer for invalid fields on page load also

--- themes/javascript/src/cp/v3/form_validation.js
+++ themes/javascript/src/cp/v3/form_validation.js
@@ -49,6 +49,12 @@ EE.cp.formValidation = {
 
 			that._sendAjaxRequest($(this));
 		});
+
+		// Upon loading the page with invalid fields, bind the text field
+		// timer to correct the validation as the user types
+		$('fieldset.invalid').each(function() {
+			that._bindTextFieldTimer($(this));
+		});
 	},
 
 	/**
