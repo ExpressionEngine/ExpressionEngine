67ce27b079b43890664ac9df6d42978682a705bc
Seth Croston Barber <seth.barber@ellislab.com>
2014-12-29 15:14:32 -0800

    Removing the old addons JS

--- themes/javascript/compressed/cp/addons/package_settings.js
+++
@@ -1,1 +0,0 @@
-!function(n){var e={init:function(){this.extension_enable_verify()},extension_enable_verify:function(){EE.extensions_disabled&&n("#mainContent form").submit(function(){return confirm(EE.extensions_disabled_warning)?(n(this).append(n("<input>",{type:"hidden",name:"enable_extensions",value:"yes"})),!0):!1})}};e.init()}(jQuery);
