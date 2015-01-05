e984ff48b57d04ad7f09225c853aa10ae373cbb8
Seth Croston Barber <seth.barber@ellislab.com>
2015-01-05 15:51:44 -0800

    Fixing a logic bug
    One cannot use the dependency injection container if it is not there....

--- system/codeigniter/system/core/Loader.php
+++ system/codeigniter/system/core/Loader.php
@@ -748,7 +748,7 @@ class CI_Loader {
 		}
 
 
-		if (isset($_ci_view))
+		if (isset($_ci_view) && isset(ee()->di))
 		{
 			echo ee('View')->make($_ci_view)->parse($_ci_path, $this->_ci_cached_vars, $rewrite);
 		}
