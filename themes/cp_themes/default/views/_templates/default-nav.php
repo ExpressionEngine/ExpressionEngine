f7f3b77c44409085691b73b4742004ce814c46e3
Kevin Cupp <kevin.cupp@gmail.com>
2015-01-06 11:59:42 -0500

    Page headers can now be set in controllers

--- themes/cp_themes/default/views/_templates/default-nav.php
+++ themes/cp_themes/default/views/_templates/default-nav.php
@@ -1,5 +1,33 @@
 <?php extend_template('wrapper'); ?>
 
+<?php if (isset($header)): ?>
+	<div class="col-group">
+		<div class="col w-16 last">
+			<div class="box full mb">
+				<div class="tbl-ctrls">
+					<?php if (isset($header['form_url'])): ?>
+						<?=form_open($header['form_url'])?>
+							<fieldset class="tbl-search right">
+								<input placeholder="<?=lang('type_phrase')?>" type="text" value="">
+								<input class="btn submit" type="submit" value="<?=lang('search')?>">
+							</fieldset>
+					<?php endif ?>
+						<h1>
+							<?=$header['title']?>
+							<?php if (isset($header['toolbar_items']))
+							{
+								echo ee()->load->view('_shared/toolbar', $header, TRUE);
+							} ?>
+						</h1>
+					<?php if (isset($header['form_url'])): ?>
+						</form>
+					<?php endif ?>
+				</div>
+			</div>
+		</div>
+	</div>
+<?php endif ?>
+
 <div class="col-group">
 	<?=$left_nav?>
 	<div class="col w-12 last">
