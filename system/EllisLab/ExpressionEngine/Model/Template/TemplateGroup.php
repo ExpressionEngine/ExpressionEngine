ac2f653b8646a1b4aab16635411179f97ab210c2
Seth Croston Barber <seth.barber@ellislab.com>
2015-01-07 16:12:01 -0800

    Adding a boolean setter and getter for the Template Group is_site_default property

--- system/EllisLab/ExpressionEngine/Model/Template/TemplateGroup.php
+++ system/EllisLab/ExpressionEngine/Model/Template/TemplateGroup.php
@@ -63,4 +63,41 @@ class TemplateGroup extends Model {
 	protected $group_order;
 	protected $is_site_default;
 
+	/**
+	 * A setter for the is_site_default property
+	 *
+	 * @param str|bool $new_value Accept TRUE or 'y' for 'yes' or FALSE or 'n'
+	 *   for 'no'
+	 * @throws InvalidArgumentException if the provided argument is not a
+	 *   boolean or is not 'y' or 'n'.
+	 * @return void
+	 */
+	protected function set__is_site_default($new_value)
+	{
+		if ($new_value == TRUE || $new_value == 'y')
+		{
+			$this->is_site_default = 'y';
+		}
+
+		elseif ($new_value == FALSE || $new_value == 'n')
+		{
+			$this->is_site_default = 'n';
+		}
+
+		else
+		{
+			throw new InvalidArgumentException('is_site_default must be TRUE or "y", or FALSE or "n"');
+		}
+	}
+
+	/**
+	 * A getter for the is_site_default property
+	 *
+	 * @return bool TRUE if this is the default; FALSE if not
+	 */
+	protected function get__is_site_default()
+	{
+		return ($this->is_site_default == 'y');
+	}
+
 }
