20b7d0363a54144e0668d733d0651dca18f366c0
Seth Croston Barber <seth.barber@ellislab.com>
2015-01-07 20:29:56 -0800

    Cannot use "new" as a class name, so going with "create" as the URL segment

--- themes/cp_themes/default/views/design/menu.php
+++ themes/cp_themes/default/views/design/menu.php
@@ -1,6 +1,6 @@
 <div class="col w-4">
 	<div class="box sidebar">
-		<h2><?=lang('template_groups')?> <a class="btn action" href="<?=cp_url('design/group/new')?>"><?=lang('new')?></a></h2>
+		<h2><?=lang('template_groups')?> <a class="btn action" href="<?=cp_url('design/group/create')?>"><?=lang('new')?></a></h2>
 		<div class="scroll-wrap">
 			<ul class="folder-list">
 				<?php foreach ($template_groups as $group): ?>
@@ -27,8 +27,8 @@
 				<?php endforeach; ?>
 			</ul>
 		</div>
-		<h2><a href="<?=cp_url('design/snippets')?>"><?=lang('template_partials')?></a> <a class="btn action" href="<?=cp_url('design/snippets/new')?>"><?=lang('new')?></a></h2>
-		<h2><a href="<?=cp_url('design/vars')?>"><?=lang('template_variables')?></a> <a class="btn action" href="<?=cp_url('design/vars/new')?>"><?=lang('new')?></a></h2>
+		<h2><a href="<?=cp_url('design/snippets')?>"><?=lang('template_partials')?></a> <a class="btn action" href="<?=cp_url('design/snippets/create')?>"><?=lang('new')?></a></h2>
+		<h2><a href="<?=cp_url('design/vars')?>"><?=lang('template_variables')?></a> <a class="btn action" href="<?=cp_url('design/vars/create')?>"><?=lang('new')?></a></h2>
 		<h2><a href="<?=cp_url('design/routes')?>"><?=lang('template_routes')?></a></h2>
 	</div>
 </div>
