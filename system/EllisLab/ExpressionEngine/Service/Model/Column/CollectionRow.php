9e34b11bfab7e8a284bef836e119110ac2347fb0
Pascal Kriete <mail@pascal.io>
2015-01-14 16:47:32 -0500

    Attempt at a collection column type. A serialized collection of stuff in a column. Why do we do this to ourselves?!

---
+++ system/EllisLab/ExpressionEngine/Service/Model/Column/CollectionRow.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace EllisLab\ExpressionEngine\Service\Model\Column;
+
+/**
+ * ExpressionEngine - by EllisLab
+ *
+ * @package		ExpressionEngine
+ * @author		EllisLab Dev Team
+ * @copyright	Copyright (c) 2003 - 2014, EllisLab, Inc.
+ * @license		http://ellislab.com/expressionengine/user-guide/license.html
+ * @link		http://ellislab.com
+ * @since		Version 3.0
+ * @filesource
+ */
+
+// ------------------------------------------------------------------------
+
+/**
+ * ExpressionEngine Column Interface
+ *
+ * @package		ExpressionEngine
+ * @subpackage	Model
+ * @category	Service
+ * @author		EllisLab Dev Team
+ * @link		http://ellislab.com
+ */
+abstract class CollectionRow {
+
+	protected $_parent;
+
+	public function setParentColumn($parent)
+	{
+		$this->_parent = $parent;
+	}
+
+	public function delete()
+	{
+		$this->_parent->deleteRow($this);
+	}
+}
