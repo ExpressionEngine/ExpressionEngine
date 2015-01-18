18298201ee21a5c7cc9dcfa1dea0550416949cde
Pascal Kriete <mail@pascal.io>
2015-01-18 11:14:03 -0500

    Comments and type hints for the event emitter

--- system/EllisLab/ExpressionEngine/Library/Event/Emitter.php
+++ system/EllisLab/ExpressionEngine/Library/Event/Emitter.php
@@ -2,14 +2,26 @@
 
 namespace EllisLab\ExpressionEngine\Library\Event;
 
+use Closure;
+
 /**
  * Based loosely on the node.js class of the same name.
  */
 class Emitter {
 
+	/**
+	 * @var bound event listeners
+	 */
 	protected $events = array();
 
-	public function on($event, $listener)
+	/**
+	 * Bind an event listener
+	 *
+	 * @param String $event Event name
+	 * @param Closure $listener The event listener callback
+	 * @return $this
+	 */
+	public function on($event, Closure $listener)
 	{
 		$hash = $this->hash($listener);
 		$this->events[$event][$hash] = $listener;
@@ -17,7 +29,14 @@ class Emitter {
 		return $this;
 	}
 
-	public function once($event, $listener)
+	/**
+	 * Bind an event listener that only fires once
+	 *
+	 * @param String $event Event name
+	 * @param Closure $listener The event listener callback
+	 * @return $this
+	 */
+	public function once($event, Closure $listener)
 	{
 		$self = $this;
 
@@ -28,7 +47,14 @@ class Emitter {
 		});
 	}
 
-	public function off($event, $listener = NULL)
+	/**
+	 * Unbind an event listener or all listeners on a given event
+	 *
+	 * @param String $event Event name
+	 * @param Closure $listener The event listener callback [optional]
+	 * @return $this
+	 */
+	public function off($event, Closure $listener = NULL)
 	{
 		if (isset($listener))
 		{
@@ -43,6 +69,13 @@ class Emitter {
 		return $this;
 	}
 
+	/**
+	 * Emit an event
+	 *
+	 * @param String $event Event name
+	 * @param Any number of additional parameters to pass to the listeners
+	 * @return $this
+	 */
 	public function emit(/* $event, ...$args */)
 	{
 		$args = func_get_args();
@@ -59,7 +92,15 @@ class Emitter {
 		return $this;
 	}
 
-	protected function hash($listener)
+	/**
+	 * Support method to create a listener hash
+	 *
+	 * Down the line we might be able to support all callable's.
+	 *
+	 * @param Closure $listener Listener element
+	 * @return String hash of the listener
+	 */
+	protected function hash(Closure $listener)
 	{
 		return spl_object_hash($listener);
 	}
