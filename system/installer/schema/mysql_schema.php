36285f4c0a38537828a472c3b90966099c7bbfb0
Seth Croston Barber <seth.barber@ellislab.com>
2015-01-21 11:50:55 -0800

    Removing the watermarks table from the schema

--- system/installer/schema/mysql_schema.php
+++ system/installer/schema/mysql_schema.php
@@ -1281,31 +1281,6 @@ class EE_Schema {
 			KEY `upload_location_id` (`upload_location_id`)
 		)";
 
-		$Q[] = "CREATE TABLE `exp_file_watermarks` (
-			`wm_id` int(4) unsigned NOT NULL AUTO_INCREMENT,
-			`wm_name` varchar(80) DEFAULT NULL,
-			`wm_type` varchar(10) DEFAULT 'text',
-			`wm_image_path` varchar(100) DEFAULT NULL,
-			`wm_test_image_path` varchar(100) DEFAULT NULL,
-			`wm_use_font` char(1) DEFAULT 'y',
-			`wm_font` varchar(30) DEFAULT NULL,
-			`wm_font_size` int(3) unsigned DEFAULT NULL,
-			`wm_text` varchar(100) DEFAULT NULL,
-			`wm_vrt_alignment` varchar(10) DEFAULT 'top',
-			`wm_hor_alignment` varchar(10) DEFAULT 'left',
-			`wm_padding` int(3) unsigned DEFAULT NULL,
-			`wm_opacity` int(3) unsigned DEFAULT NULL,
-			`wm_hor_offset` int(4) unsigned DEFAULT NULL,
-			`wm_vrt_offset` int(4) unsigned DEFAULT NULL,
-			`wm_x_transp` int(4) DEFAULT NULL,
-			`wm_y_transp` int(4) DEFAULT NULL,
-			`wm_font_color` varchar(7) DEFAULT NULL,
-			`wm_use_drop_shadow` char(1) DEFAULT 'y',
-			`wm_shadow_distance` int(3) unsigned DEFAULT NULL,
-			`wm_shadow_color` varchar(7) DEFAULT NULL,
-			PRIMARY KEY (`wm_id`)
-		)";
-
 		// Developer log table
 		$Q[] = "CREATE TABLE `exp_developer_log` (
 			`log_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
