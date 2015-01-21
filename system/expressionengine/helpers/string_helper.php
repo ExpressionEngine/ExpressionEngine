45bce6a4268f3d816182cb8e831799a6731abb7c
Pascal Kriete <mail@pascal.io>
2015-01-21 18:59:26 -0500

    Merging extended helpers

--- system/expressionengine/helpers/string_helper.php
+++ system/expressionengine/helpers/string_helper.php
@@ -1,28 +1,27 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
 /**
- * CodeIgniter
+ * ExpressionEngine - by EllisLab
  *
- * An open source application development framework for PHP 5.2.4 or newer
- *
- * @package		CodeIgniter
+ * @package		ExpressionEngine
  * @author		EllisLab Dev Team
- * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc.
- * @license		http://codeigniter.com/user_guide/license.html
- * @link		http://codeigniter.com
- * @since		Version 1.0
+ * @copyright	Copyright (c) 2003 - 2014, EllisLab, Inc.
+ * @license		http://ellislab.com/expressionengine/user-guide/license.html
+ * @link		http://ellislab.com
+ * @since		Version 2.0
  * @filesource
  */
 
 // ------------------------------------------------------------------------
 
 /**
- * CodeIgniter String Helpers
+ * ExpressionEngine String Helper
  *
- * @package		CodeIgniter
+ * @package		ExpressionEngine
  * @subpackage	Helpers
  * @category	Helpers
  * @author		EllisLab Dev Team
- * @link		http://codeigniter.com/user_guide/helpers/string_helper.html
+ * @link		http://ellislab.com
  */
 
 // ------------------------------------------------------------------------
@@ -285,6 +284,82 @@ if ( ! function_exists('repeater'))
 	}
 }
 
+// ------------------------------------------------------------------------
 
-/* End of file string_helper.php */
-/* Location: ./system/helpers/string_helper.php */
+ /**
+ * Unique Marker
+ *
+ * The template library and some of our modules temporarily replace
+ * pieces of code with a random string. These need to be unique per
+ * request to avoid potential security issues.
+ *
+ * @access	public
+ * @param	string	marker identifier
+ * @return	string
+ */
+function unique_marker($ident)
+{
+	static $rand;
+
+	if ( ! $rand)
+	{
+		$rand = random_string('alnum', 32);
+	}
+
+	return $rand.$ident;
+}
+
+// ----------------------------------------------------------------------------
+
+/**
+ * Just like trim, but also removes non-breaking spaces
+ *
+ * @param string $string The string to trim
+ * @return string The trimmed string
+ */
+function trim_nbs($string)
+{
+	return trim($string, " \t\n\r\0\xB\xA0".chr(0xC2).chr(0xA0));
+}
+
+// ----------------------------------------------------------------------------
+
+
+/**
+ * Validates format of submitted license number, for soft validation
+ *
+ * @param string	$license	the string to run the pattern check on
+ * @return bool		TRUE on pattern math, FALSE on failure
+ **/
+function valid_license_pattern($license)
+{
+	if (IS_CORE && $license == 'CORE LICENSE')
+	{
+		return TRUE;
+	}
+
+	if ( ! preg_match('/^[\d]{4}-[\d]{4}-[\d]{4}-[\d]{4}$/', $license))
+	{
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
+// ----------------------------------------------------------------------------
+
+/**
+ * Returns the surrounding character of a string, if it exists
+ *
+ * @param	string	$string		The string to check
+ * @return	mixed	The surrounding character, or FALSE if there isn't one
+ */
+function surrounding_character($string)
+{
+	$first_char = substr($string, 0, 1);
+
+	return ($first_char == substr($string, -1, 1)) ? $first_char : FALSE;
+}
+
+/* End of file EE_string_helper.php */
+/* Location: ./system/expressionengine/helpers/EE_string_helper.php */
--- system/expressionengine/helpers/url_helper.php
+++ system/expressionengine/helpers/url_helper.php
@@ -2,11 +2,11 @@
 /**
  * CodeIgniter
  *
- * An open source application development framework for PHP 5.2.4 or newer
+ * An open source application development framework for PHP 4.3.2 or newer
  *
  * @package		CodeIgniter
  * @author		EllisLab Dev Team
- * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc.
+ * @copyright	Copyright (c) 2006, EllisLab, Inc.
  * @license		http://codeigniter.com/user_guide/license.html
  * @link		http://codeigniter.com
  * @since		Version 1.0
@@ -16,14 +16,144 @@
 // ------------------------------------------------------------------------
 
 /**
- * CodeIgniter URL Helpers
+ * ExpressionEngine URL Helper
  *
- * @package		CodeIgniter
+ * @package		ExpressionEngine
  * @subpackage	Helpers
  * @category	Helpers
  * @author		EllisLab Dev Team
- * @link		http://codeigniter.com/user_guide/helpers/url_helper.html
+ * @link		http://ellislab.com
  */
+
+// ------------------------------------------------------------------------
+
+/**
+ * Create a CP Path
+ *
+ * @param	string	$path				controller/method path
+ * @param	mixed	$qs					query string [array|string]
+ * @param	bool	$force_base_cp_url	whether to force the 'cp_url' base address on CP generated URLs
+ * @return	string
+ */
+function cp_url($path, $qs = '', $force_base_cp_url = FALSE)
+{
+	$path = trim($path, '/');
+	$path = preg_replace('#^cp(/|$)#', '', $path);
+
+	if (is_array($qs))
+	{
+		$qs = http_build_query($qs, AMP);
+	}
+
+	$s = ee()->session->session_id();
+
+	if ($s)
+	{
+		// Remove AMP from the beginning of the query string if it exists
+		$qs = preg_replace('#^'.AMP.'#', '', $qs.AMP.'S='.$s);
+	}
+
+	$path = rtrim('?/cp/'.$path, '/');
+
+	$base = (REQ == 'CP' && ! $force_base_cp_url) ? SELF : ee()->config->item('cp_url');
+
+	return $base.$path.rtrim('&'.$qs, '&');
+}
+
+// ------------------------------------------------------------------------
+
+/**
+ * Create URL Title
+ *
+ * Takes a "title" string as input and creates a
+ * human-friendly URL string with either a dash
+ * or an underscore as the word separator.
+ *
+ * @review maybe roll into CI proper
+ *
+ * @access	public
+ * @param	string	the string
+ * @param	string	the separator: dash, or underscore
+ * @return	string
+ */
+if ( ! function_exists('url_title'))
+{
+	function url_title($str, $separator = 'dash', $lowercase = FALSE)
+	{
+		if (UTF8_ENABLED)
+		{
+			$CI =& get_instance();
+			$CI->load->helper('text');
+
+			$str = utf8_decode($str);
+			$str = preg_replace_callback('/(.)/', 'convert_accented_characters', $str);
+		}
+
+		$separator = ($separator == 'dash') ? '-' : '_';
+
+		$trans = array(
+						'&\#\d+?;'					=> '',
+						'&\S+?;'					=> '',
+						'\s+|/+'					=> $separator,
+						'[^a-z0-9\-\._]'			=> '',
+						$separator.'+'				=> $separator,
+						'^[-_]+|[-_]+$'				=> '',
+						'\.+$'						=> ''
+					  );
+
+		$str = strip_tags($str);
+
+		foreach ($trans as $key => $val)
+		{
+			$str = preg_replace("#".$key."#i", $val, $str);
+		}
+
+		if ($lowercase === TRUE)
+		{
+			$str = strtolower($str);
+		}
+
+		return trim(stripslashes($str));
+	}
+}
+
+// --------------------------------------------------------------------
+
+/**
+ * Anchor Link
+ *
+ * Creates an anchor based on the local URL.
+ *
+ * @access	public
+ * @param	string	the URL
+ * @param	string	the link title
+ * @param	mixed	any attributes
+ * @return	string
+ */
+function anchor($uri = '', $title = '', $attributes = '')
+{
+    $title = (string) $title;
+
+    $site_url = is_array($uri) ? implode('/', $uri) : $uri;
+
+    if (REQ != 'CP' && ! preg_match('!^\w+://! i', $site_url))
+    {
+        $site_url = ee()->functions->fetch_site_index(TRUE).$site_url;
+    }
+
+    if ($title == '')
+    {
+        $title = $site_url;
+    }
+
+    if ($attributes != '')
+    {
+        $attributes = _parse_attributes($attributes);
+    }
+
+    return '<a href="'.$site_url.'"'.$attributes.'>'.$title.'</a>';
+}
+
 
 // ------------------------------------------------------------------------
 
@@ -119,48 +249,6 @@ if ( ! function_exists('index_page'))
 	{
 		$CI =& get_instance();
 		return $CI->config->item('index_page');
-	}
-}
-
-// ------------------------------------------------------------------------
-
-/**
- * Anchor Link
- *
- * Creates an anchor based on the local URL.
- *
- * @access	public
- * @param	string	the URL
- * @param	string	the link title
- * @param	mixed	any attributes
- * @return	string
- */
-if ( ! function_exists('anchor'))
-{
-	function anchor($uri = '', $title = '', $attributes = '')
-	{
-		$title = (string) $title;
-
-		if ( ! is_array($uri))
-		{
-			$site_url = ( ! preg_match('!^\w+://! i', $uri)) ? site_url($uri) : $uri;
-		}
-		else
-		{
-			$site_url = site_url($uri);
-		}
-
-		if ($title == '')
-		{
-			$title = $site_url;
-		}
-
-		if ($attributes != '')
-		{
-			$attributes = _parse_attributes($attributes);
-		}
-
-		return '<a href="'.$site_url.'"'.$attributes.'>'.$title.'</a>';
 	}
 }
 
@@ -460,62 +548,6 @@ if ( ! function_exists('prep_url'))
 // ------------------------------------------------------------------------
 
 /**
- * Create URL Title
- *
- * Takes a "title" string as input and creates a
- * human-friendly URL string with either a dash
- * or an underscore as the word separator.
- *
- * @access	public
- * @param	string	the string
- * @param	string	the separator: dash, or underscore
- * @return	string
- */
-if ( ! function_exists('url_title'))
-{
-	function url_title($str, $separator = 'dash', $lowercase = FALSE)
-	{
-		if ($separator == 'dash')
-		{
-			$search		= '_';
-			$replace	= '-';
-		}
-		else
-		{
-			$search		= '-';
-			$replace	= '_';
-		}
-
-		$trans = array(
-						'&\#\d+?;'				=> '',
-						'&\S+?;'				=> '',
-						'\s+'					=> $replace,
-						'[^a-z0-9\-\._]'		=> '',
-						$replace.'+'			=> $replace,
-						$replace.'$'			=> $replace,
-						'^'.$replace			=> $replace,
-						'\.+$'					=> ''
-					);
-
-		$str = strip_tags($str);
-
-		foreach ($trans as $key => $val)
-		{
-			$str = preg_replace("#".$key."#i", $val, $str);
-		}
-
-		if ($lowercase === TRUE)
-		{
-			$str = strtolower($str);
-		}
-
-		return trim(stripslashes($str));
-	}
-}
-
-// ------------------------------------------------------------------------
-
-/**
  * Header Redirect
  *
  * Header redirect in two flavors
@@ -607,6 +639,7 @@ if ( ! function_exists('_parse_attributes'))
 	}
 }
 
+// --------------------------------------------------------------------
 
-/* End of file url_helper.php */
-/* Location: ./system/helpers/url_helper.php */
+/* End of file EE_url_helper.php */
+/* Location: ./system/expressionengine/helpers/EE_url_helper.php */
