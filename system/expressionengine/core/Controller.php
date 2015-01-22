5fa9854cda9d054ad7d2c301a85862ed9e322b2e
Pascal Kriete <mail@pascal.io>
2015-01-21 19:25:33 -0500

    Moving CI_Controller

--- system/expressionengine/core/Controller.php
+++ system/expressionengine/core/Controller.php
@@ -15,6 +15,52 @@
 // ------------------------------------------------------------------------
 
 /**
+ * CodeIgniter Application Controller Class
+ *
+ * This class object is the super class that every library in
+ * CodeIgniter will be assigned to.
+ *
+ * @package		CodeIgniter
+ * @subpackage	Libraries
+ * @category	Libraries
+ * @author		EllisLab Dev Team
+ * @link		http://codeigniter.com/user_guide/general/controllers.html
+ */
+class CI_Controller {
+
+	private static $instance;
+
+	/**
+	 * Constructor
+	 */
+	public function __construct()
+	{
+		self::$instance =& $this;
+
+		// Assign all the class objects that were instantiated by the
+		// bootstrap file (boot.php) to local class variables
+		// so that CI can run as one big super object.
+		foreach (is_loaded() as $var => $class)
+		{
+			$this->$var =& load_class($class);
+		}
+
+		$this->load =& load_class('Loader', 'core', 'EE_');
+
+		$this->load->set_base_classes()->ci_autoloader();
+
+		log_message('debug', "Controller Class Initialized");
+	}
+
+	public static function &get_instance()
+	{
+		return self::$instance;
+	}
+}
+
+// ------------------------------------------------------------------------
+
+/**
  * ExpressionEngine Controller
  *
  * @package		ExpressionEngine
