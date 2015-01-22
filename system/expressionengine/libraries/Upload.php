f63e80f07635b0a679a1c16ea60d99973dde8fd2
Pascal Kriete <mail@pascal.io>
2015-01-21 19:02:38 -0500

    Merging upload

--- system/expressionengine/libraries/Upload.php
+++ system/expressionengine/libraries/Upload.php
@@ -1,30 +1,28 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 /**
- * CodeIgniter
+ * ExpressionEngine - by EllisLab
  *
- * An open source application development framework for PHP 5.2.4 or newer
- *
- * @package		CodeIgniter
+ * @package		ExpressionEngine
  * @author		EllisLab Dev Team
- * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc.
- * @license		http://codeigniter.com/user_guide/license.html
- * @link		http://codeigniter.com
- * @since		Version 1.0
+ * @copyright	Copyright (c) 2003 - 2014, EllisLab, Inc.
+ * @license		http://ellislab.com/expressionengine/user-guide/license.html
+ * @link		http://ellislab.com
+ * @since		Version 2.0
  * @filesource
  */
 
 // ------------------------------------------------------------------------
 
 /**
- * File Uploading Class
+ * ExpressionEngine Core Upload Class
  *
- * @package		CodeIgniter
- * @subpackage	Libraries
- * @category	Uploads
+ * @package		ExpressionEngine
+ * @subpackage	Core
+ * @category	Core
  * @author		EllisLab Dev Team
- * @link		http://codeigniter.com/user_guide/libraries/file_uploading.html
+ * @link		http://ellislab.com
  */
-class CI_Upload {
+class EE_Upload {
 
 	public $max_size				= 0;
 	public $max_width				= 0;
@@ -52,85 +50,29 @@ class CI_Upload {
 	public $temp_prefix				= "temp_file_";
 	public $client_name				= '';
 
+	protected $use_temp_dir			= FALSE;
 	protected $_file_name_override	= '';
 
 	/**
 	 * Constructor
-	 *
-	 * @access	public
 	 */
-	public function __construct($props = array())
+	function __construct($props = array())
 	{
 		if (count($props) > 0)
 		{
 			$this->initialize($props);
 		}
 
+		// Make a local reference to the ExpressionEngine super object
+		$this->EE =& get_instance();
+
+		ee()->load->helper('xss');
+
+		$props['xss_clean'] = xss_check();
+
+		$this->initialize($props);
+
 		log_message('debug', "Upload Class Initialized");
-	}
-
-	// --------------------------------------------------------------------
-
-	/**
-	 * Initialize preferences
-	 *
-	 * @param	array
-	 * @return	void
-	 */
-	public function initialize($config = array())
-	{
-		$defaults = array(
-							'max_size'			=> 0,
-							'max_width'			=> 0,
-							'max_height'		=> 0,
-							'max_filename'		=> 0,
-							'allowed_types'		=> "",
-							'file_temp'			=> "",
-							'file_name'			=> "",
-							'orig_name'			=> "",
-							'file_type'			=> "",
-							'file_size'			=> "",
-							'file_ext'			=> "",
-							'upload_path'		=> "",
-							'overwrite'			=> FALSE,
-							'encrypt_name'		=> FALSE,
-							'is_image'			=> FALSE,
-							'image_width'		=> '',
-							'image_height'		=> '',
-							'image_type'		=> '',
-							'image_size_str'	=> '',
-							'error_msg'			=> array(),
-							'mimes'				=> array(),
-							'remove_spaces'		=> TRUE,
-							'xss_clean'			=> FALSE,
-							'temp_prefix'		=> "temp_file_",
-							'client_name'		=> ''
-						);
-
-
-		foreach ($defaults as $key => $val)
-		{
-			if (isset($config[$key]))
-			{
-				$method = 'set_'.$key;
-				if (method_exists($this, $method))
-				{
-					$this->$method($config[$key]);
-				}
-				else
-				{
-					$this->$key = $config[$key];
-				}
-			}
-			else
-			{
-				$this->$key = $val;
-			}
-		}
-
-		// if a file_name was provided in the config, use it instead of the user input
-		// supplied file name for all uploads until initialized again
-		$this->_file_name_override = $this->file_name;
 	}
 
 	// --------------------------------------------------------------------
@@ -678,45 +620,6 @@ class CI_Upload {
 	// --------------------------------------------------------------------
 
 	/**
-	 * Validate Upload Path
-	 *
-	 * Verifies that it is a valid upload path with proper permissions.
-	 *
-	 *
-	 * @return	bool
-	 */
-	public function validate_upload_path()
-	{
-		if ($this->upload_path == '')
-		{
-			$this->set_error('upload_no_filepath');
-			return FALSE;
-		}
-
-		if (function_exists('realpath') AND @realpath($this->upload_path) !== FALSE)
-		{
-			$this->upload_path = str_replace("\\", "/", realpath($this->upload_path));
-		}
-
-		if ( ! @is_dir($this->upload_path))
-		{
-			$this->set_error('upload_no_filepath');
-			return FALSE;
-		}
-
-		if ( ! is_really_writable($this->upload_path))
-		{
-			$this->set_error('upload_not_writable');
-			return FALSE;
-		}
-
-		$this->upload_path = preg_replace("/(.+?)\/*$/", "\\1/",  $this->upload_path);
-		return TRUE;
-	}
-
-	// --------------------------------------------------------------------
-
-	/**
 	 * Extract the file extension
 	 *
 	 * @param	string
@@ -1008,8 +911,214 @@ class CI_Upload {
 
 	// --------------------------------------------------------------------
 
-}
-// END Upload Class
+	/**
+	 * Overwrite OR Rename Files Manually
+	 *
+	 * @access	public
+	 * @param string $original_files Path to the original file
+	 * @param string $new The new file name
+	 * @param boolean $type_match Should we make sure the extensions match?
+	 * @return boolean TRUE if it was renamed properly, FALSE otherwise
+	 */
+   function file_overwrite($original_file = '', $new = '', $type_match = TRUE)
+	{
+		$this->file_name = $new;
 
-/* End of file Upload.php */
-/* Location: ./system/libraries/Upload.php */
+		// If renaming a file, it should have same file type suffix as the original
+		if ($type_match === TRUE)
+		{
+			if (sizeof(explode('.', $this->file_name)) == 1 OR (array_pop(explode('.', $this->file_name)) != array_pop(explode('.', $original_file))))
+			{
+				$this->set_error('invalid_filetype');
+				return FALSE;
+			}
+		}
+
+		if ($this->remove_spaces == 1)
+		{
+			$this->file_name = preg_replace("/\s+/", "_", $this->file_name);
+			$original_file = preg_replace("/\s+/", "_", $original_file);
+		}
+
+		// Check to make sure the file doesn't already exist
+		if (file_exists($this->upload_path . $this->file_name))
+		{
+			$this->set_error('file_exists');
+			return FALSE;
+		}
+
+		if ( ! @copy($this->upload_path.$original_file, $this->upload_path.$this->file_name))
+		{
+			$this->set_error('copy_error');
+			return FALSE;
+		}
+
+		unlink ($this->upload_path.$original_file);
+
+		return TRUE;
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * Validate Upload Path
+	 *
+	 * Verifies that it is a valid upload path with proper permissions.
+	 *
+	 * @access	public
+	 */
+	public function validate_upload_path()
+	{
+		if ($this->use_temp_dir)
+		{
+			$path = $this->_discover_temp_path();
+
+			if ($path)
+			{
+				$this->upload_path = $path;
+			}
+			else
+			{
+				$this->set_error('No usable temp directory found.');
+				return FALSE;
+			}
+		}
+
+		if ($this->upload_path == '')
+		{
+			$this->set_error('upload_no_filepath');
+			return FALSE;
+		}
+
+		if (function_exists('realpath') AND @realpath($this->upload_path) !== FALSE)
+		{
+			$this->upload_path = str_replace("\\", "/", realpath($this->upload_path));
+		}
+
+		if ( ! @is_dir($this->upload_path))
+		{
+			$this->set_error('upload_no_filepath');
+			return FALSE;
+		}
+
+		if ( ! is_really_writable($this->upload_path))
+		{
+			$this->set_error('upload_not_writable');
+			return FALSE;
+		}
+
+		$this->upload_path = preg_replace("/(.+?)\/*$/", "\\1/",  $this->upload_path);
+		return TRUE;
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * Keep the file in the temp directory?
+	 *
+	 * @access	public
+	 */
+	public function initialize($config = array())
+	{
+		if (isset($config['use_temp_dir']) && $config['use_temp_dir'] === TRUE)
+		{
+			$this->use_temp_dir = TRUE;
+		}
+		else
+		{
+			$this->use_temp_dir = FALSE;
+		}
+
+		$defaults = array(
+							'max_size'			=> 0,
+							'max_width'			=> 0,
+							'max_height'		=> 0,
+							'max_filename'		=> 0,
+							'allowed_types'		=> "",
+							'file_temp'			=> "",
+							'file_name'			=> "",
+							'orig_name'			=> "",
+							'file_type'			=> "",
+							'file_size'			=> "",
+							'file_ext'			=> "",
+							'upload_path'		=> "",
+							'overwrite'			=> FALSE,
+							'encrypt_name'		=> FALSE,
+							'is_image'			=> FALSE,
+							'image_width'		=> '',
+							'image_height'		=> '',
+							'image_type'		=> '',
+							'image_size_str'	=> '',
+							'error_msg'			=> array(),
+							'mimes'				=> array(),
+							'remove_spaces'		=> TRUE,
+							'xss_clean'			=> FALSE,
+							'temp_prefix'		=> "temp_file_",
+							'client_name'		=> ''
+						);
+
+
+		foreach ($defaults as $key => $val)
+		{
+			if (isset($config[$key]))
+			{
+				$method = 'set_'.$key;
+				if (method_exists($this, $method))
+				{
+					$this->$method($config[$key]);
+				}
+				else
+				{
+					$this->$key = $config[$key];
+				}
+			}
+			else
+			{
+				$this->$key = $val;
+			}
+		}
+
+		// if a file_name was provided in the config, use it instead of the user input
+		// supplied file name for all uploads until initialized again
+		$this->_file_name_override = $this->file_name;
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * Find a valid temp directory?
+	 *
+	 * @access	public
+	 */
+	public function _discover_temp_path()
+	{
+		$attempt = array();
+        $ini_path = ini_get('upload_tmp_dir');
+
+        if ($ini_path)
+        {
+            $attempt[] = realpath($ini_path);
+        }
+
+		$attempt[] = sys_get_temp_dir();
+		$attempt[] = @getenv('TMP');
+		$attempt[] = @getenv('TMPDIR');
+		$attempt[] = @getenv('TEMP');
+
+		$valid_temps = array_filter($attempt);	// remove false's
+
+		foreach ($valid_temps as $dir)
+		{
+			if (is_readable($dir) && is_writable($dir))
+			{
+				return $dir;
+			}
+		}
+
+		return FALSE;
+	}
+}
+// END CLASS
+
+/* End of file EE_Upload.php */
+/* Location: ./system/expressionengine/libraries/EE_Upload.php */
