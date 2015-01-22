7fd0ececd9bff4ad5e9f12e9ed3dc13336183956
Pascal Kriete <mail@pascal.io>
2015-01-22 12:41:49 -0500

    Using filesystem for is_really_writable

--- system/EllisLab/ExpressionEngine/Boot/boot.common.php
+++ system/EllisLab/ExpressionEngine/Boot/boot.common.php
@@ -1,4 +1,7 @@
 <?php  if ( ! defined('SYSPATH')) exit('No direct script access allowed');
+
+use EllisLab\ExpressionEngine\Service\Filesystem\Filesystem;
+
 /**
  * CodeIgniter
  *
@@ -53,40 +56,15 @@
  * the file, based on the read-only attribute.  is_writable() is also unreliable
  * on Unix servers if safe_mode is on.
  *
+ * @deprecated 3.0.0
  * @access	private
  * @return	void
  */
 	function is_really_writable($file)
 	{
-		// If we're on a Unix server with safe_mode off we call is_writable
-		if (DIRECTORY_SEPARATOR == '/' AND @ini_get("safe_mode") == FALSE)
-		{
-			return is_writable($file);
-		}
+		$fs = new Filesystem();
 
-		// For windows servers and safe_mode "on" installations we'll actually
-		// write a file then read it.  Bah...
-		if (is_dir($file))
-		{
-			$file = rtrim($file, '/').'/'.md5(mt_rand(1,100).mt_rand(1,100));
-
-			if (($fp = @fopen($file, FOPEN_WRITE_CREATE)) === FALSE)
-			{
-				return FALSE;
-			}
-
-			fclose($fp);
-			@chmod($file, DIR_WRITE_MODE);
-			@unlink($file);
-			return TRUE;
-		}
-		elseif (($fp = @fopen($file, FOPEN_WRITE_CREATE)) === FALSE)
-		{
-			return FALSE;
-		}
-
-		fclose($fp);
-		return TRUE;
+		return $fs->isWritable($file);
 	}
 
 // ------------------------------------------------------------------------
