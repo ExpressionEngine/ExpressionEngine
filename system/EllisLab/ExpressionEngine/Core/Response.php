130859a4f14195cf7374b10b7ccb22d25db0de78
Pascal Kriete <mail@pascal.io>
2015-01-25 14:42:48 -0500

    Request and response object. It's the future in here.

---
+++ system/EllisLab/ExpressionEngine/Core/Response.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace EllisLab\ExpressionEngine\Core;
+
+class Response {
+
+	protected $body = '';
+
+	protected $status = 200;
+
+	protected $headers = array();
+
+	protected $compress = FALSE;
+
+	/**
+	 *
+	 */
+	public function setBody($str)
+	{
+		$this->body = $str;
+	}
+
+	/**
+	 *
+	 */
+	public function appendBody($str)
+	{
+		$this->body .= $str;
+	}
+
+	/**
+	 *
+	 */
+	public function setHeader($header, $value = NULL)
+	{
+		if ( ! isset($value))
+		{
+			list($header, $value) = explode(':', $header, 2);
+		}
+
+		$this->headers[$header] = $value;
+	}
+
+	/**
+	 *
+	 */
+	public function send()
+	{
+		// smoke and mirrors
+		return $GLOBALS['OUT']->_display();
+
+		$this->sendHeaders();
+		$this->sendBody();
+	}
+
+	/**
+	 *
+	 */
+	public function enableCompression()
+	{
+		if ($this->supportCompression())
+		{
+			$this->compress = TRUE;
+		}
+	}
+
+	/**
+	 *
+	 */
+	public function disableCompression()
+	{
+		$this->compress = FALSE;
+	}
+
+	/**
+	 *
+	 */
+	public function supportsCompression()
+	{
+		return (
+			$this->clientSupportsCompression() &&
+			$this->serverSupportsCompression()
+		);
+	}
+
+	/**
+	 *
+	 */
+	public function compressionEnabled()
+	{
+		return $this->compress == TRUE && $this->status != 304;
+	}
+
+	/**
+	 *
+	 */
+	protected function sendHeaders()
+	{
+		foreach ($this->headers as $name => $value)
+		{
+			@header($name, $value);
+		}
+	}
+
+	/**
+	 *
+	 */
+	protected function sendBody()
+	{
+		if ($this->compressionEnabled())
+		{
+			ob_start('ob_gzhandler');
+		}
+
+		echo $this->body;
+	}
+
+	/**
+	 *
+	 */
+	protected function clientSupportsCompression()
+	{
+		$header = 'HTTP_ACCEPT_ENCODING';
+
+		return (
+			isset($_SERVER[$header]) &&
+			strpos($_SERVER[$header], 'gzip') !== FALSE
+		);
+
+	}
+
+	/**
+	 *
+	 */
+	protected function serverSupportsCompression()
+	{
+		$zlib_enabled = (bool) @ini_get('zlib.output_compression');
+
+		return $zlib_enabled == FALSE && extension_loaded('zlib');
+	}
+}
