1d1632ef4acac987e1ca2cd161cefbd82d0ca932
Pascal Kriete <mail@pascal.io>
2015-01-25 14:49:47 -0500

    Rejigging the bootstrap file to boot the new app, which hides away the legacy code.

--- system/EllisLab/ExpressionEngine/Boot/boot.php
+++ system/EllisLab/ExpressionEngine/Boot/boot.php
@@ -1,4 +1,5 @@
 <?php  if ( ! defined('SYSPATH')) exit('No direct script access allowed');
+
 /**
  * ExpressionEngine - by EllisLab
  *
@@ -14,50 +15,30 @@
 /*
  * ------------------------------------------------------
  *  Set and load the framework constants
+ *
+ *  BASEPATH - path to the legacy app folder. Most legacy
+ *             files check for this (`if ! defined ...`)
  * ------------------------------------------------------
  */
-	// Path to the legacy app folder. Most legacy files
-	// check for this at the top, so it's hard to remove
+	define('BASEPATH', SYSPATH.'expressionengine/');
 
-	define('BASEPATH', SYSPATH.'/expressionengine/');
-
-	require(BASEPATH.'config/constants.php');
+	require BASEPATH.'config/constants.php';
 
 /*
  * ------------------------------------------------------
  *  Load the autoloader and register it
  * ------------------------------------------------------
  */
-	require(SYSPATH.'EllisLab/ExpressionEngine/Service/Autoloader.php');
+	require SYSPATH.'EllisLab/ExpressionEngine/Core/Autoloader.php';
 
-	EllisLab\ExpressionEngine\Service\Autoloader::getInstance()->register();
-
+	EllisLab\ExpressionEngine\Core\Autoloader::getInstance()->register();
 
 /*
  * ------------------------------------------------------
  *  Load the global functions
  * ------------------------------------------------------
  */
-	require(__DIR__.'/boot.common.php');
-
-/*
- * ------------------------------------------------------
- *  Check for the installer if we're booting the CP
- * ------------------------------------------------------
- */
-	if (FALSE && is_dir($system_path.'installer/'))
-	{
-		define('APPPATH', SYSPATH.'installer/');
-		define('EE_APPPATH', BASEPATH);
-
-		get_config(array('subclass_prefix' => 'Installer_'));
-	}
-	else
-	{
-		define('APPPATH', BASEPATH);
-
-		get_config(array('subclass_prefix' => 'EE_'));
-	}
+	require __DIR__.'/boot.common.php';
 
 /*
  * ------------------------------------------------------
@@ -68,275 +49,98 @@
 
 /*
  * ------------------------------------------------------
- *  Set mandatory config overrides
+ *  Check for the installer if we're booting the CP
  * ------------------------------------------------------
  */
+	use EllisLab\ExpressionEngine\Core;
 
-	get_config(array(
-		'directory_trigger'  => 'D',
-		'controller_trigger' => 'C',
-		'function_trigger'   => 'M'
-	));
-
-/*
- * ------------------------------------------------------
- *  Set a liberal script execution time limit
- * ------------------------------------------------------
- */
-	if (function_exists("set_time_limit") == TRUE AND @ini_get("safe_mode") == 0)
+	if (FALSE && defined('REQ') && REQ == 'CP' && is_dir(SYSPATH.'installer/'))
 	{
-		@set_time_limit(300);
-	}
-
-/*
- * ------------------------------------------------------
- *  Start the timer... tick tock tick tock...
- * ------------------------------------------------------
- */
-	$BM =& load_class('Benchmark', 'core');
-	$BM->mark('total_execution_time_start');
-	$BM->mark('loading_time:_base_classes_start');
-
-
-/*
- * ------------------------------------------------------
- *  Instantiate the config class
- * ------------------------------------------------------
- */
-	$CFG =& load_class('Config', 'core');
-
-	// Do we have any manually set config items in the index.php file?
-	if (isset($assign_to_config))
-	{
-		$CFG->_assign_to_config($assign_to_config);
-	}
-
-/*
- * ------------------------------------------------------
- *  Instantiate the UTF-8 class
- * ------------------------------------------------------
- *
- * Note: Order here is rather important as the UTF-8
- * class needs to be used very early on, but it cannot
- * properly determine if UTf-8 can be supported until
- * after the Config class is instantiated.
- *
- */
-
-	$UNI =& load_class('Utf8', 'core');
-
-/*
- * ------------------------------------------------------
- *  Instantiate the URI class
- * ------------------------------------------------------
- */
-	$URI =& load_class('URI', 'core');
-
-/*
- * ------------------------------------------------------
- *  Instantiate the routing class and set the routing
- * ------------------------------------------------------
- */
-	$RTR =& load_class('Router', 'core');
-	$RTR->_set_routing();
-
-	// Set any routing overrides that may exist in the main index file
-	if (isset($routing))
-	{
-		$RTR->_set_overrides($routing);
-	}
-
-/*
- * ------------------------------------------------------
- *  Instantiate the output class
- * ------------------------------------------------------
- */
-	$OUT =& load_class('Output', 'core');
-
-/*
- * -----------------------------------------------------
- * Load the security class for xss and csrf support
- * -----------------------------------------------------
- */
-	$SEC =& load_class('Security', 'core');
-
-/*
- * ------------------------------------------------------
- *  Load the Input class and sanitize globals
- * ------------------------------------------------------
- */
-	$IN	=& load_class('Input', 'core');
-
-/*
- * ------------------------------------------------------
- *  Load the Language class
- * ------------------------------------------------------
- */
-	$LANG =& load_class('Lang', 'core');
-
-
-/*
- * ------------------------------------------------------
- *  Alias CI_ prefixed stuff we used to have in case
- *  someone type hints. Definitely don't rely on this
- *  staying here for terribly long.
- *
- *  These were consolidated with the old EE libraries to
- *  reduce dead weight.
- *  Done again in the loader, but we do it here for the
- *  above core classes.
- * ------------------------------------------------------
- */
-	class_alias('EE_Benchmark', 'CI_Benchmark');
-	class_alias('EE_Config', 'CI_Config');
-	class_alias('EE_Input', 'CI_Input');
-	class_alias('EE_Lang', 'CI_Lang');
-	class_alias('EE_Output', 'CI_Output');
-	class_alias('EE_URI', 'CI_URI');
-	class_alias('EE_Utf8', 'CI_Utf8');
-	class_alias('EE_Router', 'CI_Router');
-	class_alias('EE_Security', 'CI_Security');
-	// do these at the bottom of their respective files?
-	//class_alias('EE_Exceptions', 'CI_Exceptions');
-
-
-/*
- * ------------------------------------------------------
- *  Load the app controller and local controller
- * ------------------------------------------------------
- *
- */
-	// Load the base controller class
-	require SYSPATH.'expressionengine/core/Controller.php';
-
-	function &get_instance()
-	{
-		return CI_Controller::get_instance();
-	}
-
-
-	function ee($dep = NULL)
-	{
-		static $EE;
-		if ( ! $EE) $EE = get_instance();
-
-		if (isset($dep) && isset($EE->di))
-		{
-			return $EE->di->make($dep);
-		}
-
-		return $EE;
-	}
-
-
-	if (file_exists(APPPATH.'core/'.$CFG->config['subclass_prefix'].'Controller.php'))
-	{
-		require APPPATH.'core/'.$CFG->config['subclass_prefix'].'Controller.php';
-	}
-
-	// Load the local application controller
-	// Note: The Router class automatically validates the controller path using the router->_validate_request().
-	// If this include fails it means that the default controller in the Routes.php file is not resolving to something valid.
-	if ( ! file_exists(APPPATH.'controllers/'.$RTR->fetch_directory().$RTR->fetch_class().'.php'))
-	{
-		show_error('Unable to load your default controller. Please make sure the controller specified in your Routes.php file is valid.');
-	}
-
-	include(APPPATH.'controllers/'.$RTR->fetch_directory().$RTR->fetch_class().'.php');
-
-	// Set a mark point for benchmarking
-	$BM->mark('loading_time:_base_classes_end');
-
-/*
- * ------------------------------------------------------
- *  Security check
- * ------------------------------------------------------
- *
- *  None of the functions in the app controller or the
- *  loader class can be called via the URI, nor can
- *  controller functions that begin with an underscore
- */
-	$class  = $RTR->fetch_class();
-	$method = $RTR->fetch_method();
-
-	if ( ! class_exists($class)
-		OR strncmp($method, '_', 1) == 0
-		OR in_array(strtolower($method), array_map('strtolower', get_class_methods('CI_Controller')))
-		)
-	{
-		show_404("{$class}/{$method}");
-	}
-
-/*
- * ------------------------------------------------------
- *  Instantiate the requested controller
- * ------------------------------------------------------
- */
-	// Mark a start point so we can benchmark the controller
-	$BM->mark('controller_execution_time_( '.$class.' / '.$method.' )_start');
-
-	$CI = new $class();
-
-/*
- * ------------------------------------------------------
- *  Call the requested method
- * ------------------------------------------------------
- */
-	// Is there a "remap" function? If so, we call it instead
-	if (method_exists($CI, '_remap'))
-	{
-		$CI->_remap($method, array_slice($URI->rsegments, 2));
+		$core = new Core\Installer();
 	}
 	else
 	{
-		// is_callable() returns TRUE on some versions of PHP 5 for private and protected
-		// methods, so we'll use this workaround for consistent behavior
-		if ( ! in_array(strtolower($method), array_map('strtolower', get_class_methods($CI))))
-		{
-			show_404("{$class}/{$method}");
-		}
-		try {
-			// Call the requested method.
-			// Any URI segments present (besides the class/function) will be passed to the method for convenience
-			call_user_func_array(array(&$CI, $method), array_slice($URI->rsegments, 2));
-		}
-		catch(Exception $ex)
-		{
-			echo '<div>
-					<h1>Exception Caught</h1>
-					<p><strong>' . $ex->getMessage() . '</strong></p>
-					<p><em>'  . $ex->getFile() . ':' . $ex->getLine() . '<em></p>
-					<p>Stack Trace:
-						<pre>' . str_replace('#', "\n#", str_replace(':', ":\n\t\t", $ex->getTraceAsString())) . '</pre>
-					</p>
-				</div>';
-			die('Fatal Error.');
-		}
-
+		$core = new Core\ExpressionEngine();
 	}
 
-
-	// Mark a benchmark end point
-	$BM->mark('controller_execution_time_( '.$class.' / '.$method.' )_end');
-
+/*
+ * ------------------------------------------------------
+ *  Boot the core
+ * ------------------------------------------------------
+ */
+	$core->boot();
 
 /*
  * ------------------------------------------------------
- *  Send the final rendered output to the browser
+ *  Set config items from the index.php file
  * ------------------------------------------------------
  */
-	$OUT->_display();
-
-/*
- * ------------------------------------------------------
- *  Close the DB connection if one exists
- * ------------------------------------------------------
- */
-	if (class_exists('CI_DB') AND isset($CI->db))
+	if (isset($assign_to_config))
 	{
-		$CI->db->close();
+		$core->overrideConfig($assign_to_config);
 	}
 
+/*
+ * ------------------------------------------------------
+ *  Set routing overrides from the index.php file
+ * ------------------------------------------------------
+ */
+	if (isset($routing))
+	{
+		$core->overrideRouting($routing);
+	}
 
-/* End of file CodeIgniter.php */
-/* Location: ./system/core/CodeIgniter.php */
+/*
+ * ------------------------------------------------------
+ *  Create global helper functions
+ *
+ *  Using `CI` for the global name, just in case someone
+ *  is relying on that instead of get_instance()
+ * ------------------------------------------------------
+ */
+	$CI = $core->getLegacyApp()->getFacade();
+
+	function get_instance()
+	{
+		global $CI;
+		return $CI;
+	}
+
+	function ee($dep = NULL)
+	{
+		$facade = get_instance();
+
+		if (isset($dep) && isset($facade->di))
+		{
+			$args = func_get_args();
+			return call_user_func_array(array($facade->di, 'make'), $args);
+			return $facade->di->make($dep);
+		}
+
+		return $facade;
+	}
+
+/*
+ * ------------------------------------------------------
+ *  Parse the request
+ * ------------------------------------------------------
+ */
+	$request = Core\Request::fromGlobals();
+
+/*
+ * ------------------------------------------------------
+ *  Run the request and get a response
+ * ------------------------------------------------------
+ */
+	$response = $core->run($request);
+
+/*
+ * ------------------------------------------------------
+ *  Send the response
+ * ------------------------------------------------------
+ */
+	$response->send();
+
+
+/* End of file boot.php */
+/* Location: ./system/EllisLab/ExpressionEngine/Boot/boot.php */
