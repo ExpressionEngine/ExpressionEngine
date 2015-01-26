76bf16047cf2d8495b62fc90ba86b94a53a3c639
Pascal Kriete <mail@pascal.io>
2015-01-26 10:37:43 -0500

    No leading backslashes on "use"

--- system/expressionengine/libraries/Typography.php
+++ system/expressionengine/libraries/Typography.php
@@ -1,7 +1,7 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-use \Michelf\MarkdownExtra;
-use \EllisLab\ExpressionEngine\Core\Autoloader;
+use Michelf\MarkdownExtra;
+use EllisLab\ExpressionEngine\Core\Autoloader;
 
 /**
  * ExpressionEngine - by EllisLab
