b932055123b7e565671b238747900b640e1de99a
Seth Croston Barber <seth.barber@ellislab.com>
2015-01-28 14:45:55 -0800

    Rendering the form based on the layout

--- themes/ee/cp_themes/default/views/publish/edit/entry.php
+++ themes/ee/cp_themes/default/views/publish/edit/entry.php
@@ -6,20 +6,19 @@
 	</h1>
 	<div class="tab-bar">
 		<ul>
-			<li><a class="act" href="" rel="t-0"><?=lang('publish')?></a></li>
-			<li><a href="" rel="t-1"><?=lang('date')?></a></li>
-			<li><a href="" rel="t-2"><?=lang('categories')?></a></li>
-			<li><a href="" rel="t-3"><?=lang('options')?></a></li>
-			<li><a href="" rel="t-4"><?=lang('pages')?></a></li>
+			<?php foreach ($layout as $index => $tab): ?>
+			<li><a<?php if ($index == 0): ?> class="act"<?php endif; ?> href="" rel="t-<?=$index?>"><?=lang($tab['name'])?></a></li>
+			<?php endforeach; ?>
 		</ul>
 	</div>
 	<?=form_open($form_url, $form_attributes, (isset($form_hidden)) ? $form_hidden : array())?>
-		<div class="tab t-0 tab-open">
-			<?php foreach ($entry->getCustomFields() as $field): ?>
+		<?php foreach ($layout as $index => $tab): ?>
+		<div class="tab t-<?=$index?><?php if ($index == 0): ?> tab-open<?php endif; ?>">
+			<?php foreach ($tab['fields'] as $field): ?>
 			<fieldset class="col-group">
 				<div class="setting-txt col w-16">
-					<h3><span class="ico sub-arrow"></span><?=$field->type_info->field_label?><?php if ($field->type_info->field_required == 'y'): ?> <span class="required" title="required field">&#10033;</span><?php endif; ?></h3>
-					<em><?=$field->type_info->field_instructions?></em>
+					<h3><span class="ico sub-arrow"></span><?=$field->getLabel()?><?php if ($field->isRequired()): ?> <span class="required" title="required field">&#10033;</span><?php endif; ?></h3>
+					<em><?=$field->getInstructions()?></em>
 				</div>
 				<div class="setting-field col w-16 last">
 					<?=$field->getForm()?>
@@ -27,14 +26,7 @@
 			</fieldset>
 			<?php endforeach; ?>
 		</div>
-		<div class="tab t-1">
-		</div>
-		<div class="tab t-2">
-		</div>
-		<div class="tab t-3">
-		</div>
-		<div class="tab t-4">
-		</div>
+		<?php endforeach; ?>
 		<fieldset class="form-ctrls">
 			<?=cp_form_submit(lang('btn_edit_entry'), lang('btn_edit_entry_working'))?>
 		</fieldset>
