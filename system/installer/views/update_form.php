8f447a7fb5a210ecfa43babf2f9965f7f541e74b
Wes Baker <wes@wesbaker.com>
2015-01-28 14:41:56 -0500

    Using the lang keys in the update form

--- system/installer/views/update_form.php
+++ system/installer/views/update_form.php
@@ -1,7 +1,7 @@
 <div class="alert inline warn">
-	<p><b>Please</b> read <a href="">Updating ExpressionEngine</a> <strong>before</strong> starting.</p>
-	<p>Please <b>back up</b> your database before updating ExpressionEngine</p>
+	<p><?=lang('update_note')?></p>
+	<p><?=lang('update_backup')?></p>
 </div>
 <fieldset class="install-btn">
-	<input class="btn" type="submit" value="Start Update">
+	<input class="btn" type="submit" value="<?=lang('start_update')?>">
 </fieldset>
