996460c29333c1e1cd27d3647b2bfd9f0442cadd
Seth Croston Barber <seth.barber@ellislab.com>
2015-02-05 11:32:21 -0800

    Design controllers routable methods tests

---
+++ system/EllisLab/Tests/ExpressionEngine/Controllers/Design/GroupTest.php
@@ -0,0 +1,29 @@
+<?php
+namespace EllisLab\Tests\ExpressionEngine\Controllers\Design;
+
+class GroupTest extends \PHPUnit_Framework_TestCase {
+
+	public static function setUpBeforeClass()
+	{
+		require_once(APPPATH.'core/Controller.php');
+	}
+
+	public function testRoutableMethods()
+	{
+		$controller_methods = array();
+
+		foreach (get_class_methods('EllisLab\ExpressionEngine\Controllers\Design\Group') as $method)
+		{
+			$method = strtolower($method);
+			if (strncmp($method, '_', 1) != 0)
+			{
+				$controller_methods[] = $method;
+			}
+		}
+
+		sort($controller_methods);
+
+		$this->assertEquals(array('create', 'edit', 'remove'), $controller_methods);
+	}
+
+}
