8e0944655af1a5849faa4912215cfa4aee70daf9
Seth Croston Barber <seth.barber@ellislab.com>
2015-02-05 11:56:59 -0800

    Utilities controllers routable methods tests

---
+++ system/EllisLab/Tests/ExpressionEngine/Controllers/Utilities/ImportConverterTest.php
@@ -0,0 +1,29 @@
+<?php
+namespace EllisLab\Tests\ExpressionEngine\Controllers\Utilities;
+
+class ImportConverterTest extends \PHPUnit_Framework_TestCase {
+
+	public static function setUpBeforeClass()
+	{
+		require_once(APPPATH.'core/Controller.php');
+	}
+
+	public function testRoutableMethods()
+	{
+		$controller_methods = array();
+
+		foreach (get_class_methods('EllisLab\ExpressionEngine\Controllers\Utilities\ImportConverter') as $method)
+		{
+			$method = strtolower($method);
+			if (strncmp($method, '_', 1) != 0)
+			{
+				$controller_methods[] = $method;
+			}
+		}
+
+		sort($controller_methods);
+
+		$this->assertEquals(array('downloadxml', 'import_fieldmap', 'importcodeoutput', 'importfieldmapconfirm', 'index'), $controller_methods);
+	}
+
+}
---
+++ system/EllisLab/Tests/ExpressionEngine/Controllers/Utilities/TranslateTest.php
@@ -0,0 +1,31 @@
+<?php
+namespace EllisLab\Tests\ExpressionEngine\Controllers\Utilities;
+
+class TranslateTest extends \PHPUnit_Framework_TestCase {
+
+	public static function setUpBeforeClass()
+	{
+		require_once(APPPATH.'core/Controller.php');
+	}
+
+	public function testRoutableMethods()
+	{
+		$controller_methods = array();
+
+		foreach (get_class_methods('EllisLab\ExpressionEngine\Controllers\Utilities\Translate') as $method)
+		{
+			$method = strtolower($method);
+			if (strncmp($method, '_', 1) != 0)
+			{
+				$controller_methods[] = $method;
+			}
+		}
+
+		sort($controller_methods);
+
+		// This one has more routable functions due to __call(), we need to
+		// test those as well @TODO
+		$this->assertEquals(array('index'), $controller_methods);
+	}
+
+}
