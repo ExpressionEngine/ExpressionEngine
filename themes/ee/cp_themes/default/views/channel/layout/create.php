51ba99035e8a39cd7d5c41d6b178014665cfe540
Seth Croston Barber <seth.barber@ellislab.com>
2015-02-05 14:49:55 -0800

    Added the alerts

--- themes/ee/cp_themes/default/views/channel/layout/create.php
+++ themes/ee/cp_themes/default/views/channel/layout/create.php
@@ -13,6 +13,7 @@
 		<a class="btn action add-tab" href="#"><?=lang('add_tab')?></a>
 	</div>
 	<?=form_open($form_url, 'class="settings ajax-validate"')?>
+		<?=ee('Alert')->get('layout-form')?>
 		<?php foreach ($layout->getTabs() as $index => $tab): ?>
 		<div class="tab t-<?=$index?><?php if ($index == 0): ?> tab-open<?php endif; ?>">
 		<?php foreach ($tab->getFields() as $field): ?>
