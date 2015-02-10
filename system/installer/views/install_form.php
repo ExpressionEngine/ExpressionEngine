52997becce457a1e45511f5066a967f0f206399c
Wes Baker <wes@wesbaker.com>
2015-02-10 17:17:02 -0500

    Install form now shows form errors below the note, input errors are inline, and data persists

--- system/installer/views/install_form.php
+++ system/installer/views/install_form.php
@@ -1,3 +1,6 @@
+<div class="alert inline warn">
+	<p><b>Please</b> read <a href="https://ellislab.com/expressionengine/user-guide/installation/installation.html">Installing ExpressionEngine</a> <strong>before</strong> starting.</p>
+</div>
 <?php if ( ! empty($errors)): ?>
 	<div class="alert inline issue">
 		<h3><?=lang('error_occurred')?></h3>
@@ -6,54 +9,55 @@
 		<?php endforeach ?>
 	</div>
 <?php endif ?>
-
-<div class="alert inline warn">
-	<p><b>Please</b> read <a href="">Installing ExpressionEngine</a> <strong>before</strong> starting.</p>
-</div>
-<fieldset class="col-group">
+<fieldset class="col-group <?=form_error_class('db_hostname')?>">
 	<div class="setting-txt col w-8">
 		<h3><?=lang('db_hostname')?> <span class="required" title="required field">&#10033;</span></h3>
 		<em><?=lang('db_hostname_note')?></em>
 	</div>
 	<div class="setting-field col w-8 last">
-		<input name="db_hostname" class="required" type="text" value="localhost">
+		<input name="db_hostname" class="required" type="text" value="<?=set_value('db_hostname', 'localhost')?>">
+		<?=form_error('db_hostname');?>
 	</div>
 </fieldset>
-<fieldset class="col-group">
+<fieldset class="col-group <?=form_error_class('db_name')?>">
 	<div class="setting-txt col w-8">
 		<h3><?=lang('db_name')?> <span class="required" title="required field">&#10033;</span></h3>
 		<em><?=lang('db_name_note')?></em>
 		<em><mark><?=lang('db_name_warning')?></mark></em>
 	</div>
 	<div class="setting-field col w-8 last">
-		<input name="db_name" class="required" type="text" value="">
+		<input name="db_name" class="required" type="text" value="<?=set_value('db_name')?>">
+		<?=form_error('db_name');?>
 	</div>
 </fieldset>
-<fieldset class="col-group">
+<fieldset class="col-group <?=form_error_class('db_username')?>">
 	<div class="setting-txt col w-8">
 		<h3><?=lang('db_username')?> <span class="required" title="required field">&#10033;</span></h3>
 		<em><?=lang('db_username_note')?></em>
 	</div>
 	<div class="setting-field col w-8 last">
-		<input name="db_username" class="required" type="text" value="">
+		<input name="db_username" class="required" type="text" value="<?=set_value('db_username')?>">
+		<?=form_error('db_username');?>
 	</div>
 </fieldset>
-<fieldset class="col-group">
+<fieldset class="col-group <?=form_error_class('db_password')?>">
 	<div class="setting-txt col w-8">
-		<h3><?=lang('db_password')?> <span class="required" title="required field">&#10033;</span></h3>
+		<h3><?=lang('db_password')?></h3>
 		<em><?=lang('db_password_note')?></em>
 	</div>
 	<div class="setting-field col w-8 last">
-		<input name="db_password" class="required" type="password" value="">
+		<input name="db_password" class="required" type="password" value="<?=set_value('db_password')?>">
+		<?=form_error('db_password');?>
 	</div>
 </fieldset>
-<fieldset class="col-group last">
+<fieldset class="col-group  <?=form_error_class('db_prefix')?> last">
 	<div class="setting-txt col w-8">
 		<h3><?=lang('db_prefix')?> <span class="required" title="required field">&#10033;</span></h3>
 		<em><?=lang('db_prefix_note')?></em>
 	</div>
 	<div class="setting-field col w-8 last">
-		<input name="db_prefix" class="required" type="text" value="exp">
+		<input name="db_prefix" class="required" type="text" value="<?=set_value('db_prefix', 'exp')?>">
+		<?=form_error('db_prefix');?>
 	</div>
 </fieldset>
 <h2><?=lang('default_theme')?></h2>
@@ -63,36 +67,39 @@
 		<em><?=lang('install_default_theme_info')?></em>
 	</div>
 	<div class="setting-field col w-8 last">
-		<label class="choice mr yes"><input name="install_default_theme" value="y" type="radio"> <?=lang('yes')?></label>
-		<label class="choice chosen no"><input name="install_default_theme" value="n" type="radio" checked="checked"> <?=lang('no')?></label>
+		<label class="choice mr yes"><input name="install_default_theme" value="y" type="radio" <?=set_radio('install_default_theme', 'y');?>> <?=lang('yes')?></label>
+		<label class="choice chosen no"><input name="install_default_theme" value="n" type="radio" <?=set_radio('install_default_theme', 'n', TRUE);?>> <?=lang('no')?></label>
 	</div>
 </fieldset>
 <h2><?=lang('administrator_account')?></h2>
-<fieldset class="col-group">
+<fieldset class="col-group <?=form_error_class('username')?>">
 	<div class="setting-txt col w-8">
 		<h3><?=lang('username')?> <span class="required" title="required field">&#10033;</span></h3>
 		<em><?=lang('username_note')?></em>
 	</div>
 	<div class="setting-field col w-8 last">
-		<input name="username" class="required" type="text" value="">
+		<input name="username" class="required" type="text" value="<?=set_value('username')?>">
+		<?=form_error('username');?>
 	</div>
 </fieldset>
-<fieldset class="col-group">
+<fieldset class="col-group <?=form_error_class('email_address')?>">
 	<div class="setting-txt col w-8">
 		<h3><?=lang('e_mail')?> <span class="required" title="required field">&#10033;</span></h3>
 		<em><?=lang('e_mail_note')?></em>
 	</div>
 	<div class="setting-field col w-8 last">
-		<input name="email_address" class="required" type="text" value="">
+		<input name="email_address" class="required" type="text" value="<?=set_value('email_address')?>">
+		<?=form_error('email_address');?>
 	</div>
 </fieldset>
-<fieldset class="col-group last">
+<fieldset class="col-group  <?=form_error_class('password')?> last">
 	<div class="setting-txt col w-8">
 		<h3><?=lang('password')?> <span class="required" title="required field">&#10033;</span></h3>
 		<em><?=lang('password_note')?></em>
 	</div>
 	<div class="setting-field col w-8 last">
 		<input name="password" class="required" type="password" value="">
+		<?=form_error('password');?>
 	</div>
 </fieldset>
 <fieldset class="form-ctrls">
