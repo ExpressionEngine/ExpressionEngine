7f5829b872f1757ff3704bdd61835a49d5de0a4f
Pascal Kriete <mail@pascal.io>
2015-02-10 12:15:38 -0500

    Alphabetizing

--- system/expressionengine/language/english/form_validation_lang.php
+++ system/expressionengine/language/english/form_validation_lang.php
@@ -1,27 +1,27 @@
 <?php
 
-$lang['required']			= "This field is required.";
-$lang['isset']				= "The %s field must have a value.";
-$lang['valid_email']		= "This field must contain a valid email address.";
-$lang['valid_emails']		= "This field must contain all valid email addresses.";
-$lang['valid_url']			= "This field must contain a valid URL.";
-$lang['valid_ip']			= "This field must contain a valid IP.";
-$lang['auth_password']		= "The password entered is incorrect.";
-$lang['file_exists']		= "The path you submitted is not valid.";
-$lang['writable']			= "The path you submitted is not writable.";
-$lang['min_length']			= "This field must be at least %s characters in length.";
-$lang['max_length']			= "This field can not exceed %s characters in length.";
-$lang['exact_length']		= "This field must be exactly %s characters in length.";
 $lang['alpha']				= "This field may only contain alphabetical characters.";
-$lang['alpha_numeric']		= "This field may only contain alpha-numeric characters.";
 $lang['alpha_dash']			= "This field may only contain alpha-numeric characters, underscores, and dashes.";
 $lang['alpha_dash_space']	= "This field may only contain alpha-numeric characters, underscores, dashes, and spaces.";
-$lang['numeric']			= "This field must contain only numbers.";
-$lang['is_numeric']			= "This field must contain only numeric characters.";
+$lang['alpha_numeric']		= "This field may only contain alpha-numeric characters.";
+$lang['auth_password']		= "The password entered is incorrect.";
+$lang['exact_length']		= "This field must be exactly %s characters in length.";
+$lang['file_exists']		= "The path you submitted is not valid.";
 $lang['integer']			= "This field must contain an integer.";
-$lang['matches']			= "This field does not match the %s field.";
 $lang['is_natural']			= "This field must contain only positive numbers.";
 $lang['is_natural_no_zero']	= "This field must contain a number greater than zero.";
+$lang['is_numeric']			= "This field must contain only numeric characters.";
+$lang['isset']				= "The %s field must have a value.";
+$lang['matches']			= "This field does not match the %s field.";
+$lang['max_length']			= "This field can not exceed %s characters in length.";
+$lang['min_length']			= "This field must be at least %s characters in length.";
+$lang['numeric']			= "This field must contain only numbers.";
+$lang['required']			= "This field is required.";
+$lang['valid_email']		= "This field must contain a valid email address.";
+$lang['valid_emails']		= "This field must contain all valid email addresses.";
+$lang['valid_ip']			= "This field must contain a valid IP.";
+$lang['valid_url']			= "This field must contain a valid URL.";
+$lang['writable']			= "The path you submitted is not writable.";
 
 
 /* End of file form_validation_lang.php */
