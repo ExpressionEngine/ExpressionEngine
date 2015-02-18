513c03580c8fbe66633be8b4958a75d33594ddef
Pascal Kriete <mail@pascal.io>
2015-02-18 15:34:29 -0500

    Adding a gateway that can deal with variable column tables

---
+++ system/EllisLab/ExpressionEngine/Model/Content/VariableColumnGateway.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace EllisLab\ExpressionEngine\Model\Content;
+
+use EllisLab\ExpressionEngine\Service\Model\Gateway;
+
+class VariableColumnGateway extends Gateway {
+
+	/**
+	 *
+	 */
+	public function getFieldList()
+	{
+		if ( ! isset($this->_field_list_cache))
+		{
+			$all = ee('Database')
+				->newQuery()
+				->list_fields($this->getTableName());
+
+			$known = parent::getFieldList();
+
+			$this->_field_list_cache = array_merge($known, $all);
+		}
+
+		return $this->_field_list_cache;
+	}
+
+}
