264cce528bd2128f4b479078a7063401f213c38f
Seth Croston Barber <seth.barber@ellislab.com>
2015-02-26 10:15:00 -0800

    Stacking the radios when we have more than 2. Adding scroll-wrap when we have more than 5.

--- system/expressionengine/fieldtypes/radio/views/publish.php
+++ system/expressionengine/fieldtypes/radio/views/publish.php
@@ -1,5 +1,19 @@
+<?php if (count($options) > 5): ?>
+<div class="scroll-wrap">
+<?php endif; ?>
 <?php
-	$class = 'choice mr';
+	$default_class = 'choice';
+
+	if (count($options) > 2)
+	{
+		$default_class .= ' block';
+		$class = $default_class;
+	}
+	else
+	{
+		$class = $default_class . ' mr';
+	}
+
 	foreach ($options as $key => $value):
 		if ($selected == $key)
 		{
@@ -15,6 +29,11 @@
 			$class .= ' no';
 		}
 ?>
-	<label class="<?=$class?>"><?=form_radio($field_name, $key, ($selected == $key))?> <?=$value?></label>
-	<?php $class = 'choice'; ?>
+	<label class="<?=$class?>">
+		<?=form_radio($field_name, $key, ($selected == $key))?> <?=$value?>
+	</label>
+	<?php $class = $default_class; ?>
 <?php endforeach; ?>
+<?php if (count($options) > 2): ?>
+</div>
+<?php endif; ?>
