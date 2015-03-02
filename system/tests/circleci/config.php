da4b5049f6ec584fcd05b94a09216c74de87a7c1
Kevin Cupp <kevin.cupp@gmail.com>
2015-03-02 16:47:26 -0500

    Adding cache driver to Circle config.php

--- system/tests/circleci/config.php
+++ system/tests/circleci/config.php
@@ -31,6 +31,7 @@ $config['is_system_on'] = 'y';
 $config['allow_extensions'] = 'y';
 $config['cookie_prefix'] = '';
 $config['use_compressed_js'] = 'n';
+$config['cache_driver'] = 'file';
 $config['database'] = array(
 	'expressionengine' => array(
 		'database' => 'circle_test',
