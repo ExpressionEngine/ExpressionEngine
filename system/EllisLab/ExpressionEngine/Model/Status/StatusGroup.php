e59277eb2ee1080fdbbec3b549f2748808ac363c
Kevin Cupp <kevin.cupp@gmail.com>
2015-03-09 21:27:13 -0400

    Attempting to add default statuses when a status group is saved

--- system/EllisLab/ExpressionEngine/Model/Status/StatusGroup.php
+++ system/EllisLab/ExpressionEngine/Model/Status/StatusGroup.php
@@ -31,6 +31,7 @@ class StatusGroup extends Model {
 
 	protected static $_primary_key = 'group_id';
 	protected static $_table_name = 'status_groups';
+	protected static $_events = array('afterSave');
 
 	protected static $_relationships = array(
 		'Site' => array(
@@ -45,4 +46,26 @@ class StatusGroup extends Model {
 	protected $group_id;
 	protected $site_id;
 	protected $group_name;
+
+	/**
+	 * On save, make sure default statuses exist
+	 */
+	public function onAfterSave()
+	{
+		$open = $this->getFrontend()->make('Status');
+		$open->group_id = $this->getId();
+		$open->status_id = 1;
+		$open->site_id = $this->site_id;
+		$open->status = 'open';
+		$open->status_order = 1;
+		$open->save();
+
+		$closed = $this->getFrontend()->make('Status');
+		$closed->group_id = $this->getId();
+		$closed->status_id = 2;
+		$closed->site_id = $this->site_id;
+		$closed->status = 'closed';
+		$closed->status_order = 2;
+		$closed->save();
+	}
 }
