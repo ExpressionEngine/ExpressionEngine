23bcb5bf0b76acffe4efa8f38ce8c6d0f389fabb
Quinn Chrzan <quinnchrzan@gmail.com>
2015-03-12 20:03:24 -0400

    Updating picker js with callback functionality

--- themes/ee/javascript/src/cp/files/picker.js
+++ themes/ee/javascript/src/cp/files/picker.js
@@ -22,16 +22,43 @@
 "use strict";
 
 (function ($) {
+
+	var picker_url;
+	var modal;
+	var input_value;
+	var input_name;
+	var input_img;
+
 	$(document).ready(function () {
 		$('.modal-file').on('click', 'a:not([href=""])', function(e) {
 			e.preventDefault();
 			$(this).parents('div.box').load($(this).attr('href'));
 		});
 		$('.filepicker').click(function (e) {
-			$("." + $(this).attr('rel') + " div.box").load($(this).attr('href'));
+			picker_url = $(this).attr('href');
+			input_value = $(# + $(this).data('input-value'));
+			input_name = $(# + $(this).data('input-name'));
+			input_img = $(# + $(this).data('input-img'));
+			modal = $("." + $(this).attr('rel'));
+			modal.find(" div.box").load(picker_url);
 		});
-		$('.modal-file').on('click', 'tr', function(e) {
-			console.log($(this).find("input[type='checkbox']").val());
+		$('.modal-file').on('click', 'tbody > tr', function(e) {
+			var id = $(this).find("input[type='checkbox']").val();
+			var file_url = picker_url.replace(/directory=.+(?=&)/ig, 'file=' + id);
+
+			if (typeof EE.file_picker_url != 'undefined')
+			{
+				callback = EE.file_picker_callback;
+			} else {
+				callback = function(data) {
+					modal.find('.m-close').click();
+					input_value.val(data.id);
+					input_name.html(data.file_name);
+					input_img.html("<img src='" + data.path + "' />");
+				}
+			}
+
+			$.get(file_url, callback);
 		});
 	});
 })(jQuery);
