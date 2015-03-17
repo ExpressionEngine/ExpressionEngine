01307a3b44cf81bcabb5ce0477187e093471b0d2
Quinn Chrzan <quinnchrzan@gmail.com>
2015-03-17 11:20:31 -0400

    Creating filepicker module

---
+++ system/EllisLab/ExpressionEngine/Module/FilePicker/addon.setup.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+	'vendor' => 'EllisLab',
+	'product' => 'ExpressionEngine FilePicker',
+	'description' => 'The worlds most flexible file picker.',
+	'namespace' => 'EllisLab\ExpressionEngine\Module\FilePicker',
+);
