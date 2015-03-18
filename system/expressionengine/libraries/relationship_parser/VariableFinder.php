7b303bff591048153cb4a3abf5abb84a8607d914
Pascal Kriete <mail@pascal.io>
2015-03-18 09:03:00 -0400

    Make sure only annotations are removed when interleaving tags.

--- system/expressionengine/libraries/relationship_parser/VariableFinder.php
+++ system/expressionengine/libraries/relationship_parser/VariableFinder.php
@@ -70,7 +70,10 @@ class VariableFinder {
 		$lexer = new Lexer();
 		$regex = $this->wrapRegex('^', '$');
 
-		$str = preg_replace('/{!--(.*?)--}/', '', $str);
+		// Remove annotations for this step to prevent the automatic
+		// line-number adjustment in the conditional lexer. The original
+		// string will still have them when we actually prep the conditionals.
+		$str = preg_replace('/{!-- ra:(\w+) --}/', '', $str);
 
 		// Get the token stream
 		$tokens = $lexer->tokenize($str);
