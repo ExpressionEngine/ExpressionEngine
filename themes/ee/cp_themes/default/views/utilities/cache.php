730e7890962038e7b5a6ba1941b54a0b9c7927ce
Seth Croston Barber <seth.barber@ellislab.com>
2015-03-24 15:18:11 -0700

    Localizing the title attribute for the requird field glyph.

--- themes/ee/cp_themes/default/views/utilities/cache.php
+++ themes/ee/cp_themes/default/views/utilities/cache.php
@@ -5,7 +5,7 @@
 	<?=ee('Alert')->getAllInlines()?>
 	<fieldset class="col-group last">
 		<div class="setting-txt col w-8">
-			<h3><?=lang('caches_to_clear')?> <span class="required" title="required field">&#10033;</span></h3>
+			<h3><?=lang('caches_to_clear')?> <span class="required" title="<?=lang('required_field')?>">&#10033;</span></h3>
 			<em><?=lang('caches_to_clear_desc')?></em>
 		</div>
 		<div class="setting-field col w-8 last">
