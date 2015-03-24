e16cfafa3c7f86f2ec67384e1d17138c642c75fe
Quinn Chrzan <quinnchrzan@gmail.com>
2015-03-24 10:49:31 -0400

    adding routes view

---
+++ themes/ee/cp_themes/default/views/design/routes/index.php
@@ -0,0 +1,13 @@
+<?php extend_template('default-nav'); ?>
+
+<div class="tbl-ctrls">
+	<?=form_open($form_url)?>
+		<fieldset class="tbl-search right">
+			<a class="btn tn action" href="<?=cp_url('design/snippets/create')?>"><?=lang('create_new')?></a>
+		</fieldset>
+		<h1><?=$cp_heading?></h1>
+		<?=ee('Alert')->getAllInlines()?>
+		<?php $this->view('_shared/table', $table); ?>
+		<?php $this->view('_shared/pagination'); ?>
+	<?=form_close()?>
+</div>
