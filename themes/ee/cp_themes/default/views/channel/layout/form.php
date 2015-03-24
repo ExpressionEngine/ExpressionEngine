e95da131d659e48493c31a60650960501614cca7
Seth Croston Barber <seth.barber@ellislab.com>
2015-03-24 15:18:01 -0700

    Localizing the title attribute for the requird field glyph.

--- themes/ee/cp_themes/default/views/channel/layout/form.php
+++ themes/ee/cp_themes/default/views/channel/layout/form.php
@@ -61,7 +61,7 @@
 		<h2><?=lang('layout_options')?></h2>
 		<fieldset class="col-group">
 			<div class="setting-txt col w-8">
-				<h3><?=lang('name')?> <span class="required" title="required field">&#10033;</span></h3>
+				<h3><?=lang('name')?> <span class="required" title="<?=lang('required_field')?>">&#10033;</span></h3>
 				<em><?=lang('name_desc')?></em>
 			</div>
 			<div class="setting-field col w-8 last">
@@ -71,7 +71,7 @@
 		</fieldset>
 		<fieldset class="col-group last">
 			<div class="setting-txt col w-8">
-				<h3><?=lang('member_group(s)')?> <span class="required" title="required field">&#10033;</span></h3>
+				<h3><?=lang('member_group(s)')?> <span class="required" title="<?=lang('required_field')?>">&#10033;</span></h3>
 				<em><?=lang('member_group(s)_desc')?></em>
 			</div>
 			<div class="setting-field col w-8 last">
