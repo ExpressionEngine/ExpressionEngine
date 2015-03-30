95459060ffa45e1021e3cde155cf635aed232ecf
Quinn Chrzan <quinnchrzan@gmail.com>
2015-03-30 10:24:21 -0400

    Removing unused button from routes view

--- themes/ee/cp_themes/default/views/design/routes/index.php
+++ themes/ee/cp_themes/default/views/design/routes/index.php
@@ -2,9 +2,6 @@
 
 <div class="tbl-ctrls">
 	<?=form_open($form_url)?>
-		<fieldset class="tbl-search right">
-			<a class="btn tn action" href="<?=cp_url('design/snippets/create')?>"><?=lang('create_new')?></a>
-		</fieldset>
 		<h1><?=$cp_heading?></h1>
 		<?=ee('Alert')->getAllInlines()?>
 		<?php $this->view('_shared/table', $table); ?>
