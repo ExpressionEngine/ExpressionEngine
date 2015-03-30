c544639331b3d6f4926080108f87e544fc777e35
Kevin Cupp <kevin.cupp@gmail.com>
2015-03-30 16:34:36 -0400

    Tests for status group create/edit

---
+++ system/tests/rspec/tests/channel/test_status_group_create.rb
@@ -0,0 +1,90 @@
+require './bootstrap.rb'
+
+feature 'Status Group Create/Edit' do
+
+  before(:each) do
+    cp_session
+    @page = StatusGroupCreate.new
+    @page.load
+    no_php_js_errors
+  end
+
+  it 'shows the Status Group Create/Edit page' do
+    @page.all_there?.should == true
+    @page.should have_text 'Create Status Group'
+  end
+
+  it 'should validate regular fields' do
+    duplicate_group = 'A status group already exists with the same name.'
+
+    @page.submit
+
+    no_php_js_errors
+    should_have_form_errors(@page)
+    @page.should have_text 'Attention: Status group not saved'
+    should_have_error_text(@page.group_name, $required_error)
+
+    # AJAX validation
+    # Required name
+    @page.load
+    @page.group_name.trigger 'blur'
+    @page.wait_for_error_message_count(1)
+    should_have_error_text(@page.group_name, $required_error)
+    should_have_form_errors(@page)
+
+    @page.group_name.set 'Test'
+    @page.group_name.trigger 'blur'
+    @page.wait_for_error_message_count(0)
+    should_have_no_error_text(@page.group_name)
+    should_have_no_form_errors(@page)
+
+    # Duplicate group name
+    @page.group_name.set 'Default'
+    @page.group_name.trigger 'blur'
+    @page.wait_for_error_message_count(1)
+    should_have_error_text(@page.group_name, 'A status group already exists with the same name.')
+    should_have_form_errors(@page)
+
+    @page.group_name.set 'Test'
+    @page.group_name.trigger 'blur'
+    @page.wait_for_error_message_count(0)
+    should_have_no_error_text(@page.group_name)
+    should_have_no_form_errors(@page)
+  end
+
+  it 'should reject XSS' do
+    @page.group_name.set $xss_vector
+    @page.group_name.trigger 'blur'
+    @page.wait_for_error_message_count(1)
+    should_have_error_text(@page.group_name, $xss_error)
+    should_have_form_errors(@page)
+  end
+
+  it 'should save a new status group and load edit form' do
+    @page.group_name.set 'Test'
+    @page.submit
+    no_php_js_errors
+
+    @page.should have_text 'Status group saved'
+    @page.should have_text 'Edit Status Group'
+    should_have_no_form_errors(@page)
+
+    @page.group_name.value.should == 'Test'
+  end
+
+  it 'should edit an existing status group' do
+    @page.load_edit_for_status_group(1)
+    @page.should have_text 'Edit Status Group'
+    no_php_js_errors
+
+    @page.group_name.value.should == 'Default'
+
+    @page.group_name.set 'Test'
+    @page.submit
+
+    @page.group_name.value.should == 'Test'
+
+    @page.should have_text 'Edit Status Group'
+    @page.should have_text 'Status group saved'
+  end
+end
