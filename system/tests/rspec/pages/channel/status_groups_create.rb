06064b2878974d8de52918118bcd86f9803efa77
Kevin Cupp <kevin.cupp@gmail.com>
2015-03-30 16:35:31 -0400

    Tests for statuses listing

--- system/tests/rspec/pages/channel/status_groups_create.rb
+++ system/tests/rspec/pages/channel/status_groups_create.rb
@@ -8,4 +8,12 @@ class StatusGroupCreate < ControlPanelPage
 		click_link 'Status Groups'
 		click_link 'Create New'
 	end
+
+	def load_edit_for_status_group(number)
+		self.open_dev_menu
+		click_link 'Channel Manager'
+		click_link 'Status Groups'
+
+		find('tbody tr:nth-child('+number.to_s+') li.edit a').click
+	end
 end
