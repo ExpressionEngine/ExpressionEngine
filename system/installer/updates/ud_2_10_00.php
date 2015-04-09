195bcb3ded52de7f1ba6b1788f350c7ec68df3e6
Robin Sowell <robin.sowell@ellislab.com>
2015-04-09 12:30:27 -0400

    Updating member dates in the 2.10 updater...
    New members could have the old 2 digit years, so we need to update those.
    
    Also need to update exp_sites again- those could be off as well.

--- system/installer/updates/ud_2_10_00.php
+++ system/installer/updates/ud_2_10_00.php
@@ -34,7 +34,72 @@ class Updater {
 	 */
 	public function do_update()
 	{
+		ee()->load->dbforge();
+
+		$steps = new ProgressIterator(
+			array(
+				'_date_format_years'
+			)
+		);
+
+		foreach ($steps as $k => $v)
+		{
+			$this->$v();
+		}
+
 		return TRUE;
+	}
+
+	// -------------------------------------------------------------------------
+
+	/**
+	 * Change all date formatting columns to show full years again
+	 * @return void
+	 */
+	private function _date_format_years()
+	{
+		// Update members' date formats
+		ee()->db->update(
+			'members',
+			array('date_format' => '%n/%j/%Y'),
+			array('date_format' => '%n/%j/%y')
+		);
+		ee()->db->update(
+			'members',
+			array('date_format' => '%j/%n/%Y'),
+			array('date_format' => '%j-%n-%y')
+		);
+
+		// Update the site preferences
+		$sites = ee()->db->select('site_id')->get('sites');
+		$msm_config = new MSM_Config();
+
+		if ($sites->num_rows() > 0)
+		{
+			foreach ($sites->result_array() as $row)
+			{
+				$msm_config->site_prefs('', $row['site_id']);
+
+				$localization_preferences = array();
+
+				if ($msm_config->item('date_format') == '%n/%j/%y')
+				{
+					$localization_preferences['date_format'] = '%n/%j/%Y';
+				}
+				elseif ($msm_config->item('date_format') == '%j-%n-%y')
+				{
+					$localization_preferences['date_format'] = '%j/%n/%Y';
+				}
+
+				if ( ! empty($localization_preferences))
+				{
+					$msm_config->update_site_prefs(
+						$localization_preferences,
+						$row['site_id']
+					);
+				}
+			}
+		}
 	}
 }
 // EOF
