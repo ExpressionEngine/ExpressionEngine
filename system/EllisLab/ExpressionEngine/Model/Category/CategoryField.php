b783a6d06feb045a5aea1231164cca2690d1588c
Pascal Kriete <mail@pascal.io>
2015-04-09 23:45:59 -0400

    Category Fields!

---
+++ system/EllisLab/ExpressionEngine/Model/Category/CategoryField.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace EllisLab\ExpressionEngine\Model\Category;
+
+use EllisLab\ExpressionEngine\Model\Content\FieldModel;
+
+/**
+ * ExpressionEngine - by EllisLab
+ *
+ * @package		ExpressionEngine
+ * @author		EllisLab Dev Team
+ * @copyright	Copyright (c) 2003 - 2014, EllisLab, Inc.
+ * @license		http://ellislab.com/expressionengine/user-guide/license.html
+ * @link		http://ellislab.com
+ * @since		Version 3.0
+ * @filesource
+ */
+
+// ------------------------------------------------------------------------
+
+/**
+ * ExpressionEngine Category Field Model
+ *
+ * @package		ExpressionEngine
+ * @subpackage	Category
+ * @category	Model
+ * @author		EllisLab Dev Team
+ * @link		http://ellislab.com
+ */
+class CategoryField extends FieldModel {
+
+	protected static $_primary_key = 'field_id';
+	protected static $_table_name = 'category_fields';
+
+	protected $field_id;
+	protected $site_id;
+	protected $group_id;
+	protected $field_name;
+	protected $field_label;
+	protected $field_type;
+	protected $field_list_items;
+	protected $field_maxl;
+	protected $field_ta_rows;
+	protected $field_default_fmt;
+	protected $field_show_fmt;
+	protected $field_text_direction;
+	protected $field_required;
+	protected $field_order;
+
+	public function getDataTable()
+	{
+		return 'category_field_data';
+	}
+}
