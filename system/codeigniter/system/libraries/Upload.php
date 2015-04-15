89d8c44d25b0ce4936676395eb8270de63ac251a
Seth Croston Barber <seth.barber@ellislab.com>
2015-04-15 14:12:43 -0700

    Fixing a bug: ofFile() needs an argument.

--- system/codeigniter/system/libraries/Upload.php
+++ system/codeigniter/system/libraries/Upload.php
@@ -195,7 +195,7 @@ class CI_Upload {
 		// Set the uploaded data as class variables
 		$this->file_temp = $_FILES[$field]['tmp_name'];
 		$this->file_size = $_FILES[$field]['size'];
-		$this->file_type = ee()->mime_type->ofFile();
+		$this->file_type = ee()->mime_type->ofFile($this->file_temp);
 		$this->file_name = $this->_prep_filename($_FILES[$field]['name']);
 		$this->file_ext	 = $this->get_extension($this->file_name);
 		$this->client_name = $this->file_name;
