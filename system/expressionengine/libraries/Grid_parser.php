4cef8aca5c113dbb5d419a68b21cf0bce1947af2
Pascal Kriete <mail@pascal.io>
2015-04-28 01:42:19 -0400

    Fixing a relationship-in-grid bug where ordering the grid by random sometimes created PHP errors
    Regression via c692512e2. Shuffle does not preserve keys.

--- system/expressionengine/libraries/Grid_parser.php
+++ system/expressionengine/libraries/Grid_parser.php
@@ -239,7 +239,17 @@ class Grid_parser {
 		// Order by random
 		if ($params['orderby'] == 'random')
 		{
-			shuffle($entry_data);
+			// key preserving shuffle of $entry_data
+			$keys = array_keys($entry_data);
+
+			$shuffled_entry_data = array();
+
+			foreach ($keys as $key)
+			{
+				$shuffled_entry_data[$key] = $entry_data[$key];
+			}
+
+			$entry_data = $shuffled_entry_data;
 		}
 
 		// We'll handle limit and offset parameters this way; we can't do
