a1e94ab1f26bc78311b9d93fa31bf5d847b1ced3
Seth Croston Barber <seth.barber@ellislab.com>
2015-05-28 13:38:25 -0700

    Adjusting the view to use the file picker.

--- system/ee/legacy/fieldtypes/file/views/publish.php
+++ system/ee/legacy/fieldtypes/file/views/publish.php
@@ -1,12 +1,9 @@
 <input type="hidden" name="<?=$field_name?>" value="<?=$value?>">
-<?php if ($file): ?>
-<figure class="file-chosen">
+<figure class="file-chosen<?php if ( ! $file) echo " hidden";?>" id="<?=$field_name?>">
 	<img src="<?=$file['url']?>" alt="<?=$file['title']?>">
 	<ul class="toolbar">
 		<li class="edit"><a href="" title="<?=lang('edit')?>"></a></li>
 		<li class="remove"><a href="" title="<?=lang('remove')?>"></a></li>
 	</ul>
 </figure>
-<?php else: ?>
-<p class="solo-btn"><a class="btn action m-link" href="#" rel="modal-file" data-field-name="<?=$field_name?>"><?=lang('upload_file')?></a></p>
-<?php endif; ?>
+<p class="solo-btn<?php if ($file) echo " hidden";?>"><a class="btn action m-link filepicker" href="<?=$fp_url?>" rel="modal-file" data-field-name="<?=$field_name?>" data-input-image="<?=$field_name?>" data-input-value="<?=$field_name?>"><?=lang('upload_file')?></a></p>
