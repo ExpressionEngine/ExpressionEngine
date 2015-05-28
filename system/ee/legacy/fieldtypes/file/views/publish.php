202a548702b1b5f9a60e6cf07470a823ac48c677
Seth Croston Barber <seth.barber@ellislab.com>
2015-05-28 14:28:34 -0700

    Have the edit button trigger the modal too.

--- system/ee/legacy/fieldtypes/file/views/publish.php
+++ system/ee/legacy/fieldtypes/file/views/publish.php
@@ -2,7 +2,7 @@
 <figure class="file-chosen<?php if ( ! $file) echo " hidden";?>" id="<?=$field_name?>">
 	<img src="<?=$file['url']?>" alt="<?=$file['title']?>">
 	<ul class="toolbar">
-		<li class="edit"><a href="" title="<?=lang('edit')?>"></a></li>
+		<li class="edit"><a class="m-link filepicker" href="<?=$fp_url?>" rel="modal-file" data-field-name="<?=$field_name?>" data-input-image="<?=$field_name?>" data-input-value="<?=$field_name?>" title="<?=lang('edit')?>"></a></li>
 		<li class="remove"><a href="" title="<?=lang('remove')?>"></a></li>
 	</ul>
 </figure>
