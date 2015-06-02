01641e5c53c34e17c86900888ed4c676f8a1ca49
Seth Croston Barber <seth.barber@ellislab.com>
2015-06-02 15:42:14 -0700

    Relocating the button so the error message is last.

--- system/ee/legacy/fieldtypes/file/views/publish.php
+++ system/ee/legacy/fieldtypes/file/views/publish.php
@@ -1,4 +1,5 @@
 <input type="hidden" name="<?=$field_name?>" value="<?=$value?>">
+<p class="solo-btn<?php if ($file) echo " hidden";?>"><a class="btn action m-link filepicker" href="<?=$fp_url?>" rel="modal-file" data-input-image="<?=$field_name?>" data-input-value="<?=$field_name?>"><?=lang('upload_file')?></a></p>
 <figure class="file-chosen<?php if ( ! $file) echo " hidden";?>">
 	<img id="<?=$field_name?>" src="<?=$thumbnail?>" alt="<?=($file) ? $file->title : ''?>">
 	<ul class="toolbar">
@@ -9,4 +10,3 @@
 <?php if($file && ! $file->exists()): ?>
 <em><?=sprintf(lang('file_ft_cannot_find_file'), $file->file_name)?></em>
 <?php endif; ?>
-<p class="solo-btn<?php if ($file) echo " hidden";?>"><a class="btn action m-link filepicker" href="<?=$fp_url?>" rel="modal-file" data-input-image="<?=$field_name?>" data-input-value="<?=$field_name?>"><?=lang('upload_file')?></a></p>
