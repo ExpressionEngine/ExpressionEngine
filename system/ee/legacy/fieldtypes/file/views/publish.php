7d502701659ad528a0124a44ce034999051b73d0
Seth Croston Barber <seth.barber@ellislab.com>
2015-06-05 14:15:47 -0700

    Using the FilePicker plugin.

--- system/ee/legacy/fieldtypes/file/views/publish.php
+++ system/ee/legacy/fieldtypes/file/views/publish.php
@@ -1,9 +1,9 @@
 <input type="hidden" name="<?=$field_name?>" value="<?=$value?>">
-<p class="solo-btn<?php if ($file) echo " hidden";?>"><a class="btn action m-link filepicker" href="<?=$fp_url?>" rel="modal-file" data-input-image="<?=$field_name?>" data-input-value="<?=$field_name?>"><?=lang('upload_file')?></a></p>
+<p class="solo-btn<?php if ($file) echo " hidden";?>"><a class="btn action m-link file-field-filepicker" href="<?=$fp_url?>" rel="modal-file" data-input-image="<?=$field_name?>" data-input-value="<?=$field_name?>"><?=lang('upload_file')?></a></p>
 <figure class="file-chosen<?php if ( ! $file) echo " hidden";?>">
 	<img id="<?=$field_name?>" src="<?=$thumbnail?>" alt="<?=($file) ? $file->title : ''?>">
 	<ul class="toolbar">
-		<li class="edit"><a class="m-link filepicker" href="<?=$fp_url?>" rel="modal-file" data-field-name="<?=$field_name?>" data-input-image="<?=$field_name?>" data-input-value="<?=$field_name?>" title="<?=lang('edit')?>"></a></li>
+		<li class="edit"><a class="m-link file-field-filepicker" href="<?=$fp_url?>" rel="modal-file" data-field-name="<?=$field_name?>" data-input-image="<?=$field_name?>" data-input-value="<?=$field_name?>" title="<?=lang('edit')?>"></a></li>
 		<li class="remove"><a href="" title="<?=lang('remove')?>"></a></li>
 	</ul>
 </figure>
