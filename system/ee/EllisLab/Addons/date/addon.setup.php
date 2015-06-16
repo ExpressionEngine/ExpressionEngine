07437549e0a780fdabf5b1e26d7c7ac59f99a4ab
Seth Croston Barber <seth.barber@ellislab.com>
2015-06-16 11:25:10 -0700

    Adding addon.setup.php files for fieldtypes

---
+++ system/ee/EllisLab/Addons/date/addon.setup.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+	'author'      => 'EllisLab',
+	'author_url'  => 'http://ellislab.com/',
+	'name'        => 'Date',
+	'description' => '',
+	'version'     => '1.0',
+	'namespace'   => 'EllisLab\Addons\Date',
+	'settings_exist' => FALSE,
+);
---
+++ system/ee/EllisLab/Addons/text/addon.setup.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+	'author'      => 'EllisLab',
+	'author_url'  => 'http://ellislab.com/',
+	'name'        => 'Text Input',
+	'description' => '',
+	'version'     => '1.0',
+	'namespace'   => 'EllisLab\Addons\TextInput',
+	'settings_exist' => FALSE,
+);
