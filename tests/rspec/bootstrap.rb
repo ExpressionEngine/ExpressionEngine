336eac6b0c3b106ad8429096a58fb7a093d44d7e
Wes Baker <wes.baker@ellislab.com>
2015-06-17 17:05:15 -0400

    Whitespace

--- tests/rspec/bootstrap.rb
+++ tests/rspec/bootstrap.rb
@@ -5,17 +5,14 @@ require 'mysql2'
 require 'site_prism'
 require 'image_size'
 require './config.rb'
-
-if File.exists?('./config.local.rb') then
-	require './config.local.rb'
-end
+require './config.local.rb' if File.exist?('./config.local.rb')
 
 $db = Mysql2::Client.new(
-	:host     => $test_config[:db_host],
-	:database => $test_config[:db_name],
-	:username => $test_config[:db_username],
-	:password => $test_config[:db_password],
-	:flags    => Mysql2::Client::MULTI_STATEMENTS
+  host:     $test_config[:db_host],
+  database: $test_config[:db_name],
+  username: $test_config[:db_username],
+  password: $test_config[:db_password],
+  flags:    Mysql2::Client::MULTI_STATEMENTS
 )
 
 # Include our helpers
@@ -39,39 +36,38 @@ Capybara.run_server = false
 
 # Configure hook to run after each example
 RSpec.configure do |config|
+  # Keep using 'should' syntax in RSpec 3
+  config.expect_with :rspec do |c|
+    c.syntax = :should
+  end
 
-	# Keep using 'should' syntax in RSpec 3
-	config.expect_with :rspec do |c|
-		c.syntax = :should
-	end
+  # Before each example...
+  config.before(:each) do
+    # Whitelist URLs
+    page.driver.allow_url $test_config[:app_host]
+    page.driver.allow_url 'ellislab.com'
+    page.driver.allow_url 'google-analytics.com'
 
-	# Before each example...
-	config.before(:each) do
-		# Whitelist URLs
-		page.driver.allow_url $test_config[:app_host]
-		page.driver.allow_url 'ellislab.com'
-		page.driver.allow_url 'google-analytics.com'
+    # Re-import clean database
+    reset_db
+  end
 
-		# Re-import clean database
-		reset_db
-	end
+  # After each example...
+  config.after(:each) do
+    example = RSpec.current_example
+    # If the example failed, take a screenshot to help us spot the problem
+    unless example.exception.nil?
+      page.save_screenshot('screenshots/' + example.description + '.png')
+    end
 
-	# After each example...
-	config.after(:each) do
-		example = RSpec.current_example
-		# If the example failed, take a screenshot to help us spot the problem
-		if example.exception != nil
-			page.save_screenshot('screenshots/'+example.description+'.png');
-		end
-
-		# Check for PHP or Javascript errors on the page
-		begin
-			no_php_js_errors
-		rescue => error
-			# Raise another exception so that RSpec sees the example as a failure
-			page.save_screenshot('screenshots/'+example.description+'.png');
-			puts 'Screenshot taken: ' + example.description+'.png'
-			raise StandardError, error.message
-		end
-	end
+    # Check for PHP or Javascript errors on the page
+    begin
+      no_php_js_errors
+    rescue => error
+      # Raise another exception so that RSpec sees the example as a failure
+      page.save_screenshot('screenshots/' + example.description + '.png')
+      puts 'Screenshot taken: ' + example.description + '.png'
+      raise StandardError, error.message
+    end
+  end
 end
--- tests/rspec/config.rb
+++ tests/rspec/config.rb
@@ -4,9 +4,12 @@
 # environment settings there.
 #
 $test_config = {
-	:app_host    => 'http://ee2.test:8080/', # URL Capybara will use to access your EE install
-	:db_host     => 'localhost', # DB settings for resetting your database
-	:db_name     => 'circle_test',
-	:db_username => 'ubuntu',
-	:db_password => ''
+  # URL Capybara will use to access your EE install
+  app_host:    'http://ee2.test:8080/',
+
+  # DB settings for resetting your database
+  db_host:     'localhost',
+  db_name:     'circle_test',
+  db_username: 'ubuntu',
+  db_password: ''
 }
