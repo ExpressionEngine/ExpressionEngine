52279cfba7c034a58be77b2490794d1a5aaec21e
Wes Baker <wes.baker@ellislab.com>
2015-06-18 14:12:21 -0400

    Basic install is now tested.

--- tests/rspec/pages/installer/installer.rb
+++ tests/rspec/pages/installer/installer.rb
@@ -1,4 +1,11 @@
-class Installer < SitePrism::Page
-  set_url '/system/index.php'
-  element :submit, 'form input[type=submit]'
+module Installer
+  class Base < SitePrism::Page
+    set_url '/system/index.php'
+
+    section :install_form, Installer::Form, 'section.wrap'
+    section :install_success, Installer::Success, 'section.wrap'
+
+    element :header, 'h1'
+    element :req_title, 'h1 .req-title'
+  end
 end
--- tests/rspec/tests/installer/test_installer.rb
+++ tests/rspec/tests/installer/test_installer.rb
@@ -2,14 +2,20 @@ ENV['area'] = 'installer'
 require './bootstrap.rb'
 
 feature 'Installer' do
+  def swap(file, before, after)
+    file = File.expand_path(file)
+    temp = File.read(file).gsub(before, after)
+    File.open(file, 'w') { |f| f.puts temp }
+  end
+
   before :all do
     # Make sure boot.php does not have the FALSE &&
     @boot = File.expand_path('../../system/ee/EllisLab/ExpressionEngine/Boot/boot.php')
-    text = File.read(@boot).gsub(
+    swap(
+      @boot,
       "if (FALSE && defined('REQ') && REQ == 'CP' && is_dir(SYSPATH.'ee/installer/'))",
       "if (defined('REQ') && REQ == 'CP' && is_dir(SYSPATH.'ee/installer/'))"
     )
-    File.open(@boot, 'w') { |f| f.puts text }
 
     # Rename config.php temporarily
     @config = File.expand_path('../../system/user/config/config.php')
@@ -17,7 +23,15 @@ feature 'Installer' do
     File.rename(@config, @config_temp)
     File.open(@config, 'w')
 
-    @page = Installer.new
+    # Disable directory renaming
+    @wizard = File.expand_path('../../system/ee/installer/controllers/wizard.php')
+    swap(
+      @wizard,
+      'return rename(APPPATH, $new_path);',
+      '// return rename(APPPATH, $new_path);'
+    )
+
+    @page = Installer::Base.new
   end
 
   before :each do
@@ -27,11 +41,17 @@ feature 'Installer' do
 
   after :all do
     # Add the FALSE && back into boot.php
-    text = File.read(@boot).gsub(
+    swap(
+      @boot,
       "if (defined('REQ') && REQ == 'CP' && is_dir(SYSPATH.'ee/installer/'))",
       "if (FALSE && defined('REQ') && REQ == 'CP' && is_dir(SYSPATH.'ee/installer/'))"
     )
-    File.open(@boot, 'w') { |f| f.puts text }
+
+    swap(
+      @wizard,
+      '// return rename(APPPATH, $new_path);',
+      'return rename(APPPATH, $new_path);'
+    )
 
     # Put config.php back
     File.delete(@config)
@@ -39,6 +59,22 @@ feature 'Installer' do
   end
 
   it 'should load installer' do
-    @page.all_there?.should == true
+    @page.install_form.all_there?.should == true
+  end
+
+  it 'should install successfully' do
+    @page.install_form.db_hostname.set $test_config[:db_host]
+    @page.install_form.db_name.set $test_config[:db_name]
+    @page.install_form.db_username.set $test_config[:db_username]
+    @page.install_form.db_password.set $test_config[:db_password]
+    @page.install_form.username.set 'admin'
+    @page.install_form.email_address.set 'hello@ellislab.com'
+    @page.install_form.password.set 'password'
+    @page.install_form.install_submit.click
+
+    no_php_js_errors
+    @page.req_title.text.should eq 'Completed'
+    @page.install_success.success_header.text.should match /ExpressionEngine (\d+\.\d+\.\d+) is now installed/
+    @page.install_success.all_there?.should == true
   end
 end
