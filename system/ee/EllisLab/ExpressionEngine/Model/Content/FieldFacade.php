26819af27b3290309cee13229cecd783a0af0de6
Pascal Kriete <mail@pascal.io>
2015-06-23 14:12:04 -0400

    Removing a check that was in the old api that should never be false

--- system/ee/EllisLab/ExpressionEngine/Model/Content/FieldFacade.php
+++ system/ee/EllisLab/ExpressionEngine/Model/Content/FieldFacade.php
@@ -154,14 +154,7 @@ class FieldFacade {
 	{
 		$data = $this->initField();
 
-		// initField can sometimes return a string if the field has a
-		// string_override key.
-		if (is_string($data))
-		{
-			return $data;
-		}
-
-		$field_value = set_value($this->getName(), $data['field_data']);
+		$field_value = $data['field_data'];
 
 		return ee()->api_channel_fields->apply('display_publish_field', array($field_value));
 	}
