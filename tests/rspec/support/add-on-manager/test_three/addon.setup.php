94331add21108c0dc7036bfdc2deb965929ddb04
Seth Croston Barber <seth.barber@ellislab.com>
2015-06-23 12:26:03 -0700

    Adding some dummy add-ons for testing the third party table

---
+++ tests/rspec/support/add-on-manager/test_three/addon.setup.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+	'author'      => 'Example Inc.',
+	'author_url'  => 'http://example.com/',
+	'name'        => 'Test Three',
+	'description' => '',
+	'version'     => '1.0',
+	'namespace'   => 'Example\Test\Three',
+	'settings_exist' => FALSE,
+
+	'plugin.typography' => TRUE
+);
---
+++ tests/rspec/support/add-on-manager/test_two/addon.setup.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+	'author'      => 'Example Inc.',
+	'author_url'  => 'http://example.com/',
+	'name'        => 'Test Two',
+	'description' => '',
+	'version'     => '1.0',
+	'namespace'   => 'Example\Test\Two',
+	'settings_exist' => FALSE,
+
+	'plugin.typography' => TRUE
+);
