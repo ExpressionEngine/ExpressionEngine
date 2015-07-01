af810a48d25b56231cbc23189e995baca058838d
Quinn Chrzan <quinnchrzan@gmail.com>
2015-07-01 13:09:38 -0400

    More language standardization

--- system/ee/legacy/language/english/comment_lang.php
+++ system/ee/legacy/language/english/comment_lang.php
@@ -6,310 +6,212 @@ $lang = array(
 // Required for MODULES page
 //----------------------------------------
 
-"comment_module_name" =>
-"Comment",
+"comment_module_name" => "Comment",
 
-"comment_module_description" =>
-"User commenting system",
+"comment_module_description" => "User commenting system",
 
 //----------------------------------------
 
 
-'comment_moved' =>
-'Comment moved', 
+'comment_moved' => 'Comment moved', 
 
-'comments_moved' =>
-'Comments moved',
+'comments_moved' => 'Comments moved',
 
-'filter_comments' =>
-'Filter Comments',
+'filter_comments' => 'Filter Comments',
 
-'comment' =>
-'Comment',
+'comment' => 'Comment',
 
-'comments' =>
-'Comments',
+'comments' => 'Comments',
 
-'edit_comment' =>
-'Edit Comment',
+'edit_comment' => 'Edit Comment',
 
-'settings' =>
-'Settings',
+'settings' => 'Settings',
 
-'comment_settings' =>
-'Comment Settings',
+'comment_settings' => 'Comment Settings',
 
-'delete_confirm' =>
-'Delete Comment Confirmation',
+'delete_confirm' => 'Delete Comment Confirmation',
 
-'delete_comment_confirm' =>
-'Are you sure you want to permanently delete this comment?',
+'delete_comment_confirm' => 'Are you sure you want to permanently delete this comment?',
 
-'delete_comments_confirm' =>
-'Are you sure you want to permanently delete these comments?',
+'delete_comments_confirm' => 'Are you sure you want to permanently delete these comments?',
 
-'comment_deleted' =>
-'Comment Deleted',
+'comment_deleted' => 'Comment Deleted',
 
-'status_changed' =>
-'Status Changed',
+'status_changed' => 'Status Changed',
 
-'comment_word_censoring' =>
-'Force word censoring for comments', 
+'comment_word_censoring' => 'Force word censoring for comments', 
 
-'comment_moderation_override' =>
-'Moderate expired entries',
+'comment_moderation_override' => 'Moderate expired entries',
 
-'comment_smart_notifications' =>
-'Enable Smart Notifications',
+'comment_smart_notifications' => 'Enable Smart Notifications',
 
 'selection_required' => 
 'At least one selection is required to submit',
 
-'close_selected' =>
-'Set Status to Closed',
+'close_selected' => 'Set Status to Closed',
 
-'open_selected' =>
-'Set Status to Open',
+'open_selected' => 'Set Status to Open',
 
-'pending_selected' =>
-'Set Status to Pending',
+'pending_selected' => 'Set Status to Pending',
 
-'move_selected' =>
-'Move Selected',
+'move_selected' => 'Move Selected',
 
-'instructions' =>
-'Instructions: ',
+'instructions' => 'Instructions: ',
 
-'move' =>
-'Move',
+'move' => 'Move',
 
-'move_comment_instr' =>
-'Enter the new entry id number:', 
+'move_comment_instr' => 'Enter the new entry id number:', 
 
-'registered_member' =>
-'Registered member',
+'registered_member' => 'Registered member',
 
-'url' =>
-'URL',
+'url' => 'URL',
 
-'location' =>
-'Location',
+'location' => 'Location',
 
-'choose_entry_for_comment_move' =>
-'Choose Entry for Comment Move',
+'choose_entry_for_comment_move' => 'Choose Entry for Comment Move',
 
-'move_comments_to_entry' =>
-'Move Comments to Entry',
+'move_comments_to_entry' => 'Move Comments to Entry',
 
-'filter_by_channel' =>
-'Filter by Channel',
+'filter_by_channel' => 'Filter by Channel',
 
 
-'filter_by_status' =>
-'Filter by Status',
+'filter_by_status' => 'Filter by Status',
 
-'filter_by_author' =>
-'Filter by Author',
+'filter_by_author' => 'Filter by Author',
 
-'collapsed_display' =>
-'Collapsed Display',
+'collapsed_display' => 'Collapsed Display',
 
-'expanded_display' =>
-'Expanded Display',
+'expanded_display' => 'Expanded Display',
 
-'date_range' =>
-'Date Range',
+'date_range' => 'Date Range',
 
-'keywords' =>
-'Keywords',
+'keywords' => 'Keywords',
 
-'open' =>
-'Open',
+'open' => 'Open',
 
-'closed' =>
-'Closed',
+'closed' => 'Closed',
 
-'entry_title' =>
-'Entry Title',
+'entry_title' => 'Entry Title',
 
-'email' =>
-'Email',
+'email' => 'Email',
 
-'name' =>
-'Name',
+'name' => 'Name',
 
-'ip_address' =>
-'IP',
+'ip_address' => 'IP',
 
-'delete_selected' =>
-'Delete Selected',
+'delete_selected' => 'Delete Selected',
 
 
-'blacklist' =>
-'Add IPs to Blacklist',
+'blacklist' => 'Add IPs to Blacklist',
 
-'search_in_comments' =>
-'Search in Comments',
+'search_in_comments' => 'Search in Comments',
 
-'search_in_ips' =>
-'Search in IPs',
+'search_in_ips' => 'Search in IPs',
 
-'search_in_entry_titles' =>
-'Search in Entry Titles',
+'search_in_entry_titles' => 'Search in Entry Titles',
 
-'search_in_names' =>
-'Search in Names',
+'search_in_names' => 'Search in Names',
 
-'search_in_emails' =>
-'Search in Email Addresses',
+'search_in_emails' => 'Search in Email Addresses',
 
-'today' =>
-'Today',
+'today' => 'Today',
 
-'past_day' =>
-'Last 24 hours',
+'past_day' => 'Last 24 hours',
 
-'past_week' =>
-'Last 7 days',
+'past_week' => 'Last 7 days',
 
-'past_month' =>
-'Last 30 days',
+'past_month' => 'Last 30 days',
 
-'past_six_months' =>
-'Last 180 days',
+'past_six_months' => 'Last 180 days',
 
-'past_year' =>
-'Last 365 days',
+'past_year' => 'Last 365 days',
 
-'any_date' =>
-'Custom Date Range',
+'any_date' => 'Custom Date Range',
 
 
 //
 
-"cmt_commenting_has_expired" =>
-"Commenting is not available in this channel entry.",
+"cmt_commenting_has_expired" => "Commenting is not available in this channel entry.",
 
-"cmt_name_not_allowed" =>
-"The name you are posting with is reserved.  Please choose a different name.",
+"cmt_name_not_allowed" => "The name you are posting with is reserved.  Please choose a different name.",
 
-"cmt_must_be_member" =>
-"You must be a registered member to post comments",
+"cmt_must_be_member" => "You must be a registered member to post comments",
 
-"cmt_no_authorized_for_comments" =>
-"You are not allowed to post comments",
+"cmt_no_authorized_for_comments" => "You are not allowed to post comments",
 
-"cmt_account_not_active" =>
-"Your membership account has not been activated yet",
+"cmt_account_not_active" => "Your membership account has not been activated yet",
 
-"cmt_comments_not_allowed" =>
-"Comments are not allowed in this channel",
+"cmt_comments_not_allowed" => "Comments are not allowed in this channel",
 
-"cmt_missing_name" =>
-"The name field is required",
+"cmt_missing_name" => "The name field is required",
 
-"cmt_missing_comment" =>
-"The comment field is required",
+"cmt_missing_comment" => "The comment field is required",
 
-"cmt_missing_email" =>
-"The email field is required",
+"cmt_missing_email" => "The email field is required",
 
-"cmt_invalid_email" =>
-"The email address you submitted is not valid",
+"cmt_invalid_email" => "The email address you submitted is not valid",
 
-"cmt_banned_email" =>
-"The email address you submitted is banned",
+"cmt_banned_email" => "The email address you submitted is banned",
 
-"cmt_too_large" =>
-"The comment you submitted contains %n characters.  Only %x characters are allowed.",
+"cmt_too_large" => "The comment you submitted contains %n characters.  Only %x characters are allowed.",
 
-"cmt_comments_timelock" =>
-"You are only allowed to post comments every %s seconds.",
+"cmt_comments_timelock" => "You are only allowed to post comments every %s seconds.",
 
-"cmt_duplicate_comment_warning" =>
-"Unable to receive your comment at this time.",
+"cmt_duplicate_comment_warning" => "Unable to receive your comment at this time.",
 
-"cmt_no_preview_template_specified" =>
-"The comment form tag does not contain the location of your preview template.",
+"cmt_no_preview_template_specified" => "The comment form tag does not contain the location of your preview template.",
 
-"cmt_comment_response_title" =>
-"Someone just responded to your comment",
+"cmt_comment_response_title" => "Someone just responded to your comment",
 
-"cmt_comment_response_body" =>
-"Someone just responded to the entry you subscribed to at:",
+"cmt_comment_response_body" => "Someone just responded to the entry you subscribed to at:",
 
-"cmt_comment_title" =>
-"The title of the entry is:",
+"cmt_comment_title" => "The title of the entry is:",
 
-"cmt_comment_url" =>
-"You can see the comment at the following URL:",
+"cmt_comment_url" => "You can see the comment at the following URL:",
 
-"cmt_comment_removal" =>
-"To stop receiving notifications for this comment, click here:",
+"cmt_comment_removal" => "To stop receiving notifications for this comment, click here:",
 
-"cmt_notification_removal" =>
-"Notification removal",
+"cmt_notification_removal" => "Notification removal",
 
-"cmt_you_have_been_removed" =>
-"You have been removed from receiving further notifications.",
+"cmt_you_have_been_removed" => "You have been removed from receiving further notifications.",
 
-"cmt_comment_accepted" =>
-"Comment Accepted",
+"cmt_comment_accepted" => "Comment Accepted",
 
-"cmt_will_be_reviewed" =>
-"Your comment will be reviewed by a moderator for approval.",
+"cmt_will_be_reviewed" => "Your comment will be reviewed by a moderator for approval.",
 
-"cmt_return_to_comments" =>
-"Return to Comments",
+"cmt_return_to_comments" => "Return to Comments",
 
-'you_have_been_subscribed' =>
-'You are now subscribed to the following entry:',
+'you_have_been_subscribed' => 'You are now subscribed to the following entry:',
 
-'you_have_been_unsubscribed' =>
-'Your subscription to the following entry has been removed:',
+'you_have_been_unsubscribed' => 'Your subscription to the following entry has been removed:',
 
-'cmt_subscribe' =>
-'Subscribe to this comment',
+'cmt_subscribe' => 'Subscribe to this comment',
 
-'cmt_unsubscribe' =>
-'Un-subscribe from this comment',
+'cmt_unsubscribe' => 'Un-subscribe from this comment',
 
-'invalid_entry_id' =>
-'Invalid Entry ID',
+'invalid_entry_id' => 'Invalid Entry ID',
 
-'no_valid_comments' =>
-'No Valid Comments',
+'no_valid_comments' => 'No Valid Comments',
 
-'comment_edit_time_limit' =>
-'Comment Edit Time Limit (in seconds, set to 0 for no limit)',
+'comment_edit_time_limit' => 'Comment Edit Time Limit (in seconds, set to 0 for no limit)',
 
-'already_subscribed' =>
-'You are already subscribed to this comment.',
+'already_subscribed' => 'You are already subscribed to this comment.',
 
-'not_currently_subscribed' =>
-'You do not appear to be subscribed to the entry ID you submitted',
+'not_currently_subscribed' => 'You do not appear to be subscribed to the entry ID you submitted',
 
-'invalid_subscription' =>
-'The URL you submitted is invalid',
+'invalid_subscription' => 'The URL you submitted is invalid',
 
-'settings_updated' =>
-'Settings Updated',
+'settings_updated' => 'Settings Updated',
 
-'comment_updated' =>
-'Comment Updated',
+'comment_updated' => 'Comment Updated',
 
-'start_date' =>
-'Start Date',
+'start_date' => 'Start Date',
 
-'end_date' =>
-'End Date',
+'end_date' => 'End Date',
 
-'expand' =>
-'Expand',
+'expand' => 'Expand',
 
-'no_results' =>
-'No Results',
+'no_results' => 'No Results',
 
 );
 
