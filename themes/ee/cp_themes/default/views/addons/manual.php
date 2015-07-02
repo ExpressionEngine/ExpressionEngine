ab67794ac3c46eb6ece59c0bfa0ff6984bc69311
Derek Jones <derek.jones@ellislab.com>
2015-07-02 15:27:14 -0700

    redundant modals, whitespace

--- themes/ee/cp_themes/default/views/addons/manual.php
+++ themes/ee/cp_themes/default/views/addons/manual.php
@@ -1,8 +1,6 @@
 <?php extend_template('default-nav'); ?>
 
-			<h1><?=$name?> <?=$version?><br><i><?=lang('author')?>: <a href="<?=$author_url?>" rel="external"><?=$author?></a><br><?=$description?></i></h1>
-			<div class="md-wrap">
-				<?=$readme?>
-			</div>
-
-<?php if (isset($blocks['modals'])) echo $blocks['modals']; ?>
+	<h1><?=$name?> <?=$version?><br><i><?=lang('author')?>: <a href="<?=$author_url?>" rel="external"><?=$author?></a><br><?=$description?></i></h1>
+	<div class="md-wrap">
+		<?=$readme?>
+	</div>
