512811122d4a4ebc9bbb67dc1983bc623afd4120
Derek Jones <derek.jones@ellislab.com>
2015-07-02 17:19:39 -0700

    shipped plugins are major version updates
    also fixing URLs to https

--- system/ee/EllisLab/Addons/markdown/addon.setup.php
+++ system/ee/EllisLab/Addons/markdown/addon.setup.php
@@ -2,10 +2,10 @@
 
 return array(
 	'author'      => 'EllisLab',
-	'author_url'  => 'http://ellislab.com/',
+	'author_url'  => 'https://ellislab.com/',
 	'name'        => 'Markdown',
 	'description' => 'Parse text using Markdown and Smartypants',
-	'version'     => '1.1',
+	'version'     => '2.0',
 	'namespace'   => 'EllisLab\Addons\Markdown',
 	'settings_exist' => FALSE,
 	'plugin.typography' => TRUE
