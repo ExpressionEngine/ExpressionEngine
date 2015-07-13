9c1309482742466ee1b085b311bb9c6e7a282123
Wes Baker <wes@wesbaker.com>
2015-07-13 15:41:08 -0400

    Changing all addon.setup files to use semver

--- system/ee/EllisLab/Addons/multi_select/addon.setup.php
+++ system/ee/EllisLab/Addons/multi_select/addon.setup.php
@@ -5,7 +5,7 @@ return array(
 	'author_url'  => 'https://ellislab.com/',
 	'name'        => 'Multi Select',
 	'description' => '',
-	'version'     => '1.0',
+	'version'     => '1.0.0',
 	'namespace'   => 'EllisLab\Addons\MultiSelect',
 	'settings_exist' => FALSE,
 );
