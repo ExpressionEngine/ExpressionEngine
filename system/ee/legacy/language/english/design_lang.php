9120b053ff028b9b69e0644fe100217922d0d77f
Seth Croston Barber <seth.barber@ellislab.com>
2015-07-13 16:04:38 -0700

    Fixing a lang key

--- system/ee/legacy/language/english/design_lang.php
+++ system/ee/legacy/language/english/design_lang.php
@@ -160,7 +160,7 @@ $lang = array(
 
 'variable' => 'Variable',
 
-'no_global_variables' => 'There are currently no Template Variables',
+'no_template_variables' => 'There are currently no Template Variables',
 
 'no_snippets' => 'There are currently no Template Partials',
 
