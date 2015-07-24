db454aebb0ed12f35b209651b5c0472d6b885e50
Wes Baker <wes@wesbaker.com>
2015-07-24 11:59:22 -0400

    DRYing up the update tests.

--- tests/rspec/tests/installer/test_updater.rb
+++ tests/rspec/tests/installer/test_updater.rb
@@ -43,24 +43,15 @@ feature 'Updater' do
   context 'when updating from 2.x to 3.x' do
     it 'upgrades using localhost as the database host' do
       @installer.replace_database_config(@database, 'localhost')
-
-      @page.should have(0).inline_errors
-      @page.header.text.should include "Update ExpressionEngine #{@version} to 3.0.0"
-      @page.submit.click
-
-      @page.header.text.should include "Updating ExpressionEngine #{@version} to 3.0.0"
-      @page.req_title.text.should include 'Processing | Step 2 of 3'
-
-      sleep 1 # Wait for the updater to finish
-
-      @page.header.text.should include 'ExpressionEngine 3.0.0 Installed'
-      @page.req_title.text.should include 'Completed'
-      @page.has_submit?.should == true
+      test_update
     end
 
     it 'upgrades using 127.0.0.1 as the database host' do
       @installer.replace_database_config(@database, '127.0.0.1')
+      test_update
+    end
 
+    def test_update
       @page.should have(0).inline_errors
       @page.header.text.should include "Update ExpressionEngine #{@version} to 3.0.0"
       @page.submit.click
