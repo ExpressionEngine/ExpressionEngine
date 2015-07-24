47f2f28718dcd197883a557aed8ad525417eb2df
Quinn Chrzan <quinnchrzan@gmail.com>
2015-07-24 16:34:56 -0400

    Adding initial addon.setup.php for spam module

---
+++ system/ee/EllisLab/Addons/spam/addon.setup.php
@@ -0,0 +1,12 @@
+<?php
+
+use EllisLab\ExpressionEngine\Addons\Spam;
+
+return array(
+	'author'      => 'EllisLab',
+	'author_url'  => 'https://ellislab.com/',
+	'name'        => 'ExpressionEngine Spam Module',
+	'version'     => '1.0.0',
+	'namespace'   => 'EllisLab\Addons\Spam',
+	'settings_exist' => TRUE,
+);
