4dab8d34e5c10678bffa5194b0e657d3cd43a4ad
Quinn Chrzan <quinnchrzan@gmail.com>
2015-07-27 16:07:47 -0400

    Starting to move the spam module over to using the dic internally

--- system/ee/EllisLab/Addons/spam/addon.setup.php
+++ system/ee/EllisLab/Addons/spam/addon.setup.php
@@ -13,7 +13,63 @@ return array(
 		'Spam' => function($ee)
 		{
 			return new Spam();
-		}
+		},
+		'Spam/Training' => function($ee)
+		{
+			return new Training();
+		},
+		'Spam/Classifier' => function($ee)
+		{
+			return new Classifier();
+		},
+		'Spam/Distribution' => function($ee)
+		{
+			return new Distribution();
+		},
+		'Spam/Document' => function($ee)
+		{
+			return new Document();
+		},
+		'Spam/Expectation' => function($ee)
+		{
+			return new Expectation();
+		},
+		'Spam/Source' => function($ee)
+		{
+			return new Source();
+		},
+		'Spam/Tokenizer' => function($ee)
+		{
+			return new Tokenizer();
+		},
+		'Spam/Vectorize' => function($ee)
+		{
+			return new Vectorize();
+		},
+		'Spam/Vectorizers/ASCIIPrintable' => function($ee)
+		{
+			return new ASCIIPrintable();
+		},
+		'Spam/Vectorizers/Entropy' => function($ee)
+		{
+			return new Entropy();
+		},
+		'Spam/Vectorizers/Links' => function($ee)
+		{
+			return new Links();
+		},
+		'Spam/Vectorizers/Punctuation' => function($ee)
+		{
+			return new Punctuation();
+		},
+		'Spam/Vectorizers/Spaces' => function($ee)
+		{
+			return new Spaces();
+		},
+		'Spam/Vectorizers/Tfidf' => function($ee)
+		{
+			return new Tfidf();
+		},
 	),
 	'models' => array(
 		'SpamKernel' => 'Model\SpamKernel',
