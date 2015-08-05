3dab3261d10c1397d8d89cc411b9afa75c263d89
Derek Jones <derek.jones@ellislab.com>
2015-08-04 22:03:18 -0700

    add POST to Profiler

--- system/ee/EllisLab/ExpressionEngine/Service/Profiler/Section/Post.php
+++ system/ee/EllisLab/ExpressionEngine/Service/Profiler/Section/Post.php
@@ -1,1 +1,59 @@
-Post.php
+<?php
+
+namespace EllisLab\ExpressionEngine\Service\Profiler\Section;
+
+use EllisLab\ExpressionEngine\Service\Profiler\ProfilerSection;
+
+/**
+ * ExpressionEngine - by EllisLab
+ *
+ * @package		ExpressionEngine
+ * @author		EllisLab Dev Team
+ * @copyright	Copyright (c) 2003 - 2015, EllisLab, Inc.
+ * @license		https://ellislab.com/expressionengine/user-guide/license.html
+ * @link		https://ellislab.com
+ * @since		Version 3.0
+ * @filesource
+ */
+
+// ------------------------------------------------------------------------
+
+/**
+ * ExpressionEngine Post Profiler Section
+ *
+ * @package		ExpressionEngine
+ * @subpackage	Profiler\Section
+ * @category	Service
+ * @author		EllisLab Dev Team
+ * @link		https://ellislab.com
+ */
+class Post extends ProfilerSection {
+
+	/**
+	 * Set the section's data
+	 *
+	 * @return void
+	 **/
+	public function setData()
+	{
+		if (count($_POST) == 0)
+		{
+			$data = lang('profiler_no_post');
+		}
+		else
+		{
+			foreach ($_POST as $key => $val)
+			{
+				if ( ! is_numeric($key))
+				{
+					$key = "'".$key."'";
+				}
+
+				$data["_POST[{$key}]"] = htmlspecialchars(stripslashes(print_r($val, TRUE)));
+			}
+
+		}
+
+		$this->data = array(lang('profiler_post') => $data);
+	}
+}
