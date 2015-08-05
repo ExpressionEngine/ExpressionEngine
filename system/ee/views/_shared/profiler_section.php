1724ba50d1cc2d9b37838312b95bf6cb480d3aa6
Derek Jones <derek.jones@ellislab.com>
2015-08-04 22:29:45 -0700

    stub for display

--- system/ee/views/_shared/profiler_section.php
+++ system/ee/views/_shared/profiler_section.php
@@ -1,1 +1,17 @@
-<?php print_r($profiler_data);?>
+<?php foreach ($profiler_data as $label => $data): ?>
+<fieldset>
+	<legend><?=$label?></legend>
+	<?php if ( ! is_array($data)): ?>
+		<p><?=$data?></p>
+	<?php else: ?>
+		<table>
+			<?php foreach ($data as $key => $value): ?>
+				<tr>
+					<td><?=$key?></td>
+					<td><?=$value?></td>
+				</tr>
+			<?php endforeach; ?>
+		</table>
+	<?php endif; ?>
+</fieldset>
+<?php endforeach; ?>
