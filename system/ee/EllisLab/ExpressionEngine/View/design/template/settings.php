21b6db1744b5fa132977690ec0c8add89e9a4a14
Kevin Cupp <kevin.cupp@gmail.com>
2015-08-07 14:49:43 -0400

    Fixed an issue where the tabs in the Template Settings & Access modal were not working
    Fixes #504

--- system/ee/EllisLab/ExpressionEngine/View/design/template/settings.php
+++ system/ee/EllisLab/ExpressionEngine/View/design/template/settings.php
@@ -4,15 +4,15 @@
 		<li><a class="act" href="" rel="t-0"><?=lang('settings')?></a></li>
 		<li><a href="" rel="t-1"><?=lang('access')?></a></li>
 	</ul>
+	<?=form_open($form_url, 'class="settings ajax-validate"')?>
+		<div class="tab t-0 tab-open">
+			<?=$settings?>
+		</div>
+		<div class="tab t-1">
+			<?=$access?>
+		</div>
+		<fieldset class="form-ctrls">
+			<?=cp_form_submit('btn_save_settings', 'btn_saving')?>
+		</fieldset>
+	</form>
 </div>
-<?=form_open($form_url, 'class="settings ajax-validate"')?>
-	<div class="tab t-0 tab-open">
-		<?=$settings?>
-	</div>
-	<div class="tab t-1">
-		<?=$access?>
-	</div>
-	<fieldset class="form-ctrls">
-		<?=cp_form_submit('btn_save_settings', 'btn_saving')?>
-	</fieldset>
-</form>
