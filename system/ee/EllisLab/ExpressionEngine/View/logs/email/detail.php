3233d1837013e2762b4ea2eb6797b259df695cfd
Wes Baker <wes.baker@ellislab.com>
2015-08-12 15:33:11 -0400

    Fixing date formatting issues with communicate email cache

--- system/ee/EllisLab/ExpressionEngine/View/logs/email/detail.php
+++ system/ee/EllisLab/ExpressionEngine/View/logs/email/detail.php
@@ -8,7 +8,7 @@
 			<ul class="toolbar">
 				<li class="remove"><a href="" class="m-link" rel="modal-confirm-remove" title="remove"></a></li>
 			</ul>
-			<h3><b>Date:</b> <?=$localize->human_time($email->cache_date)?><br><b>From:</b> <a href="<?=ee('CP/URL', 'myaccount', array('id' => $email->member_id))?>"><?=$email->member_name?></a><br><b>To:</b> <?=$email->recipient_name?><br><b>Subject:</b> <?=$email->subject?></h3>
+			<h3><b>Date:</b> <?=$localize->human_time($email->cache_date->format('U'))?><br><b>From:</b> <a href="<?=ee('CP/URL', 'myaccount', array('id' => $email->member_id))?>"><?=$email->member_name?></a><br><b>To:</b> <?=$email->recipient_name?><br><b>Subject:</b> <?=$email->subject?></h3>
 			<div class="message">
 				<?=$email->message?>
 			</div>
