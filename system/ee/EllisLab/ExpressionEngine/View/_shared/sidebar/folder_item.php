c69748adfe76a7ecb25c4a21f650593accb937d4
Seth Croston Barber <seth.barber@ellislab.com>
2015-08-13 14:21:47 -0700

    Conditionally showing the remove button (see: system templates)

--- system/ee/EllisLab/ExpressionEngine/View/_shared/sidebar/folder_item.php
+++ system/ee/EllisLab/ExpressionEngine/View/_shared/sidebar/folder_item.php
@@ -2,6 +2,8 @@
 	<a href="<?=$url?>"><?=$text?></a>
 	<ul class="toolbar">
 		<li class="edit"><a href="<?=$edit_url?>" title="<?=lang('edit')?>"></a></li>
+		<?php if ($confirm): ?>
 		<li class="remove"><a class="m-link" rel="modal-confirm-<?=$modal_name?>" href="" title="<?=lang('remove')?>" data-confirm="<?=$confirm?>" data-<?=$key?>="<?=$value?>"></a></li>
+		<?php endif; ?>
 	</ul>
 </li>
