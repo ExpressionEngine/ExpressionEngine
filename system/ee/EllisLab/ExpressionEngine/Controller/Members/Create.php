a9d1df4a5c28a96128c2257da85b8eb07ba63030
Seth Croston Barber <seth.barber@ellislab.com>
2015-08-14 13:13:04 -0700

    Adjusting the active state in the Member Manager

--- system/ee/EllisLab/ExpressionEngine/Controller/Members/Create.php
+++ system/ee/EllisLab/ExpressionEngine/Controller/Members/Create.php
@@ -132,6 +132,8 @@ class Create extends Members {
 			ee()->view->set_message('issue', lang('settings_save_error'), lang('settings_save_error_desc'));
 		}
 
+		$this->generateSidebar('all_members');
+
 		ee()->view->base_url = $this->base_url;
 		ee()->view->ajax_validate = TRUE;
 		ee()->view->cp_page_title = lang('register_member');
