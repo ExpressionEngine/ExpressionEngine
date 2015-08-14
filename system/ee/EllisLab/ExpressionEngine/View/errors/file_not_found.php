1cd8465dfa2bdd3db94ff563528c093a1bc298d4
Pascal Kriete <mail@pascal.io>
2015-08-14 11:56:29 -0400

    Adding a 404 controller and view

---
+++ system/ee/EllisLab/ExpressionEngine/View/errors/file_not_found.php
@@ -0,0 +1,15 @@
+<?php $this->extend('_templates/default-nav'); ?>
+
+<div class="col-group snap">
+	<div class="col w-16 last">
+		<div class="box">
+			<h1>404: Item does not exist.</h1>
+			<div class="txt-wrap">
+				<p>Sorry, we could not find the item you are trying to access in the system.</p>
+				<?php if (trim($url)): ?>
+					<p><b>URL:</b> <?=$url?></p>
+				<?php endif; ?>
+			</div>
+		</div>
+	</div>
+</div>
