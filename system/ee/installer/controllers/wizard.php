ExpressionEngine/ExpressionEngine

commit 39a1f89badd98866abf2b12084633cfa067ace7f

More granular errors in the installer.
Fixes #488

system/ee/installer/controllers/wizard.php
@@ -394,7 +394,7 @@ class Wizard extends CI_Controller {
 		}
 
 		// Can we connect?
-		if ( ! $this->db_connect($db))
+		if ($this->db_connect($db) !== TRUE)
 		{
 			$this->set_output('error', array('error' => lang('database_no_config')));
 			return FALSE;
@@ -518,6 +518,72 @@ class Wizard extends CI_Controller {
 	// --------------------------------------------------------------------
 
 	/**
+	 * Checks if the database host is valid
+	 *
+	 * @return boolean TRUE if successful, FALSE otherwise
+	 */
+	public function valid_db_host()
+	{
+		return $this->db_validation(2002, function () {
+			ee()->form_validation->set_message(
+				'valid_db_host',
+				lang('database_invalid_host')
+			);
+		});
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * Check if the database given is valid
+	 *
+	 * @return boolean TRUE if successful, FALSE otherwise
+	 */
+	public function valid_db_database()
+	{
+		return $this->db_validation(1049, function() {
+			ee()->form_validation->set_message(
+				'valid_db_database',
+				lang('database_invalid_database')
+			);
+		});
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * Given a MySQL error number, will check to see if that error was thrown
+	 * and call the given callable if it is
+	 *
+	 * @param int $error_number The MySQL error number
+	 * @param Callable $callable The function to call in case the error was thrown
+	 * @return boolean TRUE if successful, FALSE otherwise
+	 */
+	private function db_validation($error_number, Callable $callable)
+	{
+		if ( ! isset($this->db_connect_attempt))
+		{
+			$this->db_connect_attempt = $this->db_connect(array(
+				'hostname' => ee()->input->post('db_hostname'),
+				'database' => ee()->input->post('db_name'),
+				'username' => ee()->input->post('db_username'),
+				'password' => ee()->input->post('db_password'),
+				'dbprefix' => 'exp'
+			));
+		}
+
+		if ($this->db_connect_attempt == $error_number)
+		{
+			$callable();
+			return FALSE;
+		}
+
+		return TRUE;
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
 	 * Form validation callback for checking DB prefixes
 	 *
 	 * @param  string $db_prefix DB Prefix to validate
@@ -575,12 +641,12 @@ class Wizard extends CI_Controller {
 			array(
 				'field' => 'db_hostname',
 				'label' => 'lang:db_hostname',
-				'rules' => 'required'
+				'rules' => 'required|callback_valid_db_host'
 			),
 			array(
 				'field' => 'db_name',
 				'label' => 'lang:db_name',
-				'rules' => 'required'
+				'rules' => 'required|callback_valid_db_database'
 			),
 			array(
 				'field' => 'db_username',
@@ -645,9 +711,14 @@ class Wizard extends CI_Controller {
 			'dbcollat' => 'utf8_general_ci'
 		);
 
-		if ( ! $this->db_connect($db))
-		{
-			$errors[] = lang('database_no_connect');
+		switch ($this->db_connect($db)) {
+			case 1045:
+				$errors[] = lang('database_invalid_user');
+				break;
+
+			default:
+				$errors[] = lang('database_no_connect');
+				break;
 		}
 
 		// Does the specified database schema type exist?
@@ -1253,9 +1324,10 @@ class Wizard extends CI_Controller {
 				return $this->db_connect($db);
 			}
 
-			return FALSE;
+			return ($e->getCode()) ?: FALSE;
 		}
 
+		ee()->remove('db');
 		ee()->set('db', $db_object);
 
 		return TRUE;

system/ee/installer/language/english/installer_lang.php
@@ -58,6 +58,9 @@ $lang = array(
 'unwritable_config'                  => 'Your config.php file does not appear to have the proper file permissions. Please set the file permissions to 666 (or the equivalent write permissions for your server) on the following file: system/user/config/config.php',
 'json_parser_missing'                => 'Your instance of PHP does not support the <code>json_encode</code> and <code>json_decode</code> methods.',
 'unwritable_cache_folder'            => 'Your cache folder does not appear to have proper permissions. Please set the folder permissions to 777 (or the equivalent write permissions for your server) on the following folder: system/user/cache',
+'database_invalid_host'              => 'The database host you submitted is invalid.',
+'database_invalid_database'          => 'The database name you submitted is invalid.',
+'database_invalid_user'              => 'The database user and password combination 	you submitted is invalid.',
 'database_no_config'                 => 'Unable to connect to your database using the configuration settings found in the following file: config/config.php file. Please correct the settings so that the update can proceed.',
 'database_no_data'                   => 'Unable to locate any database connection information.',
 'database_no_connect'                => 'Unable to connect to your database using the configuration settings you submitted.',

commit 4136db24288016107b2a1d45076a2fb973db1880

hack to fix a db prefix issue introduced in 39a1f89badd98866abf2b12084633cfa067ace7f
This is incomplete, as it does not allow custom prefixes. One or more
objects appear to be overwritten by this connection attempt.

system/ee/installer/controllers/wizard.php
@@ -576,7 +576,7 @@ class Wizard extends CI_Controller {
 				'database' => ee()->input->post('db_name'),
 				'username' => ee()->input->post('db_username'),
 				'password' => ee()->input->post('db_password'),
-				'dbprefix' => 'exp'
+				'dbprefix' => 'exp_'
 			));
 		}
 