64284e4558236576396a348fee31ddd6ce7611b0
Kevin Cupp <kevin.cupp@gmail.com>
2015-08-21 18:47:37 -0400

    Adding non-null module IDs to upload prefs in database.sql

--- tests/rspec/sql/database.sql
+++ tests/rspec/sql/database.sql
@@ -2042,8 +2042,8 @@ UNLOCK TABLES;
 
 LOCK TABLES `exp_upload_prefs` WRITE;
 INSERT INTO `exp_upload_prefs` (`id`, `site_id`, `name`, `server_path`, `url`, `allowed_types`, `max_size`, `max_height`, `max_width`, `properties`, `pre_format`, `post_format`, `file_properties`, `file_pre_format`, `file_post_format`, `cat_group`, `batch_location`, `module_id`) VALUES
-	(1,1,'Main Upload Directory','../images/uploads/','/images/uploads/','all','','','','style="border: 0;" alt="image"','','','','','',NULL,NULL,NULL),
-	(2,1,'About','../themes/ee/site/agile_records/images/uploads/','/themes/ee/site/agile_records/images/uploads/','img','','','','','','','','','',NULL,NULL,NULL),
+	(1,1,'Main Upload Directory','../images/uploads/','/images/uploads/','all','','','','style="border: 0;" alt="image"','','','','','',NULL,NULL,0),
+	(2,1,'About','../themes/ee/site/agile_records/images/uploads/','/themes/ee/site/agile_records/images/uploads/','img','','','','','','','','','',NULL,NULL,0),
 	(3,1,'Avatars','../images/avatars/','/images/avatars/','img','50','100','100',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,4),
 	(4,1,'Signature Attachments','../images/signature_attachments/','/images/signature_attachments/','img','30','80','480',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,4),
 	(5,1,'PM Attachments','../images/pm_attachments/','/images/pm_attachments/','img','250',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,4);
