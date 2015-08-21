df690b768b4de9831148c5772c6da2447ab55b0b
Derek Jones <derek.jones@ellislab.com>
2015-08-20 18:00:31 -0700

    stub for default content file
    [ci skip]

---
+++ themes/ee/site/default/default_content.php
@@ -0,0 +1,13 @@
+<?php
+// create status group
+// create categories
+// create fields
+// create field groups
+// create Channel, assigned with the above groups
+
+// add upload locations
+// add files
+
+// add entries
+
+// set site_404 and strict_urls, etc.
