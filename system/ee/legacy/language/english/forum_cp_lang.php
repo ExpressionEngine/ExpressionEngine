b389e47294a24da3abc6e0fce1c1db6bd0abcc43
Seth Croston Barber <seth.barber@ellislab.com>
2015-08-21 12:03:29 -0700

    Fixing a language key typo.

--- system/ee/legacy/language/english/forum_cp_lang.php
+++ system/ee/legacy/language/english/forum_cp_lang.php
@@ -376,8 +376,8 @@ $lang = array(
 'invalid_member_group' => 'The member group you submitted does not appear to be valid',
 'invalid_username' => 'The username you submitted does not appear to be valid',
 
-'fadmins_removed' => 'Administrators Removed',
-'fadmins_removed_desc' => 'The following administrators were removed',
+'admins_removed' => 'Administrators Removed',
+'admins_removed_desc' => 'The following administrators were removed',
 
 // Moderators
 
