15af2a81394125dd75858b14abe08e42c4292cd5
Wes Baker <wes.baker@ellislab.com>
2015-08-27 13:12:35 -0400

    Whitespace! It's back!

--- system/ee/EllisLab/ExpressionEngine/Model/Member/MemberGroup.php
+++ system/ee/EllisLab/ExpressionEngine/Model/Member/MemberGroup.php
@@ -270,27 +270,27 @@ class MemberGroup extends Model {
 	 */
 	public function onAfterInsert()
 	{
-	    $this->setId($this->group_id);
+		$this->setId($this->group_id);
 
-	    $sites = $this->getFrontend()->get('Site')
-	        ->fields('site_id')
-	        ->all()
-	        ->pluck('site_id');
+		$sites = $this->getFrontend()->get('Site')
+			->fields('site_id')
+			->all()
+			->pluck('site_id');
 
-	    foreach ($sites as $site_id)
-	    {
-	        $group = $this->getFrontend()->get('MemberGroup')
-	            ->filter('group_id', $this->group_id)
-	            ->filter('site_id', $site_id)
-	            ->first();
+		foreach ($sites as $site_id)
+		{
+			$group = $this->getFrontend()->get('MemberGroup')
+				->filter('group_id', $this->group_id)
+				->filter('site_id', $site_id)
+				->first();
 
-	        if ( ! $group)
-	        {
-	            $data = $this->getValues();
-	            $data['site_id'] = (int) $site_id;
-	            $this->getFrontend()->make('MemberGroup', $data)->save();
-	        }
-	    }
+			if ( ! $group)
+			{
+				$data = $this->getValues();
+				$data['site_id'] = (int) $site_id;
+				$this->getFrontend()->make('MemberGroup', $data)->save();
+			}
+		}
 	}
 
 	protected function constrainQueryToSelf($query)
