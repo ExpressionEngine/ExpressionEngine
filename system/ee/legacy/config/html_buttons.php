19a8ba559d26b76bcf43da8583cfc222f167045a
James Mathias <james.mathias@ellislab.com>
2015-08-27 10:36:19 -0500

    updating HTML button classes and tag names

--- system/ee/legacy/config/html_buttons.php
+++ system/ee/legacy/config/html_buttons.php
@@ -27,63 +27,63 @@ $installation_defaults = array('bold', 'italic', 'blockquote', 'anchor', 'pictur
 
 $predefined_buttons = array(
 							'bold' 		=> array(
-												'tag_name'  => 'b',
+												'tag_name'  => 'Bold text',
 												'tag_open'  => '<strong>',
 												'tag_close' => '</strong>',
 												'accesskey' => 'b',
 												'classname'	 => 'html-bold'
 												),
 							'italic'	=> array(
-												'tag_name'  => 'i',
+												'tag_name'  => 'Italic text',
 												'tag_open'  => '<em>',
 												'tag_close' => '</em>',
 												'accesskey' => 'i',
 												'classname'	 => 'html-italic'
 												),
 							'strike'	=> array(
-												'tag_name'  => 's',
+												'tag_name'  => 'Strikethrough text',
 												'tag_open'  => '<del>',
 												'tag_close' => '</del>',
 												'accesskey' => 's',
-												'classname'	 => 'strikethrough'
+												'classname'	 => 'html-strike'
 												),
 							'ins'	 	=> array(
-												'tag_name'  => 'insert',
+												'tag_name'  => 'Insert text',
 												'tag_open'  => '<ins>',
 												'tag_close' => '</ins>',
 												'accesskey' => '',
-												'classname'	 => 'btn_ins'
+												'classname'	 => 'html-ins'
 												),
 							'ul'		=> array(
-												'tag_name'  => 'ul',
+												'tag_name'  => 'Unordered list',
 												'tag_open'  => '<ul>',
 												'tag_close' => '</ul>',
 												'accesskey' => 'u',
 												'classname'	 => 'html-order-list'
 												),
 							'ol'		=> array(
-												'tag_name'  => 'ol',
+												'tag_name'  => 'Ordered list',
 												'tag_open'  => '<ol>',
 												'tag_close' => '</ol>',
 												'accesskey' => 'o',
 												'classname'	 => 'html-order-list'
 												),
 							'blockquote'	=> array(
-												'tag_name'  => 'blockquote',
+												'tag_name'  => 'Blockquote',
 												'tag_open'  => '<blockquote>',
 												'tag_close' => '</blockquote>',
 												'accesskey' => 'q',
 												'classname'	 => 'html-quote'
 												),
 							'anchor'	=> array(
-												'tag_name'  => 'a',
+												'tag_name'  => 'Link',
 												'tag_open'  => '<a href="[![Link:!:http://]!]"(!( title="[![Title]!]")!)>',
 												'tag_close' => '</a>',
 												'accesskey' => 'a',
 												'classname'	 => 'html-link'
 												),
 							'picture'	=> array(
-												'tag_name'  => 'img',
+												'tag_name'  => 'Image',
 												'tag_open'  => '<img src="[![Link:!:http://]!]" alt="[![Alternative text]!]" />',
 												'tag_close' => '',
 												'accesskey' => '',
