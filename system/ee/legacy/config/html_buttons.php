bd0daaa5ac102e11b0793daba8b660af20ed6be6
Seth Croston Barber <seth.barber@ellislab.com>
2015-08-27 08:49:13 -0700

    Localizing the tag names.

--- system/ee/legacy/config/html_buttons.php
+++ system/ee/legacy/config/html_buttons.php
@@ -27,63 +27,63 @@ $installation_defaults = array('bold', 'italic', 'blockquote', 'anchor', 'pictur
 
 $predefined_buttons = array(
 							'bold' 		=> array(
-												'tag_name'  => 'Bold text',
+												'tag_name'  => lang('html_btn_bold'),
 												'tag_open'  => '<strong>',
 												'tag_close' => '</strong>',
 												'accesskey' => 'b',
 												'classname'	 => 'html-bold'
 												),
 							'italic'	=> array(
-												'tag_name'  => 'Italic text',
+												'tag_name'  => lang('html_btn_italic'),
 												'tag_open'  => '<em>',
 												'tag_close' => '</em>',
 												'accesskey' => 'i',
 												'classname'	 => 'html-italic'
 												),
 							'strike'	=> array(
-												'tag_name'  => 'Strikethrough text',
+												'tag_name'  => lang('html_btn_strike'),
 												'tag_open'  => '<del>',
 												'tag_close' => '</del>',
 												'accesskey' => 's',
 												'classname'	 => 'html-strike'
 												),
 							'ins'	 	=> array(
-												'tag_name'  => 'Insert text',
+												'tag_name'  => lang('html_btn_ins'),
 												'tag_open'  => '<ins>',
 												'tag_close' => '</ins>',
 												'accesskey' => '',
 												'classname'	 => 'html-ins'
 												),
 							'ul'		=> array(
-												'tag_name'  => 'Unordered list',
+												'tag_name'  => lang('html_btn_ul'),
 												'tag_open'  => '<ul>',
 												'tag_close' => '</ul>',
 												'accesskey' => 'u',
 												'classname'	 => 'html-order-list'
 												),
 							'ol'		=> array(
-												'tag_name'  => 'Ordered list',
+												'tag_name'  => lang('html_btn_ol'),
 												'tag_open'  => '<ol>',
 												'tag_close' => '</ol>',
 												'accesskey' => 'o',
 												'classname'	 => 'html-order-list'
 												),
 							'blockquote'	=> array(
-												'tag_name'  => 'Blockquote',
+												'tag_name'  => lang('html_btn_blockquote'),
 												'tag_open'  => '<blockquote>',
 												'tag_close' => '</blockquote>',
 												'accesskey' => 'q',
 												'classname'	 => 'html-quote'
 												),
 							'anchor'	=> array(
-												'tag_name'  => 'Link',
+												'tag_name'  => lang('html_btn_anchor'),
 												'tag_open'  => '<a href="[![Link:!:http://]!]"(!( title="[![Title]!]")!)>',
 												'tag_close' => '</a>',
 												'accesskey' => 'a',
 												'classname'	 => 'html-link'
 												),
 							'picture'	=> array(
-												'tag_name'  => 'Image',
+												'tag_name'  => lang('html_btn_picture'),
 												'tag_open'  => '<img src="[![Link:!:http://]!]" alt="[![Alternative text]!]" />',
 												'tag_close' => '',
 												'accesskey' => '',
