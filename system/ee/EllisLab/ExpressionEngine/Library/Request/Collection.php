9762a3f683fedd4ab894957da04000fa31cea2ee
Quinn Chrzan <quinnchrzan@gmail.com>
2015-09-21 11:48:52 -0400

    Adding logic to handle a curl multi exec with a rolling request limit window

--- system/ee/EllisLab/ExpressionEngine/Library/Request/Collection.php
+++ system/ee/EllisLab/ExpressionEngine/Library/Request/Collection.php
@@ -70,6 +70,56 @@ class RequestCollection extends Collection {
 
 	public function rollingCurl($requests)
 	{
+		$window = (sizeof($urls) < $this->window) ? sizeof($urls) : $this->window;
+		$master = curl_multi_init();
+		$curl_arr = array();
+		$options = ($custom_options) ? ($std_options + $custom_options) : $std_options;
+
+		for ($i = 0; $i < $window; $i++)
+		{
+			$ch = curl_init();
+			curl_setopt_array($ch, $request[$i]->config);
+			curl_multi_add_handle($master, $ch);
+		}
+
+		while ($running === TRUE)
+		{
+			while(($execrun = curl_multi_exec($master, $running)) == CURLM_CALL_MULTI_PERFORM);
+			{
+				if($execrun != CURLM_OK)
+				{
+					break;
+				}
+			}
+
+			while($done = curl_multi_info_read($master))
+			{
+				$info = curl_getinfo($done['handle']);
+				$output = curl_multi_getcontent($done['handle']);
+
+				if ( ! empty($this->callback))
+				{
+					call_user_func($this->callback, $output);
+				}
+				else
+				{
+					call_user_func($requests[$i]->callback, $output);
+				}
+
+				if (isset($requests[$i + 1]))
+				{
+					$ch = curl_init();
+					curl_setopt_array($ch,$requests[$i++]->config);
+					curl_multi_add_handle($master, $ch);
+				}
+
+				curl_multi_remove_handle($master, $done['handle']);
+			}
+
+			usleep(10000);
+		}
+
+		curl_multi_close($multi);
 	}
 
 }
