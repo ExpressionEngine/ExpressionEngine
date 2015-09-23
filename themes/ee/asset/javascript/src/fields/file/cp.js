901ccd56cbcf906ca302f6c4458c86838224cfaf
Kevin Cupp <kevin.cupp@gmail.com>
2015-09-23 16:28:49 -0400

    Binding file picker setup on Grid row display

--- themes/ee/asset/javascript/src/fields/file/cp.js
+++ themes/ee/asset/javascript/src/fields/file/cp.js
@@ -14,37 +14,45 @@
 
 (function ($) {
 	$(document).ready(function () {
-		$('.file-field-filepicker').FilePicker({
-			callback: function(data, references) {
-				var input = references.input_value;
+		function setupFileField(container) {
+			$('.file-field-filepicker', container).FilePicker({
+				callback: function(data, references) {
+					var input = references.input_value;
 
-				// Close the modal
-				references.modal.find('.m-close').click();
+					// Close the modal
+					references.modal.find('.m-close').click();
 
-				// Assign the value {filedir_#}filename.ext
-				input.val('{filedir_' + data.upload_location_id + '}' + data.file_name);
+					// Assign the value {filedir_#}filename.ext
+					input.val('{filedir_' + data.upload_location_id + '}' + data.file_name);
 
-				// Set the thumbnail
-				references.input_img.attr('src', data.path);
+					// Set the thumbnail
+					references.input_img.attr('src', data.path);
 
-				// Show the figure
-				input.siblings('figure').show();
+					// Show the figure
+					input.siblings('figure').show();
 
-				// Hide the upload button
-				input.siblings('p.solo-btn').hide();
+					// Hide the upload button
+					input.siblings('p.solo-btn').hide();
 
-				// Hide the "missing file" error
-				input.siblings('em').hide();
-			}
-		});
+					// Hide the "missing file" error
+					input.siblings('em').hide();
+				}
+			});
 
-		$('li.remove a').click(function (e) {
-			var figure = $(this).closest('figure');
-			figure.hide();
-			figure.siblings('em').hide(); // Hide the "missing file" erorr
-			figure.siblings('input[type="hidden"]').val('');
-			figure.siblings('p.solo-btn').show();
-			e.preventDefault();
-		});
+			$('li.remove a').click(function (e) {
+				var figure = $(this).closest('figure');
+				figure.hide();
+				figure.siblings('em').hide(); // Hide the "missing file" erorr
+				figure.siblings('input[type="hidden"]').val('');
+				figure.siblings('p.solo-btn').show();
+				e.preventDefault();
+			});
+		}
+
+		setupFileField();
+
+		Grid.bind('file', 'display', function(cell) {
+			setupFileField(cell);
+		})
 	});
 })(jQuery);
