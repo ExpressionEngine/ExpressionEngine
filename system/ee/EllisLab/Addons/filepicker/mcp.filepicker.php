f91b8ab2a98845e66124a5569ccbd2e3323866a1
Kevin Cupp <kevin.cupp@gmail.com>
2015-09-23 21:15:58 -0400

    Reverting part of my recent thumbnail commit, this wasn't needed

--- system/ee/EllisLab/Addons/filepicker/mcp.filepicker.php
+++ system/ee/EllisLab/Addons/filepicker/mcp.filepicker.php
@@ -190,7 +190,7 @@ class Filepicker_mcp {
 
 		$result = $file->getValues();
 
-		$result['path'] = $file->getAbsoluteThumbnailURL();
+		$result['path'] = $file->getAbsoluteURL();
 		$result['thumb_path'] = ee('Thumbnail')->get($file)->url;
 		$result['isImage'] = $file->isImage();
 
