71a134f715da609c7fb581db6b35a2dc30c7390d
Seth Croston Barber <seth.barber@ellislab.com>
2015-10-01 14:58:47 -0700

    More CP/URL updating

--- system/ee/legacy/libraries/Menu.php
+++ system/ee/legacy/libraries/Menu.php
@@ -94,7 +94,7 @@ class EE_Menu {
 			{
 				if ($site_id != ee()->config->item('site_id'))
 				{
-					$menu[$site_name] = ee('CP/URL', 'msm/switch_to/' . $site_id, array('page' => $site_backlink));
+					$menu[$site_name] = ee('CP/URL')->make('msm/switch_to/' . $site_id, array('page' => $site_backlink));
 				}
 			}
 		}
@@ -124,7 +124,7 @@ class EE_Menu {
 			foreach($channels->result() as $channel)
 			{
 				// Create link
-				$menu['create'][$channel->channel_title] = ee('CP/URL', 'publish/create/' . $channel->channel_id);
+				$menu['create'][$channel->channel_title] = ee('CP/URL')->make('publish/create/' . $channel->channel_id);
 
 				// Edit link
 				$menu['edit'][$channel->channel_title] = ee('CP/URL')->make('publish/edit', array('filter_by_channel' => $channel->channel_id));
@@ -161,32 +161,32 @@ class EE_Menu {
 			'can_delete_categories'
 		))
 		{
-			$menu['channel_manager'] = ee('CP/URL', 'channels');
+			$menu['channel_manager'] = ee('CP/URL')->make('channels');
 		}
 
 		if (ee()->cp->allowed_group('can_access_design'))
 		{
-			$menu['template_manager'] = ee('CP/URL', 'design');
+			$menu['template_manager'] = ee('CP/URL')->make('design');
 		}
 
 		if (ee()->config->item('multiple_sites_enabled') == 'y' && ee()->cp->allowed_group('can_access_addons'))
 		{
-			$menu['msm_manager'] = ee('CP/URL', 'msm');
+			$menu['msm_manager'] = ee('CP/URL')->make('msm');
 		}
 
 		if (ee()->cp->allowed_group('can_access_addons'))
 		{
-			$menu['addon_manager'] = ee('CP/URL', 'addons');
+			$menu['addon_manager'] = ee('CP/URL')->make('addons');
 		}
 
 		if (ee()->cp->allowed_group('can_access_utilities'))
 		{
-			$menu['utilities'] = ee('CP/URL', 'utilities');
+			$menu['utilities'] = ee('CP/URL')->make('utilities');
 		}
 
 		if (ee()->cp->allowed_group('can_access_logs'))
 		{
-			$menu['logs'] = ee('CP/URL', 'logs');
+			$menu['logs'] = ee('CP/URL')->make('logs');
 		}
 
 		return $menu;
@@ -246,10 +246,10 @@ class EE_Menu {
 	 * Sets up left sidebar navigation given an array of data like this:
 	 *
 	 * array(
-	 *     'key_of_heading' => ee('CP/URL', 'optional/link'),
+	 *     'key_of_heading' => ee('CP/URL')->make('optional/link'),
 	 *     'heading_with_no_link',
 	 *     array(
-	 *         'item_in_subsection' => ee('CP/URL', 'sub/section')
+	 *         'item_in_subsection' => ee('CP/URL')->make('sub/section')
 	 *     )
 	 * )
 	 *
