d411e8160a6ad2dbce5c88ff6c222ed8675f4596
Kevin Cupp <kevin.cupp@gmail.com>
2015-10-08 13:44:48 -0400

    Fixing field name in URLs test

--- tests/rspec/pages/settings/urls.rb
+++ tests/rspec/pages/settings/urls.rb
@@ -8,7 +8,7 @@ class UrlsSettings < ControlPanelPage
 	element :doc_url, 'input[name=doc_url]'
 	element :profile_trigger, 'input[name=profile_trigger]'
 	element :category_segment_trigger, 'input[name=reserved_category_word]'
-	elements :category_url, 'input[name=use_category_name]'
+	elements :use_category_name, 'input[name=use_category_name]'
 	element :url_title_separator, 'select[name=word_separator]'
 
 	def load
