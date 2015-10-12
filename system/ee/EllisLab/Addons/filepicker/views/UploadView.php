a33c628f1f0f785e579670b59a78561a7d852331
Quinn Chrzan <quinnchrzan@gmail.com>
2015-10-12 08:12:32 -0400

    Adding missing uploadview from file picker, doh!

---
+++ system/ee/EllisLab/Addons/filepicker/views/UploadView.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" lang="en-us" dir="ltr">
+		<meta content="width=device-width, initial-scale=1.0" name="viewport">
+		<?php if (isset($meta_refresh)): ?>
+		<meta http-equiv='refresh' content='<?=$meta_refresh['rate']?>; url=<?=$meta_refresh['url']?>'>
+		<?php endif;?>
+
+		<?=ee()->view->head_link('css/common.min.css'); ?>
+		<?=ee()->view->head_link('css/jquery-ui-1.8.16.custom.css'); ?>
+		<?php if (ee()->extensions->active_hook('cp_css_end') === TRUE):?>
+		<link rel="stylesheet" href="<?=ee('CP/URL', 'css/cp_global_ext', array('theme' => ee()->cp->cp_theme))?>" type="text/css" />
+		<?php endif;?>
+		<!-- <link href="touch-icon-iphone.png" rel="apple-touch-icon-precomposed" sizes="114x114">
+		<link href="touch-icon-ipad.png" rel="apple-touch-icon-precomposed" sizes="144x144"> -->
+
+		<?php
+		foreach (ee()->cp->get_head() as $item)
+		{
+			echo $item."\n";
+		}
+		?>
+	</head>
+	<body id="top">
+		<div class="box"><?=$content?></div>
+	</body>
+</html>
