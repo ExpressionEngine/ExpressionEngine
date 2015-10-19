e9302e1e294fcbe22563c69dd042a7f240c37d17
Seth Croston Barber <seth.barber@ellislab.com>
2015-10-19 15:22:07 -0700

    Added tests related to bug #21191

--- tests/rspec/tests/channel/test_layout_form.rb
+++ tests/rspec/tests/channel/test_layout_form.rb
@@ -6,6 +6,7 @@ feature 'Channel Layouts: Create/Edit' do
 		@page = ChannelLayoutForm.new
 		@page.load
 		no_php_js_errors
+		@page.displayed?
 	end
 
 	it 'display the Create Form Layout view' do
@@ -17,21 +18,101 @@ feature 'Channel Layouts: Create/Edit' do
 		@page.should have_publish_tab
 		@page.should have_date_tab
 		@page.should have_hide_date_tab
-		@page.should have_categories_tab
-		@page.should have_hide_categories_tab
-		@page.should have_options_tab
-		@page.should have_hide_options_tab
 		@page.should have_layout_name
 		@page.should have_member_groups
 		@page.should have_submit_button
 	end
 
-	# Bug #21191
 	context 'Hiding the Options Tab' do
 		it 'should still be hidden with an invalid form' do
+			hide_options_tab = @page.hide_tab_4
+
+			# Confirm the icon is for hiding
+			hide_options_tab[:class].should eq 'tab-on'
+
+			hide_options_tab.trigger('click')
+
+			# Confirm the icon is for showing
+			hide_options_tab[:class].should eq 'tab-off'
+
+			@page.submit_button.click
+			no_php_js_errors
+
+			@page.should have_alert
+
+			hide_options_tab = @page.hide_tab_4
+			hide_options_tab[:class].should eq 'tab-off'
 		end
 
 		it 'should be hidden when saved' do
+			hide_options_tab = @page.hide_tab_4
+
+			# Confirm the icon is for hiding
+			hide_options_tab[:class].should eq 'tab-on'
+
+			hide_options_tab.trigger('click')
+
+			# Confirm the icon is for showing
+			hide_options_tab[:class].should eq 'tab-off'
+
+			@page.layout_name.set 'Default'
+			@page.submit_button.click
+			no_php_js_errors
+
+			@page.edit(1)
+			hide_options_tab = @page.hide_tab_4
+			hide_options_tab[:class].should eq 'tab-off'
+		end
+
+		# Bug #21191
+		context 'Channel has no Categories' do
+			before(:each) do
+				visit '/system/index.php?/cp/channels/edit/1'
+				channel = ChannelCreate.new
+				channel.cat_group.each {|cat| cat.set false}
+				channel.submit
+				@page.load
+			end
+
+			it 'should still be hidden with an invalid form' do
+				hide_options_tab = @page.hide_tab_3
+
+				# Confirm the icon is for hiding
+				hide_options_tab[:class].should eq 'tab-on'
+
+				hide_options_tab.trigger('click')
+
+				# Confirm the icon is for showing
+				hide_options_tab[:class].should eq 'tab-off'
+
+				@page.submit_button.click
+				no_php_js_errors
+
+				@page.should have_alert
+
+				hide_options_tab = @page.hide_tab_3
+				hide_options_tab[:class].should eq 'tab-off'
+			end
+
+			it 'should be hidden when saved' do
+				hide_options_tab = @page.hide_tab_3
+
+				# Confirm the icon is for hiding
+				hide_options_tab[:class].should eq 'tab-on'
+
+				hide_options_tab.trigger('click')
+
+				# Confirm the icon is for showing
+				hide_options_tab[:class].should eq 'tab-off'
+
+				@page.layout_name.set 'Default'
+				@page.submit_button.click
+				no_php_js_errors
+
+				@page.edit(1)
+				hide_options_tab = @page.hide_tab_3
+				hide_options_tab[:class].should eq 'tab-off'
+			end
 		end
 	end
 
