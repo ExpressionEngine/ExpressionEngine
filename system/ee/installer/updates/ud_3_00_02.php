b0f0b3ae14d586ec7e60807e50553c4c3e9506a5
Seth Croston Barber <seth.barber@ellislab.com>
2015-10-29 09:39:45 -0700

    Updating some columns to allow null and to default to null.

--- system/ee/installer/updates/ud_3_00_02.php
+++ system/ee/installer/updates/ud_3_00_02.php
@@ -34,7 +34,47 @@ class Updater {
 	 */
 	public function do_update()
 	{
+		ee()->load->dbforge();
+
+		$steps = new ProgressIterator(
+			array(
+				'_update_member_field_schema',
+			)
+		);
+
+		foreach ($steps as $k => $v)
+		{
+			$this->$v();
+		}
+
 		return TRUE;
+	}
+
+	/**
+	 * Sets three columns to allow NULL and to default to NULL. This matches
+	 * the schema we install.
+	 */
+	private function _update_member_field_schema()
+	{
+		ee()->smartforge->modify_column('member_fields', array(
+			'm_field_maxl' => array(
+				'type' => 'smallint(3)',
+				'null' => TRUE,
+			),
+			'm_field_width' => array(
+				'type' => 'varchar(6)',
+				'null' => TRUE,
+			),
+			'm_field_order' => array(
+				'type' => 'int(3)',
+				'null' => TRUE,
+			),
+		));
+
+		foreach(array('m_field_maxl', 'm_field_width', 'm_field_order') as $col)
+		{
+			ee()->db->query("ALTER TABLE exp_member_fields ALTER COLUMN " . $col . " SET DEFAULT NULL");
+		}
 	}
 }
 /* END CLASS */
