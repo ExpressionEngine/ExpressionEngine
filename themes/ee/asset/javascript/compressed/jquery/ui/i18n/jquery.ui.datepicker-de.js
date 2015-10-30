25f73a703ea80e4b30bc8c56c34b857f96b7fa10
Wes Baker <wes.baker@ellislab.com>
2015-10-30 13:35:31 -0400

    Deleting compressed javascript files.

--- themes/ee/asset/javascript/compressed/jquery/ui/i18n/jquery.ui.datepicker-de.js
+++
@@ -1,7 +0,0 @@
-/* German initialisation for the jQuery UI date picker plugin. */
-/* Written by Milian Wolff (mail@milianw.de). */
-!function(e){"function"==typeof define&&define.amd?
-// AMD. Register as an anonymous module.
-define(["../datepicker"],e):
-// Browser globals
-e(jQuery.datepicker)}(function(e){return e.regional.de={closeText:"Schließen",prevText:"&#x3C;Zurück",nextText:"Vor&#x3E;",currentText:"Heute",monthNames:["Januar","Februar","März","April","Mai","Juni","Juli","August","September","Oktober","November","Dezember"],monthNamesShort:["Jan","Feb","Mär","Apr","Mai","Jun","Jul","Aug","Sep","Okt","Nov","Dez"],dayNames:["Sonntag","Montag","Dienstag","Mittwoch","Donnerstag","Freitag","Samstag"],dayNamesShort:["So","Mo","Di","Mi","Do","Fr","Sa"],dayNamesMin:["So","Mo","Di","Mi","Do","Fr","Sa"],weekHeader:"KW",dateFormat:"dd.mm.yy",firstDay:1,isRTL:!1,showMonthAfterYear:!1,yearSuffix:""},e.setDefaults(e.regional.de),e.regional.de});
--- themes/ee/asset/javascript/compressed/jquery/ui/i18n/jquery.ui.datepicker-sv.js
+++
@@ -1,7 +0,0 @@
-/* Swedish initialisation for the jQuery UI date picker plugin. */
-/* Written by Anders Ekdahl ( anders@nomadiz.se). */
-!function(e){"function"==typeof define&&define.amd?
-// AMD. Register as an anonymous module.
-define(["../datepicker"],e):
-// Browser globals
-e(jQuery.datepicker)}(function(e){return e.regional.sv={closeText:"Stäng",prevText:"&#xAB;Förra",nextText:"Nästa&#xBB;",currentText:"Idag",monthNames:["Januari","Februari","Mars","April","Maj","Juni","Juli","Augusti","September","Oktober","November","December"],monthNamesShort:["Jan","Feb","Mar","Apr","Maj","Jun","Jul","Aug","Sep","Okt","Nov","Dec"],dayNamesShort:["Sön","Mån","Tis","Ons","Tor","Fre","Lör"],dayNames:["Söndag","Måndag","Tisdag","Onsdag","Torsdag","Fredag","Lördag"],dayNamesMin:["Sö","Må","Ti","On","To","Fr","Lö"],weekHeader:"Ve",dateFormat:"yy-mm-dd",firstDay:1,isRTL:!1,showMonthAfterYear:!1,yearSuffix:""},e.setDefaults(e.regional.sv),e.regional.sv});
