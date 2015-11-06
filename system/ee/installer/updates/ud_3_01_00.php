72e34db9f6da27972dee9796ee181e7a565aaab7
Kevin Cupp <kevin.cupp@gmail.com>
2015-11-06 16:50:47 -0500

    Adding update routine to fix member field column names

--- system/ee/installer/updates/ud_3_01_00.php
+++ system/ee/installer/updates/ud_3_01_00.php
@@ -34,6 +34,47 @@ class Updater {
 	 */
 	public function do_update()
 	{
+		ee()->load->dbforge();
+
+		$steps = new ProgressIterator(
+			array(
+				'_update_member_data_column_names',
+			)
+		);
+
+		foreach ($steps as $k => $v)
+		{
+			$this->$v();
+		}
+
 		return TRUE;
 	}
+
+	/**
+	 * Fields created in 3.0 were missing the 'm_' prefix on their data columns,
+	 * so we need to add the prefix back
+	 */
+	private function _update_member_data_column_names()
+	{
+		$member_data_columns = ee()->db->list_fields('member_data');
+
+		$columns_to_modify = array();
+		foreach ($member_data_columns as $column)
+		{
+			if ($column == 'member_id' OR 						// Don't rename the primary key
+				substr($column, 0, 2) == 'm_' OR 				// or if it already has the prefix
+				in_array('m_'.$column, $member_data_columns)) 	// or if the prefixed column already exists (?!)
+			{
+				continue;
+			}
+
+			$columns_to_modify[$column] = array(
+				'name' => 'm_'.$column,
+				'type' => (strpos($column, 'field_ft_') !== FALSE) ? 'tinytext' : 'text'
+			);
+		}
+
+		ee()->smartforge->modify_column('member_data', $columns_to_modify);
+	}
 }
+// EOF
