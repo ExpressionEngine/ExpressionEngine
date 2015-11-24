eca65a9dd267a7158090e972b0bf19d69a7c2a3b
Wes Baker <wes.baker@ellislab.com>
2015-11-24 16:59:01 -0500

    Fixing some formatting issues with Filepicker module, also settling on normal casing for module_name.

--- system/ee/EllisLab/Addons/filepicker/mcp.filepicker.php
+++ system/ee/EllisLab/Addons/filepicker/mcp.filepicker.php
@@ -1,4 +1,28 @@
-<?php
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * ExpressionEngine - by EllisLab
+ *
+ * @package		ExpressionEngine
+ * @author		EllisLab Dev Team
+ * @copyright	Copyright (c) 2003 - 2015, EllisLab, Inc.
+ * @license		https://ellislab.com/expressionengine/user-guide/license.html
+ * @link		http://ellislab.com
+ * @since		Version 3.0
+ * @filesource
+ */
+
+// --------------------------------------------------------------------
+
+/**
+ * ExpressionEngine File Picker Module
+ *
+ * @package		ExpressionEngine
+ * @subpackage	Modules
+ * @category	Modules
+ * @author		EllisLab Dev Team
+ * @link		http://ellislab.com
+ */
 
 use EllisLab\ExpressionEngine\Model\File\UploadDestination;
 use EllisLab\Addons\FilePicker\FilePicker as Picker;
