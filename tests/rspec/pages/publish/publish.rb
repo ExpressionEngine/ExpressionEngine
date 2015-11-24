062bee84650e754287607f78a7f0a12f2616bbce
Wes Baker <wes.baker@ellislab.com>
2015-11-24 14:14:58 -0500

    Added automated tests for switching file modal view style

--- tests/rspec/pages/publish/publish.rb
+++ tests/rspec/pages/publish/publish.rb
@@ -1,7 +1,11 @@
 class Publish < ControlPanelPage
   element :title, 'input[name=title]'
   element :url_title, 'input[name=url_title]'
+
+  elements :file_fields, 'a.file-field-filepicker'
   elements :chosen_files, '.file-chosen img'
+
+  section :file_modal, FileModal, '.modal-file', visible: false
 
   def load
     visit '/system/index.php?/cp/publish/create/1'
--- tests/rspec/tests/publish/test_publish.rb
+++ tests/rspec/tests/publish/test_publish.rb
@@ -1,32 +1,58 @@
 require './bootstrap.rb'
 
 feature 'Publish Page' do
-  before(:each) do
+  before :each do
     cp_session
     @page = Publish.new
-    @page.load
     no_php_js_errors
   end
 
-  it 'properly assigns image data when using the filepicker modal in a channel with two file fields' do
-    channel_field_form = ChannelFieldForm.new
-    channel_field_form.create_field(
-      group_id: 1,
-      type: 'File',
-      label: 'Second File'
-    )
+  context 'when using file fields' do
+    before :each do
+      channel_field_form = ChannelFieldForm.new
+      channel_field_form.create_field(
+        group_id: 1,
+        type: 'File',
+        label: 'Second File',
+        fields: { allowed_directories: 'About (Agile Records)' }
+      )
 
-    @page.load
-    @page.has_title?
-    @page.has_url_title?
-
-    all('a.btn.file-field-filepicker').each do |link|
-      link.click
-      @page.wait_until_modal_visible
-      find('.modal-wrap table.file-list tbody tr:first-child').click
-      @page.wait_until_modal_invisible
+      @page.load
+      @page.has_title?
+      @page.has_url_title?
     end
 
-    @page.chosen_files.should have_at_least(2).items
+    it 'the file field properly assigns image data when using the filepicker modal in a channel with two file fields' do
+      @page.file_fields.each do |link|
+        link.click
+        @page.wait_until_modal_visible
+        @page.file_modal.wait_for_filters
+
+        @page.file_modal.files[0].click
+
+        @page.wait_until_modal_invisible
+      end
+
+      @page.chosen_files.should have_at_least(2).items
+    end
+
+    it 'the file field restricts you to the chosen directory' do
+      @page.file_fields[1].click
+
+      @page.wait_until_modal_visible
+      @page.file_modal.wait_for_filters
+
+      @page.file_modal.filters.should have(2).items
+      @page.file_modal.title.should_not == 'All Files'
+      @page.file_modal.has_upload_button?
+
+      @page.file_modal.filters[-1].click
+      @page.file_modal.view_filters[0].click
+
+      @page.file_modal.wait_for_filters
+      @page.file_modal.filters.should have(2).items
+      @page.file_modal.title.should_not == 'All Files'
+      @page.file_modal.has_upload_button?
+    end
   end
 end
