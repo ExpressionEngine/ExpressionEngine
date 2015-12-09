369ec8b5f88672a3fbe6bd7e2190ea8fed2f73e3
Robin Sowell <robin.sowell@ellislab.com>
2015-12-09 11:01:00 -0500

    Fixed a bug where forum previews did not fall back to using the default index template if running the forums through the templates.

--- system/ee/EllisLab/Addons/forum/mod.forum_core.php
+++ system/ee/EllisLab/Addons/forum/mod.forum_core.php
@@ -5007,7 +5007,7 @@ class Forum_Core extends Forum {
 			// so we need to store the submission errors that will be needed for display
 			ee()->session->cache['forum']['submission_error'] = $this->submission_error;
 
-			ee()->TMPL->run_template_engine($x['0'], (( ! isset($x['1'])) ? 'index' : $x['1']));
+			ee()->TMPL->run_template_engine();
 
 			return;
 		}
