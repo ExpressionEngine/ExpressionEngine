4ffc6a1c3f18ab8f232b41a696cf9edf2e92906d
Seth Croston Barber <seth.barber@ellislab.com>
2015-12-10 14:41:21 -0800

    Adding fieldtype compatibily metadata to our fieldtypes.

--- system/ee/EllisLab/Addons/rte/addon.setup.php
+++ system/ee/EllisLab/Addons/rte/addon.setup.php
@@ -1,14 +1,19 @@
 <?php
 
 return array(
-	'author'      => 'EllisLab',
-	'author_url'  => 'https://ellislab.com/',
-	'name'        => 'Rich Text Editor',
-	'description' => '',
-	'version'     => '1.0.1',
-	'namespace'   => 'EllisLab\Addons\Rte',
+	'author'         => 'EllisLab',
+	'author_url'     => 'https://ellislab.com/',
+	'name'           => 'Rich Text Editor',
+	'description'    => '',
+	'version'        => '1.0.1',
+	'namespace'      => 'EllisLab\Addons\Rte',
 	'settings_exist' => TRUE,
-	'docs_url'    => 'https://ellislab.com/expressionengine/user-guide/modules/rte/index.html',
+	'docs_url'       => 'https://ellislab.com/expressionengine/user-guide/modules/rte/index.html',
+	'fieldtypes'     => array(
+		'rte' => array(
+			'compatibility' => 'text'
+		)
+	),
 
 	'models' => array(
 		'Tool' => 'Model\Tool',
--- system/ee/EllisLab/Addons/text/addon.setup.php
+++ system/ee/EllisLab/Addons/text/addon.setup.php
@@ -8,5 +8,10 @@ return array(
 	'version'        => '1.0.0',
 	'namespace'      => 'EllisLab\Addons\TextInput',
 	'settings_exist' => FALSE,
-	'built_in'       => TRUE
+	'built_in'       => TRUE,
+	'fieldtypes'     => array(
+		'text' => array(
+			'compatibility' => 'text'
+		)
+	)
 );
