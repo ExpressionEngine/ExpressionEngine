cf5b8b1638da4f7d8fcf53fb9dd0b25323699aab
Pascal Kriete <mail@pascal.io>
2015-12-14 21:52:05 -0500

    Make sure global variables are loaded on the frontend

--- system/ee/legacy/libraries/Template.php
+++ system/ee/legacy/libraries/Template.php
@@ -2895,16 +2895,11 @@ class EE_Template {
 
 		//  Parse User-defined Global Variables first so that
 		//  they can use other standard globals
-		ee()->db->select('variable_name, variable_data');
-		ee()->db->where('site_id', ee()->config->item('site_id'));
-		$query = ee()->db->get('global_variables');
+		$variables = ee('Model')->make('GlobalVariable')->loadAll();
 
-		if ($query->num_rows() > 0)
+		foreach ($variables as $var)
 		{
-			foreach ($query->result_array() as $row)
-			{
-				$str = str_replace(LD.$row['variable_name'].RD, $row['variable_data'], $str);
-			}
+			$str = str_replace(LD.$var->variable_name.RD, $var->variable_data, $str);
 		}
 
 		// {hits}
