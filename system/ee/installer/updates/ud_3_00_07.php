77df3133f78430a8007a39b04989c6e80d027cda
Wes Baker <wes.baker@ellislab.com>
2015-12-30 17:18:40 -0500

    Update to fix the default member listing

---
+++ system/ee/installer/updates/ud_3_00_07.php
@@ -0,0 +1,69 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * ExpressionEngine - by EllisLab
+ *
+ * @package		ExpressionEngine
+ * @author		EllisLab Dev Team
+ * @copyright	Copyright (c) 2003 - 2015, EllisLab, Inc.
+ * @license		https://ellislab.com/expressionengine/user-guide/license.html
+ * @link		http://ellislab.com
+ * @since		Version 3.0.7
+ * @filesource
+ */
+
+// ------------------------------------------------------------------------
+
+/**
+ * ExpressionEngine Update Class
+ *
+ * @package		ExpressionEngine
+ * @subpackage	Core
+ * @category	Core
+ * @author		EllisLab Dev Team
+ * @link		http://ellislab.com
+ */
+class Updater {
+
+	var $version_suffix = '';
+
+	/**
+	 * Do Update
+	 *
+	 * @return TRUE
+	 */
+	public function do_update()
+	{
+		ee()->load->dbforge();
+
+		$steps = new ProgressIterator(
+			array(
+				'update_memberlist_order_by',
+			)
+		);
+
+		foreach ($steps as $k => $v)
+		{
+			$this->$v();
+		}
+
+		return TRUE;
+	}
+
+	// -------------------------------------------------------------------------
+
+	/**
+	 * Set the memberlist order_by to the new default
+	 *
+	 * @return void
+	 */
+	private function update_memberlist_order_by()
+	{
+		$msm_config = new MSM_Config();
+		$msm_config->update_site_prefs(array('memberlist_order_by' => 'id'));
+	}
+}
+/* END CLASS */
+
+/* End of file ud_3_00_07.php */
+/* Location: ./system/expressionengine/installer/updates/ud_3_00_07.php */
