53ea6a60f7aabc44983e1f6e5079606ae76a0073
Wes Baker <wes.baker@ellislab.com>
2016-01-08 11:20:31 -0500

    More tests for the forum tab.

--- tests/rspec/tests/publish/test_forum_tab.rb
+++ tests/rspec/tests/publish/test_forum_tab.rb
@@ -1,6 +1,9 @@
 require './bootstrap.rb'
 
 feature 'Forum Tab' do
+  let(:title) { 'Forum Tab Test' }
+  let(:body) { 'Lorem ipsum dolor sit amet...' }
+
   before :each do
     cp_session
     @page = Publish.new
@@ -18,19 +21,8 @@ feature 'Forum Tab' do
     @page.forum_tab.should have_forum_topic_id
   end
 
-  it 'associates a channel entry with a forum post when specifying a forum topic ID'
   it 'creates a forum post when entering data into the forum tab' do
-    title = 'Forum Tab Test'
-    body = 'Test content. Lorem ipsum dolor sit amet...'
-
-    @page.title.set title
-    @page.tab_links[4].click
-    @page.forum_tab.forum_title.set title
-    @page.forum_tab.forum_body.set body
-    @page.submit
-
-    @page.all_there?.should == false
-    @page.alert.has_content?("The entry #{title} has been created.").should == true
+    create_entry
 
     $db.query('SELECT title, body FROM exp_forum_topics').each do |row|
       row['title'].should == title
@@ -42,7 +34,44 @@ feature 'Forum Tab' do
     end
   end
 
-  it 'invalidates an entry with both new post content and a forum topic ID'
+  it 'associates a channel entry with a forum post when specifying a forum topic ID' do
+    first_entry = nil
+
+    create_entry
+
+    $db.query('SELECT entry_id, count(entry_id) as count FROM exp_channel_titles WHERE forum_topic_id = 1').each do |row|
+      row['count'].should == 1
+      first_entry = row['entry_id']
+    end
+
+    @page.load
+    @page.title.set 'Second Forum Tab Test'
+    @page.tab_links[4].click
+    @page.forum_tab.forum_topic_id.set 1
+    @page.submit
+
+    $db.query('SELECT count(entry_id) as count FROM exp_channel_titles WHERE forum_topic_id = 1').each do |row|
+      row['count'].should == 1
+      row['entry_id'].should_not == first_entry
+    end
+  end
+
+  it 'invalidates an entry with both new post content and a forum topic ID' do
+    create_entry
+
+    @page.load
+    @page.tab_links[4].click
+    @page.forum_tab.forum_title.set 'Something'
+    @page.forum_tab.forum_body.set 'Lorem ipsum dolor sit amet...'
+    @page.forum_tab.forum_topic_id.set '1'
+    @page.forum_tab.forum_topic_id.trigger 'blur'
+    @page.wait_for_error_message_count(1)
+    should_have_form_errors(@page)
+    should_have_error_text(
+      @page.forum_tab.forum_topic_id,
+      'Do not specify a forum Title or Body when setting a Forum Topic ID.'
+    )
+  end
 
   it 'invalidates an entry with an invalid forum topic ID' do
     @page.tab_links[4].click
@@ -60,5 +89,16 @@ feature 'Forum Tab' do
     @page.wait_for_error_message_count(0)
     should_have_no_form_errors(@page)
     should_have_no_error_text(@page.forum_tab.forum_topic_id)
+  end
+
+  def create_entry
+    @page.title.set title
+    @page.tab_links[4].click
+    @page.forum_tab.forum_title.set title
+    @page.forum_tab.forum_body.set body
+    @page.submit
+
+    @page.all_there?.should == false
+    @page.alert.has_content?("The entry #{title} has been created.").should == true
   end
 end
