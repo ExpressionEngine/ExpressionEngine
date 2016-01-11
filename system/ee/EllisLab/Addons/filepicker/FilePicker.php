6788cd14f96df6d13d63302e9725098c43712ead
Seth Croston Barber <seth.barber@ellislab.com>
2016-01-11 12:37:56 -0800

    Using the DI so that localize can be injected.

--- system/ee/EllisLab/Addons/filepicker/FilePicker.php
+++ system/ee/EllisLab/Addons/filepicker/FilePicker.php
@@ -88,7 +88,7 @@ class FilePicker {
 
 	public function buildTableFromFileCollection(Collection $files, $limit = 20, $selected = NULL)
 	{
-		$table = Table::fromGlobals(array(
+		$table = ee('CP/Table', array(
 			'autosort' => TRUE,
 			'limit' => $limit,
 			'class' => 'file-list'
