b17564018cc5158aed5b330bc09c716255319c96
Seth Croston Barber <seth.barber@ellislab.com>
2016-02-05 13:25:03 -0800

    Changing the name to "Toggle (On/Off)"

--- system/ee/EllisLab/Addons/boolean/addon.setup.php
+++ system/ee/EllisLab/Addons/boolean/addon.setup.php
@@ -3,8 +3,8 @@
 return array(
 	'author'         => 'EllisLab',
 	'author_url'     => 'https://ellislab.com/',
-	'name'           => 'Boolean (Yes/No)',
-	'description'    => 'Yes/No or On/Off Buttons',
+	'name'           => 'Toggle (On/Off)',
+	'description'    => 'On/Off Toggle Switch',
 	'version'        => '1.0.0',
 	'namespace'      => 'EllisLab\Addons\BooleanField',
 	'settings_exist' => FALSE,
