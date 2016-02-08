831b190ace245dbda6de0bf35c613fd20c033965
Seth Croston Barber <seth.barber@ellislab.com>
2016-02-08 08:47:52 -0800

    Various code cleanup bits.

--- system/ee/EllisLab/Addons/boolean/ft.boolean.php
+++ system/ee/EllisLab/Addons/boolean/ft.boolean.php
@@ -7,7 +7,7 @@
  * @copyright	Copyright (c) 2003 - 2016, EllisLab, Inc.
  * @license		https://expressionengine.com/license
  * @link		https://ellislab.com
- * @since		Version 2.0
+ * @since		Version 3.2.0
  * @filesource
  */
 
@@ -24,22 +24,18 @@
  */
 class Boolean_ft extends EE_Fieldtype {
 
-	var $info = array(
-		'name'		=> 'Toggle (On/Off)',
-		'version'	=> '1.0.0'
-	);
+	var $info = array();
 
 	var $has_array_data = FALSE;
 
-	// used in display_field() below to set
-	// some defaults for third party usage
-	var $settings_vars = array(
-		'field_text_direction'	=> 'rtl',
-		'field_pre_populate'	=> 'n',
-		'field_list_items'		=> array(),
-		'field_pre_field_id'	=> '',
-		'field_pre_channel_id'	=> ''
-	);
+	public function __construct()
+	{
+		$addon = ee('Addon')->get('toggle');
+		$this->info = array(
+			'name'    => $addon->getName(),
+			'version' => $addon->getVersion()
+		);
+	}
 
 	function validate($data)
 	{
@@ -72,13 +68,6 @@ class Boolean_ft extends EE_Fieldtype {
 
 	private function _display_field($data, $container = 'fieldset')
 	{
-		array_merge($this->settings, $this->settings_vars);
-
-		$text_direction = (isset($this->settings['field_text_direction']))
-			? $this->settings['field_text_direction'] : 'ltr';
-
-		$extra = ($this->get_setting('field_disabled')) ? 'disabled' : '';
-
 		if (REQ == 'CP')
 		{
 			ee()->cp->add_js_script(array(
@@ -90,7 +79,7 @@ class Boolean_ft extends EE_Fieldtype {
 			return ee('View')->make('boolean:publish')->render(array(
 				'field_name' => $this->field_name,
 				'selected'   => $data,
-				'extra'      => $extra
+				'disabled'   => $this->get_setting('field_disabled')
 			));
 		}
 
@@ -99,28 +88,28 @@ class Boolean_ft extends EE_Fieldtype {
 			lang('off') => 0
 		);
 
-		$r = '';
+		$html = '';
 		$class = 'choice mr';
 
 		foreach($field_options as $key => $value)
 		{
 			$selected = ($value == $data);
 
-			$r .= '<label>'.form_radio($this->field_name, $value, $selected, $extra).NBS.$key.'</label>';
+			$html .= '<label>'.form_radio($this->field_name, $value, $selected, $extra).NBS.$key.'</label>';
 		}
 
 		switch ($container)
 		{
 			case 'grid':
-				$r = $this->grid_padding_container($r);
+				$html = $this->grid_padding_container($html);
 				break;
 
 			default:
-				$r = form_fieldset('').$r.form_fieldset_close();
+				$html = form_fieldset('').$html.form_fieldset_close();
 				break;
 		}
 
-		return $r;
+		return $html;
 	}
 
 	// -------------------------------
