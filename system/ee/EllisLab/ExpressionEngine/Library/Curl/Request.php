3a8e9d2ba654b9de0d5f2b94d520a81029249fa9
Kevin Cupp <kevin.cupp@gmail.com>
2016-02-16 16:43:18 -0500

    Adding ability to access headers from a response in Curl service

--- system/ee/EllisLab/ExpressionEngine/Library/Curl/Request.php
+++ system/ee/EllisLab/ExpressionEngine/Library/Curl/Request.php
@@ -9,6 +9,7 @@ abstract class Request {
 		$this->config = array(
 			CURLOPT_URL => $url,
     		CURLOPT_RETURNTRANSFER => 1,
+    		CURLOPT_HEADER => 1,
 		);
 
 		foreach ($data as $key => $val)
@@ -25,10 +26,16 @@ abstract class Request {
 		}
 	}
 
-	public function exec() {
+	public function exec()
+	{
 		$curl = curl_init();
 		curl_setopt_array($curl, $this->config);
-		$data = curl_exec($curl);
+
+		$response = curl_exec($curl);
+		list($headers, $data) = explode("\r\n\r\n", $response, 2);
+
+		$this->setHeaders($headers, $curl);
+
 		curl_close($curl);
 
 		if ( ! empty($this->callback))
@@ -42,6 +49,46 @@ abstract class Request {
 	public function callback($data)
 	{
 		return $data;
+	}
+
+	/**
+	 * Given a string of headers from a cURL response, creates an associative array
+	 * class property of the headers
+	 *
+	 * @param	string		$headers	String output of headers from cURL
+	 * @param	resource	$curl		Current cURL resource
+	 */
+	protected function setHeaders($headers, $curl)
+	{
+		$this->headers = curl_getinfo($curl);
+
+		foreach (explode("\r\n", $headers) as $i => $line)
+		{
+			if ($i === 0)
+			{
+				continue;
+			}
+
+			list($key, $value) = explode(': ', $line);
+
+			$this->headers[$key] = $value;
+		}
+	}
+
+	/**
+	 * Returns the requested header value
+	 *
+	 * @param	string	$key	Header to return value for
+	 * @return	string	Value of header, or FALSE if header not found
+	 */
+	public function getHeader($key)
+	{
+		if (isset($this->headers[$key]))
+		{
+			return $this->headers[$key];
+		}
+
+		return FALSE;
 	}
 
 }
