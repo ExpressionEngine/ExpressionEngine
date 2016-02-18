55b1f4d14f59fbfb98a731a287cc80d908ba9e5c
Pascal Kriete <mail@pascal.io>
2016-02-18 17:24:41 -0500

    Field deletions need to process closures last to fix Robin's issue. I'm not convinced this will work for the whole model graph, a better solution may be to not process the belongsTo if it is to be deleted, but it's worth a shot.

--- system/ee/EllisLab/ExpressionEngine/Service/Model/Query/Delete.php
+++ system/ee/EllisLab/ExpressionEngine/Service/Model/Query/Delete.php
@@ -211,12 +211,12 @@ class Delete extends Query {
 
 			if ($a[1] instanceOf \Closure)
 			{
-				return 5e10;
+				return -5e10;
 			}
 
 			if ($b[1] instanceOf \Closure)
 			{
-				return -5e10;
+				return 5e10;
 			}
 
 			return count($a[1]) - count($b[1]);
