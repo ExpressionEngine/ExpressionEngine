36f44225680ce5472c20dafbd5025e7737f11d4c
Pascal Kriete <mail@pascal.io>
2016-03-01 10:26:19 -0500

    Docblocks and return type consistency for the variable column model

--- system/ee/EllisLab/ExpressionEngine/Service/Model/VariableColumnModel.php
+++ system/ee/EllisLab/ExpressionEngine/Service/Model/VariableColumnModel.php
@@ -27,13 +27,30 @@ namespace EllisLab\ExpressionEngine\Service\Model;
  */
 class VariableColumnModel extends Model {
 
+	/**
+	 * @var Array Dictionary of values that have been set that were not explicitly
+	 *            defined in the subclass.
+	 */
 	protected $_variable_values = array();
 
+	/**
+	 * Check if a property exists. By default, variable name properties are any
+	 * that don't start with an underscore.
+	 *
+	 * @param String $name Property name
+	 * @return bool Class has property?
+	 */
 	public function hasProperty($name)
 	{
 		return ($name !== '' && $name[0] != '_');
 	}
 
+	/**
+	 * Get property without passing through a getter
+	 *
+	 * @param String $name Property name
+	 * @return Mixed Property value
+	 */
 	public function getRawProperty($name)
 	{
 		if (parent::hasProperty($name))
@@ -44,6 +61,13 @@ class VariableColumnModel extends Model {
 		return $this->getVariableValue($name);
 	}
 
+	/**
+	 * Set property without passing through a setter
+	 *
+	 * @param String $name Property name
+	 * @param Mixed $value Property value
+	 * @return $this
+	 */
 	public function setRawProperty($name, $value)
 	{
 		if (parent::hasProperty($name))
@@ -54,13 +78,26 @@ class VariableColumnModel extends Model {
 		$this->backupIfChanging($name, $this->getVariableValue($name), $value);
 
 		$this->_variable_values[$name] = $value;
+
+		return $this;
 	}
 
+	/**
+	 * Get values as an array
+	 *
+	 * @return Array of values
+	 */
 	public function getValues()
 	{
 		return array_merge(parent::getValues(), $this->_variable_values);
 	}
 
+	/**
+	 * Helper method to get the value of an undeclared column.
+	 *
+	 * @return String $name Name of the column
+	 * @return Mixed Value of the column
+	 */
 	protected function getVariableValue($name)
 	{
 		if ( ! array_key_exists($name, $this->_variable_values))
