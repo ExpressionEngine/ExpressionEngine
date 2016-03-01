20e46761dcfe1f6f1805a22de662b1482c784241
Wes Baker <wes.baker@ellislab.com>
2016-03-01 17:08:59 -0500

    Fixed a bug (#21577) where the RTE would grow when switching from WYSIWYG to Source View.

--- themes/ee/asset/javascript/src/jquery/plugins/wysihat.js
+++ themes/ee/asset/javascript/src/jquery/plugins/wysihat.js
@@ -214,11 +214,11 @@ WysiHat.Editor.prototype = {
 				{
 					if (that.$field.is(':visible'))
 					{
-						that.$editor.height(that.$field.outerHeight());
+						that.$editor.outerHeight(that.$field.outerHeight());
 					}
 					else if (that.$editor.is(':visible'))
 					{
-						that.$field.height(that.$editor.outerHeight());
+						that.$field.outerHeight(that.$editor.outerHeight());
 					}
 				}
 			})
