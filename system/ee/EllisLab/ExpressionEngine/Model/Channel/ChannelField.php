695005ee365e1e0fd0e23e5b2748a91d96be77ff
Seth Croston Barber <seth.barber@ellislab.com>
2016-03-03 15:22:54 -0800

    Fixed bugs related to layouts and deleting custom fields.

--- system/ee/EllisLab/ExpressionEngine/Model/Channel/ChannelField.php
+++ system/ee/EllisLab/ExpressionEngine/Model/Channel/ChannelField.php
@@ -189,7 +189,7 @@ class ChannelField extends FieldModel {
 					{
 						if ($field_info['field'] == 'field_id_' . $this->field_id)
 						{
-							unset($field_layout[$i]['fields'][$j]);
+							array_splice($field_layout[$i]['fields'], $j, 1);
 							break 2;
 						}
 					}
