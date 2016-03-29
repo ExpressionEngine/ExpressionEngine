d59badb908e3eaaa39606ae0a8e83e815cbd816a
Wes Baker <wes.baker@ellislab.com>
2016-03-29 14:52:59 -0400

    Correcting the [code] output

--- system/ee/legacy/libraries/Typography.php
+++ system/ee/legacy/libraries/Typography.php
@@ -1639,7 +1639,7 @@ class EE_Typography {
 		{
 			foreach ($this->code_chunks as $key => $val)
 			{
-				$str = str_replace('[div class="codeblock"]{'.$key.'yH45k02wsSdrp}[/div]', '<div class="codeblock"><pre>{'.$key.'yH45k02wsSdrp}</pre></div>', $str);
+				$str = str_replace('[div class="codeblock"]{'.$key.'yH45k02wsSdrp}[/div]', '<div class="codeblock">{'.$key.'yH45k02wsSdrp}</div>', $str);
 			}
 		}
 
@@ -2132,7 +2132,11 @@ while (--j >= 0)
 			}
 			else
 			{
-				$str = str_replace(array('[code]', '[/code]'),	array('<code>', '</code>'),	$str);
+				$str = str_replace(
+					array('[code]', '[/code]'),
+					array('<pre><code>', '</code></pre>'),
+					$str
+				);
 			}
 		}
 
