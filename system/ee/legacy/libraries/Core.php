8f84562c462d0cff89010b4a29643bf30a75a386
Wes Baker <wes.baker@ellislab.com>
2016-03-31 17:30:10 -0400

    Version bump for 3.3.0 for testing.

--- system/ee/legacy/libraries/Core.php
+++ system/ee/legacy/libraries/Core.php
@@ -82,7 +82,7 @@ class EE_Core {
 		// application constants
 		define('IS_CORE',		FALSE);
 		define('APP_NAME',		'ExpressionEngine'.(IS_CORE ? ' Core' : ''));
-		define('APP_BUILD',		'20160318');
+		define('APP_BUILD',		'20160331');
 		define('APP_VER',		'3.3.0');
 		define('APP_VER_ID',	'');
 		define('SLASH',			'&#47;');
