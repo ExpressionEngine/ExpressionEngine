205c111086038c9d85e378fd6ec4bf774e6a830b
Wes Baker <wes.baker@ellislab.com>
2016-04-06 14:53:03 -0400

    Adding empty PHPUnit tests so @scbarber and I don't clash.

---
+++ system/ee/EllisLab/Tests/ExpressionEngine/Service/ChannelSet/ExportTest.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace EllisLab\Tests\ExpressionEngine\Service\ChannelSet;
+
+class ExportTest extends \PHPUnit_Framework_TestCase {
+
+}
---
+++ system/ee/EllisLab/Tests/ExpressionEngine/Service/ChannelSet/Set.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace EllisLab\Tests\ExpressionEngine\Service\ChannelSet;
+
+class SetTest extends \PHPUnit_Framework_TestCase {
+
+}
