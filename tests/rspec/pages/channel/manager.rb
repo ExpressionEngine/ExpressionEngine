4a6728e589b8340e4efc0f0efcef41378ad59163
Wes Baker <wes.baker@ellislab.com>
2016-04-07 13:25:54 -0400

    Adding method to get channel ID from channel title/name.
    [ci skip]

--- tests/rspec/pages/channel/manager.rb
+++ tests/rspec/pages/channel/manager.rb
@@ -7,6 +7,19 @@ class ChannelManager < ControlPanelPage
   elements :channel_titles, 'table tr td:nth-child(2)'
   elements :channel_names, 'table tr td:nth-child(3)'
 
+  # Get a channel ID from a channel name or title
+  #
+  # @param [String] name The channel name/title to look for
+  # @raise [RuntimeError] if the channel name does not exist
+  # @return [Integer] The channel's ID
+  def get_channel_id_from_name(name)
+    channels.each do |element|
+      return element.find('td:nth-child(1)').text.to_i if element.text.downcase.include? name.downcase
+    end
+
+    raise 'No known channel'
+  end
+
   def load
     self.open_dev_menu
     click_link 'Channel Manager'
