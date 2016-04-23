a6cb09a7523ad11032a04d9c119ab08fceb10719
Pascal Kriete <mail@pascal.io>
2016-04-22 22:48:22 -0400

    Test error counts didn't match what was happening in the browser, spun through the entire 5s timeout several times without failing.
    Clicking through, the only way this works out is to set the password
    correctly.

--- tests/rspec/pages/cp_page.rb
+++ tests/rspec/pages/cp_page.rb
@@ -83,5 +83,10 @@ class ControlPanelPage < SitePrism::Page
       sleep 0.01
       i += 1 # Prevent infinite loop
     end
+
+    # Element is still there after our timeout? No good.
+    if element_count != count && i == (seconds * 100)
+      raise StandardError, "Error message did not disappear"
+    end
   end
 end
