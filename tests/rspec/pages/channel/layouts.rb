daaf3a6affb8b952eaf6a393b5554118b4acff44
Wes Baker <wes@wesbaker.com>
2016-05-05 16:41:33 -0400

    The visible: false parameter on element(s) does nothing

--- tests/rspec/pages/channel/layouts.rb
+++ tests/rspec/pages/channel/layouts.rb
@@ -6,8 +6,8 @@ class ChannelLayouts < ChannelMangerPage
   element :create_new_button, 'div.col.w-12 div.box form fieldset.tbl-search.right a.action'
 
   element :perpage_filter, 'div.col.w-12 div.box form h1 + div.filters ul li:first-child'
-  element :perpage_filter_menu, 'div.col.w-12 div.box form h1 + div.filters ul li:first-child div.sub-menu ul', visible: false
-  element :perpage_manual_filter, 'input[name="perpage"]', visible: false
+  element :perpage_filter_menu, 'div.col.w-12 div.box form h1 + div.filters ul li:first-child div.sub-menu ul'
+  element :perpage_manual_filter, 'input[name="perpage"]'
 
   # Main box's table elements
   element :name_header, 'div.box form div.tbl-wrap table tr th:first-child'
@@ -19,13 +19,13 @@ class ChannelLayouts < ChannelMangerPage
   elements :member_groups, 'div.box form div.tbl-wrap table tr td:nth-child(2)'
   elements :manage_actions, 'div.box form div.tbl-wrap table tr td:nth-child(3)'
 
-  element :bulk_action, 'form fieldset.tbl-bulk-act select[name="bulk_action"]', visible: false
+  element :bulk_action, 'form fieldset.tbl-bulk-act select[name="bulk_action"]'
   element :action_submit_button, 'form fieldset.tbl-bulk-act button.submit'
 
   element :no_results, 'tr.no-results'
 
-  element :remove_layout_modal, 'div.modal-confirm-remove', visible: false
-  element :remove_layout_modal_submit_button, 'div.modal-confirm-remove .form-ctrls input.btn', visible: false
+  element :remove_layout_modal, 'div.modal-confirm-remove'
+  element :remove_layout_modal_submit_button, 'div.modal-confirm-remove .form-ctrls input.btn'
 
   def load_layouts_for_channel(number)
     self.open_dev_menu
--- tests/rspec/pages/members/_groups_list.rb
+++ tests/rspec/pages/members/_groups_list.rb
@@ -1,6 +1,6 @@
 class MemberGroupsList < SitePrism::Section
-  element :batch_actions, 'select[name=bulk_action]', visible: false
-  element :batch_submit, '.tbl-bulk-act button', visible: false
+  element :batch_actions, 'select[name=bulk_action]'
+  element :batch_submit, '.tbl-bulk-act button'
   element :table, 'table'
   elements :groups, 'form table tbody tr'
 end
