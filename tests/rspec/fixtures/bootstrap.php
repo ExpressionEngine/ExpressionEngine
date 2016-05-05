d532e28ee4d5d11714a3e9fa915a325fcb48c26f
Wes Baker <wes@wesbaker.com>
2016-05-05 16:41:47 -0400

    Copied over new config stuff for rspec fixtures bootstrap.

--- tests/rspec/fixtures/bootstrap.php
+++ tests/rspec/fixtures/bootstrap.php
@@ -56,9 +56,32 @@ function &get_config($replace = array())
 		}
 	}
 
+	// Merge in defaults
+	$config = array_merge(default_config_items(), $config);
+
 	$_config[0] =& $config;
 
 	return $_config[0];
+}
+
+function default_config_items()
+{
+	return array(
+		'allow_extensions'   => 'y',
+		'cache_driver'       => 'file',
+		'cache_path'         => '',
+		'charset'            => 'UTF-8',
+		'cookie_httponly'    => 'y',
+		'debug'              => 1,
+		'doc_url'            => 'https://docs.expressionengine.com/v3/',
+		'encryption_key'     => '',
+		'is_system_on'       => 'y',
+		'log_date_format'    => 'Y-m-d H:i:s',
+		'log_threshold'      => 0,
+		'rewrite_short_tags' => TRUE,
+		'subclass_prefix'    => 'EE_',
+		'uri_protocol'       => 'AUTO',
+	);
 }
 
 function config_item($item)
