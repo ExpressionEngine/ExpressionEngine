e42b0d1bb6364069f7e7df36ca19fa59624d1d73
Wes Baker <wes@wesbaker.com>
2016-05-06 11:45:59 -0400

    Fixed automated tests since :visible => false is gone.

--- tests/rspec/pages/members/_groups_list.rb
+++ tests/rspec/pages/members/_groups_list.rb
@@ -1,6 +1,6 @@
 class MemberGroupsList < SitePrism::Section
   element :batch_actions, 'select[name=bulk_action]'
   element :batch_submit, '.tbl-bulk-act button'
-  element :table, 'table'
+  element :groups_table, 'table'
   elements :groups, 'form table tbody tr'
 end
