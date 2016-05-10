9c3a2fe139c8ee3d5e703fadc6cbe8499b2979db
Wes Baker <wes@wesbaker.com>
2016-05-10 11:47:23 -0400

    Bumping versions for 3.3.1
    [ci skip]

--- system/ee/legacy/libraries/Core.php
+++ system/ee/legacy/libraries/Core.php
@@ -82,8 +82,8 @@ class EE_Core {
 		// application constants
 		define('IS_CORE',		FALSE);
 		define('APP_NAME',		'ExpressionEngine'.(IS_CORE ? ' Core' : ''));
-		define('APP_BUILD',		'20160419');
-		define('APP_VER',		'3.3.0');
+		define('APP_BUILD',		'20160510');
+		define('APP_VER',		'3.3.1');
 		define('APP_VER_ID',	'');
 		define('SLASH',			'&#47;');
 		define('LD',			'{');
