13f16f1338f55232ca2688682697f055657ec6a7
Kevin Cupp <kevin.cupp@gmail.com>
2016-05-23 16:49:24 -0400

    A start on file updater unit tests

--- system/ee/installer/updater/EllisLab/Tests/ExpressionEngine/Service/Updater/FileUpdaterTest.php
+++ system/ee/installer/updater/EllisLab/Tests/ExpressionEngine/Service/Updater/FileUpdaterTest.php
@@ -7,8 +7,28 @@ use Mockery;
 
 class FileUpdaterTest extends \PHPUnit_Framework_TestCase {
 
-	public function testSomething()
+	public function setUp()
 	{
-		// Hey, test something here
+		$this->filesystem = Mockery::mock('EllisLab\ExpressionEngine\Library\Filesystem\Filesystem');
+		$this->config = Mockery::mock('EllisLab\ExpressionEngine\Service\Config\File');
+		$this->verifier = Mockery::mock('EllisLab\ExpressionEngine\Service\Updater\Verifier');
+		$this->logger = Mockery::mock('EllisLab\ExpressionEngine\Service\Updater\Logger');
+
+		$this->logger->shouldReceive('log');
+
+		$this->fileupdater = new Downloader($this->license_number, $this->payload_url, $this->curl, $this->filesystem, $this->zip_archive, $this->config, $this->verifier, $this->logger, $this->requirements, $this->sites);
+	}
+
+	public function testBackup()
+	{
+
+	}
+
+	protected function move($source, $destination, Array $exclusions = [], $copy = FALSE)
+	{
+		$this->filesystem->shouldReceive('exists')->with($destination)->andReturn(TRUE);
+		$this->filesystem->shouldReceive('isDir')->with($destination)->andReturn(TRUE);
+
+		$this->filesystem->shouldReceive('getDirectoryContents')->with($source)->andReturn([]);
 	}
 }
