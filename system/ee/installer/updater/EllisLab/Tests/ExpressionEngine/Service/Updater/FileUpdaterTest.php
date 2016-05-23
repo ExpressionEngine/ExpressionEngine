7c7206dca75c9c90175be9a97360b4b0c9e9e1b6
Kevin Cupp <kevin.cupp@gmail.com>
2016-05-23 16:08:45 -0400

    Adding PHPUnit stuff to updater micro app

---
+++ system/ee/installer/updater/EllisLab/Tests/ExpressionEngine/Service/Updater/FileUpdaterTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace EllisLab\Tests\ExpressionEngine\Service\Updater;
+
+use EllisLab\ExpressionEngine\Service\Updater\FileUpdater;
+use Mockery;
+
+class FileUpdaterTest extends \PHPUnit_Framework_TestCase {
+
+	public function testSomething()
+	{
+		// Hey, test something here
+	}
+}
