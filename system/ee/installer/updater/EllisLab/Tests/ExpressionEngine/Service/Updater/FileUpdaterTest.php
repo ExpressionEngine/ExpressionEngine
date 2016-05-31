75d4b339c6fa6df84294ea7b05655fbdcd7df53d
Kevin Cupp <kevin.cupp@gmail.com>
2016-05-31 16:13:49 -0400

    Unit tests for file updater that assume everything is working
    (no failure testing yet)

--- system/ee/installer/updater/EllisLab/Tests/ExpressionEngine/Service/Updater/FileUpdaterTest.php
+++ system/ee/installer/updater/EllisLab/Tests/ExpressionEngine/Service/Updater/FileUpdaterTest.php
@@ -16,15 +16,99 @@ class FileUpdaterTest extends \PHPUnit_Framework_TestCase {
 
 		$this->logger->shouldReceive('log');
 
-		$this->fileupdater = new Downloader($this->license_number, $this->payload_url, $this->curl, $this->filesystem, $this->zip_archive, $this->config, $this->verifier, $this->logger, $this->requirements, $this->sites);
+		$this->config->shouldReceive('get')
+			->with('cache_path')
+			->andReturn('cache/path/');
+
+		$configs_path = 'cache/path/ee_update/configs.json';
+		$this->filesystem->shouldReceive('exists')->with($configs_path)->andReturn(TRUE);
+		$this->filesystem->shouldReceive('read')->with($configs_path)->andReturn('{"update_path":"\/cache\/path\/ee_update\/","archive_path":"\/cache\/path\/ee_update\/ExpressionEngine","theme_paths":{"1":"\/themes\/"}}');
+
+		$this->fileupdater = new FileUpdater($this->filesystem, $this->config, $this->verifier, $this->logger);
 	}
 
-	public function testBackup()
+	public function testBackupExistingInstallFiles()
 	{
+		$this->shouldCallMove(
+			SYSPATH.'ee/',
+			'cache/path/ee_update/backups/system_ee/',
+			[SYSPATH.'ee/updater']
+		);
+		$this->shouldCallMove(
+			'/themes/',
+			'cache/path/ee_update/backups/themes_ee/'
+		);
 
+		$this->fileupdater->backupExistingInstallFiles();
 	}
 
-	protected function move($source, $destination, Array $exclusions = [], $copy = FALSE)
+	public function testMoveNewInstallFiles()
+	{
+		$this->shouldCallMove(
+			'/cache/path/ee_update/ExpressionEngine/system/ee/',
+			SYSPATH.'ee/'
+		);
+		$this->shouldCallMove(
+			'/cache/path/ee_update/ExpressionEngine/themes/ee/',
+			'/themes/'
+		);
+
+		$this->fileupdater->moveNewInstallFiles();
+	}
+
+	public function testVerifyNewFiles()
+	{
+		$hash_manifiest = SYSPATH . '/ee/updater/hash-manifest';
+		$exclusions = ['system/ee/installer/updater'];
+
+		$this->verifier->shouldReceive('verifyPath')->with(
+			SYSPATH . '/ee',
+			$hash_manifiest,
+			'system/ee',
+			$exclusions
+		)->andReturn(TRUE);
+
+		$this->verifier->shouldReceive('verifyPath')->with(
+			'/themes/ee',
+			$hash_manifiest,
+			'themes/ee',
+			$exclusions
+		)->andReturn(TRUE);
+
+		$this->fileupdater->verifyNewFiles();
+	}
+
+	public function testRollbackFiles()
+	{
+		$this->shouldCallRollbackFiles();
+		$this->fileupdater->rollbackFiles();
+	}
+
+	protected function shouldCallRollbackFiles()
+	{
+		$this->shouldCallMove(
+			SYSPATH.'ee/',
+			'/cache/path/ee_update/ExpressionEngine/system/ee/',
+			[SYSPATH.'ee/updater']
+		);
+
+		$this->shouldCallMove(
+			'/themes/',
+			'/cache/path/ee_update/ExpressionEngine/themes/ee/'
+		);
+
+		$this->shouldCallMove(
+			'cache/path/ee_update/backups/system_ee/',
+			SYSPATH.'ee/'
+		);
+
+		$this->shouldCallMove(
+			'cache/path/ee_update/backups/themes_ee/',
+			'/themes/'
+		);
+	}
+
+	protected function shouldCallMove($source, $destination, Array $exclusions = [], $copy = FALSE)
 	{
 		$this->filesystem->shouldReceive('exists')->with($destination)->andReturn(TRUE);
 		$this->filesystem->shouldReceive('isDir')->with($destination)->andReturn(TRUE);
