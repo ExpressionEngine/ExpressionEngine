891ebff35908cdcf9e288ad9f157449e5b15df3c
Kevin Cupp <kevin.cupp@gmail.com>
2016-06-01 14:36:40 -0400

    Testing move and delete protected methods

--- system/ee/installer/updater/EllisLab/Tests/ExpressionEngine/Service/Updater/FileUpdaterTest.php
+++ system/ee/installer/updater/EllisLab/Tests/ExpressionEngine/Service/Updater/FileUpdaterTest.php
@@ -3,6 +3,7 @@
 namespace EllisLab\Tests\ExpressionEngine\Service\Updater;
 
 use EllisLab\ExpressionEngine\Service\Updater\FileUpdater;
+use EllisLab\ExpressionEngine\Service\Updater\UpdaterException;
 use Mockery;
 
 class FileUpdaterTest extends \PHPUnit_Framework_TestCase {
@@ -24,6 +25,9 @@ class FileUpdaterTest extends \PHPUnit_Framework_TestCase {
 		$this->filesystem->shouldReceive('exists')->with($configs_path)->andReturn(TRUE);
 		$this->filesystem->shouldReceive('read')->with($configs_path)->andReturn('{"update_path":"\/cache\/path\/ee_update\/","archive_path":"\/cache\/path\/ee_update\/ExpressionEngine","theme_paths":{"1":"\/themes\/"}}');
 
+		$this->archive_path = '/cache/path/ee_update/ExpressionEngine/';
+		$this->backups_path = 'cache/path/ee_update/backups/';
+
 		$this->fileupdater = new FileUpdater($this->filesystem, $this->config, $this->verifier, $this->logger);
 	}
 
@@ -32,12 +36,12 @@ class FileUpdaterTest extends \PHPUnit_Framework_TestCase {
 		// Single themes folder
 		$this->shouldCallMove(
 			SYSPATH.'ee/',
-			'cache/path/ee_update/backups/system_ee/',
+			$this->backups_path.'system_ee/',
 			[SYSPATH.'ee/updater']
 		);
 		$this->shouldCallMove(
 			'/themes/ee/',
-			'cache/path/ee_update/backups/themes_ee/'
+			$this->backups_path.'themes_ee/'
 		);
 
 		$this->fileupdater->backupExistingInstallFiles();
@@ -46,12 +50,12 @@ class FileUpdaterTest extends \PHPUnit_Framework_TestCase {
 		$this->fileupdater->configs['theme_paths'] = [1 => '/themes/', 2 => '/themes/'];
 		$this->shouldCallMove(
 			SYSPATH.'ee/',
-			'cache/path/ee_update/backups/system_ee/',
+			$this->backups_path.'system_ee/',
 			[SYSPATH.'ee/updater']
 		);
 		$this->shouldCallMove(
 			'/themes/ee/',
-			'cache/path/ee_update/backups/themes_ee/'
+			$this->backups_path.'themes_ee/'
 		);
 
 		$this->fileupdater->backupExistingInstallFiles();
@@ -60,12 +64,12 @@ class FileUpdaterTest extends \PHPUnit_Framework_TestCase {
 		$this->fileupdater->configs['theme_paths'] = [1 => '/themes/', 2 => '/some/other/site/themes/'];
 		$this->shouldCallMove(
 			SYSPATH.'ee/',
-			'cache/path/ee_update/backups/system_ee/',
+			$this->backups_path.'system_ee/',
 			[SYSPATH.'ee/updater']
 		);
 		$this->shouldCallMove(
 			'/themes/ee/',
-			'cache/path/ee_update/backups/themes_ee/'
+			$this->backups_path.'themes_ee/'
 		);
 
 		$this->fileupdater->backupExistingInstallFiles();
@@ -75,11 +79,11 @@ class FileUpdaterTest extends \PHPUnit_Framework_TestCase {
 	{
 		// Single themes folder
 		$this->shouldCallMove(
-			'/cache/path/ee_update/ExpressionEngine/system/ee/',
+			$this->archive_path.'system/ee/',
 			SYSPATH.'ee/'
 		);
 		$this->shouldCallMove(
-			'/cache/path/ee_update/ExpressionEngine/themes/ee/',
+			$this->archive_path.'themes/ee/',
 			'/themes/ee/'
 		);
 
@@ -88,11 +92,11 @@ class FileUpdaterTest extends \PHPUnit_Framework_TestCase {
 		// Multiple themes folders, but are the same
 		$this->fileupdater->configs['theme_paths'] = [1 => '/themes/', 2 => '/themes/'];
 		$this->shouldCallMove(
-			'/cache/path/ee_update/ExpressionEngine/system/ee/',
+			$this->archive_path.'system/ee/',
 			SYSPATH.'ee/'
 		);
 		$this->shouldCallMove(
-			'/cache/path/ee_update/ExpressionEngine/themes/ee/',
+			$this->archive_path.'themes/ee/',
 			'/themes/ee/'
 		);
 
@@ -101,14 +105,14 @@ class FileUpdaterTest extends \PHPUnit_Framework_TestCase {
 		// Multiple unique themes folders
 		$this->fileupdater->configs['theme_paths'] = [1 => '/themes/', 2 => '/some/other/site/themes/'];
 		$this->shouldCallMove(
-			'/cache/path/ee_update/ExpressionEngine/system/ee/',
+			$this->archive_path.'system/ee/',
 			SYSPATH.'ee/'
 		);
 
 		foreach ($this->fileupdater->configs['theme_paths'] as $theme_path)
 		{
 			$this->shouldCallMove(
-				'/cache/path/ee_update/ExpressionEngine/themes/ee/',
+				$this->archive_path.'themes/ee/',
 				$theme_path.'ee/',
 				[],
 				TRUE
@@ -193,7 +197,7 @@ class FileUpdaterTest extends \PHPUnit_Framework_TestCase {
 		$this->fileupdater->configs['theme_paths'] = [1 => '/themes/', 2 => '/some/other/site/themes/'];
 		$this->shouldCallMove(
 			SYSPATH.'ee/',
-			'/cache/path/ee_update/ExpressionEngine/system/ee/',
+			$this->archive_path.'system/ee/',
 			[SYSPATH.'ee/updater']
 		);
 		foreach ($this->fileupdater->configs['theme_paths'] as $theme_path)
@@ -201,13 +205,13 @@ class FileUpdaterTest extends \PHPUnit_Framework_TestCase {
 			$this->shouldCallDelete($theme_path.'ee/');
 		}
 		$this->shouldCallMove(
-			'cache/path/ee_update/backups/system_ee/',
+			$this->backups_path.'system_ee/',
 			SYSPATH.'ee/'
 		);
 		foreach ($this->fileupdater->configs['theme_paths'] as $theme_path)
 		{
 			$this->shouldCallMove(
-				'cache/path/ee_update/backups/themes_ee/',
+				$this->backups_path.'themes_ee/',
 				$theme_path.'ee/',
 				[],
 				TRUE
@@ -217,17 +221,132 @@ class FileUpdaterTest extends \PHPUnit_Framework_TestCase {
 		$this->fileupdater->rollbackFiles();
 	}
 
+	public function testMove()
+	{
+		// move() is protected, so we'll go through the backup method and test
+		// via our mocks; we've also already pretty well tested what happens
+		// under ideal circumstances, so we'll only manufacture failures here
+
+		$source = SYSPATH.'ee/';
+		$destination = $this->backups_path.'system_ee/';
+
+		// Destination directory doesn't exist?
+		$this->filesystem->shouldReceive('exists')->with($destination)->andReturn(FALSE)->once();
+		$this->filesystem->shouldReceive('mkDir')->with($destination, FALSE)->andReturn(TRUE)->once();
+		$this->filesystem->shouldReceive('getDirectoryContents')->with($source)->andReturn([])->once();
+
+		$source = '/themes/ee/';
+		$destination = $this->backups_path.'themes_ee/';
+
+		$this->filesystem->shouldReceive('exists')->with($destination)->andReturn(FALSE)->once();
+		$this->filesystem->shouldReceive('mkDir')->with($destination, FALSE)->andReturn(TRUE)->once();
+		$this->filesystem->shouldReceive('getDirectoryContents')->with($source)->andReturn([])->once();
+
+		$this->fileupdater->backupExistingInstallFiles();
+
+		// Destination isn't a directory
+		$source = SYSPATH.'ee/';
+		$destination = $this->backups_path.'system_ee/';
+		$this->filesystem->shouldReceive('exists')->with($destination)->andReturn(TRUE)->once();
+		$this->filesystem->shouldReceive('isDir')->with($destination)->andReturn(FALSE)->once();
+
+		try
+		{
+			$this->fileupdater->backupExistingInstallFiles();
+			$this->fail();
+		}
+		catch (UpdaterException $e)
+		{
+			$this->assertEquals(18, $e->getCode());
+		}
+
+		// Should exclude files
+		$this->filesystem->shouldReceive('exists')->with($destination)->andReturn(FALSE)->once();
+		$this->filesystem->shouldReceive('mkDir')->with($destination, FALSE)->andReturn(TRUE)->once();
+		$this->filesystem->shouldReceive('getDirectoryContents')->with($source)->andReturn([
+			$source.'index.html',
+			$source.'updater',
+			$source.'.DS_Store',
+		])->once();
+
+		$new_path = str_replace($source, $destination, $source.'index.html');
+		$this->filesystem->shouldReceive('isWritable')->with($source.'index.html')->andReturn(TRUE)->once();
+		$this->filesystem->shouldReceive('isWritable')->with($new_path)->andReturn(TRUE)->once();
+		$this->filesystem->shouldReceive('rename')->with($source.'index.html', $new_path)->andReturn(TRUE)->once();
+
+		$source = '/themes/ee/';
+		$destination = $this->backups_path.'themes_ee/';
+
+		$this->filesystem->shouldReceive('exists')->with($destination)->andReturn(FALSE)->once();
+		$this->filesystem->shouldReceive('mkDir')->with($destination, FALSE)->andReturn(TRUE)->once();
+
+		$file_path = $source.'index.html';
+		$this->filesystem->shouldReceive('getDirectoryContents')->with($source)->andReturn([])->once();
+
+		$this->fileupdater->backupExistingInstallFiles();
+
+		// Should complain if an attempted move path isn't writable
+		$source = SYSPATH.'ee/';
+		$destination = $this->backups_path.'system_ee/';
+		$this->filesystem->shouldReceive('exists')->with($destination)->andReturn(FALSE)->once();
+		$this->filesystem->shouldReceive('mkDir')->with($destination, FALSE)->andReturn(TRUE)->once();
+		$this->filesystem->shouldReceive('getDirectoryContents')->with($source)->andReturn([
+			$source.'index.html',
+		])->once();
+
+		$new_path = str_replace($source, $destination, $source.'index.html');
+		$this->filesystem->shouldReceive('isWritable')->with($source.'index.html')->andReturn(TRUE)->once();
+		$this->filesystem->shouldReceive('isWritable')->with($new_path)->andReturn(FALSE)->once();
+
+		try
+		{
+			$this->fileupdater->backupExistingInstallFiles();
+			$this->fail();
+		}
+		catch (UpdaterException $e)
+		{
+			$this->assertEquals(19, $e->getCode());
+		}
+	}
+
+	public function testDelete()
+	{
+		// We just want to test that it fails to delete if not writable, we've
+		// tested the function works under correct circumstances elsewhere
+
+		// Multiple unique themes folders
+		$this->fileupdater->configs['theme_paths'] = [1 => '/themes/', 2 => '/some/other/site/themes/'];
+		$this->shouldCallMove(
+			SYSPATH.'ee/',
+			$this->archive_path.'system/ee/',
+			[SYSPATH.'ee/updater']
+		);
+		$directory = '/themes/ee/';
+		$this->filesystem->shouldReceive('getDirectoryContents')->with($directory)->andReturn([$directory.'index.html'])->once();
+		$this->filesystem->shouldReceive('isWritable')->with($directory.'index.html')->andReturn(FALSE)->once();
+
+		try
+		{
+			$this->fileupdater->rollbackFiles();
+			$this->fail();
+		}
+		catch (UpdaterException $e)
+		{
+			$this->assertEquals(20, $e->getCode());
+		}
+	}
+
 	protected function shouldCallRollbackFiles()
 	{
 		$this->shouldCallMove(
 			SYSPATH.'ee/',
-			'/cache/path/ee_update/ExpressionEngine/system/ee/',
+			$this->archive_path.'system/ee/',
 			[SYSPATH.'ee/updater']
 		);
 
 		$this->shouldCallMove(
 			'/themes/ee/',
-			'/cache/path/ee_update/ExpressionEngine/themes/ee/'
+			$this->archive_path.'themes/ee/'
 		);
 
 		$this->shouldCallMove(
