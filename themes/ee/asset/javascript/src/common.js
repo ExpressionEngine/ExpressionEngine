c58c6ebb53ae5ea7e8bbfc80f87f24a1b8819c38
Pascal Kriete <mail@pascal.io>
2016-06-13 12:54:59 -0400

    Can't yet remove the old nav js. Thanks semver.

--- themes/ee/asset/javascript/src/common.js
+++ themes/ee/asset/javascript/src/common.js
@@ -100,8 +100,54 @@ $(document).ready(function(){
 			}
 		});
 
+		// =========
+		// sub menus
+		// =========
+
+			// listen for clicks on elements with a class of has-sub
+			$('.has-sub').on('click',function(){
+				// close OTHER open sub menus
+				// when clicking THIS sub menu trigger
+				// thanks me :D
+				$('.open').not(this)
+					// remove the class of open
+					.removeClass('open')
+					// hide all siblings of open with a class of sub-menu
+					.siblings('.sub-menu').hide();
+
+				// toggles THIS sub menu
+				// thanks pascal
+				$(this)
+					// toggle of siblings of THIS
+					// with a class of sub-menu
+					.siblings('.sub-menu').toggle()
+					// go back to THIS and...
+					.end()
+					// toggle a class of open on THIS
+					.toggleClass('open');
+				// stop THIS from reloading
+				// the source window and appending to the URI
+				// and stop propagation up to document
+				return false;
+			});
+
+			// listen for clicks to the document
+			$(document).on('click',function(e){
+				// check to see if we are inside a sub-menu or not.
+				if(!$(e.target).closest('.sub-menu').length){
+					// close OTHER open sub menus
+					// when clicking outside ANY sub menu trigger
+					// thanks me :D
+					$('.open')
+						// remove the class of open
+						.removeClass('open')
+						// hide all siblings of open with a class of sub-menu
+						.siblings('.sub-menu').hide();
+				}
+			});
+
 	// =========
-	// sub menus
+	// sub menus (NEW)
 	// =========
 
 		// listen for clicks on elements with a class of has-sub
