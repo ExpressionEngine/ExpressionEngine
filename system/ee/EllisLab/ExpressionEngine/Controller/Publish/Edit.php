bc5a0de3a751e8a751e68366f289311dffa92a6a
Kevin Cupp <kevin.cupp@gmail.com>
2016-06-15 14:02:54 -0700

    Reversing max entries conditional and only pluralizing message if needed

--- system/ee/EllisLab/ExpressionEngine/Controller/Publish/Edit.php
+++ system/ee/EllisLab/ExpressionEngine/Controller/Publish/Edit.php
@@ -132,12 +132,14 @@ class Edit extends AbstractPublishController {
 			$channel = ee('Model')->get('Channel', $channel_id)->first();
 			$vars['create_button'] = '<a class="btn tn action" href="'.ee('CP/URL', 'publish/create/' . $channel_id).'">'.sprintf(lang('btn_create_new_entry_in_channel'), $channel->channel_title).'</a>';
 
-			if ($channel->max_entries !== '0' && $channel->max_entries >= $count)
+			if ($channel->max_entries !== '0' && $count >= $channel->max_entries)
 			{
+				$desc_key = ($channel->max_entries === '1')
+					? 'entry_limit_reached_one_desc' : 'entry_limit_reached_desc';
 				ee('CP/Alert')->makeInline()
 					->asWarning()
 					->withTitle(lang('entry_limit_reached'))
-					->addToBody(sprintf(lang('entry_limit_reached_desc'), $channel->max_entries))
+					->addToBody(sprintf(lang($desc_key), $channel->max_entries))
 					->now();
 			}
 		}
