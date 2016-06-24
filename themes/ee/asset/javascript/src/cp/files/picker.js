90dc386e72bf0597ee331a09f53ff1eeefce795f
Seth Croston Barber <seth.barber@ellislab.com>
2016-06-24 14:02:07 -0700

    Updating the filepicker URL for textareas and RTE when filters are chosen.
    Fixes #1023

--- themes/ee/asset/javascript/src/cp/files/picker.js
+++ themes/ee/asset/javascript/src/cp/files/picker.js
@@ -80,7 +80,14 @@
 
 		$('.modal-file').on('click', '.filters a:not([href=""]), .paginate a:not([href=""]), thead a:not([href=""])', function(e) {
 			e.preventDefault();
-			$(this).parents('div.box').load($(this).attr('href'));
+			var new_url = $(this).attr('href');
+
+			console.log(options);
+
+			$(this).parents('div.box').load(new_url);
+			if ($(options.source).hasClass('markItUpButton') || $(options.source).hasClass('rte-upload')) {
+				$('.publish .toolbar.rte li.m-link[rel="modal-file"], .publish .toolbar.html-btns li.m-link[rel="modal-file"]').attr('href', new_url);
+			}
 		});
 		// Capture form submission
 		$('.modal-file').on('submit', 'form', function(e) {
