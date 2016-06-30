340e02d5a7ba307e6cad9e8d02d6e29c296bd266
Pascal Kriete <mail@pascal.io>
2016-06-30 14:48:39 -0400

    Adding a menu controller

---
+++ system/ee/EllisLab/ExpressionEngine/View/settings/menu-manager/index.php
@@ -0,0 +1,34 @@
+<?php $this->extend('_templates/default-nav-table'); ?>
+
+<div class="tbl-ctrls">
+	<?=form_open($table['base_url'])?>
+		<fieldset class="tbl-search right">
+			<a class="btn tn action" href="<?=ee('CP/URL')->make('settings/menu-manager/create-set')?>"><?=lang('create_new')?></a>
+		</fieldset>
+		<h1><?=$cp_page_title?></h1>
+		<?=ee('CP/Alert')->getAllInlines()?>
+		<?php $this->embed('_shared/table', $table); ?>
+		<?=$pagination?>
+		<fieldset class="tbl-bulk-act hidden">
+			<select name="bulk_action">
+				<option>-- <?=lang('with_selected')?> --</option>
+				<option value="remove" data-confirm-trigger="selected" rel="modal-confirm-remove"><?=lang('remove')?></option>
+			</select>
+			<input class="btn submit" data-conditional-modal="confirm-trigger" type="submit" value="<?=lang('submit')?>">
+		</fieldset>
+	</form>
+</div>
+
+<?php
+
+$modal_vars = array(
+	'name'		=> 'modal-confirm-remove',
+	'form_url'	=> ee('CP/URL')->make('settings/menu-manager/remove-set', ee()->cp->get_url_state()),
+	'hidden'	=> array(
+		'bulk_action'	=> 'remove'
+	)
+);
+
+$modal = $this->make('ee:_shared/modal_confirm_remove')->render($modal_vars);
+ee('CP/Modal')->addModal('remove', $modal);
+?>
---
+++ system/ee/EllisLab/ExpressionEngine/View/settings/menu-manager/reorder.php
@@ -0,0 +1,26 @@
+<div class="nestable">
+	<ul class="nested-list managed">
+		<?php $this->embed('settings/menu-manager/item'); ?>
+	</ul>
+</div>
+
+<?php
+
+$modal_vars = array(
+	'name'		=> 'modal-menu-confirm-remove',
+	'form_url'	=> ee('CP/URL')->make('settings/menu-manager/remove-item', ee()->cp->get_url_state()),
+	'hidden'	=> array(
+		'bulk_action'	=> 'remove',
+		'item_id' => ''
+	)
+);
+
+$modal = $this->make('ee:_shared/modal_confirm_remove')->render($modal_vars);
+ee('CP/Modal')->addModal('remove', $modal);
+
+
+$modal_vars = array('name' => 'modal-menu-edit', 'contents' => '');
+$modal = $this->make('ee:_shared/modal-form')->render($modal_vars);
+ee('CP/Modal')->addModal('menu-edit', $modal);
+
+?>
