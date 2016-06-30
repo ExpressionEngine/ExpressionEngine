be443ee6874af5d757d1eadb7c86d0eda6886bde
Pascal Kriete <mail@pascal.io>
2016-06-30 16:08:54 -0400

    Updater and installer stuff

--- system/ee/installer/updates/ud_3_04_00.php
+++ system/ee/installer/updates/ud_3_04_00.php
@@ -38,6 +38,7 @@ class Updater {
 			array(
 				'add_can_view_homepage_news_permission',
 				'add_channel_max_entries_column',
+				'add_menu_tables'
 			)
 		);
 
@@ -80,6 +81,95 @@ class Updater {
 				),
 			)
 		);
+	}
+
+	/**
+	 * Add menu_set_id to members and create tables: menu_sets, menu_set_items
+	 */
+	private function add_menu_tables()
+	{
+		ee()->smartforge->add_column(
+			'member_groups',
+			array(
+				'menu_set_id'      => array(
+					'type'         => 'int',
+					'null'         => FALSE,
+					'unsigned'     => TRUE,
+					'default'      => 0
+				),
+			)
+		);
+
+		ee()->dbforge->add_field(
+			array(
+				'item_id' => array(
+					'type'			 => 'int',
+					'constraint'     => 10,
+					'null'			 => FALSE,
+					'unsigned'		 => TRUE,
+					'auto_increment' => TRUE
+				),
+				'parent_id' => array(
+					'type'			=> 'int',
+					'constraint'    => 10,
+					'unsigned'		=> TRUE,
+					'null'			=> FALSE
+				),
+				'set_id' => array(
+					'type'			=> 'varchar',
+					'constraint'    => 512,
+					'null'			=> TRUE
+				),
+				'name' => array(
+					'type'			=> 'varchar',
+					'constraint'    => 50,
+					'null'			=> TRUE
+				),
+				'data' => array(
+					'type'			=> 'varchar',
+					'constraint'    => 255,
+					'null'			=> TRUE
+				),
+				'type' => array(
+					'type'			=> 'varchar',
+					'constraint'    => 10,
+					'null'			=> TRUE
+				),
+				'sort' => array(
+					'type'			=> 'int',
+					'constraint'    => 5,
+					'unsigned'		=> TRUE,
+					'null'			=> FALSE,
+					'default'       => 0
+				)
+			)
+		);
+
+		ee()->dbforge->add_key('item_id', TRUE);
+		ee()->dbforge->add_key('set_id');
+		ee()->smartforge->create_table('menu_items');
+
+		ee()->dbforge->add_field(
+			array(
+				'set_id' => array(
+					'type'			 => 'int',
+					'constraint'     => 10,
+					'null'			 => FALSE,
+					'unsigned'		 => TRUE,
+					'auto_increment' => TRUE
+				),
+				'name' => array(
+					'type'			=> 'varchar',
+					'constraint'    => 50,
+					'null'			=> TRUE
+				)
+			)
+		);
+
+		ee()->dbforge->add_key('set_id', TRUE);
+		ee()->smartforge->create_table('menu_sets');
+
+		ee()->db->insert('menu_sets', array('name' => 'Default'));
 	}
 }
 
