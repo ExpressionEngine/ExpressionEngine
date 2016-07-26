b4a492cd28349efd59d4cc0f8cdcd1983364e959
Robin Sowell <robin.sowell@ellislab.com>
2016-07-26 10:37:29 -0400

    updated version number

--- system/ee/legacy/libraries/Core.php
+++ system/ee/legacy/libraries/Core.php
@@ -82,9 +82,9 @@ class EE_Core {
 		// application constants
 		define('IS_CORE',		FALSE);
 		define('APP_NAME',		'ExpressionEngine'.(IS_CORE ? ' Core' : ''));
-		define('APP_BUILD',		'20160715');
+		define('APP_BUILD',		'20160726');
 		define('APP_VER',		'3.4.0');
-		define('APP_VER_ID',	'dp.3');
+		define('APP_VER_ID',	'');
 		define('SLASH',			'&#47;');
 		define('LD',			'{');
 		define('RD',			'}');
