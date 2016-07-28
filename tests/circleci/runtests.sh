d45da2f425ed37ef98340b298049a1d8a1cfb923
Pascal Kriete <mail@pascal.io>
2016-07-28 08:55:24 -0400

    We do not want the dmr release

--- tests/circleci/runtests.sh
+++ tests/circleci/runtests.sh
@@ -14,7 +14,7 @@ installmysql() {
 	export DEBIAN_FRONTEND=noninteractive
 	curl -LO https://dev.mysql.com/get/mysql-apt-config_0.7.3-1_all.deb
 	echo mysql-apt-config mysql-apt-config/select-product          select Apply              | sudo debconf-set-selections
-	echo mysql-apt-config mysql-apt-config/select-server           select mysql-5.7-dmr      | sudo debconf-set-selections
+	echo mysql-apt-config mysql-apt-config/select-server           select mysql-5.7          | sudo debconf-set-selections
 	echo mysql-apt-config mysql-apt-config/select-connector-python select none               | sudo debconf-set-selections
 	echo mysql-apt-config mysql-apt-config/select-workbench        select none               | sudo debconf-set-selections
 	echo mysql-apt-config mysql-apt-config/select-utilities        select none               | sudo debconf-set-selections
