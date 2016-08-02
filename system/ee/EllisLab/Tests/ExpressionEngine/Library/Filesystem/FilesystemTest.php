e8172421fda46c83bd24ab27f06e1df09bb31194
Seth Croston Barber <seth.barber@ellislab.com>
2016-08-02 09:12:47 -0700

    :white_check_mark: Adding unit tests for the unique file name getter.

---
+++ system/ee/EllisLab/Tests/ExpressionEngine/Library/Filesystem/FilesystemTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace EllisLab\Tests\ExpressionEngine\Library\Filesystem;
+
+use EllisLab\ExpressionEngine\Library\Filesystem\Filesystem;
+
+class FilesystemTest extends \PHPUnit_Framework_TestCase {
+
+	protected $fs;
+	protected $dummy_files;
+	protected $path;
+
+	public function setUp()
+	{
+		$this->fs = new Filesystem();
+
+		$this->path = realpath(__DIR__.'/../../../support/') . '/';
+
+		$this->dummy_files = array(
+			'image001.jpg',
+			'image002.jpg',
+			'image002_2.jpg',
+			'vacation_2016_01_02.jpg',
+			'unique_test.txt'
+		);
+
+		foreach ($this->dummy_files as $file)
+		{
+			touch($this->path.$file);
+		}
+
+		for ($x = 1; $x < 101; $x++)
+		{
+			touch($this->path.'unique_test_'.$x.'.txt');
+		}
+	}
+
+	public function tearDown()
+	{
+		foreach ($this->dummy_files as $file)
+		{
+			unlink($this->path.$file);
+		}
+
+		for ($x = 1; $x < 101; $x++)
+		{
+			unlink($this->path.'unique_test_'.$x.'.txt');
+		}
+	}
+
+	/**
+	 * @dataProvider uniqueFilenamesProvider
+	 */
+	public function testGetUniqueFilename($description, $in, $out)
+	{
+		$this->assertEquals($this->fs->getUniqueFilename($this->path.$in), $this->path.$out, $description);
+	}
+
+	public function uniqueFilenamesProvider()
+	{
+		return array(
+			array('File is already unique',            'DSC_0001.jpg',            'DSC_0001.jpg'),
+			array('Adds an underscore',                'image001.jpg',            'image001_1.jpg'),
+			array('Rename picks up where it left off', 'image002.jpg',            'image002_3.jpg'),
+			array('Handles extra underscores',         'vacation_2016_01_02.jpg', 'vacation_2016_01_02_1.jpg'),
+			array('Handles partial matches',           'vacation.jpg',            'vacation.jpg'),
+			array('Exceeds 100 renames',               'unique_test.txt',         'unique_test_101.txt')
+		);
+	}
+
+}
+
+// EOF
