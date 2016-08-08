82a9e6aea151e860f9d14094564a1e4467c804d9
Pascal Kriete <mail@pascal.io>
2016-08-08 16:45:17 -0400

    Top buttons = bottom buttons

--- system/ee/EllisLab/ExpressionEngine/View/publish/entry.php
+++ system/ee/EllisLab/ExpressionEngine/View/publish/entry.php
@@ -62,10 +62,20 @@
 			<?=ee('CP/Alert')->getAllInlines()?>
 			<?php if ($extra_publish_controls): ?>
 				<fieldset class="form-ctrls top">
-					<?php if ($entry->Channel->enable_versioning): ?>
-					<input class="btn draft" type="submit" name="save_revision" value="<?=lang('btn_save_revision')?>">
-					<?php endif; ?>
-					<?=cp_form_submit($button_text, lang('btn_saving'))?>
+					<?php
+						$class = 'btn';
+						$disabled = '';
+
+						if ((isset($errors) && $errors->isNotValid()))
+						{
+							$class = 'btn disable';
+							$disabled = 'disabled="disabled"';
+						}
+
+						$just_save = trim(sprintf(lang('btn_save'), ''));
+					?>
+					<button class="<?=$class?>" <?=$disabled?> name="submit" type="submit" value="edit" data-submit-text="<?=$just_save?>" data-work-text="<?=lang('btn_saving')?>"><?=($disabled) ? lang('btn_fix_errors') : $just_save?></button>
+					<button class="<?=$class?>" <?=$disabled?> name="submit" type="submit" value="finish" data-submit-text="<?=lang('btn_save_and_close')?>" data-work-text="<?=lang('btn_saving')?>"><?=($disabled) ? lang('btn_fix_errors') : lang('btn_save_and_close')?></button>
 				</fieldset>
 			<?php endif ?>
 			<?php foreach ($layout->getTabs() as $index => $tab): ?>
