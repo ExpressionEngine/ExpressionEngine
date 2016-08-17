157d3eff42505d85cb432d6d439e1a78465217a8
Kevin Cupp <kevin.cupp@gmail.com>
2016-08-17 16:04:29 -0400

    Updating phar code to run the update and adding compiled phar

--- system/ee/installer/updater/eecms.phar/src/index.php
+++ system/ee/installer/updater/eecms.phar/src/index.php
@@ -2,4 +2,23 @@
 
 require_once 'phar://eecms.phar/bootstrap.php';
 
+try
+{
+	ee('Updater\Runner')->run();
+}
+catch (\Exception $e)
+{
+	echo $e->getMessage();
+	exit;
+}
+
+$updater_boot = SYSPATH.'/ee/updater/boot.php';
+if (file_exists($updater_boot))
+{
+	require_once $updater_boot;
+
+	$runner = new EllisLab\ExpressionEngine\Updater\Service\Updater\Runner();
+	$runner->run();
+}
+
 echo 'It works!';
