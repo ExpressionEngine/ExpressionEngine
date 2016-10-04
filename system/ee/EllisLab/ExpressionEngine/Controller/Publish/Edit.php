cbab216675d7b591529404d98194a3384de759d2
Derek Jones <derek.jones@ellislab.com>
2016-10-04 11:35:12 -0700

    Making some strict conditionals loose due to DB return values
    see
    https://github.com/EllisLab/ExpressionEngine/commit/a681b1b71d21c063c6e7
    057026e6a85aea957396

--- system/ee/EllisLab/ExpressionEngine/Controller/Publish/Edit.php
+++ system/ee/EllisLab/ExpressionEngine/Controller/Publish/Edit.php
@@ -133,7 +133,7 @@ class Edit extends AbstractPublishController {
 			$vars['create_button'] = '<a class="btn tn action" href="'.ee('CP/URL', 'publish/create/' . $channel_id).'">'.sprintf(lang('btn_create_new_entry_in_channel'), $channel->channel_title).'</a>';
 
 			// Have we reached the max entries limit for this channel?
-			if ($channel->max_entries !== '0' && $count >= $channel->max_entries)
+			if ($channel->max_entries != 0 && $count >= $channel->max_entries)
 			{
 				// Don't show create button
 				$vars['create_button'] = '';
