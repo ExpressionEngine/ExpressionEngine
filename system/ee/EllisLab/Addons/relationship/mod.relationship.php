09f51ce61db9849c58752c2f05ecc2865bb92afa
Kevin Cupp <kevin.cupp@gmail.com>
2016-11-07 20:47:41 -0500

    Adding relationship module file for ACT AJAX endpoint

---
+++ system/ee/EllisLab/Addons/relationship/mod.relationship.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * ExpressionEngine - by EllisLab
+ *
+ * @package		ExpressionEngine
+ * @author		EllisLab Dev Team
+ * @copyright	Copyright (c) 2003 - 2016, EllisLab, Inc.
+ * @license		https://expressionengine.com/license
+ * @link		https://ellislab.com
+ * @since		Version 3.4.5
+ * @filesource
+ */
+
+// --------------------------------------------------------------------
+
+/**
+ * ExpressionEngine Relationship Module
+ *
+ * @package		ExpressionEngine
+ * @subpackage	Modules
+ * @category	Modules
+ * @author		EllisLab Dev Team
+ * @link		https://ellislab.com
+ */
+
+class Relationship {
+
+	public function entryList()
+	{
+		/*if ( ! AJAX_REQUEST OR ! ee()->session->userdata('member_id'))
+		{
+			show_error(lang('unauthorized_access'));
+		}*/
+		echo 'hi';
+	}
+}
+
+// EOF
