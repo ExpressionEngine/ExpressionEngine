88f043ad466ed6917fd71e36739c9e20727b77df
Kevin Cupp <kevin.cupp@gmail.com>
2016-11-21 19:03:52 -0500

    Moved Dockerfile build stuff to its own repo

--- tests/docker/build/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp /app/tests/docker/runtests.sh .
-chmod 755 runtests.sh
-./runtests.sh $@
--- tests/docker/build/updategems.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source /usr/local/rvm/scripts/rvm
-
-pushd /app/tests/rspec
-	bundle install --no-deployment --path=~/gems/
-popd
