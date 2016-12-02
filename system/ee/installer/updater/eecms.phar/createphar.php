93c4e9ea1dc59651282d52372b5cd94b34f5d676
Kevin Cupp <kevin.cupp@gmail.com>
2016-12-02 18:02:42 -0500

    Make eecms.phar creation just dump into system/ee

--- system/ee/installer/updater/eecms.phar/createphar.php
+++ system/ee/installer/updater/eecms.phar/createphar.php
@@ -1,7 +1,7 @@
 <?php
 
 $src_folder = __DIR__.'/src';
-$build_folder = __DIR__.'/build';
+$build_folder = realpath(__DIR__.'/../../../');
 
 $phar = new Phar($build_folder . '/eecms.phar', 0, 'eecms.phar');
 $phar->setStub($phar->createDefaultStub('index.php'));
