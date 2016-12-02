c54f2a4e420bdb922b2129ffb425b7da02ae8153
Kevin Cupp <kevin.cupp@gmail.com>
2016-12-02 18:04:02 -0500

    Updating completely from the CLI works again

--- system/ee/installer/updater/eecms.phar/src/index.php
+++ system/ee/installer/updater/eecms.phar/src/index.php
@@ -1,103 +1,44 @@
 <?php
 
+require_once 'phar://eecms.phar/bootstrap.php';
+
 $project_base = realpath('../').'/';
 
 // TODO: Add some test to make sure the CLI file hasn't been moved out of system/ee and complain if it has
 
 // Path constants
+define('SELF', basename(__FILE__));
 define('SYSPATH', $project_base);
+define('SYSDIR', basename($project_base));
 define('DEBUG', 1);
 
-if (isset($argv[1]))
+$args = parseArguments();
+
+if ( ! isset($args['--no-bootstrap']))
 {
-	switch ($argv[1]) {
-		case 'update':
-		case 'upgrade':
-		case '_updateDatabase':
-			$bootstrap = SYSPATH.'ee/EllisLab/ExpressionEngine/Boot/boot.php';
-			if (file_exists($bootstrap))
-			{
-				define('BOOT_ONLY', TRUE);
-				require_once $bootstrap;
-			}
-			break;
-		case '_updaterMicroapp':
-		case '_rollback':
-			$updater_boot = SYSPATH.'/ee/updater/boot.php';
-			if (file_exists($updater_boot))
-			{
-				require_once $updater_boot;
-			}
-			break;
-		default:
-			# code...
-			break;
+	// Currently needed for installer conditional in boot.php
+	if ( ! defined('REQ'))
+	{
+		define('REQ', 'CP');
 	}
 
-	switch ($argv[1]) {
-		case 'update':
-		case 'upgrade':
-			_beginUpgrade();
-			break;
-		case '_updaterMicroapp':
-			_updaterMicroapp();
-			break;
-		case '_updateDatabase':
-			_updateDatabase();
-			break;
-		case '_rollback';
-			_rollback();
-			break;
-		default:
-			# code...
-			break;
+	$bootstrap = SYSPATH.'ee/EllisLab/ExpressionEngine/Boot/boot.php';
+	if (file_exists($bootstrap))
+	{
+		define('BOOT_ONLY', TRUE);
+		require_once $bootstrap;
 	}
 }
 
-function _beginUpgrade()
+$supported_commands = ['upgrade'];
+
+if (isset($args[0]) && in_array($args[0], $supported_commands))
 {
-	try
-	{
-		ee('Updater/Runner')->run();
-	}
-	catch (\Exception $e)
-	{
-		echo $e->getMessage();
-		exit;
-	}
-	echo system('php eecms.phar _updaterMicroapp');
+	$command = array_shift($args);
+	require_once 'phar://eecms.phar/'.$command.'.php';
+	new Command($args);
 }
-
-function _updaterMicroapp()
+else
 {
-	$runner = new EllisLab\ExpressionEngine\Updater\Service\Updater\Runner();
-
-	while (strpos($runner->getNextStep(), 'updateDatabase') === FALSE && $runner->getNextStep() !== FALSE)
-	{
-		$runner->runStep($runner->getNextStep());
-	}
-
-	echo system('php eecms.phar _updateDatabase');
-}
-
-function _updateDatabase()
-{
-	// TODO: We still need access to the updater autoloader here
-	$runner = new EllisLab\ExpressionEngine\Updater\Service\Updater\Runner();
-
-	$return = $runner->runStep('updateDatabase');
-
-	while ($return != 'rollback')
-	{
-		$return = $runner->runStep($return);
-	}
-
-	echo 'finished db updates';
-	echo system('php eecms.phar _rollback');
-}
-
-function _rollback()
-{
-	$runner = new EllisLab\ExpressionEngine\Updater\Service\Updater\Runner();
-	$runner->runStep('rollback');
+	exit('show help');
 }
---
+++ system/ee/installer/updater/eecms.phar/src/upgrade.php
@@ -0,0 +1,72 @@
+<?php
+
+$updater_boot = SYSPATH.'/ee/updater/boot.php';
+if (file_exists($updater_boot))
+{
+	require_once $updater_boot;
+}
+
+class Command {
+
+	public function __construct($params = [])
+	{
+		if (isset($params['microapp']))
+		{
+			$step = (isset($params['step'])) ? $params['step'] : NULL;
+			$this->updaterMicroapp($step);
+			return;
+		}
+
+		$this->start();
+	}
+
+	public function start()
+	{
+		try
+		{
+			ee('Updater/Runner')->run();
+		}
+		catch (\Exception $e)
+		{
+			echo $e->getMessage();
+			exit;
+		}
+
+		// TODO: Abstract into helper method to run other eecms CLI commands?
+		system('php eecms.phar upgrade --microapp --no-bootstrap');
+	}
+
+	public function updaterMicroapp($step = NULL)
+	{
+		$runner = new EllisLab\ExpressionEngine\Updater\Service\Updater\Runner();
+
+		if ( ! $step)
+		{
+			$step = $runner->getFirstStep();
+		}
+
+		try
+		{
+			$runner->runStep($step);
+		}
+		catch (\Exception $e)
+		{
+			// TODO: try to get logger, log message
+			echo $e->getMessage();
+		}
+
+		if (($next_step = $runner->getNextStep()) !== FALSE)
+		{
+			$cmd = 'php eecms.phar upgrade --microapp --step="'.$next_step.'"';
+
+			if (strpos($next_step, 'updateDatabase') === FALSE)
+			{
+				$cmd .= ' --no-bootstrap';
+			}
+
+			system($cmd);
+		}
+	}
+}
+
+// EOF
