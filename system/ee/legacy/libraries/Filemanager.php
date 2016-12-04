49e41144068af35db38aecafba798e98a5f1b627
Kevin Cupp <kevin.cupp@gmail.com>
2016-12-04 14:26:36 -0500

    Fix PHP 7.1 incompatibility when uploading a file

--- system/ee/legacy/libraries/Filemanager.php
+++ system/ee/legacy/libraries/Filemanager.php
@@ -2013,7 +2013,7 @@ class Filemanager {
 		$config = array(
 			'file_name'		=> $clean_filename,
 			'upload_path'	=> $dir['server_path'],
-			'max_size'		=> round($dir['max_size']*1024, 3)
+			'max_size'		=> round((int)$dir['max_size']*1024, 3)
 		);
 
 		// Restricted upload directory?
