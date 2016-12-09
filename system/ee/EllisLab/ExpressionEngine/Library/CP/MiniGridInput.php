066bf31030b05a8a8646414df0381ab0c664a891
Kevin Cupp <kevin.cupp@gmail.com>
2016-12-09 16:58:56 -0500

    Add MiniGridInput service

---
+++ system/ee/EllisLab/ExpressionEngine/Library/CP/MiniGridInput.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace EllisLab\ExpressionEngine\Library\CP;
+
+/**
+ * ExpressionEngine - by EllisLab
+ *
+ * @package		ExpressionEngine
+ * @author		EllisLab Dev Team
+ * @copyright	Copyright (c) 2003 - 2016, EllisLab, Inc.
+ * @license		https://expressionengine.com/license
+ * @link		https://ellislab.com
+ * @since		Version 3.0
+ * @filesource
+ */
+
+// ------------------------------------------------------------------------
+
+/**
+ * ExpressionEngine Grid Input Class
+ *
+ * @package		ExpressionEngine
+ * @subpackage	Library
+ * @category	CP
+ * @author		EllisLab Dev Team
+ * @link		https://ellislab.com
+ */
+
+class MiniGridInput extends GridInput {
+
+	/**
+	 * Loads necessary JS and CSS, override's parent
+	 */
+	public function loadAssets()
+	{
+		static $assets_loaded;
+
+		if ( ! $assets_loaded)
+		{
+			$this->cp->add_js_script('ui', 'sortable');
+			$this->cp->add_js_script('file', 'cp/grid');
+
+			$assets_loaded = TRUE;
+		}
+
+		$settings = array(
+			'grid_min_rows' => $this->config['grid_min_rows'],
+			'grid_max_rows' => $this->config['grid_max_rows']
+		);
+
+		$name = $this->config['field_name'];
+
+		if (REQ == 'CP')
+		{
+			// Set settings as a global for easy reinstantiation of field
+			// by third parties
+			$this->javascript->set_global('mini_grid_field_settings.'.$name, $settings);
+
+			// getElementById instead of $('#...') for field names that have
+			// brackets in them
+			$this->javascript->output('EE.miniGrid(document.getElementById("'.$name.'"));');
+		}
+	}
+}
+
+// EOF
