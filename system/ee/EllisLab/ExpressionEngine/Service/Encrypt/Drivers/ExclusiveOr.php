d74a0920e0bc0600df05b6237cb47b087fa25e61
Seth Croston Barber <seth.barber@ellislab.com>
2016-12-15 14:20:59 -0800

    Implementing the XOR driver.

--- system/ee/EllisLab/ExpressionEngine/Service/Encrypt/Drivers/ExclusiveOr.php
+++ system/ee/EllisLab/ExpressionEngine/Service/Encrypt/Drivers/ExclusiveOr.php
@@ -29,10 +29,51 @@ class ExclusiveOr implements Driver {
 
 	public function encode($string, $key)
 	{
+		$rand = '';
+		while (strlen($rand) < 32)
+		{
+			$rand .= mt_rand(0, mt_getrandmax());
+		}
+
+		$rand = ee('Encrypt')->hash($rand);
+
+		$enc = '';
+		for ($i = 0; $i < strlen($string); $i++)
+		{
+			$enc .= substr($rand, ($i % strlen($rand)), 1).(substr($rand, ($i % strlen($rand)), 1) ^ substr($string, $i, 1));
+		}
+
+		return $this->merge($enc, $key);
 	}
 
 	public function decode($data, $key)
 	{
+		$string = $this->merge($string, $key);
+
+		$dec = '';
+		for ($i = 0; $i < strlen($string); $i++)
+		{
+			$dec .= (substr($string, $i++, 1) ^ substr($string, $i, 1));
+		}
+
+		return $dec;
+	}
+
+	/**
+	 * XOR key + string Combiner
+	 *
+	 * Takes a string and key as input and computes the difference using XOR
+	 */
+	protected function merge($string, $key)
+	{
+		$hash = ee('Encrypt')->hash($key);
+		$str = '';
+		for ($i = 0; $i < strlen($string); $i++)
+		{
+			$str .= substr($string, $i, 1) ^ substr($hash, ($i % strlen($hash)), 1);
+		}
+
+		return $str;
 	}
 
 }
