695450dab58dc3a7d8b616aa9eaa3c8a0eb1d0af
Kevin Cupp <kevin.cupp@gmail.com>
2016-12-22 16:06:11 -0500

    Remove validation errors from fields hidden by form group JS

--- themes/ee/asset/javascript/src/cp/form_group.js
+++ themes/ee/asset/javascript/src/cp/form_group.js
@@ -83,6 +83,17 @@ $(document).ready(function() {
 function toggleFields(fields, show, key) {
 	toggleInputs(fields, key, show);
 	fields.toggle(show);
+
+	fields.each(function(i, field) {
+		var fieldset = $(field).closest('fieldset');
+
+		if (fieldset.hasClass('invalid')) {
+			if (fieldset.find('input:visible').not('input.btn').size() == 0) {
+				fieldset.removeClass('invalid');
+				fieldset.find('em.ee-form-error-message').remove();
+			}
+		}
+	});
 }
 
 function toggleSections(sections, show, key) {
