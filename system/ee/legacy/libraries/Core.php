40ab853dbec02f259ea5e0e37cae3ac2ee6629e0
Seth Croston Barber <seth.barber@ellislab.com>
2016-12-30 10:08:33 -0800

    Vesion bump!

--- system/ee/legacy/libraries/Core.php
+++ system/ee/legacy/libraries/Core.php
@@ -82,8 +82,8 @@ class EE_Core {
 		// application constants
 		define('IS_CORE',		FALSE);
 		define('APP_NAME',		'ExpressionEngine'.(IS_CORE ? ' Core' : ''));
-		define('APP_BUILD',		'20161213');
-		define('APP_VER',		'3.4.6');
+		define('APP_BUILD',		'20161230');
+		define('APP_VER',		'3.4.7');
 		define('APP_VER_ID',	'');
 		define('SLASH',			'&#47;');
 		define('LD',			'{');
