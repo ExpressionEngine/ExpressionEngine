808c7f0364aff21544d0fb1e0ead28abe9aba337
Robin Sowell <robin.sowell@ellislab.com>
2017-02-24 17:38:44 -0500

    Member field updater working....
    It does not move the data from exp_members to exp_member_data cause was going to bulk update and talking to Seth, his table changes will alter how I can do this- making it much easier.
    
    But does create new fields in exp_member_fields and exp_member_data and drops from exp_members.

--- system/ee/installer/updates/ud_4_00_00.php
+++ system/ee/installer/updates/ud_4_00_00.php
@@ -59,80 +59,58 @@ class Updater {
 		ee()->lang->load('member');
 		ee()->load->model('member_model');
 
+		// Do we need a preflight
+
 		$fields = array(
 			'url' => array(
 				'field_label' => lang('url'),
-				'field_description' => lang('url_desc')
+				'field_description' => lang('url_desc'),
+				'field_type' => 'url'
 					),
 			'location' => array(
 				'field_label' => lang('location'),
-				'field_description' => lang('location_desc')
+				'field_description' => lang('location_desc'),
+				'field_type' => 'text'
 					),
 			'occupation' => array(
 				'field_label' => lang('occupation'),
-				'field_description' => ''
+				'field_description' => '',
+				'field_type' => 'text'
 					),
 			'interests' => array(
 				'field_label' => lang('interests'),
-				'field_description' => ''
-					),
-			'bday_d' => array(
-				'field_label' => lang('birthday'),
-				'field_description' => lang('year')
-					),
-			'bday_m' => array(
-				'field_label' => lang('birthday'),
-				'field_description' => lang('month')
-					),
-			'bday_y' => array(
-				'field_label' => lang('birthday'),
-				'field_description' => lang('day')
+				'field_description' => '',
+				'field_type' => 'text'
 					),
 			'aol_im' => array(
 				'field_label' => lang('mbr_aol_im'),
-				'field_description' => ''
+				'field_description' => '',
+				'field_type' => 'text'
 					),
 			'yahoo_im' => array(
 				'field_label' => lang('yahoo_im'),
-				'field_description' => ''
+				'field_description' => '',
+				'field_type' => 'text'
 					),
 			'msn_im' => array(
 				'field_label' => lang('msn_im'),
-				'field_description' => ''
+				'field_description' => '',
+				'field_type' => 'text'
 					),
 			'icq' => array(
 				'field_label' => lang('icq'),
-				'field_description' => ''
+				'field_description' => '',
+				'field_type' => 'text'
 					),
 			'bio' => array(
 				'field_label' => lang('biography'),
-				'field_description' => lang('biography_desc')
-					),
-
-		// For any fields that have content, create a custom field
-
-		// SELECT MAX(url), MAX(location) etc.
-		// OR
-		// Loop the array SELECT MAX or SELECT COUNT WHERE url = ''
-		// OR
-/*
-(
-  SELECT GROUP_CONCAT(member_id)
-     FROM exp_members
-    WHERE `url` IS NULL
-       OR `url` = ''
-) `url`,
-(
-  SELECT GROUP_CONCAT(member_id)
-    FROM exp_members
-   WHERE `location` IS NULL
-) `location2`,
-
-
-*/
-
-
-		// Let's just pick one for now
+				'field_description' => lang('biography_desc'),
+				'field_type' => 'textarea'
+				),
+			'bday_d' => array(),
+			'bday_m' => array(),
+			'bday_y' => array()
+			);
 
 		foreach ($fields as $field => $data)
 		{
@@ -140,19 +118,47 @@ class Updater {
 		}
 
 		$query = ee()->db->get('members');
+		$move = $query->row_array();
 
-		// Double check what this does with null
-		$move = array_filter($query->result_array());
+		// Removes all false and null, including 0
+		$move = array_filter($move);
 
 		if (empty($move))
 		{
 			return;
 		}
 
-		// Create custom fields
-		foreach ($fields as $name => $data)
+		// If they have any birthday fields, we'll create a birthday variable
+		$birthday = FALSE;
+		foreach (array('bday_d', 'bday_m', 'bday_y') as $bday)
 		{
-			if ( ! in_array($name, $move))
+			if (array_key_exists($bday, $move))
+			{
+				$fields['birthday'] = array(
+					'field_label' => lang('birthday'),
+					'field_description' => '',
+					'field_type' => 'date'
+				);
+
+				$move['birthday'] = TRUE;
+
+				$birthday = TRUE;
+			}
+
+			unset($move[$bday]);
+		}
+
+
+		// Safety check- does field already exist in exp_member_fields
+		$existing = ee('Model')->get('MemberField')->fields('m_field_name')->all()->pluck('m_field_name');
+
+
+		ee()->load->library('api');
+
+		// Create custom fields
+		foreach ($move as $name => $val)
+		{
+			if (in_array($name, $existing) OR in_array($name, array('bday_d', 'bday_m', 'bday_y')))
 			{
 				continue;
 			}
@@ -160,32 +166,26 @@ class Updater {
 			$new_fields[] = $name;
 
 			$field = ee('Model')->make('MemberField');
-			$field->field_type = 'text';
-			$field->field_label = $data['field_label'];
-			$field->field_name = $name;
-			$field->description = $data['field_description'];
+
+			$field->m_field_type = $fields[$name]['field_type'];
+
+
+			$field->m_field_label = $fields[$name]['field_label'];
+			$field->m_field_name = $name;
+			$field->m_field_description = $fields[$name]['field_description'];
 
 			$field->save();
 		}
 
 
 		// Copy custom field data
+		// Seth can do this via select into query- need to merge.
 
-
-		$m_data = ee()->db->select('member_id, '. implode(', ' $new_fields))
-			->ee()->db->get('members')
-			->result_array();
-
-
-		ee()->db->update_batch('member_fields', $m_data, 'member_id');
-
-
-		// Drop rows from exp_members
+		// Drop columns from exp_members
 		foreach ($fields as $field => $data)
 		{
 			ee()->smartforge->drop_column('members', $field);
 		}
-
 
 	}
 }
