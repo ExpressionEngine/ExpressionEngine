e8b89b8d2a7c0d38dd5d1b9b8f537b4fe34ccea7
Robin Sowell <robin.sowell@ellislab.com>
2017-03-03 15:45:11 -0500

    Tweaked so it skips legacy member fields.
    Needs an eyeball from @seth but otherwise dates are still parsing and such- yay.

--- system/ee/EllisLab/Addons/comment/mod.comment.php
+++ system/ee/EllisLab/Addons/comment/mod.comment.php
@@ -571,12 +571,13 @@ class Comment {
 
 		$results = $result_ids;
 		$mfields = array();
+		$mfields_data = array();
 
 		/** ----------------------------------------
 		/**  Fetch custom member field IDs
 		/** ----------------------------------------*/
 
-		ee()->db->select('m_field_id, m_field_name');
+		ee()->db->select('m_field_id, m_field_name, m_legacy_field_data');
 		$query = ee()->db->get('member_fields');
 
 		if ($query->num_rows() > 0)
@@ -584,6 +585,7 @@ class Comment {
 			foreach ($query->result_array() as $row)
 			{
 				$mfields[$row['m_field_name']] = $row['m_field_id'];
+				$mfields_data[$row['m_field_id']] = $row['m_legacy_field_data'];
 			}
 		}
 
@@ -604,8 +606,13 @@ class Comment {
 			channel_titles.title, channel_titles.url_title, channel_titles.author_id AS entry_author_id, channel_titles.allow_comments, channel_titles.comment_expiration_date,
 			channels.comment_text_formatting, channels.comment_html_formatting, channels.comment_allow_img_urls, channels.comment_auto_link_urls, channels.channel_url, channels.comment_url, channels.channel_title, channels.channel_name AS channel_short_name, channels.comment_system_enabled';
 
-		foreach ($mfields as $field_id)
+		foreach ($mfields_data as $field_id => $legacy_field)
 		{
+			if ($legacy_field == 'y')
+			{
+				continue;
+			}
+			
 			$table = "exp_member_data_field_{$field_id}";
 			$select .= ", {$table}.data AS m_field_id_{$field_id}";
 			$select .= ", {$table}.metadata AS m_field_ft_{$field_id}";
@@ -618,8 +625,13 @@ class Comment {
 		ee()->db->join('members',			'members.member_id = comments.author_id',		'left');
 		ee()->db->join('member_data',		'member_data.member_id = members.member_id',	'left');
 
-		foreach ($mfields as $field_id)
+		foreach ($mfields_data as $field_id => $legacy_field)
 		{
+			if ($legacy_field == 'y')
+			{
+				continue;
+			}
+
 			$table = "exp_member_data_field_{$field_id}";
 			ee()->db->join($table, "{$table}.member_id = members.member_id", 'left');
 		}
