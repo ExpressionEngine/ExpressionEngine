d2492108756460160dd10757deeb7252117656e1
Kevin Cupp <kevin.cupp@gmail.com>
2017-03-14 19:55:40 -0400

    Get rid of defunct bootstrapping code in updater microapp

--- system/ee/installer/updater/boot.php
+++ system/ee/installer/updater/boot.php
@@ -9,6 +9,7 @@ if (file_exists(SYSPATH.'ee/EllisLab/ExpressionEngine/Boot/boot.php') &&
 	(strpos($_GET['step'], 'backupDatabase') === 0 OR
 		strpos($_GET['step'], 'updateDatabase') === 0 OR
 		$_GET['step'] == 'checkForDbUpdates' OR
+		$_GET['step'] == 'restoreDatabase' OR
 		$_GET['step'] == 'selfDestruct'))
 {
 	define('BOOT_ONLY', TRUE);
@@ -16,89 +17,16 @@ if (file_exists(SYSPATH.'ee/EllisLab/ExpressionEngine/Boot/boot.php') &&
 }
 else
 {
-	if ( ! function_exists('is_php'))
-	{
-		require __DIR__.'/EllisLab/ExpressionEngine/Updater/Boot/boot.common.php';
-	}
-
-	/*
-	 * ------------------------------------------------------
-	 *  Constants
-	 * ------------------------------------------------------
-	 */
-	if ( ! defined('BASEPATH') && file_exists($autoloader_path = SYSPATH . 'ee/EllisLab/ExpressionEngine/Core/Autoloader.php'))
+	if ( ! defined('BASEPATH'))
 	{
 		define('BASEPATH', SYSPATH.'ee/legacy/');
+		define('PATH_CACHE',  SYSPATH . 'user/cache/');
 		define('FILE_READ_MODE', 0644);
 		define('FILE_WRITE_MODE', 0666);
 		define('DIR_READ_MODE', 0755);
 		define('DIR_WRITE_MODE', 0777);
 
-		define('APPPATH',  BASEPATH);
-		define('PATH_THIRD',  SYSPATH.'user/addons/');
-		define('PATH_ADDONS',  SYSPATH.'ee/EllisLab/Addons/');
-		define('PATH_CACHE',  SYSPATH . 'user/cache/');
-
-		define('LD', '{');
-		define('RD', '}');
-
-		define('IS_CORE', FALSE);
-		define('FIXTURE', TRUE);
-
-		require $autoloader_path;
-
-		$autoloader = EllisLab\ExpressionEngine\Core\Autoloader::getInstance()
-			->addPrefix('EllisLab', SYSPATH.'ee/EllisLab/');
-		$autoloader->register();
-
-		$di = new EllisLab\ExpressionEngine\Service\Dependency\InjectionContainer();
-		$reg = new EllisLab\ExpressionEngine\Core\ProviderRegistry($di);
-		$app = new EllisLab\ExpressionEngine\Core\Application(
-			$autoloader,
-			$di,
-			$reg
-		);
-
-		$provider = $app->addProvider(
-			SYSPATH.'ee/EllisLab/ExpressionEngine',
-			'app.setup.php',
-			'ee'
-		);
-
-		$provider->setConfigPath(SYSPATH.'user/config');
-
-		$di->register('App', function($di, $prefix = NULL) use ($app)
-		{
-			if (isset($prefix))
-			{
-				return $app->get($prefix);
-			}
-
-			return $app;
-		});
-
-		function ee($dep = NULL)
-		{
-			if (isset($dep))
-			{
-				global $di;
-				return call_user_func_array(array($di, 'make'), func_get_args());
-			}
-			static $EE;
-			if ( ! $EE)	$EE = new stdClass();
-			return $EE;
-		}
-
-		function get_instance()
-		{
-			return ee();
-		}
-
-		ee()->di = $di;
-
-		// DB Stuff
-		require_once(BASEPATH.'database/DB.php');
-		ee()->db = DB('', NULL);
+		require __DIR__.'/EllisLab/ExpressionEngine/Updater/Boot/boot.common.php';
 	}
 }
 
@@ -121,8 +49,13 @@ else
  * ------------------------------------------------------
  */
 
-	if (php_sapi_name() != 'cli')
+	if (REQ != 'CLI')
 	{
+		if (strtoupper($_SERVER['REQUEST_METHOD']) !== 'POST')
+		{
+			exit('The updater folder is still present. Delete the folder at system/ee/updater to access the control panel.');
+		}
+
 		$directory = (isset($_GET['D']) && $_GET['D'] !== 'cp') ? $_GET['D'] : 'updater';
 		$controller = (isset($_GET['C'])) ? $_GET['C'] : 'updater';
 		$method = (isset($_GET['M'])) ? $_GET['M'] : 'index';
@@ -142,12 +75,6 @@ else
 			echo json_encode($return);
 			exit;
 		}
-	}
-	else {
-		set_error_handler(function ($code, $message, $file, $line)
-		{
-			throw new Exception($message."\n\n".$file.':'.$line);
-		});
 	}
 
 	function routeRequest($directory, $controller, $method = '')
