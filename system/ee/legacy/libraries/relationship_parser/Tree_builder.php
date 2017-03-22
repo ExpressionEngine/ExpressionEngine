d821e2fc02b7626ca826ad24e7bc328808efee07
Kevin Cupp <kevin.cupp@gmail.com>
2017-03-22 15:49:25 -0400

    Fixed a bug (#23029) where using a Relationship shortcut tag inside a Grid tag pair would not parse when it was the only Relationship field in the channel

--- system/ee/legacy/libraries/relationship_parser/Tree_builder.php
+++ system/ee/legacy/libraries/relationship_parser/Tree_builder.php
@@ -231,8 +231,8 @@ class EE_relationship_tree_builder {
 			return NULL;
 		}
 
-		$all_fields = $this->relationship_field_names;
-		$all_fields = implode('|', $all_fields).'|parents|siblings';
+		$all_fields = array_merge($this->relationship_field_names, array('parents', 'siblings'));
+		$all_fields = implode('|', $all_fields);
 
 		// Regex to separate out the relationship prefix part from the rest
 		// {rel:pre:fix:tag:modified param="value"}
