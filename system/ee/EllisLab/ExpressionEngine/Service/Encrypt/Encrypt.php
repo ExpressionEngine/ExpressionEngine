77158d176ec9ad457ae06438fc735bdfac3594e8
Kevin Cupp <kevin.cupp@gmail.com>
2017-03-31 15:29:17 -0400

    `PHP_INT_MIN` not available until 7.0

--- system/ee/EllisLab/ExpressionEngine/Service/Encrypt/Encrypt.php
+++ system/ee/EllisLab/ExpressionEngine/Service/Encrypt/Encrypt.php
@@ -286,7 +286,7 @@ class Encrypt {
 	 */
 	public function generateKey()
 	{
-		return sha1(uniqid(random_int(PHP_INT_MIN, PHP_INT_MAX), TRUE));
+		return sha1(uniqid(random_int(-PHP_INT_MAX, PHP_INT_MAX), TRUE));
 	}
 }
 
