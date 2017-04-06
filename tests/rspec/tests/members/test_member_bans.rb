ce79565a6e83658a0036e8056a3b01f749a664ea
Robin Sowell <robin.sowell@ellislab.com>
2017-04-06 17:24:09 -0400

    Whew- last tweak for banned member test.
    Copy paste is not always my friend.

--- tests/rspec/tests/members/test_member_bans.rb
+++ tests/rspec/tests/members/test_member_bans.rb
@@ -13,24 +13,23 @@ feature 'Banned Member List' do
     @page.should have_member_table
   end
 
-
   # Confirming phrase search
   it 'searches by phrases' do
-      @page.phrase_search.set 'banned1'
-      @page.search_submit_button.click
-      no_php_js_errors
+    @page.phrase_search.set 'banned1'
+    @page.search_submit_button.click
+    no_php_js_errors
 
-      @page.first_party_heading.text.should eq 'Search Results we found 1 results for "banned1"'
-      @page.phrase_search.value.should eq 'banned1'
-      @page.should have_text 'banned1'
-      @page.should have(1).members
+    @page.heading.text.should eq 'Search Results we found 1 results for "banned1"'
+    @page.phrase_search.value.should eq 'banned1'
+    @page.should have_text 'banned1'
+    @page.should have(1).members
   end
 
   it 'shows no results on a failed search'  do
-      @page.phrase_search.set 'admin'
-      @page.search_submit_button.click
+    @page.phrase_search.set 'admin'
+    @page.search_submit_button.click
 
-    @page.first_party_heading.text.should eq 'Search Results we found 0 results for "admin"'
+    @page.heading.text.should eq 'Search Results we found 0 results for "admin"'
     @page.phrase_search.value.should eq 'admin'
     @page.should have_no_results
     @page.should_not have_pagination
