f7b85f6f2ed4b719ff02218f6b329e39d48e08c7
Robin Sowell <robin.sowell@ellislab.com>
2017-04-06 17:41:57 -0400

    Seriously?  2 != 0 typo.  Fix for banned member modal test

--- tests/rspec/tests/members/test_member_bans.rb
+++ tests/rspec/tests/members/test_member_bans.rb
@@ -38,7 +38,7 @@ feature 'Banned Member List' do
    it 'displays an itemzied modal when attempting to remove 1 member' do
     member_name = @page.usernames[0].text
 
-    @page.members[1].find('input[type="checkbox"]').set true
+    @page.members[0].find('input[type="checkbox"]').set true
     @page.wait_until_bulk_action_visible
     @page.bulk_action.select "Remove"
     @page.action_submit_button.click
