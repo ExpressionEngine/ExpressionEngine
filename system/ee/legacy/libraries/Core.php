b6ae7d8075e95f935882f03942689e6b0b76e179
Robin <robin.sowell@ellislab.com>
2017-05-17 15:30:51 -0400

    version bump to correct repo

--- system/ee/legacy/libraries/Core.php
+++ system/ee/legacy/libraries/Core.php
@@ -82,7 +82,7 @@ class EE_Core {
 		// application constants
 		define('IS_CORE',		FALSE);
 		define('APP_NAME',		'ExpressionEngine'.(IS_CORE ? ' Core' : ''));
-		define('APP_BUILD',		'20170508');
+		define('APP_BUILD',		'20170517');
 		define('APP_VER',		'3.5.7');
 		define('APP_VER_ID',	'');
 		define('SLASH',			'&#47;');
