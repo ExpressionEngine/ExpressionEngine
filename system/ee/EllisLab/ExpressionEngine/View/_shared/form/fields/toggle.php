f56d9153fe6ea618191fd8fac002907566a4500b
Kevin Cupp <kevin.cupp@gmail.com>
2017-08-03 12:01:20 -0400

    Fix bug where Toggle always expected group toggle info

--- system/ee/EllisLab/ExpressionEngine/View/_shared/form/fields/toggle.php
+++ system/ee/EllisLab/ExpressionEngine/View/_shared/form/fields/toggle.php
@@ -5,7 +5,7 @@ if (isset($yes_no) && $yes_no)
 }
 ?>
 <a href="#" class="toggle-btn <?=(get_bool_from_string($value)) ? 'on' : 'off' ?> <?=(isset($yes_no) && $yes_no) ? 'yes_no' : ''?> <?=($disabled) ? 'disabled' : ''?>" data-toggle-for="<?=$field_name?>">
-	<input type="hidden" name="<?=$field_name?>" value="<?=form_prep($value, $field_name)?>"<?php if ($group_toggle): ?> data-group-toggle='<?=json_encode($group_toggle)?>'<?php endif ?>>
+	<input type="hidden" name="<?=$field_name?>" value="<?=form_prep($value, $field_name)?>"<?php if (isset($group_toggle)): ?> data-group-toggle='<?=json_encode($group_toggle)?>'<?php endif ?>>
 	<span class="slider"></span>
 	<span class="option"></span>
 </a>
