8188a1d8c35db8461acf256d6cb2b8cb5f5ac5a7
Kevin Cupp <kevin.cupp@gmail.com>
2017-09-14 19:05:12 -0400

    Use MutableSelectField for Menu Sets

--- themes/ee/asset/javascript/src/cp/settings/menu-manager/edit.js
+++ themes/ee/asset/javascript/src/cp/settings/menu-manager/edit.js
@@ -6,70 +6,14 @@
  * @license   https://expressionengine.com/license
  */
 
-var MenuSets = {
+(function($) {
 
-	addButton: $('a[rel=modal-menu-item]'),
-
-	init: function() {
-		this.listContainer = this.addButton.parent()
-		this.bindAdd()
-		this.bindEdit()
-		this.bindRemove()
-	},
-
-	bindAdd: function() {
-		var that = this
-		this.addButton.on('click', function(e) {
-			e.preventDefault()
-			that.openForm(EE.item_create_url)
-		})
-	},
-
-	bindEdit: function() {
-		var that = this
-		this.listContainer.on('click', '.nestable-item label > a', function(e) {
-			e.preventDefault()
-			var itemId = $(this).closest('.nestable-item').data('id')
-			that.openForm(EE.item_edit_url.replace('###', itemId))
-		})
-	},
-
-	bindRemove: function() {
-		var modal = $('.modal-menu-confirm-remove'),
-			that = this
-
-		this.listContainer.on('select:removeItem', 'li', function(e, item) {
-			EE.cp.Modal.openConfirmRemove(
-				'modal-menu-confirm-remove',
-				'item_id',
-				item.label,
-				item.value,
-				function(result) {
-					that.replaceList(result.reorder_list)
-				}
-			)
-		})
-	},
-
-	openForm: function(url) {
-		var that = this
-		EE.cp.ModalForm.openForm({
-			url: url,
-			load: function(modal) {
-				EE.cp.form_group_toggle(modal.find('input[data-group-toggle]:checked'))
-				EE.grid(document.getElementById("submenu"), EE.grid_field_settings['submenu'])
-			},
-			success: function(result) {
-				that.replaceList(result.reorder_list)
-			}
-		})
-	},
-
-	replaceList: function(listHtml) {
-		this.listContainer.find('[data-select-react]').remove()
-		this.listContainer.prepend(listHtml)
-		SelectField.renderFields(this.listContainer)
+options = {
+	onFormLoad: function(modal) {
+		EE.grid(document.getElementById('submenu'), EE.grid_field_settings['submenu'])
 	}
 }
 
-MenuSets.init()
+var menuSetItems = new MutableSelectField('menu_items', Object.assign(EE.menuSetsItem, options))
+
+})(jQuery);
