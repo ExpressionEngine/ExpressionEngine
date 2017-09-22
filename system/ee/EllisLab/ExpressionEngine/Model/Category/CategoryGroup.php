ca3fbc853cf28a49319cd4457e2f36a1e8d2896c
Kevin Cupp <kevin.cupp@gmail.com>
2017-09-22 17:55:14 -0400

    Add model validation to CategoryGroup model

--- system/ee/EllisLab/ExpressionEngine/Model/Category/CategoryGroup.php
+++ system/ee/EllisLab/ExpressionEngine/Model/Category/CategoryGroup.php
@@ -38,6 +38,13 @@ class CategoryGroup extends StructureModel {
 		),
 	);
 
+	protected static $_validation_rules = array(
+		'group_name'            => 'required|unique[site_id]',
+		'sort_order'            => 'isNatural',
+		'field_html_formatting' => 'enum[all,safe,none]',
+		'exclude_group'         => 'enum[0,1,2]'
+	);
+
 	// Properties
 	protected $group_id;
 	protected $site_id;
