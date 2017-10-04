8f07e06b44ae5a627761595897321c2f40519702
Seth Croston Barber <seth.barber@ellislab.com>
2017-10-04 14:08:36 -0700

    :memo: More docblockery
    [ci skip]

--- system/ee/EllisLab/Addons/fluid_block/Service/Tag.php
+++ system/ee/EllisLab/Addons/fluid_block/Service/Tag.php
@@ -1,20 +1,15 @@
 <?php
+/**
+ * ExpressionEngine (https://expressionengine.com)
+ *
+ * @link      https://expressionengine.com/
+ * @copyright Copyright (c) 2003-2017, EllisLab, Inc. (https://ellislab.com)
+ * @license   https://expressionengine.com/license
+ */
 
 namespace EllisLab\Addons\FluidBlock\Service;
 
 use EllisLab\ExpressionEngine\Model\Content\FieldFacade;
-
-/**
- * ExpressionEngine - by EllisLab
- *
- * @package		ExpressionEngine
- * @author		EllisLab Dev Team
- * @copyright	Copyright (c) 2003 - 2016, EllisLab, Inc.
- * @license		https://expressionengine.com/license
- * @link		https://ellislab.com
- * @since		Version 3.0
- * @filesource
- */
 
 /**
  * A tag for a fieldtype within a Fluid Block
--- system/ee/legacy/libraries/Fluid_block_parser.php
+++ system/ee/legacy/libraries/Fluid_block_parser.php
@@ -1,29 +1,16 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 /**
- * ExpressionEngine - by EllisLab
+ * ExpressionEngine (https://expressionengine.com)
  *
- * @package		ExpressionEngine
- * @author		EllisLab Dev Team
- * @copyright	Copyright (c) 2003 - 2016, EllisLab, Inc.
- * @license		https://expressionengine.com/license
- * @link		https://ellislab.com
- * @since		Version 4.0
- * @filesource
+ * @link      https://expressionengine.com/
+ * @copyright Copyright (c) 2003-2017, EllisLab, Inc. (https://ellislab.com)
+ * @license   https://expressionengine.com/license
  */
-
-// ------------------------------------------------------------------------
 
 /**
- * ExpressionEngine Fluid Block parser Class
- *
- * @package		ExpressionEngine
- * @subpackage	Libraries
- * @category	Modules
- * @author		EllisLab Dev Team
- * @link		https://ellislab.com
+ * Fluid Block Parser
  */
-
 class Fluid_block_parser {
 
 	public $modifiers = array();
