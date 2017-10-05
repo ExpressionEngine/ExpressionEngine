29d9fc5c9edc8f029fbd53f78b66fce74e60350e
Derek Jones <derek.jones@ellislab.com>
2017-10-05 16:52:54 -0700

    Get modifiers working for category fields

--- system/ee/legacy/libraries/channel_entries_parser/components/Category.php
+++ system/ee/legacy/libraries/channel_entries_parser/components/Category.php
@@ -179,6 +179,15 @@ class EE_Channel_category_parser implements EE_Channel_parser_component {
 						$temp = preg_replace("#".LD."path=.+?".RD."#", ee()->functions->create_url("SITE_INDEX"), $temp);
 					}
 
+					// super confusing, so documenting this legacy array here:
+					// $v[0] = cat_id
+					// $v[1] = parent_id
+					// $v[2] = cat_name
+					// $v[3] = cat_image
+					// $v[4] = cat_description
+					// $v[5] = group_id
+					// $v[6] = cat_url_title
+
 					ee()->load->library('file_field');
 					$cat_image = ee()->file_field->parse_field($v[3]);
 
@@ -200,10 +209,11 @@ class EE_Channel_category_parser implements EE_Channel_parser_component {
 
 					$cond = $cat_vars;
 
-					// add custom fields for conditionals prep
+					// add custom fields for conditionals prep and parsing
 					foreach ($obj->channel()->catfields as $cv)
 					{
 						$cond[$cv['field_name']] = ( ! isset($v['field_id_'.$cv['field_id']])) ? '' : $v['field_id_'.$cv['field_id']];
+						$cat_vars[$cv['field_name']] = $cond[$cv['field_name']];
 					}
 
 					$temp = ee()->functions->prep_conditionals($temp, $cond);
@@ -215,7 +225,7 @@ class EE_Channel_category_parser implements EE_Channel_parser_component {
 					}
 
 					$variables = ee('Variables/Parser')->extractVariables($temp);
-					$temp = $obj->channel()->parseCategoryFields($v[0], $v, $temp, array_keys($variables['var_single']));
+					$temp = $obj->channel()->parseCategoryFields($v[0], array_merge($v, $cat_vars), $temp, array_keys($variables['var_single']));
 
 					$cats .= $temp;
 
