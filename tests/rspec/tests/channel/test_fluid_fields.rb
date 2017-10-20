674d09983495c4959d1dc630b2dcc3641f5a5d8e
Seth Croston Barber <seth.barber@ellislab.com>
2017-10-20 09:46:45 -0700

    Renaming the test.

--- tests/rspec/tests/channel/test_fluid_fields.rb
+++ tests/rspec/tests/channel/test_fluid_fields.rb
@@ -1,6 +1,6 @@
 require './bootstrap.rb'
 
-feature 'Fluid Field Field' do
+feature 'Fluid Fields' do
   before(:each) do
     cp_session
     @list = ChannelFields.new
@@ -9,10 +9,10 @@ feature 'Fluid Field Field' do
     no_php_js_errors
   end
 
-  it 'creates a fluid block field' do
+  it 'creates a fluid field' do
     @page.field_type.select 'Fluid Field'
     @page.field_label.set 'Fluid Field Body'
-    @page.field_name.set 'fluid_block_body'
+    @page.field_name.set 'fluid_field_body'
     find("input[type='checkbox'][name='field_channel_fields[]'][value='1']").click
     find("input[type='checkbox'][name='field_channel_fields[]'][value='2']").click
     find("input[type='checkbox'][name='field_channel_fields[]'][value='3']").click
@@ -24,12 +24,12 @@ feature 'Fluid Field Field' do
     @list.alert.has_content?('The field Fluid Field Body has been').should == true
 
     @list.fields.any? { |f| f.text.include?('Fluid Field Body') }.should == true
-    @list.fields.any? { |f| f.text.include?('{fluid_block_body}') }.should == true
+    @list.fields.any? { |f| f.text.include?('{fluid_field_body}') }.should == true
 
     @page.load_edit_for_custom_field('Fluid Field Body')
-    @page.field_type.value.should eq 'fluid_block'
+    @page.field_type.value.should eq 'fluid_field'
     @page.field_label.value.should eq 'Fluid Field Body'
-    @page.field_name.value.should eq 'fluid_block_body'
+    @page.field_name.value.should eq 'fluid_field_body'
     find("input[type='checkbox'][name='field_channel_fields[]'][value='1']").checked?.should == true
     find("input[type='checkbox'][name='field_channel_fields[]'][value='2']").checked?.should == true
     find("input[type='checkbox'][name='field_channel_fields[]'][value='3']").checked?.should == true
@@ -39,11 +39,11 @@ feature 'Fluid Field Field' do
     find("input[type='checkbox'][name='field_channel_fields[]'][value='7']").checked?.should == true
   end
 
-  context 'when editing a fluid block field' do
-    it 'can add a new field to the block' do
+  context 'when editing a fluid field' do
+    it 'can add a new field to the fluid field' do
       @page.field_type.select 'Fluid Field'
       @page.field_label.set 'Fluid Field Body'
-      @page.field_name.set 'fluid_block_body'
+      @page.field_name.set 'fluid_field_body'
       find("input[type='checkbox'][name='field_channel_fields[]'][value='2']").click
       find("input[type='checkbox'][name='field_channel_fields[]'][value='3']").click
       find("input[type='checkbox'][name='field_channel_fields[]'][value='5']").click
@@ -76,10 +76,10 @@ feature 'Fluid Field Field' do
       find("input[type='checkbox'][name='field_channel_fields[]'][value='7']").checked?.should == true
     end
 
-    it 'can remove a field from the block' do
+    it 'can remove a field from the fluid field' do
       @page.field_type.select 'Fluid Field'
       @page.field_label.set 'Fluid Field Body'
-      @page.field_name.set 'fluid_block_body'
+      @page.field_name.set 'fluid_field_body'
       find("input[type='checkbox'][name='field_channel_fields[]'][value='1']").click
       find("input[type='checkbox'][name='field_channel_fields[]'][value='2']").click
       find("input[type='checkbox'][name='field_channel_fields[]'][value='3']").click
@@ -116,10 +116,10 @@ feature 'Fluid Field Field' do
     end
   end
 
-  it 'deletes a fluid block field' do
+  it 'deletes a fluid field' do
     @page.field_type.select 'Fluid Field'
     @page.field_label.set 'Fluid Field Body'
-    @page.field_name.set 'fluid_block_body'
+    @page.field_name.set 'fluid_field_body'
     find("input[type='checkbox'][name='field_channel_fields[]'][value='1']").click
     find("input[type='checkbox'][name='field_channel_fields[]'][value='2']").click
     find("input[type='checkbox'][name='field_channel_fields[]'][value='3']").click
