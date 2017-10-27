1399be68e4dcd5b5017fa040748bbe53832c48a5
Derek Jones <derek.jones@ellislab.com>
2017-10-26 17:59:54 -0700

    Apply radio helpers to settings/debug-output tests

--- tests/rspec/pages/settings/debug_output.rb
+++ tests/rspec/pages/settings/debug_output.rb
@@ -1,9 +1,7 @@
 class DebugOutput < ControlPanelPage
 
   # This is ridiculous
-  element :debug_2, 'input[name=debug][value="2"]'
-  element :debug_1, 'input[name=debug][value="1"]'
-  element :debug_0, 'input[name=debug][value="0"]'
+  elements :debug, 'input[name=debug]'
   element :show_profiler_toggle, 'a[data-toggle-for=show_profiler]'
   element :show_profiler, 'input[name=show_profiler]', :visible => false
   element :enable_devlog_alerts_toggle, 'a[data-toggle-for=enable_devlog_alerts]'
@@ -15,10 +13,7 @@ class DebugOutput < ControlPanelPage
   element :send_headers_toggle, 'a[data-toggle-for=send_headers]'
   element :send_headers, 'input[name=send_headers]', :visible => false
   elements :redirect_method, 'input[name=redirect_method]'
-  element :redirect_method_checked, 'input[name=redirect_method][checked="checked"]'
   elements :cache_driver, 'input[name=cache_driver]'
-  element :cache_driver_checked, 'input[name=cache_driver][checked="checked"]'
-  element :cache_driver_memcached, 'input[name=cache_driver][value="memcached"]'
   element :max_caches, 'input[name=max_caches]'
 
   def load
--- tests/rspec/tests/settings/test_debug_output.rb
+++ tests/rspec/tests/settings/test_debug_output.rb
@@ -22,16 +22,14 @@ feature 'Debugging & Output Settings' do
     send_headers = ee_config(item: 'send_headers')
 
     # This is ridiculous -- testing *each* radio button's status
-    @page.debug_2.checked?.should == (debug == '2')
-    @page.debug_1.checked?.should == (debug == '1')
-    @page.debug_0.checked?.should == (debug == '0')
+    @page.debug.has_checked_radio(debug)
     @page.show_profiler.value.should == show_profiler
     @page.enable_devlog_alerts.value.should == enable_devlog_alerts
     @page.gzip_output.value.should == gzip_output
     @page.force_query_string.value.should == force_query_string
     @page.send_headers.value.should == send_headers
-    @page.redirect_method_checked.value.should == ee_config(item: 'redirect_method')
-    @page.cache_driver_checked.value.should == ee_config(item: 'cache_driver')
+    @page.redirect_method.has_checked_radio(ee_config(item: 'redirect_method'))
+    @page.cache_driver.has_checked_radio(ee_config(item: 'cache_driver'))
     @page.max_caches.value.should == ee_config(item: 'max_caches')
   end
 
@@ -67,24 +65,24 @@ feature 'Debugging & Output Settings' do
     force_query_string = ee_config(item: 'force_query_string')
     send_headers = ee_config(item: 'send_headers')
 
-    @page.debug_0.click
+    @page.debug.choose_radio_option('0')
     @page.show_profiler_toggle.click
     @page.enable_devlog_alerts_toggle.click
     @page.gzip_output_toggle.click
     @page.force_query_string_toggle.click
     @page.send_headers_toggle.click
-    @page.cache_driver_memcached.click
+    @page.cache_driver.choose_radio_option('memcached')
     @page.max_caches.set '300'
     @page.submit
 
     @page.should have_text 'Preferences updated'
-    @page.debug_0.checked?.should == true
+    @page.debug.has_checked_radio('0')
     @page.show_profiler.value.should_not == show_profiler
     @page.enable_devlog_alerts.value.should_not == enable_devlog_alerts
     @page.gzip_output.value.should_not == gzip_output
     @page.force_query_string.value.should_not == force_query_string
     @page.send_headers.value.should_not == send_headers
-    @page.cache_driver_memcached.checked?.should == true
+    @page.cache_driver.has_checked_radio('memcached')
     @page.max_caches.value.should == '300'
 
     # Should show a message when the selected caching driver
