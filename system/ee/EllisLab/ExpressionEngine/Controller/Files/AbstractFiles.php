ce4ab6d1ac00afe99cce708f3707171dbec777b9
Kevin Cupp <kevin.cupp@gmail.com>
2017-10-27 15:23:04 -0400

    Show Upload File button accordingly based on number of upload folders

--- system/ee/EllisLab/ExpressionEngine/Controller/Files/AbstractFiles.php
+++ system/ee/EllisLab/ExpressionEngine/Controller/Files/AbstractFiles.php
@@ -114,6 +114,7 @@ abstract class AbstractFiles extends CP_Controller {
 
 	protected function stdHeader($active = NULL)
 	{
+		$upload_destinations = [];
 		if (ee()->cp->allowed_group('can_upload_new_files'))
 		{
 			$upload_destinations = ee('Model')->get('UploadDestination')
@@ -157,10 +158,11 @@ abstract class AbstractFiles extends CP_Controller {
 		ee()->view->header = array(
 			'title' => lang('file_manager'),
 			'toolbar_items' => $toolbar_items,
-			'action_button' => ee()->cp->allowed_group('can_upload_new_files') ? [
+			'action_button' => ee()->cp->allowed_group('can_upload_new_files') && $upload_destinations->count() ? [
 				'text' => lang('upload_file'),
 				'filter_placeholder' => lang('filter_upload_directories'),
-				'choices' => $choices
+				'choices' => count($choices) > 1 ? $choices : NULL,
+				'href' => ee('CP/URL')->make('files/upload/' . $upload_destinations->first()->getId())->compile()
 			] : NULL
 		);
 	}
