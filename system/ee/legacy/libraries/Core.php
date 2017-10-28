050bc4039139d5295dbf3a43b3326c9cd3134336
Derek Jones <derek.jones@ellislab.com>
2017-10-27 18:41:56 -0700

    Version bump for 4.0.0-dp.3

--- system/ee/legacy/libraries/Core.php
+++ system/ee/legacy/libraries/Core.php
@@ -69,9 +69,9 @@ class EE_Core {
 		// application constants
 		define('IS_CORE',		FALSE);
 		define('APP_NAME',		'ExpressionEngine'.(IS_CORE ? ' Core' : ''));
-		define('APP_BUILD',		'20171025');
-		define('APP_VER',		'4.0.0-dp.2');
-		define('APP_VER_ID',	'dp.2');
+		define('APP_BUILD',		'20171027');
+		define('APP_VER',		'4.0.0-dp.3');
+		define('APP_VER_ID',	'dp.3');
 		define('SLASH',			'&#47;');
 		define('LD',			'{');
 		define('RD',			'}');
