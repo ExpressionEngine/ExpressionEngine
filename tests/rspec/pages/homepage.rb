9bda961bcb35157e8de6524c15ed527f5f9890ad
Seth Croston Barber <seth.barber@ellislab.com>
2017-11-13 10:52:33 -0800

    Adjusting the dom selectors for the homepage

--- tests/rspec/pages/homepage.rb
+++ tests/rspec/pages/homepage.rb
@@ -1,7 +1,7 @@
 class Homepage < ControlPanelPage
-  element :home, 'a.home'
-  element :comment_box, '.home-layout .col-group:nth-child(2) .box'
-  element :comment_info, '.home-layout .col-group:nth-child(2) .box .info'
+  element :home, 'a.nav-home'
+  element :comment_box, '.home-layout .col-group:nth-child(2) .col:first-child .box'
+  element :comment_info, '.home-layout .col-group:nth-child(2) .col:first-child .box .info'
 
   def load
     home.click
