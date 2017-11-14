7e968eb1f6b8e22f81e5ec9ee46b5415d12f8752
Derek Jones <derek.jones@ellislab.com>
2017-11-14 14:25:35 -0800

    Fix Member Fields List tests

--- tests/rspec/pages/members/fields.rb
+++ tests/rspec/pages/members/fields.rb
@@ -1,11 +1,11 @@
 class MemberFields < ControlPanelPage
 
-  element :member_actions, 'select[name=bulk_action]'
+  element :member_actions, 'select[name=bulk_action]', :visible => false
   element :member_fields_table, 'table'
-  element :member_fields_create, '.tbl-search a'
+  element :member_fields_create, 'fieldset.tbl-search a'
 
   def load
     main_menu.members_btn.click
-    find('.sidebar li a[href$=cp/members/fields]').click
+    click_link 'Member Fields'
   end
 end
