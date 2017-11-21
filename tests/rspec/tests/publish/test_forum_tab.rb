691bdf5c523cc3f55b0311f64e8e80ccc6afdad5
Kevin Cupp <kevin.cupp@gmail.com>
2017-11-21 00:07:19 -0500

    Fix publish button click in Forum Tab tests

--- tests/rspec/tests/publish/test_forum_tab.rb
+++ tests/rspec/tests/publish/test_forum_tab.rb
@@ -135,7 +135,7 @@ feature 'Forum Tab' do
     edit.entry_rows[0].find('.toolbar-wrap a[href*="publish/edit/entry"]').click
 
     @page.title.set title + " Edited"
-    @page.submit_buttons[1].click
+    @page.submit_buttons[2].click
 
     @page.all_there?.should == false
     @page.alert.has_content?("The entry #{title} Edited has been updated.").should == true
