70e1e90fcad7ab0f636b2716ac62cab651e31166
Kevin Cupp <kevin.cupp@gmail.com>
2017-11-30 12:11:09 -0500

    General changes for upgraded Docker container and Circle 2.0
    Sorry this isn’t more broken up

--- tests/docker/runtests.sh
+++ tests/docker/runtests.sh
@@ -9,11 +9,25 @@ echo "domain local" > /etc/resolv.conf
 echo "nameserver 8.8.8.8" >> /etc/resolv.conf
 echo "nameserver 8.8.4.4" >> /etc/resolv.conf
 
+function load_php_version {
+	PHP_VERSION_ASPLODE=(${PHP_VERSION//./ })
+	PHP_MAJOR_VERSION=${PHP_VERSION_ASPLODE[0]}
+
+	source ~/.phpbrew/bashrc
+	echo "Loading PHP ${PHP_VERSION} ..."
+	phpbrew use php-$PHP_VERSION
+	# Prevent other PHPs from loading
+	echo "" > /etc/apache2/mods-available/php5.load
+	echo "" > /etc/apache2/mods-available/php7.load
+	echo "" > /etc/apache2/mods-available/php7.0.load
+	echo "LoadModule php${PHP_MAJOR_VERSION}_module /usr/lib/apache2/modules/libphp${PHP_VERSION}.so" > /etc/apache2/mods-available/php5.load
+}
+
 while [[ $# > 0 ]]
 	do
 	key="$1"
 
-	PHP_VERSION="7.1.0"
+	PHP_VERSION="7.1.11"
 
 	case $key in
 		-p|--php)
@@ -32,67 +46,49 @@ while [[ $# > 0 ]]
 	shift # past argument or value
 done
 
-export PHP_VERSION
+load_php_version
 
-cp /app/ee.tar /var/www/html/
+function setup_permissions {
+	pushd /var/www/html/ > /dev/null
+		cp tests/docker/config.php system/user/config/
+		cp tests/docker/config.rb tests/rspec/
+		cp tests/docker/EllisLabUpdate.pub system/ee/EllisLab/ExpressionEngine
+		cp tests/circleci/license.key system/user/config/
+		chmod 666 system/user/config/config.php
+		chmod -R 777 system/user/cache
+		chmod -R 777 system/user/templates
+		chmod -R 777 system/user/language
+		chmod 777 tests/rspec/support/tmp
+		chmod -R 777 tests/rspec/support/file-sync/uploads
+		chmod -R 777 images
+		# JS Shim for ES5/ES6 with Capybara
+		cp themes/ee/asset/javascript/src/react/react.min.js themes/ee/asset/javascript/src/react/react.min~orig.js
+		cat tests/rspec/shim.min.js themes/ee/asset/javascript/src/react/react.min.js > themes/ee/asset/javascript/src/react/react.min-shimmed.js
+		mv themes/ee/asset/javascript/src/react/react.min-shimmed.js themes/ee/asset/javascript/src/react/react.min.js
+	popd > /dev/null
+}
 
-pushd /var/www/html/ > /dev/null
-	tar xmf ee.tar > /dev/null
-	# TODO automatically replace app_version?
-	cp tests/docker/config.php system/user/config/
-	cp tests/docker/config.rb tests/rspec/
-	cp tests/docker/EllisLabUpdate.pub system/ee/EllisLab/ExpressionEngine
-	cp tests/circleci/license.key system/user/config/
-	chmod 666 system/user/config/config.php
-	chmod -R 777 system/user/cache
-	chmod -R 777 system/user/templates
-	chmod -R 777 system/user/language
-	chmod 777 tests/rspec/support/tmp
-	chmod -R 777 tests/rspec/support/file-sync/uploads
-	chmod -R 777 images
-	# JS Shim for ES5/ES6 with Capybara
-	cp themes/ee/asset/javascript/src/react/react.min.js themes/ee/asset/javascript/src/react/react.min~orig.js
-	cat tests/rspec/shim.min.js themes/ee/asset/javascript/src/react/react.min.js > themes/ee/asset/javascript/src/react/react.min-shimmed.js
-	mv themes/ee/asset/javascript/src/react/react.min-shimmed.js themes/ee/asset/javascript/src/react/react.min.js
-popd > /dev/null
+setup_permissions
 
-rm /app/ee.tar
+ARTIFACTS_DIR="/app/artifacts/${PHP_VERSION}"
+mkdir -p $ARTIFACTS_DIR
 
-PHP_VERSION_ASPLODE=(${PHP_VERSION//./ })
-PHP_MAJOR_VERSION=${PHP_VERSION_ASPLODE[0]}
+function start_apache_mysql {
+	# https://github.com/docker/for-linux/issues/72
+	find /var/lib/mysql -type f -exec touch {} \;
 
-source ~/.phpbrew/bashrc
-echo "Loading PHP ${PHP_VERSION} ..."
-phpbrew use php-$PHP_VERSION
-# Prevent other PHPs from loading
-echo "" > /etc/apache2/mods-available/php5.load
-echo "" > /etc/apache2/mods-available/php7.load
-echo "" > /etc/apache2/mods-available/php7.0.load
-echo "LoadModule php${PHP_MAJOR_VERSION}_module /usr/lib/apache2/modules/libphp${PHP_VERSION}.so" > /etc/apache2/mods-available/php5.load
+	service apache2 start > /dev/null
+	service mysql start > /dev/null
+}
 
-if [ "${COMMAND}" == "lint" ]; then
-	CORE_COUNT=`shell grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu`
-	pushd /var/www/html/
-		find -L . -name '*.php' -not -path "./system/ee/EllisLab/Tests/vendor/*" -not -path "./node_modules/*" -not -name "config_tmpl.php" | parallel -j $CORE_COUNT php -l {}
-	popd
-	exit
-fi
-
-service apache2 start > /dev/null
-service mysql start > /dev/null
-
-if [ "${COMMAND}" == "test" ]; then
-
-	mkdir -p /app/tests/artifacts/${PHP_VERSION}
-
-	# Lint
+function lint_php_files {
 	pushd /var/www/html/ > /dev/null
 		for file in `find -L . -type f -name "*.php" -not -path "./system/ee/EllisLab/Tests/vendor/*" -not -path "./node_modules/*" -not -name "config_tmpl.php"`; do
 			RESULTS=`php -l $file`
 
 			if [ "$RESULTS" != "No syntax errors detected in $file" ] ; then
-				echo $RESULTS >> /app/tests/artifacts/$PHP_VERSION/phplint.txt
-				((STATUS+=1))
+				echo $RESULTS | tee -a $ARTIFACTS_DIR/phplint.txt
+				((STATUS+=${PIPESTATUS[0]}))
 			fi
 		done
 	popd > /dev/null
@@ -101,38 +97,42 @@ if [ "${COMMAND}" == "test" ]; then
 	if [ "${STATUS}" -gt "0" ]; then
 		exit $STATUS
 	fi
+}
 
+function run_unit_tests {
 	# PHPUnit tests
 	pushd /var/www/html/system/ee/EllisLab/Tests/
 		printf "Running PHPUnit tests\n\n"
 		composer install --prefer-source --no-interaction
-		vendor/bin/phpunit ExpressionEngine/ > /app/tests/artifacts/$PHP_VERSION/phpunit.txt
+		vendor/bin/phpunit ExpressionEngine/ | tee $ARTIFACTS_DIR/phpunit.txt
 
 		# Save our exit status code
-		((STATUS+=$?))
+		((STATUS+=${PIPESTATUS[0]}))
 
 		# Remove CLI colors
-		sed -i -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})*)?m//g" /app/tests/artifacts/$PHP_VERSION/phpunit.txt
+		sed -i -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})*)?m//g" $ARTIFACTS_DIR/phpunit.txt
 	popd
 
 	# Updater microapp unit tests
 	pushd /var/www/html/system/ee/installer/updater/EllisLab/Tests/
 		printf "Running PHPUnit tests\n\n"
 		composer install --prefer-source --no-interaction
-		vendor/bin/phpunit ExpressionEngine/ > /app/tests/artifacts/$PHP_VERSION/phpunit-updater.txt
+		vendor/bin/phpunit ExpressionEngine/ | tee $ARTIFACTS_DIR/phpunit-updater.txt
 
 		# Save our exit status code
-		((STATUS+=$?))
+		((STATUS+=${PIPESTATUS[0]}))
 
 		# Remove CLI colors
-		sed -i -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})*)?m//g" /app/tests/artifacts/$PHP_VERSION/phpunit-updater.txt
+		sed -i -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})*)?m//g" $ARTIFACTS_DIR/phpunit-updater.txt
 	popd
 
 	# Bail early if PHP Unit tests failed
 	if [ "${STATUS}" -gt "0" ]; then
 		exit $STATUS
 	fi
+}
 
+function run_rspec_tests {
 	if [ "${FILES}" == "" ]; then
 		FILES="tests/**/*.rb"
 	fi
@@ -144,19 +144,30 @@ if [ "${COMMAND}" == "test" ]; then
 
 	pushd /var/www/html/tests/rspec > /dev/null
 		bundle install --no-deployment --path=~/gems/ > /dev/null
-		xvfb-run -a bundle exec rspec -c -fd -fh -o /app/tests/artifacts/${PHP_VERSION}/rspec.html $FILES
+		xvfb-run -a bundle exec rspec -c -fd -fh -o screenshots/rspec.html $FILES
 
 		# Append status code for this test
 		((STATUS+=$?))
 	popd > /dev/null
 
 	if [ -d "/var/www/html/tests/rspec/screenshots" ]; then
-		mkdir -p /app/tests/rspec/screenshots
-		pushd /app/tests/rspec/screenshots > /dev/null
+		pushd $ARTIFACTS_DIR > /dev/null
 			#rm -rf *
 			cp -r /var/www/html/tests/rspec/screenshots/* .
 		popd > /dev/null
 	fi
+}
+
+if [ "${COMMAND}" == "circleci" ]; then
+	lint_php_files
+	run_unit_tests
+	start_apache_mysql
+	run_rspec_tests
+elif [ "${COMMAND}" == "test" ]; then
+	start_apache_mysql
+	run_rspec_tests
+elif [ "${COMMAND}" == "unittest" ]; then
+	run_unit_tests
 fi
 
 exit $STATUS
