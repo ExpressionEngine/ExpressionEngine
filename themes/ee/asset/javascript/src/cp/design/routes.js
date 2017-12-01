7ca99e20f0b6ae1001499c1b1b45924377dd73e2
Seth Croston Barber <seth.barber@ellislab.com>
2017-12-01 11:35:46 -0800

    Fixing the JS to match the new React component.

--- themes/ee/asset/javascript/src/cp/design/routes.js
+++ themes/ee/asset/javascript/src/cp/design/routes.js
@@ -11,21 +11,8 @@ $(document).ready(function () {
 	$('.grid-publish').removeClass('grid-publish');
 	$('.grid-input-form').removeClass('grid-input-form');
 
-
-	$('table').on('change', 'select', function (e) {
-		var group = $('option:selected', this).closest('optgroup').attr('label');
-		$(this).closest('td').next().html(group);
-
-		$('option:disabled').removeAttr('disabled');
-
-		$('option:selected').each(function (index, element) {
-			if (element.value) {
-				$('option[value=' + element.value + ']:not(:selected)').attr('disabled', 'disabled');
-			}
-		});
-	});
-
 	$('#routes').on('grid:addRow', function(e, el) {
 		$(el).addClass('setting-field');
+		SelectField.renderFields($(el).find('td').eq(1));
 	});
 });
