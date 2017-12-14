ab3ba69c30de76de57a48c670051997725349c6c
Kevin Cupp <kevin.cupp@gmail.com>
2017-12-14 15:08:50 -0500

    Re-enable Save buttons when deleting an invalid Grid column
    Fixes #1681

--- themes/ee/asset/javascript/src/cp/grid.js
+++ themes/ee/asset/javascript/src/cp/grid.js
@@ -573,7 +573,14 @@ Grid.Settings.prototype = {
 
 			// Trigger validation on any invalid inputs in case the validaiton
 			// errors were due to a duplicate column name/label in this column
-			$('fieldset.fieldset-invalid input', that.root).trigger('change');
+			var invalidFields = $('fieldset.fieldset-invalid input', that.root)
+			if (invalidFields.size()) {
+				invalidFields.trigger('blur')
+			} else {
+			// Or, the deleted column contained the only validation errors, trigger
+			// validation on another known field to re-enable submit buttons
+				$('input[name=field_name]').trigger('blur')
+			}
 		});
 	},
 
