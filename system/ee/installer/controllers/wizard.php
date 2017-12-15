68500db0ced38cf0cebebd8e9027584f9d4dcef3
Kevin Cupp <kevin.cupp@gmail.com>
2017-12-15 14:09:29 -0500

    Version bump and update file for 4.0.3

--- system/ee/installer/controllers/wizard.php
+++ system/ee/installer/controllers/wizard.php
@@ -12,7 +12,7 @@
  */
 class Wizard extends CI_Controller {
 
-	public $version           = '4.0.2';	// The version being installed
+	public $version           = '4.0.3';	// The version being installed
 	public $installed_version = ''; 		// The version the user is currently running (assuming they are running EE)
 	public $minimum_php       = '5.3.10';	// Minimum version required to run EE
 	public $schema            = NULL;		// This will contain the schema object with our queries
