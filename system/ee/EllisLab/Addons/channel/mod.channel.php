ExpressionEngine/ExpressionEngine

commit 5ff7d96d6afd3117f8a4e1e293efe127c2178765

Parse all category variables in the same place, fix formatting
Cat custom fields were not respecting their formatting as they were
being replaced with a blunt str_replace() in the entries Parser. This
stops that, lets all category variables be parsed with the same logic.
Includes a change to the FieldModel with unknown implications. We call
the setup_handler and overwrite the field_types array of the channel
fields API so many times and from so many different places it makes my
head spin. But calling the setup handler again in this case was causing
a nulled copy of the field type to be used in apply() rather than the
one we bothered setting up as the $fieldtype in this method. Since I
have no idea how many different places are using this “TEMPORARY,
VOLATILE, DO NOT USE” method, I have no idea what I just broke. Yey.

system/ee/EllisLab/Addons/channel/mod.channel.php
@@ -4090,8 +4090,7 @@ class Channel {
 			$variables = ee()->TMPL->var_single;
 		}
 
-		// native metadata fields with modifiers will pass through here. Thos without modifiers
-		// will have already been handled with a simple string replace by EE_Channel_category_parser
+		// native metadata fields with will pass through here, treat them like text fields
 		ee()->api_channel_fields->include_handler('text');
 		$fieldtype = ee()->api_channel_fields->setup_handler('text', TRUE);
 		ee()->api_channel_fields->field_types['text'] = $fieldtype;
@@ -4100,6 +4099,12 @@ class Channel {
 		{
 			$var_props = ee('Variables/Parser')->parseVariableProperties($tag);
 			$field_name = $var_props['field_name'];
+
+			// only deal with variables we own
+			if ( ! isset($data[$field_name]))
+			{
+				continue;
+			}
 
 			if (isset($field_index[$field_name]) && isset($data['field_id_'.$field_index[$field_name]]))
 			{
@@ -4121,18 +4126,22 @@ class Channel {
 					$tag
 				);
 			}
-			elseif (isset($data[$field_name]) && ! empty($var_props['modifier']))
+			elseif (isset($data[$field_name]))
 			{
 				$content = $data[$field_name];
-				$parse_fnc = 'replace_'.$var_props['modifier'];
 
-				if (method_exists($fieldtype, $parse_fnc))
+				if ( ! empty($var_props['modifier']))
 				{
-					$content = ee()->api_channel_fields->apply($parse_fnc, array(
-						$content,
-						$var_props['params'],
-						FALSE
-					));
+					$parse_fnc = 'replace_'.$var_props['modifier'];
+
+					if (method_exists($fieldtype, $parse_fnc))
+					{
+						$content = ee()->api_channel_fields->apply($parse_fnc, array(
+							$content,
+							$var_props['params'],
+							FALSE
+						));
+					}
 				}
 
 				$chunk = str_replace(LD.$tag.RD, $content, $chunk);

system/ee/EllisLab/ExpressionEngine/Model/Content/FieldModel.php
@@ -503,9 +503,6 @@ abstract class FieldModel extends Model {
 		$parse_fnc = ($modifier) ? 'replace_'.$modifier : 'replace_tag';
 		if (method_exists($fieldtype, $parse_fnc))
 		{
-			ee()->api_channel_fields->include_handler($this->field_type);
-			ee()->api_channel_fields->setup_handler($this->field_type, TRUE);
-			ee()->api_channel_fields->field_types[$this->field_type] = $fieldtype;
 			$data = ee()->api_channel_fields->apply($parse_fnc, array(
 				$data,
 				$params,

system/ee/legacy/libraries/channel_entries_parser/components/Category.php
@@ -218,12 +218,6 @@ class EE_Channel_category_parser implements EE_Channel_parser_component {
 
 					$temp = ee()->functions->prep_conditionals($temp, $cond);
 
-					// and parse the variables
-					foreach ($cat_vars as $cat_var => $cat_val)
-					{
-						$temp = str_replace(LD.$cat_var.RD, $cat_val, $temp);
-					}
-
 					$variables = ee('Variables/Parser')->extractVariables($temp);
 					$temp = $obj->channel()->parseCategoryFields($v[0], array_merge($v, $cat_vars), $temp, array_keys($variables['var_single']));
 

commit d155945d2dc354f3b09422ca4092bfb589a0ab2f

Fix custom category field parsing outside of channel:entries
Bug #23400
Broken by 5ff7d96d6afd3117f8a4e1e293efe127c2178765

system/ee/EllisLab/Addons/channel/mod.channel.php
@@ -3218,7 +3218,7 @@ class Channel {
 					}
 				}
 
-				$chunk = $this->parseCategoryFields($cat_vars['category_id'], $val, $chunk);
+				$chunk = $this->parseCategoryFields($cat_vars['category_id'], array_merge($val, $cat_vars), $chunk);
 
 				/** --------------------------------
 				/**  {count}
@@ -3622,7 +3622,7 @@ class Channel {
 							$chunk = str_replace($ckey, reduce_double_slashes($cval.'/'.$cat_seg), $chunk);
 						}
 
-						$chunk = $this->parseCategoryFields($cat_vars['category_id'], $row, $chunk);
+						$chunk = $this->parseCategoryFields($cat_vars['category_id'], array_merge($row, $cat_vars), $chunk);
 
 						// Check to see if we need to parse {filedir_n}
 						if (strpos($chunk, '{filedir_') !== FALSE)
@@ -4047,7 +4047,7 @@ class Channel {
 					}
 				}
 
-				$chunk = $this->parseCategoryFields($cat_vars['category_id'], $val, $chunk);
+				$chunk = $this->parseCategoryFields($cat_vars['category_id'], array_merge($val, $cat_vars), $chunk);
 
 				/** --------------------------------
 				/**  {count}
@@ -4503,7 +4503,7 @@ class Channel {
 			ee()->TMPL->tagdata = ee()->file_field->parse_string(ee()->TMPL->tagdata);
 		}
 
-		ee()->TMPL->tagdata = $this->parseCategoryFields($cat_vars['category_id'], $row, ee()->TMPL->tagdata);
+		ee()->TMPL->tagdata = $this->parseCategoryFields($cat_vars['category_id'], array_merge($row, $cat_vars), ee()->TMPL->tagdata);
 
 		return ee()->TMPL->tagdata;
 	}