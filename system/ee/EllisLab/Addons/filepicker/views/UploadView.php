c130f81cbe66058ee566f3e2a32533473bcf2ae7
Kevin Cupp <kevin.cupp@gmail.com>
2018-01-12 14:46:16 -0500

    Disallow viewport zooming when filling in forms on iOS
    Closes #1729

--- system/ee/EllisLab/Addons/filepicker/views/UploadView.php
+++ system/ee/EllisLab/Addons/filepicker/views/UploadView.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" lang="en-us" dir="ltr">
-		<meta content="width=device-width, initial-scale=1.0" name="viewport">
+		<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"  name="viewport">
 		<?php if (isset($meta_refresh)): ?>
 		<meta http-equiv='refresh' content='<?=$meta_refresh['rate']?>; url=<?=$meta_refresh['url']?>'>
 		<?php endif;?>
--- system/ee/installer/views/container.php
+++ system/ee/installer/views/container.php
@@ -3,7 +3,7 @@
 	<head>
 		<title><?=$title?></title>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" lang="en-us" dir="ltr">
-		<meta content="width=device-width, initial-scale=1.0" name="viewport">
+		<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"  name="viewport">
 		<link href="<?=$theme_url?>/ee/cp/css/out.min.css" rel="stylesheet">
 		<?php
 		if (isset($extra_header))
