74cae5d72bdb76421a21b465d96bf9ce0755d0e7
Kevin Cupp <kevin.cupp@gmail.com>
2018-01-15 15:51:26 -0500

    Retain order of select list items in its selection

--- themes/ee/asset/javascript/src/fields/relationship/relationship.js
+++ themes/ee/asset/javascript/src/fields/relationship/relationship.js
@@ -73,6 +73,7 @@ var Relationship = function (_React$Component) {
           multi: this.props.multi,
           selected: this.state.selected,
           selectionChanged: this.selectionChanged,
+          selectionShouldRetainItemOrder: false,
           noResults: this.props.no_results,
           filterable: true,
           tooMany: true,
