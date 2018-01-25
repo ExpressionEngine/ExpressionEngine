e639c10dff9d2ec149f32e3d660ed46d06f67501
Robin <robin.sowell@ellislab.com>
2018-01-24 19:08:41 -0500

    Adding 3.5.15 update file- no db changes

---
+++ system/ee/installer/updates/ud_3_05_15.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * ExpressionEngine (https://expressionengine.com)
+ *
+ * @link      https://expressionengine.com/
+ * @copyright Copyright (c) 2003-2018, EllisLab, Inc. (https://ellislab.com)
+ * @license   https://expressionengine.com/license
+ */
+
+namespace EllisLab\ExpressionEngine\Updater\Version_3_5_15;
+
+/**
+ * Update
+ */
+class Updater {
+
+	var $version_suffix = '';
+
+	/**
+	 * Do Update
+	 *
+	 * @return TRUE
+	 */
+	public function do_update()
+	{
+		return TRUE;
+	}
+}
+
+// EOF
