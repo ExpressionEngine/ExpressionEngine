a889905ecb922bf9eb316aadc57b6f33c2f33e22
Derek Jones <derek.jones@ellislab.com>
2018-02-16 15:19:59 -0800

    Refactor legacy Form Validation out of Member/Profile/Auth controller

--- system/ee/EllisLab/ExpressionEngine/Controller/Members/Profile/Auth.php
+++ system/ee/EllisLab/ExpressionEngine/Controller/Members/Profile/Auth.php
@@ -25,11 +25,81 @@ class Auth extends Settings {
 	{
 		$this->base_url = ee('CP/URL')->make($this->base_url, $this->query_string);
 
-		ee()->view->base_url = $this->base_url;
-		ee()->view->ajax_validate = TRUE;
-		ee()->view->cp_page_title = lang('auth_settings');
-		ee()->view->save_btn_text = 'btn_authenticate_and_save';
-		ee()->view->save_btn_text_working = 'btn_saving';
+		$vars['errors'] = NULL;
+
+		if ( ! empty($_POST))
+		{
+			// set and save the member as the various permissions allow
+			if ($this->config->item('allow_username_change') == 'y' OR
+				$this->session->userdata('group_id') == 1)
+			{
+				 $this->member->username = ee()->input->post('username');
+			}
+
+			// If the screen name field is empty, we'll assign is from the username field.
+			if (ee()->input->post('screen_name') == '')
+			{
+				$this->member->screen_name = ee()->input->post('username');
+			}
+			else
+			{
+				$this->member->screen_name = ee()->input->post('screen_name');
+			}
+
+			// require authentication to change user/pass
+			$validator = ee('Validation')->make();
+			$validator->setRule('verify_password', 'authenticated');
+
+			if (ee()->input->post('password'))
+			{
+				$this->member->password = ee()->input->post('password');
+				$validator->setRule('confirm_password', 'matches[password]');
+			}
+
+			$result = $this->member->validate();
+			$password_confirm = $validator->validate($_POST);
+
+			// Add password confirmation failure to main result object
+			if ($password_confirm->failed())
+			{
+				$rules = $password_confirm->getFailed();
+				foreach ($rules as $field => $rule)
+				{
+					$result->addFailed($field, $rule[0]);
+				}
+			}
+
+			if (AJAX_REQUEST)
+			{
+				return ee('Validation')->ajax($result);
+			}
+
+			if ($result->isValid())
+			{
+				var_dump($this->member->password);
+				// if the password was set, need to hash it before saving and kill all other sessions
+				if (ee()->input->post('password'))
+				{
+					$this->member->hashAndUpdatePassword($this->member->password);
+				}
+				var_dump($this->member->password);
+				$this->member->save();
+
+				ee('CP/Alert')->makeInline('shared-form')
+					->asSuccess()
+					->withTitle(lang('member_updated'))
+					->addToBody(lang('member_updated_desc'))
+					->defer();
+				ee()->functions->redirect($this->base_url);
+			}
+
+			$vars['errors'] = $result;
+			ee('CP/Alert')->makeInline('shared-form')
+				->asIssue()
+				->withTitle(lang('settings_save_error'))
+				->addToBody(lang('settings_save_error_desc'))
+				->now();
+		}
 
 		$vars['sections'] = array(
 			array(
@@ -90,7 +160,7 @@ class Auth extends Settings {
 					'title' => 'existing_password',
 					'desc' => 'existing_password_exp',
 					'fields' => array(
-						'current_password' => array(
+						'verify_password' => array(
 							'type'      => 'password',
 							'required' => TRUE,
 							'maxlength' => PASSWORD_MAX_LENGTH
@@ -100,101 +170,11 @@ class Auth extends Settings {
 			)
 		);
 
-		ee()->form_validation->set_rules(array(
-			array(
-				 'field'   => 'username',
-				 'label'   => 'lang:username',
-				 'rules'   => 'required|valid_username'
-			),
-			array(
-				 'field'   => 'screen_name',
-				 'label'   => 'lang:screen_name',
-				 'rules'   => 'required|valid_screen_name'
-			),
-			array(
-				 'field'   => 'password',
-				 'label'   => 'lang:new_password',
-			),
-			array(
-				 'field'   => 'confirm_password',
-				 'label'   => 'lang:confirm_password',
-				 'rules'   => 'matches[password]'
-			),
-			array(
-				 'field'   => 'current_password',
-				 'label'   => 'lang:current_password',
-				 'rules'   => 'required|auth_password'
-			)
-		));
-
-		if (AJAX_REQUEST)
-		{
-			ee()->form_validation->run_ajax();
-			exit;
-		}
-		elseif (ee()->form_validation->run() !== FALSE)
-		{
-			// set and save the member as the various permissions allow
-			if ($this->config->item('allow_username_change') == 'y' OR
-				$this->session->userdata('group_id') == 1)
-			{
-				 $this->member->username = ee()->input->post('username');
-			}
-
-			// If the screen name field is empty, we'll assign is from the username field.
-			if (ee()->input->post('screen_name') == '')
-			{
-				$this->member->screen_name = ee()->input->post('username');
-			}
-			else
-			{
-				$this->member->screen_name = ee()->input->post('screen_name');
-			}
-
-			if (ee()->input->post('password'))
-			{
-				$this->member->password = ee()->input->post('password');
-			}
-
-			$result = $this->member->validate();
-
-			// Display errors if there are any
-			if ( ! $result->isValid())
-			{
-				$vars['errors'] = $result;
-				ee('CP/Alert')->makeInline('shared-form')
-					->asIssue()
-					->withTitle(lang('settings_save_error'))
-					->addToBody(lang('settings_save_error_desc'))
-					->now();
-
-				ee()->cp->render('settings/form', $vars);
-				return;
-			}
-
-			// if the password was set, need to hash it before saving and kill all other sessions
-			if (ee()->input->post('password'))
-			{
-				$this->member->hashAndUpdatePassword($this->member->password);
-			}
-
-			$this->member->save();
-
-			ee('CP/Alert')->makeInline('shared-form')
-				->asSuccess()
-				->withTitle(lang('member_updated'))
-				->addToBody(lang('member_updated_desc'))
-				->defer();
-			ee()->functions->redirect($this->base_url);
-		}
-		elseif (ee()->form_validation->errors_exist())
-		{
-			ee('CP/Alert')->makeInline('shared-form')
-				->asIssue()
-				->withTitle(lang('settings_save_error'))
-				->addToBody(lang('settings_save_error_desc'))
-				->now();
-		}
+		ee()->view->base_url = $this->base_url;
+		ee()->view->ajax_validate = TRUE;
+		ee()->view->cp_page_title = lang('auth_settings');
+		ee()->view->save_btn_text = 'btn_authenticate_and_save';
+		ee()->view->save_btn_text_working = 'btn_saving';
 
 		ee()->cp->render('settings/form', $vars);
 	}
