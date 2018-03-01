9fb6ff3e2e82adb9327ba934ac7a96f3d3bf0f1a
Kevin Cupp <kevin.cupp@gmail.com>
2018-03-01 14:35:38 -0500

    Version bump to 4.1 actual

--- system/ee/legacy/libraries/Core.php
+++ system/ee/legacy/libraries/Core.php
@@ -69,9 +69,9 @@ class EE_Core {
 		// application constants
 		define('IS_CORE',		FALSE);
 		define('APP_NAME',		'ExpressionEngine'.(IS_CORE ? ' Core' : ''));
-		define('APP_BUILD',		'20180226');
-		define('APP_VER',		'4.1.0-dp.3');
-		define('APP_VER_ID',	'dp.3');
+		define('APP_BUILD',		'20180301');
+		define('APP_VER',		'4.1.0');
+		define('APP_VER_ID',	'');
 		define('SLASH',			'&#47;');
 		define('LD',			'{');
 		define('RD',			'}');
