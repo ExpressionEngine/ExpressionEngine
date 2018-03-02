9e3b72f37051bd9df975826b81267fcfad1835b1
Derek Jones <derek.jones@ellislab.com>
2018-03-02 14:41:15 -0800

    4.1.1 Would be nice to actually bump the version number

--- system/ee/legacy/libraries/Core.php
+++ system/ee/legacy/libraries/Core.php
@@ -69,8 +69,8 @@ class EE_Core {
 		// application constants
 		define('IS_CORE',		FALSE);
 		define('APP_NAME',		'ExpressionEngine'.(IS_CORE ? ' Core' : ''));
-		define('APP_BUILD',		'20180301');
-		define('APP_VER',		'4.1.0');
+		define('APP_BUILD',		'20180302');
+		define('APP_VER',		'4.1.1');
 		define('APP_VER_ID',	'');
 		define('SLASH',			'&#47;');
 		define('LD',			'{');
