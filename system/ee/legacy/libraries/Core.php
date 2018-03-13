f10180c81c8272f6ecb85c39afab7a8a700319cc
Robin <robin.sowell@ellislab.com>
2018-03-13 13:22:49 -0400

    bumped app build
    No idea if that mattered.  but since I haven’t tagged….

--- system/ee/legacy/libraries/Core.php
+++ system/ee/legacy/libraries/Core.php
@@ -69,7 +69,7 @@ class EE_Core {
 		// application constants
 		define('IS_CORE',		FALSE);
 		define('APP_NAME',		'ExpressionEngine'.(IS_CORE ? ' Core' : ''));
-		define('APP_BUILD',		'20180312');
+		define('APP_BUILD',		'20180313');
 		define('APP_VER',		'4.1.2');
 		define('APP_VER_ID',	'');
 		define('SLASH',			'&#47;');
