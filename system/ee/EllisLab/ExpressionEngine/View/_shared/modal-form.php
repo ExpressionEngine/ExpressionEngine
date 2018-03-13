1a0266e4514ad47074ce29d4ed6665b62f92098f
Kevin Cupp <kevin.cupp@gmail.com>
2018-03-13 16:30:29 -0400

    Allow modal form view to go full-width

--- system/ee/EllisLab/ExpressionEngine/View/_shared/modal-form.php
+++ system/ee/EllisLab/ExpressionEngine/View/_shared/modal-form.php
@@ -1,4 +1,4 @@
-<div class="app-modal app-modal--side" rel="<?=$name?>">
+<div class="app-modal app-modal--side<?=isset($full) && $full ? ' app-modal--full' : ''?>" rel="<?=$name?>">
 	<div class="app-modal__content">
 		<div class="app-modal__dismiss">
 			<a class="js-modal-close" rel="<?=$name?>" href="#"><?=lang('close_modal')?></a> <span class="txt-fade">[esc]</span>
