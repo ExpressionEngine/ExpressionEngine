ff2355b6c9bf763fde165bdff642d897f99fdbcc
Derek Jones <derek.jones@ellislab.com>
2018-03-14 17:00:39 -0700

    Fix fatal error when upgrading from < 2.11
    The method update_grid_search() was never added to v3, it was a late patch to v2. This update bug was introduced in v3 here, where I wanted to make sure updates were all in parity: 97377b3
    
    It isn't a data loss bug, so #wontfix in v3, just keep it from preventing an upgrade.

--- system/ee/installer/updates/ud_2_11_00.php
+++ system/ee/installer/updates/ud_2_11_00.php
@@ -34,46 +34,7 @@ class Updater {
 	 */
 	public function do_update()
 	{
-		ee()->load->dbforge();
-
-		$steps = new ProgressIterator(
-			array(
-				'update_grid_field_search'
-			)
-		);
-
-		foreach ($steps as $k => $v)
-		{
-			$this->$v();
-		}
-
 		return TRUE;
-	}
-
-
-	// --------------------------------------------------------------------
-
-	private function update_grid_field_search()
-	{
-		ee()->load->model('grid_model');
-
-		// Get list of grid fields
-		$fields = ee()->db->select('field_id')
-			->where('field_type', 'grid')
-			->where('field_search', 'y')
-			->get('channel_fields')
-			->result_array();
-
-		if (empty($fields))
-		{
-			return;
-		}
-
-		$fields = array_map(function($element) {
-			return $element['field_id'];
-		}, $fields);
-
-		ee()->grid_model->update_grid_search($fields);
 	}
 }
 
