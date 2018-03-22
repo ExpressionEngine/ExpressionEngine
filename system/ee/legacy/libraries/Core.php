a90c2ecbc00a7e3e86911971043e84cb7e816841
Robin <robin.sowell@ellislab.com>
2018-03-22 17:23:21 -0400

    Build bump

--- system/ee/legacy/libraries/Core.php
+++ system/ee/legacy/libraries/Core.php
@@ -69,7 +69,7 @@ class EE_Core {
 		// application constants
 		define('IS_CORE',		FALSE);
 		define('APP_NAME',		'ExpressionEngine'.(IS_CORE ? ' Core' : ''));
-		define('APP_BUILD',		'20180315');
+		define('APP_BUILD',		'20180322');
 		define('APP_VER',		'4.1.3');
 		define('APP_VER_ID',	'');
 		define('SLASH',			'&#47;');
