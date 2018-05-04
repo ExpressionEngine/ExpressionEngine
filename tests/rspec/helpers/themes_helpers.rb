13989333663c5ea78e34468c5e9009b284c646df
Robin <robin.sowell@ellislab.com>
2018-05-04 18:35:00 -0400

    Test is still failing, WHY

--- tests/rspec/helpers/themes_helpers.rb
+++ tests/rspec/helpers/themes_helpers.rb
@@ -3,19 +3,27 @@ module Themes
   class Prepare
     def initialize
       themes             = '../../themes/'
-      @member_theme_src  = File.expand_path('ee/member', themes)
-      @forum_theme_src   = File.expand_path('ee/forum', themes)
-      @user_theme_dir    = File.expand_path('user', themes)
+      system             = '../../system/'
+      @member_theme_src  = File.expand_path('ee/templates/_themes/member', system)
+      @member_assets_src  = File.expand_path('ee/member', themes)
+      @forum_theme_src   = File.expand_path('ee/templates/_themes/forum', system)
+      @forum_assets_src  = File.expand_path('ee/forum', themes)
+
+      @user_assets_dir    = File.expand_path('user', themes)
+      @user_theme_dir    = File.expand_path('user/templates/_themes', system)
     end
 
     # Copies themes/ee/member themes to themes/user
     def copy_member_themes
       FileUtils.copy_entry @member_theme_src, @user_theme_dir + '/member'
+      FileUtils.copy_entry @member_assets_src, @user_assets_dir + '/member'
+
     end
 
     # Copies themes/ee/forum themes to themes/user
     def copy_forum_themes
       FileUtils.copy_entry @forum_theme_src, @user_theme_dir + '/forum'
+      FileUtils.copy_entry @forum_assets_src, @user_assets_dir + '/forum'
     end
   end
 end
