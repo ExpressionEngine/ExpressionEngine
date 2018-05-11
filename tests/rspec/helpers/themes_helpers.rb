f33ee4be4b969f2ce1e0c1a247d7e012312e78ee
Robin <robin.sowell@ellislab.com>
2018-05-11 11:57:56 -0400

    Added a couple of tests to make sure it still works if
    they put their templates in themes/user for now.

--- tests/rspec/helpers/themes_helpers.rb
+++ tests/rspec/helpers/themes_helpers.rb
@@ -14,16 +14,30 @@ module Themes
       FileUtils.mkdir_p @user_theme_dir
     end
 
-    # Copies themes/ee/member themes to themes/user
+    # Copies themes/ee/member assets to themes/user and templates to system/user
     def copy_member_themes
       FileUtils.copy_entry @member_theme_src, @user_theme_dir + '/member'
       FileUtils.copy_entry @member_assets_src, @user_assets_dir + '/member'
 
     end
 
-    # Copies themes/ee/forum themes to themes/user
+    # Copies themes/ee/member assets to themes/user and templates to themes/user
+    # In v5 this will not be a needed check
+    def copy_member_themes_to_user
+      FileUtils.copy_entry @member_theme_src, @user_assets_dir + '/member'
+      FileUtils.copy_entry @member_assets_src, @user_assets_dir + '/member'
+    end
+
+    # Copies themes/ee/forum assets to themes/user and templates to system/user/
     def copy_forum_themes
       FileUtils.copy_entry @forum_theme_src, @user_theme_dir + '/forum'
+      FileUtils.copy_entry @forum_assets_src, @user_assets_dir + '/forum'
+    end
+
+    # Copies themes/ee/forum assets to themes/user and templates to themes/user
+    # In v5 this will not be a needed check
+    def copy_forum_themes_to_user
+      FileUtils.copy_entry @forum_theme_src, @user_assets_dir + '/forum'
       FileUtils.copy_entry @forum_assets_src, @user_assets_dir + '/forum'
     end
   end
