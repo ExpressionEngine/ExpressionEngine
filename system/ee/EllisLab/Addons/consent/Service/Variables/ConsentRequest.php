02f32d5ea969b278b7ee3b7167c06263e806d69a
Derek Jones <derek.jones@ellislab.com>
2018-05-13 11:20:33 -0700

    Add variables to Consent variables service
    Change to match model format

--- system/ee/EllisLab/Addons/consent/Service/Variables/ConsentRequest.php
+++ system/ee/EllisLab/Addons/consent/Service/Variables/ConsentRequest.php
@@ -9,7 +9,7 @@
 
 namespace EllisLab\Addons\Consent\Service\Variables;
 
-use EllisLab\ExpressionEngine\Model\Consent\Consent as ConsentModel;
+use EllisLab\ExpressionEngine\Model\Consent\ConsentRequest as ConsentRequestModel;
 use EllisLab\ExpressionEngine\Service\Template\Variables;
 
 /**
@@ -18,18 +18,18 @@ use EllisLab\ExpressionEngine\Service\Template\Variables;
 class ConsentRequest extends Variables {
 
 	/**
-	 * @var object EllisLab\ExpressionEngine\Model\Consent\Consent
+	 * @var object EllisLab\ExpressionEngine\Model\Consent\ConsentRequest
 	 */
-	private $consent;
+	private $request;
 
 	/**
 	 * Constructor
 	 *
-	 * @param object $consent EllisLab\ExpressionEngine\Model\Consent\Consent
+	 * @param object $consent EllisLab\ExpressionEngine\Model\Consent\ConsentRequest
 	 */
-	public function __construct(ConsentModel $consent)
+	public function __construct(ConsentRequestModel $request)
 	{
-		$this->consent = $consent;
+		$this->request = $request;
 
 		parent::__construct();
 	}
@@ -53,14 +53,22 @@ class ConsentRequest extends Variables {
 		]);
 
 		$typography_prefs = [
-			'text_format'	=> $consent->request_format,
+			'text_format'	=> $this->request->CurrentVersion->request_format,
 			'html_format'	=> 'all',
 			'auto_links'	=> FALSE,
 			'allow_img_url' => FALSE,
 		];
 
 		$this->variables = [
-
+			'consent_creation_date' => $this->date($this->request->CurrentVersion->create_date),
+			'consent_edit_date' => $this->date($this->request->CurrentVersion->edit_date),
+			//'consent_granted' => $this->request->granted,
+			//'consent_granted_date' => $this->request->granted_date,
+			'consent_id' => $this->request->getId(),
+			'consent_request' => $this->typography($this->request->CurrentVersion->request, $typography_prefs),
+			'consent_short_name' => $this->request->consent_name,
+			'consent_title' => $this->request->title,
+			'consent_version_id' => $this->request->consent_request_version_id,
 		];
 
 		return $this->variables;
