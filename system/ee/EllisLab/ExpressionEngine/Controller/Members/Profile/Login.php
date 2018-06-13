cbb88889e1219939ef304092745349b4c9d80ad0
Kevin Cupp <kevin.cupp@gmail.com>
2018-06-13 15:41:36 -0400

    Use sudo mode where we ask for password confirmation

--- system/ee/EllisLab/ExpressionEngine/Controller/Members/Profile/Login.php
+++ system/ee/EllisLab/ExpressionEngine/Controller/Members/Profile/Login.php
@@ -52,21 +52,39 @@ class Login extends Profile {
 						'other' => array('type' => 'text')
 					)
 				)
-			),
-			'secure_form_ctrls' => array(
+			)
+		);
+
+		$rules = [
+			[
+				'field' => 'redirect',
+				'label' => 'lang:redirect_to',
+				'rules' => 'enum[site_index,cp_index,other]'
+			]
+		];
+
+		if ( ! ee('Session')->isWithinAuthTimeout())
+		{
+			$vars['sections']['secure_form_ctrls'] = array(
 				array(
 					'title' => 'existing_password',
 					'desc' => 'existing_password_exp',
 					'fields' => array(
 						'password' => array(
 							'type'      => 'password',
-							'required' => TRUE,
+							'required'  => TRUE,
 							'maxlength' => PASSWORD_MAX_LENGTH
 						)
 					)
 				)
-			)
-		);
+			);
+
+			$rules[] = [
+				'field' => 'password',
+				'label' => 'lang:password',
+				'rules' => 'required|auth_password[useAuthTimeout]'
+			];
+		}
 
 		if ($this->member->getMemberGroup()->can_access_cp == 'y')
 		{
@@ -82,13 +100,7 @@ class Login extends Profile {
 			->addToBody(sprintf(lang('login_as_warning'), $this->member->screen_name), 'warning')
 			->now();
 
-		ee()->form_validation->set_rules(array(
-			array(
-				 'field'   => 'password',
-				 'label'   => 'lang:password',
-				 'rules'   => 'required'
-			)
-		));
+		ee()->form_validation->set_rules($rules);
 
 		if (AJAX_REQUEST)
 		{
@@ -118,18 +130,6 @@ class Login extends Profile {
 
 	private function login()
 	{
-		// Check password authentication
-		ee()->load->library('auth');
-		$validate = ee()->auth->authenticate_id(
-			ee()->session->userdata['member_id'],
-			ee()->input->post('password')
-		);
-
-		if ( ! $validate)
-		{
-			show_error(lang('unauthorized_access'), 403);
-		}
-
 		ee()->logger->log_action(sprintf(
 			lang('member_login_as'),
 			$this->member->username,
