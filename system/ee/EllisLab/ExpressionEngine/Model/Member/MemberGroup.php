d7d383ea6d6ee7a6119ba0b48b840171fb6206ca
Seth Croston Barber <seth.barber@ellislab.com>
2018-07-11 14:31:14 -0700

    Adding the new permissions table

--- system/ee/EllisLab/ExpressionEngine/Model/Member/MemberGroup.php
+++ system/ee/EllisLab/ExpressionEngine/Model/Member/MemberGroup.php
@@ -30,99 +30,9 @@ class MemberGroup extends StructureModel {
 
 	protected static $_typed_columns = array(
 		'is_locked'                      => 'boolString',
-		'can_view_offline_system'        => 'boolString',
-		'can_view_online_system'         => 'boolString',
-		'can_access_cp'                  => 'boolString',
-		'can_access_footer_report_bug'   => 'boolString',
-		'can_access_footer_new_ticket'   => 'boolString',
-		'can_access_footer_user_guide'   => 'boolString',
-		'can_view_homepage_news'         => 'boolString',
-		'can_access_files'               => 'boolString',
-		'can_access_design'              => 'boolString',
-		'can_access_addons'              => 'boolString',
-		'can_access_members'             => 'boolString',
-		'can_access_sys_prefs'           => 'boolString',
-		'can_access_comm'                => 'boolString',
-		'can_access_utilities'           => 'boolString',
-		'can_access_data'                => 'boolString',
-		'can_access_logs'                => 'boolString',
-		'can_admin_design'               => 'boolString',
-		'can_delete_members'             => 'boolString',
-		'can_admin_mbr_groups'           => 'boolString',
-		'can_admin_mbr_templates'        => 'boolString',
-		'can_ban_users'                  => 'boolString',
-		'can_admin_addons'              => 'boolString',
-		'can_edit_categories'            => 'boolString',
-		'can_delete_categories'          => 'boolString',
-		'can_view_other_entries'         => 'boolString',
-		'can_edit_other_entries'         => 'boolString',
-		'can_assign_post_authors'        => 'boolString',
-		'can_create_entries'             => 'boolString',
-		'can_edit_self_entries'          => 'boolString',
-		'can_delete_self_entries'        => 'boolString',
-		'can_delete_all_entries'         => 'boolString',
-		'can_view_other_comments'        => 'boolString',
-		'can_edit_own_comments'          => 'boolString',
-		'can_delete_own_comments'        => 'boolString',
-		'can_edit_all_comments'          => 'boolString',
-		'can_delete_all_comments'        => 'boolString',
-		'can_moderate_comments'          => 'boolString',
-		'can_send_cached_email'          => 'boolString',
-		'can_email_member_groups'        => 'boolString',
-		'can_email_from_profile'         => 'boolString',
-		'can_view_profiles'              => 'boolString',
-		'can_edit_html_buttons'          => 'boolString',
-		'can_delete_self'                => 'boolString',
-		'can_post_comments'              => 'boolString',
 		'exclude_from_moderation'        => 'boolString',
-		'can_search'                     => 'boolString',
-		'can_send_private_messages'      => 'boolString',
-		'can_attach_in_private_messages' => 'boolString',
-		'can_send_bulletins'             => 'boolString',
 		'include_in_authorlist'          => 'boolString',
 		'include_in_memberlist'          => 'boolString',
-		'can_upload_new_files'          => 'boolString',
-		'can_edit_files'                => 'boolString',
-		'can_delete_files'              => 'boolString',
-		'can_upload_new_toolsets'        => 'boolString',
-		'can_edit_toolsets'              => 'boolString',
-		'can_delete_toolsets'            => 'boolString',
-		'can_create_upload_directories'  => 'boolString',
-		'can_edit_upload_directories'    => 'boolString',
-		'can_delete_upload_directories'  => 'boolString',
-		'can_create_channels'            => 'boolString',
-		'can_edit_channels'              => 'boolString',
-		'can_delete_channels'            => 'boolString',
-		'can_create_channel_fields'      => 'boolString',
-		'can_edit_channel_fields'        => 'boolString',
-		'can_delete_channel_fields'      => 'boolString',
-		'can_create_statuses'            => 'boolString',
-		'can_delete_statuses'            => 'boolString',
-		'can_edit_statuses'              => 'boolString',
-		'can_create_categories'          => 'boolString',
-		'can_create_member_groups'       => 'boolString',
-		'can_delete_member_groups'       => 'boolString',
-		'can_edit_member_groups'         => 'boolString',
-		'can_create_members'             => 'boolString',
-		'can_edit_members'               => 'boolString',
-		'can_create_new_templates'       => 'boolString',
-		'can_edit_templates'             => 'boolString',
-		'can_delete_templates'           => 'boolString',
-		'can_create_template_groups'     => 'boolString',
-		'can_edit_template_groups'       => 'boolString',
-		'can_delete_template_groups'     => 'boolString',
-		'can_create_template_partials'   => 'boolString',
-		'can_edit_template_partials'     => 'boolString',
-		'can_delete_template_partials'   => 'boolString',
-		'can_create_template_variables'  => 'boolString',
-		'can_delete_template_variables'  => 'boolString',
-		'can_edit_template_variables'    => 'boolString',
-		'can_access_security_settings'   => 'boolString',
-		'can_access_translate'           => 'boolString',
-		'can_access_import'	             => 'boolString',
-		'can_access_sql_manager'         => 'boolString',
-		'can_admin_channels'             => 'boolString',
-		'can_manage_consents'            => 'boolString',
 	);
 
 
@@ -218,106 +128,16 @@ class MemberGroup extends StructureModel {
 	protected $group_description;
 	protected $is_locked;
 	protected $menu_set_id;
-	protected $can_view_offline_system;
-	protected $can_view_online_system;
-	protected $can_access_cp;
-	protected $can_access_footer_report_bug;
-	protected $can_access_footer_new_ticket;
-	protected $can_access_footer_user_guide;
-	protected $can_view_homepage_news;
-	protected $can_access_files;
-	protected $can_access_design;
-	protected $can_access_addons;
-	protected $can_access_members;
-	protected $can_access_sys_prefs;
-	protected $can_access_comm;
-	protected $can_access_utilities;
-	protected $can_access_data;
-	protected $can_access_logs;
-	protected $can_admin_design;
-	protected $can_delete_members;
-	protected $can_admin_mbr_groups;
-	protected $can_admin_mbr_templates;
-	protected $can_ban_users;
-	protected $can_admin_addons;
-	protected $can_edit_categories;
-	protected $can_delete_categories;
-	protected $can_view_other_entries;
-	protected $can_edit_other_entries;
-	protected $can_assign_post_authors;
-	protected $can_create_entries;
-	protected $can_edit_self_entries;
-	protected $can_delete_self_entries;
-	protected $can_delete_all_entries;
-	protected $can_view_other_comments;
-	protected $can_edit_own_comments;
-	protected $can_delete_own_comments;
-	protected $can_edit_all_comments;
-	protected $can_delete_all_comments;
-	protected $can_moderate_comments;
-	protected $can_send_cached_email;
-	protected $can_email_member_groups;
-	protected $can_email_from_profile;
-	protected $can_view_profiles;
-	protected $can_edit_html_buttons;
-	protected $can_delete_self;
 	protected $mbr_delete_notify_emails;
-	protected $can_post_comments;
 	protected $exclude_from_moderation;
-	protected $can_search;
 	protected $search_flood_control;
-	protected $can_send_private_messages;
 	protected $prv_msg_send_limit;
 	protected $prv_msg_storage_limit;
-	protected $can_attach_in_private_messages;
-	protected $can_send_bulletins;
 	protected $include_in_authorlist;
 	protected $include_in_memberlist;
 	protected $cp_homepage;
 	protected $cp_homepage_channel;
 	protected $cp_homepage_custom;
-	protected $can_upload_new_files;
-	protected $can_edit_files;
-	protected $can_delete_files;
-	protected $can_upload_new_toolsets;
-	protected $can_edit_toolsets;
-	protected $can_delete_toolsets;
-	protected $can_create_upload_directories;
-	protected $can_edit_upload_directories;
-	protected $can_delete_upload_directories;
-	protected $can_create_channels;
-	protected $can_edit_channels;
-	protected $can_delete_channels;
-	protected $can_create_channel_fields;
-	protected $can_edit_channel_fields;
-	protected $can_delete_channel_fields;
-	protected $can_create_statuses;
-	protected $can_delete_statuses;
-	protected $can_edit_statuses;
-	protected $can_create_categories;
-	protected $can_create_member_groups;
-	protected $can_delete_member_groups;
-	protected $can_edit_member_groups;
-	protected $can_create_members;
-	protected $can_edit_members;
-	protected $can_create_new_templates;
-	protected $can_edit_templates;
-	protected $can_delete_templates;
-	protected $can_create_template_groups;
-	protected $can_edit_template_groups;
-	protected $can_delete_template_groups;
-	protected $can_create_template_partials;
-	protected $can_edit_template_partials;
-	protected $can_delete_template_partials;
-	protected $can_create_template_variables;
-	protected $can_delete_template_variables;
-	protected $can_edit_template_variables;
-	protected $can_access_security_settings;
-	protected $can_access_translate;
-	protected $can_access_import;
-	protected $can_access_sql_manager;
-	protected $can_admin_channels;
-	protected $can_manage_consents;
 
 	/**
 	 * Ensure group ID is set for new records
--- system/ee/installer/updates/ud_5_00_00.php
+++ system/ee/installer/updates/ud_5_00_00.php
@@ -26,6 +26,7 @@ class Updater {
 		$steps = new \ProgressIterator(
 			[
 				'addTheMemberToGroupPivotTable',
+				'addAndPopulatePermissionsTable'
 			]
 		);
 
@@ -46,23 +47,187 @@ class Updater {
 
 		// Add the Many-to-Many tables
 		ee()->dbforge->add_field(
-			array(
-				'member_id' => array(
+			[
+				'member_id' => [
 					'type'       => 'int',
 					'constraint' => 10,
 					'unsigned'   => TRUE,
 					'null'       => FALSE
-				),
-				'group_id' => array(
+				],
+				'group_id' => [
 					'type'       => 'int',
 					'constraint' => 4,
 					'unsigned'   => TRUE,
 					'null'       => FALSE
-				)
-			)
+				]
+			]
 		);
-		ee()->dbforge->add_key(array('member_id', 'group_id'), TRUE);
+		ee()->dbforge->add_key(['member_id', 'group_id'], TRUE);
 		ee()->smartforge->create_table('members_member_groups');
+	}
+
+	private function addAndPopulatePermissionsTable()
+	{
+		if (ee()->db->table_exists('permissions'))
+		{
+			return;
+		}
+
+		ee()->dbforge->add_field(
+			[
+				'permission_id' => [
+					'type'           => 'int',
+					'constraint'     => 10,
+					'null'           => FALSE,
+					'unsigned'       => TRUE,
+					'auto_increment' => TRUE
+				],
+				'group_id' => [
+					'type'       => 'int',
+					'constraint' => 4,
+					'unsigned'   => TRUE,
+					'null'       => FALSE
+				],
+				'site_id' => [
+					'type'       => 'int',
+					'constraint' => 5,
+					'unsigned'   => TRUE,
+					'null'       => FALSE
+				],
+				'permission' => [
+					'type'       => 'varchar',
+					'constraint' => 32,
+					'null'       => FALSE
+				],
+			]
+		);
+		ee()->dbforge->add_key('permission_id', TRUE);
+		ee()->dbforge->add_key(['group_id', 'site_id'], TRUE);
+		ee()->smartforge->create_table('permissions');
+
+		// Migrate permissions to the new table
+		$insert = [];
+		$permissions = [
+			'can_view_offline_system',
+			'can_view_online_system',
+			'can_access_cp',
+			'can_access_footer_report_bug',
+			'can_access_footer_new_ticket',
+			'can_access_footer_user_guide',
+			'can_view_homepage_news',
+			'can_access_files',
+			'can_access_design',
+			'can_access_addons',
+			'can_access_members',
+			'can_access_sys_prefs',
+			'can_access_comm',
+			'can_access_utilities',
+			'can_access_data',
+			'can_access_logs',
+			'can_admin_channels',
+			'can_admin_design',
+			'can_delete_members',
+			'can_admin_mbr_groups',
+			'can_admin_mbr_templates',
+			'can_ban_users',
+			'can_admin_addons',
+			'can_edit_categories',
+			'can_delete_categories',
+			'can_view_other_entries',
+			'can_edit_other_entries',
+			'can_assign_post_authors',
+			'can_delete_self_entries',
+			'can_delete_all_entries',
+			'can_view_other_comments',
+			'can_edit_own_comments',
+			'can_delete_own_comments',
+			'can_edit_all_comments',
+			'can_delete_all_comments',
+			'can_moderate_comments',
+			'can_send_cached_email',
+			'can_email_member_groups',
+			'can_email_from_profile',
+			'can_view_profiles',
+			'can_edit_html_buttons',
+			'can_delete_self',
+			'can_post_comments',
+			'can_search',
+			'can_send_private_messages',
+			'can_attach_in_private_messages',
+			'can_send_bulletins',
+			'can_create_entries',
+			'can_edit_self_entries',
+			'can_upload_new_files',
+			'can_edit_files',
+			'can_delete_files',
+			'can_upload_new_toolsets',
+			'can_edit_toolsets',
+			'can_delete_toolsets',
+			'can_create_upload_directories',
+			'can_edit_upload_directories',
+			'can_delete_upload_directories',
+			'can_create_channels',
+			'can_edit_channels',
+			'can_delete_channels',
+			'can_create_channel_fields',
+			'can_edit_channel_fields',
+			'can_delete_channel_fields',
+			'can_create_statuses',
+			'can_delete_statuses',
+			'can_edit_statuses',
+			'can_create_categories',
+			'can_create_member_groups',
+			'can_delete_member_groups',
+			'can_edit_member_groups',
+			'can_create_members',
+			'can_edit_members',
+			'can_create_new_templates',
+			'can_edit_templates',
+			'can_delete_templates',
+			'can_create_template_groups',
+			'can_edit_template_groups',
+			'can_delete_template_groups',
+			'can_create_template_partials',
+			'can_edit_template_partials',
+			'can_delete_template_partials',
+			'can_create_template_variables',
+			'can_delete_template_variables',
+			'can_edit_template_variables',
+			'can_access_security_settings',
+			'can_access_translate',
+			'can_access_import',
+			'can_access_sql_manager',
+			'can_moderate_spam',
+			'can_manage_consents'
+		];
+
+		$groups = ee()->db->get('member_groups');
+
+		foreach ($groups->result() as $group)
+		{
+			foreach ($permissions as $permission)
+			{
+				// Since we assume "no" we only need to store "yes"
+				if ($group->$permission == 'y')
+				{
+					$insert[] = [
+						'group_id'   => $group->group_id,
+						'site_id'    => $group->site_id,
+						'permission' => $permission
+					];
+				}
+			}
+		}
+
+		if ( ! empty($insert))
+		{
+			ee()->db->insert_batch('permissions', $insert);
+		}
+
+		foreach ($permissions as $permission)
+		{
+			ee()->smartforge->drop_column('member_groups', $permission);
+		}
 	}
 }
 
