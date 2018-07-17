c0f545ac3d0ceeb8e1a11dd7be25c45e039e22bc
Seth Croston Barber <seth.barber@ellislab.com>
2018-07-17 13:40:32 -0700

    Adding Role Model :joy:

---
+++ system/ee/EllisLab/ExpressionEngine/Model/Role/Role.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * ExpressionEngine (https://expressionengine.com)
+ *
+ * @link      https://expressionengine.com/
+ * @copyright Copyright (c) 2003-2018, EllisLab, Inc. (https://ellislab.com)
+ * @license   https://expressionengine.com/license
+ */
+
+namespace EllisLab\ExpressionEngine\Model\Role;
+
+use EllisLab\ExpressionEngine\Service\Model\Model;
+
+/**
+ * Role Model
+ */
+class Role extends Model {
+
+	protected static $_primary_key = 'role_id';
+	protected static $_table_name = 'roles';
+
+	protected static $_typed_columns = [
+		'role_id' => 'int',
+	];
+
+	protected static $_relationships = [
+		'Members' => array(
+			'type' => 'hasAndBelongsToMany',
+			'model' => 'Member',
+			'pivot' => array(
+				'table' => 'members_roles'
+			),
+			'weak' => TRUE
+		),
+	];
+
+	protected static $_validation_rules = [
+		'role_id' => 'required',
+		'name'    => 'required',
+	];
+
+	// protected static $_events = [];
+
+	// Properties
+	protected $role_id;
+	protected $name;
+	protected $description;
+
+}
+
+// EOF
