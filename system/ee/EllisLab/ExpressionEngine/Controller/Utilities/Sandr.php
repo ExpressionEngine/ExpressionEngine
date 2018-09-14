3c9d818a92c325fcc15224787dea3c2be1e235a6
Seth Croston Barber <seth.barber@ellislab.com>
2018-09-14 14:54:03 -0700

    Removing the exp_member_group table reference

--- system/ee/EllisLab/ExpressionEngine/Controller/Utilities/Sandr.php
+++ system/ee/EllisLab/ExpressionEngine/Controller/Utilities/Sandr.php
@@ -134,11 +134,6 @@ class Sandr extends Utilities {
 					'file_properties',
 					'url'
 				),
-				'exp_member_groups' => array(
-					'group_title',
-					'group_description',
-					'mbr_delete_notify_emails'
-				),
 				'exp_global_variables'	=> array('variable_data'),
 				'exp_categories'		=> array('cat_image'),
 				'exp_forums'			=> array(
