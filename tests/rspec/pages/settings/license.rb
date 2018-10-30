4bee5c14419632cb9be0a8149b5de8ec51fc73e2
Kevin Cupp <kevin.cupp@gmail.com>
2018-10-30 16:32:50 -0400

    Remove license page tests

--- tests/rspec/pages/settings/license.rb
+++
@@ -1,9 +0,0 @@
-class LicenseSettings < ControlPanelPage
-
-  element :license_file, 'input[name=license_file]'
-
-  def load
-    settings_btn.click
-    click_link 'License & Registration'
-  end
-end
