d5941934b8a76cf266e4bd01cef7b1b194162a75
Kevin Cupp <kevin.cupp@gmail.com>
2018-11-21 16:26:27 -0500

    Fix some merge conflict casualties

--- system/ee/installer/updates/ud_6_00_00.php
+++ system/ee/installer/updates/ud_6_00_00.php
@@ -1,22 +1,14 @@
 <?php
 /**
-<<<<<<< HEAD
- * ExpressionEngine (https://expressionengine.com)
- *
- * @link      https://expressionengine.com/
- * @copyright Copyright (c) 2003-2017, EllisLab, Inc. (https://ellislab.com)
- * @license   https://expressionengine.com/license
-=======
  * This source file is part of the open source project
  * ExpressionEngine (https://expressionengine.com)
  *
  * @link      https://expressionengine.com/
  * @copyright Copyright (c) 2003-2018, EllisLab, Inc. (https://ellislab.com)
  * @license   https://expressionengine.com/license Licensed under Apache License, Version 2.0
->>>>>>> stability
  */
 
-namespace EllisLab\ExpressionEngine\Updater\Version_5_0_0;
+namespace EllisLab\ExpressionEngine\Updater\Version_6_0_0;
 
 /**
  * Update
