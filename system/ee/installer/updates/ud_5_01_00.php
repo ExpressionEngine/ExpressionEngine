937b22d5bc0ff625866fef91cc7ad17c202010c4
Kevin Cupp <kevin.cupp@gmail.com>
2018-11-21 16:07:44 -0500

    Fix namespace in 5.1.0 update file

--- system/ee/installer/updates/ud_5_01_00.php
+++ system/ee/installer/updates/ud_5_01_00.php
@@ -8,7 +8,7 @@
  * @license   https://expressionengine.com/license Licensed under Apache License, Version 2.0
  */
 
-namespace EllisLab\ExpressionEngine\Updater\Version_5_0_0;
+namespace EllisLab\ExpressionEngine\Updater\Version_5_1_0;
 
 /**
  * Update
