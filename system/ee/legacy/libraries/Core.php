75e230a1992eeb9ec8f3ddcaa67d8d4f2abd7860
Kevin Cupp <kevin.cupp@gmail.com>
2018-12-21 12:41:38 -0500

    Add 4.3.7 update file and version bump

--- system/ee/legacy/libraries/Core.php
+++ system/ee/legacy/libraries/Core.php
@@ -69,8 +69,8 @@ class EE_Core {
 		// application constants
 		define('IS_CORE',		FALSE);
 		define('APP_NAME',		'ExpressionEngine'.(IS_CORE ? ' Core' : ''));
-		define('APP_BUILD',		'20181002');
-		define('APP_VER',		'4.3.6');
+		define('APP_BUILD',		'20181221');
+		define('APP_VER',		'4.3.7');
 		define('APP_VER_ID',	'');
 		define('SLASH',			'&#47;');
 		define('LD',			'{');
