fd89304a63ba8cf267ae147b729e9debc0c7bc0a
Kevin Cupp <kevin.cupp@gmail.com>
2019-01-03 14:37:51 -0500

    Add Cookie service for easy setting/getting of signed cookies

--- system/ee/EllisLab/ExpressionEngine/app.setup.php
+++ system/ee/EllisLab/ExpressionEngine/app.setup.php
@@ -16,6 +16,7 @@ use EllisLab\ExpressionEngine\Service\Category;
 use EllisLab\ExpressionEngine\Service\ChannelSet;
 use EllisLab\ExpressionEngine\Service\Config;
 use EllisLab\ExpressionEngine\Service\Consent;
+use EllisLab\ExpressionEngine\Service\Cookie;
 use EllisLab\ExpressionEngine\Service\CustomMenu;
 use EllisLab\ExpressionEngine\Service\Database;
 use EllisLab\ExpressionEngine\Service\Encrypt;
@@ -375,6 +376,11 @@ return [
 			return new ChannelSet\Factory(
 				ee()->config->item('site_id')
 			);
+		},
+
+		'Cookie' => function($ee)
+		{
+			return new Cookie\Cookie();
 		},
 
 		'CookieRegistry' => function($ee)
