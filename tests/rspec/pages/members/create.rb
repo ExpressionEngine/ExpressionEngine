72d4c2dbdb58ff2373b59a00bef7705ed1b22c00
Seth Croston Barber <seth.barber@ellislab.com>
2019-03-06 09:55:13 -0800

    Adding the tabs and role elements

--- tests/rspec/pages/members/create.rb
+++ tests/rspec/pages/members/create.rb
@@ -7,10 +7,16 @@ module Member
     element :save_and_new_button, 'form .form-btns-top button[type=submit][value=save_and_new]'
     element :save_and_close_button, 'form .form-btns-top button[type=submit][value=save_and_close]'
 
-    elements :member_groups, 'input[name=group_id]'
     element :username, 'input[name=username]'
     element :email, 'input[name=email]'
     element :password, 'input[name=password]'
     element :confirm_password, 'input[name=confirm_password]'
+
+    element :member_tab, '.tabs li a[rel="t-0"]'
+    element :roles_tab, '.tabs li a[rel="t-1"]'
+
+    elements :primary_roles, 'input[name=role_id]'
+    elements :role_groups, 'input[name=role_groups]'
+    elements :roles, 'input[name=roles]'
   end
 end
