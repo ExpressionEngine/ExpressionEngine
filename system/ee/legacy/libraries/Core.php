3d3d00dc7b7db8576063a1f51967b2f22dfee352
Derek Jones <derek.jones@ellislab.com>
2019-05-27 16:13:25 -0700

    Version bump to 5.2.3

--- system/ee/legacy/libraries/Core.php
+++ system/ee/legacy/libraries/Core.php
@@ -72,8 +72,8 @@ class EE_Core {
 
 		// application constants
 		define('APP_NAME',		'ExpressionEngine');
-		define('APP_BUILD',		'20190328');
-		define('APP_VER',		'5.2.2');
+		define('APP_BUILD',		'20190527');
+		define('APP_VER',		'5.2.3');
 		define('APP_VER_ID',	'');
 		define('SLASH',			'&#47;');
 		define('LD',			'{');
