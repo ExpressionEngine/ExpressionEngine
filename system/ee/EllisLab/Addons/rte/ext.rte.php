c52ead244a5db4fd0deb0fbab4527d6d3c25bbe3
Matt Johnson <matt@packettide.com>
2019-06-06 23:11:20 -0400

    Control panel links update to not point to v4/ docs, but to latest docs (#225)
    * Control panel links update to not point to v4/ docs, but to latest docs
    
    * Added changelog entry

--- system/ee/EllisLab/Addons/rte/ext.rte.php
+++ system/ee/EllisLab/Addons/rte/ext.rte.php
@@ -16,7 +16,7 @@ class Rte_ext {
 	var $name			= 'Rich Text Editor';
 	var $version		= '1.0.1';
 	var $settings_exist	= 'n';
-	var $docs_url		= 'https://docs.expressionengine.com/v4/add-ons/rte/control_panel/index.html';
+	var $docs_url		= DOC_URL.'add-ons/rte/control_panel/index.html';
 	var $required_by	= array('module', 'fieldtype');
 
 	private $module = 'rte';
