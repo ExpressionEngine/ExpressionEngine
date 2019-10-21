45b0e97d742096270cc5175d21063e209e56f157
Jordan-Ellis <jordanrhodesellis@gmail.com>
2019-10-21 15:41:38 -0600

    Update 404 page

--- system/ee/EllisLab/ExpressionEngine/View/errors/file_not_found.php
+++ system/ee/EllisLab/ExpressionEngine/View/errors/file_not_found.php
@@ -1,15 +1,13 @@
 <?php $this->extend('_templates/wrapper'); ?>
 
-<div class="col-group snap">
-	<div class="col w-16 last">
-		<div class="box">
-			<h1><?=lang('404_does_not_exist')?></h1>
-			<div class="txt-wrap">
-				<p><?=lang('404_does_not_exist_desc')?></p>
-				<?php if (trim($url)): ?>
-					<p><b><?=lang('url')?>:</b> <?=$url?></p>
-				<?php endif; ?>
-			</div>
-		</div>
+<div class="four-o-four">
+	<div class="four-o-four__inner">
+	<h1 class="four-o-four__title"><?=lang('404_does_not_exist')?></h1>
+	<div class="four-o-four__body typography">
+		<p><?=lang('404_does_not_exist_desc')?></p>
+		<?php if (trim($url)): ?>
+			<p><b><?=lang('url')?>:</b> <?=$url?></p>
+		<?php endif; ?>
+	</div>
 	</div>
 </div>
