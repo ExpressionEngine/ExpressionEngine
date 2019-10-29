e30938cf1740523493c822288ced425ecab38837
Jordan-Ellis <jordanrhodesellis@gmail.com>
2019-10-29 13:09:19 -0600

    Implement file card html

--- system/ee/EllisLab/ExpressionEngine/View/_shared/thumb.php
+++ system/ee/EllisLab/ExpressionEngine/View/_shared/thumb.php
@@ -1,12 +1,18 @@
-<div class="tbl-list-wrap">
-	<div class="tbl-list selection-area">
+	<div class="file-card-wrapper selection-area">
 		<?php foreach ($files as $file): ?>
-				<div align="center">
-					<img src="<?=ee('Thumbnail')->get($file)->url?>" /><br />
-					<?=$file->title?>
+				<div class="file-card">
+					<div class="file-card__preview">
+						<img src="<?=ee('Thumbnail')->get($file)->url?>" />
+					</div>
+
+					<div class="file-card__info">
+						<div class="file-card__info-name"><?=$file->title?></div>
+						<div class="file-card__info-subtitle">1200x850 - 1MB</div>
+					</div>
 				</div>
 		<?php endforeach; ?>
-		<?php if (empty($data) && isset($no_results)): ?>
+	</div>
+	<?php if (empty($data) && isset($no_results)): ?>
 			<div class="tbl-row no-results">
 				<div class="none">
 					<p><?=$no_results['text']?><?php if (isset($no_results['href'])): ?> <a href="<?=$no_results['href']?>"><?=lang('add_new')?></a><?php endif ?></p>
@@ -15,5 +21,3 @@
 				</div>
 			</div>
 		<?php endif ?>
-	</div>
-</div>
