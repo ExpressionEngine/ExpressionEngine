2f2b71810e462e599f5be018ad27525cc951c36f
Jordan-Ellis <jordanrhodesellis@gmail.com>
2019-11-07 13:20:48 -0700

    Change button type

--- system/ee/EllisLab/Addons/fluid_field/views/filters.php
+++ system/ee/EllisLab/Addons/fluid_field/views/filters.php
@@ -1,4 +1,4 @@
-<a href="#" class="js-dropdown-toggle button button--secondary"><i class="icon--add"></i> <?=lang('add')?></a>
+<a href="#" class="js-dropdown-toggle button button--secondary-alt"><i class="icon--add"></i> <?=lang('add')?></a>
 <div class="dropdown">
 	<?php foreach ($fields as $field): ?>
 		<a href="#" class="dropdown__link" data-field-name="<?=$field->getShortName()?>"><?=$field->getItem('field_label')?></a>
