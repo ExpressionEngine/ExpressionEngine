f5fff24749b7c62d45fbe442c34a4d1e57d9436f
Jordan-Ellis <jordanrhodesellis@gmail.com>
2019-11-07 17:32:28 -0700

    Update the login page

--- system/ee/EllisLab/ExpressionEngine/View/_templates/login.php
+++ system/ee/EllisLab/ExpressionEngine/View/_templates/login.php
@@ -4,18 +4,13 @@
 		<?=ee()->view->head_title($cp_page_title)?>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" lang="en-us" dir="ltr">
 		<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"  name="viewport">
-		<?=ee()->view->head_link('css/out.min.css'); ?>
+		<?=ee()->view->head_link('css/common.min.css'); ?>
 	</head>
 	<body>
-		<section class="flex-wrap">
-			<section class="wrap">
+		<section class="login-container">
+			<section class="login">
 
 				<?=$child_view?>
-
-				<section class="bar">
-					<p class="left"><a href="https://expressionengine.com/" rel="external"><b>ExpressionEngine</b></a></p>
-					<p class="right">&copy;<?=ee()->localize->format_date('%Y')?> <a href="https://ellislab.com/" rel="external">EllisLab</a> Corp.</p>
-				</section>
 
 			</section>
 		</section>
