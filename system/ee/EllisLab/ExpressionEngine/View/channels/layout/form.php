594febf5437d805793f7341ffd89d179226aeef2
Jordan-Ellis <jordanrhodesellis@gmail.com>
2019-11-22 22:41:00 -0700

    Implement the new channel layout styles.

--- system/ee/EllisLab/ExpressionEngine/View/channels/layout/form.php
+++ system/ee/EllisLab/ExpressionEngine/View/channels/layout/form.php
@@ -1,67 +1,73 @@
-<?php $this->extend('_templates/default-nav', array(), 'outer_box'); ?>
+<?php $this->extend('_templates/default-nav'); ?>
 
 <div class="form-standard has-tabs publish" data-publish>
 	<?=form_open($form_url, 'class="ajax-validate"')?>
 	<div class="form-btns form-btns-top">
-		<h1><?=$cp_page_title?></h1>
 		<?php $this->embed('ee:_shared/form/buttons'); ?>
 	</div>
 	<div class="tab-wrap">
-		<ul class="tabs layout">
+		<?=ee('CP/Alert')->get('layout-form')?>
+
+		<?=$form?>
+
+		<div class="field-instruct">
+			<label><?=lang('tabs')?></label>
+		</div>
+
+		<div class="tab-bar tab-bar--editable layout">
+			<div class="tab-bar__tabs">
 			<?php foreach ($layout->getTabs() as $index => $tab): ?>
 				<?php
 				$icon = '';
 				if (strpos($tab->id, 'custom_') !== FALSE)
 				{
-					$icon = '<span class="tab-remove">';
+					$icon = '<i class="tab-remove">';
 				}
 				else
 				{
 					if ($tab->isVisible())
 					{
-						$icon = '<span class="tab-on">';
+						$icon = '<i class="tab-on">';
 					}
 					else
 					{
-						$icon = '<span class="tab-off">';
+						$icon = '<i class="tab-off">';
 					}
 				}
 				?>
-			<li><a<?php if ($index == 0): ?> class="act"<?php endif; ?> href="" rel="t-<?=$index?>"><?=lang($tab->title)?></a> <?php if ($tab->title != 'publish'): ?><?=$icon?></span><?php endif; ?></li>
+				<a class="tab-bar__tab js-tab-button <?php if ($index == 0): ?>active<?php endif; ?>" href="" rel="t-<?=$index?>"><?=lang($tab->title)?> <?php if ($tab->title != 'publish'): ?><?=$icon?></i><?php endif; ?></a>
 			<?php endforeach; ?>
-		</ul>
-		<a class="add-tab m-link" rel="modal-add-new-tab" href="#"><?=lang('add_tab')?></a>
+			</div>
+
+			<a class="tab-bar__right-button button button--small button--action m-link" rel="modal-add-new-tab" href="#"><?=lang('add_tab')?></a>
+		</div>
 			<input type="hidden" name="field_layout" value='<?=json_encode($channel_layout->field_layout)?>'>
-			<?=ee('CP/Alert')->get('layout-form')?>
+
 			<?php foreach ($layout->getTabs() as $index => $tab): ?>
 			<div class="tab t-<?=$index?><?php if ($index == 0): ?> tab-open<?php endif; ?>">
-				<div class="layout-grid-wrap">
+				<div class="layout-item-wrapper">
 			<?php $fields = $tab->getFields();
 			foreach ($fields as $field): ?>
-				<div class="col-group">
-					<div class="col w-16">
-						<div class="layout-item">
-							<span class="reorder"></span>
-							<div class="field-instruct">
-								<label><?=$field->getLabel()?> <span class="faded">(<?=$field->getTypeName()?>)</label>
-								<div class="field-options">
-									<?php if ($field->isRequired()): ?>
-									<label class="field-option-required"><?=ucwords(lang('required_field'))?></label>
-									<?php else: ?>
-									<label class="field-option-hide"><input type="checkbox"<?php if ( ! $field->isVisible()): ?> checked="checked"<?php endif ?>><?=lang('hide')?></label>
-									<?php endif; ?>
-									<label class="field-option-collapse"><input type="checkbox"<?php if ($field->isCollapsed()):?> checked="checked"<?php endif ?>><?=lang('collapse')?></label>
+						<div class="js-layout-item">
+							<div class="layout-item">
+								<div class="layout-item__handle ui-sortable-handle"></div>
+								<div class="layout-item__content">
+									<label class="layout-item__title"><?=$field->getLabel()?> <span class="faded">(<?=$field->getTypeName()?>)</label>
+									<div class="layout-item__options">
+										<?php if ($field->isRequired()): ?>
+										<label class="field-option-required"><?=ucwords(lang('required_field'))?></label>
+										<?php else: ?>
+										<label class="field-option-hide"><input class="checkbox checkbox--small" type="checkbox"<?php if ( ! $field->isVisible()): ?> checked="checked"<?php endif ?>><?=lang('hide')?></label>
+										<?php endif; ?>
+										<label class="field-option-collapse"><input class="checkbox checkbox--small" type="checkbox"<?php if ($field->isCollapsed()):?> checked="checked"<?php endif ?>><?=lang('collapse')?></label>
+									</div>
 								</div>
 							</div>
 						</div>
-					</div>
-				</div>
 			<?php endforeach; ?>
 				</div>
 			</div>
 			<?php endforeach; ?>
-
-			<?=$form?>
 
 			<div class="form-btns">
 				<?php $this->embed('ee:_shared/form/buttons'); ?>
