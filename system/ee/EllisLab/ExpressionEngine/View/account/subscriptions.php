6c1667be464952ca0b03e1ae4ab91b7ebb44f03d
Jordan-Ellis <jordanrhodesellis@gmail.com>
2019-12-19 18:48:30 -0700

    Fix subscriptions page title-bar

--- system/ee/EllisLab/ExpressionEngine/View/account/subscriptions.php
+++ system/ee/EllisLab/ExpressionEngine/View/account/subscriptions.php
@@ -1,17 +1,19 @@
 <?php $this->extend('_templates/default-nav'); ?>
 
 <?=form_open($table['base_url'])?>
-	<fieldset class="tbl-search right">
-		<input placeholder="<?=lang('type_phrase')?>" type="text" name="search" value="<?=htmlentities($table['search'], ENT_QUOTES, 'UTF-8')?>">
-		<input class="btn submit" type="submit" value="<?=lang('search_subscriptions')?>">
-	</fieldset>
-	<h1>
-		<?php echo isset($cp_heading) ? $cp_heading : $cp_page_title?>
-	</h1>
-
-	<?php if (isset($filters)) echo $filters; ?>
-
 	<div class="app-notice-wrap"><?=ee('CP/Alert')->getAllInlines()?></div>
+
+	<div class="title-bar">
+		<h2 class="title-bar__title">
+			<?php echo isset($cp_heading) ? $cp_heading : $cp_page_title?></br>
+		</h2>
+		<?php if (isset($filters)) echo $filters; ?>
+		<div class="title-bar__extra-tools">
+			<div class="search-input">
+			<input class="search-input__input" placeholder="<?=lang('search')?>" type="text" name="search" value="<?=htmlentities($table['search'], ENT_QUOTES, 'UTF-8')?>">
+			</div>
+		</div>
+	</div>
 
 	<?php $this->embed('_shared/table', $table); ?>
 
