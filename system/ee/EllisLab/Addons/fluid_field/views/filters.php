fcbc4fe9b267b13e3b1b9647ec0adf6c26f20b55
Jordan-Ellis <jordanrhodesellis@gmail.com>
2020-01-19 11:41:33 -0700

    Add spacing to icon

--- system/ee/EllisLab/Addons/fluid_field/views/filters.php
+++ system/ee/EllisLab/Addons/fluid_field/views/filters.php
@@ -1,4 +1,4 @@
-<button type="button" class="js-dropdown-toggle button button--secondary-alt"><i class="icon--add"></i> <?=lang('add_field')?></button>
+<button type="button" class="js-dropdown-toggle button button--secondary-alt"><i class="icon--add icon-left"></i> <?=lang('add_field')?></button>
 <div class="dropdown">
 	<?php foreach ($fields as $field): ?>
 		<a href="#" class="dropdown__link" data-field-name="<?=$field->getShortName()?>"><?=$field->getItem('field_label')?></a>
