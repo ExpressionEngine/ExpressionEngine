162fdf656039f0e82408a6f2012fb78a5c887384
Matt Johnson <matt@packettide.com>
2020-02-14 17:18:29 -0500

    Add example pro member tag

--- system/ee/EllisLab/Addons/pro/levelups/member/mod.member.php
+++ system/ee/EllisLab/Addons/pro/levelups/member/mod.member.php
@@ -6,5 +6,11 @@ LiteLoader::loadIntoNamespace('member/mod.member.php');
 
 class Member extends Lite\Member
 {
-
+	/**
+	 * This is an example of where you would put a pro member function
+	 */
+	public function pro_member_tag()
+	{
+		// Do something cool
+	}
 }
