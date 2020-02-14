7303d8cae824d0860646205edc3f9ecba93d8ffd
Matt Johnson <matt@packettide.com>
2020-02-14 17:48:39 -0500

    Add the logout form tag with a pro border

--- system/ee/EllisLab/Addons/pro/levelups/member/mod.member.php
+++ system/ee/EllisLab/Addons/pro/levelups/member/mod.member.php
@@ -6,11 +6,37 @@ LiteLoader::loadIntoNamespace('member/mod.member.php');
 
 class Member extends Lite\Member
 {
-	/**
-	 * This is an example of where you would put a pro member function
-	 */
-	public function pro_member_tag()
-	{
-		// Do something cool
-	}
+    /**
+     * Manual Logout Form
+     *
+     * This lets users create a stand-alone logout form in any template
+     */
+    public function logout_form()
+    {
+        // Create form
+        $data['hidden_fields'] = array(
+            'ACT' => ee()->functions->fetch_action_id('Member', 'member_logout'),
+            'RET' => (ee()->TMPL->fetch_param('return') && ee()->TMPL->fetch_param('return') != "") ?
+                ee()->TMPL->fetch_param('return') : '-2'
+          );
+
+        if (ee()->TMPL->fetch_param('form_name') && ee()->TMPL->fetch_param('form_name') != "") {
+            $data['name'] = ee()->TMPL->fetch_param('form_name');
+        }
+
+        $data['id'] = ee()->TMPL->form_id;
+        $data['class'] = ee()->TMPL->form_class;
+        $data['action'] = ee()->TMPL->fetch_param('action');
+
+        // PRO logout form gets a really cool border!
+        $res = "<style type='text/css'>
+        form { border: 5px dotted #ccc!important; 	-webkit-animation: blink .5s step-end infinite alternate; }
+	    @-webkit-keyframes blink { 50% { border-color: #e7ff23; }  }</style>";
+
+        $res .= ee()->functions->form_declaration($data);
+        $res .= stripslashes(ee()->TMPL->tagdata);
+        $res .= "</form>";
+
+        return $res;
+    }
 }
