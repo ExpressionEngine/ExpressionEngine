67742d83bacc7edc44da3716c9f1ba93e4d4e91f
Matt Johnson <matt@packettide.com>
2020-02-14 16:56:14 -0500

    Add the member addon. This addon stomps over the native one, and extends it

---
+++ system/ee/EllisLab/Addons/pro/levelups/member/addon.setup.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+	'author'         => 'EllisLab',
+	'author_url'     => 'https://ellislab.com/',
+	'name'           => 'Member',
+	'description'    => '',
+	'version'        => '3.0.0',
+	'namespace'      => 'EllisLab\Addons\Member',
+	'settings_exist' => FALSE,
+	'built_in'       => TRUE
+);
---
+++ system/ee/EllisLab/Addons/pro/levelups/member/mod.member.php
@@ -0,0 +1,10 @@
+<?php
+
+use EllisLab\Addons\Pro\Components\LiteLoader;
+
+LiteLoader::loadIntoNamespace('member/mod.member.php');
+
+class Member extends Lite\Member
+{
+
+}
