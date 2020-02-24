58b090ce9ccef0952ac1c920098701f9097df54d
Yuri Salimovskiy <yuri@designingit.com>
2020-02-24 08:49:34 +0200

    stop tracking pro version

--- system/ee/EllisLab/Addons/pro/levelups/channel/View/single_field_editor.php
+++
@@ -1,8 +0,0 @@
-{custom_fields}
-	{formatting_buttons}
-	{display_field}
-{/custom_fields}
-<input type="submit" />
-<?php if ($full_link): ?>
-<a href="<?=$full_link?>"><?php echo lang('edit_in_full_form') ?></a>
-<?php endif; ?>
