b414d98ab0f82d2effb2e07d60fc5babc24a3cc8
Bryan Nielsen <bryan@packettide.com>
2020-03-05 17:03:53 -0500

    Remove php 7.4 from tests until support is added

--- tests/cypress/support/config.php
+++ tests/cypress/support/config.php
@@ -41,7 +41,8 @@ $config['cache_driver'] = 'file';
 $config['database'] = array(
 	'expressionengine' => array(
 		'database' => 'ee-test',
-		'username' => 'root'
+        'username' => 'root',
+        'password' => 'root',
 	),
 );
 
