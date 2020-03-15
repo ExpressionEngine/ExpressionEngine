38447081458acc432a4b70f08272e8152b096e0c
Douglas Black <dougblackjr@gmail.com>
2020-03-15 08:50:44 -0400

    Removing safe mode

--- system/ee/legacy/libraries/Core.php
+++ system/ee/legacy/libraries/Core.php
@@ -41,7 +41,7 @@ class EE_Core {
 		}
 
 		// Set a liberal script execution time limit, making it shorter for front-end requests than CI's default
-		if (function_exists("set_time_limit") == TRUE AND @ini_get("safe_mode") == 0)
+		if (function_exists("set_time_limit") == TRUE)
 		{
 			@set_time_limit((REQ == 'CP') ? 300 : 90);
 		}
