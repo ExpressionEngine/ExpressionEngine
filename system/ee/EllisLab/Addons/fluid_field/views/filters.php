843f6d30333832dd9846a22019e09cc817f010e3
Yuri Salimovskiy <yuri@designingit.com>
2020-03-27 10:29:03 +0200

    EECORE-79 Fluid UI changes to use bigger buttons with icons

--- system/ee/EllisLab/Addons/fluid_field/views/filters.php
+++ system/ee/EllisLab/Addons/fluid_field/views/filters.php
@@ -1,6 +1,19 @@
-<button type="button" class="js-dropdown-toggle button button--secondary-alt"><i class="icon--add icon-left"></i> <?=lang('add_field')?></button>
-<div class="dropdown">
+<?php if (count($fields) < 20) : ?>
+
 	<?php foreach ($fields as $field): ?>
-		<a href="#" class="dropdown__link" data-field-name="<?=$field->getShortName()?>"><?=$field->getItem('field_label')?></a>
+		<a href="#" class="button button--auto button--secondary-alt" data-field-name="<?=$field->getShortName()?>">
+			<img src="<?=$field->getIcon()?>" width="24" height="24" /><br />
+			<?=lang('add')?> <?=$field->getItem('field_label')?>
+		</a>
 	<?php endforeach; ?>
-</div>
+
+<?php else: ?>
+
+	<a href="javascript:void(0)" class="js-dropdown-toggle button button--auto button--secondary-alt"><i class="fa-2x icon--add"></i><br /> <?=lang('add_field')?></a>
+	<div class="dropdown">
+		<?php foreach ($fields as $field): ?>
+			<a href="#" class="dropdown__link" data-field-name="<?=$field->getShortName()?>"><img src="<?=$field->getIcon()?>" width="12" height="12" /> <?=$field->getItem('field_label')?></a>
+		<?php endforeach; ?>
+	</div>
+
+<?php endif; ?>
