c2204ada98700470138486f78365bd0f6c548130
Tom Jaeger <tomjaeger@Toms-MBP-2.nycap.rr.com>
2020-04-24 08:40:19 -0400

    updated welcome message

--- system/ee/EllisLab/ExpressionEngine/View/_shared/dashboard/welcome.php
+++ system/ee/EllisLab/ExpressionEngine/View/_shared/dashboard/welcome.php
@@ -1,35 +1,38 @@
 <style type="text/css">
-.ee-main--dashboard {
-	background: var(--ee-bg-0);
-}
+
 </style>
 
-<div class="dashboard__item dashboard__item--full widget" id="welcome-screen" style="display: none">
+<div class="dashboard__item dashboard__item--full color: var(--ee-text-normal);" id="welcome-screen" style="display: none">
 	<div class="widget__title-bar">
-		<h2 class="widget__title">Welcome to new ExpressionEngine</h2>
+		
 
-		<div>
-			<a class="button button--secondary-alt js-jump-menu-trigger" href="#">Show Jump Menu</a>
-			<a class="button button--secondary-alt show-sidebar" href="#">Show Main Navigation</a>
-		</div>
+		
 	</div>
 
-	<div style="margin: auto; text-align: center">
-	<img src="/themes/ee/asset/img/expressionengine-logo.png">
+	<div style="margin: auto; text-align: center; width:225px">
+		<img src="/themes/ee/asset/img/expressionengine-logo.png">
 	</div>
+	<div style="margin: auto; text-align: left; width:600px; padding-top: 50px">
+	<h3 style="text-align: center;">Welcome to EE 6 Alpha</h3>
+	<p style="padding-top: 25px;">We want to take this opportunity to introduce you to what we believe is one of the largest innovations added to ExpressionEngine in the last 5 years.</p>
+	<h5 style="padding-top: 25px;">The Jump Menu</h5>
+	<p style="padding-top: 25px;">Q. What is it?
+		<br>
+		A. It's kind of like Alphred for a CMS, and the fastest way to use the control panel -- by a long shot.
+		<br>
+		<br>
+		<br>
+		Q. How powerful is it?
+		<br>
+		A. Powerful enough that we created a navless control panel option.
+		<br>
+		<br>
+		<br>
+		<h1 style="text-align: center;">⌘ + J</h1>
+	</p>
 
-	<p>Welcome Alpha testers!</p>
-	<p>ExpressionEngine stores your content inside <em>Channels</em>. A channel is simply a container that holds information. This information might be the text of an article or blog post, or it could be an image, or some other type of information. Channel information exists independently from the pages your visitors see.</p><p class="alert alert--hint"> <strong>Important Concept:</strong> A channel is just data. There is no assumed association between this data and any particular page of your site.</p><p>In your control panel, Channels are managed and configured in the <a href="../control-panel/channels.html">Channel Manager</a>. ExpressionEngine supports a wide variety of <a href="../fieldtypes/overview.html">Field Types</a>, so that the information in each of your channels can be stored appropriately for that data type.</p><p>For example, you might need a “staff” page with employee biographies. To manage this you could create a channel with fields for the name, biography, an image, and any other relevant info.</p><p>Content is added to a channel by clicking the <strong>Edit</strong> button in the main control panel navigation.</p>
-	<p>In ExpressionEngine, a page (or a page component such as a header or footer) is called a <em>Template</em>.</p><p>The simplest way to think of a Template is as a container that represents a single page of your site. As such, a Template may contain anything that a webpage might contain: HTML, JavaScript, etc. A Template can also be a smaller component of your page. Through the use of the <a href="../templates/embedding.html">Embed Tag</a> you can insert a Template into another Template. This allows you to reuse components such as headers or footers.</p><p>In addition to HTML and other markup, Templates will usually contain ExpressionEngine Tags. These Tags allow you to pull data from your channels (or from any other module, plugin, or add-on) and display it in a template.</p><p>Templates are organized into Template Groups. A Template Group is analogous to a folder on your server.</p>
-	<ul>
-	<li>A Channel consists of “information”–your articles, comments, preferences, and other related “data.”</li>
-	<li>A Template represents a single page or a smaller section of your site.</li>
-	<li>A Template Group contains a collection of Templates.</li>
-	<li>ExpressionEngine Tags permit you to show data from a Channel, or any other Module or add-on, in your Templates.</li>
-	</ul>
-	<p class="alert alert--hint"> <strong>Tip:</strong> Check out our <a href="../getting-started/ten-minute-primer.html">10 Minute Primer</a> to get you started fast.</p>
 	<?=form_open(ee('CP/URL')->make('homepage/set-viewmode'))?>
-	<div class="button-group">
+	<div class="button-group" style="padding-top: 35px;">
 		<?=form_button(['name' => 'ee_cp_viewmode', 'value' => 'jumpmenu', 'type' => 'submit'], "Proceed with Jump Menu only", 'class="btn" data-submit-text="Proceed with Jump Menu only" data-work-text="Please wait..."')?>
 		<?=form_button(['name' => 'ee_cp_viewmode', 'value' => 'classic', 'type' => 'submit'], "Proceed with Jump Menu and Main Navigation", 'class="btn" data-submit-text="Proceed with Jump Menu and Main Navigation" data-work-text="Please wait..."')?>
 	</div>
