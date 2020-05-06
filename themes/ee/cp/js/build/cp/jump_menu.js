089577f7fed98e8f304aced0e82defb14b825b04
Yuri Salimovskiy <yuri@packettide.com>
2020-05-06 09:16:57 +0300

    cypress compatibility for jump menu EECORE-333

--- themes/ee/cp/js/build/cp/jump_menu.js
+++ themes/ee/cp/js/build/cp/jump_menu.js
@@ -41,7 +41,8 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
 /**
  * EE Jump Menu
  */
-var jumpContainer = window.top;
+var jumpContainer = (typeof(window.top.Cypress) !== 'undefined') ? window : window.top;
+
 EE.cp.JumpMenu = {
   typingAjaxDelay: 400,
   // Internal Variables
