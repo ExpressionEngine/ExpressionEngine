837d7d8d0a73d5aa7969dacf038a481c20962d10
Yuri Salimovskiy <yuri@packettide.com>
2020-05-18 15:10:13 +0300

    sql manager fixes

--- system/ee/ExpressionEngine/View/_shared/form/bulk-action-bar.php
+++ system/ee/ExpressionEngine/View/_shared/form/bulk-action-bar.php
@@ -13,5 +13,5 @@
 		><?=$option['text']?></option>
 		<?php endforeach; ?>
 	</select>
-		<button name="bulk_action_submit" class="button button--primary"<?php if (isset($modal) && $modal) : ?> data-conditional-modal="confirm-trigger" <?php endif; ?> type="submit"<?php if (isset($ajax_url)) : ?> data-confirm-ajax="<?=$ajax_url?>"<?php endif; ?>><?=lang('submit')?></button>
+		<button name="bulk_action_submit" value="submit" class="button button--primary"<?php if (isset($modal) && $modal) : ?> data-conditional-modal="confirm-trigger" <?php endif; ?> type="submit"<?php if (isset($ajax_url)) : ?> data-confirm-ajax="<?=$ajax_url?>"<?php endif; ?>><?=lang('submit')?></button>
 </fieldset>
