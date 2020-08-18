03e5f0603be8ea09f91d5691bdae76326fa734e3
Andy Johnson <andy@shapingthepage.com>
2020-08-18 13:33:24 -0400

    Styling updates for redirect page

--- system/ee/ExpressionEngine/View/errors/redirect.php
+++ system/ee/ExpressionEngine/View/errors/redirect.php
@@ -1,13 +1,18 @@
 <?php $this->extend('_templates/out'); ?>
 
-<div class="box warn">
-	<h1>Redirect Warning <span class="icon-issue"></span></h1>
-	<div class="updater-msg">
+<div class="panel redirect">
+  <div class="panel-heading">
+    <h3>Redirect Warning</h3>
+  </div>
+	<div class="panel-body">
 		<p>You’re opening a new web page going to host <b><?=$host?></b> that is not part of <?=config_item('site_label')?>.</p>
 		<p>Please double check that the address is correct.</p>
 		<div class="alert-notice">
-			<p><?=$url?></p>
+			<code><?=$url?></code>
 		</div>
-		<p class="msg-choices"><?=$link?> or <a href="<?=config_item('site_url')?>">Cancel</a></p>
+
 	</div>
+  <div class="panel-footer">
+    <?=$link?> <a href="<?=config_item('site_url')?>" class="button button--default">Cancel</a>
+  </div>
 </div>
