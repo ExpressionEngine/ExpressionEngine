112f9e6fdfc7fcdcbfb52983ebbaf9d23f112f01
Yuri Salimovskiy <yuri@packettide.com>
2020-09-04 12:39:27 +0300

    try to fix Cypress error when running on Github

--- tests/cypress/cypress/plugins/index.js
+++ tests/cypress/cypress/plugins/index.js
@@ -121,5 +121,12 @@ module.exports = (on, config) => {
         }
     })
 
+    on('before:browser:launch', (browser, launchOptions) => {
+        if (browser.name === 'chrome' && browser.isHeadless) {
+            launchOptions.args.push('--disable-gpu');
+            return launchOptions
+        }
+    });
+
     return config;
 }
