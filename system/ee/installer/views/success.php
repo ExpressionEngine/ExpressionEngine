c4507588d1c069f8388c71e0601eeb0a0cdfd7aa
Andy Johnson <andy@shapingthepage.com>
2020-10-29 10:52:48 -0400

    Styling tweaks for update success message

--- system/ee/installer/views/success.php
+++ system/ee/installer/views/success.php
@@ -63,25 +63,34 @@
   </div>
   <div class="panel-body">
     <div class="updater-msg">
-  		<p><?=$success_note?></p>
+  		<p style="margin-bottom: 20px;"><?=$success_note?></p>
 
   		<?php if ( ! empty($update_notices)): ?>
   			<?php foreach ($update_notices as $notice): ?>
-  				<div class="alert-notice">
-  					<?php if ($notice->is_header): ?>
-  						<p><b><?=$notice->message?></b></p>
-  					<?php else: ?>
-  						<p><?=$notice->message?></p>
-  					<?php endif?>
+  				<div class="alert alert--attention">
+            <div class="alert__icon">
+              <i class="fas fa-info-circle fa-fw"></i>
+            </div>
+            <div class="alert__content">
+    					<?php if ($notice->is_header): ?>
+                <div class="alert__title">
+                  <p><?=$notice->message?></p>
+                </div>
+    					<?php else: ?>
+    						<p><?=$notice->message?></p>
+    					<?php endif?>
+            </div>
   				</div>
   			<?php endforeach;?>
   		<?php endif ?>
-  		<p class="msg-choices">
-  			<a href="<?=$cp_login_url?>"><?=lang('cp_login')?></a>
-  			<?php if ($mailing_list): ?>
-  				<a href="<?=$action?>&download=mailing_list.zip"><?=lang('download_mailing_list')?></a>
-  			<?php endif; ?>
-  		</p>
   	</div>
+  </div>
+  <div class="panel-footer">
+    <p class="msg-choices">
+      <a href="<?=$cp_login_url?>" class="button button--primary"><?=lang('cp_login')?></a>
+      <?php if ($mailing_list): ?>
+        <a href="<?=$action?>&download=mailing_list.zip"><?=lang('download_mailing_list')?></a>
+      <?php endif; ?>
+    </p>
   </div>
 </div>
