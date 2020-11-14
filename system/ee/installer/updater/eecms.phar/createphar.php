14acc3c7f7250610ff6aec062e3e3ac495e30967
Andy Johnson <andy@shapingthepage.com>
2020-11-14 15:33:52 -0500

    Fresh EE install

--- system/ee/installer/updater/eecms.phar/createphar.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$src_folder = __DIR__.'/src';
-$build_folder = realpath(__DIR__.'/../../../');
-
-$phar = new Phar($build_folder . '/eecms.phar', 0, 'eecms.phar');
-$phar->setStub("#!/usr/bin/env php\n".$phar->createDefaultStub('index.php'));
-$phar->buildFromDirectory($src_folder);
-rename($build_folder.'/eecms.phar', $build_folder.'/eecms');
--- system/ee/installer/views/surveys/survey_2_9_0.php
+++
@@ -1,1 +0,0 @@
-<?php $this->load->view('surveys/default_survey'); ?>
