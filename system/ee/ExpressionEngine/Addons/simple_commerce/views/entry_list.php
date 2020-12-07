efa06c782c05a554addb42ec24e0c709288f243f
Yuri Salimovskiy <yuri@packettide.com>
2020-12-07 13:44:18 +0200

    simple commerce UI fixes EECORE-861

--- system/ee/ExpressionEngine/Addons/simple_commerce/views/entry_list.php
+++ system/ee/ExpressionEngine/Addons/simple_commerce/views/entry_list.php
@@ -1,10 +1,22 @@
 <div class="panel">
-	<?=form_open($form_url, 'class="tbl-ctrls"')?>
+	<div class="tbl-ctrls">
+	<?=form_open($form_url, ['data-search-url' => $base_url])?>
     <div class="panel-heading">
       <div class="app-notice-wrap"><?=ee('CP/Alert')->getAllInlines()?></div>
   		<h3 class="title-bar__title"><?=sprintf(lang('create_new_item_step'), 1)?><br><i><?=lang('create_new_item_step_desc')?></i></h3>
 
-  		<?php if (isset($filters)) echo $filters; ?>
+		<div class="filter-search-bar" style="background: none; padding: 10px 0 0 0; border: none;">
+
+			<!-- All filters (not including search input) are contained within 'filter-search-bar__filter-row' -->
+			<div class="filter-search-bar__filter-row">
+				<?php if (isset($filters)) echo $filters; ?>
+			</div>
+
+			<!-- The search input and non-filter controls are contained within 'filter-search-bar__search-row' -->
+			<div class="filter-search-bar__search-row">
+				<?php if (isset($filters_search)) echo $filters_search; ?>
+			</div>
+		</div>
     </div>
 
 		<?php $this->embed('ee:_shared/table', $table); ?>
@@ -24,4 +36,5 @@
 		]); ?>
 		<?php endif; ?>
 	<?=form_close()?>
+	</div>
 </div>
