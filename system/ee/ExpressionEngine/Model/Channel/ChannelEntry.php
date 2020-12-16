a2460afa2d252dd9a38eae4eeac03ec5d9588572
Yuri Salimovskiy <yuri@packettide.com>
2020-12-16 15:33:54 +0200

    check for var existance before checking its value

--- system/ee/ExpressionEngine/Model/Channel/ChannelEntry.php
+++ system/ee/ExpressionEngine/Model/Channel/ChannelEntry.php
@@ -1190,11 +1190,11 @@ class ChannelEntry extends ContentModel {
 		}
 
 		$member = ee()->session->getMember();
-		if (!empty($member) || $this->Channel->ChannelFormSettings->allow_guest_posts == 'y') {
+		if (!empty($member) || (isset($this->Channel->ChannelFormSettings->allow_guest_posts) && $this->Channel->ChannelFormSettings->allow_guest_posts == 'y')) {
 			$assigned_statuses = !empty($member) ? $member->getAssignedStatuses()->pluck('status_id') : [];
 
 			foreach ($all_statuses as $status) {
-				if ($this->Channel->ChannelFormSettings->allow_guest_posts == 'y' || ee('Permission')->isSuperAdmin() || in_array($status->getId(), $assigned_statuses)) {
+				if (ee('Permission')->isSuperAdmin() || (isset($this->Channel->ChannelFormSettings->allow_guest_posts) && $this->Channel->ChannelFormSettings->allow_guest_posts == 'y') || in_array($status->getId(), $assigned_statuses)) {
 					$status_options[] = $status->getSelectOptionComponent();
 				}
 			}
