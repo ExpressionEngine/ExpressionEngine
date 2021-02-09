853e927e87f1562ac5b04db393ded1ef3c298d72
Yuri Salimovskiy <yuri@packettide.com>
2021-02-09 08:55:32 +0200

    Fix #800 File Chooser not displaying on Text Input field EECORE-966

--- system/ee/ExpressionEngine/Addons/text/views/publish.php
+++ system/ee/ExpressionEngine/Addons/text/views/publish.php
@@ -6,20 +6,23 @@ $show_file_selector = (isset($settings['field_show_file_selector']) && $settings
 
 if ($show_fmt or $show_file_selector): ?>
 <div class="format-options">
+    <div class="d-flex align-items-center">
 
-	<?php if ($show_file_selector): ?>
-	<ul class="toolbar">
-		<li class="upload"><a class="m-link textarea-field-filepicker" href="<?=$fp_url?>" rel="modal-file" title="<?=lang('upload_file')?>" rel="modal-file" data-input-value="<?=$name?>"></a></li>
-		<?php if ($show_fmt): ?>
-		<li class="form-element">
-			<?=form_dropdown('field_ft_' . $settings['field_id'], $format_options, $settings['field_fmt'])?>
-		</li>
-		<?php endif; ?>
-	</ul>
-	<?php endif; ?>
+    <?php if ($show_file_selector): ?>
+        <div class="button-toolbar toolbar">
+            <div class="button-group button-group-xsmall">
+                <a class="m-link textarea-field-filepicker html-upload button button--default" href="<?=$fp_url?>" rel="modal-file" title="<?=lang('upload_file')?>" rel="modal-file" data-input-value="<?=$name?>"></a>
+                <?php if ($show_fmt): ?>
+                    <?=form_dropdown('field_ft_' . $settings['field_id'], $format_options, $settings['field_fmt'])?>
+                    <span class="mr-s"></span>
+                <?php endif; ?>
+            </div>
+        </div>
+    <?php endif; ?>
 
-	<?php if (! $show_file_selector && $show_fmt): ?>
-		<?=form_dropdown('field_ft_' . $settings['field_id'], $format_options, $settings['field_fmt'])?>
-	<?php endif; ?>
+    <?php if (! $show_file_selector && $show_fmt): ?>
+        <?=form_dropdown('field_ft_' . $settings['field_id'], $format_options, $settings['field_fmt'])?>
+    <?php endif; ?>
+    </div>
 </div>
 <?php endif; ?>
