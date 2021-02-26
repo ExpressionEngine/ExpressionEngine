73a7f9d17cfa4029e07bb6656d8313c391d10bdc
Yuri Salimovskiy <yuri@packettide.com>
2021-02-26 14:21:36 +0200

    enable live previews (even if not set up) if preview URL is directly specified

--- system/ee/ExpressionEngine/Controller/Publish/AbstractPublish.php
+++ system/ee/ExpressionEngine/Controller/Publish/AbstractPublish.php
@@ -526,7 +526,7 @@ abstract class AbstractPublish extends CP_Controller
 
     protected function createLivePreviewModal(ChannelEntry $entry)
     {
-        if ($entry->isLivePreviewable()) {
+        if ($entry->isLivePreviewable() || ee()->input->get('return') != '') {
             $lp_domain_mismatch = false;
             if (isset($_SERVER['HTTP_HOST']) && !empty($_SERVER['HTTP_HOST'])) {
                 $lp_domain_mismatch = true;
