9cd9088d23ee3096446cac482fdca4d8c02b4c21
Matt Johnson <matt@packettide.com>
2021-04-07 15:41:18 -0400

    Removes the unused generator addon files

--- system/ee/ExpressionEngine/Service/Generator/GeneratorAddon/language/english/generator_lang.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$lang = [
-    'generator_module_name' => 'Generator',
-    'generator_module_description' => 'Generate EE Addons right in EE',
-    'generator_settings' => 'Generator Settings',
-    '' => '',
-];
--- system/ee/ExpressionEngine/Service/Generator/GeneratorAddon/mcp.generator.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (! defined('BASEPATH')) {
-    exit('No direct script access allowed');
-}
-
-class Generator_mcp
-{
-}
