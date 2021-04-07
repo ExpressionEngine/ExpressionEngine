6018c4da305db8036f809efaa1e057edcc4782ad
Matt Johnson <matt@packettide.com>
2021-04-07 14:25:22 -0400

    Fixes issues with the model stub

--- system/ee/ExpressionEngine/Service/Generator/stubs/model.php
+++ system/ee/ExpressionEngine/Service/Generator/stubs/model.php
@@ -4,11 +4,11 @@ namespace {{namespace}}\Models;
 
 use ExpressionEngine\Service\Model\Model;
 
-class {{class}} extends Model {
-
+class {{class}} extends Model
+{
     // Documentation: https://docs.expressionengine.com/latest/development/services/model/building-your-own.html
     // You can get this model by using:
-    // ee('Model')->get('{slug}:{class}');
+    // ee('Model')->get('{{namespace}}:{{class}}');
 
     protected static $_primary_key = 'id';
     protected static $_table_name = 'my_awesome_table';
@@ -18,9 +18,6 @@ class {{class}} extends Model {
 
     public function __construct()
     {
-
         // Make magic, my friend
-
     }
-
 }
