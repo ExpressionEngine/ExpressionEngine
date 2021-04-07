d743159c7b2e18e9fd2d879c5beb517722dca825
Matt Johnson <matt@packettide.com>
2021-04-07 13:03:46 -0400

    Fixed issue with the Command Generator

--- system/ee/ExpressionEngine/Service/Generator/CommandGenerator.php
+++ system/ee/ExpressionEngine/Service/Generator/CommandGenerator.php
@@ -5,138 +5,128 @@ namespace ExpressionEngine\Service\Generator;
 use ExpressionEngine\Library\Filesystem\Filesystem;
 use ExpressionEngine\Library\Filesystem\FilesystemException;
 
-class CommandGenerator {
+class CommandGenerator
+{
+    protected $name;
+    protected $className;
+    protected $signature;
+    protected $addon;
+    protected $addonClass;
+    protected $generatorPath;
+    protected $addonPath;
+    protected $stubPath;
 
-	protected $name;
-	protected $className;
-	protected $signature;
-	protected $addon;
-	protected $addonClass;
-	protected $generatorPath;
-	protected $addonPath;
-	protected $stubPath;
+    public function __construct(array $data)
+    {
+        ee()->load->helper('string');
 
-	public function __construct(array $data)
-	{
+        $this->name = $data['name'];
+        $this->addon = $data['addon'];
+        $this->addonClass = studly($data['addon']);
+        $this->className = studly($data['name']);
+        $this->fullClass = $this->addonClass . '\\Commands\\' . studly($data['name']);
+        $this->signature = $data['signature'];
+        $this->description = $data['description'];
 
-		ee()->load->helper('string');
+        $this->init();
+    }
 
-		$this->name = $data['name'];
-		$this->addon = $data['addon'];
-		$this->addonClass = studly($data['addon']);
-		$this->className = studly($data['name']);
-		$this->fullClass = $this->addonClass . '\\Commands\\' . studly($data['name']);
-		$this->signature = $data['signature'];
-		$this->description = $data['description'];
+    private function init()
+    {
+        $this->generatorPath = SYSPATH . 'ee/ExpressionEngine/Service/Generator';
+        $this->addonPath = SYSPATH . 'user/addons/' . $this->addon . '/';
+        $this->commandsPath = SYSPATH . 'user/addons/' . $this->addon . '/Commands/';
+        $filesystem = new Filesystem();
 
-		$this->init();
-	}
+        // Get stub path
+        $this->stubPath = $this->generatorPath . '/stubs' . '/';
 
-	private function init()
-	{
+        if (! $filesystem->isDir($this->commandsPath)) {
+            $filesystem->mkDir($this->commandsPath);
+        }
+    }
 
-		$this->generatorPath = SYSPATH . 'ee/ExpressionEngine/Service/Generator';
-		$this->addonPath = SYSPATH . 'user/addons/' . $this->addon . '/';
-		$this->commandsPath = SYSPATH . 'user/addons/' . $this->addon . '/Commands/';
-		$filesystem = new Filesystem;
+    public function build()
+    {
+        $filesystem = new Filesystem();
 
-		// Get stub path
-		$this->stubPath = $this->generatorPath . '/stubs' . '/';
+        $commandStub = $filesystem->read($this->stub('command.php'));
+        $commandStub = $this->write('name', $this->name, $commandStub);
+        $commandStub = $this->write('class', $this->className, $commandStub);
+        $commandStub = $this->write('signature', $this->signature, $commandStub);
+        $commandStub = $this->write('description', $this->description, $commandStub);
 
-		if ( ! $filesystem->isDir($this->commandsPath) ) {
-		    $filesystem->mkDir($this->commandsPath);
-		}
+        $this->putFile(studly('Command' . $this->className) . '.php', $commandStub);
 
-	}
+        $this->addCommandToAddonSetup();
 
-	public function build()
-	{
+        return true;
+    }
 
-		$filesystem = new Filesystem;
+    private function addCommandToAddonSetup()
+    {
+        $filesystem = new Filesystem();
 
-		$commandStub = $filesystem->read($this->stub('command.php'));
-		$commandStub = $this->write('name', $this->name, $commandStub);
-		$commandStub = $this->write('class', $this->className, $commandStub);
-		$commandStub = $this->write('signature', $this->signature, $commandStub);
-		$commandStub = $this->write('description', $this->description, $commandStub);
+        try {
+            $addonSetup = $filesystem->read($this->addonPath . 'addon.setup.php');
+        } catch (FilesystemException $e) {
+            return false;
+        } catch (\Exception $e) {
+            return false;
+        }
 
-		$this->putFile(studly('Command' . $this->className) . '.php', $commandStub);
+        $commandString = "    '{$this->signature}' => {$this->fullClass}::class,";
+        $useCommandString = "use {$this->fullClass};";
 
-		$this->addCommandToAddonSetup();
+        $filesystem->findAndReplace($this->addonPath . 'addon.setup.php', "<?php", "<?php\n\n{$useCommandString}\n\n");
 
-		return true;
+        if (string_contains($addonSetup, "'commands'") || string_contains($addonSetup, '"commands"')) {
+            $commandStub = $filesystem->read($this->stub('command.addon.php'));
+            $commandStub = $this->write('command_data', $commandString, $commandStub);
 
-	}
+            preg_match('(\]\;|\)\;)', $addonSetup, $matches);
 
-	private function addCommandToAddonSetup()
-	{
+            if (! empty($matches)) {
+                $last = array_values(array_slice($matches, -1))[0];
 
-		$filesystem = new Filesystem;
+                $addonSetup = $this->write($last, $commandStub . "\n\n" . $last, $addonSetup);
+            } else {
+                $stringToReplace = string_contains($addonSetup, "'commands'")
+                                ? '"commands"'
+                                : "'commands'";
 
-		try {
-			$addonSetup = $filesystem->read($this->addonPath . 'addon.setup.php');
-		try {
-			$addonSetup = $filesystem->read($this->addonPath . 'addon.setup.php');
-		} catch (FilesystemException $e) {
-			return false;
-		} catch (\Exception $e) {
-			return false;
-		}
+                // TODO: Find command array and add $commandString to the array
+                preg_match('/(\'|\")commands(\'|\")(\s+)=>(\s+)(\[|array\()/', $addonSetup, $matches);
 
-		$commandString = "    '{$this->signature}' => {$this->fullClass}::class,";
-		$useCommandString = "use {$this->fullClass};";
+                if (! empty($matches) && isset($matches[1])) {
+                    $addonSetup = $this->write($matches[1], $matches[1] . "\n\n" . $commandString, $addonSetup);
+                }
+            }
+        }
+    }
 
-		$filesystem->findAndReplace($this->addonPath . 'addon.setup.php', "<?php", "<?php\n\n{$useCommandString}\n\n");
+    private function stub($file)
+    {
+        return $this->stubPath . $file;
+    }
 
-		if(string_contains($addonSetup, "'commands'") || string_contains($addonSetup, '"commands"')) {
-			$commandStub = $filesystem->read($this->stub('command.addon.php'));
-			$commandStub = $this->write('command_data', $commandString, $commandStub);
+    private function write($key, $value, $file)
+    {
+        return str_replace('{{' . $key . '}}', $value, $file);
+    }
 
-			preg_match('(\]\;|\)\;)', $addonSetup, $matches);
+    private function putFile($name, $contents, $path = null)
+    {
+        $filesystem = new Filesystem();
 
-			if(! empty($matches)) {
-				$last = array_values(array_slice($matches, -1))[0];
+        if ($path) {
+            $path = trim($path, '/') . '/';
+        } else {
+            $path = '';
+        }
 
-				$addonSetup = $this->write($last, $commandStub . "\n\n" . $last, $addonSetup);
-
-		} else {
-			$stringToReplace = string_contains($addonSetup, "'commands'")
-								? '"commands"'
-								: "'commands'";
-
-			// TODO: Find command array and add $commandString to the array
-			preg_match('/(\'|\")commands(\'|\")(\s+)=>(\s+)(\[|array\()/', $addonSetup, $matches);
-
-			if(! empty($matches) && isset($matches[1])) {
-				$addonSetup = $this->write($matches[1], $matches[1] . "\n\n" . $commandString, $addonSetup);
-			}
-		}
-
-	}
-
-	private function stub($file)
-	{
-		return $this->stubPath . $file;
-	}
-
-	private function write($key, $value, $file)
-	{
-		return str_replace('{{' . $key . '}}', $value, $file);
-	}
-
-	private function putFile($name, $contents, $path = null)
-	{
-
-		$filesystem = new Filesystem;
-
-		if($path) {
-			$path = trim($path, '/') . '/';
-		} else {
-			$path = '';
-		}
-
-		if(!$filesystem->exists($this->commandsPath . $path . $name)) {
-			$filesystem->write($this->commandsPath . $path . $name, $contents);
-		}
-	}
+        if (!$filesystem->exists($this->commandsPath . $path . $name)) {
+            $filesystem->write($this->commandsPath . $path . $name, $contents);
+        }
+    }
 }
