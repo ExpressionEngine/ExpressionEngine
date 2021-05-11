4ed22f6cfa534cb55700725225cff4f5709c23d6
Yuri Salimovskiy <yuri@packettide.com>
2021-05-11 14:16:14 +0300

    update copyright date to 2021

--- system/ee/ExpressionEngine/Boot/boot.common.php
+++ system/ee/ExpressionEngine/Boot/boot.common.php
@@ -8,7 +8,7 @@
  * ExpressionEngine (https://expressionengine.com)
  *
  * @link      https://expressionengine.com/
- * @copyright Copyright (c) 2003-2020, Packet Tide, LLC (https://www.packettide.com)
+ * @copyright Copyright (c) 2003-2021, Packet Tide, LLC (https://www.packettide.com)
  * @license   https://expressionengine.com/license Licensed under Apache License, Version 2.0
  */
 
