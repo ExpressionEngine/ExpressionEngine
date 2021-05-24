7d58cb656b0c25f085bfcb0a88fe6cd1a2520e4c
Yuri Salimovskiy <yuri@packettide.com>
2021-05-24 16:52:49 +0300

    Template tags to display list of cookies
    EEPRO-80

--- system/ee/ExpressionEngine/Addons/consent/mod.consent.php
+++ system/ee/ExpressionEngine/Addons/consent/mod.consent.php
@@ -27,6 +27,35 @@ class Consent
         ee()->lang->loadfile('consent');
     }
 
+    public function cookies()
+    {
+        if (IS_PRO && ee('pro:Access')->hasValidLicense()) {
+            $allCookies = ee('Model')->get('CookieSetting')->all();
+            $type = null;
+            if (in_array(ee()->TMPL->fetch_param('type'), ['necessary', 'functionality', 'ferformance', 'targeting'])) {
+                $type = ee()->TMPL->fetch_param('type');
+                $fn = 'is' . $type;
+            }
+            $vars = [];
+            foreach ($allCookies as $cookie) {
+                if (empty($type) || ee('CookieRegistry')->{$fn}($cookie->cookie_name)) {
+                    $vars[] = [
+                        'cookie_title' => $cookie->cookie_title,
+                        'cookie_name' => $cookie_prefix . $cookie->cookie_name,
+                        'cookie_description' => $cookie->cookie_description,
+                        'cookie_lifetime' => ($cookie->cookie_enforced_lifetime !== null) ? $cookie->cookie_enforced_lifetime : $cookie->cookie_lifetime,
+                        'cookie_provider' => $cookie->cookie_provider
+                    ];
+                }
+            }
+            if (empty($vars)) {
+                return ee()->TMPL->no_results();
+            }
+            return ee()->TMPL->parse_variables(ee()->TMPL->tagdata, $vars);
+        }
+        return ee()->TMPL->no_results();
+    }
+
     /**
      * {exp:consent:form}
      *
