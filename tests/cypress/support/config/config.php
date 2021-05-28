0b5ae6e706ce6c108a9daac71824190ac8f8ffca
Geoff Cowan <geoff@orionesque.com>
2021-05-28 15:25:11 +0100

    Cypress config update

--- tests/cypress/support/config/config.php
+++ tests/cypress/support/config/config.php
@@ -13,21 +13,6 @@ $config['session_crypt_key'] = '1f307a8afe66e692c2689508a5d9f783606379a8';
 $config['base_path'] = $_SERVER['DOCUMENT_ROOT'];
 $config['base_url'] = 'http://localhost:8888/';
 $config['site_url'] = $config['base_url'];
-<<<<<<< HEAD
-$config['app_version'] = '6.0.4';
-$config['license_contact'] = 'ellislab.developers@gmail.com';
-$config['license_number'] = '1234-5678-9123-4567';
-$config['debug'] = '1';
-$config['cp_url'] = 'http://localhost:8888/system/index.php';
-$config['theme_folder_url'] = 'http://localhost:8888/themes/';
-$config['theme_folder_path'] = '/home/ubuntu/ExpressionEngine/themes/';
-$config['doc_url'] = 'https://ellislab.com/expressionengine/user-guide/';
-$config['is_system_on'] = 'y';
-$config['allow_extensions'] = 'y';
-$config['cookie_prefix'] = '';
-$config['cache_driver'] = 'file';
-=======
->>>>>>> 6.dev
 $config['database'] = array(
 	'expressionengine' => array(
 		'hostname' => '127.0.0.1',
