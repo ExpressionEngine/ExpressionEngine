caefe63ce6d6471e3e9eef2cb654002875173eb5
Yuri Salimovskiy <yuri@packettide.com>
2021-06-02 09:26:26 +0300

    add some waiting for tests

--- tests/cypress/cypress/integration/settings/urls.ee6.js
+++ tests/cypress/cypress/integration/settings/urls.ee6.js
@@ -63,9 +63,9 @@ context('URL and Path Settings', () => {
 
   it('should only show member trigger if enabled', () => {
     cy.eeConfig({item: 'legacy_member_templates', value: 'y'}).then(() => {
-      cy.wait(5000)
+      cy.wait(1000)
       cy.eeConfig({item: 'profile_trigger'}).then((config) => {
-        cy.wait(5000)
+        cy.wait(1000)
         expect(config).to.be.not.empty
         page.load()
         cy.hasNoErrors()
@@ -76,6 +76,7 @@ context('URL and Path Settings', () => {
     })
 
     cy.eeConfig({item: 'legacy_member_templates', value: 'n'}).then(() => {
+      cy.wait(3000)
       page.load()
       cy.hasNoErrors()
       page.get('profile_trigger').should('not.exist')
