eb16db4a04c2b5ecbb01ad971141e93553e2ba05
Jace Richardson <obfuscode@users.noreply.github.com>
2021-06-03 13:12:48 -0400

    Removed license validation data from console logging on error.

--- themes/ee/asset/javascript/src/cp/global_start.js
+++ themes/ee/asset/javascript/src/cp/global_start.js
@@ -322,14 +322,14 @@ EE.cp.validateLicense = function() {
 
 					},
 					error: function (data, textStatus, errorThrown) {
-						console.log('Error Data:', data, data.responseJSON.message, 'textStatus:', textStatus, 'errorThrown:', errorThrown);
+						console.log('Error Data:', data.responseJSON.message, 'textStatus:', textStatus, 'errorThrown:', errorThrown);
 					}
 				});
 			}
 		},
 
 		error: function(data, textStatus, errorThrown) {
-			console.log('Error Data:', data, data.responseJSON.message, 'textStatus:', textStatus, 'errorThrown:', errorThrown);
+			console.log('Error Data:', data.responseJSON.message, 'textStatus:', textStatus, 'errorThrown:', errorThrown);
 		}
 	});
 }
