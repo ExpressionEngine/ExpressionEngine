69acfe10f9844f1028a359d486806916d910debd
Yuri Salimovskiy <yuri@packettide.com>
2021-06-14 16:38:15 +0300

    only show custom logo/favicon when Pro is installed and licensed
    EEPRO-150

--- system/ee/ExpressionEngine/View/_templates/out.php
+++ system/ee/ExpressionEngine/View/_templates/out.php
@@ -4,7 +4,7 @@
 		<title><?=$cp_page_title?> | ExpressionEngine</title>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8">
 		<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"  name="viewport">
-		<?php if (ee()->config->item('favicon')) : ?>
+		<?php if (IS_PRO && ee('pro:Access')->hasValidLicense() && ee()->config->item('favicon')) : ?>
 		<link rel="icon" type="image/x-icon" href="<?=ee()->config->item('favicon')?>" />
 		<?php endif; ?>
 		<meta name="referrer" content="no-referrer">
