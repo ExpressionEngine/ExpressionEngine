6b7098a4c64f0cd91cc7ed4572ffd17ab46d9bec
Bradford Akin <bakin1637@gmail.com>
2021-06-18 10:22:09 -0400

    Changed get to use ee's way and added in disable config setting

--- system/ee/ExpressionEngine/Addons/forum/mod.forum_core.php
+++ system/ee/ExpressionEngine/Addons/forum/mod.forum_core.php
@@ -5931,7 +5931,8 @@ class Forum_Core extends Forum
         $viewpath = ($query->row('announcement') == 'n') ? 'viewthread/' : 'viewannounce/';
         $viewpath .= ($query->row('announcement') == 'n') ? $_GET['topic_id'] : $_GET['topic_id'] . '_' . $query->row('forum_id') ;
 
-        if (!array_key_exists('csrf_token',$_GET) ||  ! $this->_mod_permission('can_change_status', $query->row('forum_id')) || $_GET['csrf_token'] != CSRF_TOKEN) {
+        $token = ee('Request')->get('csrf_token');
+        if ( (! bool_config_item('disable_csrf_protection') &&  !array_key_exists('csrf_token',$_GET)  )  ||  ! $this->_mod_permission('can_change_status', $query->row('forum_id')) ||  (! bool_config_item('disable_csrf_protection') && $token != CSRF_TOKEN)) {
             return ee()->output->show_user_error('general', array(lang('not_authorized')));
         }
 
