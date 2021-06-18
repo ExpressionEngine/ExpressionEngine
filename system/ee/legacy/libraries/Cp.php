0d1157b3bdebe4973aa2d0491d08edd941b35878
Jace Richardson <obfuscode@users.noreply.github.com>
2021-06-18 11:20:42 -0400

    Misc phpcs fixes. Added check for pro action when making accessResponseURL

--- system/ee/legacy/libraries/Cp.php
+++ system/ee/legacy/libraries/Cp.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This source file is part of the open source project
  * ExpressionEngine (https://expressionengine.com)
@@ -18,7 +19,7 @@ class Cp
     protected $its_all_in_your_head = array();
     protected $footer_item = array();
 
-    public $cp_theme_url = '';	// base URL to the CP theme folder
+    public $cp_theme_url = ''; // base URL to the CP theme folder
     public $installed_modules = false;
     public $requests = array();
     public $loaded = array();
@@ -57,7 +58,7 @@ class Cp
     /**
      * Set Certain Default Control Panel View Variables
      *
-     * @return	void
+     * @return void
      */
     public function set_default_view_variables()
     {
@@ -114,7 +115,7 @@ class Cp
             'cp_quicklinks' => $this->_get_quicklinks($member->getQuicklinks()),
 
             'EE_view_disable' => false,
-            'is_super_admin' => (ee('Permission')->isSuperAdmin()) ? true : false,	// for conditional use in view files
+            'is_super_admin' => (ee('Permission')->isSuperAdmin()) ? true : false, // for conditional use in view files
         );
 
         // global table data
@@ -165,7 +166,7 @@ class Cp
             'THEME_URL' => $this->cp_theme_url,
             'hasRememberMe' => (bool) ee()->remember->exists(),
             'cp.updateCheckURL' => ee('CP/URL', 'settings/general/version-check')->compile(),
-            'cp.accessResponseURL' => ee()->functions->fetch_site_index() . QUERY_MARKER . 'ACT=' . $this->fetch_action_id('Pro', 'handleAccessResponse'),
+            'cp.accessResponseURL' => $this->fetch_action_id('Pro', 'handleAccessResponse') ? ee()->functions->fetch_site_index() . QUERY_MARKER . 'ACT=' . $this->fetch_action_id('Pro', 'handleAccessResponse') : false,
             'site_id' => ee()->config->item('site_id'),
             'site_name' => ee()->config->item('site_name'),
             'site_url' => ee()->config->item('site_url'),
@@ -173,7 +174,6 @@ class Cp
         ));
 
         if (ee()->session->flashdata('update:completed')) {
-
             $updateCompletedScript = "
                 $(document).ready(function() {
                     document.getElementsByClassName('js-about')[0].click();
@@ -278,8 +278,7 @@ class Cp
         ee()->load->helper('text');
         ee()->load->library('el_pings');
 
-        if (ee()->config->item('new_version_check') == 'y' &&
-            $new_version = ee()->el_pings->getUpgradeInfo()) {
+        if (ee()->config->item('new_version_check') == 'y' && $new_version = ee()->el_pings->getUpgradeInfo()) {
             ee()->view->new_version = $new_version;
             $version_major = explode('.', APP_VER, 2)[0];
             $update_version_major = explode('.', $new_version['version'], 2)[0];
@@ -412,7 +411,6 @@ class Cp
                 }
             }
         }
-        
 
         if (! empty($notices)) {
             if (! $alert) {
@@ -490,7 +488,7 @@ class Cp
      * Requests a file from ExpressionEngine.com that informs us what the current available version
      * of ExpressionEngine.
      *
-     * @return	bool|string
+     * @return bool|string
      */
     protected function _version_check()
     {
@@ -515,8 +513,8 @@ class Cp
      * To be used to create url's that "mask" the real location of the
      * users control panel.  Eg:  http://example.com/index.php?URL=http://example2.com
      *
-     * @param string	URL
-     * @return string	Masked URL
+     * @param string   URL
+     * @return string  Masked URL
      */
     public function masked_url($url)
     {
@@ -587,8 +585,8 @@ class Cp
      * Seal the current combo loader and reopen a new one.
      *
      * @param bool Whether to include 'common.js' automatically
-     * @access	private
-     * @return	array
+     * @access private
+     * @return array
      */
     public function _seal_combo_loader($include_common = true)
     {
@@ -638,9 +636,9 @@ class Cp
      * Get last modification time of a js file.
      * Returns highest if passed an array.
      *
-     * @param	string
-     * @param	mixed
-     * @return	int
+     * @param string
+     * @param mixed
+     * @return int
      */
     public function _get_js_mtime($type, $name)
     {
@@ -655,18 +653,22 @@ class Cp
         }
 
         switch ($type) {
-            case 'ui':			$file = PATH_THEMES_GLOBAL_ASSET . 'javascript/' . PATH_JS . '/jquery/ui/jquery.ui.' . $name . '.js';
-
+            case 'ui':
+                $file = PATH_THEMES_GLOBAL_ASSET . 'javascript/' . PATH_JS . '/jquery/ui/jquery.ui.' . $name . '.js';
                 break;
-            case 'plugin':		$file = PATH_THEMES_GLOBAL_ASSET . 'javascript/' . PATH_JS . '/jquery/plugins/' . $name . '.js';
 
+            case 'plugin':
+                $file = PATH_THEMES_GLOBAL_ASSET . 'javascript/' . PATH_JS . '/jquery/plugins/' . $name . '.js';
                 break;
-            case 'file':		$file = PATH_THEMES_GLOBAL_ASSET . 'javascript/' . PATH_JS . '/' . $name . '.js';
 
+            case 'file':
+                $file = PATH_THEMES_GLOBAL_ASSET . 'javascript/' . PATH_JS . '/' . $name . '.js';
                 break;
-            case 'pro_file':		$file = PATH_PRO_THEMES . 'js/' . $name . '.js';
 
+            case 'pro_file':
+                $file = PATH_PRO_THEMES . 'js/' . $name . '.js';
                 break;
+
             case 'package':
                 if (strpos($name, ':') !== false) {
                     list($package, $name) = explode(':', $name);
@@ -675,11 +677,12 @@ class Cp
                 }
 
                 $file = PATH_THIRD . $package . '/javascript/' . $name . '.js';
-
                 break;
-            case 'fp_module':	$file = PATH_ADDONS . $name . '/javascript/' . $name . '.js';
 
+            case 'fp_module':
+                $file = PATH_ADDONS . $name . '/javascript/' . $name . '.js';
                 break;
+
             default:
                 return 0;
         }
@@ -690,9 +693,9 @@ class Cp
     /**
      * Set the right navigation
      *
-     * @param	array
-     * @param	string
-     * @return	int
+     * @param array
+     * @param string
+     * @return int
      */
     public function set_right_nav($nav = array())
     {
@@ -703,7 +706,7 @@ class Cp
      * URL to the current page unless POST data exists - in which case it
      * goes to the root controller.  To use the result, prefix it with BASE.AMP
      *
-     * @return	string
+     * @return string
      */
     public function get_safe_refresh()
     {
@@ -760,7 +763,7 @@ class Cp
      * the state of a view if you need to go away from it, POST requests for
      * example.
      *
-     * @return	array	GET array filtered of proprietary CP keys
+     * @return array GET array filtered of proprietary CP keys
      */
     public function get_url_state()
     {
@@ -772,11 +775,11 @@ class Cp
     }
 
     /**
-     * 	Get Quicklinks
+     * Get Quicklinks
      *
-     * 	Does a lookup for quick links.  Based on the URL we determine if it is external or not
+     * Does a lookup for quick links.  Based on the URL we determine if it is external or not
      *
-     * 	@return array
+     * @return array
      */
     private function _get_quicklinks($quick_links)
     {
@@ -802,7 +805,7 @@ class Cp
     /**
      * Abstracted Way to Add a Breadcrumb Links
      *
-     * @return	void
+     * @return void
      */
     public function set_breadcrumb($link, $title)
     {
@@ -821,8 +824,8 @@ class Cp
      *
      * Load a javascript file from a package
      *
-     * @param	string
-     * @return	void
+     * @param  string
+     * @return void
      */
     public function load_package_js($file)
     {
@@ -837,8 +840,8 @@ class Cp
      *
      * Load a stylesheet from a package
      *
-     * @param	string
-     * @return	void
+     * @param  string
+     * @return void
      */
     public function load_package_css($file)
     {
@@ -859,8 +862,8 @@ class Cp
      *
      * Add any string to the <head> tag
      *
-     * @param	string
-     * @return	string
+     * @param  string
+     * @return string
      */
     public function add_to_head($data)
     {
@@ -889,8 +892,8 @@ class Cp
      *
      * Add any string above the </body> tag
      *
-     * @param	string
-     * @return	string
+     * @param  string
+     * @return string
      */
     public function add_to_foot($data)
     {
@@ -913,8 +916,8 @@ class Cp
      * Member access validation
      *
      * @deprecated 5.0.0 Use ee('Permission')->hasAny() instead
-     * @param	string  any number of permission names
-     * @return	bool    TRUE if member has any permissions in the set
+     * @param  string  any number of permission names
+     * @return bool    TRUE if member has any permissions in the set
      */
     public function allowed_group_any()
     {
@@ -932,8 +935,8 @@ class Cp
      * Member access validation
      *
      * @deprecated 5.0.0 Use ee('Permission')->hasAll() instead
-     * @param	string  any number of permission names
-     * @return	bool    TRUE if member has all permissions
+     * @param  string  any number of permission names
+     * @return bool    TRUE if member has all permissions
      */
     public function allowed_group()
     {
@@ -976,7 +979,7 @@ class Cp
      *
      * Tracks "reserved" words to avoid variable name collision
      *
-     * @return	array
+     * @return array
      */
     public function invalid_custom_field_names()
     {
@@ -1042,11 +1045,11 @@ class Cp
     }
 
     /**
-     * 	Fetch Action IDs
+     * Fetch Action IDs
      *
-     *	@param string
-     * 	@param string
-     *	@return mixed
+     *  @param string
+     *  @param string
+     *  @return mixed
      */
     public function fetch_action_id($class, $method)
     {
@@ -1065,10 +1068,9 @@ class Cp
     /**
      * Site Switching Logic
      *
-     * @param	int		$site_id	ID of site to switch to
-     * @param	string	$redirect	Optional URL to redirect to after site
-     * 								switching is successful
-     * @return	void
+     * @param  int     $site_id    ID of site to switch to
+     * @param  string  $redirect   Optional URL to redirect to after site switching is successful
+     * @return void
      */
     public function switch_site($site_id, $redirect = '')
     {
