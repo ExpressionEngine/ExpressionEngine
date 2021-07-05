3929787d2d41dc1076137f2bfeb30c5bf8be8ddc
Yuri Salimovskiy <yuri@packettide.com>
2021-07-05 08:28:53 +0300

    ensure Pro tags stripped from templates even if Pro is uninstalled

--- system/ee/legacy/core/Output.php
+++ system/ee/legacy/core/Output.php
@@ -337,6 +337,12 @@ class EE_Output
         if (IS_PRO) {
             $output = ee('pro:Dock')->buildOutput($output);
         }
+        if (REQ == 'PAGE' || (REQ == 'ACTION' && ee('LivePreview')->hasEntryData())) {
+            if (isset(ee()->TMPL) && is_object(ee()->TMPL) && in_array(ee()->TMPL->template_type, ['webpage'])) {
+                $output = preg_replace("/\{frontedit_link\s+(.*)\}/sU", '', $output);
+                $output = preg_replace("/\<\!--\s*(\/\/\s*)*disable\s*frontedit\s*--\>/sU", '', $output);
+            }
+        }
         // --------------------------------------------------------------------
 
         // Do we need to generate profile data?
