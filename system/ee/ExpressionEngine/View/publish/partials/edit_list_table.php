c4692fd364a12acc2cd0e386c1e8ff9216ea06ad
Andy Johnson <andy@shapingthepage.com>
2021-07-13 17:09:45 -0400

    Persist filter search bar styles through ajax load

--- system/ee/ExpressionEngine/View/publish/partials/edit_list_table.php
+++ system/ee/ExpressionEngine/View/publish/partials/edit_list_table.php
@@ -11,7 +11,7 @@
           <?php endif; ?>
         </div>
 
-        <div class="filter-search-bar" <?php if (ee()->input->get('hide_closer') === 'y') : ?> style="padding: 10px 10px;"<?php endif; ?>>
+        <div class="filter-search-bar" <?php if (ee()->uri->segment(3) == 'prolet') : ?> style="padding: 10px 10px;"<?php endif; ?>>
 
             <!-- All filters (not including search input) are contained within 'filter-search-bar__filter-row' -->
             <div class="filter-search-bar__filter-row">
