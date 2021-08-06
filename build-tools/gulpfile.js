81498776153bc65cb164c3403d2969c2af38273f
Yuri Salimovskiy <yuri@packettide.com>
2021-08-06 10:47:15 +0300

    do not uglify js as that is causing error

--- build-tools/gulpfile.js
+++ build-tools/gulpfile.js
@@ -687,7 +687,7 @@ var compressJs = function (path) {
 	console.log('Compress Path:', path + 'themes/ee/asset/javascript/src/**/*.js');
 	// process.exit();
 	return gulp.src([path + 'themes/ee/asset/javascript/src/**/*.js', '!' + path + 'themes/ee/asset/javascript/src/fields/rte/redactor/plugins/', '!' + path + 'themes/ee/asset/javascript/src/fields/rte/redactor/redactor.css', '!' + path + 'themes/ee/asset/javascript/src/fields/rte/redactor/redactor.js'])
-		.pipe(plugin.uglify({preserveComments: 'some'}))
+		//.pipe(plugin.uglify({preserveComments: 'some'}))
 		.pipe(gulp.dest(path + 'themes/ee/asset/javascript/compressed/'));
 };
 
