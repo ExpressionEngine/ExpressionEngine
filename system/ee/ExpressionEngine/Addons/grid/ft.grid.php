25019a030a7ce8f78c0e2d72931be203175a3e5c
Yuri Salimovskiy <yuri@packettide.com>
2021-08-18 07:55:52 +0300

    declare Fluid and Grid as complex data structures

--- system/ee/ExpressionEngine/Addons/grid/ft.grid.php
+++ system/ee/ExpressionEngine/Addons/grid/ft.grid.php
@@ -20,6 +20,8 @@ class Grid_ft extends EE_Fieldtype
 
     public $has_array_data = true;
 
+    public $complex_data_structure = true;
+
     public $size = 'large';
 
     public $settings_form_field_name = 'grid';
