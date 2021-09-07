050bd0f35a36d32b32f2c02facdbf3235e3e5ec7
Yuri Salimovskiy <yuri@packettide.com>
2021-09-07 11:38:14 +0300

    fix the bug when CP URL was wrong sometimes after showing idle modal
    EECORE-1006

--- themes/ee/asset/javascript/src/cp/global_start.js
+++ themes/ee/asset/javascript/src/cp/global_start.js
@@ -448,7 +448,7 @@ EE.cp.setBasePath = function(newBase, skipBroadcast /* internal */) {
 	// url to avoid that issue. You still cannot use the back button after
 	// logging back in, but how likely are you to remember what page you
 	// were on before leaving this one open for 20 minutes anyways?
-	if (typeof window.history.pushState == 'function') {
+	if (typeof window.history.pushState == 'function' && oldBaseS[1] != '') {
 		window.history.replaceState(
 			null,
 			document.title,
