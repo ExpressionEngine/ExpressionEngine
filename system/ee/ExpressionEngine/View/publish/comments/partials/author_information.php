d0d0904da73ece58a9559bad22146c2219f066b7
Andy Johnson <andy@shapingthepage.com>
2021-09-14 11:08:00 -0400

    Comments author info styling

--- system/ee/ExpressionEngine/View/publish/comments/partials/author_information.php
+++ system/ee/ExpressionEngine/View/publish/comments/partials/author_information.php
@@ -1,5 +1,5 @@
-<div class="d-flex align-items-center">
-	<?php if ($comment->author_id && !is_null($comment->Author)): 
+<div class="author-info-comments">
+	<?php if ($comment->author_id && !is_null($comment->Author)):
 		if (!ee('Permission')->isSuperAdmin()) {
 			$can_operate_member = (bool) (ee('Permission')->can('edit_members') && $member->PrimaryRole->is_locked != 'y');
 		} else {
@@ -8,9 +8,9 @@
 		$avatar_url = ($comment->Author->avatar_filename) ? ee()->config->slash_item('avatar_url') . $comment->Author->avatar_filename : (URL_THEMES . 'asset/img/default-avatar.png');
 	?>
 
-	<img src="<?=$avatar_url?>" alt="<?=$comment->Author->screen_name?>" class="avatar-icon add-mrg-right">
+	<img src="<?=$avatar_url?>" alt="<?=$comment->Author->screen_name?>" class="avatar-icon">
 	<?php endif; ?>
-	<div>
+	<div class="author-details">
 		<?php if ($can_operate_member): ?>
 			<a href="<?=ee('CP/URL')->make('members/profile', ['id' => $comment->author_id])?>"><?=$comment->name?></a><br>
 		<?php else: ?>
