ExpressionEngine/ExpressionEngine

commit fea88fe2acb0bb1aa54d525189b82fa41bdb0eda

eager load roles before using getAllRoles() EECORE-1021

system/ee/ExpressionEngine/Addons/forum/mod.forum_core.php
@@ -675,7 +675,7 @@ class Forum_Core extends Forum
                 return ee('Permission')->isSuperAdmin();
             }
 
-            $member = ee('Model')->get('Member', $member_id)->first();
+            $member = ee('Model')->get('Member', $member_id)->with('PrimaryRole', 'Roles', 'RoleGroups')->first();
 
             if (! $member) {
                 return false;
@@ -7036,7 +7036,7 @@ class Forum_Core extends Forum
         }
 
         // Fetch the member info
-        $member = ee('Model')->get('Member', $this->current_id)->first();
+        $member = ee('Model')->get('Member', $this->current_id)->with('PrimaryRole', 'Roles', 'RoleGroups')->first();
 
         if (! $member) {
             return $this->trigger_error();
@@ -7100,7 +7100,7 @@ class Forum_Core extends Forum
         }
 
         // Fetch the member info
-        $member = ee('Model')->get('Member', $this->current_id)->first();
+        $member = ee('Model')->get('Member', $this->current_id)->with('PrimaryRole', 'Roles', 'RoleGroups')->first();
 
         if (! $member) {
             return $this->trigger_error();
@@ -9509,6 +9509,7 @@ class Forum_Core extends Forum
         /**  Validate Username and Password
         /** ----------------------------------*/
         $member = ee('Model')->get('Member')
+            ->with('PrimaryRole', 'Roles', 'RoleGroups')
             ->filter('username', $username)
             ->first();
 

system/ee/ExpressionEngine/Addons/member/mod.member.php
@@ -2562,6 +2562,7 @@ class Member
 
         $member = ee('Model')
             ->get('Member', $member_id)
+            ->with('PrimaryRole', 'Roles', 'RoleGroups')
             ->first();
 
         if (!$member) {
@@ -2598,6 +2599,7 @@ class Member
 
         $member = ee('Model')
             ->get('Member', $member_id)
+            ->with('PrimaryRole', 'Roles', 'RoleGroups')
             ->first();
 
         if (!$member) {

system/ee/ExpressionEngine/Controller/Members/Profile/Profile.php
@@ -51,7 +51,7 @@ class Profile extends CP_Controller
         $this->query_string = $qs;
         $this->base_url = ee('CP/URL')->make('members/profile/settings');
         $this->base_url->setQueryStringVariable('id', $id);
-        $this->member = ee('Model')->get('Member', $id)->first();
+        $this->member = ee('Model')->get('Member', $id)->with('PrimaryRole', 'Roles', 'RoleGroups')->first();
 
         if (is_null($this->member)) {
             show_404();

system/ee/legacy/libraries/Auth.php
@@ -125,7 +125,7 @@ class Auth
         $authed = $this->_retrieve_http_basic();
 
         if ($authed !== false) {
-            $member = ee('Model')->get('Member', $authed->member('member_id'))->first();
+            $member = ee('Model')->get('Member', $authed->member('member_id'))->with('PrimaryRole', 'Roles', 'RoleGroups')->first();
 
             $authed = false;
 
@@ -619,7 +619,7 @@ class Auth_result
     public function has_permission($perm)
     {
         if (empty($this->permissions)) {
-            $member = ee('Model')->get('Member', $this->member('member_id'))->first();
+            $member = ee('Model')->get('Member', $this->member('member_id'))->with('PrimaryRole', 'Roles', 'RoleGroups')->first();
 
             if ($member->role_id == 1) {
                 return true;

system/ee/legacy/libraries/Session.php
@@ -352,7 +352,7 @@ class EE_Session
     public function create_new_session($member_id, $admin_session = false, $can_debug = false)
     {
         if (! is_object($this->member_model) || $this->member_model->member_id != $member_id) {
-            $this->member_model = ee('Model')->get('Member', $member_id)->first();
+            $this->member_model = ee('Model')->get('Member', $member_id)->with('PrimaryRole', 'Roles', 'RoleGroups')->first();
         }
 
         if ($this->access_cp == true or $this->member_model->can('access_cp')) {
@@ -1181,7 +1181,7 @@ class EE_Session
 
         if (! is_object($this->member_model) || $member_model->member_id != $member_id) {
             $this->member_model = ee('Model')->get('Member', $member_id)
-                ->with('PrimaryRole')
+                ->with('PrimaryRole', 'Roles', 'RoleGroups')
                 ->first();
         }
 

system/ee/ExpressionEngine/Addons/relationship/libraries/Relationships_ft_cp.php
@@ -169,7 +169,7 @@ class Relationships_ft_cp
 
         // Then all authors who are in those groups or who have author access
         $members = ee('Model')->get('Member')
-            ->fields('member_id', 'screen_name', 'username')
+            ->with('PrimaryRole', 'Roles', 'RoleGroups')
             ->filter('in_authorlist', 'y')
             ->order('screen_name', 'asc')
             ->order('username', 'asc')

system/ee/ExpressionEngine/Controller/Homepage/Homepage.php
@@ -19,6 +19,10 @@ class Homepage extends CP_Controller
 {
     public function index()
     {
+        $mbr = ee('Model')->get('Member', 1)->with('PrimaryRole', 'Roles', 'RoleGroups')->fields('member_id')->all()->first();
+        var_dump($mbr->PrimaryRole->toArray());
+        exit();
+        
         $this->redirectIfNoSegments();
 
         ee('CP/Alert')->makeDeprecationNotice()->now();

system/ee/ExpressionEngine/Controller/Members/Members.php
@@ -1303,7 +1303,7 @@ class Members extends CP_Controller
         $role_ids = $role_ids->indexBy('role_id');
 
         $members = ee('Model')->get('Member', $member_ids)
-            ->fields('member_id', 'screen_name', 'email')
+            ->with('PrimaryRole', 'Roles', 'RoleGroups')
             ->all();
 
         foreach ($members as $member) {

commit cb9bfcb03e28acdb2270689ba6986f2ae88ae31d

fix the issue with member roles loading
Since we added eager loading to associated Roles for member,
we need to change the queries to fetch all records and then
use `first()` to get the first model from collection.
The issue was initially introduced with [fea88fe]

fixes #1439

EECORE-1456

system/ee/ExpressionEngine/Addons/forum/mod.forum_core.php
@@ -676,7 +676,7 @@ class Forum_Core extends Forum
                 return ee('Permission')->isSuperAdmin();
             }
 
-            $member = ee('Model')->get('Member', $member_id)->with('PrimaryRole', 'Roles', 'RoleGroups')->first();
+            $member = ee('Model')->get('Member', $member_id)->with('PrimaryRole', 'Roles', 'RoleGroups')->all()->first();
 
             if (! $member) {
                 return false;
@@ -7037,7 +7037,7 @@ class Forum_Core extends Forum
         }
 
         // Fetch the member info
-        $member = ee('Model')->get('Member', $this->current_id)->with('PrimaryRole', 'Roles', 'RoleGroups')->first();
+        $member = ee('Model')->get('Member', $this->current_id)->with('PrimaryRole', 'Roles', 'RoleGroups')->all()->first();
 
         if (! $member) {
             return $this->trigger_error();
@@ -7101,7 +7101,7 @@ class Forum_Core extends Forum
         }
 
         // Fetch the member info
-        $member = ee('Model')->get('Member', $this->current_id)->with('PrimaryRole', 'Roles', 'RoleGroups')->first();
+        $member = ee('Model')->get('Member', $this->current_id)->with('PrimaryRole', 'Roles', 'RoleGroups')->all()->first();
 
         if (! $member) {
             return $this->trigger_error();
@@ -9512,6 +9512,7 @@ class Forum_Core extends Forum
         $member = ee('Model')->get('Member')
             ->with('PrimaryRole', 'Roles', 'RoleGroups')
             ->filter('username', $username)
+            ->all()
             ->first();
 
         if (! $member) {

system/ee/ExpressionEngine/Addons/member/mod.member.php
@@ -2598,6 +2598,7 @@ class Member
         $member = ee('Model')
             ->get('Member', $member_id)
             ->with('PrimaryRole', 'Roles', 'RoleGroups')
+            ->all()
             ->first();
 
         if (!$member) {

system/ee/ExpressionEngine/Controller/Members/Profile/Profile.php
@@ -51,7 +51,7 @@ class Profile extends CP_Controller
         $this->query_string = $qs;
         $this->base_url = ee('CP/URL')->make('members/profile/settings');
         $this->base_url->setQueryStringVariable('id', $id);
-        $this->member = ee('Model')->get('Member', $id)->with('PrimaryRole', 'Roles', 'RoleGroups')->first();
+        $this->member = ee('Model')->get('Member', $id)->with('PrimaryRole', 'Roles', 'RoleGroups')->all()->first();
 
         if (is_null($this->member)) {
             show_404();

system/ee/legacy/libraries/Auth.php
@@ -125,7 +125,7 @@ class Auth
         $authed = $this->_retrieve_http_basic();
 
         if ($authed !== false) {
-            $member = ee('Model')->get('Member', $authed->member('member_id'))->with('PrimaryRole', 'Roles', 'RoleGroups')->first();
+            $member = ee('Model')->get('Member', $authed->member('member_id'))->with('PrimaryRole', 'Roles', 'RoleGroups')->all()->first();
 
             $authed = false;
 
@@ -619,7 +619,7 @@ class Auth_result
     public function has_permission($perm)
     {
         if (empty($this->permissions)) {
-            $member = ee('Model')->get('Member', $this->member('member_id'))->with('PrimaryRole', 'Roles', 'RoleGroups')->first();
+            $member = ee('Model')->get('Member', $this->member('member_id'))->with('PrimaryRole', 'Roles', 'RoleGroups')->all()->first();
 
             if ($member->role_id == 1) {
                 return true;

system/ee/legacy/libraries/Session.php
@@ -353,7 +353,7 @@ class EE_Session
     public function create_new_session($member_id, $admin_session = false, $can_debug = false)
     {
         if (! is_object($this->member_model) || $this->member_model->member_id != $member_id) {
-            $this->member_model = ee('Model')->get('Member', $member_id)->with('PrimaryRole', 'Roles', 'RoleGroups')->first();
+            $this->member_model = ee('Model')->get('Member', $member_id)->with('PrimaryRole', 'Roles', 'RoleGroups')->all()->first();
         }
 
         if ($this->access_cp == true or $this->member_model->can('access_cp')) {
@@ -1183,6 +1183,7 @@ class EE_Session
         if (! is_object($this->member_model) || $member_model->member_id != $member_id) {
             $this->member_model = ee('Model')->get('Member', $member_id)
                 ->with('PrimaryRole', 'Roles', 'RoleGroups')
+                ->all()
                 ->first();
         }
 