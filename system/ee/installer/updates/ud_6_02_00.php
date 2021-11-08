9f630fd4175403a18a7caa6192d67e035d4afe95
Matt Johnson <matt@packettide.com>
2021-11-08 14:52:21 -0500

    Enable CLI in the settings by default
    EECORE-1388

---
+++ system/ee/installer/updates/ud_6_02_00.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * This source file is part of the open source project
+ * ExpressionEngine (https://expressionengine.com)
+ *
+ * @link      https://expressionengine.com/
+ * @copyright Copyright (c) 2003-2021, Packet Tide, LLC (https://www.packettide.com)
+ * @license   https://expressionengine.com/license Licensed under Apache License, Version 2.0
+ */
+
+namespace ExpressionEngine\Updater\Version_6_2_0;
+
+/**
+ * Update
+ */
+class Updater
+{
+    public $version_suffix = '';
+
+    /**
+     * Do Update
+     *
+     * @return TRUE
+     */
+    public function do_update()
+    {
+        // Enable the CLI by default
+        ee()->config->update_site_prefs(['enable_cli' => 'y'], 'all');
+
+        return true;
+    }
+}
+
+// EOF
