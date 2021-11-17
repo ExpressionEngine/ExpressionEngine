cb99358d05ff5533d054a7a65ec97539ba7df96b
Yulya Lebed <yulya@packettide.com>
2021-11-17 17:02:35 +0200

    recreate script for Members and added to the reset password page.

--- system/ee/ExpressionEngine/View/account/reset_password.php
+++ system/ee/ExpressionEngine/View/account/reset_password.php
@@ -12,13 +12,17 @@
 			<div class="field-instruct">
 			<?=lang('new_password', 'password')?>
 			</div>
-			<?=form_password(array('dir' => 'ltr', 'name' => "password", 'id' => "password", 'maxlength' => PASSWORD_MAX_LENGTH, 'autocomplete' => 'off'))?>
+			<div class="field-control">
+				<?=form_password(array('dir' => 'ltr', 'name' => "password", 'id' => "password", 'maxlength' => PASSWORD_MAX_LENGTH, 'autocomplete' => 'off'))?>
+			</div>
 		</fieldset>
 		<fieldset>
 			<div class="field-instruct">
 			<?=lang('new_password_confirm', 'password_confirm')?>
 			</div>
-			<?=form_password(array('dir' => 'ltr', 'name' => "password_confirm", 'id' => "password_confirm", 'maxlength' => PASSWORD_MAX_LENGTH, 'autocomplete' => 'off'))?>
+			<div class="field-control">
+				<?=form_password(array('dir' => 'ltr', 'name' => "password_confirm", 'id' => "password_confirm", 'maxlength' => PASSWORD_MAX_LENGTH, 'autocomplete' => 'off'))?>
+			</div>
 		</fieldset>
 		<fieldset class="last text-center">
 			<?=form_hidden('resetcode', $resetcode)?>
@@ -27,10 +31,72 @@
 	<?=form_close()?>
 </div>
 
+<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 <script type="text/javascript">
 	var EE = {
 		cp: {
 			validatePasswordUrl: "<?=ee('CP/URL', 'login/validate_password')->compile()?>"
 		}
 	}
+
+
+	// Check password strength indicator
+	function passwordStrengthIndicator(field) {
+
+		var form = field.parents('form'),
+				action = form.attr('action'),
+				data = form.serialize();
+
+			if ( (typeof(EE.cp.validatePasswordUrl) != 'undefined') && (field.attr('name') == 'password') ) {
+				$(field).parent('.field-control').css('position', 'relative');
+
+				$.ajax({
+					type: 'POST',
+					url: EE.cp.validatePasswordUrl,
+					dataType: 'json',
+					data: data+'&ee_fv_field='+field.attr('name'),
+					success: function (result) {
+						console.log('result', result);
+						if (result['rank'] == 0) {
+							$('.rank-wrap').remove();
+							return;
+						} else {
+							var rank_text = result['rank_text'].toLowerCase();
+							var rank = result['rank'];
+							var classList = 'status-tag '+rank_text;
+							if (!$('.rank-wrap').length) {
+								$(field).after('<div class="rank-wrap"><p class="'+classList+'"><span class="rank_text">'+rank_text+'</span></p></div>');
+							} else {
+								$('.rank-wrap > p').attr('class', classList);
+								$('.rank-wrap .rank_text').text(rank_text);
+							}
+						}
+					},
+					error: function(err) {
+						console.log('err', err);
+					}
+				})
+			}
+	}
+
+	var passwordTimeout = null
+
+	// Typing into the password field
+	$('body').on('keyup', 'input[name="password"]', function() {
+
+		var field = $(this);
+		var val = $(this).val();
+		clearTimeout(passwordTimeout)
+		passwordTimeout = setTimeout(function() {
+			if(val == 0) {
+				if ($('.rank-wrap').length) {
+					$('.rank-wrap').remove();
+				}
+			} else {
+				passwordStrengthIndicator(field);
+			}
+
+			passwordTimeout = null
+		}, 1000)
+	});
 </script>
