8884e796bf0e4f463b32e054e4d2f7d121e61b68
andy <andy@packettide.com>
2021-12-10 08:25:37 -0500

    update redirect page to no attract bots and match 6.x changes

--- system/ee/EllisLab/ExpressionEngine/View/_templates/out.php
+++ system/ee/EllisLab/ExpressionEngine/View/_templates/out.php
@@ -5,6 +5,7 @@
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" lang="en-us" dir="ltr">
 		<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"  name="viewport">
 		<meta name="referrer" content="no-referrer">
+		<meta name="robots" content="noindex, nofollow">
 		<?=ee()->view->head_link('css/out.min.css')?>
 	</head>
 	<body>
@@ -16,7 +17,7 @@
 				<?php if ( ! isset($branded) OR $branded !== FALSE):?>
 					<section class="bar">
 						<p class="left"><a href="https://expressionengine.com/" rel="external"><b>ExpressionEngine</b></a></p>
-						<p class="right">&copy;<?=date('Y')?> <a href="https://ellislab.com/" rel="external">EllisLab</a> Corp.</p>
+						<p style="float: right;">&copy;<?=date('Y')?> <a href="https://packettide.com/" rel="external">Packet Tide</a>, LLC</p>
 					</section>
 				<?php endif; ?>
 			</section>
