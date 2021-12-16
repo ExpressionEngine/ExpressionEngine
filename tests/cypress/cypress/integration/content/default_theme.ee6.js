3299e5161da514c158e480aac1b8437305184d17
Yuri Salimovskiy <yuri@packettide.com>
2021-12-16 10:39:51 +0200

    some more PHP 8.1 updates

--- tests/cypress/cypress/integration/content/default_theme.ee6.js
+++ tests/cypress/cypress/integration/content/default_theme.ee6.js
@@ -11,61 +11,63 @@ const install_success = new Success
 
 context('Install with default theme', () => {
   before(function() {
-
+    cy.task('installer:enable')
   })
 
   beforeEach(function(){
     // Delete existing config and create a new one
     cy.task('db:clear')
     cy.task('cache:clear')
-    cy.task('installer:enable').then(() => {
 
-      let installer_folder = '../../system/ee/installer';
-      cy.task('filesystem:list', {target: '../../system/ee/'}).then((files) => {
-        for (const item in files) {
-          cy.log(files[item]);
-          if (files[item].indexOf('system/ee/installer_') >= 0) {
-            installer_folder = files[item];
-            cy.log(installer_folder);
-            cy.task('filesystem:delete', '../../system/ee/installer').then(()=>{
-              cy.task('filesystem:rename', {from: installer_folder, to: '../../system/ee/installer'})
-            })
-          }
-        }
-        cy.task('installer:create_config').then((path)=>{
-          cy.log(path)
-          //cy.screenshot({capture: 'runner'})
-        })
-
-        cy.task('filesystem:delete', '../../system/user/cache/mailing_list.zip')
-
-        page.load()
+    cy.task('installer:create_config').then((path)=>{
+        cy.log(path)
         //cy.screenshot({capture: 'runner'})
-        //cy.screenshot({capture: 'fullPage'})
-        cy.hasNoErrors()
+    })
 
-        install_form.get('db_hostname').clear().type('localhost')
-        install_form.get('db_name').clear().type(Cypress.env("DB_DATABASE"))
-        install_form.get('db_username').clear().type(Cypress.env("DB_USER"))
-        install_form.get('db_password').clear()
-        if (Cypress.env("DB_PASSWORD") != '') {
-            install_form.get('db_password').type(Cypress.env("DB_PASSWORD"))
+    page.load()
+    //cy.screenshot({capture: 'runner'})
+    //cy.screenshot({capture: 'fullPage'})
+    cy.hasNoErrors()
+
+    install_form.get('db_hostname').clear().type('ddev-ee6-php81-db')
+    install_form.get('db_name').clear().type(Cypress.env("DB_DATABASE"))
+    install_form.get('db_username').clear().type(Cypress.env("DB_USER"))
+    install_form.get('db_password').clear()
+    if (Cypress.env("DB_PASSWORD") != '') {
+        install_form.get('db_password').type(Cypress.env("DB_PASSWORD"))
+    }
+    install_form.get('install_default_theme').check()
+    install_form.get('username').clear().type('admin')
+    install_form.get('email_address').clear().type('hello@expressionengine.com')
+    install_form.get('password').clear().type('password')
+    install_form.get('license_agreement').click()
+    install_form.get('install_submit').click()
+
+    cy.hasNoErrors()
+
+  })
+
+  afterEach(function(){
+    let installer_folder = '../../system/ee/installer';
+    cy.task('filesystem:list', {target: '../../system/ee/'}).then((files) => {
+      for (const item in files) {
+        if (files[item].indexOf('system/ee/installer_') >= 0) {
+          installer_folder = files[item];
+          cy.task('filesystem:delete', '../../system/ee/installer').then(()=>{
+            cy.task('filesystem:rename', {from: installer_folder, to: '../../system/ee/installer'})
+          })
         }
-        install_form.get('install_default_theme').check()
-        install_form.get('username').clear().type('admin')
-        install_form.get('email_address').clear().type('hello@expressionengine.com')
-        install_form.get('password').clear().type('password')
-        install_form.get('license_agreement').click()
-        install_form.get('install_submit').click()
-
-        cy.hasNoErrors()
-      })
+      }
     })
   })
 
   after(function(){
     cy.task('installer:disable')
-    cy.task('installer:revert_config')
+    cy.task('installer:revert_config').then(()=>{
+        cy.task('installer:replace_config', {
+          file: 'support/config/config.php'
+        })
+    })
   })
 
   it('pages in default theme have no errors', () => {
@@ -85,7 +87,7 @@ context('Install with default theme', () => {
     cy.hasNoErrors()
     cy.visit('/index.php/blog/entry/the-one-with-rope-cutting')
     cy.hasNoErrors()
-    cy.visit('/index.php/member/1')
+    cy.visit('/index.php/member/1', {failOnStatusCode: false})
     cy.hasNoErrors()
     cy.visit('/index.php/blog')
     cy.hasNoErrors()
