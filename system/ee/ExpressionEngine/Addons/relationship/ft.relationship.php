a85e8812c902ea9e0c6a79fe26066f211ce6e693
Yuri Salimovskiy <yuri@packettide.com>
2021-12-20 17:58:30 +0200

    fix the validation code

--- system/ee/ExpressionEngine/Addons/relationship/ft.relationship.php
+++ system/ee/ExpressionEngine/Addons/relationship/ft.relationship.php
@@ -80,9 +80,12 @@ class Relationship_ft extends EE_Fieldtype implements ColumnInterface
         $validator = ee('Validation')->make($rules);
 
         $validator->defineRule('gtLimit', function ($key, $value, $parameters, $rule) use ($data) {
-            if ($data['allow_multiple'] && $data['limit'] < $data['rel_min']) {
-                $rule->stop();
-                return lang('rel_ft_min_settings_error');
+            if (
+                ((isset($data['allow_multiple']) && $data['allow_multiple'] == true) || (isset($data['relationship_allow_multiple']) && $data['relationship_allow_multiple'] == 'y'))
+                && $data['limit'] < $data['rel_min']
+            ) {
+                    $rule->stop();
+                    return lang('rel_ft_min_settings_error');
             }
             return true;
         });
