de6aa55c5d14403a5327b8fb84e74b30bdcd4103
Gilbert <gilbert@hopstudios.com>
2021-12-28 09:13:01 -0800

    Adding language file

--- system/ee/language/english/filemanager_lang.php
+++ system/ee/language/english/filemanager_lang.php
@@ -68,6 +68,8 @@ $lang = array(
 
     'invalid_short_name' => 'The image manipulation short name may only contain alpha-numeric characters, dashes and underscores, and cannot be named "thumbs".',
 
+    'list' => 'List',
+
     'location' => 'Location',
 
     'no_file' => 'No file selected',
