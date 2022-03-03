311fd661d305ca0d2a42e40d2d46bdfb4abf7251
robinsowell <robin.sowell@expressionengine.com>
2022-03-03 16:30:32 -0500

    Fixed mysql error in update 3.3.3
    Update EE 3.3.2 to 3.3.3
    
    Exception Caught
    SQLSTATE[42S22]: Column not found: 1054 Unknown column 'CategoryField_category_fields.legacy_field_data' in 'field list':

--- system/ee/installer/updates/ud_3_03_03.php
+++ system/ee/installer/updates/ud_3_03_03.php
@@ -66,6 +66,20 @@ class Updater
      */
     private function update_category_fields()
     {
+        if (! ee()->db->field_exists('legacy_field_data', 'category_fields')) {
+            ee()->smartforge->add_column(
+                'category_fields',
+                array(
+                    'legacy_field_data' => array(
+                        'type' => 'CHAR(1)',
+                        'null' => false,
+                        'default' => 'n'
+                    )
+                )
+            );
+            ee()->db->update('category_fields', array('legacy_field_data' => 'y'));
+        }
+        
         $category_fields = ee('Model')->get('CategoryField')
             ->all()
             ->indexBy('field_id');
