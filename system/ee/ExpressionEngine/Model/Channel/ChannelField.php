daf7176f82aae0c37edcc526a3d3305dd0da932b
Yuri Salimovskiy <yuri@packettide.com>
2022-03-10 15:59:53 +0200

    add missing property on Channel Field model

--- system/ee/ExpressionEngine/Model/Channel/ChannelField.php
+++ system/ee/ExpressionEngine/Model/Channel/ChannelField.php
@@ -110,6 +110,7 @@ class ChannelField extends FieldModel
     protected $field_content_type;
     protected $field_settings;
     protected $legacy_field_data;
+    protected $enable_frontedit;
 
     public function getStructure()
     {
