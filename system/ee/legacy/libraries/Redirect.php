1aa4e176728b2ca4787c80ef1a64a54cf3e2bde7
Doug Black Jr <dougblackjr@gmail.com>
2022-03-16 14:57:33 -0400

    Fixing session data

--- system/ee/legacy/libraries/Redirect.php
+++ system/ee/legacy/libraries/Redirect.php
@@ -23,7 +23,9 @@ if (! isset($_GET['URL'])) {
 // Should we allow front end redirects via the URL query string?
 if (REQ === 'PAGE' && !ee()->config->item('allow_url_redirects_from_site')) {
     // Check if they don't have an active CP session
-    if ((!isset(ee()->session->userdata['access_cp']) || !ee()->session->userdata['access_cp']) && !ee()->config->item('allow_url_redirects_from_site_without_cp_session')) {
+    if ((!isset(ee()->session->access_cp) || (isset(ee()->session->access_cp) && !ee()->session->access_cp))
+        && !ee()->config->item('allow_url_redirects_from_site_without_cp_session')
+    ) {
         show_404();
     }
 }
