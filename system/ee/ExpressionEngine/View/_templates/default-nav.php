5d3849e1a915dcd968b1742e76d6546c1c20ff29
Andy Johnson <andy@shapingthepage.com>
2022-03-18 16:29:36 -0400

    Hard coded new Upgrade Success banner

--- system/ee/ExpressionEngine/View/_templates/default-nav.php
+++ system/ee/ExpressionEngine/View/_templates/default-nav.php
@@ -1,5 +1,31 @@
 <?php $this->extend('_templates/wrapper'); ?>
 
+<!-- Upgrade Success banner should ONLY display on dashboard: -->
+<div class="upgrade-success-banner__wrapper" style="background-image: url('<?=URL_THEMES?>asset/img/ee-pro-promo-banner-bg@2x.png');">
+  <a href="" class="upgrade-success-banner__title">&#127881; ExpressionEngine <strong>6.1.0</strong></a>
+  <a href="" class="upgrade-success-banner__title-link"><i class="fas fa-clipboard"></i> Release Notes&hellip;</a>
+
+  <!-- Change number of blurb columns via inline grid style below IF less than 3 blurbs: -->
+  <div class="upgrade-success-banner__blurb-wrapper" style="grid-template-columns: repeat(3, 1fr);">
+    <a href="" class="upgrade-success-banner__blurb">
+      <i class="fas fa-star fa-fw"></i>
+      <h6>Introducting ExpressionEngine Pro</h6>
+      <p>Cotidieque sea accommodare reprimique eos laudem maiorum agam scribentur indoctum aeque recteque ea delicata viderer nam. <span class="upgrade-success-banner__blurb-learn">Learn more&hellip;</span></p>
+    </a>
+    <a href="" class="upgrade-success-banner__blurb">
+      <i class="fas fa-code fa-fw"></i>
+      <h6>Major CLI Improvements</h6>
+      <p>Cotidieque sea accommodare reprimique eos laudem maiorum agam scribentur indoctum aeque recteque ea delicata viderer nam. <span class="upgrade-success-banner__blurb-learn">Learn more&hellip;</span></p>
+    </a>
+    <a href="" class="upgrade-success-banner__blurb">
+      <i class="fas fa-paragraph fa-fw"></i>
+      <h6>Use Redactor for Rich Text Editing</h6>
+      <p>Cotidieque sea accommodare reprimique eos laudem maiorum agam scribentur indoctum aeque recteque ea delicata viderer nam. <span class="upgrade-success-banner__blurb-learn">Learn more&hellip;</span></p>
+    </a>
+  </div>
+  <a href="<?=ee('CP/URL')->make('homepage/dismissBanner')->compile();?>" class="banner-dismiss"></a>
+</div>
+
 <?php if (!(defined('IS_PRO') && IS_PRO) && ee()->session->getMember()->dismissed_pro_banner!='y'): ?>
   <div class="promo-banner-outer">
     <a href="https://expressionengine.com/pro" class="promo-banner-wrapper" style="background-image: url('<?=URL_THEMES?>asset/img/ee-pro-promo-banner-bg@2x.png'); background-color: #fff;" target="_blank">
