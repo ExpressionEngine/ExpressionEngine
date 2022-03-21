4640cef497a2bdb9d414762b3c83cc7c237b0f7c
Andy Johnson <andy@shapingthepage.com>
2022-03-21 09:31:33 -0400

    Add target blank to upgrade banner links

--- system/ee/ExpressionEngine/View/_templates/default-nav.php
+++ system/ee/ExpressionEngine/View/_templates/default-nav.php
@@ -2,22 +2,22 @@
 
 <!-- Upgrade Success banner should ONLY display on dashboard: -->
 <div class="upgrade-success-banner__wrapper" style="background-image: url('<?=URL_THEMES?>asset/img/ee-pro-promo-banner-bg@2x.png');">
-  <a href="" class="upgrade-success-banner__title">&#127881; ExpressionEngine <strong>6.1.0</strong></a>
-  <a href="" class="upgrade-success-banner__title-link"><i class="fas fa-clipboard"></i> Release Notes&hellip;</a>
+  <a href="" class="upgrade-success-banner__title" target="_blank">&#127881; ExpressionEngine <strong>6.1.0</strong></a>
+  <a href="" class="upgrade-success-banner__title-link" target="_blank"><i class="fas fa-clipboard"></i> Release Notes&hellip;</a>
 
   <!-- Change number of blurb columns via inline grid style below IF less than 3 blurbs: -->
   <div class="upgrade-success-banner__blurb-wrapper" style="grid-template-columns: repeat(3, 1fr);">
-    <a href="" class="upgrade-success-banner__blurb">
+    <a href="" class="upgrade-success-banner__blurb" target="_blank">
       <i class="fas fa-star fa-fw"></i>
       <h6>Introducting ExpressionEngine Pro</h6>
       <p>Cotidieque sea accommodare reprimique eos laudem maiorum agam scribentur indoctum aeque recteque ea delicata viderer nam. <span class="upgrade-success-banner__blurb-learn">Learn more&hellip;</span></p>
     </a>
-    <a href="" class="upgrade-success-banner__blurb">
+    <a href="" class="upgrade-success-banner__blurb" target="_blank">
       <i class="fas fa-code fa-fw"></i>
       <h6>Major CLI Improvements</h6>
       <p>Cotidieque sea accommodare reprimique eos laudem maiorum agam scribentur indoctum aeque recteque ea delicata viderer nam. <span class="upgrade-success-banner__blurb-learn">Learn more&hellip;</span></p>
     </a>
-    <a href="" class="upgrade-success-banner__blurb">
+    <a href="" class="upgrade-success-banner__blurb" target="_blank">
       <i class="fas fa-paragraph fa-fw"></i>
       <h6>Use Redactor for Rich Text Editing</h6>
       <p>Cotidieque sea accommodare reprimique eos laudem maiorum agam scribentur indoctum aeque recteque ea delicata viderer nam. <span class="upgrade-success-banner__blurb-learn">Learn more&hellip;</span></p>
