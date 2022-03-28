fd054cc767c37e9ceb862bdde8e55818b0e6f740
Yuri Salimovskiy <yuri@packettide.com>
2022-03-28 12:03:42 +0300

    display version number when viewing revision
    EECORE-1760

--- system/ee/ExpressionEngine/View/publish/partials/revision_badge.php
+++ system/ee/ExpressionEngine/View/publish/partials/revision_badge.php
@@ -1,3 +1,3 @@
 <span class="app-badge ml-s button-group-xsmall">
-    <span class="txt-only button button--default"><b><?=sprintf(lang('version_no'), $version_id)?></b> (<?=ee()->localize->human_time($version_date->format('U'))?>)</span>
+    <span class="txt-only button button--default"><b><?=sprintf(lang('version_no'), $number)?></b> (<?=ee()->localize->human_time($version_date->format('U'))?>)</span>
 </span>
