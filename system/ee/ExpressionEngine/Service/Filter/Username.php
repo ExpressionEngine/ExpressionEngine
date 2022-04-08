cded373f5fa5fdb094a4316fdcac9697eaa41957
Yuri Salimovskiy <yuri@packettide.com>
2022-04-08 09:32:05 +0300

    fix the issue where filter for username in CP Filters could not be cleared
    EECORE-1802
    
    fixes #860

--- system/ee/ExpressionEngine/Service/Filter/Username.php
+++ system/ee/ExpressionEngine/Service/Filter/Username.php
@@ -180,6 +180,10 @@ class Username extends Filter
                 $value;
         }
 
+        // Create a filter URL without this filter (per-filter clear).
+        $url_without_filter = clone $url;
+        $url_without_filter->removeQueryStringVariable($this->name);
+
         $filter = array(
             'label' => $this->label,
             'name' => $this->name,
@@ -189,6 +193,7 @@ class Username extends Filter
             'custom_value' => (array_key_exists($this->name, $_POST)) ? $_POST[$this->name] : false,
             'placeholder' => $this->placeholder,
             'options' => $options,
+            'url_without_filter' => $url_without_filter,
         );
 
         return $view->make('_shared/filters/filter')->render($filter);
