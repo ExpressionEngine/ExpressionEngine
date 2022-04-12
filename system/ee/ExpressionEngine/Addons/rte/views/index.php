fa684b9cc6876f36ab7d4cb84548b2f867edd16c
Yuri Salimovskiy <yuri@packettide.com>
2022-04-12 10:51:52 +0300

    make indentation consitent

--- system/ee/ExpressionEngine/Addons/rte/views/index.php
+++ system/ee/ExpressionEngine/Addons/rte/views/index.php
@@ -1,41 +1,41 @@
 <div class="mb">
-	<?php $this->embed('ee:_shared/form')?>
+    <?php $this->embed('ee:_shared/form')?>
 </div>
 <br />
 <div class="snap table-list-wrap">
-	<div class="panel">
-		<div class="tbl-ctrls">
-			<?=form_open(ee('CP/URL')->make('addons/settings/rte/update_toolsets'))?>
-				<div class="panel-heading">
-          <div class="form-btns form-btns-top">
-  					<div class="title-bar title-bar--large">
-              <h3 class="title-bar__title"><?=lang('available_tool_sets')?></h3>
-              <div class="title-bar__extra-tools">
-    						<a class="button button--primary tn action" href="<?=ee('CP/URL')->make('addons/settings/rte/edit_toolset')?>"><?=lang('create_new')?></a>
-              </div>
-  					</div>
-          </div>
-				</div>
+    <div class="panel">
+        <div class="tbl-ctrls">
+            <?=form_open(ee('CP/URL')->make('addons/settings/rte/update_toolsets'))?>
+                <div class="panel-heading">
+                    <div class="form-btns form-btns-top">
+                        <div class="title-bar title-bar--large">
+                            <h3 class="title-bar__title"><?=lang('available_tool_sets')?></h3>
+                            <div class="title-bar__extra-tools">
+                                <a class="button button--primary tn action" href="<?=ee('CP/URL')->make('addons/settings/rte/edit_toolset')?>"><?=lang('create_new')?></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-				<?php $this->embed('ee:_shared/table', $table); ?>
+                <?php $this->embed('ee:_shared/table', $table); ?>
 
-				<?php $this->embed('ee:_shared/form/bulk-action-bar', [
-				    'options' => [
-				        [
-				            'value' => "",
-				            'text' => '-- ' . lang('with_selected') . ' --'
-				        ],
-				        [
-				            'value' => "remove",
-				            'text' => lang('delete'),
-				            'attrs' => ' data-confirm-trigger="selected" rel="modal-confirm-remove"'
-				        ]
-				    ],
-				    'modal' => true
-				]); ?>
-			<?=form_close();?>
-		</div>
-	</div>
+                <?php $this->embed('ee:_shared/form/bulk-action-bar', [
+                    'options' => [
+                        [
+                            'value' => "",
+                            'text' => '-- ' . lang('with_selected') . ' --'
+                        ],
+                        [
+                            'value' => "remove",
+                            'text' => lang('delete'),
+                            'attrs' => ' data-confirm-trigger="selected" rel="modal-confirm-remove"'
+                        ]
+                    ],
+                    'modal' => true
+                ]); ?>
+            <?=form_close();?>
+        </div>
+    </div>
 </div>
 
 <?php
