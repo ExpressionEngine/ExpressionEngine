d855a0df8c19604def4fee5b6075387f3a6dcb69
Yuri Salimovskiy <yuri@packettide.com>
2022-05-05 10:36:59 +0300

    fix the issue where PHP notices were shown when using slider as CP component (#2060)
    * fix the issue where PHP noticed were shown when using slider as CP component
    
    EECORE-1837
    
    * fix PHP 8.1 notices

--- system/ee/legacy/helpers/form_helper.php
+++ system/ee/legacy/helpers/form_helper.php
@@ -31,10 +31,6 @@ if (! defined('BASEPATH')) {
 if (REQ == 'CP') {
     function form_open($action = '', $attributes = array(), $hidden = array())
     {
-        if (strpos($action, '://') === false && strpos($action, BASE) !== 0) {
-            //$action = BASE.AMP.$action;
-        }
-
         $action = ee()->uri->reformat($action);
 
         $form = '<form action="' . $action . '"';
