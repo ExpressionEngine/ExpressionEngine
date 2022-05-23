ad5b14306795b59f1fb03f2e7399f51a66268448
sobral <robson@oficiodesign.com>
2022-05-22 23:06:50 -0300

    Add tests of cached templates

--- tests/cypress/cypress/integration/design/template_of_resources.ee6.js
+++ tests/cypress/cypress/integration/design/template_of_resources.ee6.js
@@ -3,6 +3,17 @@
 import SiteForm from '../../elements/pages/site/SiteForm';
 import SiteManager from '../../elements/pages/site/SiteManager';
 
+const files = [
+	'../../system/user/templates/default_site/resources.group/style.css',
+	'../../system/user/templates/default_site/resources.group/script.js',
+	'../../system/user/templates/second_site/resources.group/style.css',
+]
+const replaceColors = {
+	'cyan': 'red',
+	'magenta': 'lime',
+	'yellow': 'blue',
+}
+
 context('Design', () => {
 	before(function () {
 		cy.task('db:seed')
@@ -71,44 +82,79 @@ context('Design', () => {
 		})
 	})
 
-  describe('loading of resources from updated templates', function () {
-    before(function () {
-      const files = [
-        '../../system/user/templates/default_site/resources.group/style.css',
-        '../../system/user/templates/default_site/resources.group/script.js',
-        '../../system/user/templates/second_site/resources.group/style.css',
-      ]
-      const replaceColors = {
-        'cyan': 'red',
-        'magenta': 'lime',
-        'yellow': 'blue',
-      }
+	describe('loading of resources from updated templates', function () {
+		before(function () {
+			files.forEach((file) => {
+				cy.readFile(file, (err, data) => {
+					if (err) {
+					return console.error(err);
+					};
+				}).then((data) => {
+					Object.keys(replaceColors).forEach((x)=> {
+					data = data.replace(x, replaceColors[x]);
+					})
+					cy.writeFile(file, data);
+				})
+			})
+		})
 
-      files.forEach((file) => {
-        cy.readFile(file, (err, data) => {
-          if (err) {
-            return console.error(err);
-          };
-        }).then((data) => {
-          Object.keys(replaceColors).forEach((x)=> {
-            data = data.replace(x, replaceColors[x]);
-          })
-          cy.writeFile(file, data);
-        })
-      })
-    })
+		it('loads stylesheet resource template from the current site', function () {
+			cy.logFrontendPerformance()
+			cy.get("#first").should('have.css', 'background-color', 'rgb(255, 0, 0)')
+		})
 
-    it('loads stylesheet resource template from the current site', function () {
-      cy.logFrontendPerformance()
-      cy.get("#first").should('have.css', 'background-color', 'rgb(255, 0, 0)')
-    })
+		it('loads script resource template from the current site', function () {
+			cy.get("#second").should('have.css', 'background-color', 'rgb(0, 255, 0)')
+		})
 
-    it('loads script resource template from the current site', function () {
-      cy.get("#second").should('have.css', 'background-color', 'rgb(0, 255, 0)')
-    })
+		it('loads stylesheet resource template from a different MSM site', function () {
+			cy.get("#third").should('have.css', 'background-color', 'rgb(0, 0, 255)')
+		})
+	})
 
-    it('loads stylesheet resource template from a different MSM site', function () {
-      cy.get("#third").should('have.css', 'background-color', 'rgb(0, 0, 255)')
-    })
-  })
+	describe('loading of resources on a cached HTML template', function () {
+		before(function () {
+			cy.authVisit('admin.php?/cp/design/manager/resources')
+
+			cy.get('.app-listing__row a').contains('index').click()
+
+			cy.get('.js-tab-button.tab-bar__tab').contains('Settings').click()
+			cy.get('[data-toggle-for="cache"][data-state="off"]').click()
+			cy.get('button').contains('Save').click()
+
+			// visit the page to save a cached version
+			cy.visit('index.php/resources/index')
+		})
+
+		beforeEach(function () {
+			cy.visit('index.php/resources/index')
+		})
+
+		it('respects the saved styles', function () {
+			cy.get("#first").should('have.css', 'background-color', 'rgb(255, 0, 0)')
+			cy.get("#second").should('have.css', 'background-color', 'rgb(0, 255, 0)')
+			cy.get("#third").should('have.css', 'background-color', 'rgb(0, 0, 255)')
+		})
+
+		it('serves new styles', function () {
+			files.forEach((file) => {
+				cy.readFile(file, (err, data) => {
+					if (err) {
+						return console.error(err);
+					};
+				}).then((data) => {
+					Object.keys(replaceColors).forEach((x) => {
+						data = data.replace(replaceColors[x], x);
+					})
+					cy.writeFile(file, data);
+				})
+			})
+
+			cy.get("#first").should('have.css', 'background-color', 'rgb(0, 255, 255)')
+
+			cy.get("#second").should('have.css', 'background-color', 'rgb(255, 0, 255)')
+
+			cy.get("#third").should('have.css', 'background-color', 'rgb(255, 255, 0)')
+		})
+	})
 })
