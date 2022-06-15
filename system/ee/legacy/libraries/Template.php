9693efcfa9c595fa1f8a87e7e747a8903919c4cc
JCOGS Design <13821249+jcogs-design@users.noreply.github.com>
2022-06-15 15:29:01 +0100

    Fix php8.1 null assignment deprecation warning
    Line 1779 is passing null to `str_replace` under some conditions and assuming null will be replaced by ''.  php 8.1 doesn't like this and issues a deprecation warning. This uses the null coalescing operator to replace null with '' when the function is called if necessary.

--- system/ee/legacy/libraries/Template.php
+++ system/ee/legacy/libraries/Template.php
@@ -1776,7 +1776,7 @@ class EE_Template
 
                 // Replace the temporary markers we added earlier with the fully parsed data
 
-                $this->template = str_replace('M' . $i . $this->marker, $return_data, $this->template);
+                $this->template = str_replace('M' . $i . $this->marker,  $return_data ?? '', $this->template);
 
                 // Initialize data in case there are susequent loops
 
