adc717217dbf1a53aa79e79f1b29657b57739312
Yuri Salimovskiy <yuri@packettide.com>
2022-06-21 10:44:55 +0300

    only show filepicker upload button if invoked from textarea / RTE

--- system/ee/ExpressionEngine/Library/CP/FileManager/Traits/FileManagerTrait.php
+++ system/ee/ExpressionEngine/Library/CP/FileManager/Traits/FileManagerTrait.php
@@ -47,6 +47,11 @@ trait FileManagerTrait
                     'requested_directory' => $requested_directory
                 ]);
             }
+            if (!empty(ee('Request')->get('hasUpload'))) {
+                $base_url->addQueryStringVariables([
+                    'hasUpload' => ee('Request')->get('hasUpload')
+                ]);
+            }
         }
 
         $files = ee('Model')->get($model)
@@ -233,7 +238,9 @@ trait FileManagerTrait
             'imitationButton' => true
         ];
 
-        $table->setNoResultsHTML(ee('View')->make('ee:_shared/file/upload-widget')->render(['component' => $uploaderComponent]), 'file-upload-widget');
+        if (!$filepickerMode || ee('Request')->get('hasUpload') == 1) {
+            $table->setNoResultsHTML(ee('View')->make('ee:_shared/file/upload-widget')->render(['component' => $uploaderComponent]), 'file-upload-widget');
+        }
 
         if (! empty($uploadLocation) && $uploadLocation->subfolders_on_top === true) {
             // $files->fields('model_type');
@@ -303,6 +310,11 @@ trait FileManagerTrait
                     if (!empty($requested_directory)) {
                         $attrs['data-filter-url']->addQueryStringVariables([
                             'requested_directory' => $requested_directory
+                        ]);
+                    }
+                    if (!empty(ee('Request')->get('hasUpload'))) {
+                        $attrs['data-filter-url']->addQueryStringVariables([
+                            'hasUpload' => ee('Request')->get('hasUpload')
                         ]);
                     }
                 }
