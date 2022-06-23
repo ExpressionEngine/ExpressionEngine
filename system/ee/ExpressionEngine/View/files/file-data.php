410bb93a0f7fcc89da26076ca7d8f21a1cd4f7c5
Yuri Salimovskiy <yuri@packettide.com>
2022-06-23 18:13:05 +0300

    tests for editing non-image

--- system/ee/ExpressionEngine/View/files/file-data.php
+++ system/ee/ExpressionEngine/View/files/file-data.php
@@ -2,7 +2,7 @@
     <?php foreach ($data as $key => $value) : ?>
         <p class="f_metadata-item">
             <span class="f_meta-name"><?=lang($key)?></span>
-            <span class="f_meta-info"><?=$value?></span>
+            <span class="f_meta-info f_meta-info-<?=$key?>"><?=$value?></span>
         </p>
     <?php endforeach; ?>
 </div>
