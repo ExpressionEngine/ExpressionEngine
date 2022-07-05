ab54106fb7b3e8909aa0de230e44332685f061a5
Yuri Salimovskiy <yuri@packettide.com>
2022-07-05 12:04:48 +0300

    more subfolders tests

--- tests/cypress/cypress/integration/files/subfolders.ee6.js
+++ tests/cypress/cypress/integration/files/subfolders.ee6.js
@@ -1,11 +1,11 @@
 /// <reference types="Cypress" />
 
 import UploadEdit from '../../elements/pages/files/UploadEdit';
-import WatermarkEdit from '../../elements/pages/files/WatermarkEdit';
 import FileManager from '../../elements/pages/files/FileManager';
 const page = new UploadEdit;
 const managerPage = new FileManager;
-const watermark = new WatermarkEdit;
+const { _, $ } = Cypress
+
 const upload_path = "{base_path}/images"
 
 context('Upload Destination Subfolders', () => {
@@ -13,7 +13,7 @@ context('Upload Destination Subfolders', () => {
     before(function() {
         cy.task('db:seed')
             //cleanup folders
-        cy.task('filesystem:delete', '../../images/about/sub*')
+        cy.task('filesystem:delete', '../../images/uploads/*')
     })
 
     beforeEach(function() {
@@ -30,7 +30,7 @@ context('Upload Destination Subfolders', () => {
     })
 
     it('Enable Subfolder support', () => {
-        page.load_edit_for_dir(1)
+        page.load_edit_for_dir(2)
         page.get('name').should('exist')
         page.get('toggle_subfolder_support').should('have.class', 'off')
         page.get('toggle_subfolder_support').click()
@@ -40,7 +40,7 @@ context('Upload Destination Subfolders', () => {
     })
 
     it('Can create a new folder', () => {
-        managerPage.load_for_dir(1)
+        managerPage.load_for_dir(2)
         managerPage.get('new_folder_button').click()
 
         cy.get('.modal-new-folder input[name="folder_name"]').type('sub1')
@@ -52,7 +52,7 @@ context('Upload Destination Subfolders', () => {
     })
 
     it('Cannot create a folder that already exists', () => {
-        managerPage.load_for_dir(1)
+        managerPage.load_for_dir(2)
         managerPage.get('new_folder_button').click()
 
         cy.get('.modal-new-folder input[name="folder_name"]').type('sub1')
@@ -64,7 +64,7 @@ context('Upload Destination Subfolders', () => {
     })
 
     it('Cannot create a folder with special characters', () => {
-        managerPage.load_for_dir(1)
+        managerPage.load_for_dir(2)
         managerPage.get('new_folder_button').click()
 
         cy.get('.modal-new-folder input[name="folder_name"]').type('sub1/sub2')
@@ -76,7 +76,7 @@ context('Upload Destination Subfolders', () => {
     })
 
     it('Can create a nested folder', () => {
-        managerPage.load_for_dir(1)
+        managerPage.load_for_dir(2)
         managerPage.get('new_folder_button').click()
 
         cy.get('.modal-new-folder [data-input-value="upload_location"] .js-dropdown-toggle').click()
@@ -90,13 +90,13 @@ context('Upload Destination Subfolders', () => {
     })
 
     it('Cannot move a folder into current parent', () => {
-        managerPage.load_for_dir(1)
+        managerPage.load_for_dir(2)
 
         cy.get('.app-listing__row[title="sub1"] .button-toolbar').click()
-        cy.get('.toolbar.has-open-dropdown .move').click()
+        cy.get('.toolbar.has-open-dropdown .move').click({force: true})
 
         cy.get('.modal-confirm-move-file [data-input-value="upload_location"] .js-dropdown-toggle').click()
-        cy.get('.modal-confirm-move-file .select__dropdown-item').contains('About').click()
+        cy.get('.modal-confirm-move-file .select__dropdown-item').contains('Main Upload Directory').click()
         cy.get('.modal-confirm-move-file #fieldset-confirm .toggle-btn').click()
         cy.get('.modal-confirm-move-file button[type="submit"]').click()
 
@@ -106,7 +106,7 @@ context('Upload Destination Subfolders', () => {
     })
 
     it('Can move a folder', () => {
-        managerPage.load_for_dir(1)
+        managerPage.load_for_dir(2)
 
         cy.get('.app-listing__row[title="sub1"] a').contains('sub1').click()
 
@@ -114,7 +114,7 @@ context('Upload Destination Subfolders', () => {
         cy.get('.toolbar.has-open-dropdown .move').click({ force: true })
 
         cy.get('.modal-confirm-move-file [data-input-value="upload_location"] .js-dropdown-toggle').click()
-        cy.get('.modal-confirm-move-file .select__dropdown-item').contains('About').click()
+        cy.get('.modal-confirm-move-file .select__dropdown-item').contains('Main Upload Directory').click()
 
         cy.get('.modal-confirm-move-file button[type="submit"]').click()
 
@@ -124,7 +124,7 @@ context('Upload Destination Subfolders', () => {
     })
 
     it('Cannot rename a folder to an existing folder name', () => {
-        managerPage.load_for_dir(1)
+        managerPage.load_for_dir(2)
 
         cy.get('.app-listing__row[title="sub2"] .button-toolbar').click()
 
@@ -139,7 +139,7 @@ context('Upload Destination Subfolders', () => {
     })
 
     it('Can rename a folder', () => {
-        managerPage.load_for_dir(1)
+        managerPage.load_for_dir(2)
 
         cy.get('.app-listing__row[title="sub2"] .button-toolbar').click()
 
@@ -154,23 +154,124 @@ context('Upload Destination Subfolders', () => {
     })
 
     it('Can upload a file to a folder', () => {
-        managerPage.load_for_dir(1)
+        managerPage.load_for_dir(2)
 
         cy.get('.app-listing__row[title="sub1"] a').contains('sub1').click()
 
         cy.intercept('/admin.php?/cp/addons/settings/filepicker/ajax-upload').as('upload')
         cy.intercept('/admin.php?/cp/files/directory/*').as('table')
-        managerPage.get('file_input').find('.file-field__dropzone').attachFile('../../support/file/programming.gif', { subjectType: 'drag-n-drop' })
+        managerPage.get('file_input').find('.file-field__dropzone').attachFile('../../support/file/README.md', { subjectType: 'drag-n-drop' })
 
         cy.wait('@upload')
         cy.wait('@table')
         cy.hasNoErrors()
 
-        cy.get('..app-listing__row').contains("programming.gif")
+        cy.get('.app-listing__row').contains("README.md")
+    })
+    
+    it('Can move a file to a folder', () => {
+        managerPage.load_for_dir(2)
+        let filename = 'LICENSE.txt';
+
+        cy.intercept('/admin.php?/cp/addons/settings/filepicker/ajax-upload').as('upload')
+        cy.intercept('/admin.php?/cp/files/directory/*').as('table')
+        cy.get('.file-upload-widget').then(function(widget) {
+            $(widget).removeClass('hidden')
+        })
+        managerPage.get('file_input').find('.file-field__dropzone').attachFile(('../../../../LICENSE.txt'), { subjectType: 'drag-n-drop' })
+
+        cy.wait('@upload')
+        cy.wait('@table')
+        cy.hasNoErrors()
+        cy.get('.app-listing__row').should('contain', filename)
+
+        cy.get('.app-listing__row:contains(' + filename + ') .app-listing__cell .js-dropdown-toggle').click()
+        cy.get('.app-listing__row:contains(' + filename + ') .app-listing__cell .dropdown').should('be.visible')
+        cy.get('.app-listing__row:contains(' + filename + ') .app-listing__cell .dropdown--open .dropdown__link:contains("Move")').click({force: true})
+        managerPage.get('modal').should('be.visible')
+        managerPage.get('modal').find('.js-dropdown-toggle').click()
+        managerPage.get('modal').find('.select--open .select__dropdown-item').contains('sub1').click()
+        managerPage.get('modal').find('.button--primary').filter(':visible').first().click()
+        cy.hasNoErrors()
+
+        //file is not here anymore
+        managerPage.get('files').should('not.contain', filename)
+        //but is in subfolder
+        cy.get('.app-listing__row[title="sub1"] a').contains('sub1').click()
+        managerPage.get('files').should('contain', filename)
+        //and is editable
+        cy.get('.app-listing__row:contains(' + filename + ') .app-listing__cell .js-dropdown-toggle').click()
+        cy.get('.app-listing__row:contains(' + filename + ') .app-listing__cell .dropdown').should('be.visible')
+        cy.get('.app-listing__row:contains(' + filename + ') .app-listing__cell .dropdown--open .dropdown__link:contains("Edit")').click({force: true})
+        cy.get('.main-nav__title').should('contain', 'Edit File')
+        cy.get('.title-bar__title').should('contain', filename)
+    })
+
+    it('Can move file between folders', () => {
+        managerPage.load_for_dir(2)
+        cy.get('.app-listing__row[title="sub1"] a').contains('sub1').click()
+
+        let filename = '';
+        managerPage.get('title_names').eq(0).find('a').invoke('text').then((text) => {
+            filename = text.trim()
+
+            managerPage.get('files').eq(0).find('.app-listing__cell .js-dropdown-toggle').click()
+            managerPage.get('files').eq(0).find('.app-listing__cell .dropdown').should('be.visible')
+            managerPage.get('files').eq(0).find(".app-listing__cell .dropdown--open .dropdown__link:contains('Move')").click({force: true})
+            managerPage.get('modal').should('be.visible')
+            managerPage.get('modal').find('.js-dropdown-toggle').click()
+            managerPage.get('modal').find('.select--open .select__dropdown-item').contains('sub3').click()
+            managerPage.get('modal').find('.button--primary').filter(':visible').first().click()
+            cy.hasNoErrors()
+
+            //file is not here anymore
+            cy.get('.ee-main__content form .table-responsive table').should('not.contain', filename)
+            //but is in subfolder
+            managerPage.load_for_dir(2)
+            cy.get('.app-listing__row[title="sub3"] a').contains('sub3').click()
+            managerPage.get('files').should('contain', filename)
+            //and is editable
+            cy.get('.app-listing__row:contains(' + filename + ') .app-listing__cell .js-dropdown-toggle').click()
+            cy.get('.app-listing__row:contains(' + filename + ') .app-listing__cell .dropdown').should('be.visible')
+            cy.get('.app-listing__row:contains(' + filename + ') .app-listing__cell .dropdown--open .dropdown__link:contains("Edit")').click({force: true})
+            cy.get('.main-nav__title').should('contain', 'Edit File')
+            cy.get('.title-bar__title').should('contain', filename)
+        })
+    })
+
+    it('Cannot move file if it already exists', () => {
+        managerPage.load_for_dir(2)
+        let filename = 'LICENSE.txt';
+
+        cy.intercept('/admin.php?/cp/addons/settings/filepicker/ajax-upload').as('upload')
+        cy.intercept('/admin.php?/cp/files/directory/*').as('table')
+        cy.get('.file-upload-widget').then(function(widget) {
+            $(widget).removeClass('hidden')
+        })
+        managerPage.get('file_input').find('.file-field__dropzone').attachFile(('../../../../LICENSE.txt'), { subjectType: 'drag-n-drop' })
+
+        cy.wait('@upload')
+        cy.wait('@table')
+        cy.get('.app-listing__row').should('contain', filename)
+        cy.hasNoErrors()
+
+        cy.get('.app-listing__row:contains(' + filename + ') .app-listing__cell .js-dropdown-toggle').click()
+        cy.get('.app-listing__row:contains(' + filename + ') .app-listing__cell .dropdown').should('be.visible')
+        cy.get('.app-listing__row:contains(' + filename + ') .app-listing__cell .dropdown--open .dropdown__link:contains("Move")').click({force: true})
+        managerPage.get('modal').should('be.visible')
+        managerPage.get('modal').find('.js-dropdown-toggle').click()
+        managerPage.get('modal').find('.select--open .select__dropdown-item').contains('sub3').click()
+        managerPage.get('modal').find('.button--primary').filter(':visible').first().click()
+        cy.hasNoErrors()
+
+        cy.get('.app-notice---important').contains('Could not move some files')
+
+        //file is still in its place
+        managerPage.get('files').should('contain', filename)
     })
 
     it('Can delete a folder', () => {
-        managerPage.load_for_dir(1)
+        managerPage.load_for_dir(2)
 
         cy.get('.app-listing__row[title="sub3"] .button-toolbar').click()
         cy.get('.toolbar.has-open-dropdown .delete').click({ force: true })
