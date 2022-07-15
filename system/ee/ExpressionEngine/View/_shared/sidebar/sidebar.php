ea6051b086a0fb00823caa9c179b41343ecc3b34
Yulya Lebed <yulya@packettide.com>
2022-07-15 10:21:56 +0200

    fixed secondary sidebar Collapse part

--- system/ee/ExpressionEngine/View/_shared/sidebar/sidebar.php
+++ system/ee/ExpressionEngine/View/_shared/sidebar/sidebar.php
@@ -2,4 +2,9 @@
     <div class="box sidebar <?=$class?>">
         <?=$sidebar?>
     </div>
+    <div class="secondary-sidebar-toggle">
+        <a href="" class="secondary-sidebar-toggle__target <?=(isset($left_nav_collapsed) && $left_nav_collapsed ? 'collapsed' : '')?>" title="<?=lang('toggle_sidebar')?>">
+            <i class="fal fa-angle-<?=(isset($left_nav_collapsed) && $left_nav_collapsed ? 'right' : 'left')?>"></i>
+        </a>
+    </div>
 </div>
--- system/ee/ExpressionEngine/View/_templates/default-nav.php
+++ system/ee/ExpressionEngine/View/_templates/default-nav.php
@@ -119,11 +119,6 @@
             <?=$left_nav?>
 
             <div class="container" style="position: relative;">
-                <div class="secondary-sidebar-toggle">
-                    <a href="" class="secondary-sidebar-toggle__target <?=(isset($left_nav_collapsed) && $left_nav_collapsed ? 'collapsed' : '')?>" title="<?=lang('toggle_sidebar')?>">
-                        <i class="fal fa-angle-<?=(isset($left_nav_collapsed) && $left_nav_collapsed ? 'right' : 'left')?>"></i>
-                    </a>
-                </div>
                 <?=$child_view?>
             </div>
         </div>
