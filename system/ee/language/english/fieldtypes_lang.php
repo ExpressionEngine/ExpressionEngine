0b8ecc9ba2ada570e75f0b237e2c6f313ce8293b
Andy Johnson <andy@shapingthepage.com>
2022-07-25 13:25:01 -0400

    Consistent case usage for upload dropzone

--- system/ee/language/english/fieldtypes_lang.php
+++ system/ee/language/english/fieldtypes_lang.php
@@ -149,7 +149,7 @@ $lang = array(
 
     'file_dnd_choose_file_directory' => 'Choose a File Directory',
 
-    'file_dnd_upload_to' => 'Upload To',
+    'file_dnd_upload_to' => 'Upload to',
 
     'file_dnd_choose_directory_before_uploading' => 'You must choose a directory to upload files',
 
