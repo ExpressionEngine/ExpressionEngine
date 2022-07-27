bb748baeaf6d35629c71a7a9020c6e9864078356
Yuri Salimovskiy <yuri@packettide.com>
2022-07-27 12:44:50 +0300

    fix loading CSS/JS for packages that became first-party
    EECORE-2028

--- system/ee/legacy/libraries/Cp.php
+++ system/ee/legacy/libraries/Cp.php
@@ -866,7 +866,7 @@ class Cp
     public function load_package_js($file)
     {
         $current_top_path = ee()->load->first_package_path();
-        $package = trim(str_replace(array(PATH_THIRD, 'views'), '', $current_top_path), '/');
+        $package = trim(str_replace(array(PATH_THIRD, PATH_ADDONS, 'views'), '', $current_top_path), '/');
 
         $this->add_js_script(array('package' => $package . ':' . $file));
     }
@@ -882,7 +882,7 @@ class Cp
     public function load_package_css($file)
     {
         $current_top_path = ee()->load->first_package_path();
-        $package = trim(str_replace(array(PATH_THIRD, 'views'), '', $current_top_path), '/');
+        $package = trim(str_replace(array(PATH_THIRD, PATH_ADDONS, 'views'), '', $current_top_path), '/');
 
         if (REQ == 'CP') {
             $url = BASE . AMP . 'C=css' . AMP . 'M=third_party' . AMP . 'package=' . $package . AMP . 'file=' . $file;
