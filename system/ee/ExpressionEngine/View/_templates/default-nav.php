bb4405f19facd9517e7aebf9d8b33ae33cbf3a93
Yulya Lebed <yulya@packettide.com>
2022-08-02 12:56:38 +0200

    moved banner code to separate file, add script to close the banner

--- system/ee/ExpressionEngine/View/_templates/default-nav.php
+++ system/ee/ExpressionEngine/View/_templates/default-nav.php
@@ -1,30 +1,7 @@
 <?php $this->extend('_templates/wrapper'); ?>
 
 <!-- Upgrade Success banner should ONLY display on dashboard: -->
-<div class="upgrade-success-banner__wrapper" style="background-image: url('<?=URL_THEMES?>asset/img/ee-pro-promo-banner-bg@2x.png');">
-  <a href="" class="upgrade-success-banner__title" target="_blank">&#127881; ExpressionEngine <strong>6.1.0</strong></a>
-  <a href="" class="upgrade-success-banner__title-link" target="_blank"><i class="fas fa-clipboard"></i> Release Notes&hellip;</a>
-
-  <!-- Change number of blurb columns via inline grid style below IF less than 3 blurbs: -->
-  <div class="upgrade-success-banner__blurb-wrapper" style="grid-template-columns: repeat(3, 1fr);">
-    <a href="" class="upgrade-success-banner__blurb" target="_blank">
-      <i class="fas fa-star fa-fw"></i>
-      <h6>Introducting ExpressionEngine Pro</h6>
-      <p>Cotidieque sea accommodare reprimique eos laudem maiorum agam scribentur indoctum aeque recteque ea delicata viderer nam. <span class="upgrade-success-banner__blurb-learn">Learn more&hellip;</span></p>
-    </a>
-    <a href="" class="upgrade-success-banner__blurb" target="_blank">
-      <i class="fas fa-code fa-fw"></i>
-      <h6>Major CLI Improvements</h6>
-      <p>Cotidieque sea accommodare reprimique eos laudem maiorum agam scribentur indoctum aeque recteque ea delicata viderer nam. <span class="upgrade-success-banner__blurb-learn">Learn more&hellip;</span></p>
-    </a>
-    <a href="" class="upgrade-success-banner__blurb" target="_blank">
-      <i class="fas fa-paragraph fa-fw"></i>
-      <h6>Use Redactor for Rich Text Editing</h6>
-      <p>Cotidieque sea accommodare reprimique eos laudem maiorum agam scribentur indoctum aeque recteque ea delicata viderer nam. <span class="upgrade-success-banner__blurb-learn">Learn more&hellip;</span></p>
-    </a>
-  </div>
-  <a href="<?=ee('CP/URL')->make('homepage/dismissBanner')->compile();?>" class="banner-dismiss"></a>
-</div>
+<?php $this->embed('_shared/upgrade_success_banner'); ?>
 
 <?php if (!(defined('IS_PRO') && IS_PRO) && ee()->session->getMember()->dismissed_pro_banner!='y'): ?>
   <div class="promo-banner-outer">
--- themes/ee/asset/javascript/src/common.js
+++ themes/ee/asset/javascript/src/common.js
@@ -339,6 +339,14 @@ $(document).ready(function(){
 		$(this).parent().remove();
 		$.get(EE.cp.dismissBannerURL);
 	})
+	
+	// Collapse navigation sidebar
+	// -------------------------------------------------------------------
+	$('.upgrade-banner-dismiss').on('click', function (e) {
+		e.preventDefault();
+		$(this).parent().remove();
+		$.get(EE.cp.upgradeSuccessBannerURL);
+	})
 
 	// Toggle Developer Menu
 	// -------------------------------------------------------------------
