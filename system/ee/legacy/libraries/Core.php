950736fefd0ba500934e78a3f9881053712c5b16
Yuri Salimovskiy <yuri@packettide.com>
2022-08-03 16:17:14 +0300

    7.0.1 version files

--- system/ee/legacy/libraries/Core.php
+++ system/ee/legacy/libraries/Core.php
@@ -74,8 +74,8 @@ class EE_Core
 
         // application constants
         define('APP_NAME', 'ExpressionEngine');
-        define('APP_BUILD', '20220802');
-        define('APP_VER', '7.0.0');
+        define('APP_BUILD', '20220803');
+        define('APP_VER', '7.0.1');
         define('APP_VER_ID', '');
         define('SLASH', '&#47;');
         define('LD', '{');
