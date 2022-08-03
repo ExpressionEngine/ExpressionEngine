7e35abb250b9c9b3d30b056c069839acd6839060
Yuri Salimovskiy <yuri@packettide.com>
2022-08-03 13:32:51 +0300

    conditional database manipulation

--- tests/cypress/cypress/integration/updater/test_z_one_click.ee6.js
+++ tests/cypress/cypress/integration/updater/test_z_one_click.ee6.js
@@ -14,9 +14,13 @@ context('One-Click Updater', () => {
     // This test is also used in the pre-release.yml workflow and gets a copy of 6.1.5
     // We've just selected the same version here to not interfere with that test
     // but also allow this to do a simple check for working updater in current code
-    cy.eeConfig({item: 'app_version', value: '6.1.5'})
-    // add column that gets removed in 6.3.5
-    cy.task('db:query', "ALTER TABLE exp_members ADD COLUMN `dismissed_pro_banner` CHAR(1) NULL DEFAULT 'n'")
+    cy.eeConfig({item: 'app_version'}) .then((config) => {
+      if (config != '6.1.5') {
+        cy.eeConfig({item: 'app_version', value: '6.1.5'})
+        // add column that gets removed in 6.3.5
+        cy.task('db:query', "ALTER TABLE exp_members ADD COLUMN `dismissed_pro_banner` CHAR(1) NULL DEFAULT 'n'")
+      }
+    })
   })
 
   beforeEach(function() {
