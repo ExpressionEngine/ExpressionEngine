a24b2b2f0f150b308a5efd4dca867d905004e619
Yuri Salimovskiy <yuri@packettide.com>
2022-08-10 16:33:02 +0300

    Resolved #2076, #2141 where button to update template routes was missing some functionality

--- system/ee/ExpressionEngine/View/design/routes/index.php
+++ system/ee/ExpressionEngine/View/design/routes/index.php
@@ -1,24 +1,29 @@
 <?php $this->extend('_templates/default-nav'); ?>
 <div class="panel">
-	<?=form_open($form_url)?>
-  <div class="panel-heading">
-		<div class="app-notice-wrap"><?=ee('CP/Alert')->getAllInlines()?></div>
+    <?=form_open($form_url)?>
+    <div class="panel-heading">
+        <div class="app-notice-wrap"><?=ee('CP/Alert')->getAllInlines()?></div>
 
-		<div class="title-bar">
-			<h3 class="title-bar__title"><?=$cp_heading?><br><i><?=$cp_sub_heading?></i></h3>
-		</div>
-  </div>
+        <div class="form-btns form-btns-top">
+            <div class="title-bar title-bar--large">
+            <h3 class="title-bar__title"><?=$cp_heading?><br><i><?=$cp_sub_heading?></i></h3>
+
+                <div class="title-bar__extra-tools">
+                    <?php $this->embed('ee:_shared/form/buttons'); ?>
+                </div>
+            </div>
+        </div>
+    </div>
 
   <div class="panel-body">
-		<?php $this->embed('_shared/table', $table); ?>
-		<?php $this->embed('_shared/pagination'); ?>
+        <?php $this->embed('_shared/table', $table); ?>
+        <?php $this->embed('_shared/pagination'); ?>
   </div>
-    <?php if (! empty($table['data'])): ?>
-      <div class="panel-footer">
-  			<div class="form-btns">
-				<?=cp_form_submit('update', 'btn_saving')?>
-  			</div>
-      </div>
-		<?php endif ?>
-	<?=form_close()?>
+
+    <div class="panel-footer">
+        <div class="form-btns">
+            <?php $this->embed('ee:_shared/form/buttons'); ?>
+        </div>
+    </div>
+    <?=form_close()?>
 </div>
