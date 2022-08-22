4e24e78d7b1bc028aef3f3cc74cea276135cf662
Yulya Lebed <yulya@packettide.com>
2022-08-22 11:09:36 +0200

    Resolved #1882 where deferring relationship field was not working for fields with data

--- themes/ee/cp/js/build/components/relationship.js
+++ themes/ee/cp/js/build/components/relationship.js
@@ -489,15 +489,6 @@ function (_React$Component) {
           });
         });
       });
-
-      if ($('.react-deferred-loading--relationship ul.list-group').length) {
-        var $wrapper = $('.react-deferred-loading--relationship ul.list-group').parents('.react-deferred-loading');
-        $($wrapper, context).each(function () {
-          var props = JSON.parse(window.atob($(this).data('relationshipReact')));
-          props.name = $(this).data('inputValue');
-          ReactDOM.render(React.createElement(Relationship, props, null), this);
-        });
-      }
     }
   }]);
 
