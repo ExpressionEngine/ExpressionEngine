b77191e21c6fae4f1a46ef7c4153d82131cc044e
Yuri Salimovskiy <yuri@packettide.com>
2022-08-22 18:43:11 +0300

    bring back update file

--- system/ee/installer/updates/ud_7_01_02.php
+++ system/ee/installer/updates/ud_7_01_02.php
@@ -25,7 +25,23 @@ class Updater
      */
     public function do_update()
     {
+        $steps = new \ProgressIterator(
+            [
+                'dropOrphanMemberPivotRecords',
+            ]
+        );
+
+        foreach ($steps as $k => $v) {
+            $this->$v();
+        }
+
         return true;
+    }
+    
+    private function dropOrphanMemberPivotRecords()
+    {
+        ee('db')->query('DELETE FROM exp_members_roles WHERE member_id NOT IN (SELECT member_id FROM exp_members)');
+        ee('db')->query('DELETE FROM exp_members_role_groups WHERE member_id NOT IN (SELECT member_id FROM exp_members)');
     }
 }
 
