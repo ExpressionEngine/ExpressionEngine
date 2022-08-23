9ea87e6e490babd09a70f4cada0b9f70c53aab2d
Yuri Salimovskiy <yuri@packettide.com>
2022-08-23 15:08:55 +0300

    Resolved #2116 where disabling MFA globally could cause redirect loop on login for some users
    EECORE-2107

--- system/ee/legacy/controllers/cp/login.php
+++ system/ee/legacy/controllers/cp/login.php
@@ -73,7 +73,8 @@ class Login extends CP_Controller
             $member = ee('Model')->get('Member', ee()->session->userdata('member_id'))->first();
             $return_path = $member->getCPHomepageURL();
         }
-        if (!IS_PRO || !ee('pro:Access')->hasValidLicense() || (ee()->config->item('enable_mfa') !== false && ee()->config->item('enable_mfa') !== 'y') || ee()->session->userdata('mfa_flag') == 'skip') {
+
+        if (!IS_PRO || (ee()->config->item('enable_mfa') !== false && ee()->config->item('enable_mfa') !== 'y') || ee()->session->userdata('mfa_flag') == 'skip') {
             $return_path = $return_path . (ee()->input->get_post('after') ? '&after=' . ee()->input->get_post('after') : '');
 
             // If there is a URL= parameter in the return URL folks could end up anywhere
