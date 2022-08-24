b90018d5d3821e0a41f48c4cacc433860fcdc574
Yulya Lebed <yulya@packettide.com>
2022-08-24 10:28:09 +0200

    added confirm_exit modal to prevent close the relasionship modal window if it has a data

--- themes/ee/asset/javascript/src/cp/modal_form.js
+++ themes/ee/asset/javascript/src/cp/modal_form.js
@@ -171,12 +171,27 @@ EE.cp.ModalForm = {
 
 		var that = this
 		$(iframe).contents().find('body').on('click', '.js-modal-close', function(e) {
-			that.modal.trigger('modal:close')
+			if (sessionStorage.getItem("preventNavigateAway") == 'true') {
+				isNavigatingAway = confirm(EE.lang.confirm_exit);
+				if (isNavigatingAway) {
+					that.modal.trigger('modal:close')
+				}
+			} else {
+				that.modal.trigger('modal:close')
+			}
 			e.preventDefault();
 		})
 		$(iframe).contents().find('body').on('keydown', function(e) {
 			if (e.keyCode === 27) {
-				that.modal.trigger('modal:close')
+				if (sessionStorage.getItem("preventNavigateAway") == 'true') {
+					isNavigatingAway = confirm(EE.lang.confirm_exit);
+					if (isNavigatingAway) {
+						that.modal.trigger('modal:close')
+					}
+				} else {
+					that.modal.trigger('modal:close')
+				}
+				e.preventDefault();
 			}
 		});
 	}
