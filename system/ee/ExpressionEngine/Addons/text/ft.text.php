e5135aaa794ddc18b18e2d753608ca89c83c2592
Yulya Lebed <yulya@packettide.com>
2022-09-15 13:48:26 +0200

    fixed JavaScript error FileField is not defined  when uploading a file

--- system/ee/ExpressionEngine/Addons/text/ft.text.php
+++ system/ee/ExpressionEngine/Addons/text/ft.text.php
@@ -150,7 +150,10 @@ class Text_ft extends EE_Fieldtype
                 $vars['fp_url'] = ee('CP/URL')->make($fp->controller, array('directory' => 'all'));
 
                 ee()->cp->add_js_script(array(
-                    'file' => array('fields/textarea/cp'),
+                    'file' => array(
+                        'fields/textarea/cp',
+                        'fields/textarea/textarea'
+                    ),
                     'plugin' => array('ee_txtarea')
                 ));
             }
