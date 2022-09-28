09b2f57712b69305899382a14836b3fb802591cd
Yulya Lebed <yulya@packettide.com>
2022-09-28 16:20:06 +0200

    added a script for manipulating overwrite, skip, edit buttons

--- system/ee/ExpressionEngine/View/portage/conflict.php
+++ system/ee/ExpressionEngine/View/portage/conflict.php
@@ -1,13 +1,23 @@
-<div class="fields-grid-item fields-grid-item---open" data-field-name="">
+<div class="fields-grid-item fields-grid-item---open grid-item-conflict" data-field-name="">
     <input type="hidden" name="<?=$baseKey?>[portage__action]" value="" >
     <div class="fields-grid-tools">
         <a class="fields-grid-tool-overwrite js-grid-tool-overwrite" href="" title="<?=lang('grid_overwright_field')?>"><span class="sr-only">Overwrite row</span></a>
         <a class="fields-grid-tool-edit hidden js-grid-tool-edit" href="" title="<?=lang('grid_edit_field')?>"><span class="sr-only">Edit row</span></a>
         <a class="fields-grid-tool-remove js-grid-tool-remove" href="" title="<?=lang('grid_skip_field')?>"><span class="sr-only">Skip row</span></a>
     </div>
+
     <div class="toggle-content">
         <div class="fields-grid-common">
             <?=$this->embed('_shared/form/section', ['name' => $name, 'settings' => $fields])?>
         </div>
     </div>
+</div>
+<div class="alert alert--warning" style="display: none">
+    <div class="alert__icon"><i class="fal fa-exclamation-circle fa-fw"></i></div>
+    <div class="alert__content">
+        <p class="alert__title"></p>
+    </div>
+    <a href="" class="alert__close">
+        <i class="fal fa-times alert__close-icon"></i>
+    </a>
 </div>
--- themes/ee/asset/javascript/src/cp/portage.js
+++ themes/ee/asset/javascript/src/cp/portage.js
@@ -12,7 +12,40 @@
 "use strict";
 
 $(document).ready(function() {
-    
+    $('body').on('click', '.js-grid-tool-overwrite', function(e){
+        e.preventDefault();
+        var parent = $(this).parents('.grid-item-conflict');
+        var alert = parent.next();
+        parent.find('> input[type=hidden]').val('overwrite');
+        parent.addClass('grid-item--collapsed');
+        parent.find('.toggle-content input[type=text]').prop('disabled', true);
+        alert.find('.alert__title').text('The existing data will be overwritten.');
+        alert.css('display', 'flex');
+    })
+
+    $('body').on('click', '.js-grid-tool-remove', function(e){
+        e.preventDefault();
+        var parent = $(this).parents('.grid-item-conflict');
+        var alert = parent.next();
+        parent.find('> input[type=hidden]').val('skip');
+        parent.addClass('grid-item--collapsed');
+        parent.find('.toggle-content input[type=text]').prop('disabled', true);
+        parent.css('opacity', '50%');
+        alert.find('.alert__title').text('Data will be deleted from portage ');
+        alert.css('display', 'flex');
+    })
+
+    $('body').on('click', '.js-grid-tool-edit', function(e){
+        e.preventDefault();
+        var parent = $(this).parents('.grid-item-conflict');
+        var alert = parent.next();
+        parent.find('> input[type=hidden]').val('');
+        parent.removeClass('grid-item--collapsed');
+        parent.css('opacity', '100%');
+        parent.find('.toggle-content input[type=text]').prop('disabled', false);
+        alert.css('display', 'none');
+        alert.find('.alert__title').empty();
+    })
 });
 
 })(jQuery);
