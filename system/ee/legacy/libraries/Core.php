188480432b903cf572f6c3658b4c35cd52f5ea51
Yuri Salimovskiy <yuri@packettide.com>
2022-11-21 10:29:11 +0200

    Resolved #2107 where Cookie Consents were not fully installed when upading from EE2
    Update EE to 7.2.1

--- system/ee/legacy/libraries/Core.php
+++ system/ee/legacy/libraries/Core.php
@@ -74,8 +74,8 @@ class EE_Core
 
         // application constants
         define('APP_NAME', 'ExpressionEngine');
-        define('APP_BUILD', '20221021');
-        define('APP_VER', '7.2.0');
+        define('APP_BUILD', '20221121');
+        define('APP_VER', '7.2.1');
         define('APP_VER_ID', '');
         define('SLASH', '&#47;');
         define('LD', '{');
