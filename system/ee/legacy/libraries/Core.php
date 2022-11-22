2294f6f34c80cca27fe93e041a9460da23928f92
Andy McCormick <andy@packettide.com>
2022-11-22 15:39:39 -0500

    git commit -m"7.2.2 bump"

--- system/ee/legacy/libraries/Core.php
+++ system/ee/legacy/libraries/Core.php
@@ -74,8 +74,8 @@ class EE_Core
 
         // application constants
         define('APP_NAME', 'ExpressionEngine');
-        define('APP_BUILD', '20221121');
-        define('APP_VER', '7.2.1');
+        define('APP_BUILD', '20221122');
+        define('APP_VER', '7.2.2');
         define('APP_VER_ID', '');
         define('SLASH', '&#47;');
         define('LD', '{');
