d7d03cf90c8c3d97cd12112dea16acea6925c6b1
Matt Johnson <matt@packettide.com>
2022-12-07 13:01:05 -0700

    Clean up make addon command

--- system/ee/ExpressionEngine/Cli/Commands/CommandMakeAddon.php
+++ system/ee/ExpressionEngine/Cli/Commands/CommandMakeAddon.php
@@ -87,82 +87,12 @@ class CommandMakeAddon extends Cli
             "Add-on " . lang('command_make_addon_author_url_question')
         );
 
-        // // If they passed the settings flag, always take that
-        // if ($this->option('--has-settings')) {
-        //     $this->data['has_settings'] = $this->option('--has-settings');
-        // } elseif ($this->data['type'] == 'plugin') {
-        //     // Default to no if it's a plugin
-        //     $this->data['has_settings'] = 'no';
-        // } else {
-        //     // Ask if not passed and not a plugin
-        //     $this->data['has_settings'] = $this->confirm(lang('command_make_addon_does_your') . "{$this->type['slug']} " . lang('command_make_addon_have_settings_question'));
-        // }
-
-        // $this->getTypeSpecificData();
-        // $this->getAdvancedSettings();
-
         $this->info('command_make_addon_lets_build');
 
         $this->build();
 
         $this->info('command_make_addon_created_successfully');
     }
-
-    // private function getTypeSpecificData()
-    // {
-    //     // Extension specific options
-    //     if ($this->type['slug'] == 'extension') {
-    //         // No hooks were passed, so we're giving info on the hooks
-    //         if (! $this->option('--hooks')) {
-    //             $this->info('command_make_addon_what_hooks_to_use');
-    //         }
-
-    //         // Set or ask for what hooks to use
-    //         $this->data['hooks'] = $this->getOptionOrAsk(
-    //             '--hooks',
-    //             'command_make_addon_ext_hooks',
-    //             'example_hook'
-    //         );
-    //     }
-
-    //     // Fieldtype specific options
-    //     if ($this->type['slug'] == 'fieldtype') {
-    //         $this->data['compatibility'] = $this->ask('command_make_addon_ft_compatibility');
-    //     }
-    // }
-
-    // private function getAdvancedSettings()
-    // {
-    //     if ($this->option('--typography')) {
-    //         $this->data['typography'] = $this->option('--typography');
-    //     }
-    //     if ($this->option('--services')) {
-    //         $this->data['services'] = $this->option('--services');
-    //     }
-    //     if ($this->option('--models')) {
-    //         $this->data['models'] = $this->option('--models');
-    //     }
-    //     if ($this->option('--consents')) {
-    //         $this->data['consents'] = $this->option('--consents');
-    //     }
-    //     if ($this->option('--cookies')) {
-    //         $cookies = [];
-
-    //         foreach ($this->option('--cookies') as $cookie) {
-    //             if (strpos($cookie, ':') === false) {
-    //                 continue;
-    //             }
-
-    //             $cookieSplit = explode(':', $cookie);
-    //             $cookies[$cookieSplit[0]] = $cookieSplit[1];
-    //         }
-
-    //         $this->data['cookies'] = $cookies;
-    //     }
-
-    //     // Set the compatibilty mode
-    //     $this->data['compatibility-mode'] = (bool) $this->option('--compatibility-mode');
-    // }
 
     private function build()
     {
@@ -175,22 +105,6 @@ class CommandMakeAddon extends Cli
             $this->fail($e->getMessage());
         }
     }
-
-    // private function getType()
-    // {
-    //     $type = $this->getTypeFromOptions() ?: $this->ask(lang('command_make_addon_what_type_of_addon') . ' [' . implode(', ', $this->types) . ']');
-
-    //     if (! in_array($type, $this->types)) {
-    //         $this->fail('command_make_addon_select_proper_addon');
-    //     }
-
-    //     $this->type = [
-    //         'name' => ucfirst($type),
-    //         'slug' => $type,
-    //     ];
-
-    //     return $type;
-    // }
 
     private function getName()
     {
@@ -211,15 +125,4 @@ class CommandMakeAddon extends Cli
 
         return $name;
     }
-
-    // private function getTypeFromOptions()
-    // {
-    //     foreach ($this->types as $type) {
-    //         if ($this->option('--' . $type)) {
-    //             return $type;
-    //         }
-    //     }
-
-    //     return null;
-    // }
 }
