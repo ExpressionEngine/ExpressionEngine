9fa88d43da0fbdef92e5e3ad6cc8e117ca8fe472
Matt Johnson <matt@packettide.com>
2022-12-12 16:55:01 -0500

    Bump version

--- system/ee/legacy/libraries/Core.php
+++ system/ee/legacy/libraries/Core.php
@@ -68,8 +68,8 @@ class EE_Core
 
         // application constants
         define('APP_NAME', 'ExpressionEngine');
-        define('APP_BUILD', '20221201');
-        define('APP_VER', '6.4.2');
+        define('APP_BUILD', '20221212');
+        define('APP_VER', '6.4.3');
         define('APP_VER_ID', '');
         define('SLASH', '&#47;');
         define('LD', '{');
