9baecf9343aefb34a0ad56fa1df51ae59e676542
Jonathan Hardisty <hardisty.jc@gmail.com>
2022-12-21 15:58:36 -0600

    Update mod.member_auth.php
    Moves the reduce_double_slashes($reset_url) function down a line so it picks up from both branches of the if-statement

--- system/ee/ExpressionEngine/Addons/member/mod.member_auth.php
+++ system/ee/ExpressionEngine/Addons/member/mod.member_auth.php
@@ -768,11 +768,11 @@ class Member_auth extends Member
                 $reset_url = ee()->functions->fetch_site_index(0, 0) . '/' . $reset_url;
             }
         } else {
-            $reset_url = reduce_double_slashes(ee()->functions->fetch_site_index(0, 0) . '/' . ee()->config->item('profile_trigger') . '/reset_password');
+            $reset_url = ee()->functions->fetch_site_index(0, 0) . '/' . ee()->config->item('profile_trigger') . '/reset_password';
         }
 
         // Add the reset code and possible forum_id to the reset pass url.
-        $reset_url .= '?id=' . $resetcode . $forum_id;
+        $reset_url = reduce_double_slashes($reset_url . '?id=' . $resetcode . $forum_id);
 
         if (! empty($protected['email_template'])) {
             $email_template = ee()->TMPL->fetch_template_and_parse_from_path($protected['email_template']);
