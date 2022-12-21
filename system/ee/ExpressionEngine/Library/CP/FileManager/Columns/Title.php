e450d9a53637cecf2a62498b63256c12c1d923b7
Yuri Salimovskiy <yuri@packettide.com>
2022-12-21 12:38:13 +0200

    update file title column definition

--- system/ee/ExpressionEngine/Library/CP/FileManager/Columns/Title.php
+++ system/ee/ExpressionEngine/Library/CP/FileManager/Columns/Title.php
@@ -17,7 +17,7 @@ use ExpressionEngine\Library\CP\EntryManager;
  */
 class Title extends EntryManager\Columns\Title
 {
-    public function renderTableCell($data, $field_id, $file, $viewtype = 'list')
+    public function renderTableCell($data, $field_id, $entry, $viewtype = 'list', $pickerMode = false, $addQueryString = [])
     {
         $title = $file->title;
 
