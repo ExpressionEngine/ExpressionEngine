2c20dd4fdd49fad1c75b1961aacc424681341518
Yuri Salimovskiy <yuri@packettide.com>
2023-01-27 16:30:17 +0200

    bump version to 6.4.5

--- system/ee/legacy/libraries/Core.php
+++ system/ee/legacy/libraries/Core.php
@@ -74,8 +74,8 @@ class EE_Core
 
         // application constants
         define('APP_NAME', 'ExpressionEngine');
-        define('APP_BUILD', '20230105');
-        define('APP_VER', '6.4.4');
+        define('APP_BUILD', '20230127');
+        define('APP_VER', '6.4.5');
         define('APP_VER_ID', '');
         define('SLASH', '&#47;');
         define('LD', '{');
