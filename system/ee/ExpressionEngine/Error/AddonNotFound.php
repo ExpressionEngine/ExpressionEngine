ff3d98ca89b23c0c60dc33fead7842f8bf7ca483
Yuri Salimovskiy <yuri@packettide.com>
2023-01-30 16:24:03 +0200

    Resolved #2787 where missing fieldtype would prevent adding new field or entry

---
+++ system/ee/ExpressionEngine/Error/AddonNotFound.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This source file is part of the open source project
+ * ExpressionEngine (https://expressionengine.com)
+ *
+ * @link      https://expressionengine.com/
+ * @copyright Copyright (c) 2003-2023, Packet Tide, LLC (https://www.packettide.com)
+ * @license   https://expressionengine.com/license Licensed under Apache License, Version 2.0
+ */
+
+namespace ExpressionEngine\Error;
+
+/**
+ * AddonNotFound Error
+ */
+class AddonNotFound extends \Exception
+{
+}
+
+// EOF
