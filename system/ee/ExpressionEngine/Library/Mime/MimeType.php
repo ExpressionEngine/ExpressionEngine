ff9083299bf7257346105721aa7f0255c731f5b8
Yuri Salimovskiy <yuri@packettide.com>
2023-02-01 14:10:09 +0200

    Resolved #2808 where some servers did not accept SVG files to be uploaded into image-only directory

--- system/ee/ExpressionEngine/Library/Mime/MimeType.php
+++ system/ee/ExpressionEngine/Library/Mime/MimeType.php
@@ -193,9 +193,9 @@ class MimeType
             return false;
         }
 
-        if ($mime == 'image/svg+xml') {
+        if (strpos($mime, 'image/svg') === 0) {
             $file = file_get_contents($path);
-            if ((strpos($file, '<?xml') !==0 && strpos($file, '<svg') !== 0) || strpos($file, '<svg') === false) {
+            if ((strpos($file, '<?xml') !== 0 && strpos($file, '<svg') !== 0) || strpos($file, '<svg') === false) {
                 return false;
             }
             return true;
