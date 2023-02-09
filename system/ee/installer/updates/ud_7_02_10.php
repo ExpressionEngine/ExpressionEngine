4486d4bca25dcb03d3be61b4fcd0ecc9139fa5aa
Bryan Nielsen <bryan@packettide.com>
2023-02-09 13:16:02 -0500

    Resolved missing index, cp member count speed
    Fixes #2481

---
+++ system/ee/installer/updates/ud_7_02_10.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * This source file is part of the open source project
+ * ExpressionEngine (https://expressionengine.com)
+ *
+ * @link      https://expressionengine.com/
+ * @copyright Copyright (c) 2003-2023, Packet Tide, LLC (https://www.packettide.com)
+ * @license   https://expressionengine.com/license Licensed under Apache License, Version 2.0
+ */
+
+namespace ExpressionEngine\Updater\Version_7_2_10;
+
+/**
+ * Update
+ */
+class Updater
+{
+    public $version_suffix = '';
+
+    /**
+     * Do Update
+     *
+     * @return TRUE
+     */
+    public function do_update()
+    {
+        // Add key for roles_role_groups.group_id
+        ee()->smartforge->add_key('roles_role_groups', 'group_id', 'group_id_idx');
+    }
+}
+
+// EOF
