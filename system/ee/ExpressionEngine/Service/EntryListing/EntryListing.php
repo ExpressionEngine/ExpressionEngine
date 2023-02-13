23d0c445dcbfd4f98ad02de963b0bbdb4d99e3b0
robinsowell <robin.sowell@expressionengine.com>
2023-02-13 18:13:45 -0500

    forgot to pull username for author filter conditional

--- system/ee/ExpressionEngine/Service/EntryListing/EntryListing.php
+++ system/ee/ExpressionEngine/Service/EntryListing/EntryListing.php
@@ -372,7 +372,7 @@ class EntryListing
     private function createAuthorFilter($channel_id = null)
     {
         $db = ee('db')->distinct()
-            ->select('t.author_id, m.screen_name')
+            ->select('t.author_id, m.screen_name, m.username')
             ->from('channel_titles t')
             ->join('members m', 'm.member_id = t.author_id', 'LEFT')
             ->order_by('screen_name', 'asc');
