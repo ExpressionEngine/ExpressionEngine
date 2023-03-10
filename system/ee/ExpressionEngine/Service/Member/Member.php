9c3efd6d7092893611d614001db959f2759b0f4e
Yuri Salimovskiy <yuri@packettide.com>
2023-03-10 12:42:00 +0200

    Resolved #3093 where deprecation message could be shown when editing member using PHP 8.1

--- system/ee/ExpressionEngine/Service/Member/Member.php
+++ system/ee/ExpressionEngine/Service/Member/Member.php
@@ -86,6 +86,9 @@ class Member
     public function calculatePasswordComplexity($password)
     {
         $rank = 0;
+        if (empty($password)) {
+            return $rank;
+        }
         $length = strlen($password);
         $charsCount = [
             'upper' => 0,
