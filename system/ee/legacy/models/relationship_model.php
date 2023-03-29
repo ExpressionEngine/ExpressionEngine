5425af34bf59e51576720eead2263e9049e9101b
Yuri Salimovskiy <yuri@packettide.com>
2023-03-29 10:18:49 +0300

    fix LivePreview for relationships in Grid

--- system/ee/legacy/models/relationship_model.php
+++ system/ee/legacy/models/relationship_model.php
@@ -243,7 +243,7 @@ class Relationship_model extends CI_Model
     {
         if ($fluid_field_data_id) {
             list($fluid_field, $sub_field_id) = explode(',', $fluid_field_data_id);
-            $data = $data[$fluid_field]['fields'][$sub_field_id];
+            $data = reset($data[$fluid_field]['fields'][$sub_field_id]);
         }
 
         foreach ($grid_field_ids as $field_id) {
