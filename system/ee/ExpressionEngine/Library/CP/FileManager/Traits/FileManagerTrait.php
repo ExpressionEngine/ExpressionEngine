2e7c235b2b60eea8652269d259d63797b9be91f1
Yuri Salimovskiy <yuri@packettide.com>
2023-03-31 11:02:37 +0300

    Resolved #3027 where upload directories with angle brackets were not displayed correctly in file manager filters

--- system/ee/ExpressionEngine/Library/CP/FileManager/Traits/FileManagerTrait.php
+++ system/ee/ExpressionEngine/Library/CP/FileManager/Traits/FileManagerTrait.php
@@ -15,7 +15,6 @@ use ExpressionEngine\Library\CP\FileManager\ColumnFactory;
 
 trait FileManagerTrait
 {
-    
     protected function listingsPage($uploadLocation = null, $view_type = 'list', $filepickerMode = false)
     {
         $vars = array();
@@ -85,7 +84,7 @@ trait FileManagerTrait
                 $vars['breadcrumbs'] = array_merge([$base_url->compile() => $uploadLocation->name], array_reverse($breadcrumbs));
                 $base_url->setQueryStringVariable('directory_id', (int) ee('Request')->get('directory_id'));
             }
-        } else if (bool_config_item('file_manager_compatibility_mode')) {
+        } elseif (bool_config_item('file_manager_compatibility_mode')) {
             $files->filter('directory_id', 0);
         }
 
@@ -259,7 +258,7 @@ trait FileManagerTrait
         }
 
         $sort_field = ($sort_col == 'date_added') ? 'upload_date' : $columns[$sort_col]->getEntryManagerColumnSortField();
-        $preselectedFileId =ee()->session->flashdata('file_id');
+        $preselectedFileId = ee()->session->flashdata('file_id');
 
         if ($preselectedFileId) {
             $files = $files->order('FIELD( file_id, ' . $preselectedFileId . ' )', 'DESC', false);
@@ -312,7 +311,7 @@ trait FileManagerTrait
             ];
 
             if ($file->isDirectory()) {
-                $attrs['file_upload_id'] = $file->upload_location_id.'.'.$file->file_id;
+                $attrs['file_upload_id'] = $file->upload_location_id . '.' . $file->file_id;
             }
 
             if (! $file->exists()) {
@@ -386,7 +385,7 @@ trait FileManagerTrait
             'lang.remove_confirm' => lang('file') . ': <b>### ' . lang('files') . '</b>',
             'viewManager.saveDefaultUrl' => ee('CP/URL')->make('files/views/save-default', ['upload_id' => $upload_location_id, 'viewtype' => $view_type])->compile()
         ]);
-        
+
         ee()->cp->add_js_script(array(
             'file' => array(
                 'cp/confirm_remove',
@@ -414,7 +413,7 @@ trait FileManagerTrait
             if ($destination->memberHasAccess(ee()->session->getMember()) === false) {
                 continue;
             }
-            $options[$destination->getId()] = htmlspecialchars($destination->name, ENT_QUOTES, 'UTF-8');
+            $options[$destination->getId()] = $destination->name;
         }
 
         $filter = ee('CP/Filter')->make('requested_directory', lang('upload_location'), $options);
