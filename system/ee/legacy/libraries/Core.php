a7df2314c7822688ab5e057f6f5bf180c82ccd65
Yuri Salimovskiy <yuri@packettide.com>
2023-04-14 08:46:46 +0300

    Set version to 6.4.11

--- system/ee/legacy/libraries/Core.php
+++ system/ee/legacy/libraries/Core.php
@@ -74,8 +74,8 @@ class EE_Core
 
         // application constants
         define('APP_NAME', 'ExpressionEngine');
-        define('APP_BUILD', '20230406');
-        define('APP_VER', '6.4.10');
+        define('APP_BUILD', '20230414');
+        define('APP_VER', '6.4.11');
         define('APP_VER_ID', '');
         define('SLASH', '&#47;');
         define('LD', '{');
