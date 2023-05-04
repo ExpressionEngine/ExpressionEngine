53c03a069d9d6cb577ef1434e008dab051978bf8
Yuri Salimovskiy <yuri@packettide.com>
2023-05-04 10:40:28 +0300

    Added validation for image manipulation names to not be numeric; #2884

---
+++ system/ee/ExpressionEngine/Service/Validation/Rule/NotNumeric.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This source file is part of the open source project
+ * ExpressionEngine (https://expressionengine.com)
+ *
+ * @link      https://expressionengine.com/
+ * @copyright Copyright (c) 2003-2023, Packet Tide, LLC (https://www.packettide.com)
+ * @license   https://expressionengine.com/license Licensed under Apache License, Version 2.0
+ */
+
+namespace ExpressionEngine\Service\Validation\Rule;
+
+use ExpressionEngine\Service\Validation\ValidationRule;
+
+/**
+ * Not Number Validation Rule
+ */
+class NotNumeric extends ValidationRule
+{
+    public function validate($key, $value)
+    {
+        return ! (bool) preg_match('/^[0-9.-]+$/', (string) $value);
+    }
+
+    public function getLanguageKey()
+    {
+        return 'is_not_numeric';
+    }
+}
