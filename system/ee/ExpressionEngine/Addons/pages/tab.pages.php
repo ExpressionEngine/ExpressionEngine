056eddb533ef31b1cdeb74c79c20e05885135a20
Yuri Salimovskiy <yuri@packettide.com>
2023-05-16 15:21:48 +0300

    Resolved #3402 where PHP 8.1 deprecation noticed were shown when bulk editing entries without Pages URL set but Pages module installed

--- system/ee/ExpressionEngine/Addons/pages/tab.pages.php
+++ system/ee/ExpressionEngine/Addons/pages/tab.pages.php
@@ -238,6 +238,9 @@ class Pages_tab
     private function makeValidURIRule()
     {
         return function ($field, $value) {
+            if (empty($value)) {
+                return true;
+            }
             $c_page_uri = preg_replace(
                 "#[^a-zA-Z0-9_\-/\.]+$#i",
                 '',
@@ -261,6 +264,9 @@ class Pages_tab
     private function makeValidSegmentCountRule()
     {
         return function ($field, $value) {
+            if (empty($value)) {
+                return true;
+            }
             $value_segs = substr_count(trim($value, '/'), '/');
 
             // More than 9 pages URI segs?  goodbye
@@ -281,6 +287,9 @@ class Pages_tab
     private function makeNotDuplicatedRule($entry)
     {
         return function ($field, $value) use ($entry) {
+            if (empty($value)) {
+                return true;
+            }
             $static_pages = ee()->config->item('site_pages');
             $uris = $static_pages[ee()->config->item('site_id')]['uris'];
 
