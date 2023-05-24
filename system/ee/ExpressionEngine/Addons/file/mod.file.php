02d11ce179cb8a6a3d12fe126dce9d5e42f69369
Matt Johnson <moosematt11@gmail.com>
2023-05-24 11:18:48 -0700

    Add enable variable in file module class for php 8.2 compatibility
    Resolves  #3450

--- system/ee/ExpressionEngine/Addons/file/mod.file.php
+++ system/ee/ExpressionEngine/Addons/file/mod.file.php
@@ -18,7 +18,8 @@ class File
     public $categories = array();
     public $catfields = array();
     public $valid_thumbs = array();
-    public $query;	
+    public $enable = array();
+    public $query;
     public $return_data = '';
 
     /**
