8904b6ce97c551cff9ce8966dbbfc6159c5ee152
Tom Jaeger <Tom@PacketTide.com>
2023-06-07 10:51:52 -0400

    Update upgrade_success_banner.php
    Few tweaks

--- system/ee/ExpressionEngine/View/_shared/upgrade_success_banner.php
+++ system/ee/ExpressionEngine/View/_shared/upgrade_success_banner.php
@@ -9,22 +9,22 @@
         <a href="https://expressionengine.com/blog/expressionengine-7.3" class="upgrade-success-banner__blurb" target="_blank">
             <i class="fal fa-newspaper fa-fw"></i>
             <h6>ExpressionEngine 7.3.0</h6>
-            <p>Packed with the features targeted to improve and speed up your content management process. <span class="upgrade-success-banner__blurb-learn">Read the blog post&hellip;</span></p>
+            <p>Packed with features targeted to speed up and improve your content management process. <span class="upgrade-success-banner__blurb-learn">Read the blog post&hellip;</span></p>
         </a>
         <a href="https://docs.expressionengine.com/latest/fieldtypes/fluid.html" class="upgrade-success-banner__blurb" target="_blank">
             <i class="fal fa-rectangles-mixed fa-fw"></i>
             <h6>Field Groups in Fluid</h6>
-            <p>Assign group of fields to Fluid field, operate and display those in template as a single unit of content.</p>
+            <p>Assign field groups to a Fluid field, to easily tie the content admin experience to components in your templates and on your site!</p>
         </a>
         <a href="https://docs.expressionengine.com/latest/templates/variable-modifiers.html" class="upgrade-success-banner__blurb" target="_blank">
             <i class="fal fa-key-skeleton-left-right fa-fw"></i>
             <h6>Chained Variable Modifiers</h6>
-            <p><code>{image:resize:rotate:webp}</code> is now easy. Apply multiple modifiers to fields and variables with a single template tag.</p>
+            <p><code>{image:resize:rotate:webp}</code> is that simple, and right in your template! Apply multiple modifiers to fields, variables and content with a single template tag.</p>
         </a>
         <a href="https://docs.expressionengine.com/latest/cli/intro.html" class="upgrade-success-banner__blurb" target="_blank">
             <i class="fal fa-rectangle-terminal fa-fw"></i>
             <h6>New CLI commands</h6>
-            <p>Manage add-ons, back up the database, change system settings and do other things with Command Line Interface.</p>
+            <p>Manage add-ons, back up the database, change system settings and a bunch of additional commands to help you manage and deploy your ExpressionEngine sites have been added to the Command Line Interface.</p>
         </a>
     </div>
     <a href="<?=ee('CP/URL')->make('homepage/dismiss-banner')->compile();?>" class="banner-dismiss"></a>
