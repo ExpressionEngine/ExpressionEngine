be3c23a72134ae0e529bc367da9eb818e40c5fd3
Matt Johnson <matt@packettide.com>
2023-06-07 10:48:21 -0500

    Update upgrade_success_banner.php

--- system/ee/ExpressionEngine/View/_shared/upgrade_success_banner.php
+++ system/ee/ExpressionEngine/View/_shared/upgrade_success_banner.php
@@ -14,17 +14,17 @@
         <a href="https://docs.expressionengine.com/latest/fieldtypes/fluid.html" class="upgrade-success-banner__blurb" target="_blank">
             <i class="fal fa-rectangles-mixed fa-fw"></i>
             <h6>Field Groups in Fluid</h6>
-            <p>Assign field groups to a Fluid field, to easily tie the content admin experience to components in your templates and on your site!</p>
+            <p>Assign field groups to a fluid field, to easily tie the content admin experience to components in your templates and on your site!</p>
         </a>
         <a href="https://docs.expressionengine.com/latest/templates/variable-modifiers.html" class="upgrade-success-banner__blurb" target="_blank">
             <i class="fal fa-key-skeleton-left-right fa-fw"></i>
             <h6>Chained Variable Modifiers</h6>
-            <p><code>{image:resize:rotate:webp}</code> is that simple, and right in your template! Apply multiple modifiers to fields, variables and content with a single template tag.</p>
+            <p><code>{image:resize:rotate:webp}</code> is that simple, and right in your template! Apply multiple modifiers to fields, variables, and content with a single template tag.</p>
         </a>
         <a href="https://docs.expressionengine.com/latest/cli/intro.html" class="upgrade-success-banner__blurb" target="_blank">
             <i class="fal fa-rectangle-terminal fa-fw"></i>
             <h6>New CLI commands</h6>
-            <p>Manage add-ons, back up the database, change system settings and a bunch of additional commands to help you manage and deploy your ExpressionEngine sites have been added to the Command Line Interface.</p>
+            <p>Manage add-ons, back up the database, change system settings, and a bunch of additional commands to help you manage and deploy your ExpresionEngine sites have been added to the Command Line Interface.</p>
         </a>
     </div>
     <a href="<?=ee('CP/URL')->make('homepage/dismiss-banner')->compile();?>" class="banner-dismiss"></a>
