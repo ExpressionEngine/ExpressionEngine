fda5cc7864f6e82212e06b6a5dcb39b101845c41
Yuri Salimovskiy <yuri@packettide.com>
2023-06-08 18:00:02 +0300

    set version to 7.3.1

--- system/ee/legacy/libraries/Core.php
+++ system/ee/legacy/libraries/Core.php
@@ -74,8 +74,8 @@ class EE_Core
 
         // application constants
         define('APP_NAME', 'ExpressionEngine');
-        define('APP_BUILD', '20230123');
-        define('APP_VER', '7.3.0');
+        define('APP_BUILD', '20230608');
+        define('APP_VER', '7.3.1');
         define('APP_VER_ID', '');
         define('SLASH', '&#47;');
         define('LD', '{');
