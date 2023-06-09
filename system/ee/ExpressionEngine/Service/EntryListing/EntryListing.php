5769fbd2c2560a1ad74ab4c36d2d570de9aded02
Yuri Salimovskiy <yuri@packettide.com>
2023-06-09 14:17:24 +0300

    only fetch each field once

--- system/ee/ExpressionEngine/Service/EntryListing/EntryListing.php
+++ system/ee/ExpressionEngine/Service/EntryListing/EntryListing.php
@@ -175,9 +175,11 @@ class EntryListing
     {
         static $channel = null;
 
-        if (is_null($channel)
+        if (
+            is_null($channel)
             && $this->channel_filter
-            && $this->channel_filter->value()) {
+            && $this->channel_filter->value()
+        ) {
             $channel = ee('Model')->get('Channel', $this->channel_filter->value())
                 ->first();
         }
@@ -285,10 +287,9 @@ class EntryListing
             } else {
                 show_error(lang('unauthorized_access'), 403);
             }
-        }
-        // If we have no selected channel filter, and we are not an admin, we
-        // need to filter via WHERE IN
-        else {
+        } else {
+            // If we have no selected channel filter, and we are not an admin, we
+            // need to filter via WHERE IN
             if (! $this->is_admin) {
                 if (empty($this->allowed_channels)) {
                     show_error(lang('no_channels'));
@@ -350,7 +351,7 @@ class EntryListing
                         break;
                 }
 
-                $entries->search($search_fields, $this->search_value);
+                $entries->search(array_unique($search_fields), $this->search_value);
             }
         }
 
