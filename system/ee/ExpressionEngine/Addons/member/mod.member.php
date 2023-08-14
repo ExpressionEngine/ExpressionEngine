a54c2b54faa6e19ceca17c21382e7eb159f2d54d
Eric Swierczek <eric.swierczek@jamf.com>
2023-08-14 10:16:46 -0500

    Add member:role_groups template tag

--- system/ee/ExpressionEngine/Addons/member/mod.member.php
+++ system/ee/ExpressionEngine/Addons/member/mod.member.php
@@ -2620,6 +2620,42 @@ class Member
     }
 
     /**
+     * Member Role Groups list
+     */
+    public function role_groups()
+    {
+        $member_id = (!ee()->TMPL->fetch_param('member_id')) ? ee()->session->userdata('member_id') : ee()->TMPL->fetch_param('member_id');
+
+        $member = ee('Model')
+            ->get('Member', $member_id)
+            ->with('RoleGroups')
+            ->all()
+            ->first();
+
+        if (!$member) {
+            return ee()->TMPL->no_results();
+        }
+
+        $vars = [];
+        foreach($member->RoleGroups->asArray() as $roleGroup) {
+            if ($roleGroup->group_id === 0 && $roleGroup->name === null) {
+                continue;
+            }
+
+            $vars[] = [
+                'role_group_id' => $roleGroup->group_id,
+                'role_group_name' => $roleGroup->name,
+            ];
+        };
+
+        if (!$vars) {
+            return ee()->TMPL->no_results();
+        }
+
+        return ee()->TMPL->parse_variables(ee()->TMPL->tagdata, $vars);
+    }
+
+    /**
      * Member roles list
      */
     public function roles()
