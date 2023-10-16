8f988c65ea2b7185d8a8e42a890e01d1c865e381
Yuri Salimovskiy <yuri@packettide.com>
2023-10-16 13:38:53 +0300

    update test for Cypress 13

--- tests/cypress/cypress/integration/publish/field_short_names.electron.js
+++ tests/cypress/cypress/integration/publish/field_short_names.electron.js
@@ -45,46 +45,32 @@ context('Display field short names on Publish', {browser: 'electron'}, () => {
       cy.get('fieldset[data-field_id=title] .app-badge').should('exist').should('contain', '{title}')
       // cypress does not have way to test css :hover, so we skip the UI testing here
       cy.get('fieldset[data-field_id=title] .app-badge').trigger('click')
-      cy.window().its('navigator.permissions').invoke('query', { name: 'clipboard-read' }).its('state').then(cy.log) // do this first to avoid error
-      cy.window().its('navigator.clipboard').invoke('readText').then(text => {
-        expect(text).to.eq('{title}')
-      })
-      
+      cy.window().its('navigator.permissions').then((api) => api.query({ name: 'clipboard-read' })).its('state').then(cy.log) // do this first to avoid error
+      cy.window().its('navigator.clipboard').then((clip) => clip.readText()).should('equal', '{title}')
+
       cy.get('fieldset[data-field_id=url_title] .app-badge').should('exist').should('contain', '{url_title}')
       cy.get('fieldset[data-field_id=url_title] .app-badge').trigger('click')
-      cy.window().its('navigator.clipboard').invoke('readText').then(text => {
-        expect(text).to.eq('{url_title}')
-      })
+      cy.window().its('navigator.clipboard').then((clip) => clip.readText()).should('equal', '{url_title}')
 
       cy.get('fieldset[data-field_id=17] .app-badge').should('exist').should('contain', '{rel_item}')
       cy.get('fieldset[data-field_id=17] .app-badge').trigger('click')
-      cy.window().its('navigator.clipboard').invoke('readText').then(text => {
-        expect(text).to.eq('{rel_item}')
-      })
+      cy.window().its('navigator.clipboard').then((clip) => clip.readText()).should('equal', '{rel_item}')
 
       // Grid
       cy.get('[data-field_id=19] .field-instruct .app-badge').should('exist').should('contain', '{stupid_grid}')
       cy.get('[data-field_id=19] .field-instruct .app-badge').trigger('click')
-      cy.window().its('navigator.clipboard').invoke('readText').then(text => {
-        expect(text).to.eq('{stupid_grid}')
-      })
+      cy.window().its('navigator.clipboard').then((clip) => clip.readText()).should('equal', '{stupid_grid}')
       cy.get('[data-field_id=19] .grid-field__table tr th:nth-child(2) .app-badge').should('exist').should('contain', '{stupid_grid:text_one}')
       cy.get('[data-field_id=19] .grid-field__table tr th:nth-child(2) .app-badge').trigger('click')
-      cy.window().its('navigator.clipboard').invoke('readText').then(text => {
-        expect(text).to.eq('{stupid_grid:text_one}')
-      })
+      cy.window().its('navigator.clipboard').then((clip) => clip.readText()).should('equal', '{stupid_grid:text_one}')
       cy.get('[data-field_id=19] .grid-field__table tr th:nth-child(3) .app-badge').should('exist').should('contain', '{stupid_grid:text_two}')
       cy.get('[data-field_id=19] .grid-field__table tr th:nth-child(3) .app-badge').trigger('click')
-      cy.window().its('navigator.clipboard').invoke('readText').then(text => {
-        expect(text).to.eq('{stupid_grid:text_two}')
-      })
+      cy.window().its('navigator.clipboard').then((clip) => clip.readText()).should('equal', '{stupid_grid:text_two}')
 
       // Fluid
       cy.get('fieldset[data-field_id=10] > .field-instruct .app-badge').should('exist').should('contain', '{corpse}')
       cy.get('fieldset[data-field_id=10] > .field-instruct .app-badge').trigger('click')
-      cy.window().its('navigator.clipboard').invoke('readText').then(text => {
-        expect(text).to.eq('{corpse}')
-      })
+      cy.window().its('navigator.clipboard').then((clip) => clip.readText()).should('equal', '{corpse}')
 
       available_fields.forEach(function(field, index) {
         fluid_field.get('actions_menu.fields').eq(index).click()
@@ -93,25 +79,17 @@ context('Display field short names on Publish', {browser: 'electron'}, () => {
 
       cy.get('fieldset[data-field_id=10] [data-field-name=rel_item]:visible .app-badge').should('exist').should('contain', '{corpse:rel_item}')
       cy.get('fieldset[data-field_id=10] [data-field-name=rel_item]:visible .app-badge').trigger('click')
-      cy.window().its('navigator.clipboard').invoke('readText').then(text => {
-        expect(text).to.eq('{corpse:rel_item}')
-      })
+      cy.window().its('navigator.clipboard').then((clip) => clip.readText()).should('equal', '{corpse:rel_item}')
 
       cy.get('fieldset[data-field_id=10] [data-field-name=stupid_grid]:visible .field-instruct .app-badge').should('exist').should('contain', '{corpse:stupid_grid}')
       cy.get('fieldset[data-field_id=10] [data-field-name=stupid_grid]:visible .field-instruct .app-badge').trigger('click')
-      cy.window().its('navigator.clipboard').invoke('readText').then(text => {
-        expect(text).to.eq('{corpse:stupid_grid}')
-      })
+      cy.window().its('navigator.clipboard').then((clip) => clip.readText()).should('equal', '{corpse:stupid_grid}')
       cy.get('fieldset[data-field_id=10] [data-field-name=stupid_grid]:visible .grid-field__table tr th:nth-child(2) .app-badge').should('exist').should('contain', '{stupid_grid:text_one}')
       cy.get('fieldset[data-field_id=10] [data-field-name=stupid_grid]:visible .grid-field__table tr th:nth-child(2) .app-badge').trigger('click')
-      cy.window().its('navigator.clipboard').invoke('readText').then(text => {
-        expect(text).to.eq('{stupid_grid:text_one}')
-      })
+      cy.window().its('navigator.clipboard').then((clip) => clip.readText()).should('equal', '{stupid_grid:text_one}')
       cy.get('fieldset[data-field_id=10] [data-field-name=stupid_grid]:visible .grid-field__table tr th:nth-child(3) .app-badge').should('exist').should('contain', '{stupid_grid:text_two}')
       cy.get('fieldset[data-field_id=10] [data-field-name=stupid_grid]:visible .grid-field__table tr th:nth-child(3) .app-badge').trigger('click')
-      cy.window().its('navigator.clipboard').invoke('readText').then(text => {
-        expect(text).to.eq('{stupid_grid:text_two}')
-      })
+      cy.window().its('navigator.clipboard').then((clip) => clip.readText()).should('equal', '{stupid_grid:text_two}')
     })
 
     it('Field short names not visible when turned off for Role', () => {
