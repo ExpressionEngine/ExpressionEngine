6a8d4b9475688d56b23d6ae1f8680df1c7127496
Yuri Salimovskiy <yuri@packettide.com>
2023-10-24 17:21:07 +0300

    adjust button color

--- system/ee/ExpressionEngine/Addons/file/views/publish.php
+++ system/ee/ExpressionEngine/Addons/file/views/publish.php
@@ -17,7 +17,7 @@
 	<div class="fields-upload-chosen-controls">
 		<div class="fields-upload-tools">
 			<div class="button-group button-group-small">
-				<a href="" class="edit-meta button button--default" title="<?=lang('edit_meta')?>"><i class="fa-light fa-money-check-pen"></i></a>
+				<a href="" class="edit-meta button button--default" title="<?=lang('edit_meta')?>"><i class="fal fa-money-check-pen"></i></a>
 				<?=$fp_edit?>
 				<a href="" class="remove button button--default" title="<?=lang('remove')?>"><i class="fa fa-times"></i></a>
 			</div>
