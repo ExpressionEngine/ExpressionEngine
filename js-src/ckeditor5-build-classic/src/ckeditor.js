dbe01bd52bf3bb3ade66849512b0da8aeaf982fa
Yuri Salimovskiy <yuri@packettide.com>
2023-11-15 15:01:51 +0200

    Update to CKEditor 40

--- js-src/ckeditor5-build-classic/src/ckeditor.js
+++ js-src/ckeditor5-build-classic/src/ckeditor.js
@@ -49,8 +49,8 @@ import { Style } from '@ckeditor/ckeditor5-style';
 import { GeneralHtmlSupport } from '@ckeditor/ckeditor5-html-support';
 import { SourceEditing } from '@ckeditor/ckeditor5-source-editing';
 import { ShowBlocks } from '@ckeditor/ckeditor5-show-blocks';
+import { Mention } from '@ckeditor/ckeditor5-mention';
 
-import Mention from '../plugins/ckeditor5-mention/src/mention';
 import ReadMore from '../plugins/readmore/src/readmore';
 import FileManager from '../plugins/filemanager/src/filemanager';
 import PageLinks from '../plugins/pagelinks/src/pagelinks';
