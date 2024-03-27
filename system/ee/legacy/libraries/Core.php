8100a1fbe8b8d72242bb10ebc9ef79d5241b58ff
Bryan Nielsen <bryan@packettide.com>
2024-03-27 16:00:49 -0400

    Update build date

--- system/ee/legacy/libraries/Core.php
+++ system/ee/legacy/libraries/Core.php
@@ -74,7 +74,7 @@ class EE_Core
 
         // application constants
         define('APP_NAME', 'ExpressionEngine');
-        define('APP_BUILD', '20240315');
+        define('APP_BUILD', '20240401');
         define('APP_VER', '7.4.6');
         define('APP_VER_ID', '');
         define('SLASH', '&#47;');
