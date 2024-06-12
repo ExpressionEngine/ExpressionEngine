25cf7a85cd7b9d33587259db02041ae49beabecd
Matt Johnson <moosematt11@gmail.com>
2024-06-12 06:51:00 -0700

    Cast htmlentities string to a string

--- system/ee/ExpressionEngine/Model/File/File.php
+++ system/ee/ExpressionEngine/Model/File/File.php
@@ -37,7 +37,7 @@ class File extends FileSystemEntity
 
     public function get__title()
     {
-        return \htmlentities($this->getRawProperty('title'), ENT_QUOTES, 'UTF-8');
+        return \htmlentities((string)$this->getRawProperty('title'), ENT_QUOTES, 'UTF-8');
     }
 
     public function get__file_hw_original()
