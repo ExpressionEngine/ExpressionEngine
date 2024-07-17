f12cf7f1d28d94f6eebd2157d1430d85d9d31b0f
Matt Johnson <moosematt11@gmail.com>
2024-07-17 12:19:39 -0800

    Redo edit profile page

--- system/ee/ExpressionEngine/Addons/member/stubs/profile/logout.php
+++ system/ee/ExpressionEngine/Addons/member/stubs/profile/logout.php
@@ -1,4 +1,11 @@
 {layout="<?=$template_group?>/_layout"}
+
+{if logged_out}
+    {redirect="<?=$template_group?>/login"}
+{/if}
+
+<h1>Logout</h1>
+
 {exp:member:logout_form return="<?=$template_group?>/login"}
     <input type="submit" value="Logout">
 {/exp:member:logout_form}
