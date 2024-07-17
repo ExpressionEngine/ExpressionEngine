55c8581c4eed50c021a0039adaa7f101549be4d4
Matt Johnson <moosematt11@gmail.com>
2024-07-17 12:57:37 -0800

    Update formatting

--- system/ee/ExpressionEngine/Addons/member/stubs/profile/search.php
+++ system/ee/ExpressionEngine/Addons/member/stubs/profile/search.php
@@ -1,18 +1,22 @@
 {layout="<?=$template_group?>/_layout"}
+<h1>Search for Members</h1>
 
-{exp:member:member_search return="<?=$template_group?>/index"}
-    <input type="text" name="search_keywords_1" />
-    <select name='search_field_1' class='select' >
-        <option value='screen_name'>Search Field</option>
-        <option value='screen_name'>Screen Name</option>
-        <option value='email'>Email Address</option>
-        {custom_profile_field_options}
-    </select>
+<div class="result">
 
-    <select name='search_group_id' class='select' >
-        {group_id_options}
-    </select>
+    {exp:member:member_search return="<?=$template_group?>/index"}
+        <input type="text" name="search_keywords_1" />
+        <select name='search_field_1' class='select' >
+            <option value='screen_name'>Search Field</option>
+            <option value='screen_name'>Screen Name</option>
+            <option value='email'>Email Address</option>
+            {custom_profile_field_options}
+        </select>
 
-    <div class="itempadbig">&nbsp; <input type='submit' value='search' class='submit' /></div>
+        <select name='search_group_id' class='select' >
+            {group_id_options}
+        </select>
 
-{/exp:member:member_search}
+        <div class="itempadbig">&nbsp; <input type='submit' value='search' class='submit' /></div>
+
+    {/exp:member:member_search}
+</div>
