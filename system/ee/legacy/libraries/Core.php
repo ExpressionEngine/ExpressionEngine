963872e8022efb934f3065a90f540e9dad5d9d60
Yuri Salimovskiy <yuri@packettide.com>
2024-08-18 14:49:53 +0300

    set 7.6.0 as planned release for this feature

--- system/ee/legacy/libraries/Core.php
+++ system/ee/legacy/libraries/Core.php
@@ -74,8 +74,8 @@ class EE_Core
 
         // application constants
         define('APP_NAME', 'ExpressionEngine');
-        define('APP_BUILD', '20240624');
-        define('APP_VER', '7.5.0');
+        define('APP_BUILD', '20240818');
+        define('APP_VER', '7.6.0');
         define('APP_VER_ID', '');
         define('SLASH', '&#47;');
         define('LD', '{');
