417a7fb20a685de2521e8ff88964e47eeb671f7d
Brad Akin <bakin1637@gmail.com>
2024-09-09 13:30:19 -0400

    fix php 8+ warnings when viewing the pro search keyword generated page

--- system/ee/legacy/libraries/Functions.php
+++ system/ee/legacy/libraries/Functions.php
@@ -219,6 +219,10 @@ class EE_Functions
      */
     public function prep_query_string($str)
     {
+        if ($str == null) {
+            $str = '';
+        }
+
         if (stristr($str, '.php') && substr($str, -7) == '/index/') {
             $str = substr($str, 0, -6);
         }
