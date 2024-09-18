5219a10ddd8efb3baeb1843e7420e63e2135ff32
Matt Johnson <moosematt11@gmail.com>
2024-09-18 13:39:54 -0700

    Added getFieldVars to get sub fields in fluid field generator

--- system/ee/ExpressionEngine/Addons/fluid_field/TemplateGenerators/Fluid.php
+++ system/ee/ExpressionEngine/Addons/fluid_field/TemplateGenerators/Fluid.php
@@ -24,24 +24,7 @@ class Fluid extends AbstractFieldTemplateGenerator
         $fields = ee('Model')->get('ChannelField', $this->settings['field_channel_fields'])->order('field_label')->all();
 
         foreach ($fields as $field) {
-            $fieldtypeGenerator = ee('TemplateGenerator')->getFieldtype($field->field_type);
-            $vars['fluidFields'][$field->field_name] = [
-                'field_type' => $field->field_type,
-                'field_name' => 'content',
-                'field_label' => $field->field_label,
-                'stub' => $fieldtypeGenerator['stub'],
-                'docs_url' => $fieldtypeGenerator['docs_url'],
-                'is_tag_pair' => $fieldtypeGenerator['is_tag_pair'],
-            ];
-
-            $generator = $this->makeField($field->field_type, $field, ['field_prefix' => 'content']);
-
-            // if the field has its own generator, instantiate the field and pass to generator
-            if ($generator) {
-                $vars['fluidFields'][$field->field_name] = array_merge(
-                    $vars['fluidFields'][$field->field_name], $generator->getVariables()
-                );
-            }
+            $vars['fluidFields'][$field->field_name] = $this->getFieldVars($field);
         }
 
         // Set the field_channel_field_groups setting to an empty array if it doesn't exist
@@ -57,24 +40,32 @@ class Fluid extends AbstractFieldTemplateGenerator
         foreach ($fieldGroups as $group) {
             $groupFields = [];
             foreach ($group->ChannelFields as $field) {
-                $fieldtypeGenerator = ee('TemplateGenerator')->getFieldtype($field->field_type);
-                $groupFields[$field->field_name] = [
-                    'field_type' => $field->field_type,
-                    'field_name' => 'content',
-                    'field_label' => $field->field_label,
-                    'stub' => $fieldtypeGenerator['stub'],
-                    'docs_url' => $fieldtypeGenerator['docs_url'],
-                    'is_tag_pair' => $fieldtypeGenerator['is_tag_pair'],
-                ];
-
-                $generator = $this->makeField($field->field_type, $field, ['field_prefix' => 'content']);
-
-                // if the field has its own generator, instantiate the field and pass to generator
-                if ($generator) {
-                    $groupFields[$field->field_name] = array_merge($groupFields[$field->field_name], $generator->getVariables());
-                }
+                $groupFields[$field->field_name] = $this->getFieldVars($field);
             }
             $vars['fluidFieldGroups'][$group->short_name] = $groupFields;
+        }
+
+        return $vars;
+    }
+
+    public function getFieldVars($field) {
+        $fieldtypeGenerator = ee('TemplateGenerator')->getFieldtype($field->field_type);
+        $vars = [
+            'field_type' => $field->field_type,
+            'field_name' => 'content',
+            'field_label' => $field->field_label,
+            'stub' => $fieldtypeGenerator['stub'],
+            'docs_url' => $fieldtypeGenerator['docs_url'],
+            'is_tag_pair' => $fieldtypeGenerator['is_tag_pair'],
+        ];
+
+        $generator = $this->makeField($field->field_type, $field, ['field_prefix' => 'content']);
+
+        // if the field has its own generator, instantiate the field and pass to generator
+        if ($generator) {
+            $vars = array_merge(
+                $vars, $generator->getVariables()
+            );
         }
 
         return $vars;
