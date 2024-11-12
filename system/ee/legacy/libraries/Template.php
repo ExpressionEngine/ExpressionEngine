3a20279f70d57fd7ce651273c97eb68747ea801b
Brad Akin <bakin1637@gmail.com>
2024-11-12 11:33:37 -0500

    nullable_type_declaration

--- system/ee/legacy/libraries/Template.php
+++ system/ee/legacy/libraries/Template.php
@@ -930,7 +930,7 @@ class EE_Template
      * @param   array   $layout    {layout tag match information from ``_find_layout``
      * @return  string  Layout with embeded template string
      */
-    protected function process_layout_template($template, array $layout = null)
+    protected function process_layout_template($template, ?array $layout = null)
     {
         if (!isset($layout)) {
             return $template;
