2cb69aeeb9bb4a2f4edafabce2db9289de1a1c6d
Yulya Lebed <yulya@packettide.com>
2024-11-27 11:41:36 +0100

    change code to fix tests

--- themes/ee/asset/javascript/src/cp/form_validation.js
+++ themes/ee/asset/javascript/src/cp/form_validation.js
@@ -194,7 +194,7 @@ EE.cp.formValidation = {
 			{
 				// Add "work" class to make the buttons pulsate
 				$button.addClass('work');
-				$button.prop('disabled', true);
+				$button.addClass('not-click');
 
 				// If the submit was trigger by a button click, disable it to prevent futher clicks
 				$button.each(function(index, el) {
