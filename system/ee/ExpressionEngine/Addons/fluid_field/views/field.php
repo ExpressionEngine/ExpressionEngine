5b467b87261e532a0f9aeaa8d0384d80ca62c7e1
brad <bakin1637@gmail.com>
2025-02-18 10:49:01 -0500

    add the red required symbol for fields in that fluid

--- system/ee/ExpressionEngine/Addons/fluid_field/views/field.php
+++ system/ee/ExpressionEngine/Addons/fluid_field/views/field.php
@@ -1,18 +1,23 @@
 <div class="fluid__item <?php if ($reorderable): ?>fluid__item--reorderable<?php endif ?>" data-field-name="<?=$field_name?>" data-field-type="<?=$field->getType()?>">
     <div class="fluid__item-content">
         <div class="fluid__item-fieldset ">
+            <?php $this->embed('fluid_field:item-tools');
+                $required = $field->isRequired() ? 'fieldset-required' : '';
+            ?>
 
-            <?php $this->embed('fluid_field:item-tools');?>
-
-            <div class="field-instruct">
-                <label>
-                    <i class="icon--reorder reorder"></i>
-                    <?=$field->getItem('field_label')?> <?php if ($show_field_type): ?><span class="faded">(<?=$field->getType()?>)</span><?php endif ?>
-                </label>
-                <?=(isset($field_name_prefix) ? $field->getNameBadge(['prefix' => $field_name_prefix, 'content_type' => 'fluid_field', 'fluid_id' => $fluid_field_id]) : '')?>
-                <em><?=$field->getItem('field_instructions')?></em>
+            <div class= <?=$required?>>
+                <div class="field-instruct">
+                    <label>
+                        <i class="icon--reorder reorder"></i>
+                        <?=$field->getItem('field_label')?>
+                        <?php if ($show_field_type): ?><span class="faded">(<?=$field->getType()?>)</span><?php endif ?>
+                    </label>
+                    <?=(isset($field_name_prefix) ? $field->getNameBadge(['prefix' => $field_name_prefix, 'content_type' => 'fluid_field', 'fluid_id' => $fluid_field_id]) : '')?>
+                    <em><?=$field->getItem('field_instructions')?></em>
+                </div>
             </div>
         </div>
+
 
         <div class="fluid__item-field no-drag <?=($field->getType() == 'grid' || $field->getType() == 'file_grid') ? 'fieldset-faux' : ''?>">
         <?php
