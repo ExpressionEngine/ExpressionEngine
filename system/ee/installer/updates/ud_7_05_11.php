b61c903c5763ad18835d79ca563d90a90861fdc7
robinsowell <robin.sowell@expressionengine.com>
2025-03-11 15:07:24 -0400

    Fix a missing primary key from 6.3.0 update
    exp_field_condition_sets_channel_fields could be missing a primary key if it updated at 6.3.0

--- system/ee/installer/updates/ud_7_05_11.php
+++ system/ee/installer/updates/ud_7_05_11.php
@@ -25,7 +25,41 @@ class Updater
      */
     public function do_update()
     {
+        $steps = new \ProgressIterator(
+            [
+                'updateConditionalSetsCFPrimary',
+
+            ]
+        );
+
+        foreach ($steps as $k => $v) {
+            $this->$v();
+        }
+
         return true;
+    }
+
+    // Add missing primary key which old updates missed
+    private function updateConditionalSetsCFPrimary()
+    {
+        if (! ee()->db->table_exists('field_condition_sets_channel_fields')) {
+		    return;
+		}
+
+		$query = ee()->db->query("SHOW INDEX FROM " . ee()->db->dbprefix . "field_condition_sets_channel_fields WHERE Key_name = 'Primary'");
+
+		if ($query->num_rows() !== 0) {
+		    // Primary key already exists
+			return;
+		}
+
+		// Drop the old non-primary indexes
+		// Smartforge will check that they exist
+		ee()->smartforge->drop_key('field_condition_sets_channel_fields', 'condition_set_id');
+		ee()->smartforge->drop_key('field_condition_sets_channel_fields', 'field_id');
+
+		// Add the combo primary key
+		ee()->smartforge->add_key('field_condition_sets_channel_fields', ['condition_set_id', 'field_id'], 'PRIMARY');
     }
 }
 
