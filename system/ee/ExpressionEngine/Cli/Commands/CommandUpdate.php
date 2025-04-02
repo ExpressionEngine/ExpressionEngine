71e357a106e79f76d014e40e0a6f5a7990022628
Matt Johnson <moosematt11@gmail.com>
2025-04-02 11:29:47 -0700

    Fix typo in get current version logic

--- system/ee/ExpressionEngine/Cli/Commands/CommandUpdate.php
+++ system/ee/ExpressionEngine/Cli/Commands/CommandUpdate.php
@@ -317,7 +317,7 @@ class CommandUpdate extends Cli
         $this->info('command_update_getting_info_from_ee_com');
         ee()->load->library('el_pings');
         $version_file = ee()->el_pings->get_version_info(true);
-        $this->updateType == 'curl';
+        $this->updateType = 'curl';
         $this->updateVersion = $version_file['latest_version'];
     }
 
