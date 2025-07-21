c3615d0343d65612a955b9a1b5f2a02b294386bd
Matt Johnson <moosematt11@gmail.com>
2025-07-21 09:50:18 -0600

    Update the exception thrown

--- system/ee/ExpressionEngine/Tests/ExpressionEngine/Service/Generator/CommandGeneratorTest.php
+++ system/ee/ExpressionEngine/Tests/ExpressionEngine/Service/Generator/CommandGeneratorTest.php
@@ -37,7 +37,7 @@ class CommandGeneratorTest extends TestCase
     public function it_fails_when_addon_doesnt_exist()
     {
         $this->expectException(\Exception::class);
-        $this->expectExceptionMessage('Add-on does not exists: addon_that_doesnt_exist');
+        $this->expectExceptionMessage('cli_error_the_specified_addon_does_not_exist');
 
         // Populate with sample data
         $str = new Str();
