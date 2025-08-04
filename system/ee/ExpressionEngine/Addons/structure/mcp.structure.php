fc403bb0934c1995d35e8a9d5d8391c15e255651
Tom Jaeger <Tom@PacketTide.com>
2025-08-04 17:33:16 -0400

    adding a permissoins check for switching a channel to unlisted

--- system/ee/ExpressionEngine/Addons/structure/mcp.structure.php
+++ system/ee/ExpressionEngine/Addons/structure/mcp.structure.php
@@ -1,4 +1,4 @@
-<?php
+e<?php
 
 if (! defined('BASEPATH')) {
     exit('No direct script access allowed');
@@ -697,6 +697,16 @@ class Structure_mcp
 
     public function delete_channels()
     {
+        // Check if user has admin permission
+        $settings = $this->sql->get_settings();
+        if (!$this->sql->user_access('perm_delete', $settings)) {
+            ee('CP/Alert')->makeInline('Permission Denied')
+            ->asIssue()->withTitle('Permission Denied')
+            ->canClose()->defer();
+ 
+            ee()->functions->redirect($this->base_url);
+        }
+
         $channel_ids = explode(',', ee()->input->get_post('channel_ids'));
 
         // add structure nav history beofre deleting data by channel
