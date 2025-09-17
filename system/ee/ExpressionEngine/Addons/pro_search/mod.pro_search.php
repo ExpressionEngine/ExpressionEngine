d8d793f63c32fff3676fc2dbcd2e4a580eeb1716
Eric Lamb <eric@mithra62.com>
2025-09-17 14:57:44 -0700

    temporarily disables query caching IF there any filters for use during the entry compilation of pro search results

--- system/ee/ExpressionEngine/Addons/pro_search/mod.pro_search.php
+++ system/ee/ExpressionEngine/Addons/pro_search/mod.pro_search.php
@@ -753,6 +753,12 @@ class Pro_search
         ee()->load->library('Pro_search_fields');
         ee()->load->library('Pro_search_filters');
 
+        //disable entry sql caching if using filters
+        $enable_sql_caching = ee()->config->item('enable_sql_caching');
+        if(count(ee()->pro_search_filters->names()) >= 1 && ee()->config->item('enable_sql_caching') == 'y') {
+            ee()->config->config['enable_sql_caching'] = 'n';
+        }
+
         ee()->pro_search_filters->filter();
 
         // --------------------------------------
@@ -894,6 +900,11 @@ class Pro_search
 
         if (ee()->extensions->active_hook('pro_search_channel_entries') === true) {
             $tagdata = ee()->extensions->call('pro_search_channel_entries');
+        }
+
+        //enable anew if needed
+        if($enable_sql_caching != ee()->config->item('enable_sql_caching')) {
+            ee()->config->config['enable_sql_caching'] =  ee()->config->item('changed_enable_sql_caching');
         }
 
         // --------------------------------------
