7445fc9aa02538b378ec6becd736318ee7e92a91
Tom Jaeger <Tom@PacketTide.com>
2025-11-04 15:12:00 -0500

    adding version bump for 7.5.18

--- system/ee/legacy/libraries/Core.php
+++ system/ee/legacy/libraries/Core.php
@@ -74,8 +74,8 @@ class EE_Core
 
         // application constants
         define('APP_NAME', 'ExpressionEngine');
-        define('APP_BUILD', '20251231');
-        define('APP_VER', '7.5.21');
+        define('APP_BUILD', '20251106');
+        define('APP_VER', '7.5.18');
         define('APP_VER_ID', '');
         define('SLASH', '&#47;');
         define('LD', '{');
