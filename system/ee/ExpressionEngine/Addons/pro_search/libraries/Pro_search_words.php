3d62d583b492e7ffc26233e40dce002b9f2af7df
Tom Jaeger <Tom@PacketTide.com>
2025-12-01 18:47:59 -0500

    reverting some changed

--- system/ee/ExpressionEngine/Addons/pro_search/libraries/Pro_search_words.php
+++ system/ee/ExpressionEngine/Addons/pro_search/libraries/Pro_search_words.php
@@ -211,7 +211,7 @@ class Pro_search_words
      */
     public function plural($word)
     {
-        return $this->inflect($word, 'plural');
+        return $this->_inflect($word, 'plural');
     }
 
     /**
@@ -219,7 +219,7 @@ class Pro_search_words
      */
     public function singular($word)
     {
-        return $this->inflect($word, 'singular');
+        return $this->_inflect($word, 'singular');
     }
 
     /**
