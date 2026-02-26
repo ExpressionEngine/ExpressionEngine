96aaf2d5b68e619fd044538d22e954969937f807
TomJaeger <Tom@PacketTide.com>
2026-02-26 13:53:32 -0500

    adding additional tests to the translate service

---
+++ system/ee/ExpressionEngine/Tests/support/translate_function_overrides.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Test-only namespace overrides for Translate controller function calls.
+ */
+
+namespace ExpressionEngine\Controller\Utilities;
+
+class TranslateTestFunctionOverrides
+{
+    /** @var bool|null */
+    public static $isReallyWritable = null;
+
+    /** @var bool */
+    public static $throwOnForceDownload = false;
+
+    /** @var string|null */
+    public static $tempnamPath = null;
+}
+
+if (! function_exists(__NAMESPACE__ . '\is_really_writable')) {
+    function is_really_writable($path)
+    {
+        if (TranslateTestFunctionOverrides::$isReallyWritable !== null) {
+            return TranslateTestFunctionOverrides::$isReallyWritable;
+        }
+
+        return \is_really_writable($path);
+    }
+}
+
+if (! function_exists(__NAMESPACE__ . '\force_download')) {
+    function force_download($filename, $data)
+    {
+        if (TranslateTestFunctionOverrides::$throwOnForceDownload) {
+            throw new \RuntimeException('stop-before-exit');
+        }
+
+        return \force_download($filename, $data);
+    }
+}
+
+if (! function_exists(__NAMESPACE__ . '\tempnam')) {
+    function tempnam($directory, $prefix)
+    {
+        if (TranslateTestFunctionOverrides::$tempnamPath !== null) {
+            return TranslateTestFunctionOverrides::$tempnamPath;
+        }
+
+        return \tempnam($directory, $prefix);
+    }
+}
