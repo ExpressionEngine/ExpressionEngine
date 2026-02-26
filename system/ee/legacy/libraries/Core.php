fb2f25279a841f558d4b77186e3c84575caa62c1
TomJaeger <Tom@PacketTide.com>
2026-02-26 10:36:21 -0500

    updated build date

--- system/ee/legacy/libraries/Core.php
+++ system/ee/legacy/libraries/Core.php
@@ -74,7 +74,7 @@ class EE_Core
 
         // application constants
         define('APP_NAME', 'ExpressionEngine');
-        define('APP_BUILD', '20260224');
+        define('APP_BUILD', '20260226');
         define('APP_VER', '7.5.20');
         define('APP_VER_ID', '');
         define('SLASH', '&#47;');
