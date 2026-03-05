6caa3fe566c46a353e50980cf998d493305a2440
swierczek <ericswierczek@gmail.com>
2026-03-05 12:37:02 -0600

    Update Template.php
    Add the forward url parameter logic to additional places that call redirect()

--- system/ee/legacy/libraries/Template.php
+++ system/ee/legacy/libraries/Template.php
@@ -3006,7 +3006,12 @@ class EE_Template
                     return $this->no_results;
                 }
             } else {
-                ee()->functions->redirect(ee()->functions->create_url(ee()->functions->extract_path("=" . $match[2])));
+                ee()->functions->redirect(
+                    ee()->functions->create_url(ee()->functions->extract_path("=" . $match[2])),
+                    false,
+                    null,
+                    ee()->config->item('redirect_forward_url_parameters') === 'y'
+                );
             }
         }
     }
@@ -3134,6 +3139,7 @@ class EE_Template
                     // If the status code isn't a 3xx redirect code, it will be ignored
                     // by redirect().
                     $status_code = null;
+                    $forward_url_parameters = ee()->config->item('redirect_forward_url_parameters') === 'y';
 
                     if (isset($match[5])) {
                         $status_code = $match[5];
@@ -3141,7 +3147,12 @@ class EE_Template
 
                     // handle full URLs, don't need to prepend site details
                     if (filter_var($match[2], FILTER_VALIDATE_URL)) {
-                        ee()->functions->redirect($match[2], false, $status_code);
+                        ee()->functions->redirect(
+                            $match[2],
+                            false,
+                            $status_code,
+                            $forward_url_parameters
+                        );
                     }
 
                     // Functions::redirect() exits on its own
@@ -3149,7 +3160,7 @@ class EE_Template
                         ee()->functions->create_url(ee()->functions->extract_path("=" . $match['2'])),
                         false,
                         $status_code,
-                        ee()->config->item('redirect_forward_url_parameters') === 'y'
+                        $forward_url_parameters
                     );
                 }
             }
