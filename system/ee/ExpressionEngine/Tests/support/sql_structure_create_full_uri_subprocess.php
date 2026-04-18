b4769637dccaaf1187ef6edb7603b46d3119995c
TomJaeger <Tom@PacketTide.com>
2026-04-18 14:26:42 -0400

    tests(coverage): sql.structure.php pre-refactor tests for create_full_uri

---
+++ system/ee/ExpressionEngine/Tests/support/sql_structure_create_full_uri_subprocess.php
@@ -0,0 +1,107 @@
+<?php
+
+require __DIR__ . '/../bootstrap.php';
+require_once PATH_ADDONS . 'structure/sql.structure.php';
+
+$outputFile = $argv[1] ?? null;
+
+if (! $outputFile) {
+    fwrite(STDERR, "Missing output file path.\n");
+    exit(1);
+}
+
+$target = realpath(PATH_ADDONS . 'structure/sql.structure.php');
+$linesToTrack = [1814, 1816, 1818, 1820];
+$xdebugAvailable = function_exists('xdebug_start_code_coverage') && function_exists('xdebug_get_code_coverage');
+
+if ($xdebugAvailable) {
+    $coverageFlags = 0;
+
+    if (defined('XDEBUG_CC_UNUSED')) {
+        $coverageFlags |= XDEBUG_CC_UNUSED;
+    }
+
+    if (defined('XDEBUG_CC_DEAD_CODE')) {
+        $coverageFlags |= XDEBUG_CC_DEAD_CODE;
+    }
+
+    if (defined('XDEBUG_CC_BRANCH_CHECK')) {
+        $coverageFlags |= XDEBUG_CC_BRANCH_CHECK;
+    }
+
+    if ($coverageFlags > 0) {
+        xdebug_start_code_coverage($coverageFlags);
+    }
+
+    if ($coverageFlags === 0) {
+        xdebug_start_code_coverage();
+    }
+}
+
+$sql = (new ReflectionClass('Sql_structure'))->newInstanceWithoutConstructor();
+$sql->site_id = 1;
+$sql->cache = [];
+
+$happyResult = $sql->create_full_uri('/parent', 'child');
+$preservedParentResult = $sql->create_full_uri('/parent section/', 'child?x=1#frag');
+$zeroResult = $sql->create_full_uri('/parent', '0');
+$rootParentResult = $sql->create_full_uri('/', 'child');
+$rootResult = $sql->create_full_uri('', '@#$%^&*()');
+
+$coverage = [];
+
+if ($xdebugAvailable) {
+    $coverage = xdebug_get_code_coverage();
+}
+
+$fileCoverage = $coverage[$target] ?? ['lines' => [], 'functions' => []];
+$functionCoverage = $fileCoverage['functions']['Sql_structure->create_full_uri'] ?? ['branches' => [], 'paths' => []];
+$coveredLines = [];
+
+foreach ($linesToTrack as $line) {
+    $coveredLines[$line] = (($fileCoverage['lines'][$line] ?? 0) > 0);
+}
+
+$coveredBranches = [];
+$totalBranches = 0;
+
+foreach (($functionCoverage['branches'] ?? []) as $branchId => $branch) {
+    if (count($branch['out_hit'] ?? []) <= 1) {
+        continue;
+    }
+
+    foreach (($branch['out_hit'] ?? []) as $edgeIndex => $hitCount) {
+        $totalBranches++;
+        $coveredBranches[$branchId . ':' . $edgeIndex] = ($hitCount > 0);
+    }
+}
+
+$linePercentage = count($coveredLines) > 0
+    ? count(array_filter($coveredLines)) / count($coveredLines) * 100
+    : 0.0;
+$branchPercentage = $totalBranches > 0
+    ? count(array_filter($coveredBranches)) / $totalBranches * 100
+    : 100.0;
+
+file_put_contents($outputFile, json_encode([
+    'real_module_path' => $target,
+    'happy_result' => $happyResult,
+    'preserved_parent_result' => $preservedParentResult,
+    'zero_result' => $zeroResult,
+    'root_parent_result' => $rootParentResult,
+    'root_result' => $rootResult,
+    'xdebug_available' => $xdebugAvailable,
+    'lines' => array_intersect_key($fileCoverage['lines'] ?? [], array_flip($linesToTrack)),
+    'branches' => $functionCoverage['branches'] ?? [],
+    'paths' => $functionCoverage['paths'] ?? [],
+    'line_percentage' => $linePercentage,
+    'branch_percentage' => $branchPercentage,
+    'covered_lines' => $coveredLines,
+    'covered_branches' => $coveredBranches,
+    'uncovered_lines' => array_values(array_keys(array_filter($coveredLines, function ($covered) {
+        return ! $covered;
+    }))),
+    'uncovered_branches' => array_values(array_keys(array_filter($coveredBranches, function ($covered) {
+        return ! $covered;
+    }))),
+], JSON_PRETTY_PRINT));
