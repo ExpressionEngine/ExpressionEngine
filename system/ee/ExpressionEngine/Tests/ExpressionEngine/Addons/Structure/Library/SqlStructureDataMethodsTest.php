4a1ea4a079dd4f74a1ccfeed32aaa376720fdfa4
TomJaeger <Tom@PacketTide.com>
2026-04-18 01:16:42 -0400

    tests(coverage): sql.structure.php pre-refactor tests for get_channel_type

--- system/ee/ExpressionEngine/Tests/ExpressionEngine/Addons/Structure/Library/SqlStructureDataMethodsTest.php
+++ system/ee/ExpressionEngine/Tests/ExpressionEngine/Addons/Structure/Library/SqlStructureDataMethodsTest.php
@@ -507,6 +507,57 @@ class SqlStructureDataMethodsTest extends TestCase
         $this->assertSame(0, $sql->get_page_count());
     }
 
+    public function testGetChannelTypeReturnsFalseWhenNumericChannelHasNoMatchingStructureChannel()
+    {
+        $captured = (object) ['queries' => []];
+
+        ee()->setMock('db', new class($captured, $this) {
+            private $captured;
+            private $test;
+
+            public function __construct($captured, $test)
+            {
+                $this->captured = $captured;
+                $this->test = $test;
+            }
+
+            public function query($sql)
+            {
+                $this->captured->queries[] = $sql;
+
+                return $this->test->result([], 0);
+            }
+        });
+
+        $sql = $this->makeSql();
+        $sql->site_id = 7;
+
+        $this->assertFalse($sql->get_channel_type('0'));
+        $this->assertSame([
+            "SELECT type FROM exp_structure_channels WHERE channel_id = '0' AND site_id = '7' LIMIT 1",
+        ], $captured->queries);
+    }
+
+    public function testGetChannelTypeReturnsFalseForNonNumericChannelWithoutQueryingDb()
+    {
+        $db = new class {
+            public $queryCalls = 0;
+
+            public function query($sql)
+            {
+                $this->queryCalls++;
+
+                throw new RuntimeException('get_channel_type() should not query the database for non-numeric channel IDs.');
+            }
+        };
+        ee()->setMock('db', $db);
+
+        $sql = $this->makeSql();
+
+        $this->assertFalse($sql->get_channel_type('not-a-channel'));
+        $this->assertSame(0, $db->queryCalls);
+    }
+
     public function testGetStructureChannelsBuildsExpectedQueryForAllOptionalFilters()
     {
         $captured = (object) ['queries' => []];
