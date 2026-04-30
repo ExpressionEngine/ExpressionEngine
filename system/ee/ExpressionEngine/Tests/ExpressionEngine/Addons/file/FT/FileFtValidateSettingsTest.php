c15109ed46f5ac3ad411e880b25dfa34acb0cce3
TomJaeger <Tom@PacketTide.com>
2026-04-29 21:13:43 -0400

    tests(coverage): ft.file.php to 100%

--- system/ee/ExpressionEngine/Tests/ExpressionEngine/Addons/file/FT/FileFtValidateSettingsTest.php
+++ system/ee/ExpressionEngine/Tests/ExpressionEngine/Addons/file/FT/FileFtValidateSettingsTest.php
@@ -44,4 +44,56 @@ class FileFtValidateSettingsTest extends FileFtTestBase
         $this->assertSame('allowedDirectories', $validator->defineRuleCalls[0]['name']);
         $this->assertSame([$fieldtype, '_validate_file_settings'], $validator->defineRuleCalls[0]['callback']);
     }
+
+    /**
+     * Provide representative callback payloads that should be accepted unchanged.
+     *
+     * @return array<string, array{0: mixed, 1: mixed, 2: array<string, mixed>, 3: mixed}>
+     */
+    public function validateFileSettingsProvider()
+    {
+        return [
+            'expected validator payload' => [
+                'allowed_directories',
+                [1, 4],
+                ['field_content_type' => 'image'],
+                (object) ['name' => 'allowedDirectories'],
+            ],
+            'boundary payload' => [
+                '',
+                null,
+                [],
+                null,
+            ],
+        ];
+    }
+
+    /**
+     * Assert _validate_file_settings() accepts the current callback inputs
+     * without invoking additional collaborators or mutating the result.
+     *
+     * @dataProvider validateFileSettingsProvider
+     * @param mixed $key
+     * @param mixed $value
+     * @param array<string, mixed> $params
+     * @param mixed $rule
+     * @return void
+     */
+    public function testValidateFileSettingsAlwaysReturnsTrueAndStaysSideEffectFree($key, $value, array $params, $rule)
+    {
+        $validator = new FileFtValidationValidatorStub((object) ['is_valid' => false]);
+        $validation = $this->setValidationService($validator);
+        $fieldtype = $this->makeFieldtype();
+        $initialLibraries = $this->loadRecorder->libraries;
+        $initialModels = $this->loadRecorder->models;
+        $initialPackagePaths = $this->loadRecorder->packagePaths;
+
+        $this->assertTrue($fieldtype->_validate_file_settings($key, $value, $params, $rule));
+        $this->assertSame([], $validation->makeCalls);
+        $this->assertSame([], $validator->defineRuleCalls);
+        $this->assertSame([], $validator->validateCalls);
+        $this->assertSame($initialLibraries, $this->loadRecorder->libraries);
+        $this->assertSame($initialModels, $this->loadRecorder->models);
+        $this->assertSame($initialPackagePaths, $this->loadRecorder->packagePaths);
+    }
 }
