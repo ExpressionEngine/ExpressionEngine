d308845d724dd170828a2c3e4402e5330284bdd0
Yulya Lebed <yulya@packettide.com>
2026-07-02 09:45:30 +0200

    fixed virtualized React Select issue

--- system/ee/ExpressionEngine/View/_shared/form/fields/select.php
+++ system/ee/ExpressionEngine/View/_shared/form/fields/select.php
@@ -150,9 +150,12 @@ else:
         'noResults' => isset($no_results['text']) ? lang($no_results['text']) : null,
         'splitForTwo' => isset($split_for_two) ? $split_for_two : null,
         'toggles' => isset($toggles) ? $toggles : [],
-        'virtualizationThreshold' => isset($virtualization_threshold) ? $virtualization_threshold : 100,
-        'virtualizationHeight' => isset($virtualization_height) ? $virtualization_height : 400
+        'virtualizationThreshold' => isset($virtualization_threshold) ? $virtualization_threshold : 100
     ];
+
+    if (isset($virtualization_height)) {
+        $component['virtualizationHeight'] = $virtualization_height;
+    }
     ?>
 	<div data-select-react="<?=base64_encode(json_encode($component))?>" data-input-value="<?=$field_name?>" class="<?=$class?>">
 		<div class="fields-select">
