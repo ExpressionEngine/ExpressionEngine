5d0c514cf982541c6bc2ea9797429cf30ae5aeb0
TomJaeger <Tom@PacketTide.com>
2026-09-16 14:44:52 -0400

    Correct shortcut labels in deletion confirmations

--- system/ee/ExpressionEngine/Addons/pro_search/mcp.pro_search.php
+++ system/ee/ExpressionEngine/Addons/pro_search/mcp.pro_search.php
@@ -1749,10 +1749,10 @@ class Pro_search_mcp
     // --------------------------------------------------------------------
 
     /**
-     * List shortcuts for given group
+     * List shortcuts for the given group.
      *
-     * @access      public
-     * @return      string
+     * @param int $group_id
+     * @return string
      */
     public function shortcuts($group_id)
     {
@@ -1824,7 +1824,7 @@ class Pro_search_mcp
                 'name'  => 'shortcut_id[]',
                 'value' => $id,
                 'data'  => array(
-                    'confirm' => $shortcut['shortcut_label']
+                    'confirm' => '<span>' . htmlspecialchars($shortcut['shortcut_label'], ENT_QUOTES, 'UTF-8') . '</span>'
                 )
             );
 
